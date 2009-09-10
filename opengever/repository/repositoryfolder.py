5cb48a8edca1899f005301c4837dc6fd675743ce
Jonas Baumann <j.baumann@4teamwork.ch>
2009-09-10 13:37:40 +0000

    renamed reference number field

--- opengever/repository/repositoryfolder.py
+++ opengever/repository/repositoryfolder.py
@@ -1,6 +1,6 @@
 
 from zope import schema
-from zope.interface import implements
+from zope.interface import implements, invariant, Invalid
 
 from plone.dexterity import content
 from plone.directives import form
@@ -21,21 +21,23 @@ class IRepositoryFolderSchema(metadata.IDublinCore):
             required = True
     )
 
-    record_pos = schema.Int(
-            title = _(u'Record Position'),
+    reference_number = schema.Int(
+            title = _(u'label_reference_number', default=u'Reference'),
+            description = _(u'help_reference_number', default=u''),
             required = False,
+            min = 1,
     )
 
 
-@form.default_value(field=IRepositoryFolderSchema['record_pos'])
-def record_pos_default_value(data):
-    highest_record_pos = -1
+@form.default_value(field=IRepositoryFolderSchema['reference_number'])
+def reference_number_default_value(data):
+    highest_reference_number = 0
     for obj in data.context.listFolderContents():
         if IRepositoryFolder.providedBy(obj):
-            if obj.record_pos > highest_record_pos:
-                highest_record_pos = obj.record_pos
-    highest_record_pos += 1
-    return highest_record_pos
+            if obj.reference_number > highest_reference_number:
+                highest_reference_number = obj.reference_number
+    highest_reference_number += 1
+    return highest_reference_number
 
 
 class RepositoryFolder(content.Container):
@@ -46,8 +48,8 @@ class RepositoryFolder(content.Container):
         title = u' %s' % self.effective_title
         obj = self
         while IRepositoryFolder.providedBy(obj):
-            if hasattr(obj, 'record_pos'):
-                title = unicode(obj.record_pos) + '.' + title
+            if hasattr(obj, 'reference_number'):
+                title = unicode(obj.reference_number) + '.' + title
             obj = obj.aq_inner.aq_parent
         return title
 
