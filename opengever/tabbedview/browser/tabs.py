bc60b51fc4bdd948a376f9b6586d549c2b6c5db9
Victor Baumann <v.baumann@4teamwork.ch>
2009-10-21 15:40:42 +0000

    chances for opengever

---
+++ opengever/tabbedview/browser/tabs.py
@@ -0,0 +1,29 @@
+from ftw.tabbedview.browser.views import BaseListingView
+from ftw.tabbedview.interfaces import ITabbedView
+from five import grok
+from ftw.table import helper
+
+
+class OpengeverTab(grok.View, BaseListingView):
+    grok.context(ITabbedView)
+    grok.template('generic')
+    
+    update = BaseListingView.search
+    
+    columns = (('Title','sortable_title',), 
+               ('modified','modified',helper.readable_modified), 
+               ('Creator','Creator',helper.readable_author),)    
+    
+
+class Documents(OpengeverTab):
+    grok.name('tabbedview_view-documents')
+    
+    types = ['opengever.document.document',]
+    
+class Dossiers(OpengeverTab):
+    grok.name('tabbedview_view-dossiers')
+
+    types = ['opengever.dossier.projectdossier', 'opengever.dossier.businesscasedossier',]
+
+
+        
