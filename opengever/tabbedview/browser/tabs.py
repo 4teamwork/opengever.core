703ba861e0acbdd92248d9e47e07eb9f385f8c96
Victor Baumann <v.baumann@4teamwork.ch>
2009-11-10 16:03:54 +0000

    journatl view

--- opengever/tabbedview/browser/tabs.py
+++ opengever/tabbedview/browser/tabs.py
@@ -40,8 +40,46 @@ class Dossiers(OpengeverListingTab):
     
     types = ['opengever.dossier.projectdossier', 'opengever.dossier.businesscasedossier',]
 
-# class Overview(OpengeverTab):
-#     grok.name('tabbedview_view-overview')
-#     grok.template('overview')
+class Tasks(OpengeverListingTab):
+    grok.name('tabbedview_view-tasks')
+
+    types = ['dummy.task',]
+    
+class Events(OpengeverListingTab):
+    grok.name('tabbedview_view-events')
+
+    types = ['dummy.event',]
+
+from zope.annotation.interfaces import IAnnotations, IAnnotatable
+from ftw.journal.interfaces import IAnnotationsJournalizable, IWorkflowHistoryJournalizable
+from ftw.journal.config import JOURNAL_ENTRIES_ANNOTATIONS_KEY
+from opengever.dossier.behaviors.dossier import IDossierMarker
+from ftw.table.interfaces import ITableGenerator
+from zope.component import queryUtility
+from ftw.table import helper
+         
+class Journal(grok.View, OpengeverTab):
+     grok.context(IDossierMarker)
+     grok.name('tabbedview_view-journal')
+     grok.template('journal')
+     
+     def table(self):
+         generator = queryUtility(ITableGenerator, 'ftw.tablegenerator') 
+         columns = (('title', lambda x,y: x['action']['title']), 
+                    'actor', 
+                    ('time', helper.readable_date),
+                    'comment'
+                    )
+         return generator.generate(reversed(self.data()), columns, css_mapping={'table':'journal-listing'})
+     
+     def data(self):
+         context = self.context
+         history = []
+         
+         if IAnnotationsJournalizable.providedBy(self.context):
+             annotations = IAnnotations(context)
+             return annotations.get(JOURNAL_ENTRIES_ANNOTATIONS_KEY, [])
+         elif IWorkflowHistoryJournalizable.providedBy(self.context):
+             raise NotImplemented
 
         
