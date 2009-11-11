8c671fc4e355463c2fc4b0bff4af1441ef4cd3ad
Florian Sprenger <f.sprenger@4teamwork.ch>
2009-11-11 14:54:17 +0000

    initial version

---
+++ opengever/examplecontent/setuphandlers.py
@@ -0,0 +1,24 @@
+from Products.CMFCore.utils import getToolByName
+
+def setupVarious(context):
+
+    # Ordinarily, GenericSetup handlers check for the existence of XML files.
+    # Here, we are not parsing an XML file, but we use this text file as a
+    # flag to check that we actually meant for this import step to be run.
+    # The file is found in profiles/default.
+    if context.readDataFile('opengever.examplecontent_various.txt') is None:
+        return
+
+    portal = context.getSite()
+    
+    if not portal.get("registraturplan"):
+        portal._importObjectFromFile(context.openDataFile('registraturplan.zexp'))
+
+	# add some default users
+	try:
+	    regtool.addMember("brigitte.schmid", "demo09")
+	    regtool.addMember("olivier.debenat", "demo09")
+	    regtool.addMember("hans.muster", "demo09")
+	    regtool.addMember("hugo.boss", "demo09")
+	except ValueError: #users already exist
+	    pass
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='opengever.examplecontent',
+      version=version,
+      description="Adds example content to an opengever site",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='4teamwork GmbH',
+      author_email='info@4teamwork.ch',
+      url='http://4teamwork.ch',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['opengever'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
