ebc11d4212a4691e4bbbe0f0cbb92eba20abc497
Florian Sprenger <f.sprenger@4teamwork.ch>
2009-11-19 10:43:43 +0000

    https://extranet.4teamwork.ch/projects/opengever-kanton-zug/sprint-backlog/161

--- opengever/examplecontent/setuphandlers.py
+++ opengever/examplecontent/setuphandlers.py
@@ -1,14 +1,35 @@
 from Products.CMFCore.utils import getToolByName
+from random import randint
+from plone.i18n.normalizer import urlnormalizer
+import sys
+
+class UserGenerator(object):
+    def __init__(self, context):
+        surnamesfile = context.openDataFile("surnames.csv")
+        namesfile = context.openDataFile("names.csv")
+        self.fileencoding = "iso-8859-1"
+        self.names, self.surnames = namesfile.readlines(), surnamesfile.readlines()
+        self.names_len, self.surnames_len = len(self.names), len(self.surnames)
+        surnamesfile.close()
+        namesfile.close()
+
+    def getUserData(self):
+        name = unicode(self.names[randint(0, self.names_len - 1)].decode(self.fileencoding).replace("\n", ""))
+        surname = unicode(self.surnames[randint(0, self.surnames_len - 1)].decode(self.fileencoding).replace("\n", ""))
+        return {"name"    : name,
+                "surname" : surname,
+                "email"    : "vorname.nachname@4teamwork.ch",
+                "password": "demo09",
+                "id"      : urlnormalizer.normalize(u"%s.%s" % (name, surname)).replace("-","")}
 
 def setupVarious(context):
-
     # Ordinarily, GenericSetup handlers check for the existence of XML files.
     # Here, we are not parsing an XML file, but we use this text file as a
     # flag to check that we actually meant for this import step to be run.
     # The file is found in profiles/default.
     if context.readDataFile('opengever.examplecontent_various.txt') is None:
         return
-
+        
     portal = context.getSite()
     
     if not portal.get("registraturplan"):
@@ -29,3 +50,17 @@ def setupVarious(context):
 
     arbeitsplatz = portal.get("arbeitsplatz")
     arbeitsplatz.reindexObject()
+
+    ug = UserGenerator(context)
+    for i in range(500):
+        userdata = ug.getUserData()
+        newid = userdata["id"]
+        
+        print >>sys.stdout, 'Creating user %s' % userdata["id"]
+        try:
+            member = regtool.addMember(userdata["id"], userdata["password"], ('Member',), None, properties={"username": userdata["id"], "email": userdata["email"]})
+            member.setMemberProperties({"fullname":"%s %s" % (userdata["name"], userdata["surname"])})
+        except ValueError: # memberid already exists
+            pass
+
+        
