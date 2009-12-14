73c17632e9910abf275cb031db3a1af7d33d349b
Julian Infanger <j.infanger@4teamwork.ch>
2009-12-14 13:51:17 +0000

    289 - Mandanten-ID als Attribut pro OpenGever-Instanz

--- opengever/base/interfaces.py
+++ opengever/base/interfaces.py
@@ -5,3 +5,6 @@ from zope.interface import Interface
 
 class IBaseCustodyPeriods(Interface):    
     custody_periods = schema.List(title=u"custody period", default=['0', '10', '30', '100', '150',])
+
+class IBaseClientID(Interface):
+    client_id = schema.TextLine(title=u"client id", default=u"OG")
