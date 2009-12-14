261dc7ed4ac8959effc5d02db270a6086d289841
Jonas Baumann <j.baumann@4teamwork.ch>
2009-12-14 15:20:02 +0000

    Issue #275: Hinzufügbare Typen einschränken bei Geschäftsdossier

---
+++ opengever/dossier/base.py
@@ -0,0 +1,83 @@
+
+from Acquisition import aq_inner, aq_parent
+from five import grok
+from zope.component import queryMultiAdapter
+from zope.interface import Interface
+
+from plone.dexterity.content import Container
+from plone.dexterity.interfaces import IDexterityFTI
+from Products.CMFPlone.interfaces.siteroot import IPloneSiteRoot
+from Products.CMFDefault.interfaces import ICMFDefaultSkin
+
+from opengever.dossier.interfaces import IConstrainTypeDecider
+from opengever.dossier.behaviors.dossier import IDossierMarker
+
+class DossierContainer( Container ):
+
+    def allowedContentTypes( self, *args, **kwargs ):
+        types = super(DossierContainer, self).allowedContentTypes(*args, **kwargs)
+        # calculate depth
+        depth = 0
+        obj = self
+        while IDossierMarker.providedBy( obj ):
+            depth += 1
+            obj = aq_parent( aq_inner( obj ) )
+            if IPloneSiteRoot.providedBy( obj ):
+                break
+        # the adapter decides
+        def filter_type( fti ):
+            # first we try the more specific one ...
+            decider = queryMultiAdapter( (self.REQUEST, self, fti),
+                                    IConstrainTypeDecider,
+                                    name=fti.portal_type)
+            if not decider:
+                # .. then we try the more general one
+                decider = queryMultiAdapter( (self.REQUEST, self, fti),
+                                        IConstrainTypeDecider)
+            if decider:
+                return decider.addable( depth )
+            # if we don't have an adapter, we just allow it
+            return True
+        # filter
+        return filter( filter_type, types )
+
+
+
+class DefaultConstrainTypeDecider( grok.MultiAdapter ):
+    grok.provides( IConstrainTypeDecider )
+    grok.adapts(ICMFDefaultSkin, IDossierMarker, IDexterityFTI)
+    grok.name( '' )
+
+    CONSTRAIN_CONFIGURATION = {
+        'opengever.dossier.businesscasedossier' : {
+            'opengever.dossier.businesscasedossier' : 3,
+            'opengever.dossier.projectdossier' : 1,
+            },
+        'opengever.dossier.projectdossier' : {
+            'opengever.dossier.projectdossier' : 1,
+            'opengever.dossier.businesscasedossier' : 1,
+            },
+        }
+
+
+    def __init__( self, request, context, fti ):
+        self.context = context
+        self.request = request
+        self.fti = fti
+
+    def addable( self, depth ):
+        container_type = self.context.portal_type
+        factory_type = self.fti.id
+        mapping = self.constrain_type_mapping
+        for const_ctype, const_depth, const_ftype in mapping:
+            if const_ctype==container_type and const_ftype==factory_type:
+                return depth<const_depth or const_depth==0
+        return True
+
+    @property
+    def constrain_type_mapping( self ):
+        conf = DefaultConstrainTypeDecider.CONSTRAIN_CONFIGURATION
+        for container_type, type_constr in conf.items():
+            for factory_type, max_depth in type_constr.items():
+                yield container_type, max_depth, factory_type
+
--- opengever/dossier/interfaces.py
+++ opengever/dossier/interfaces.py
@@ -7,3 +7,27 @@ class IDossierContainerTypes(Interface):
     """A type for collaborative spaces."""
     
     container_types = schema.List(title=u"container_types", default=['Ordner','Schachtel',])
+
+
+class IConstrainTypeDecider(Interface):
+    """ Adapter interface
+    The constrain type decider decides, if a type is addable in the
+    current dossier object. This decision depends on the current dossier
+    type, the type to be added and the depth (distance to the next parent
+    which is not an IDossier).
+    Descriminators: ( request, context, FTI )
+    * context : current dossier object
+    * request
+    * FTI: type to be added
+    As Optional name the portal_type of the FTI can be used. If there is
+    no such an adapter, the more general adapter without a name is used.
+    """
+
+    def __init__( self, context, request, fti ):
+        pass
+
+    def addable( self, depth ):
+        """ Returns True, if a object of type *fti* can be created in the
+        current *context*, depending on the *depth*
+        """
+        pass
