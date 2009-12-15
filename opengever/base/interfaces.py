6f876c322590f125a3efb6677e3ddb76417b0751
Julian Infanger <j.infanger@4teamwork.ch>
2009-12-15 07:14:23 +0000

    changed list items to unicode

--- opengever/base/interfaces.py
+++ opengever/base/interfaces.py
@@ -4,7 +4,7 @@ from zope.interface import Interface
 # -*- extra stuff goes here -*-
 
 class IBaseCustodyPeriods(Interface):    
-    custody_periods = schema.List(title=u"custody period", default=['0', '10', '30', '100', '150',])
+    custody_periods = schema.List(title=u"custody period", default=[u'0', u'10', u'30', u'100', u'150',])
 
 class IBaseClientID(Interface):
     client_id = schema.TextLine(title=u"client id", default=u"OG")
