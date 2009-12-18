99b65900a2f5a18a871da5f2295f9e07cb514764
Jonas Baumann <j.baumann@4teamwork.ch>
2009-12-18 17:03:57 +0000

    added participants tab

--- opengever/tabbedview/browser/tabs.py
+++ opengever/tabbedview/browser/tabs.py
@@ -1,3 +1,4 @@
+import base64
 from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
 from ftw.tabbedview.browser.views import BaseListingView
@@ -101,6 +102,8 @@ from zope.annotation.interfaces import IAnnotations, IAnnotatable
 from ftw.journal.interfaces import IAnnotationsJournalizable, IWorkflowHistoryJournalizable
 from ftw.journal.config import JOURNAL_ENTRIES_ANNOTATIONS_KEY
 from opengever.dossier.behaviors.dossier import IDossierMarker
+from opengever.dossier.behaviors.participation import IParticipationAwareMarker
+from opengever.dossier.behaviors.participation import IParticipationAware
 from opengever.repository.interfaces import IRepositoryFolder
 from ftw.table.interfaces import ITableGenerator
 from zope.component import queryUtility
@@ -251,6 +254,63 @@ class Trash(OpengeverListingTab):
         ('', helper.path_checkbox),
         ('Title', 'sortable_title', helper.linked),
         )
+
+
+class Participants(OpengeverListingTab):
+    """ Participants listing tab for dossiers using the
+    IParticipantsAware behavior
+    """
+    grok.name('tabbedview_view-participants')
+    grok.context(IParticipationAwareMarker)
+
+    def base64_oid_checkbox(item, value):
+        oid = base64.encodestring(item._p_oid)
+        attrs = {
+            'type' : 'checkbox',
+            'class' : 'noborder',
+            'name' : 'oids:list',
+            'id' : 'item-%s' % oid,
+            'value' : oid,
+            }
+        html = '<input %s />' % ' '.join(['%s="%s"' % (k, v)
+                                         for k, v in attrs.items()])
+        return html
+
+    sort_on = 'contact'
+    columns = (
+        ('', base64_oid_checkbox,),
+        ('contact', 'contact',),
+        ('role_list', 'roles',)
+        )
+
+    def update(self):
+        self.pagesize = 20
+        self.pagenumber =  int(self.request.get('pagenumber', 1))
+        self.url = self.context.absolute_url()
+
+        phandler = IParticipationAware(self.context)
+        results = list(phandler.get_participations())
+
+        # XXX implement searching
+        #if self.request.has_key('searchable_text'):
+        #    searchable_text = self.request.get('searchable_text', None)
+        #    if len(searchable_text):
+        #        searchable_text = searchable_text.endswith('*') and searchable_text[:-1] or searchable_text
+        #        filter_condition = lambda p:searchable_text in p.Title()
+        #        results = filter(filter_condition, results)
+
+        if self.sort_on.startswith('header-'):
+            sort_on = self.sort_on = self.sort_on.split('header-')[1]
+        if self.sort_on:
+            sorter = lambda a,b:cmp(getattr(a, sort_on),
+                                    geattr(b, sort_on))
+            results.sort(sorter)
+
+        if self.sort_order=='reverse':
+            results.reverse()
+
+        self.contents = results
+        self.len_results = len(self.contents)
 
 
 from plone.app.workflow.interfaces import ISharingPageRole
