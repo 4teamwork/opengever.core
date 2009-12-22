e21b2a434aefb02ccb873ace3b2d6f128072e46c
Jonas Baumann <j.baumann@4teamwork.ch>
2009-12-22 08:07:54 +0000

    Added dynamic retention period vocabulary ; some refactoring

--- opengever/base/behaviors/lifecycle.py
+++ opengever/base/behaviors/lifecycle.py
@@ -23,18 +23,6 @@ from opengever.base.interfaces import IBaseCustodyPeriods
 from opengever.base.interfaces import IRetentionPeriodRegister
 
 
-@grok.provider(IContextSourceBinder)
-def custody_periods(context):
-    voc = []
-    terms = []
-    registry = queryUtility(IRegistry)
-    proxy = registry.forInterface(IBaseCustodyPeriods)
-    voc = getattr(proxy, 'custody_periods')
-    for term in voc:
-        terms.append(SimpleVocabulary.createTerm(term))
-    return SimpleVocabulary(terms)
-
-
 class ILifeCycleMarker(Interface):
     pass
 
@@ -69,55 +57,12 @@ class ILifeCycle(form.Schema):
     custody_period = schema.Choice(
         title = _(u'label_custody_period', default=u'Custody period (years)'),
         description = _(u'help_custody_period', default=u''),
-        source = custody_periods,
+        source = u'lifecycle_custody_period_vocabulary',
         required = True,
         )
 
 
 alsoProvides(ILifeCycle, IFormFieldProvider)
-
-# RETENTION PERIOD: Vocabulary and default value
-def _get_retention_period_options(vocabulary):
-    registry = zope.component.getUtility(IRegistry)
-    proxy = registry.forInterface(IRetentionPeriodRegister)
-    options = []
-    for num in getattr(proxy, 'retention_period'):
-        num = int(num)
-        options.append((num, num))
-    return options
-grok.global_utility(utils.create_restricted_vocabulary(
-        ILifeCycle['retention_period'],
-        _get_retention_period_options,
-        message_factory=_),
-                    provides=schema.interfaces.IVocabularyFactory,
-                    name=u'lifecycle_retention_period_vocabulary')
-
-
-# ARCHIVAL VALUE: Vocabulary and default value
-ARCHIVAL_VALUE_UNCHECKED = u'archival_value : unchecked'
-ARCHIVAL_VALUE_PROMPT = u'archival_value : prompt'
-ARCHIVAL_VALUE_WORTHY = u'archival_value : archival worthy'
-ARCHIVAL_VALUE_UNWORTHY = u'archival_value : not archival worthy'
-ARCHIVAL_VALUE_SAMPLING  = u'archival_value : archival worthy with sampling'
-ARCHIVAL_VALUE_OPTIONS = (
-    (1,         ARCHIVAL_VALUE_UNCHECKED),
-    (2,         ARCHIVAL_VALUE_PROMPT),
-    (3,         ARCHIVAL_VALUE_WORTHY),
-    (3,         ARCHIVAL_VALUE_UNWORTHY),
-    (3,         ARCHIVAL_VALUE_SAMPLING ),
-    )
-grok.global_utility(utils.create_restricted_vocabulary(ILifeCycle['archival_value'],
-                                                       ARCHIVAL_VALUE_OPTIONS,
-                                                       message_factory=_),
-                    provides = schema.interfaces.IVocabularyFactory,
-                    name = u'lifecycle_archival_value_vocabulary')
-form.default_value(field=ILifeCycle['archival_value'])(
-    utils.set_default_with_acquisition(
-        field=ILifeCycle['archival_value'],
-        default = ARCHIVAL_VALUE_UNCHECKED
-        )
-    )
-
 
 # CUSTODY PERIOD / RETENTION PERIOD
 
@@ -152,34 +97,101 @@ class IntLowerEqualThanParentValidator(validator.SimpleFieldValidator):
             raise schema.interfaces.TooBig()
 
 
-# custody_period
-class CustodyPeriodValidator(IntLowerEqualThanParentValidator):
-    pass
-validator.WidgetValidatorDiscriminators(
-    CustodyPeriodValidator,
-    field=ILifeCycle['custody_period']
-    )
+# ---------- RETENTION PERIOD -----------
+# Vocabulary
+def _get_retention_period_options(vocabulary):
+    registry = zope.component.getUtility(IRegistry)
+    proxy = registry.forInterface(IRetentionPeriodRegister)
+    options = []
+    for num in getattr(proxy, 'retention_period'):
+        num = int(num)
+        options.append((num, num))
+    return options
+grok.global_utility(utils.create_restricted_vocabulary(
+        ILifeCycle['retention_period'],
+        _get_retention_period_options,
+        message_factory=_),
+                    provides=schema.interfaces.IVocabularyFactory,
+                    name=u'lifecycle_retention_period_vocabulary')
+
+# Default value
 form.default_value(field=ILifeCycle['custody_period'])(
     utils.set_default_with_acquisition(
         field=ILifeCycle['custody_period'],
         default = '10'
         )
     )
+
+# Validator
+class CustodyPeriodValidator(IntLowerEqualThanParentValidator):
+    pass
+validator.WidgetValidatorDiscriminators(
+    CustodyPeriodValidator,
+    field=ILifeCycle['custody_period']
+    )
+
 zope.component.provideAdapter(CustodyPeriodValidator)
 
 
-# retention_period
-class RetentionPeriodValidator(IntLowerEqualThanParentValidator):
-    pass
-validator.WidgetValidatorDiscriminators(
-    RetentionPeriodValidator,
-    field=ILifeCycle['retention_period']
-    )
+# ---------- CUSTODY PERIOD -----------
+# Vocabulary
+def _get_custody_period_options(context):
+    registry = zope.component.getUtility(IRegistry)
+    proxy = registry.forInterface(IBaseCustodyPeriods)
+    options = []
+    for num in getattr(proxy, 'custody_periods'):
+        num = int(num)
+        options.append((num, num))
+    return options
+grok.global_utility(utils.create_restricted_vocabulary(
+        ILifeCycle['custody_period'],
+        _get_custody_period_options,
+        message_factory=_),
+                    provides=schema.interfaces.IVocabularyFactory,
+                    name=u'lifecycle_custody_period_vocabulary')
 
+# Default value
 form.default_value(field=ILifeCycle['retention_period'])(
     utils.set_default_with_acquisition(
         field=ILifeCycle['retention_period'],
         default = 10
         )
     )
+
+# Validator
+class RetentionPeriodValidator(IntLowerEqualThanParentValidator):
+    pass
+validator.WidgetValidatorDiscriminators(
+    RetentionPeriodValidator,
+    field=ILifeCycle['retention_period']
+    )
 zope.component.provideAdapter(RetentionPeriodValidator)
+
+
+
+# ARCHIVAL VALUE: Vocabulary and default value
+ARCHIVAL_VALUE_UNCHECKED = u'archival_value : unchecked'
+ARCHIVAL_VALUE_PROMPT = u'archival_value : prompt'
+ARCHIVAL_VALUE_WORTHY = u'archival_value : archival worthy'
+ARCHIVAL_VALUE_UNWORTHY = u'archival_value : not archival worthy'
+ARCHIVAL_VALUE_SAMPLING  = u'archival_value : archival worthy with sampling'
+ARCHIVAL_VALUE_OPTIONS = (
+    (1,         ARCHIVAL_VALUE_UNCHECKED),
+    (2,         ARCHIVAL_VALUE_PROMPT),
+    (3,         ARCHIVAL_VALUE_WORTHY),
+    (3,         ARCHIVAL_VALUE_UNWORTHY),
+    (3,         ARCHIVAL_VALUE_SAMPLING ),
+    )
+grok.global_utility(utils.create_restricted_vocabulary(ILifeCycle['archival_value'],
+                                                       ARCHIVAL_VALUE_OPTIONS,
+                                                       message_factory=_),
+                    provides = schema.interfaces.IVocabularyFactory,
+                    name = u'lifecycle_archival_value_vocabulary')
+form.default_value(field=ILifeCycle['archival_value'])(
+    utils.set_default_with_acquisition(
+        field=ILifeCycle['archival_value'],
+        default = ARCHIVAL_VALUE_UNCHECKED
+        )
+    )
+
+
