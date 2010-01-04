3ac058f2400ca3108f69286010d23e9a40f9f429
Jonas Baumann <j.baumann@4teamwork.ch>
2010-01-04 14:15:16 +0000

    updated setup.py, moved version to textfile, updated history

--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0a1'
+version = open('opengever/base/version.txt').read().strip()
+maintainer = 'Jonas Baumann'
 
 setup(name='opengever.base',
       version=version,
-      description="Base package for OpenGever",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      description="Base package for OpenGever (Maintainer: %s)" % maintainer,
+      long_description=open("README.txt").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
         "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='',
-      author='Jonas Baumann, 4teamwork GmbH',
+      author='%s, 4teamwork GmbH' % maintainer,
       author_email='mailto:info@4teamwork.ch',
       url='http://psc.4teamwork.ch/4teamwork/kunden/opengever/opengever.base/',
       license='GPL2',
@@ -22,12 +27,14 @@ setup(name='opengever.base',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          'plone.dexterity',
-          'plone.autoform',
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        'plone.dexterity',
+        'plone.autoform',
+        # -*- Extra requirements: -*-
+        ],
       entry_points="""
       # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = opengever
       """,
       )
