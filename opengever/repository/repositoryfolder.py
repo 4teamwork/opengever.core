e22ea702cc10fc97bbc1c50a00ab319dc13eb765
Jonas Baumann <j.baumann@4teamwork.ch>
2010-01-04 11:09:36 +0000

    Issue #340: Aktenzeichen Dossiers und Subdossiers: Zähler zählt nicht über 2

--- opengever/repository/repositoryfolder.py
+++ opengever/repository/repositoryfolder.py
@@ -27,7 +27,6 @@ class IRepositoryFolderSchema(form.Schema):
         label = _(u'fieldset_common', default=u'Common'),
         fields = [
             u'effective_title',
-            u'reference_number',
             u'description',
             u'valid_from',
             u'valid_until',
@@ -42,13 +41,6 @@ class IRepositoryFolderSchema(form.Schema):
     effective_title = schema.TextLine(
         title = _(u'Title'),
         required = True
-        )
-
-    reference_number = schema.Int(
-        title = _(u'label_reference_number', default=u'Reference'),
-        description = _(u'help_reference_number', default=u''),
-        required = False,
-        min = 1,
         )
 
     description = schema.Text(
@@ -88,52 +80,6 @@ class IRepositoryFolderSchema(form.Schema):
          description = _(u'help_former_reference', default=u''),
          required = False,
          )
-
-
-@form.default_value(field=IRepositoryFolderSchema['reference_number'])
-def reference_number_default_value(data):
-    highest_reference_number = 0
-    for obj in data.context.listFolderContents():
-        if IRepositoryFolder.providedBy(obj):
-            if obj.reference_number > highest_reference_number:
-                highest_reference_number = obj.reference_number
-    highest_reference_number += 1
-    return highest_reference_number
-
-
-class ReferenceNumberValidator(validator.SimpleFieldValidator):
-    """
-    Reference number is uniqe per container (folder).
-    """
-    def validate(self, value):
-        super(ReferenceNumberValidator, self).validate(value)
-        if '++add++' in self.request.get('PATH_TRANSLATED', object()):
-            # context is container
-            siblings = self.context.getFolderContents(full_objects=1)
-        else:
-            parent = self.context.aq_inner.aq_parent
-            siblings = filter(lambda a:a!=self.context, parent.getFolderContents(full_objects=1))
-        sibling_ref_nums = []
-        for sibling in siblings:
-            try:
-                sibling_ref_nums.append(self.field.get(sibling))
-            except AttributeError:
-                pass
-        if value in sibling_ref_nums:
-            raise schema.interfaces.ConstraintNotSatisfied()
-
-validator.WidgetValidatorDiscriminators(
-    ReferenceNumberValidator,
-    field=IRepositoryFolderSchema['reference_number']
-    )
-zope.component.provideAdapter(ReferenceNumberValidator)
-zope.component.provideAdapter(error.ErrorViewMessage(
-        _('error_sibling_reference_number_existing', default=u'A Sibling with the same reference number is existing'),
-        error = schema.interfaces.ConstraintNotSatisfied,
-        field = IRepositoryFolderSchema['reference_number'],
-        ),
-                              name = 'message'
-                              )
 
 
 class RepositoryFolder(content.Container):
