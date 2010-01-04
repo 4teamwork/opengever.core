5737d644843338958740fd6cff85ec219f71e7a7
Jonas Baumann <j.baumann@4teamwork.ch>
2010-01-04 11:09:27 +0000

    Issue #340: Aktenzeichen Dossiers und Subdossiers: Zähler zählt nicht über 2

--- opengever/base/behaviors/reference.py
+++ opengever/base/behaviors/reference.py
@@ -10,7 +10,7 @@ from z3c.form import validator, error
 from opengever.base import _
 
 class IReferenceNumber(form.Schema):
-    
+
     form.fieldset(
         u'common',
         label = _(u'fieldset_common', default=u'Common'),
@@ -20,21 +20,22 @@ class IReferenceNumber(form.Schema):
         )
 
     reference_number = schema.Int(
-            title = _(u'label_reference_number', default=u'Reference'),
-            description = _(u'help_reference_number', default=u''),
-            required = False,
-            min = 1,
-    )
-    
+        title = _(u'label_reference_number', default=u'Reference'),
+        description = _(u'help_reference_number', default=u''),
+        required = False,
+        min = 1,
+        )
+
 alsoProvides(IReferenceNumber, form.IFormFieldProvider)
-    
+
 @form.default_value(field=IReferenceNumber['reference_number'])
 def reference_number_default_value(data):
     highest_reference_number = 0
     for obj in data.context.listFolderContents():
         if IReferenceNumberMarker.providedBy(obj):
-            if obj.reference_number > highest_reference_number:
-                highest_reference_number = obj.reference_number
+            num = IReferenceNumber(obj).reference_number
+            if num > highest_reference_number:
+                highest_reference_number = num
     highest_reference_number += 1
     return highest_reference_number
 
@@ -47,28 +48,32 @@ class ReferenceNumberValidator(validator.SimpleFieldValidator):
             siblings = self.context.getFolderContents(full_objects=1)
         else:
             parent = self.context.aq_inner.aq_parent
-            siblings = filter(lambda a:a!=self.context, parent.getFolderContents(full_objects=1))
+            siblings = filter(lambda a:a!=self.context,
+                              parent.getFolderContents(full_objects=1))
         sibling_ref_nums = []
         for sibling in siblings:
-            try:
-                sibling_ref_nums.append(self.field.get(sibling))
-            except AttributeError:
-                pass
+            if IReferenceNumberMarker.providedBy(sibling):
+                try:
+                    proxy = IReferenceNumber(sibling)
+                    sibling_ref_nums.append(self.field.get(proxy))
+                except AttributeError:
+                    pass
         if value in sibling_ref_nums:
             raise schema.interfaces.ConstraintNotSatisfied()
 
 validator.WidgetValidatorDiscriminators(
-        ReferenceNumberValidator,
-        field=IReferenceNumber['reference_number']
-)
+    ReferenceNumberValidator,
+    field=IReferenceNumber['reference_number']
+    )
 provideAdapter(ReferenceNumberValidator)
 provideAdapter(error.ErrorViewMessage(
-                _('error_sibling_reference_number_existing', default=u'A Sibling with the same reference number is existing'),
-                error = schema.interfaces.ConstraintNotSatisfied,
-                field = IReferenceNumber['reference_number'],
+        _('error_sibling_reference_number_existing',
+          default=u'A Sibling with the same reference number is existing'),
+        error = schema.interfaces.ConstraintNotSatisfied,
+        field = IReferenceNumber['reference_number'],
         ),
-        name = 'message'
-)
+               name = 'message'
+               )
 
 class IReferenceNumberMarker(Interface):
     """
