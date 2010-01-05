0efba0359c5898a374928a03a8409a113c0657cd
Victor Baumann <v.baumann@4teamwork.ch>
2010-01-05 12:18:05 +0000

    tabbedview with solr

--- opengever/tabbedview/browser/tabs.py
+++ opengever/tabbedview/browser/tabs.py
@@ -1,7 +1,7 @@
 import base64
 from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
-from ftw.tabbedview.browser.views import BaseListingView
+from ftw.tabbedview.browser.views import BaseListingView, SolrListingView
 from ftw.tabbedview.interfaces import ITabbedView
 from five import grok
 from ftw.table import helper
@@ -32,6 +32,26 @@ class OpengeverListingTab(grok.View, BaseListingView):
     search_index = 'SearchableText' #only 'SearchableText' is implemented for now
     sort_on = 'modified'
     sort_order = 'reverse'
+
+
+class OpengeverSolrListingTab(grok.View, SolrListingView):
+    grok.context(ITabbedView)
+    grok.template('generic')  
+      
+    update = SolrListingView.update    
+
+    columns = (
+                ('', helper.draggable),
+                ('', helper.path_checkbox),
+                ('Title', 'sortable_title', helper.linked),
+                ('modified', helper.readable_date), 
+                ('Creator', helper.readable_author),
+               )
+    
+
+    @property
+    def view_name(self):
+        return self.__name__.split('tabbedview_view-')[1]
 
 
 class OpengeverTab(object):
