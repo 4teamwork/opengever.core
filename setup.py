d9cc2faaba552562dbc3d55ae3377793f49ad1db
Jonas Baumann <j.baumann@4teamwork.ch>
2010-01-05 10:29:06 +0000

    metadata / setup.py update

--- setup.py
+++ setup.py
@@ -2,39 +2,42 @@ from setuptools import setup, find_packages
 import os
 
 version = open('opengever/dossier/version.txt').read().strip()
+maintainer = 'Thomas Buchberger'
 
 setup(name='opengever.dossier',
       version=version,
-      description="OpenGever Dossier Content Types",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      description="OpenGever Dossier Content Types (Maintainer: %s)" % maintainer,
+      long_description=open("README.txt").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='OpenGever',
-      author='Thomas Buchberger',
-      author_email='t.buchberger@4teamwork.ch',
-      url='http://www.4teamwork.ch',
-      license='GPL',
+      keywords='OpenGever Dossier',
+      author='%s, 4teamwork GmbH' % maintainer,
+      author_email='mailto:info@4teamwork.ch',
+      url='http://psc.4teamwork.ch/4teamwork/kunden/opengever/opengever.dossier/',
+      license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['opengever'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          'plone.app.dexterity',
-          'ftw.datepicker',
-          'collective.autopermission',
-          'opengever.base',
-          'rwproperty',
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        'plone.app.dexterity',
+        'ftw.datepicker',
+        'collective.autopermission',
+        'opengever.base',
+        'rwproperty',
+        # -*- Extra requirements: -*-
+        ],
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
-      target = plone
+      target = opengever
       """,
       )
