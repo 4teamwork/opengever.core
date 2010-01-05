051d39c1cb6878d0c3c2d865f0bbc330a4d6710f
Jonas Baumann <j.baumann@4teamwork.ch>
2010-01-05 13:08:15 +0000

    updated metadata ; set target release to 1.0a1

--- setup.py
+++ setup.py
@@ -1,34 +1,41 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = open('opengever/trash/version.txt').read().strip()
+maintainer = 'Philippe Gross'
 
 setup(name='opengever.trash',
       version=version,
-      description="the opengever trash packet, implements the special delete funcitionalty",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      description="the opengever trash packet, implements the special delete " + \
+          "funcitionalty. (Maintainer: %s)" % maintainer,
+      long_description=open("README.txt").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='',
-      author='phgross',
-      author_email='info@4teamwork.ch',
-      url='info.4teamwork.ch',
-      license='GPL',
+      keywords='opengever trash',
+      author='%s, 4teamwork GmbH' % maintainer,
+      author_email='mailto:info@4teamwork.ch',
+      url='http://psc.4teamwork.ch/4teamwork/kunden/opengever/opengever.trash/',
+      license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['opengever'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          'collective.monkeypatcher',
-          'collective.autopermission',          
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        'collective.monkeypatcher',
+        'collective.autopermission',
+        # -*- Extra requirements: -*-
+        ],
       entry_points="""
       # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = opengever
       """,
       )
