9399e983e80cbc74cd95b88ea3ce741e91b2d35e
Jonas Baumann <j.baumann@4teamwork.ch>
2010-01-05 12:54:08 +0000

    updated metadata ; changed target release to 1.0a1

--- setup.py
+++ setup.py
@@ -1,23 +1,28 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1'
+version = open('opengever/tabbedview/version.txt').read().strip()
+maintainer = 'Victor Baumann'
 
 setup(name='opengever.tabbedview',
       version=version,
-      description="opengever integration for ftw.tabbedview",
-      long_description=open("README.txt").read() + "\n" +
+      description="opengever integration for ftw.tabbedview (Maintainer %s)" % \
+          maintainer,
+      long_description=open("README.txt").read() + "\n" + \
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='',
-      author='Victor BAUMANN',
-      author_email='v.baumann@4teamwork.ch',
-      url='',
-      license='GPL',
+      author='%s, 4teamwork GmbH' % maintainer,
+      author_email='mailto:info@4teamwork.ch',
+      url='http://psc.4teamwork.ch/4teamwork/kunden/opengever/opengever.tabbedview/',
+      license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['opengever'],
       include_package_data=True,
@@ -30,5 +35,7 @@ setup(name='opengever.tabbedview',
       ],
       entry_points="""
       # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = opengever
       """,
       )
