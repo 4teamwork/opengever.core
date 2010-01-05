723d1a34e7544357a7cfd462e077b63d3b168439
Jonas Baumann <j.baumann@4teamwork.ch>
2010-01-05 11:13:38 +0000

    updated metadata / changed target release to 1.0a1

--- setup.py
+++ setup.py
@@ -1,33 +1,38 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1'
+version = open('opengever/portelts/tree/version.txt').read().strip()
+maintainer = 'Victor Baumann'
 
 setup(name='opengever.portlets.tree',
       version=version,
-      description="",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      description="(Maintainer: %s)" % maintainer,
+      long_description=open("README.txt").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='',
-      author='Victor BAUMANN',
-      author_email='v.baumann@4teamwork.ch',
-      url='http://plone.org',
-      license='GPL',
+      author='%s, 4teamwork GmbH' % maintainer,
+      author_email='mailto:info@4teamwork.ch',
+      url='http://psc.4teamwork.ch/4teamwork/kunden/opengever/opengever.portlets.tree',
+      license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['opengever', 'opengever.portlets'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        # -*- Extra requirements: -*-
+        ],
       entry_points="""
       # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = opengever
       """,
       )
