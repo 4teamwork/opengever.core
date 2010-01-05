9e827020d4c2294ddc008ee8883d4bdde119750e
Jonas Baumann <j.baumann@4teamwork.ch>
2010-01-05 10:57:21 +0000

    Updated setup.py, changed target release to 1.0a1

--- setup.py
+++ setup.py
@@ -2,19 +2,24 @@ from setuptools import setup, find_packages
 import os
 
 version = open('opengever/journal/version.txt').read().strip()
+maintainer = 'Jonas Baumann'
 
 setup(name='opengever.journal',
       version=version,
-      description="Opengever Journal integrates ftw.journal",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      description="Opengever Journal integrates ftw.journal (Maintainer: %s)" % \
+          maintainer,
+      long_description=open("README.txt").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='opengever journal ftw.journal',
-      author='Jonas Baumann, 4teamwork GmbH',
+      author='%s, 4teamwork GmbH' % maintainer,
       author_email='mailto:info@4teamwork.ch',
       url='http://psc.4teamwork.ch/4teamwork/kunden/opengever/opengever.journal',
       license='GPL2',
@@ -23,12 +28,14 @@ setup(name='opengever.journal',
       include_package_data=True,
       zip_safe=True,
       install_requires=[
-          'setuptools',
-          'ftw.journal',
-          'plone.versioningbehavior',
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        'ftw.journal',
+        'plone.versioningbehavior',
+        # -*- Extra requirements: -*-
+        ],
       entry_points="""
       # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = opengever
       """,
       )
