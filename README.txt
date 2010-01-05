c6782fd893cf59ba32c34945e996768efe7624a6
Jonas Baumann <j.baumann@4teamwork.ch>
2010-01-05 09:50:28 +0000

    updated metadata, changed target version

--- README.txt
+++ README.txt
@@ -1,20 +1,4 @@
 opengever.document
 ==================
 
-Bearbeitebare Felder:
-* foreign_reference : Fremdzeichen (Text)
-* receipt_date : Eingangsdatum (Date)
-* file : Datei (File)
-* paper_form : nur in Papierform verfügbar (Bool)
-* preserved_as_paper : in Papierform aufbewahrt (Bool)
-
-Nicht bearbeitbare Felder:
-* archival_file (PDF)
-* thumbnail (Image)
-* preview (PDF, HTML ??) » ar-preview-file (?, plone-produkt)
-
-Behaviors:
-plone.app.dexterity.behaviors.metadata.IBasic
-
-
 
--- setup.py
+++ setup.py
@@ -2,43 +2,49 @@ from setuptools import setup, find_packages
 import os
 
 version = open('opengever/document/version.txt').read().strip()
+maintainer = 'Jonas Baumann'
 
 setup(name='opengever.document',
       version=version,
-      description="OpenGever Document content type",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      description="OpenGever Document content type (Maintainer: %s)" % maintainer,
+      long_description=open("README.txt").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='opengever document',
-      author='Jonas Baumann, 4teamwork GmbH',
-      author_email='info@4teamwork.ch',
-      url='https://svn.4teamwork.ch/repos/opengever/opengever.document/',
+      author='%s, 4teamwork GmbH' % maintainer,
+      author_email='mailto:info@4teamwork.ch',
+      url='http://psc.4teamwork.ch/4teamwork/kunden/opengever/opengever.document/',
       license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['opengever'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          'Products.ARFilePreview',
-          'Products.CMFCore',
-          'Products.MimetypesRegistry',
-          'collective.filepreviewbehavior',
-          'ftw.sendmail',
-          'opengever.sqlfile',
-          'plone.app.dexterity',
-          'plone.principalsource',
-          'plone.stagingbehavior',
-          'plone.versioningbehavior',
-          'stoneagehtml', # required by ftw.sendmail, but there is no dependency in the current release
-          'ftw.datepicker',
-          'collective.autopermission',          
-      ],
+        'setuptools',
+        'Products.ARFilePreview',
+        'Products.CMFCore',
+        'Products.MimetypesRegistry',
+        'collective.filepreviewbehavior',
+        'ftw.sendmail',
+        'opengever.sqlfile',
+        'plone.app.dexterity',
+        'plone.principalsource',
+        'plone.stagingbehavior',
+        'plone.versioningbehavior',
+        'stoneagehtml', # required by ftw.sendmail, but there is no dependency in the current release
+        'ftw.datepicker',
+        'collective.autopermission',
+        ],
       entry_points="""
       # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = opengever
       """,
       )
