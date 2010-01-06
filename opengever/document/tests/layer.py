19c5ad6ff4685f12fd55a9f96d4a2d7b526d3482
Julian Infanger <j.infanger@4teamwork.ch>
2010-01-06 10:04:43 +0000

    added integratopm-tests

---
+++ opengever/document/tests/layer.py
@@ -0,0 +1,12 @@
+from Products.PloneTestCase import ptc
+import collective.testcaselayer.ptc
+
+ptc.setupPloneSite()
+
+class IntegrationTestLayer(collective.testcaselayer.ptc.BasePTCLayer):
+
+    def afterSetUp(self):
+        # Install the opengever.document product
+        self.addProfile('opengever.document:default')
+
+Layer = IntegrationTestLayer([collective.testcaselayer.ptc.ptc_layer])
---
+++ opengever/document/tests/test_document.py
@@ -0,0 +1,39 @@
+import unittest
+
+from zope.component import createObject
+from zope.component import queryUtility
+
+from plone.dexterity.interfaces import IDexterityFTI
+
+from Products.PloneTestCase.ptc import PloneTestCase
+from opengever.document.tests.layer import Layer
+
+from opengever.document.document import IDocumentSchema
+
+class TestDocumentIntegration(PloneTestCase):
+    
+    layer = Layer
+    
+    def test_adding(self):
+        self.setRoles(('Manager',))
+        self.folder.invokeFactory('opengever.document.document', 'document1')
+        d1 = self.folder['document1']
+        self.failUnless(IDocumentSchema.providedBy(d1))    
+    
+    def test_fti(self):
+        fti = queryUtility(IDexterityFTI, name='opengever.document.document')
+        self.assertNotEquals(None, fti)
+
+    def test_schema(self):
+        fti = queryUtility(IDexterityFTI, name='opengever.document.document')
+        schema = fti.lookupSchema()
+        self.assertEquals(IDocumentSchema, schema)
+
+    def test_factory(self):
+        fti = queryUtility(IDexterityFTI, name='opengever.document.document')
+        factory = fti.factory
+        new_object = createObject(factory)
+        self.failUnless(IDocumentSchema.providedBy(new_object))
+        
+def test_suite():
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
