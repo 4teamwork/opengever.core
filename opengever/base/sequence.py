1688578ac87d842324c0369ee975dcf021a80204
Julian Infanger <j.infanger@4teamwork.ch>
2010-01-11 08:44:43 +0000

    added a function to remove the sequence number

--- opengever/base/sequence.py
+++ opengever/base/sequence.py
@@ -29,6 +29,10 @@ class SequenceNumber(grok.GlobalUtility):
             ann[SEQUENCE_NUMBER_ANNOTATION_KEY] = value
         return ann.get(SEQUENCE_NUMBER_ANNOTATION_KEY)
     
+    def remove_number(self, obj):
+        ann = IAnnotations(obj)
+        if SEQUENCE_NUMBER_ANNOTATION_KEY in ann.keys():
+            del ann[SEQUENCE_NUMBER_ANNOTATION_KEY]
  
 class DefaultSequenceNumberGenerator(grok.Adapter):
     """ Provides a default sequence number generator. The portal_type of the object
