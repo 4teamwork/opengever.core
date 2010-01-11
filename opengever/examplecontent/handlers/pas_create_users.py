aae4509b1468820675ba264d6bef7ddf74014f18
Jonas Baumann <j.baumann@4teamwork.ch>
2010-01-11 13:00:29 +0000

    Issue #353: Example-Content setup profiles aufsplitten

--- opengever/examplecontent/handlers/pas_create_users.py
+++ opengever/examplecontent/handlers/pas_create_users.py
@@ -7,16 +7,15 @@ class SetupVarious(object):
         self.setup = setup
         self.portal = self.setup.getSite()
         self.openDataFile = self.setup.openDataFile
-        members = self.create_users()
-        self.create_groups(members)
+        self.create_users()
 
     def create_users(self):
-        members = []
         password = 'demo09'
         file = self.openDataFile('users.csv')
         users = self._get_objects_data(file)
         regtool = self.portal.portal_registration
         acl_users = self.portal.acl_users
+        groupstool = self.portal.portal_groups
         for user in users:
             if acl_users.getUserById(user['userid']):
                 continue
@@ -36,46 +35,13 @@ class SetupVarious(object):
                         user['lastname'],
                         )
                     })
-            members.append(member)
+            # groups are comma-seperated
+            for gid in user['groups'].split(','):
+                if not groupstool.getGroupById(gid):
+                    groupstool.addGroup(gid)
+                    print '** created group', gid
+                groupstool.getGroupById(gid).addMember(user['userid'])
             print '** created user', user['userid']
-        return members
-
-    def create_groups(self, members):
-        groupstool = self.portal.portal_groups
-        default = 'Sekretariat'
-        groups = {
-            # Sekretariat
-            'Sachbearbeiter' : [
-                'sachbearbeiter',
-                ],
-            'Vorsteher' : [
-                'vorsteher'
-                ],
-            'Verwalter' : [
-                'verwalter'
-                ],
-            'Administrators' : [
-                'admin'
-                ],
-            }
-        member_ids = [m.getProperty('username')
-                      for m in members]
-        assigned_members = []
-        for gname,gmembs in groups.items():
-            if groupstool.getGroupById(gname):
-                continue
-            groupstool.addGroup(gname)
-            for mem in gmembs:
-                groupstool.getGroupById(gname).addMember(mem)
-                assigned_members.append(mem)
-        if groupstool.getGroupById(default):
-            return
-        groupstool.addGroup(default)
-        for mem in member_ids:
-            if mem not in assigned_members:
-                groupstool.getGroupById(default).addMember(mem)
-                assigned_members.append(mem)
-
 
     def _get_objects_data(self, csv_stream):
         pos = csv_stream.tell()
