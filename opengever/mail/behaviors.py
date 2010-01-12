2bc6714e2542f3ea665c99ca2bbc744941614a17
Julian Infanger <j.infanger@4teamwork.ch>
2010-01-12 15:19:09 +0000

    383 - Neues Package opengever.mail

--- opengever/mail/behaviors.py
+++ opengever/mail/behaviors.py
@@ -1,7 +1,24 @@
-from plone.directives import form
+from zope.interface import Interface
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
+from zope.component import queryUtility
+from zope.app.intid.interfaces import IIntIds
+from opengever.mail.behaviors import IMailSettings
 
-class IMailInAddress(form.Schema):
-    
+class IMailInAddress(Interface):
     def get_email_address(self):
         """ generates an email adress for a dossier
         """
+        
+class IMailInAddressMarker(Interface):
+    pass
+
+class IMailInAddress(Interface):
+    def get_email_address(self):
+        id_util = getUtility(IIntIds)
+        intid = id_util.queryId(self.context)
+        registry = queryUtility(IRegistry)
+        proxy = registry.forInterface(IMailSettings)
+        domain = getattr(proxy,'mail_domain','fehler')
+        return '%s@%s' % (str(intid),domain)
+
--- opengever/mail/interfaces.py
+++ opengever/mail/interfaces.py
@@ -1,5 +1,5 @@
 from zope import schema
 from zope.interface import Interface
 
-class IMailDomain(Interface):
-    maildomain = schema.Text(title=u"maildomain", default="")
+class IMailSettings(Interface):
+    mail_domain = schema.Text(title=u"Mail Domain", default="opengever.4teamwork.ch")
