15cee9e229980f13f4fe91c17a88de19d7afb410
Jonas Baumann <j.baumann@4teamwork.ch>
2010-01-19 13:48:04 +0000

    autoformat

--- opengever/examplecontent/handlers/importer.py
+++ opengever/examplecontent/handlers/importer.py
@@ -8,12 +8,14 @@ from ftw.directoryservice.membership import Membership
 
 
 class LDAP_Importer(object):
-    
+
     def __call__(self, setup):
         self.fileencoding = 'utf8'
         self.setup = setup
         self.portal = self.setup.getSite()
-        self.gds = self.portal.get('gds', None)
+        self.gds = self.portal.get('gds',
+                                   self.portal.get('ogds',
+                                                   None))
         self.openDataFile = self.setup.openDataFile
         self.import_users()
 
@@ -33,17 +35,17 @@ class OpenGeverLDIF(LDIFParser):
         self.folder = folder
         self.ldapIds = []
 
-        self.skip_dn = [("zgXDirektion","directorate"),("zgXSurname", "lastname"), ("givenName", "firstname"), 
-        ("zgXDirektionAbk","directorate_abbr"), ("zgXAmt","department"), ("zgXAmtAbk", "department_abbr"),
-        ("mail", "email"), ("telephoneNumber", "phone_office"), ("uid", "userid")]
-    
+        self.skip_dn = [("zgXDirektion","directorate"),("zgXSurname", "lastname"), ("givenName", "firstname"),
+                        ("zgXDirektionAbk","directorate_abbr"), ("zgXAmt","department"), ("zgXAmtAbk", "department_abbr"),
+                        ("mail", "email"), ("telephoneNumber", "phone_office"), ("uid", "userid")]
+
     def parse(self):
         LDIFParser.parse(self)
         return
 
     def handle(self, dn, entry):
         uid = entry.get('uid', [''])[0]
-        
+
         if self.folder.get(uid,None):
             #user exist, only sync from ldap
             contact = self.folder.get(uid)
@@ -52,7 +54,7 @@ class OpenGeverLDIF(LDIFParser):
                     if contact.get(c_name)  != entry.get(l_name, [''])[0]:
                         contact.__setattr__(c_name, entry.get(l_name, [''])[0].decode('utf8'))
             self.ldapIds.append(uid)
-            
+
         else:
             #user don't exist in OG, create contact in OG
             if uid != "":
@@ -60,7 +62,7 @@ class OpenGeverLDIF(LDIFParser):
                     new_contact_id = self.folder.invokeFactory(
                         'ftw.directoryservice.contact',
                         id=uid,
-                    )
+                        )
                     new_contact = self.folder.get(new_contact_id)
                     for l_name, c_name in self.skip_dn:
                         new_contact.__setattr__(c_name, entry.get(l_name, [''])[0].decode('utf8'))
@@ -72,13 +74,13 @@ class OpenGeverLDIF(LDIFParser):
                                 'ftw.directoryservice.orgunit',
                                 id=entry.get('zgXAmtAbk')[0],
                                 title=entry.get('zgXAmt')[0].decode('utf8'),
-                            )
+                                )
                             Membership.create_membership( new_contact, self.folder.get(new_og), type="department")
                         else:
                             Membership.create_membership( new_contact, self.folder.get(entry.get('zgXAmtAbk')[0]), type="department")
                 except:
                     print entry
-                
+
             else:
                 print entry
             self.ldapIds.append(uid)
