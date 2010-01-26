9db25a4008813f9bbc32aefc15072f5ab9774c3c
Jonas Baumann <j.baumann@4teamwork.ch>
2010-01-26 15:24:06 +0000

    readable_ogds_author should be able to cope with users and members

--- opengever/tabbedview/helper.py
+++ opengever/tabbedview/helper.py
@@ -7,7 +7,13 @@ from Products.CMFCore.interfaces._tools import IMemberData
 from opengever.octopus.tentacle.interfaces import IContactInformation
 
 
-@ram.cache(lambda m,i,author: author)
+def author_cache_key(m, i, author):
+    if IPropertiedUser.providedBy(author) or IMemberData.providedBy(author):
+        return author.getId()
+    else:
+        return author
+
+@ram.cache(author_cache_key)
 def readable_ogds_author(item, author):
     if IPropertiedUser.providedBy(author) or IMemberData.providedBy(author):
         author = author.getId()
