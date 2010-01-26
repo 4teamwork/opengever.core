2edcb7e12a74dd443692bbcdc2f8573c75e102e6
Jonas Baumann <j.baumann@4teamwork.ch>
2010-01-26 15:20:46 +0000

    readable_ogds_author should be able to cope with users and members

--- opengever/tabbedview/helper.py
+++ opengever/tabbedview/helper.py
@@ -1,10 +1,15 @@
 from zope.component import getUtility
 from plone.memoize import ram
 
+from Products.PluggableAuthService.interfaces.authservice import IPropertiedUser
+from Products.CMFCore.interfaces._tools import IMemberData
+
 from opengever.octopus.tentacle.interfaces import IContactInformation
 
 
 @ram.cache(lambda m,i,author: author)
 def readable_ogds_author(item, author):
+    if IPropertiedUser.providedBy(author) or IMemberData.providedBy(author):
+        author = author.getId()
     info = getUtility(IContactInformation)
     return info.render_link(author)
