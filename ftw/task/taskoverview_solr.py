648c79bd7b9159f97978487858b12854d293f3b8
Philippe Gross <p.gross@4teamwork.ch>
2010-01-26 15:57:18 +0000

    changed taskoverview

--- ftw/task/taskoverview_solr.py
+++ ftw/task/taskoverview_solr.py
@@ -36,7 +36,7 @@ class MyTasks(OpengeverSolrListingTab):
         )
     types = ['ftw.task.task', ]
     
-    search_options = {'responsible': authenticated_member, 'review_state':['task-state-open','task-state-resolved']}
+    search_options = {'responsible': authenticated_member, 'review_state':['task-state-open','task-state-in-progress']}
 
 
 class IssuedTasks(OpengeverSolrListingTab):
