7a448aa7817c51fa674f323907dc690ea3587b22
Philippe Gross <p.gross@4teamwork.ch>
2010-03-02 11:57:01 +0000

    Issue #436 Workflow Erstellen eines Dokuments ab Vorlagen:

--- opengever/document/static/startredirect.js
+++ opengever/document/static/startredirect.js
@@ -1,5 +1,4 @@
-jq(function(){
-    $ = jq;
+startRedirect = function(){
     if (location.search.search('redirectTo=') != -1){
         temp = location.search;
         temp = temp.substr(temp.search('redirectTo') + 11, temp.length);
@@ -8,4 +7,10 @@ jq(function(){
         }
         location.href = unescape(temp);
     }
+    arbeitsraum.view_container.unbind('reload', startRedirect);
+} 
+
+jq(function(){
+    $ = jq;
+    arbeitsraum.view_container.bind('reload', startRedirect);
 });
