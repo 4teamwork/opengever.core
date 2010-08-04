a9fda02239bf92ed6d95c715ff49aa611f016afe
Thomas Buchberger <t.buchberger@4teamwork.ch>
2010-08-04 18:07:04 +0200

    Added reference number generator for dossiers (moved from opengever.base)

---
+++ opengever/dossier/reference.py
@@ -0,0 +1,28 @@
+from Acquisition import aq_parent, aq_inner
+from five import grok
+from opengever.base.interfaces import IReferenceNumber
+from opengever.base.reference import BasicReferenceNumber
+from opengever.dossier.behaviors.dossier import IDossierMarker
+import opengever.base.behaviors.reference
+
+
+class DossierReferenceNumber(BasicReferenceNumber):
+    """ Reference number for dossier types
+    """
+    grok.provides(IReferenceNumber)
+    grok.context(IDossierMarker)
+
+    def get_number(self):
+        # get local number prefix
+        num = opengever.base.behaviors.reference.IReferenceNumber(
+            self.context).reference_number
+        num = num and str(num) or ''
+        # get the parent number
+        parent_num = self.get_parent_number()
+        parent = aq_parent(aq_inner(self.context))
+        if parent_num and IDossierMarker.providedBy(parent):
+            return str(parent_num) + '.' + num
+        elif parent_num:
+            return str(parent_num) + ' / ' + num
+        else:
+            return num
