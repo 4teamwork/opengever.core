2467fa5ebcb3d3d5567e16392967d56372b91041
Philippe Gross <p.gross@4teamwork.ch>
2010-08-06 10:46:49 +0200

    change definition of byline viewlet from grok to zcml, and also moved to the viewlets directory

---
+++ opengever/dossier/viewlets/byline.py
@@ -0,0 +1,58 @@
+from zope.component import getUtility, getAdapter
+
+from plone.app.layout.viewlets import content
+from plone.memoize.instance import memoize
+
+from opengever.base.interfaces import IReferenceNumber, ISequenceNumber
+from opengever.dossier.behaviors.dossier import IDossier, IDossierMarker
+from opengever.mail.behaviors import IMailInAddressMarker, IMailInAddress
+from opengever.octopus.tentacle.interfaces import IContactInformation
+
+
+class Byline(content.DocumentBylineViewlet):
+    """ Specific DocumentByLine, for the Businesscasedossier Type"""
+
+    update = content.DocumentBylineViewlet.update
+
+    def start(self):
+        dossier = IDossier(self.context)
+        return dossier.start
+
+    def responsible(self):
+        info = getUtility(IContactInformation)
+        dossier = IDossier(self.context)
+        return info.render_link(dossier.responsible)
+
+    def end(self):
+        dossier = IDossier(self.context)
+        return dossier.end
+
+    @memoize
+    def workflow_state(self):
+        state = self.context_state.workflow_state()
+        workflows = self.tools.workflow().getWorkflowsFor(
+            self.context.aq_explicit)
+        if workflows:
+            for w in workflows:
+                if state in w.states:
+                    return w.states[state].title or state
+
+    @memoize
+    def sequence_number(self):
+        seqNumb = getUtility(ISequenceNumber)
+        return seqNumb.get_number(self.context)
+
+    @memoize
+    def reference_number(self):
+        refNumb = getAdapter(self.context, IReferenceNumber)
+        return refNumb.get_number()
+
+    def get_filing_no(self):
+        dossier = IDossierMarker(self.context)
+        return getattr(dossier, 'filing_no', None)
+
+    # TODO: should be more generic ;-)
+    #       use sequence_number instead of intid
+    def email(self):
+        if IMailInAddressMarker.providedBy(self.context):
+            return IMailInAddress(self.context).get_email_address()
