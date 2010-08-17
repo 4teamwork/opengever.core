b005478bc85f2ed48b01ba55f9c7835f10505757
Philippe Gross <p.gross@4teamwork.ch>
2010-08-17 15:58:45 +0200

    removed pdb and pep 8 validation

--- opengever/document/tests/test_document.py
+++ opengever/document/tests/test_document.py
@@ -11,15 +11,16 @@ from opengever.document.tests.layer import Layer
 
 from opengever.document.document import IDocumentSchema
 
+
 class TestDocumentIntegration(PloneTestCase):
-    
+
     layer = Layer
-    
+
     def test_adding(self):
         self.folder.invokeFactory('opengever.document.document', 'document1')
         d1 = self.folder['document1']
-        self.failUnless(IDocumentSchema.providedBy(d1))    
-    
+        self.failUnless(IDocumentSchema.providedBy(d1))
+
     def test_fti(self):
         fti = queryUtility(IDexterityFTI, name='opengever.document.document')
         self.assertNotEquals(None, fti)
@@ -34,12 +35,11 @@ class TestDocumentIntegration(PloneTestCase):
         factory = fti.factory
         new_object = createObject(factory)
         self.failUnless(IDocumentSchema.providedBy(new_object))
-    
+
     def test_upload_file(self):
         self.folder.invokeFactory('opengever.document.document', 'document1')
         d1 = self.folder['document1']
         field = IDocumentSchema['file']
-        import pdb; pdb.set_trace( )
         monk_file = NamedFile('bla bla', filename='test.txt')
         field.set(d1, monk_file)
         self.assertEquals(field.get(d1).data == 'bla bla')
