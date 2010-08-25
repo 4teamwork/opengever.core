97149a436bd13b38dc647c920fe3e65f0526bdc5
Philippe Gross <p.gross@4teamwork.ch>
2010-08-25 11:42:09 +0000

    Issue #535 Anpassungen Inhaltstyp "Mail"   https://extranet.4teamwork.ch/projects/opengever-kanton-zug/sprint-backlog/535/:
    added two indexer for the Mail type

---
+++ opengever/mail/indexer.py
@@ -0,0 +1,23 @@
+from DateTime import DateTime
+from five import grok
+from plone.indexer import indexer
+from ftw.mail.mail import IMail
+from ftw.mail import utils
+
+
+#indexes
+@indexer(IMail)
+def document_author(obj):
+    """ doucment_author indexer, return the Sender Adress """
+    import pdb; pdb.set_trace( )
+    return utils.get_header(obj.msg, 'From')
+grok.global_adapter(document_author, name='document_author')
+
+
+@indexer(IMail)
+def document_date(obj):
+    """ document_date indexer, return the from date of the mail """
+    document_date = utils.get_date_header(obj.msg, 'Date')
+    return DateTime(document_date)
+
+grok.global_adapter(document_date, name="document_date")
