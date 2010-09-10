ebc1389b83364b1f0798da06f813a1e2658455e9
Julian Infanger <j.infanger@4teamwork.ch>
2010-09-10 14:05:50 +0200

    removed console.info

--- opengever/base/browser/resources/sharing.js
+++ opengever/base/browser/resources/sharing.js
@@ -4,33 +4,4 @@ jq(function() {
         urlmatch:'$',
         urlreplace:''
         });
-    console.info('done');
-
-    /*
-    jq('div.sharing_group').each(function(){
-        obj = jq(this);
-        html = obj.html();
-        group_id = obj.attr('id');
-        new_html = '<a href="#" rel="'+group_id+'" class="show_members">';
-        new_html += html;
-        new_html += '</a>';
-        obj.html(new_html);
-    });
-    */
-
-    /*
-    jq('.show_members').live('click', function(e){
-        obj = jq(this);
-        jq.ajax({type : 'POST',
-             url : './list_groupmembers',
-             success : function(data, textStatus, XMLHttpRequest) {
-                 if (data.length > 0) {
-                     
-                     jq('#members_'+obj.attr('rel')).html(data);
-                 }
-             }
-        });
-        e.preventDefault();
-    });
-    */
 });
