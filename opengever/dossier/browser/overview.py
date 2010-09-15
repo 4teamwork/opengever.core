0a97ad1029e75717d916461998e024d88a3163a2
Philippe Gross <p.gross@4teamwork.ch>
2010-09-15 16:41:15 +0200

    rebuilding opengever.tabbedview:
    - moved DossierOverview and participants tab  from opengever.tabbedview in to opengever.dossier

---
+++ opengever/dossier/browser/overview.py
@@ -0,0 +1,92 @@
+from five import grok
+from opengever.tabbedview.browser.tabs import OpengeverTab
+from opengever.dossier.behaviors.dossier import IDossierMarker, IDossier
+from opengever.dossier import _
+
+from zope.annotation.interfaces import IAnnotations
+from ftw.journal.interfaces import IAnnotationsJournalizable,\
+    IWorkflowHistoryJournalizable
+from ftw.journal.config import JOURNAL_ENTRIES_ANNOTATIONS_KEY
+
+from opengever.dossier.behaviors.participation import IParticipationAware
+
+
+class DossierOverview(grok.View, OpengeverTab):
+    grok.context(IDossierMarker)
+    grok.name('tabbedview_view-overview')
+    grok.template('overview')
+
+    def catalog(self, types, showTrashed=False, depth=2):
+        return self.context.portal_catalog(
+            portal_type=types,
+            path=dict(depth=depth,
+                query='/'.join(self.context.getPhysicalPath())),
+            sort_on='modified',
+            sort_order='reverse')
+
+    def boxes(self):
+        if not self.context.show_subdossier():
+            items = [[dict(id = 'tasks', content=self.tasks()),
+                      dict(id = 'journal', content=self.journal()),
+                      dict(id = 'sharing', content=self.sharing())],
+                     [dict(id = 'documents', content=self.documents()), ]]
+        else:
+            items = [[dict(id = 'subdossiers', content=self.subdossiers()),
+                      dict(id = 'tasks', content=self.tasks()),
+                      dict(id = 'journal', content=self.journal()),
+                      dict(id = 'sharing', content=self.sharing())],
+                     [dict(id = 'documents', content=self.documents()), ]]
+        return items
+
+    def subdossiers(self):
+        return self.catalog(
+            ['opengever.dossier.projectdossier',
+                'opengever.dossier.businesscasedossier', ],
+            depth=1)[:5]
+
+    def tasks(self):
+        return self.catalog(['opengever.task.task', ])[:5]
+
+    def documents(self):
+        documents = self.catalog(
+            ['opengever.document.document', 'ftw.mail.mail', ])[:10]
+
+        return [{
+            'Title': document.Title,
+            'getURL': document.getURL,
+            'alt': self.context.toLocalizedTime(
+                    document.modified, long_format=1),
+            'getIcon': 'document_icon.gif',
+        } for document in documents]
+        return self.catalog(
+            ['opengever.document.document', 'ftw.mail.mail'])[:10]
+
+    def events(self):
+        return self.catalog(['dummy.event', ])[:5]
+
+    def journal(self):
+        if IAnnotationsJournalizable.providedBy(self.context):
+            annotations = IAnnotations(self.context)
+            entries = annotations.get(JOURNAL_ENTRIES_ANNOTATIONS_KEY, [])[:5]
+        elif IWorkflowHistoryJournalizable.providedBy(self.context):
+            raise NotImplemented
+
+        edict = [dict(Title=entry['action']['title'], getIcon=None)
+            for entry in reversed(entries)]
+        return edict
+
+    def sharing(self):
+        phandler = IParticipationAware(self.context)
+        results = list(phandler.get_participations())
+
+        dossier_adpt = IDossier(self.context)
+        responsible_name = _(u'label_responsible', 'Responsible')
+        results.append({'contact': dossier_adpt.responsible,
+                        'roles': responsible_name,
+                        'role_list': responsible_name,
+                        })
+        return [{
+            'Title':xx['contact'],
+            'getIcon':'user.gif',
+            }
+            for xx in results]
---
+++ opengever/dossier/browser/participants.py
@@ -0,0 +1,82 @@
+import base64
+
+from five import grok
+from opengever.tabbedview.browser.tabs import OpengeverListingTab
+from opengever.tabbedview.helper import readable_ogds_author
+from opengever.dossier.behaviors.dossier import IDossier
+from opengever.dossier.behaviors.participation import IParticipationAwareMarker
+from opengever.dossier.behaviors.participation import IParticipationAware
+from opengever.dossier import _
+
+
+class Participants(OpengeverListingTab):
+    """ Participants listing tab for dossiers using the
+    IParticipantsAware behavior
+    """
+    grok.name('tabbedview_view-participants')
+    grok.context(IParticipationAwareMarker)
+
+    def base64_oid_checkbox(item, value):
+        if not getattr(item, '_p_oid', False):
+            return ''
+        oid = base64.encodestring(item._p_oid)
+        attrs = {
+            'type': 'checkbox',
+            'class': 'noborder',
+            'name': 'oids:list',
+            'id': 'item-%s' % oid,
+            'value': oid,
+            }
+        html = '<input %s />' % ' '.join(['%s="%s"' % (k, v)
+                                          for k, v in attrs.items()])
+        return html
+
+    def icon_helper(item, value):
+        return '<img src="user.gif" alt="" title="" border="0" />'
+
+    sort_on = 'contact'
+    columns = (
+        ('', base64_oid_checkbox, ),
+        ('', icon_helper, ),
+        ('contact', 'contact', readable_ogds_author),
+        ('role_list', 'roles', ))
+
+    def update(self):
+        self.pagesize = 20
+        self.pagenumber = int(self.request.get('pagenumber', 1))
+        self.url = self.context.absolute_url()
+
+        phandler = IParticipationAware(self.context)
+        results = list(phandler.get_participations())
+
+        dossier_adpt = IDossier(self.context)
+        responsible_name = _(u'label_responsible', 'Responsible')
+        results.append({'contact': dossier_adpt.responsible,
+                        'roles': responsible_name,
+                        'role_list': responsible_name,
+                        })
+
+        # XXX implement searching
+        #if self.request.has_key('searchable_text'):
+        #    searchable_text = self.request.get('searchable_text', None)
+        #    if len(searchable_text):
+        #        searchable_text = searchable_text.endswith('*')\
+        #            and searchable_text[:-1] or searchable_text
+        #        filter_condition = lambda p:searchable_text in p.Title()
+        #        results = filter(filter_condition, results)
+
+        if self.sort_on.startswith('header-'):
+            self.sort_on = self.sort_on.split('header-')[1]
+        if self.sort_on:
+            sorter = lambda a, b: cmp(getattr(a, self.sort_on, ''),
+                                    getattr(b, self.sort_on, ''))
+            results.sort(sorter)
+
+        if self.sort_order=='reverse':
+            results.reverse()
+
+        self.contents = results
+        self.len_results = len(self.contents)
+
+    enabled_actions = ['delete_participants',
+                       'add_participant']
