cf138ac02b9aaed494da5f32b7e75ddafe865431
Philippe Gross <p.gross@4teamwork.ch>
2010-09-16 11:01:19 +0200

    added tests not working yet

---
+++ opengever/dossier/tests/test_functional_doctest.py
@@ -0,0 +1,44 @@
+from Products.PloneTestCase import ptc
+from Testing import ZopeTestCase
+from opengever.dossier.tests import layer
+import doctest
+import unittest
+
+
+MODULENAMES = ()
+
+
+TESTFILES = (
+    'resolve.txt',
+    )
+
+
+OPTIONFLAGS = (doctest.NORMALIZE_WHITESPACE |
+               doctest.ELLIPSIS |
+               doctest.REPORT_NDIFF)
+
+
+def test_suite():
+
+    suite = unittest.TestSuite()
+
+    for testfile in TESTFILES:
+        fdfs = ZopeTestCase.FunctionalDocFileSuite(
+            testfile,
+            optionflags=OPTIONFLAGS,
+            test_class=ptc.FunctionalTestCase,)
+        fdfs.layer = layer.IntegrationTestLayer
+        suite.addTest(fdfs)
+
+    for module in MODULENAMES:
+        fdts = ZopeTestCase.FunctionalDocTestSuite(
+            module,
+            optionflags=OPTIONFLAGS,
+            test_class=ptc.FunctionalTestCase)
+        fdts.layer = layer.layer
+        suite.addTest(fdts)
+
+    return suite
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
