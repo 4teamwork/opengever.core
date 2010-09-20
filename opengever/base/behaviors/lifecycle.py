e9e26358277cbc9b6da6e49dd171501cccc6845a
Julian Infanger <j.infanger@4teamwork.ch>
2010-09-20 13:53:43 +0200

    Issue #543 Metadaten diverser Inhaltstypen bereinigen: widget angepasst

--- opengever/base/behaviors/lifecycle.py
+++ opengever/base/behaviors/lifecycle.py
@@ -71,13 +71,13 @@ class ILifeCycle(form.Schema):
         source = u'lifecycle_custody_period_vocabulary',
         required = True,
         )
-
+    form.widget(date_of_cassation='ftw.datepicker.widget.DatePickerFieldWidget')
     date_of_cassation = schema.Date(
         title = _(u'label_dateofcassation', default=u'Date of cassation'),
         description = _(u'help_dateofcassation', default=u''),
         required = False,
         )
-
+    form.widget(date_of_submission='ftw.datepicker.widget.DatePickerFieldWidget')
     date_of_submission = schema.Date(
         title = _(u'label_dateofsubmission', default=u'Date of submission'),
         description = _(u'help_dateofsubmission', default=u''),
