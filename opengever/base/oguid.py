4f3c31ad15af4910798db389ac8e90ad31f5086a
Jonas Baumann <jone@jone.ch>
2010-09-21 16:49:16 +0200

    dropped tentacle, using opengever.ogds.base now

--- opengever/base/oguid.py
+++ opengever/base/oguid.py
@@ -1,7 +1,7 @@
 from five import grok
 from opengever.globalsolr.interfaces import ISearch
 from opengever.base.interfaces import IOGUid
-from opengever.octopus.tentacle.interfaces import ITentacleConfig
+from opengever.ogds.base.utils import get_client_id
 from zope.component import getUtility
 from zope.intid.interfaces import IIntIds
 
@@ -16,26 +16,26 @@ class OGUidProvider(grok.GlobalUtility):
     def get_id(self, context):
         """Returns the oguid of `context`.
         """
-        config = getUtility(ITentacleConfig)
+        client_id = get_client_id()
         intids = getUtility(IIntIds)
-        return '%s:%i' % (config.cid, intids.getId(context))
+        return '%s:%i' % (client_id, intids.getId(context))
 
     def is_on_current_client(self, oguid):
         """Returns `True` if the object with this `oguid` is stored on the
         current client, otherwise `False`.
         """
-        config = getUtility(ITentacleConfig)
+        client_id = get_client_id()
         cid, iid = oguid.split(':', 1)
-        return cid == config.cid
+        return cid == client_id
 
     def get_object(self, oguid):
         """Returns the object with the `oguid`, if it exists on the current
         client. Otherwise it returns `None`.
         """
-        config = getUtility(ITentacleConfig)
+        client_id = get_client_id()
         intids = getUtility(IIntIds)
         cid, iid = oguid.split(':', 1)
-        if cid != config.cid:
+        if cid != client_id:
             return None
         else:
             return intids.getObject(int(iid))
