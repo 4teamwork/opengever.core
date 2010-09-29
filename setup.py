a9a236c766126428d78737dc4a6ad2181c6188ea
Jonas Baumann <jone@jone.ch>
2010-09-29 09:40:33 +0200

    cleanup setup.py

--- setup.py
+++ setup.py
@@ -7,34 +7,37 @@ maintainer = 'Philippe Gross'
 
 tests_require = [
     'collective.testcaselayer',
-]
+    ]
 
 setup(name='opengever.contact',
       version=version,
-      description="the contactfolder and contact types",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      description="the contactfolder and contact types" + \
+          ' (Maintainer: %s)' % maintainer,
+      long_description=open("README.txt").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         ],
-    keywords='opengever contact',
-    author='%s, 4teamwork GmbH' % maintainer,
-    author_email='mailto:info@4teamwork.ch',
-    maintainer=maintainer,
-    url='http://psc.4teamwork.ch/4teamwork/kunden/opengever/opengever-contact/',
-    license='GPL2',
-    packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['opengever'],
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-      'setuptools',
-      'opengever.tabbedview',
-      # -*- Extra requirements: -*-
-    ],
-
-    entry_points="""
-    # -*- Entry points: -*-
-    """,
-    )
+      keywords='opengever contact',
+      author='%s, 4teamwork GmbH' % maintainer,
+      author_email='mailto:info@4teamwork.ch',
+      maintainer=maintainer,
+      url='http://psc.4teamwork.ch/4teamwork/kunden/opengever/opengever-contact/',
+      license='GPL2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['opengever'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'opengever.tabbedview',
+        # -*- Extra requirements: -*-
+        ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
