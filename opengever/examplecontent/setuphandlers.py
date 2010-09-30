dbe15af98bc07f228e74401d79e71a9f241c7d11
Philippe Gross <p.gross@4teamwork.ch>
2010-09-30 17:51:41 +0200

    - hide plone standard items - add contactfolder ect.

--- opengever/examplecontent/setuphandlers.py
+++ opengever/examplecontent/setuphandlers.py
@@ -13,11 +13,24 @@ def start_import(context):
     transmogrifier(u'opengever.examplecontent.users')
     transaction.commit()
 
+def settings(context):
+    # exclude items from the navigation we not used 
+    context.get('news').setExcludeFromNav(True)
+    context.get('news').reindexObject()
+    context.get('events').setExcludeFromNav(True)
+    context.get('events').reindexObject()
+    context.get('Members').setExcludeFromNav(True)
+    context.get('Members').reindexObject()
+    
+    # set default page
+    context.default_page = 'ordnungssystem'
 
 def setupVarious(setup):
     
     if setup.readDataFile('opengever.examplecontent.txt') is None:
-        return
+        pass
+
 
     site = setup.getSite()
     start_import(site)
+    settings(site)
