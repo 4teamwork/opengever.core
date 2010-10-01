2bccf9c3cdb651635ec6dd51547fc789a029f8fd
Jonas Baumann <jone@jone.ch>
2010-10-01 14:02:51 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,29 @@ from setuptools import setup, find_packages
 import os
 
 version = '1.0'
+maintainer = 'Victor Baumann'
 
 setup(name='opengever.tasktemplates',
       version=version,
-      description="",
-      long_description=open("README.txt").read() + "\n" +
+      description="" + \
+          ' (Maintainer: %s)' % maintainer,
+      long_description=open("README.txt").read() + "\n" + \
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
         "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='',
-      author='Victor BAUMANN',
-      author_email='v.baumann@4teamwork.ch',
-      url='',
-      license='GPL',
+      keywords='opengever task templates',
+      author='Victor Baumann',
+      author_email='mailto:info@4teamwork.ch',
+      url='http://psc.4teamwork.ch/4teamwork/kunden/opengever/' + \
+          'opengever-tasktemplates',
+      license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['opengever'],
       include_package_data=True,
@@ -27,5 +35,7 @@ setup(name='opengever.tasktemplates',
       ],
       entry_points="""
       # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )
