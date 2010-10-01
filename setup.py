bc2c3bf1dfc718128a44fcf3261f1414142668bf
Jonas Baumann <jone@jone.ch>
2010-10-01 14:02:59 +0200

    setup.py: using version.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = open('opengever/tasktemplates/version.txt').read().strip()
 maintainer = 'Victor Baumann'
 
 setup(name='opengever.tasktemplates',
