fbc7d72b8c67ff2e90037c55e24032805f75759b
Jonas Baumann <jone@jone.ch>
2010-10-04 14:18:21 +0200

    fixed tests

--- opengever/task/tests/test_response.py
+++ opengever/task/tests/test_response.py
@@ -3,11 +3,15 @@ from opengever.ogds.base import contact_info
 from opengever.task.adapters import Response
 from opengever.task.response import Base
 from opengever.task.tests.layer import Layer
+from plone.dexterity.utils import createContent, addContentToContainer
 from z3c.form import testing
 from zope.annotation.interfaces import IAttributeAnnotatable
 from zope.component import getMultiAdapter
 from zope.component import provideUtility
+from zope.event import notify
+from zope.globalrequest import setRequest
 from zope.interface import alsoProvides, implements
+from zope.lifecycleevent import ObjectCreatedEvent, ObjectAddedEvent
 import datetime
 
 
@@ -28,8 +32,11 @@ class TestResponse(PloneTestCase):
         # mock tentacle
         provideUtility(MockedContactInformation())
         # create task for testing
-        self.portal.portal_types.get('opengever.task.task').global_allow = True
-        self.folder.invokeFactory('opengever.task.task', 'task-1')
+        createContent('opengever.task.task')
+        task = createContent('opengever.task.task', title='Task')
+        notify(ObjectCreatedEvent(task))
+        task = addContentToContainer(self.folder, task, checkConstraints=False)
+        notify(ObjectAddedEvent(task))
 
     def test_base_view(self):
         view = Base(self.folder.get('task-1'), self.app.REQUEST)
@@ -38,6 +45,7 @@ class TestResponse(PloneTestCase):
     def test_add_form(self):
         request = self.app.REQUEST
         request.LANGUAGE = 'de'
+        setRequest(request)
         alsoProvides(request, IAttributeAnnotatable)
         task1 = self.folder.get('task-1')
         view = getMultiAdapter((task1, request),
--- opengever/task/tests/test_task.py
+++ opengever/task/tests/test_task.py
@@ -1,26 +1,36 @@
-import unittest
-
-from zope.component import createObject
-from zope.component import queryUtility, getUtility
-
-from plone.dexterity.interfaces import IDexterityFTI
-
 from Products.PloneTestCase.ptc import PloneTestCase
-from plone.registry.interfaces import IRegistry
-
-
-from opengever.task.tests.layer import Layer
-from opengever.task.task import ITask
 from opengever.task.adapters import IResponseContainer
 from opengever.task.response import Response
+from opengever.task.task import ITask
+from opengever.task.tests.layer import Layer
+from plone.dexterity.interfaces import IDexterityFTI
+from plone.dexterity.utils import createContent, addContentToContainer
+from plone.registry.interfaces import IRegistry
+from zope.component import createObject
+from zope.component import queryUtility, getUtility
+from zope.event import notify
+from zope.lifecycleevent import ObjectCreatedEvent, ObjectAddedEvent
+import unittest
+
+
+def create_task(parent, **kwargs):
+    createContent('opengever.task.task')
+    task = createContent('opengever.task.task', **kwargs)
+    notify(ObjectCreatedEvent(task))
+    task = addContentToContainer(parent, task, checkConstraints=False)
+    notify(ObjectAddedEvent(task))
+    return task
+
 
 class TestTaskIntegration(PloneTestCase):
-    
+
     layer = Layer
-    
+
+    def afterSetUp(self):
+        self.portal.portal_types['opengever.task.task'].global_allow = True
+
     def test_adding(self):
-        self.folder.invokeFactory('opengever.task.task', 'task-1')
-        t1 = self.folder['task-1']
+        t1 = create_task(self.folder, title='Task 1')
         self.failUnless(ITask.providedBy(t1))
 
     def test_fti(self):
@@ -39,32 +49,28 @@ class TestTaskIntegration(PloneTestCase):
         self.failUnless(ITask.providedBy(new_object))
 
     def test_view(self):
-        self.folder.invokeFactory('opengever.task.task', 'task-1')
-        t1 = self.folder['task-1']
+        t1 = create_task(self.folder, title='Task 1')
         view = t1.restrictedTraverse('@@view')
         self.failUnless(len(view.getSubTasks())==0)
-        t1.invokeFactory('opengever.task.task','task-2')
-        t2 = t1['task-2']
+        t2 = create_task(t1, title='Task 2')
         self.failUnless(view.getSubTasks()[0].getObject()==t2)
 
     def test_addresponse(self):
-        self.folder.invokeFactory('opengever.task.task', 'task-1')
-        t1 = self.folder['task-1']
+        t1 = create_task(self.folder, title='Task 1')
         res = Response("")
         container = IResponseContainer(t1)
         container.add(res)
         self.failUnless(res in container)
 
     def test_task_type_category(self):
-        self.folder.invokeFactory('opengever.task.task', 'task-1')
-        t1 = self.folder['task-1']
+        t1 = create_task(self.folder, title='Task 1')
         registry = getUtility(IRegistry)
         registry['opengever.task.interfaces.ITaskSettings.task_types_uni_ref'] = [u'Uni Ref 1',]
         t1.task_type = u'Uni Ref 1'
         self.assertEquals(u'uni_ref', t1.task_type_category)
         registry['opengever.task.interfaces.ITaskSettings.task_types_uni_val'] = [u'Uni Val 1', u'Uni Val 2']
         t1.task_type = u'Uni Val 2'
-        self.assertEquals(u'uni_val', t1.task_type_category)        
-        
+        self.assertEquals(u'uni_val', t1.task_type_category)
+
 def test_suite():
     return unittest.defaultTestLoader.loadTestsFromName(__name__)
