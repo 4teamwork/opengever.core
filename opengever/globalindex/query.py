ad7a9a6f9afc30e64561000d356c9a6c0c79e043
Philippe Gross <p.gross@4teamwork.ch>
2010-10-05 11:23:42 +0200

    added sort functionality

--- opengever/globalindex/query.py
+++ opengever/globalindex/query.py
@@ -1,6 +1,7 @@
 from opengever.globalindex import Session
 from opengever.globalindex.model.task import Task
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.sql.expression import asc, desc
 from zope.interface import implements
 from opengever.globalindex.interfaces import ITaskQuery
 
@@ -19,12 +20,35 @@ class TaskQuery(object):
             task = None
         return task
 
-    def get_tasks_for_responsible(self, responsible):
+    def get_tasks_for_responsible(self, responsible, sort_on='modified', sort_order='reverse'):
         """Returns all tasks assigned to the given responsible.
         """
-        return Session().query(Task).filter(Task.responsible==responsible).all()
+        sort_on = getattr(Task, sort_on)
+        if sort_order == 'reverse':
+            return Session().query(Task).filter(Task.responsible==responsible
+                ).order_by(desc(sort_on)).all()
+        else:
+            return Session().query(Task).filter(Task.responsible==responsible
+                ).order_by(asc(sort_on)).all()
 
-    def get_tasks_for_issuer(self, issuer):
+    def get_tasks_for_issuer(self, issuer, sort_on='modified', sort_order='reverse'):
         """Returns all tasks issued by the given issuer.
         """
-        return Session().query(Task).filter(Task.issuer==issuer).all()
+        sort_on = getattr(Task, sort_on)
+        if sort_order == 'reverse':
+            return Session().query(Task).filter(Task.issuer==issuer
+                ).order_by(desc(sort_on)).all()
+        else:
+            return Session().query(Task).filter(Task.issuer==issuer
+                ).order_by(asc(sort_on)).all()
+
+    def get_task_for_assigned_client(self, client, sort_on='modified', sort_order='reverse'):
+        """Return all task assigned to the actual client.
+        """
+        sort_on = getattr(Task, sort_on)
+        if sort_order == 'reverse':
+            return Session().query(Task).filter(Task.assigned_client==client
+                ).order_by(desc(sort_on)).all()
+        else:
+            return Session().query(Task).filter(Task.assigned_client==client
+                ).order_by(asc(sort_on)).all()
