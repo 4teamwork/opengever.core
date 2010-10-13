6e150afef74c57e8eccbc2fb513a87d29298eaa3
Jonas Baumann <jone@jone.ch>
2010-10-13 14:19:46 +0200

    added table helper for task type (vdex vocabulary)

---
+++ opengever/task/helper.py
@@ -0,0 +1,18 @@
+from opengever.task.util import getTaskTypeVocabulary
+from plone.memoize import ram
+from zope.app.component.hooks import getSite
+
+
+@ram.cache(lambda m,i,value: value)
+def task_type_helper(item, value):
+    """Translate the task type with the vdex vocabulary, which provides
+    its own translations stored in the vdex files.
+    """
+
+    voc = getTaskTypeVocabulary(getSite())
+    try:
+        term = voc.getTerm(value)
+    except LookupError:
+        return value
+    else:
+        return term.title
