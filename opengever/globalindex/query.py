5ae39a00171798b2f73ea66adbf14aad95d5490c
Jonas Baumann <jone@jone.ch>
2010-10-14 11:21:06 +0200

    changed task query utility: added _-methods for each method, just returning the query. This allows to change the query, e.g. set a limit (batching)

--- opengever/globalindex/query.py
+++ opengever/globalindex/query.py
@@ -14,41 +14,75 @@ class TaskQuery(object):
         """Returns the task identified by the given int_id and client_id.
         """
         try:
-            task = Session().query(Task).filter(Task.client_id==client_id).filter(
-                Task.int_id==int_id).one()
+            task = Session().query(Task).filter(
+                Task.client_id==client_id).filter(Task.int_id==int_id).one()
         except NoResultFound:
             task = None
         return task
 
-    def get_tasks_for_responsible(self, responsible, sort_on='modified', sort_order='reverse'):
+    def _get_tasks_for_responsible_query(self, responsible, sort_on='modified',
+                                         sort_order='reverse'):
+        """Returns a sqlalchemy query of all tasks assigned to the given
+        responsible.
+        """
+
+        sort_on = getattr(Task, sort_on)
+        if sort_order == 'reverse':
+            return Session().query(Task).filter(Task.responsible==responsible
+                                                ).order_by(desc(sort_on))
+        else:
+            return Session().query(Task).filter(Task.responsible==responsible
+                                                ).order_by(asc(sort_on))
+
+
+    def get_tasks_for_responsible(self, responsible, sort_on='modified',
+                                  sort_order='reverse'):
         """Returns all tasks assigned to the given responsible.
         """
+
+        return self._get_tasks_for_responsible_query(
+            responsible, sort_on=sort_on, sort_order=sort_order).all()
+
+    def _get_tasks_for_issuer_query(self, issuer, sort_on='modified',
+                                    sort_order='reverse'):
+        """Returns a sqlachemy query of all tasks issued by the given issuer.
+        """
+
         sort_on = getattr(Task, sort_on)
         if sort_order == 'reverse':
-            return Session().query(Task).filter(Task.responsible==responsible
-                ).order_by(desc(sort_on)).all()
+            return Session().query(Task).filter(Task.issuer==issuer
+                                                ).order_by(desc(sort_on))
         else:
-            return Session().query(Task).filter(Task.responsible==responsible
-                ).order_by(asc(sort_on)).all()
+            return Session().query(Task).filter(Task.issuer==issuer
+                                                ).order_by(asc(sort_on))
 
-    def get_tasks_for_issuer(self, issuer, sort_on='modified', sort_order='reverse'):
+    def get_tasks_for_issuer(self, issuer, sort_on='modified',
+                             sort_order='reverse'):
         """Returns all tasks issued by the given issuer.
         """
+
+        return self._get_tasks_for_issuer_query(
+            issuer, sort_on=sort_on, sort_order=sort_order).all()
+
+    def _get_tasks_for_assigned_client_query(self, client, sort_on='modified',
+                                             sort_order='reverse'):
+        """Return a sqlachemy query of all task assigned to the actual client.
+        """
+
         sort_on = getattr(Task, sort_on)
         if sort_order == 'reverse':
-            return Session().query(Task).filter(Task.issuer==issuer
-                ).order_by(desc(sort_on)).all()
+            return Session().query(Task).filter(Task.assigned_client==client
+                                                ).order_by(desc(sort_on))
         else:
-            return Session().query(Task).filter(Task.issuer==issuer
-                ).order_by(asc(sort_on)).all()
+            return Session().query(Task).filter(Task.assigned_client==client
+                                                ).order_by(asc(sort_on))
 
-    def get_task_for_assigned_client(self, client, sort_on='modified', sort_order='reverse'):
+    def get_task_for_assigned_client(self, client, sort_on='modified',
+                                     sort_order='reverse'):
         """Return all task assigned to the actual client.
         """
-        sort_on = getattr(Task, sort_on)
-        if sort_order == 'reverse':
-            return Session().query(Task).filter(Task.assigned_client==client
-                ).order_by(desc(sort_on)).all()
-        else:
-            return Session().query(Task).filter(Task.assigned_client==client
-                ).order_by(asc(sort_on)).all()
+
+        return self._get_tasks_for_assigned_client_query(
+            client, sort_on=sort_on, sort_order=sort_order).all()
+
+    get_tasks_for_assigned_client = get_task_for_assigned_client
