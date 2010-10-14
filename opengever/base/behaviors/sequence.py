7a12d962e21c9af46295097a997b31a956bc1b51
Philippe Gross <p.gross@4teamwork.ch>
2010-10-14 08:55:55 +0200

    added test for the sequence number fixed sequence nubmer problem with copied objects.

--- opengever/base/behaviors/sequence.py
+++ opengever/base/behaviors/sequence.py
@@ -3,7 +3,7 @@ from zope.component import getUtility
 from plone.indexer import indexer
 from plone.directives import form
 from opengever.base.interfaces import ISequenceNumber
-
+from zope.lifecycleevent.interfaces import IObjectCopiedEvent
 
 class ISequenceNumberBehavior(form.Schema):
     """the SequenceNumber Behavior is only used
@@ -16,3 +16,14 @@ def sequence_number(obj):
     seqNumb = getUtility(ISequenceNumber)
     return seqNumb.get_number(obj)
 grok.global_adapter(sequence_number, name='sequence_number')
+
+
+@grok.subscribe(ISequenceNumberBehavior, IObjectCopiedEvent)
+def new_sequence_number(obj, event):
+    """When a object was copied, the sequence number,
+    would also be copied. This Event handler fix that problem,
+    and generate a new one for the copy."""
+
+    seqNumb = getUtility(ISequenceNumber)
+    seqNumb.remove_number(obj)
+    seqNumb.get_number(obj)
