7f851fc06a14bb6863c9640d4f5d512c30f585e2
Jonas Baumann <jone@jone.ch>
2010-10-14 14:21:10 +0200

    use the new method in opengever.globalindex.utils for rendering the link of a indexed task item

--- opengever/task/task.py
+++ opengever/task/task.py
@@ -1,4 +1,5 @@
 from AccessControl import getSecurityManager
+from opengever.globalindex.utils import indexed_task_link
 from zope.schema.vocabulary import getVocabularyRegistry
 from Acquisition import aq_parent, aq_inner
 from Products.CMFCore.utils import getToolByName
@@ -297,53 +298,7 @@ class View(dexterity.DisplayForm):
         return controller.get_successors()
 
     def render_indexed_task(self, item):
-        """Renders a indexed task item (globalindex sqlalchemy object) either
-        with a link to the effective task (if the user has access) or just with
-        the title.
-        """
-
-        # render icon image
-        if item.icon:
-            image = '<img src="%s" /> ' % item.icon
-        else:
-            image = ''
-
-        # get the contact information utlity and the client
-        info = queryUtility(IContactInformation)
-        if info:
-            client = info.get_client_by_id(item.client_id)
-        if not info or not client:
-            return '%s<span>%s</span>' % (image, item.title)
-
-        # has the user access to the target task?
-        has_access = False
-        mtool = getToolByName(self.context, 'portal_membership')
-        member = mtool.getAuthenticatedMember()
-
-        if member:
-            principals = set(member.getGroups() + [member.getId()])
-            allowed_principals = set(item.principals)
-            has_access = len(principals & allowed_principals) > 0
-
-        # is the target on a different client? we need to make a popup if
-        # it is...
-        if item.client_id != get_client_id():
-            link_target = ' target="_blank"'
-        else:
-            link_target = ''
-
-        # embed the client
-        client_html = ' <span class="discreet">(%s)</span>' % client.title
-
-        # render the full link if he has acccess
-        inner_html = ''.join((image, item.title, client_html))
-        if has_access:
-            return '<a href="%s"%s>%s</a>' % (
-                client.public_url + '/' + item.physical_path,
-                link_target,
-                inner_html)
-        else:
-            return inner_html
+        return indexed_task_link(item, display_client=True)
 
 
 
