69542abf4306023406fbe7c1c6de5b64e1ddd99c
Jonas Baumann <jone@jone.ch>
2010-10-25 16:14:10 +0200

    Issue #529 Standard-Druckvorlagen erstellen & einbauen   https://extranet.4teamwork.ch/projects/opengever-kanton-zug/sprint-backlog/529 Beteiligte implementiert

--- opengever/latex/views/dossierdetails.py
+++ opengever/latex/views/dossierdetails.py
@@ -4,12 +4,16 @@ from Products.CMFPlone.interfaces.siteroot import IPloneSiteRoot
 from ftw.table import helper
 from opengever.base.interfaces import IReferenceNumber
 from opengever.base.interfaces import ISequenceNumber
+from opengever.dossier import _ as _dossier
 from opengever.dossier.behaviors.dossier import IDossier
+from opengever.dossier.behaviors.participation import IParticipationAware
+from opengever.dossier.browser.participants import role_list_helper
 from opengever.latex.template import LatexTemplateFile
 from opengever.latex.views.baselisting import BasePDFListing
 from opengever.ogds.base.interfaces import IContactInformation
 from opengever.ogds.base.utils import get_current_client
 from opengever.repository.interfaces import IRepositoryFolder
+from opengever.tabbedview.helper import readable_ogds_author
 from opengever.task.helper import task_type_helper
 from zope.component import getUtility
 
@@ -27,6 +31,7 @@ class DossierDetailsPDF(BasePDFListing):
         return self.main(tasks=self.get_tasks_latex(),
                          documents=self.get_documents_latex(),
                          subdossiers=self.get_subdossiers_latex(),
+                         participation=self.get_participants_latex(),
                          **self.get_main_options())
 
     def get_main_options(self):
@@ -61,7 +66,6 @@ class DossierDetailsPDF(BasePDFListing):
 
         data['responsible'] = ' '.join(
             (str(client.title), info.describe(dossier.responsible)))
-        data['participation'] = '? ? ? ? ?'
 
         data['start'] = helper.readable_date(self.context, dossier.start)
         data['end'] = helper.readable_date(self.context, dossier.end)
@@ -69,6 +73,42 @@ class DossierDetailsPDF(BasePDFListing):
         # convert to latex
         return dict([(key, self.convert(value))
                      for key, value in data.items()])
+
+    def get_participants_latex(self):
+        """Get a LaTeX listing of participants.
+        """
+
+        # get effective participants
+        phandler = IParticipationAware(self.context)
+        participants = list(phandler.get_participations())
+
+        # also append the responsible
+        class ResponsibleParticipant(object): pass
+
+        responsible = ResponsibleParticipant()
+        responsible.roles = _dossier(u'label_responsible', 'Responsible')
+        responsible.role_list = responsible.roles
+
+        dossier_adpt = IDossier(self.context)
+        responsible.contact = dossier_adpt.responsible
+        participants.append(responsible)
+
+        # sort the list
+        participants.sort(lambda a, b: cmp(getattr(a, 'contact', ''),
+                                           getattr(b, 'contact', '')))
+
+        # generate latex
+        latex = [r'{\vspace{-\baselineskip}\begin{itemize}']
+
+        for participant in participants:
+            row = '%s, %s' % (
+                readable_ogds_author(participant, participant.contact),
+                role_list_helper(participant, participant.roles))
+            latex.append(r'\item %s' % self.convert(row))
+
+        latex.append(r'\vspace{-\baselineskip}\end{itemize}}')
+
+        return '\n'.join(latex)
 
     def get_tasks_latex(self):
         """Returns the latex containing all tasks which are within
