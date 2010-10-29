d65c4f79d5013051ccf343a602bac6074d6d1904
Jonas Baumann <jone@jone.ch>
2010-10-29 18:09:30 +0200

    fixed more-menu

--- opengever/tabbedview/browser/resources/tabbedview.js
+++ opengever/tabbedview/browser/resources/tabbedview.js
@@ -1,21 +1,23 @@
 jq(function() {
-  jq('.tabbedview_view').bind('reload', function() {
-    /* update breadcrumb tooltips */
-    jq('a.rollover-breadcrumb').tooltip({
-      showURL: false,
-      track: true,
-      fade: 250
-    });
-    /* actions (<a>) should submit the form */
-    jq('#tabbedview-menu .actionicon').click(function(event) {
-      event.preventDefault();
-      jq(this).parents('form').append(jq(document.createElement('input')).attr({
-        'type' : 'hidden',
-        'name' : jq(this).attr('href'),
-        'value' : '1'
-      })).submit();
-    /* make menu */
-    });
+    jq('.tabbedview_view').bind('reload', function() {
+        /* update breadcrumb tooltips */
+        jq('a.rollover-breadcrumb').tooltip({
+            showURL: false,
+            track: true,
+            fade: 250
+        });
 
-  });
+        // make more menu work
+        initializeMenus();
+
+        /* actions (<a>) should submit the form */
+        jq('#tabbedview-menu a.actionicon').click(function(event) {
+            event.preventDefault();
+            jq(this).parents('form').append(jq(document.createElement('input')).attr({
+                'type' : 'hidden',
+                'name' : jq(this).attr('href'),
+                'value' : '1'
+            })).submit();
+        });
+    });
 });
