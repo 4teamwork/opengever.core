eba691a3e7fbeee452bf6aee28811bfd41eb2acd
Jonas Baumann <jone@jone.ch>
2010-11-02 17:54:40 +0100

    updated setup.py, added tests extras require

--- setup.py
+++ setup.py
@@ -2,21 +2,26 @@ from setuptools import setup, find_packages
 import os
 
 version = '1.0'
+maintainer = 'Thomas Buchberger'
+
+tests_require = []
 
 setup(name='opengever.globalindex',
       version=version,
-      description="A global index for opengever content",
-      long_description=open("README.txt").read() + "\n" +
+      description="A global index for opengever content" + \
+          ' (Maintainer: %s)' % maintainer,
+      long_description=open("README.txt").read() + "\n" + \
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         ],
       keywords='',
-      author='Thomas Buchberger',
-      author_email='t.buchberger@4teamwork.ch',
+      author='%s, 4teamwork GmbH' % maintainer,
+      author_email='mailto:info@4teamwork.ch',
       url='http://www.4teamwork.ch',
-      license='GPL',
+      license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['opengever'],
       include_package_data=True,
@@ -25,6 +30,8 @@ setup(name='opengever.globalindex',
           'setuptools',
           'z3c.saconfig',
       ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
