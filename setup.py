84be11ac19453157efd05e9342cbff2c1ee4171b
Jonas Baumann <jone@jone.ch>
2010-11-02 15:39:07 +0100

    updated email

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(name='opengever.ogds.base',
         ],
       keywords='opengever ogds base',
       author='%s, 4teamwork GmbH' % maintainer,
-      author_email='GPL2',
+      author_email='mailto:info@4teamwork.ch',
       maintainer=maintainer,
       url='http://psc.4teamwork.ch/4teamwork/kunden/' + \
           'opengever/opengever-ogds-base',
