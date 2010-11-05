699751b8fb6fac35f8abeac31e5359b8b0f769bb
Timon Tschanz <timon.tschanz@4teamwork.ch>
2010-11-05 15:13:18 +0100

    added some fields to repositoryroot

--- opengever/repository/repositoryroot.py
+++ opengever/repository/repositoryroot.py
@@ -1,7 +1,25 @@
 from plone.directives import form
-
+from zope import schema
+from opengever.repository import _
 
 class IRepositoryRoot(form.Schema):
     """ Repository root marker / schema interface
     """
+      
+    valid_from = schema.Date(
+        title = _(u'label_valid_from', default=u'Valid from'),
+        description = _(u'help_valid_from', default=u''),
+        required = False,
+        )
 
+    valid_until = schema.Date(
+        title = _(u'label_valid_until', default=u'Valid until'),
+        description = _(u'help_valid_until', default=u''),
+        required = False,
+        )
+    
+    version = schema.TextLine(
+        title = _(u'label_version', default=u'Version'),
+        description = _(u'help_version', default=''),
+        required = False,
+        )
