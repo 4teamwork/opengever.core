75513c7c21a2bbf77c24ffdfb61582a035ace344
Philippe Gross <p.gross@4teamwork.ch>
2010-11-09 09:31:20 +0100

    Dokument aus Vorlage, fehler bei validierung fhrt dazu das keine Fehlermeldung erscheint.: -fixed form

--- opengever/dossier/templatedossier.py
+++ opengever/dossier/templatedossier.py
@@ -44,17 +44,19 @@ class TemplateDocumentFormView(grok.View):
         default="create document with template")
 
     def __call__(self):
-        self.errors = {'title': None, 'paths': None}
+
+        self.errors = {}
         self.title = ''
         self.edit = False
         if self.request.get('form.buttons.save'):
             path = None
             if self.request.get('paths'):
                 path = self.request.get('paths')[0]
-            self.title = self.request.get('form.widgets.title').decode('utf8')
+            self.title = self.request.get('title', '').decode('utf8')
             self.edit = self.request.get('form.widgets.edit_form') == ['on']
 
             if path and self.title:
+
                 #create document
                 doc = self.context.restrictedTraverse(path)
                 clibboard = aq_parent(aq_inner(doc)).manage_copyObjects(
@@ -64,12 +66,14 @@ class TemplateDocumentFormView(grok.View):
                 annotations = IAnnotations(newdoc)
                 for a in list(annotations.keys()):
                     del annotations[a]
+
                 # change attributes: id, title, owner, creation_date ect.
                 name = "document-%s" % getUtility(ISequenceNumber).get_number(
                     newdoc)
                 member = self.context.portal_membership.getAuthenticatedMember()
                 self.context.manage_renameObject(newdoc.getId(), name)
-                event = ObjectAddedEvent(newdoc, newParent=self.context, newName=newdoc.getId())
+                event = ObjectAddedEvent(
+                    newdoc, newParent=self.context, newName=newdoc.getId())
                 notify(event)
                 newdoc.setTitle(self.title)
                 newdoc.changeOwnership(member)
@@ -77,14 +81,16 @@ class TemplateDocumentFormView(grok.View):
                 newdoc.document_date = datetime.now()
                 newdoc.creators = (member.title_or_id(), )
                 newdoc.document_author = member.title_or_id()
-                newdoc.manage_delLocalRoles([u for u, r in newdoc.get_local_roles()])
-                newdoc.manage_setLocalRoles(member.getId(), ('Owner', ))
+                newdoc.manage_delLocalRoles(
+                    [u for u, r in newdoc.get_local_roles()])
+                newdoc.manage_setLocalRoles(
+                    member.getId(), ('Owner', ))
                 event = ObjectModifiedEvent(newdoc)
                 notify(event)
                 # check if the direct-edit-mode is selected
                 if self.edit:
-                    # redirect to the parent Dossier of the new document 
-                    # and set the redirectTo parameter, which start the 
+                    # redirect to the parent Dossier of the new document
+                    # and set the redirectTo parameter, which start the
                     # zem-file download. See startredirect.js
                     manager = ICheckinCheckoutManager(newdoc)
                     wc = manager.checkout('', show_status_message=False)
@@ -92,13 +98,16 @@ class TemplateDocumentFormView(grok.View):
                     xpr = re.compile('href="(.*?)"')
                     html = portal.externalEditLink_(wc)
                     url = xpr.search(html).groups()[0]
-                    url = url.replace(portal.absolute_url(),'')
-                    get = urllib.urlencode({'redirectTo':url})
+                    url = url.replace(portal.absolute_url(), '')
+                    get = urllib.urlencode({'redirectTo': url})
 
-                    redirect_url = '%s?%s#documents_overview' % (self.context.absolute_url(), get)
-                    return self.request.RESPONSE.redirect(redirect_url)
+                    redirect_url = '%s?%s#documents_overview' % (
+                        self.context.absolute_url(), get)
+                    return self.request.RESPONSE.redirect(
+                        redirect_url)
                 else:
-                    return self.request.RESPONSE.redirect(self.context.absolute_url()+'#documents_overview')
+                    return self.request.RESPONSE.redirect(
+                        self.context.absolute_url()+'#documents_overview')
             else:
                 if path == None:
                     self.errors['paths'] = True
@@ -107,19 +116,27 @@ class TemplateDocumentFormView(grok.View):
 
         elif self.request.get('form.buttons.cancel'):
             return self.request.RESPONSE.redirect(self.context.absolute_url())
-        else:
-            templateUtil = getUtility(ITemplateUtility, 'opengever.templatedossier')
-            self.templatedossier = templateUtil.templateFolder(self.context)
-            if self.templatedossier is None:
-                status = IStatusMessage(self.request)
-                status.addStatusMessage(_("Not found the templatedossier"), type="error")
-                return self.context.request.RESPONSE.redirect(self.context.absolute_url())
-            return super(TemplateDocumentFormView, self).__call__()
+
+        # get the templatedocuments and show the form template
+        templateUtil = getUtility(
+            ITemplateUtility, 'opengever.templatedossier')
+        self.templatedossier = templateUtil.templateFolder(self.context)
+        if self.templatedossier is None:
+            status = IStatusMessage(self.request)
+            status.addStatusMessage(
+                _("Not found the templatedossier"), type="error")
+            return self.context.request.RESPONSE.redirect(
+                self.context.absolute_url())
+        return super(
+            TemplateDocumentFormView, self).__call__()
 
     def templates(self):
         generator = getUtility(ITableGenerator, 'ftw.tablegenerator')
         catalog = getToolByName(self.context, 'portal_catalog')
-        templates = catalog(path=dict(depth=1, query=self.templatedossier), portal_type="opengever.document.document")
+        templates = catalog(
+            path=dict(
+                depth=1, query=self.templatedossier),
+            portal_type="opengever.document.document")
         generator = getUtility(ITableGenerator, 'ftw.tablegenerator')
 
         columns = (
