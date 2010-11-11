998e5c8058776fbfbb47a03870c41d8c4b3edf52
Philippe Gross <p.gross@4teamwork.ch>
2010-11-11 10:58:31 +0000

    changed title and description of plone registry field

--- opengever/mail/interfaces.py
+++ opengever/mail/interfaces.py
@@ -2,4 +2,8 @@ from zope import schema
 from zope.interface import Interface
 
 class IMailSettings(Interface):
-    mail_domain = schema.Text(title=u"Mail Domain", default=u'opengever.4teamwork.ch')
+    mail_domain = schema.Text(
+        title=u"Mail domain",
+        description=u'Enter the mail domain which will be used \
+            for sending mails into this site.',
+        default=u'opengever.4teamwork.ch')
