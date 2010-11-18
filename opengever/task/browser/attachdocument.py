235a7de37c715e12a6a53df80745d63e132cbc01
Jonas Baumann <jone@jone.ch>
2010-11-18 11:56:23 +0100

    attach document: removed choose client step, since we can guess it

--- opengever/task/browser/attachdocument.py
+++ opengever/task/browser/attachdocument.py
@@ -33,14 +33,11 @@ class WizardFormMixin(object):
     """
 
     steps = (
-        ('choose_client', _(u'attachdocument_step_choose_client',
-                            default=u'1. Choose client')),
-
         ('choose_dossier', _(u'attachdocument_step_choose_dossier',
-                             default=u'2. Choose dossier')),
+                             default=u'1. Choose dossier')),
 
         ('choose_document', _(u'attachdocument_step_choose_document',
-                              default=u'3. Choose document')))
+                              default=u'2. Choose document')))
 
     def wizard_steps(self):
         """Return ordered list of wizards.
@@ -59,77 +56,27 @@ class WizardFormMixin(object):
                    'label': label,
                    'class': ' '.join(classes)}
 
+    def set_client_id(self, task):
+        """Sets the client id in the request for making the dossier / document
+        vocabularies work.
+        """
+        # to which clients is the user assigned?
+        info = getUtility(IContactInformation)
+        clients = info.get_assigned_clients()
+        client_ids = [client.client_id for client in clients]
 
+        # is the responsible client of the task one of the users assigned
+        # tasks? it should be!
+        if task.responsible_client not in client_ids:
+            raise ValueError('This user is not assigned to the repsponsible '
+                             'client of this task.')
 
-
-# ------------------- CHOSE HOME CLIENT ----------------------
-
-
-class IChooseClientSchema(form.Schema):
-    """Schema interface for choosing a sorurce client.
-    """
-
-    client = schema.Choice(
-        title=_(u'label_source_client',
-                default=u'Client'),
-        description=_(u'help_source_client',
-                      default=u''),
-        vocabulary=u'opengever.ogds.base.OtherAssignedClientsVocabulary',
-        required=True)
-
-
-class ChooseClientForm(z3c.form.form.Form, WizardFormMixin):
-    fields = z3c.form.field.Fields(IChooseClientSchema)
-    label = _(u'title_attach_document_form', u'Attach document')
-    ignoreContext = True
-    template = ViewPageTemplateFile(
-        'attachdocument_templates/wizard_wrappedform.pt')
-    step_name = 'choose_client'
-
-    @z3c.form.button.buttonAndHandler(_(u'button_cancel', default=u'Cancel'))
-    def handle_cancel(self, action):
-        return self.request.RESPONSE.redirect('.')
-
-    @z3c.form.button.buttonAndHandler(_(u'button_continue',
-                                        default=u'Continue'))
-    def handle_continue(self, action):
-        data, errors = self.extractData()
-        if not errors:
-            data = urllib.urlencode({'form.widgets.client': data['client']})
-            target = self.context.absolute_url() + \
-                '/@@choose_source_dossier?' + data
-            return self.request.RESPONSE.redirect(target)
-
-
-class ChooseClientView(layout.FormWrapper, grok.CodeView):
-    grok.context(ITask)
-    grok.name('choose_source_client')
-    form = ChooseClientForm
-
-    def __init__(self, *args, **kwargs):
-        layout.FormWrapper.__init__(self, *args, **kwargs)
-        grok.CodeView.__init__(self, *args, **kwargs)
-
-    def __call__(self):
-        factory = getUtility(
-            IVocabularyFactory,
-            name='opengever.ogds.base.OtherAssignedClientsVocabulary')
-        if not len(factory(self.context)):
-            # if there is no client in the vocabulary we cannot continue
-            msg = _(u'warning_attach_document_no_other_home_client',
-                    default=u'You are not assigned to another home client '
-                    'from where you could copy a document.')
-            IStatusMessage(self.request).addStatusMessage(msg, type='warning')
-            return self.request.RESPONSE.redirect(self.context.absolute_url())
-        else:
-            return layout.FormWrapper.__call__(self)
-
-
+        task.REQUEST.set('client', task.responsible_client)
 
 
 # ------------------- CHOSE DOSSIER --------------------------
 
-class IChooseDossierSchema(IChooseClientSchema):
+class IChooseDossierSchema(form.Schema):
     """ Form for choosing a source dossier on my home client
     """
 
@@ -153,9 +100,9 @@ class ChooseDossierForm(z3c.form.form.Form, WizardFormMixin):
         'attachdocument_templates/wizard_wrappedform.pt')
     step_name = 'choose_dossier'
 
-    def updateWidgets(self):
-        super(ChooseDossierForm, self).updateWidgets()
-        self.widgets['client'].mode = HIDDEN_MODE
+    def update(self):
+        self.set_client_id(self.context)
+        return super(ChooseDossierForm, self).update()
 
     @z3c.form.button.buttonAndHandler(_(u'button_cancel', default=u'Cancel'))
     def handle_cancel(self, action):
@@ -166,8 +113,7 @@ class ChooseDossierForm(z3c.form.form.Form, WizardFormMixin):
     def handle_continue(self, action):
         data, errors = self.extractData()
         if not errors:
-            data = urllib.urlencode({'form.widgets.client': data['client'],
-                                     'form.widgets.source_dossier':
+            data = urllib.urlencode({'form.widgets.source_dossier':
                                          data['source_dossier']})
             target = self.context.absolute_url() + \
                 '/@@choose_source_document?' + data
@@ -214,9 +160,12 @@ class ChooseDocumentForm(z3c.form.form.Form, WizardFormMixin):
         'attachdocument_templates/wizard_wrappedform.pt')
     step_name = 'choose_document'
 
+    def update(self):
+        self.set_client_id(self.context)
+        return super(ChooseDocumentForm, self).update()
+
     def updateWidgets(self):
         super(ChooseDocumentForm, self).updateWidgets()
-        self.widgets['client'].mode = HIDDEN_MODE
         self.widgets['source_dossier'].mode = HIDDEN_MODE
 
     @z3c.form.button.buttonAndHandler(_(u'button_cancel', default=u'Cancel'))
@@ -230,7 +179,7 @@ class ChooseDocumentForm(z3c.form.form.Form, WizardFormMixin):
             document = data.get('source_document')
             info = getUtility(IContactInformation)
 
-            client = info.get_client_by_id(data['client'])
+            client = info.get_client_by_id(self.request.get('client'))
             home_clients = info.get_assigned_clients()
 
             if client not in home_clients:
