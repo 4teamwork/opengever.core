12505fd9d86b4b87698becc7c3418a9b866b1e78
Jonas Baumann <jone@jone.ch>
2010-11-19 10:53:13 +0100

    Copy documents to remote client: implemented wizard, using autocomplete now, fixed various smaller issues

--- opengever/dossier/browser/transport.py
+++ opengever/dossier/browser/transport.py
@@ -1,14 +1,22 @@
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.statusmessages.interfaces import IStatusMessage
 from five import grok
+from opengever.base.interfaces import IRedirector
 from opengever.dossier import _
 from opengever.dossier.behaviors.dossier import IDossierMarker
 from opengever.ogds.base.interfaces import IContactInformation
 from opengever.ogds.base.interfaces import ITransporter
+from opengever.ogds.base.utils import get_client_id
 from plone.directives import form
+from plone.formwidget.autocomplete.widget import AutocompleteSelectionWidget
 from plone.z3cform import layout
 from z3c.form.interfaces import HIDDEN_MODE
+from z3c.form.interfaces import IFieldWidget
+from z3c.form.interfaces import INPUT_MODE
+from z3c.form.widget import FieldWidget
 from zope import schema
 from zope.component import getUtility
+from zope.interface import implementer
 from zope.schema.interfaces import IVocabularyFactory
 import os
 import urllib
@@ -19,8 +27,109 @@ class NoItemsSelected(Exception):
     pass
 
 
+class WizardFormMixin(object):
+    """Mixing for adding a method witch returns the data needed for rendering
+    the wizard steps part. This has nothing to do with the forms itselves, its
+    just the steps bar for making it wizard like.
+    """
 
-# ------------------- CHOSE HOME CLIENT ----------------------
+    steps = (
+        ('choose_client', _(u'copy_documents_step_choose_client',
+                            default=u'1. Choose client')),
+
+        ('choose_dossier', _(u'copy_documents_step_choose_dossier',
+                             default=u'2. Choose dossier')))
+
+    def wizard_steps(self):
+        """Return ordered list of wizards.
+        """
+
+        current_reached = False
+
+        for name, label in self.steps:
+            classes = ['wizard-step-%s' % name]
+            if name == self.step_name:
+                current_reached = True
+                classes.append('selected')
+            elif not current_reached:
+                classes.append('visited')
+            yield {'name': name,
+                   'label': label,
+                   'class': ' '.join(classes)}
+
+
+
+# ------------------- CUSTOM WIDGETS ----------------------
+
+class DossierAutocompleteSelectionWidget(AutocompleteSelectionWidget):
+    """Autocomplete widget for selecting a dossier from a client.
+    """
+
+    def custom_url_parameters(self):
+        info = getUtility(IContactInformation)
+        home_clients = tuple(info.get_assigned_clients())
+
+        # is the user assigned to exactly one client?
+        if len(home_clients) == 1:
+            default = home_clients[0].client_id
+        else:
+            default = None
+
+        # read the client from the request
+        client_id = self.request.get(
+            'client', self.request.get('form.widgets.client'),
+            default)
+        if isinstance(client_id, list) and len(client_id) == 1:
+            client_id = client_id[0]
+
+        # it should not be the current client
+        if client_id == get_client_id():
+            client_id = None
+
+        if client_id:
+            client = info.get_client_by_id(client_id)
+
+            # verify that the user is assigned to the requested client
+            if client not in info.get_assigned_clients():
+                raise RuntimeError(
+                    'Expected current user to be assigned to the '
+                    'client "%s" read from request' % client_id)
+
+            else:
+                return '?client=%s' % client.client_id
+
+        return ''
+
+    def js(self):
+
+        form_url = self.request.getURL()
+
+        form_prefix = self.form.prefix + self.__parent__.prefix
+        widget_name = self.name[len(form_prefix):]
+
+        url = "%s/++widget++%s/@@autocomplete-search%s" % (
+            form_url, widget_name, self.custom_url_parameters())
+
+        js_callback = self.js_callback_template % dict(id=self.id,
+                                                       name=self.name,
+                                                       klass=self.klass,
+                                                       title=self.title,
+                                                       termCount=len(self.terms))
+
+        return self.js_template % dict(id=self.id,
+                                       url=url,
+                                       minLength=self.minLength,
+                                       js_callback=js_callback,
+                                       js_extra=self.js_extra())
+
+
+@implementer(IFieldWidget)
+def DossierAutocompleteFieldWidget(field, request):
+    return FieldWidget(field, DossierAutocompleteSelectionWidget(request))
+
+
+
+# ------------------- CHOOSE HOME CLIENT ----------------------
 
 
 class IChooseClientSchema(form.Schema):
@@ -37,14 +146,14 @@ class IChooseClientSchema(form.Schema):
         required=True)
 
 
-class ChooseClientForm(z3c.form.form.Form):
+class ChooseClientForm(z3c.form.form.Form, WizardFormMixin):
     fields = z3c.form.field.Fields(IChooseClientSchema)
     label = _(u'title_attach_document_form', u'Attach document')
     ignoreContext = True
 
-    @z3c.form.button.buttonAndHandler(_(u'button_cancel', default=u'Cancel'))
-    def handle_cancel(self, action):
-        return self.request.RESPONSE.redirect('.')
+    template = ViewPageTemplateFile(
+        'transport_templates/wizard_wrappedform.pt')
+    step_name = 'choose_client'
 
     @z3c.form.button.buttonAndHandler(_(u'button_continue',
                                         default=u'Continue'))
@@ -56,6 +165,10 @@ class ChooseClientForm(z3c.form.form.Form):
             target = self.context.absolute_url() + \
                 '/@@copy-documents-to-remote-client2?' + data
             return self.request.RESPONSE.redirect(target)
+
+    @z3c.form.button.buttonAndHandler(_(u'button_cancel', default=u'Cancel'))
+    def handle_cancel(self, action):
+        return self.request.RESPONSE.redirect('.')
 
     def updateWidgets(self):
         super(ChooseClientForm, self).updateWidgets()
@@ -88,13 +201,28 @@ class ChooseClientView(layout.FormWrapper, grok.CodeView):
         factory = getUtility(
             IVocabularyFactory,
             name='opengever.ogds.base.OtherAssignedClientsVocabulary')
-        if not len(factory(self.context)):
+        voc = tuple(factory(self.context))
+
+        if not len(voc):
             # if there is no client in the vocabulary we cannot continue
             msg = _(u'warning_copy_to_remote_no_client',
                     default=u'You are not assigned to another client where you'
                     ' could copy the documents to.')
             IStatusMessage(self.request).addStatusMessage(msg, type='error')
             return self.request.RESPONSE.redirect(self.context.absolute_url())
+
+        elif len(voc) == 1:
+            # we have only one client - selecting this one does not make sense.
+            # so we redirect to the next wizard step.
+            self.form_instance.update()
+            data = urllib.urlencode(
+                {'form.widgets.paths': ';;'.join(
+                        self.form_instance.item_paths),
+                 'form.widgets.client': voc[0].token})
+            target = self.context.absolute_url() + \
+                '/@@copy-documents-to-remote-client2?' + data
+            return self.request.RESPONSE.redirect(target)
+
         else:
             return layout.FormWrapper.__call__(self)
 
@@ -117,10 +245,16 @@ class IChooseDossierSchema(IChooseClientSchema):
         )
 
 
-class CopyDocumentsToRemoteClientForm(z3c.form.form.Form):
+class CopyDocumentsToRemoteClientForm(z3c.form.form.Form, WizardFormMixin):
     fields = z3c.form.field.Fields(IChooseDossierSchema)
+    fields['target_dossier'].widgetFactory[INPUT_MODE] = \
+        DossierAutocompleteFieldWidget
     label = _(u'label_choose_target_dossier', default=u'')
     ignoreContext = True
+
+    template = ViewPageTemplateFile(
+        'transport_templates/wizard_wrappedform.pt')
+    step_name = 'choose_dossier'
 
     @z3c.form.button.buttonAndHandler(_(u'button_copy', default=u'Copy'))
     def handle_copy(self, action):
@@ -137,7 +271,14 @@ class CopyDocumentsToRemoteClientForm(z3c.form.form.Form):
                 trans.transport_to(obj, cid, target)
             redirect_to = os.path.join(client.public_url,
                                        target, '#documents')
-            return self.request.RESPONSE.redirect(redirect_to)
+            redirector = IRedirector(self.request)
+            redirector.redirect(redirect_to, target='_blank')
+            return self.request.RESPONSE.redirect(
+                self.context.absolute_url() + '#documents')
+
+    @z3c.form.button.buttonAndHandler(_(u'button_cancel', default=u'Cancel'))
+    def handle_cancel(self, action):
+        return self.request.RESPONSE.redirect('.')
 
     def updateWidgets(self):
         super(CopyDocumentsToRemoteClientForm, self).updateWidgets()
@@ -168,9 +309,7 @@ class CopyDocumentsToRemoteClientForm(z3c.form.form.Form):
         if value:
             value = value.split(';;')
             return value
-        value = self.request.get('paths')
-        if not value:
-            raise NoItemsSelected
+        value = self.request.get('paths', [])
         return value
 
 
