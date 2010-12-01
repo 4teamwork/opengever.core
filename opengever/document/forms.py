bdb9cf6e9a5811122eb51f2fcca7a024ee40578a
Jonas Baumann <jone@jone.ch>
2010-12-01 14:29:09 +0100

    added custom edit form for hiding file

---
+++ opengever/document/forms.py
@@ -0,0 +1,32 @@
+"""Document form customizations.
+"""
+
+from opengever.document.interfaces import ICheckinCheckoutManager
+from plone.dexterity.browser.edit import DefaultEditForm
+from plone.z3cform import layout
+from z3c.form.interfaces import DISPLAY_MODE
+from zope.component import queryMultiAdapter
+
+
+class DocumentEditForm(DefaultEditForm):
+    """Custom edit form for documents, which hides the file field
+    when document is not checked out.
+    """
+
+    def updateWidgets(self):
+        super(DocumentEditForm, self).updateWidgets()
+
+        # get the checkin checkout manager
+        manager = queryMultiAdapter((self.context, self.request),
+                                    ICheckinCheckoutManager)
+        if not manager:
+            return
+
+        if not manager.checked_out():
+            filefields = [g.fields.get('file') for g in self.groups
+                          if 'file' in g.fields]
+            if len(filefields)>0:
+                filefields[0].mode = DISPLAY_MODE
+
+
+DocumentEditView = layout.wrap_form(DocumentEditForm)
