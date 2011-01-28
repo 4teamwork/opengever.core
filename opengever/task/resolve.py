06ec83590ed01cb72ea05c0b5644d978f59fb53e
Jonas Baumann <jone@jone.ch>
2011-01-28 09:59:51 +0100

    Fixed task resolve check: added all valid closed-states.

--- opengever/task/resolve.py
+++ opengever/task/resolve.py
@@ -1,10 +1,14 @@
 from five import grok
 from opengever.task.task import ITask
 
-RESOLVED_TASK_STATE = 'task-state-resolved'
+
+TASK_CLOSED_STATES = ['task-state-tested-and-closed',
+                      'task-state-rejected',
+                      'task-state-cancelled']
+
 
 class ResolveCheck(grok.CodeView):
-    """The view wich check if the task can be resolved. 
+    """The view wich check if the task can be resolved.
     It would be called from the transition-expression """
 
     grok.context(ITask)
@@ -17,15 +21,17 @@ class ResolveCheck(grok.CodeView):
         wft = self.context.portal_workflow
         wf = wft.get(wft.getChainFor(self.context)[0])
         states = [s for s in wf.states]
-        states.pop(states.index(RESOLVED_TASK_STATE))
+
+        for state in TASK_CLOSED_STATES:
+            states.pop(states.index(state))
+
         query = {
             'path' : {
                 'query' : '/'.join(self.context.getPhysicalPath()),
-                'depth' : -1,
-            },
+                'depth' : -1},
             'portal_type': 'opengever.task.task',
-            'review_state': states,
-        }
+            'review_state': states}
+
         if len(self.context.portal_catalog(query)) > 1:
             return False
         else:
