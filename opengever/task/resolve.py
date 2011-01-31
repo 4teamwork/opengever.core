4991ac859ed3cf5ab9444daf38dc51546b1a2bf1
Pascal Habegger <p.habegger@4teamwork.ch>
2011-01-31 13:42:36 +0100

    Cleaned up comment / docstring.

--- opengever/task/resolve.py
+++ opengever/task/resolve.py
@@ -8,15 +8,15 @@ TASK_CLOSED_STATES = ['task-state-tested-and-closed',
 
 
 class ResolveCheck(grok.CodeView):
-    """The view wich check if the task can be resolved.
-    It would be called from the transition-expression """
+    """This view checks if the task can be resolved.
+    It will be called from a transition-expression """
 
     grok.context(ITask)
     grok.name('resolve_check')
     grok.require('zope2.View')
 
     def render(self):
-        """check if there aren't any subtask wich aren't closed yet """
+        """check if there aren't any subtasks which aren't closed yet """
 
         wft = self.context.portal_workflow
         wf = wft.get(wft.getChainFor(self.context)[0])
