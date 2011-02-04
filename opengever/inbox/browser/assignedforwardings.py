b892e3d65a4e78968659d6e2a35f5776e976f72d
Timon Tschanz <t.tschanz@4teamwork.ch>
2011-02-04 10:10:36 +0100

    added AssignedTasks tab

---
+++ opengever/inbox/browser/assignedforwardings.py
@@ -0,0 +1,28 @@
+from five import grok
+from opengever.globalindex.interfaces import ITaskQuery
+from opengever.globalindex.model.task import Task
+from opengever.ogds.base.utils import get_client_id
+from opengever.tabbedview.browser.tasklisting import GlobalTaskListingTab
+from opengever.inbox.inbox import IInbox
+from zope.component import getUtility
+
+
+class InboxAssignedForwardings(GlobalTaskListingTab):
+    """Listing tab for listing all tasks which are assigned to this inbox.
+    """
+
+    grok.name('tabbedview_view-assigned_forwardings')
+    grok.context(IInbox)
+
+    displayStates = 'forwarding-state-open'
+
+    def get_base_query(self):
+        """Returns the base search query (sqlalchemy)
+        """
+
+        principal = 'inbox:%s' % get_client_id()
+
+        query_util = getUtility(ITaskQuery)
+        return query_util._get_tasks_for_responsible_query(
+            principal, self.sort_on, self.sort_order).filter(
+            Task.review_state == self.displayStates)
--- opengever/inbox/browser/assignedtasks.py
+++ opengever/inbox/browser/assignedtasks.py
@@ -1,26 +1,11 @@
+from opengever.inbox.browser.assignedforwardings import InboxAssignedForwardings
 from five import grok
-from opengever.globalindex.interfaces import ITaskQuery
-from opengever.globalindex.model.task import Task
-from opengever.ogds.base.utils import get_client_id
-from opengever.tabbedview.browser.tasklisting import GlobalTaskListingTab
 from opengever.inbox.inbox import IInbox
-from zope.component import getUtility
 
 
-class InboxAssignedTasks(GlobalTaskListingTab):
-    """Listing tab for listing all tasks which are assigned to this inbox.
-    """
+class InboxAssignedTasks(InboxAssignedForwardings):
 
     grok.name('tabbedview_view-assigned_tasks')
     grok.context(IInbox)
 
-    def get_base_query(self):
-        """Returns the base search query (sqlalchemy)
-        """
-
-        principal = 'inbox:%s' % get_client_id()
-
-        query_util = getUtility(ITaskQuery)
-        return query_util._get_tasks_for_responsible_query(
-            principal, self.sort_on, self.sort_order).filter(
-            Task.review_state == 'forwarding-state-open')
+    displayStates = 'task-state-open'
