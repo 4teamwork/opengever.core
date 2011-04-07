040506398618e1ea0f36509fd8e41550b2a7b9eb
Lukas Graf <lukas.graf@4teamwork.ch>
2011-04-07 10:26:31 +0200

    Moved ISendable to opengever.mail to avoid circular dependency and renamed it to ISendableDocsContainer

--- opengever/inbox/inbox.py
+++ opengever/inbox/inbox.py
@@ -3,9 +3,9 @@ from opengever.inbox import _
 from plone.directives import form
 from opengever.tabbedview.browser.tabs import Tasks, Documents
 from zope import schema
-from opengever.inbox.interfaces import ISendable
+from opengever.mail.interfaces import ISendableDocsContainer
 
-class IInbox(form.Schema, ISendable):
+class IInbox(form.Schema, ISendableDocsContainer):
     """ Inbox for OpenGever
     """
 
--- opengever/inbox/interfaces.py
+++
@@ -1,5 +0,0 @@
-from zope.interface import Interface
-
-
-class ISendable(Interface):
-    """Markerinterface which defined that send_documents action is callable"""
