fee8f61bc01f29cf6c629e6b94ab454da32c4622
Lukas Graf <lukas.graf@4teamwork.ch>
2011-04-07 10:30:38 +0200

    Moved ISendable from opengever.inbox and renamed it to ISendableDocsContainer

--- opengever/mail/interfaces.py
+++ opengever/mail/interfaces.py
@@ -1,9 +1,16 @@
 from zope import schema
 from zope.interface import Interface
 
+
 class IMailSettings(Interface):
     mail_domain = schema.TextLine(
         title=u"Mail domain",
         description=u'Enter the mail domain which will be used \
             for sending mails into this site.',
         default=u'opengever.4teamwork.ch')
+
+
+class ISendableDocsContainer(Interface):
+    """Marker interface which states that the `send_documents` action is
+    callable on this container type.
+    """
