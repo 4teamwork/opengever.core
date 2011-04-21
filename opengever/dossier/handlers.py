d77a4253c4db20b664eb57339b4ca68e2b2f9dc7
Lukas Graf <lukas.graf@4teamwork.ch>
2011-04-21 12:23:49 +0200

    Added handler for IObjectModified that updates the ``containing_subdossier`` index for documents

--- opengever/dossier/handlers.py
+++ opengever/dossier/handlers.py
@@ -1,5 +1,7 @@
+from Acquisition import aq_inner, aq_parent
 from five import grok
 from Products.CMFCore.interfaces import IActionSucceededEvent
+from zope.lifecycleevent.interfaces import IObjectModifiedEvent
 from opengever.dossier.behaviors.dossier import IDossierMarker
 
 
@@ -20,3 +22,17 @@ def deactivate_subdossiers(dossier, event):
         wft = dossier.portal_workflow
         for subdossier in subdossiers:
             wft.doActionFor(subdossier.getObject(), 'dossier-transition-deactivate')
+
+@grok.subscribe(IDossierMarker, IObjectModifiedEvent)
+def reindex_contained_documents(dossier, event):
+    """When a subdossier is modified, we update the ``containing_subdossier``
+    index of all contained documents so they don't show a stale title in the
+    ``subdossier`` column
+    """
+    parent = aq_parent(aq_inner(dossier))
+    is_subdossier = IDossierMarker.providedBy(parent)
+    if is_subdossier:
+        documents = dossier.getFolderContents(
+            contentFilter={'portal_type': 'opengever.document.document'})
+        for doc in documents:
+            doc.getObject().reindexObject(idxs=['containing_subdossier'])
