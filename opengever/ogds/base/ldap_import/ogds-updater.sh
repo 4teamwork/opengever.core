98267675c2c1178424cde46e212bb0652dce2f2c
Zope <zope@0000oglx09>
2011-04-29 14:01:24 +0200

    Bash script for autamatically updating OGDS on 0000oglx09.zg.ch

---
+++ opengever/ogds/base/ldap_import/ogds-updater.sh
@@ -0,0 +1,4 @@
+export ORACLE_HOME=/usr/lib/oracle/11.2/client/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME
+
+/home/zope/plone01-ska-arch/bin/instance0 run /home/zope/ogds-updater/opengever/ogds/base/ldap_import/sync_ldap.py -s ska-arch
