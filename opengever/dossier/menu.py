5bf6b7ba7a0cd98a3d55c23c8abfcd81365286e0
Philippe Gross <philippegross@Philippe-Grosss-MacBook-Pro.local>
2011-05-24 16:25:50 +0200

    added PostFactoryMenu, for overwriting the names of subdossier in the add menu

---
+++ opengever/dossier/menu.py
@@ -0,0 +1,27 @@
+from five import grok
+from opengever.dossier.behaviors.dossier import IDossierMarker,IDossier
+from ftw.contentmenu.interfaces import IContentmenuPostFactoryMenu
+from zope.interface import Interface
+from opengever.dossier import _
+
+class DossierPostFactoryMenu(grok.MultiAdapter):
+    """If a task is added to another task, it is called subtask. So we need
+    to change the name of the task in the add-menu if we are in a task.
+    """
+
+    grok.adapts(IDossierMarker, Interface)
+    grok.implements(IContentmenuPostFactoryMenu)
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def __call__(self, factories):
+        import pdb; pdb.set_trace( )
+        if not IDossierMarker.providedBy(self.context):
+            # use default
+            return factories
+        for factory in factories:
+            if factory['extra']['id'] == u'opengever-dossier-businesscasedossier':
+                factory['title'] = _(u'Subdossier')
+        return factories
