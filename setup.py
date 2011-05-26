a9c4c9d67c7ad3799983a97877eb8935ef08aa2b
Lukas Graf <lukas.graf@4teamwork.ch>
2011-05-26 08:49:04 +0200

    Removed dependency on plone.app.async

--- setup.py
+++ setup.py
@@ -45,7 +45,6 @@ setup(name='opengever.tabbedview',
         'opengever.task',
         'plone.app.dexterity',
         'z3c.autoinclude',
-        'plone.app.async',
           #'collective.jqhistory'
           # -*- Extra requirements: -*-
       ],
