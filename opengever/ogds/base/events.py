7f7ad435c7e8e934efcd94c81539ded675508156
philippe.gross <philippe.gross@4teamwork.ch>
2011-06-16 12:13:57 +0200

    removed pdb

--- opengever/ogds/base/events.py
+++ opengever/ogds/base/events.py
@@ -3,7 +3,6 @@ from opengever.ogds.base.utils import create_session
 def setup_oracle_collation(event):
     """Setup collation for oracle engines.
     """
-    import pdb; pdb.set_trace()
     engine = event.engine
     if engine.name == 'oracle':
         engine.execute('alter session set NLS_SORT=BINARY_CI')
