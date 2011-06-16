39d0b35433d3fa24396304717c04f76d7bd95494
philippe.gross <philippe.gross@4teamwork.ch>
2011-06-16 12:12:57 +0200

    fixed collation setter, so it works fine

--- opengever/ogds/base/events.py
+++ opengever/ogds/base/events.py
@@ -1,7 +1,15 @@
+from opengever.ogds.base.utils import create_session
+
 def setup_oracle_collation(event):
     """Setup collation for oracle engines.
     """
+    import pdb; pdb.set_trace()
     engine = event.engine
     if engine.name == 'oracle':
         engine.execute('alter session set NLS_SORT=BINARY_CI')
         engine.execute('alter session set NLS_COMP=LINGUISTIC')
+
+    session = create_session()
+    if session.bind.name == 'oracle':
+        session.bind.execute('alter session set NLS_SORT=BINARY_CI')
+        session.bind.execute('alter session set NLS_COMP=LINGUISTIC')
