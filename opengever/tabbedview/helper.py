a2a64088762794b8a7b780bca0d6ec45ed33c0f9
Lukas Graf <lukas.graf@4teamwork.ch>
2011-06-17 08:49:59 +0200

    Extended `linked` helper to compute CSS classes for icons

--- opengever/tabbedview/helper.py
+++ opengever/tabbedview/helper.py
@@ -79,7 +79,21 @@ def linked(item, value):
     elif hasattr(item, 'absolute_url'):
         url_method = item.absolute_url
 
-    css_class = getattr(item, 'css_icon_class', '')
+    normalize = getUtility(IIDNormalizer).normalize
+
+    if not item.portal_type == 'opengever.document.document':
+        css_class = "contenttype-%s" % normalize(item.portal_type)
+    else:
+        # It's a document, we therefore want to display an icon
+        # for the mime type of the contained file
+        icon = getattr(item, 'getIcon', '')
+        if not icon == '':
+            # Strip '.gif' from end of icon name and remove leading 'icon_'
+            filetype = icon[:icon.rfind('.')].replace('icon_', '')
+            css_class = 'icon-%s' % normalize(filetype)
+        else:
+            # Fallback for unknown file type
+            css_class = "contenttype-%s" % normalize(item.portal_type)
 
     breadcrumb_titles = []
     for elem in item.breadcrumb_titles:
