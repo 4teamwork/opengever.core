cd05d63c6476b4be117f686a203eff34e062da29
philippe.gross <philippe.gross@4teamwork.ch>
2011-06-24 12:04:29 +0200

    moved css_class_from_brain helper method from opengever.dossier to base

--- opengever/dossier/browser/overview.py
+++ opengever/dossier/browser/overview.py
@@ -1,30 +1,12 @@
 from five import grok
+from opengever.base.browser.helper import css_class_from_brain
 from opengever.dossier import _ as _dossier
 from opengever.dossier.behaviors.dossier import IDossierMarker, IDossier
 from opengever.dossier.behaviors.participation import IParticipationAware
 from opengever.globalindex.utils import indexed_task_link_helper
 from opengever.ogds.base.interfaces import IContactInformation
 from opengever.tabbedview.browser.tabs import OpengeverTab
-from plone.i18n.normalizer.interfaces import IIDNormalizer
 from zope.component import getUtility
-
-
-def css_class_from_brain(item):
-    normalize = getUtility(IIDNormalizer).normalize
-    if not item.portal_type == 'opengever.document.document':
-        css_class = "contenttype-%s" % normalize(item.portal_type)
-    else:
-        # It's a document, we therefore want to display an icon
-        # for the mime type of the contained file
-        icon = getattr(item, 'getIcon', '')
-        if not icon == '':
-            # Strip '.gif' from end of icon name and remove leading 'icon_'
-            filetype = icon[:icon.rfind('.')].replace('icon_', '')
-            css_class = 'icon-%s' % normalize(filetype)
-        else:
-            # Fallback for unknown file type
-            css_class = "contenttype-%s" % normalize(item.portal_type)
-    return css_class
 
 
 class DossierOverview(grok.View, OpengeverTab):
