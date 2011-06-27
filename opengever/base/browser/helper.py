6a7b0670bfa4d4684119b414a2d80bbcd02564bb
Timon Tschanz <t.tschanz@4teamwork.ch>
2011-06-27 17:59:27 +0200

    added css_class_from_obj helper

--- opengever/base/browser/helper.py
+++ opengever/base/browser/helper.py
@@ -35,3 +35,20 @@ def css_class_from_brain(item):
             # Fallback for unknown file type
             css_class = "contenttype-%s" % normalize(item.portal_type)
     return css_class
+
+def css_class_from_obj(item):
+    normalize = getUtility(IIDNormalizer).normalize
+    if not item.portal_type == 'opengever.document.document':
+        css_class = "contenttype-%s" % normalize(item.portal_type)
+    else:
+        # It's a document, we therefore want to display an icon
+        # for the mime type of the contained file
+        icon = item.getIcon()
+        if not icon == '':
+            # Strip '.gif' from end of icon name and remove leading 'icon_'
+            filetype = icon[:icon.rfind('.')].replace('icon_', '')
+            css_class = 'icon-%s' % normalize(filetype)
+        else:
+            # Fallback for unknown file type
+            css_class = "contenttype-%s" % normalize(item.portal_type)
+    return css_class
