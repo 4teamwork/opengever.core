594a77a31ddef6438880bea82b046ee1f2824228
Timon Tschanz <t.tschanz@4teamwork.ch>
2011-06-27 18:06:11 +0200

    Added ByLine modifications for sprites

--- opengever/inbox/viewlets/byline.py
+++ opengever/inbox/viewlets/byline.py
@@ -1,6 +1,7 @@
 """Defines a customized byline for inbox"""
 from plone.app.layout.viewlets import content
 from opengever.mail.behaviors import IMailInAddressMarker, IMailInAddress
+from opengever.base.browser.helper import css_class_from_obj
 
 
 class InboxByline(content.DocumentBylineViewlet):
@@ -8,6 +9,10 @@ class InboxByline(content.DocumentBylineViewlet):
 
     update = content.DocumentBylineViewlet.update
 
+    def css_class_from_obj(self):
+        return css_class_from_obj(self.context)
+
+    
     def email(self):
         """Gets Email and display it in Byline"""
         if IMailInAddressMarker.providedBy(self.context):
