16c36cbd692532be1b0281252b8d7762ceb03f8e
philippe.gross <philippe.gross@4teamwork.ch>
2011-06-29 18:24:47 +0200

    fixed the collation setter, set now with a poollistener on the sqlalchemy engine

--- opengever/ogds/base/events.py
+++ opengever/ogds/base/events.py
@@ -1,14 +1,28 @@
-from opengever.ogds.base.utils import create_session
+# the sqlalchemy 0.7 way:
+# from sqlalchemy import events
+from sqlalchemy.interfaces import PoolListener
+
+# the sqlalchemy 0.7 way:
+# def alter_session_on_connect(dbapi_connection, connection_record):
+#     """
+#     """
+#     dbapi_connection.execute('alter session set NLS_SORT=BINARY_CI')
+#     dbapi_connection.execute('alter session set NLS_COMP=LINGUISTIC')
+
+
+class SetupCollationListener(PoolListener):
+
+    def connect(self, dbapi_con, con_record):
+        '''perform connect operations'''
+        cursor = dbapi_con.cursor()
+        cursor.execute('alter session set NLS_SORT=BINARY_CI')
+        cursor.execute('alter session set NLS_COMP=LINGUISTIC')
 
 def setup_oracle_collation(event):
     """Setup collation for oracle engines.
     """
     engine = event.engine
     if engine.name == 'oracle':
-        engine.execute('alter session set NLS_SORT=BINARY_CI')
-        engine.execute('alter session set NLS_COMP=LINGUISTIC')
-
-    session = create_session()
-    if session.bind.name == 'oracle':
-        session.bind.execute('alter session set NLS_SORT=BINARY_CI')
-        session.bind.execute('alter session set NLS_COMP=LINGUISTIC')
+        engine.pool.add_listener(SetupCollationListener())
+        # the sqlalchemy 0.7 way:
+        #events.listen(engine, 'connect', alter_session_on_connect)
