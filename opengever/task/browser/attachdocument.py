59fec0d6cd9b6a5157d372c5a507a54694608b0f
philippe.gross <philippe.gross@4teamwork.ch>
2011-07-19 09:19:52 +0200

    Added missing parameters for the js_template call in the ExtendedAutocompleteSelectionWidget,   works now with the newest version of the plone.formwidget.autocomplete

--- opengever/task/browser/attachdocument.py
+++ opengever/task/browser/attachdocument.py
@@ -114,6 +114,9 @@ class ExtendedAutocompleteSelectionWidget(AutocompleteSelectionWidget):
                                        url=url,
                                        minLength=self.minLength,
                                        js_callback=js_callback,
+                                       klass=self.klass,
+                                       title=self.title,
+                                       input_type=self.input_type,
                                        js_extra=self.js_extra())
 
 
