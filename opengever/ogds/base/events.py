021f3d37956bf5ce9e43ba8dc5fe53c5d39fc3b1
Lukas Graf <lukas.graf@4teamwork.ch>
2011-08-24 15:46:56 +0200

    Setting transaction isolation level when using SQLite engine.

--- opengever/ogds/base/events.py
+++ opengever/ogds/base/events.py
@@ -18,11 +18,26 @@ class SetupCollationListener(PoolListener):
         cursor.execute('alter session set NLS_SORT=BINARY_CI')
         cursor.execute('alter session set NLS_COMP=LINGUISTIC')
 
-def setup_oracle_collation(event):
-    """Setup collation for oracle engines.
+
+class SetupIsolationLevelListener(PoolListener):
+
+    def connect(self, dbapi_con, con_record):
+        '''perform connect operations'''
+        # This is needed to make (releasing) savepoints work properly with
+        # SQLite (needed in tests, particularly opengever.inbox).
+        # See http://stackoverflow.com/questions/2036378/using-savepoints-in-python-sqlite3
+        dbapi_con.isolation_level = None
+
+
+def setup_engine_options(event):
+    """Setup engine / connection options.
+        - collation for ORACLE engines
+        - transaction isolation level for SQLite engines
     """
     engine = event.engine
     if engine.name == 'oracle':
         engine.pool.add_listener(SetupCollationListener())
         # the sqlalchemy 0.7 way:
         #events.listen(engine, 'connect', alter_session_on_connect)
+    elif engine.name == 'sqlite':
+        engine.pool.add_listener(SetupIsolationLevelListener())
