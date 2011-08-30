aeb3e491c7dddb5abb759bdfb0e6447547fdf5fc
Lukas Graf <lukas.graf@4teamwork.ch>
2011-08-30 10:40:55 +0200

    Enabling versioning for documents in repositorytool.xml, custom setuphandler is not needed any more

--- opengever/document/setuphandlers.py
+++ opengever/document/setuphandlers.py
@@ -3,29 +3,6 @@ import logging
 # The profile id of your package:
 PROFILE_ID = 'profile-opengever.document:default'
 
-def setup_versioning(context):
-
-    # Ordinarily, GenericSetup handlers check for the existence of XML files.
-    # Here, we are not parsing an XML file, but we use this text file as a
-    # flag to check that we actually meant for this import step to be run.
-    # The file is found in profiles/default.
-    if context.readDataFile('opengever.document_versioning.txt') is None:
-        return
-
-    portal = context.getSite()
-
-    # enable manual versioning for Document type
-    types = [
-        'opengever.document.document',
-        ]
-    pr = getToolByName(portal, 'portal_repository')
-    versionable_types = list(pr.getVersionableContentTypes())
-    for type in types:
-        if type not in versionable_types:
-            versionable_types.append(type)
-        pr.addPolicyForContentType(type, 'version_on_revert')
-    pr.setVersionableContentTypes(versionable_types)
-
 
 def add_catalog_indexes(context, logger=None):
     """Method to add our wanted indexes to the portal_catalog.
