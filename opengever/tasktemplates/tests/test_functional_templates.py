2511c3be73eda2aa2c251f5bd53f49d644745889
Elio Schmutz <e.schmutz@4teamwork.ch>
2011-10-03 08:25:18 +0200

    added functional-tests for templates

---
+++ opengever/tasktemplates/tests/test_functional_templates.py
@@ -0,0 +1,17 @@
+from opengever.tasktemplates.browser.tasktemplates import preselected_helper
+from plone.mocktestcase import MockTestCase
+
+
+class TestFunctionalTemplates(MockTestCase):
+    """ Functional tests
+    """
+
+    def test_preselected_helper(self):
+
+        # Context
+        mock_context = self.mocker.mock()
+
+        self.replay()
+
+        self.assertEquals('preselected_yes', preselected_helper(mock_context, True))
+        self.assertEquals('', preselected_helper(mock_context, False))
