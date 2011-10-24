86e51d737a25d717426d3b42f18b6e488c240b4d
philippe.gross <philippe.gross@4teamwork.ch>
2011-10-24 16:40:20 +0200

    Adjust the task report view, so that it export only the selected tasks, which are stored in the request.

--- opengever/globalindex/browser/report.py
+++ opengever/globalindex/browser/report.py
@@ -4,7 +4,6 @@ from opengever.globalindex import _
 from Products.statusmessages.interfaces import IStatusMessage
 from opengever.globalindex.interfaces import ITaskQuery
 from zope.component import getUtility
-from opengever.ogds.base.utils import get_client_id
 from opengever.base.reporter import format_datetime, get_date_style
 from opengever.base.reporter import readable_author
 from opengever.base.reporter import StringTranslater, XLSReporter
@@ -20,6 +19,18 @@ class TaskReporter(grok.View):
     grok.require('zope2.View')
 
     def render(self):
+
+        if not self.request.get('task_ids'):
+            msg = _(
+                u'error_no_items', default=u'You have not selected any Items')
+            IStatusMessage(self.request).addStatusMessage(msg, type='error')
+            return self.request.RESPONSE.redirect(
+                self.request.form['orig_template'])
+
+        ids = self.request.get('task_ids', [])
+
+        query = getUtility(ITaskQuery)
+        tasks = query.get_tasks(ids)
 
         task_attributes = [
             {'id':'title', 'title':_('label_title')},
@@ -40,9 +51,6 @@ class TaskReporter(grok.View):
             {'id':'sequence_number', 'title':_('label_sequence_number')},
             {'id':'client_id', 'title':_('label_client_id')},
         ]
-
-        query_util = getUtility(ITaskQuery)
-        tasks = query_util.get_task_for_assigned_client(get_client_id)
 
         reporter = XLSReporter(self.context.REQUEST, task_attributes, tasks)
 
