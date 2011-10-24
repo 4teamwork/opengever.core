d5fbe5e7388083daab9c8e5b793a58a3dc5a2033
philippe.gross <philippe.gross@4teamwork.ch>
2011-10-24 16:34:56 +0200

    Adjust the dossier reporting view, so that it must be called from a tabbeview listing (object paths stored in the request).

--- opengever/dossier/browser/report.py
+++ opengever/dossier/browser/report.py
@@ -3,21 +3,38 @@ from opengever.base.reporter import format_datetime, get_date_style
 from opengever.base.reporter import readable_author
 from opengever.base.reporter import StringTranslater, XLSReporter
 from opengever.dossier import _
-from Products.CMFPlone.interfaces import IPloneSiteRoot
+from plone.dexterity.interfaces import IDexterityContent
 from Products.statusmessages.interfaces import IStatusMessage
 
 
 class DossierReporter(grok.View):
     """View that generate an excel spreadsheet with the XLSReporter,
-    which list all dossier and their important attributes.
+    which list the selected dossier (paths in request)
+    and their important attributes.
     """
 
-    grok.context(IPloneSiteRoot)
+    grok.context(IDexterityContent)
     grok.name('dossier_report')
     grok.require('zope2.View')
 
     def render(self):
 
+        if not self.request.get('paths'):
+            msg = _(
+                u'error_no_items', default=u'You have not selected any Items')
+            IStatusMessage(self.request).addStatusMessage(msg, type='error')
+            return self.request.RESPONSE.redirect(
+                self.request.form['orig_template'])
+
+        # get the given dossiers
+        portal_catalog = self.context.portal_catalog
+        dossiers = []
+        for path in self.request.get('paths'):
+            dossiers.append(
+                portal_catalog(path={'query': path, 'depth': 0})[0]
+                )
+
+        # attributes mapping
         dossier_attributes = [
             {'id':'Title', 'title':_('label_title')},
             {'id':'start', 'title':_('label_start'),
@@ -27,18 +44,14 @@ class DossierReporter(grok.View):
             {'id':'responsible', 'title':_('label_responsible'),
              'transform':readable_author},
             {'id':'filing_no', 'title':_('filing_no')},
-            {'id':'review_state', 'title':_('review_state'),
+            {'id':'review_state',
+             'title':_('label_review_state', default='Review state'),
              'transform':StringTranslater(
                 self.context.REQUEST, 'plone').translate},
             {'id':'reference', 'title':_('label_reference_number')},
         ]
 
-
-        dossiers = self.context.portal_catalog(
-            object_provides = \
-                'opengever.dossier.behaviors.dossier.IDossierMarker',
-            is_subdossier=False)
-
+        # generate the xls data with the XLSReporter
         reporter = XLSReporter(
             self.context.REQUEST, dossier_attributes, dossiers)
 
