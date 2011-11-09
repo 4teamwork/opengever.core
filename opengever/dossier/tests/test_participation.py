4303b6e912b460cd7af7c1cc5b589a106177d1d0
philippe.gross <philippe.gross@4teamwork.ch>
2011-11-09 10:20:51 +0100

    made participation test pyflakes and pep-8 valid

--- opengever/dossier/tests/test_participation.py
+++ opengever/dossier/tests/test_participation.py
@@ -1,19 +1,11 @@
 from grokcore.component.testing import grok
 from mocker import ANY
 from opengever.dossier.behaviors.participation import ParticipationHandler
-from opengever.dossier.interfaces import IParticipationCreated, IParticipationRemoved
-from opengever.dossier.testing import OPENGEVER_DOSSIER_INTEGRATION_TESTING
-from plone.dexterity.utils import createContentInContainer
+from opengever.dossier.interfaces import IParticipationCreated
+from opengever.dossier.interfaces import IParticipationRemoved
 from plone.mocktestcase import MockTestCase
-from z3c.form.testing import TestRequest
 from zope.annotation.interfaces import IAnnotations
-from zope.component import provideUtility
 from zope.interface import Interface
-from zope.interface import implements
-from zope.schema.interfaces import IVocabularyFactory
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-import transaction
-import unittest2 as unittest
 
 
 class TestParticipationHanlder(MockTestCase):
@@ -28,12 +20,14 @@ class TestParticipationHanlder(MockTestCase):
         self.mock_adapter(annotation_adapter, IAnnotations, [Interface, ])
 
         created_handler = self.mocker.mock()
-        self.expect(created_handler(ANY)).result('created event fired').count(0, 3)
-        self.mock_handler(created_handler, [IParticipationCreated,])
+        self.expect(
+            created_handler(ANY)).result('created event fired').count(0, 3)
+        self.mock_handler(created_handler, [IParticipationCreated, ])
 
         removed_handler = self.mocker.mock()
-        self.expect(removed_handler(ANY)).result('removed event fired').count(0,1)
-        self.mock_handler(removed_handler, [IParticipationRemoved,])
+        self.expect(
+            removed_handler(ANY)).result('removed event fired').count(0, 1)
+        self.mock_handler(removed_handler, [IParticipationRemoved, ])
 
     def test_participation_with_handler(self):
         self.replay()
@@ -41,15 +35,15 @@ class TestParticipationHanlder(MockTestCase):
 
         # creation
         peter = handler.create_participation(
-            {'contact':'peter', 'roles':['Reader',],})
+            {'contact': 'peter', 'roles': ['Reader', ], })
         sepp = handler.create_participation(
-            {'contact':'sepp', 'roles':['Reader','Editor'],})
+            {'contact': 'sepp', 'roles': ['Reader', 'Editor'], })
         hugo = handler.create_participation(
-            {'contact':'hugo'})
+            {'contact': 'hugo'})
 
         # test appending
         handler.append_participiation(peter)
-        self.assertEquals(handler.get_participations(), [peter,])
+        self.assertEquals(handler.get_participations(), [peter, ])
 
         handler.append_participiation(sepp)
         self.assertEquals(handler.get_participations(), [peter, sepp])
@@ -63,4 +57,4 @@ class TestParticipationHanlder(MockTestCase):
 
         # test removing
         handler.remove_participation(peter)
-        self.assertEquals(handler.get_participations(), [sepp,])
+        self.assertEquals(handler.get_participations(), [sepp, ])
