b0bfe59dfbda4f08f070e511040483ae19d19269
philippe.gross <philippe.gross@4teamwork.ch>
2011-11-09 09:19:07 +0100

    Expand templatedossier test, with a form view test.

--- opengever/dossier/tests/test_templatedossier.py
+++ opengever/dossier/tests/test_templatedossier.py
@@ -1,7 +1,12 @@
+from Testing.makerequest import makerequest
 from opengever.dossier.templatedossier import ITemplateDossier
 from opengever.dossier.testing import OPENGEVER_DOSSIER_INTEGRATION_TESTING
+from plone.app.testing import setRoles, TEST_USER_ID
 from plone.dexterity.interfaces import IDexterityFTI
+from plone.dexterity.utils import createContentInContainer
+from zope.app.component.hooks import setSite
 from zope.component import createObject, queryUtility
+import transaction
 import unittest2 as unittest
 
 
@@ -11,8 +16,8 @@ class TestTemplateDossierIntegration(unittest.TestCase):
 
     def test_adding(self):
         portal = self.layer['portal']
-        portal.invokeFactory('opengever.dossier.templatedossier', 'document1')
-        d1 = portal['document1']
+        portal.invokeFactory('opengever.dossier.templatedossier', 'templatedossier')
+        d1 = portal['templatedossier']
         self.failUnless(ITemplateDossier.providedBy(d1))
 
     def test_fti(self):
@@ -33,6 +38,24 @@ class TestTemplateDossierIntegration(unittest.TestCase):
         new_object = createObject(factory)
         self.failUnless(ITemplateDossier.providedBy(new_object))
 
-    def test_templatefolder_utility(self):
+    def test_templatefolder_view(self):
+        portal = self.layer['portal']
+        setRoles(portal, TEST_USER_ID, [
+                'Manager', 'Reader', 'Member', 'Contributor', 'Editor'])
+        portal = makerequest(portal)
+        setSite(portal)
+        templates = createContentInContainer(portal, 'opengever.dossier.templatedossier', title="templates")
+        dossier = createContentInContainer(
+            portal, 'opengever.dossier.businesscasedossier', title="dossier")
+        dossier = makerequest(dossier)
 
-        pass
+        createContentInContainer(
+            templates, 'opengever.document.document', title="empty document template")
+
+        dossier.REQUEST['ACTUAL_URL'] = dossier.absolute_url()
+        templates.reindexObject()
+        transaction.commit()
+        view = dossier.restrictedTraverse('document_with_template')
+        self.failUnless(view())
+        self.assertTrue('empty document template', view.templates())
+        import pdb; pdb.set_trace()
