c70dc98be01c8a6cea4b48f0c5f79c7a564b6e1e
philippe.gross <philippe.gross@4teamwork.ch>
2011-12-29 18:59:29 +0100

    Added test for the containing_dossier indexer.

--- opengever/dossier/tests/test_indexer.py
+++ opengever/dossier/tests/test_indexer.py
@@ -1,8 +1,9 @@
-from opengever.dossier.businesscase import IBusinessCaseDossier
+from Products.CMFCore.utils import getToolByName
+from opengever.base.behaviors.base import IOpenGeverBase
 from opengever.dossier.testing import OPENGEVER_DOSSIER_INTEGRATION_TESTING
-# from plone.dexterity.interfaces import IDexterityFTI
-# from zope.component import createObject
-# from zope.component import queryUtility
+from zope.event import notify
+from zope.interface import Interface
+from zope.lifecycleevent import ObjectModifiedEvent, Attributes
 import unittest2 as unittest
 
 
@@ -10,12 +11,50 @@ class Testindexers(unittest.TestCase):
 
     layer = OPENGEVER_DOSSIER_INTEGRATION_TESTING
 
-    # def test_containing_subdossierx(self):
+    def test_containing_dossier(self):
+        portal = self.layer['portal']
+        portal.invokeFactory('opengever.dossier.businesscasedossier', 'dossier')
+        dossier = portal.get('dossier')
+        IOpenGeverBase(dossier).title=u"Testdossier XY"
+        dossier.reindexObject()
 
-    # XXX
-    # Don't work yet because its not possible to access to the dependent vocabulary from opengever.octopus.tentacle
-    # def test_view(self):
-    #     self.folder.invokeFactory('opengever.dossier.businesscasedossier', 'dossier1')
-    #     d1 = self.folder['dossier1']
-    #     view = d1.restrictedTraverse('@@view')
-    #     self.failUnless(view())
+        dossier.invokeFactory('opengever.dossier.businesscasedossier', 'subdossier')
+        subdossier = dossier.get('subdossier')
+        subdossier.reindexObject()
+
+        dossier.invokeFactory('opengever.document.document', 'document')
+        document = dossier.get('document')
+        document.reindexObject()
+
+        catalog = getToolByName(portal, 'portal_catalog')
+
+        self.assertEquals(
+            catalog(
+                path='/'.join(subdossier.getPhysicalPath()))[0].containing_dossier,
+            u'Testdossier XY'
+            )
+
+        self.assertEquals(
+            catalog(
+                path='/'.join(document.getPhysicalPath()))[0].containing_dossier,
+            u'Testdossier XY'
+            )
+
+        #check subscriber for catch editing maindossier titel
+        IOpenGeverBase(dossier).title=u"Testdossier CHANGED"
+        dossier.reindexObject()
+        notify(
+            ObjectModifiedEvent(dossier, Attributes(Interface, 'IOpenGeverBase.title')))
+
+        self.assertEquals(
+            catalog(
+                path='/'.join(subdossier.getPhysicalPath()))[0].containing_dossier,
+            u'Testdossier CHANGED'
+            )
+
+        self.assertEquals(
+            catalog(
+                path='/'.join(document.getPhysicalPath()))[0].containing_dossier,
+            u'Testdossier CHANGED'
+            )
+
