6f444003edf4f7ce2cde3620d5ba7ddfc334a8fd
Timon Tschanz <timon.tschanz@4teamwork.ch>
2012-01-26 17:44:37 +0100

    changed grok context to make resolveoguid work everywere

--- opengever/base/browser/resolveoguid.py
+++ opengever/base/browser/resolveoguid.py
@@ -1,18 +1,18 @@
 from AccessControl.SecurityManagement import SpecialUsers
 from Products.CMFCore.utils import getToolByName
-from Products.CMFPlone.interfaces.siteroot import IPloneSiteRoot
 from five import grok
 from opengever.ogds.base.interfaces import IContactInformation
 from opengever.ogds.base.utils import get_client_id
 from zExceptions import NotFound
 from zope.component import getUtility
 from zope.intid.interfaces import IIntIds
+from zope.interface import Interface
 
 
 class ResolveOGUIDView(grok.View):
 
     grok.name('resolve_oguid')
-    grok.context(IPloneSiteRoot)
+    grok.context(Interface)
     grok.require('zope2.View')
 
     def render(self):
