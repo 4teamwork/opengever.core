fa762844b4bd7b367e973fe4b048bf89f522ff7d
philippe.gross <philippe.gross@4teamwork.ch>
2012-03-06 18:39:50 +0100

    Added Temporary fix for Title accessor to return unicode instead of utf-8.

--- opengever/contact/contact.py
+++ opengever/contact/contact.py
@@ -209,6 +209,14 @@ class Contact(Item):
         """
         pass
 
+    # XXX: Temporary fix for Title accessor
+    # return unicode instead of utf-8 bytestring
+    # because other opengever types to the same
+    def Title(self):
+        if isinstance(self.title, str):
+            return self.title.decode('utf-8')
+        return self.title
+
 
 @indexer(IContact)
 def contactid(obj):
