56188e292e5bddf20020e7f8dd5a3ce5b7f13834
philippe.gross <philippe.gross@4teamwork.ch>
2012-04-27 11:36:48 +0200

    Activate sequenceNumber and referencenumber giving for mails.

--- opengever/mail/indexer.py
+++ opengever/mail/indexer.py
@@ -1,8 +1,11 @@
+from collective import dexteritytextindexer
+from datetime import datetime
 from five import grok
 from ftw.mail import utils
 from ftw.mail.mail import IMail
+from opengever.base.interfaces import IReferenceNumber, ISequenceNumber
 from plone.indexer import indexer
-from datetime import datetime
+from zope.component import getAdapter, getUtility
 
 
 @indexer(IMail)
@@ -58,3 +61,27 @@ def checked_out(obj):
 
     return ''
 grok.global_adapter(checked_out, name='checked_out')
+
+
+class SearchableTextExtender(grok.Adapter):
+    """Specifix SearchableText Extender for the mail"""
+
+    grok.context(IMail)
+    grok.name('IOGMail')
+    grok.implements(dexteritytextindexer.IDynamicTextIndexExtender)
+
+    def __init__(self, context):
+        self.context = context
+
+    def __call__(self):
+        searchable = []
+        # append some other attributes to the searchableText index
+        # reference_number
+        refNumb = getAdapter(self.context, IReferenceNumber)
+        searchable.append(refNumb.get_number())
+
+        # sequence_number
+        seqNumb = getUtility(ISequenceNumber)
+        searchable.append(str(seqNumb.get_number(self.context)))
+
+        return ' '.join(searchable)
--- opengever/mail/tests/test_indexers.py
+++ opengever/mail/tests/test_indexers.py
@@ -1,10 +1,20 @@
+from collective.dexteritytextindexer.interfaces import \
+    IDynamicTextIndexExtender
 from datetime import date
+from ftw.mail.mail import IMail
 from ftw.testing import MockTestCase
+from opengever.base.interfaces import IReferenceNumber, ISequenceNumber
 from opengever.mail.indexer import document_author, document_date, receipt_date
 from opengever.mail.indexer import sortable_author, checked_out
+from zope.component import getAdapters, getAdapter
+from zope.interface import Interface
+from grokcore.component.testing import grok
 
 
 class TestMailIndexers(MockTestCase):
+
+    def setUp(self):
+        grok('opengever.mail.indexer')
 
     def test_checked_out(self):
         self.assertEqual(checked_out(None)(), '')
@@ -13,22 +23,25 @@ class TestMailIndexers(MockTestCase):
         mail = self.stub()
         msg = self.stub()
         self.expect(mail.msg).result(msg)
-        self.expect(msg.has_key('From')).result(True)
-        self.expect(msg.get('From')).result(u'"Hugo Boss" <hugo.boss@boss.com>')
+        self.expect('From' in msg).result(True)
+        self.expect(msg.get('From')).result(
+            u'"Hugo Boss" <hugo.boss@boss.com>')
         self.replay()
 
         self.assertEqual(
-            document_author(mail)(), '"Hugo Boss" &lt;hugo.boss@boss.com&gt;')
+            document_author(mail)(),
+            '"Hugo Boss" &lt;hugo.boss@boss.com&gt;')
 
         self.assertEqual(
-            sortable_author(mail)(), '"Hugo Boss" &lt;hugo.boss@boss.com&gt;')
+            sortable_author(mail)(),
+            '"Hugo Boss" &lt;hugo.boss@boss.com&gt;')
 
     def test_date_indexer(self):
         mail = self.stub()
         msg = self.stub()
 
         self.expect(mail.msg).result(msg)
-        self.expect(msg.has_key('Date')).result(True)
+        self.expect('Date' in msg).result(True)
         self.expect(msg.get('Date')).result('Thu, 16 Feb 2012 17:16:03 +0100')
         self.replay()
 
@@ -39,3 +52,26 @@ class TestMailIndexers(MockTestCase):
         self.assertEqual(
             receipt_date(mail)(),
             date(2012, 2, 16))
+
+    def test_reference_number(self):
+        mail = self.providing_stub([IMail, ])
+
+        ref_adapter = self.mocker.mock()
+        self.expect(ref_adapter(mail)).result(ref_adapter)
+        self.expect(
+            ref_adapter.get_number()).result('OG 1.6 / 3 / 1')
+        self.mock_adapter(
+            ref_adapter, IReferenceNumber, [Interface])
+
+        seq_adapter = self.mocker.mock()
+        self.expect(seq_adapter.get_number(mail)).result('55')
+        self.mock_utility(seq_adapter, ISequenceNumber)
+
+        self.replay()
+
+        extenders = getAdapters([mail, ], IDynamicTextIndexExtender)
+        self.assertEquals(len([aa for aa in extenders]), 1)
+
+        extender = getAdapter(
+            mail, IDynamicTextIndexExtender, u'IOGMail')
+        self.assertEquals(extender(), 'OG 1.6 / 3 / 1 55')
