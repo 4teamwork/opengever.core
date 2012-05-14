2ecdfadd0f023e160cf515fba925ff153ed21fe0
philippe.gross <philippe.gross@4teamwork.ch>
2012-05-14 17:48:45 +0200

    Added custom sort for the task_type column.

--- opengever/tabbedview/utils.py
+++ opengever/tabbedview/utils.py
@@ -1,6 +1,7 @@
 from Products.CMFCore.utils import getToolByName
 from plone.memoize import ram
 from zope.i18n import translate
+from opengever.task.util import getTaskTypeVocabulary
 
 
 @ram.cache(lambda m, c, r: 'translated_transitions_cache_key')
@@ -13,3 +14,13 @@ def get_translated_transitions(context, request):
         for state in wft.get(wid).states:
             states[state] = translate(state, domain='plone', context=request)
     return states
+
+
+@ram.cache(lambda m, c, r: 'translated_types_cache_key')
+def get_translated_types(context, request):
+    values = {}
+    for key, terms in getTaskTypeVocabulary(context).by_value.items():
+        values[key] = terms.title.lower()
+
+    print values
+    return values
