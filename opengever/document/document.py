237979b81d5cc57e1e816c66d82f6d4701c45c1b
philippe.gross <philippe.gross@4teamwork.ch>
2012-05-16 08:47:47 +0200

    Moved all indexers to a single indexers file.

--- opengever/document/document.py
+++ opengever/document/document.py
@@ -1,4 +1,4 @@
-from Acquisition import aq_inner, aq_base, aq_parent
+from Acquisition import aq_inner, aq_parent
 from Products.CMFCore.utils import getToolByName
 from Products.MimetypesRegistry.common import MimeTypeException
 from collective import dexteritytextindexer
@@ -6,15 +6,12 @@ from collective.elephantvocabulary import wrap_vocabulary
 from datetime import date
 from five import grok
 from ftw.datepicker.widget import DatePickerFieldWidget
-from opengever.base.interfaces import IReferenceNumber, ISequenceNumber
 from opengever.document import _
-from opengever.document.interfaces import ICheckinCheckoutManager
 from opengever.dossier.behaviors.dossier import IDossierMarker
 from opengever.mail.behaviors import IMailInAddress
 from opengever.ogds.base.interfaces import IContactInformation
 from opengever.tabbedview.browser.tabs import OpengeverTab
 from opengever.tabbedview.browser.tabs import Tasks
-from opengever.tabbedview.helper import readable_ogds_author
 from plone.app.layout.viewlets.interfaces import IBelowContentTitle
 from plone.app.versioningbehavior.behaviors import IVersionable
 from plone.autoform.interfaces import OMITTED_KEY
@@ -22,17 +19,15 @@ from plone.dexterity.content import Item
 from plone.directives import form, dexterity
 from plone.directives.dexterity import DisplayForm
 from plone.i18n.normalizer.interfaces import IIDNormalizer
-from plone.indexer import indexer
 from plone.namedfile.field import NamedBlobFile
 from plone.supermodel.interfaces import FIELDSETS_KEY
 from plone.supermodel.model import Fieldset
 from plone.z3cform.textlines.textlines import TextLinesFieldWidget
 from z3c.form import validator
 from z3c.form.browser import checkbox
-from zc.relation.interfaces import ICatalog
 from zope import schema
 from zope.app.intid.interfaces import IIntIds
-from zope.component import getUtility, queryMultiAdapter, getAdapter
+from zope.component import getUtility
 from zope.globalrequest import getRequest
 from zope.i18n import translate
 from zope.interface import invariant, Invalid, Interface
@@ -322,125 +317,6 @@ class Document(Item):
             # not found
             return False
         return mimetypeitem
-
-
-@indexer(IDocumentSchema)
-def related_items(obj):
-    catalog = getUtility(ICatalog)
-    intids = getUtility(IIntIds)
-
-    try:
-        obj_id = intids.getId(aq_base(obj))
-    # In some cases we might not have an intid yet.
-    except KeyError:
-        return None
-
-    results = []
-    relations = catalog.findRelations(
-        {'to_id': obj_id, 'from_attribute': 'relatedItems'})
-    for rel in relations:
-        results.append(rel.from_id)
-    return results
-
-
-grok.global_adapter(related_items, name='related_items')
-
-
-class SearchableTextExtender(grok.Adapter):
-    """Specifix SearchableText Extender for document"""
-
-    grok.context(IDocumentSchema)
-    grok.name('IDocumentSchema')
-    grok.implements(dexteritytextindexer.IDynamicTextIndexExtender)
-
-    def __init__(self, context):
-        self.context = context
-
-    def __call__(self):
-        searchable = []
-        # append some other attributes to the searchableText index
-        # reference_number
-        refNumb = getAdapter(self.context, IReferenceNumber)
-        searchable.append(refNumb.get_number())
-
-        # sequence_number
-        seqNumb = getUtility(ISequenceNumber)
-        searchable.append(str(seqNumb.get_number(self.context)))
-
-        return ' '.join(searchable)
-
-
-@indexer(IDocumentSchema)
-def document_author(obj):
-    """document_author indexer"""
-
-    context = aq_inner(obj)
-    if not context.document_author:
-        return None
-    elif isinstance(context.document_author, unicode):
-        return context.document_author.encode('utf-8')
-    else:
-        return context.document_author
-grok.global_adapter(document_author, name='document_author')
-
-
-@indexer(IDocumentSchema)
-def document_date(obj):
-    """document_date indexer"""
-
-    context = aq_inner(obj)
-    if not context.document_date:
-        return None
-    return context.document_date
-grok.global_adapter(document_date, name='document_date')
-
-
-@indexer(IDocumentSchema)
-def receipt_date(obj):
-    """receipt_date indexer, can handle None Value"""
-    context = aq_inner(obj)
-    if not context.receipt_date:
-        return None
-    return context.receipt_date
-grok.global_adapter(receipt_date, name='receipt_date')
-
-
-@indexer(IDocumentSchema)
-def delivery_date(obj):
-    """delivery_date indexer"""
-    context = aq_inner(obj)
-    if not context.delivery_date:
-        return None
-    return context.delivery_date
-grok.global_adapter(delivery_date, name='delivery_date')
-
-
-@indexer(IDocumentSchema)
-def checked_out(obj):
-    """checked_out indexer, save the userid of the
-    Member who checked the document out"""
-    manager = queryMultiAdapter((obj, obj.REQUEST), ICheckinCheckoutManager)
-    if not manager:
-        return ''
-
-    value = manager.checked_out()
-    if value:
-        return value
-
-    else:
-        return ''
-grok.global_adapter(checked_out, name='checked_out')
-
-
-@indexer(IDocumentSchema)
-def sortable_author(obj):
-    """Index to allow users to sort on document_author."""
-    author = obj.document_author
-    if author:
-        readable_author = readable_ogds_author(obj, author)
-        return readable_author
-    return ''
-grok.global_adapter(sortable_author, name='sortable_author')
 
 
 @grok.subscribe(IDocumentSchema, IObjectCreatedEvent)
