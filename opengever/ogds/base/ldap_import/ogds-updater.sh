5b6f57ea37c9e8b004d02b9f1dc93abd5f8ddf69
Lukas Graf <lukas.graf@4teamwork.ch>
2012-07-03 16:44:42 +0200

    Fixed paths to accomodate for new location of OG clients

--- opengever/ogds/base/ldap_import/ogds-updater.sh
+++ opengever/ogds/base/ldap_import/ogds-updater.sh
@@ -1,5 +1,5 @@
 export ORACLE_HOME=/usr/lib/oracle/11.2/client/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME
 
-/home/zope/plone01-ska-arch/bin/instance0 run /home/zope/ogds-updater/opengever/ogds/base/ldap_import/sync_ldap.py -s ska-arch
-/home/zope/plone01-ska-arch/bin/instance0 run /home/zope/ogds-updater/opengever/ogds/base/ldap_import/sync_ldap.py -s 'ska-arch' -c 'opengever.ogds.base.group-import'
+/home/zope/clients/plone01-ska-arch/bin/instance0 run /home/zope/ogds-updater/opengever/ogds/base/ldap_import/sync_ldap.py -s ska-arch -c 'opengever.ogds.base.user-import'
+/home/zope/clients/plone01-ska-arch/bin/instance0 run /home/zope/ogds-updater/opengever/ogds/base/ldap_import/sync_ldap.py -s ska-arch -c 'opengever.ogds.base.group-import'
