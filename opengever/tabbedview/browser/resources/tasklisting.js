0f5edf4555fa6aceaa7f5865a99764f3729b5c32
philippe.gross <philippe.gross@4teamwork.ch>
2012-10-23 09:45:11 +0200

    Adjust javscripts: Use $ instead of deprecated jq.

--- opengever/tabbedview/browser/resources/tasklisting.js
+++ opengever/tabbedview/browser/resources/tasklisting.js
@@ -1,10 +1,10 @@
-jq(function(){
+$(function(){
 
     review_state_filter = function(set_filter){
         tabbedview.prop('review_state', set_filter);
         tabbedview.reload_view();
     };
 
-    jq('#filter_all').live('click', function(){review_state_filter(false);});
-    jq('#filter_open').live('click', function(){review_state_filter(true);});
+    $('#filter_all').live('click', function(){review_state_filter(false);});
+    $('#filter_open').live('click', function(){review_state_filter(true);});
 });
