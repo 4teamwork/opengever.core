aa11f667319fcbe0e76858aa315e2725316220a7
Timon Tschanz <timon.tschanz@4teamwork.ch>
2012-10-26 16:56:02 +0200

    make test pass in plone 4.2

--- opengever/base/tests/test_source_binder.py
+++ opengever/base/tests/test_source_binder.py
@@ -13,18 +13,13 @@ class TestSourceBinder(unittest.TestCase):
         setRoles(portal, TEST_USER_ID, ['Reviewer', 'Manager'])
 
         # Repository 1
-        createContentInContainer(portal, 'opengever.repository.repositoryroot', title="Ordnungssystem1")
-        reporoot1 = portal['ordnungssystem1']
+        reporoot1 = createContentInContainer(portal, 'opengever.repository.repositoryroot', title="Ordnungssystem1")
         createContentInContainer(reporoot1, 'opengever.repository.repositoryfolder', title="Ordnungsposition1")
-        createContentInContainer(reporoot1, 'opengever.repository.repositoryfolder', title="Ordnungsposition2")
+        repofolder2 = createContentInContainer(reporoot1, 'opengever.repository.repositoryfolder', title="Ordnungsposition2")
         createContentInContainer(reporoot1, 'opengever.repository.repositoryfolder', title="Ordnungsposition3")
-        repofolder2 = reporoot1['opengever.repository.repositoryfolder-1']
-        createContentInContainer(repofolder2, 'opengever.repository.repositoryfolder', title="Ordnungsposition2-1")
-        self.repofolder2_1 = repofolder2['opengever.repository.repositoryfolder']
-
+        self.repofolder2_1 = createContentInContainer(repofolder2, 'opengever.repository.repositoryfolder', title="Ordnungsposition2-1")
         # Repository 2
-        createContentInContainer(portal, 'opengever.repository.repositoryroot', title="Ordnungssystem2")
-        reporoot2 = portal['ordnungssystem2']
+        reporoot2 = createContentInContainer(portal, 'opengever.repository.repositoryroot', title="Ordnungssystem2")
         createContentInContainer(reporoot2, 'opengever.repository.repositoryfolder', title="Ordnungsposition4")
         createContentInContainer(reporoot2, 'opengever.repository.repositoryfolder', title="Ordnungsposition5")
         createContentInContainer(reporoot2, 'opengever.repository.repositoryfolder', title="Ordnungsposition6")
