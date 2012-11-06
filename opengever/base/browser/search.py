9b5c81a4ecf7136ebd1e5c496ae1dc03679c6819
Lukas Graf <lukas.graf@4teamwork.ch>
2012-11-06 14:41:53 +0100

    Cleanup.

--- opengever/base/browser/search.py
+++ opengever/base/browser/search.py
@@ -1,4 +1,5 @@
 from plone.app.search.browser import Search
 
 class OpengeverSearch(Search):
-    """Customizing the plone defaul Search View"""
+    """Customizing the plone default Search View.
+    """
