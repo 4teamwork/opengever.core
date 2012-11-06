02bab441ae17f54de6743a19c1a1777eb0b8833d
philippe.gross <philippe.gross@4teamwork.ch>
2012-11-06 10:01:24 +0100

    Moved helper doctests in to the test_helpers test.

--- opengever/tabbedview/tests/test_helpers.py
+++ opengever/tabbedview/tests/test_helpers.py
@@ -2,6 +2,7 @@ from ftw.testing import MockTestCase
 from mocker import ANY
 from opengever.ogds.base.interfaces import IContactInformation
 from opengever.tabbedview.helper import linked_ogds_author
+from opengever.tabbedview.helper import task_id_checkbox_helper
 from opengever.tabbedview.utils import get_containg_document_tab_url
 from zope.interface import Interface
 
@@ -36,3 +37,16 @@ class TestDocumentsUrl(MockTestCase):
         self.assertEquals(
             linked_ogds_author(brain, u'james.bond'), 'james.bond')
         self.assertEquals(linked_ogds_author(brain, None), '')
+
+    def test_task_id_checkbox_helper(self):
+
+        brain = self.stub()
+        self.expect(brain.title).result('The brain of a task')
+        self.expect(brain.task_id).result('123')
+
+        self.replay()
+
+        self.assertEquals(
+            task_id_checkbox_helper(brain, ''),
+            '<input class="noborder selectable" id="123" name="task_ids:list" '
+            'title="Select The brain of a task" type="checkbox" value="123" />')
