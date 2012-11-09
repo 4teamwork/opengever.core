44d3d64b505d0b8231d3af94c4fe4031bec58fc8
philippe.gross <philippe.gross@4teamwork.ch>
2012-11-09 15:27:39 +0100

    Adjust db_connection setup, so it works fine with SQLAlchemy 0.7.x.
    Support for older SQLAlchemy than 0.7.x is dropped.Merge branch 'pg_1694_dossier_statefilter'

--- opengever/ogds/base/events.py
+++ opengever/ogds/base/events.py
@@ -1,33 +1,21 @@
-# the sqlalchemy 0.7 way:
-# from sqlalchemy import events
-from sqlalchemy.interfaces import PoolListener
-
-# the sqlalchemy 0.7 way:
-# def alter_session_on_connect(dbapi_connection, connection_record):
-#     """
-#     """
-#     dbapi_connection.execute('alter session set NLS_SORT=GERMAN_CI')
-#     dbapi_connection.execute('alter session set NLS_COMP=LINGUISTIC')
+from sqlalchemy.event import listen
 
 
-class SetupCollationListener(PoolListener):
+def alter_session_on_connect(dbapi_connection, connection_record):
+    """perform connect operations"""
 
-    def connect(self, dbapi_con, con_record):
-        '''perform connect operations'''
-        cursor = dbapi_con.cursor()
-        cursor.execute('alter session set NLS_SORT=GERMAN_CI')
-        cursor.execute('alter session set NLS_COMP=LINGUISTIC')
+    cursor = dbapi_connection.cursor()
+    cursor.execute('alter session set NLS_SORT=GERMAN_CI')
+    cursor.execute('alter session set NLS_COMP=LINGUISTIC')
 
 
-class SetupIsolationLevelListener(PoolListener):
+def setup_isolation_level_on_connect(dbapi_connection, connection_record):
+    # This is needed to make (releasing) savepoints work properly with
+    # SQLite (needed in tests, particularly opengever.inbox).
+    # See http://stackoverflow.com/questions/2036378/
+    # using-savepoints-in-python-sqlite3
 
-    def connect(self, dbapi_con, con_record):
-        '''perform connect operations'''
-        # This is needed to make (releasing) savepoints work properly with
-        # SQLite (needed in tests, particularly opengever.inbox).
-        # See http://stackoverflow.com/questions/2036378/
-        #    using-savepoints-in-python-sqlite3
-        dbapi_con.isolation_level = None
+    dbapi_connection.isolation_level = None
 
 
 def setup_engine_options(event):
@@ -37,8 +25,8 @@ def setup_engine_options(event):
     """
     engine = event.engine
     if engine.name == 'oracle':
-        engine.pool.add_listener(SetupCollationListener())
         # the sqlalchemy 0.7 way:
-        #events.listen(engine, 'connect', alter_session_on_connect)
+        listen(engine, 'connect', alter_session_on_connect)
+
     elif engine.name == 'sqlite':
-        engine.pool.add_listener(SetupIsolationLevelListener())
+        listen(engine, 'connect', setup_isolation_level_on_connect)
