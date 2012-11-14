50ed937a36afce9da5f8ad607c17fd07a53cd845
philippe.gross <philippe.gross@4teamwork.ch>
2012-11-14 13:25:07 +0100

    Activate extended tooltips also for the relateddocuments listing.

--- opengever/tabbedview/browser/tooltip_source.py
+++ opengever/tabbedview/browser/tooltip_source.py
@@ -20,6 +20,7 @@ class OpengeverTabbedviewTooltipSource(object):
     def tooltips(self):
         return [{
             'selector': u'.tabbedview-tooltip',
-            'condition': '#documents_overview, #mydocuments_overview',
+            'condition': '#documents_overview, #mydocuments_overview, ' \
+                         '#relateddocuments_overview',
             'content': u'.tabbedview-tooltip-data',
         }]
