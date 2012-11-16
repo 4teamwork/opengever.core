3a9b1994755fac17702e2852d04641427b7a1691
philippe.gross <philippe.gross@4teamwork.ch>
2012-11-16 17:00:51 +0100

    Implements new download block direct from the mockup.

--- opengever/document/browser/overview.py
+++ opengever/document/browser/overview.py
@@ -1,11 +1,22 @@
 from five import grok
+from opengever.base.browser.helper import get_css_class
 from opengever.document.document import IDocumentSchema
+from opengever.document.interfaces import ICheckinCheckoutManager
 from opengever.ogds.base.interfaces import IContactInformation
 from opengever.tabbedview.browser.tabs import OpengeverTab
 from plone.directives.dexterity import DisplayForm
-from zope.component import getUtility
-from opengever.base.browser.helper import get_css_class
+from zope.component import getUtility, queryMultiAdapter
 
+
+try:
+    from opengever.pdfconverter.behaviors.preview import IPreviewMarker
+    from opengever.pdfconverter.behaviors.preview import IPreview
+    from opengever.pdfconverter.behaviors.preview import \
+        CONVERSION_STATE_CONVERTING
+
+    PDFCONVERTER_AVAILABLE = True
+except ImportError:
+    PDFCONVERTER_AVAILABLE = False
 
 
 class Overview(DisplayForm, OpengeverTab):
@@ -17,6 +28,34 @@ class Overview(DisplayForm, OpengeverTab):
         info = getUtility(IContactInformation)
         return info.render_link(self.context.Creator())
 
+    def checked_out_link(self):
+        manager = queryMultiAdapter(
+            (self.context, self.request), ICheckinCheckoutManager)
+
+        if manager.checked_out():
+            info = getUtility(IContactInformation)
+            return info.render_link(manager.checked_out())
+
+        return None
+
     def get_css_class(self):
         return get_css_class(self.context)
 
+    def is_preview_supported(self):
+        # XXX TODO: should be
+        if PDFCONVERTER_AVAILABLE:
+            return IPreviewMarker.providedBy(self.context)
+        return False
+
+    def is_pdf_download_available(self):
+        if self.is_preview_supported():
+            if IPreview(
+                self.context).conversion_state == CONVERSION_STATE_READY:
+                return True
+        return False
+
+    def is_checkout_and_edit_available(self):
+        manager = queryMultiAdapter(
+            (self.context, self.request), ICheckinCheckoutManager)
+
+        return manager.is_checkout_allowed()
