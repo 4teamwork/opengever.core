4bf6a8dd67519cf9d31ea134d81f0ed4e39278dd
philippe.gross <philippe.gross@4teamwork.ch>
2012-11-26 14:55:40 +0100

    Search: Display the whole breadcrumbs instead of cropping after 3 elements.

--- opengever/base/browser/search.py
+++ opengever/base/browser/search.py
@@ -1,5 +1,15 @@
 from plone.app.search.browser import Search
+from zope.component import getMultiAdapter
+
 
 class OpengeverSearch(Search):
     """Customizing the plone default Search View.
     """
+
+    def breadcrumbs(self, item):
+        obj = item.getObject()
+        view = getMultiAdapter((obj, self.request), name='breadcrumbs_view')
+        # cut off the item itself
+        breadcrumbs = list(view.breadcrumbs())[:-1]
+
+        return breadcrumbs
