c42606c57643d345c086b711d16412145f25a245
philippe.gross <philippe.gross@4teamwork.ch>
2012-12-03 11:27:54 +0100

    Disalbe searchableText check in p.a.search filter_query.

--- opengever/base/browser/search.py
+++ opengever/base/browser/search.py
@@ -13,3 +13,18 @@ class OpengeverSearch(Search):
         breadcrumbs = list(view.breadcrumbs())[:-1]
 
         return breadcrumbs
+
+    def filter_query(self, query):
+        """The filter query of the standard search view (plone.app.search)
+        cancel the query generation if not SearchableText is given.
+        In some case (for example in opengever.advancedsearch), we generate
+        also searches without a searchabletext. So we temporarily fake
+        the SearchableText."""
+
+        if not self.request.form.get('SearchableText', None):
+            self.request.form['SearchableText'] = 'temporary fake text'
+            query = super(OpengeverSearch, self).filter_query(query)
+            query['SearchableText'] = ''
+            return query
+
+        return super(OpengeverSearch, self).filter_query(query)
