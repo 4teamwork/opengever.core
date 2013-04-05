21732eede93c18a36ea0e8f00e387dfb7f5cbdbb
Lukas Graf <lukas.graf@4teamwork.ch>
2013-04-05 11:13:43 +0200

    Added upgrade-step to properly migrate class for ftw.mail.mail objs.

--- opengever/mail/upgrades/to2100.py
+++ opengever/mail/upgrades/to2100.py
@@ -1,7 +1,11 @@
+from ftw.upgrade import ProgressLogger
 from ftw.upgrade import UpgradeStep
 from opengever.mail.interfaces import ISendDocumentConf
-from zope.component import getUtility
+from opengever.mail.mail import OGMail
 from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
+from zope.event import notify
+from zope.lifecycleevent import ObjectModifiedEvent
 
 
 class AddRegistryEntries(UpgradeStep):
@@ -16,3 +20,19 @@ class AddRegistryEntries(UpgradeStep):
         # update the registry entries
         for interface in interfaces:
             registry.registerInterface(interface)
+
+class MigrateMailClass(UpgradeStep):
+
+    def __call__(self):
+        catalog = self.getToolByName('portal_catalog')
+        brains = catalog(portal_type='ftw.mail.mail')
+
+        with ProgressLogger('Migrating ftw.mail.mail class', brains) as step:
+            for brain in brains:
+                self.migrate_obj(brain.getObject())
+                step()
+
+    def migrate_obj(self, obj):
+        self.migrate_class(obj, OGMail)
+        notify(ObjectModifiedEvent(obj))
+
