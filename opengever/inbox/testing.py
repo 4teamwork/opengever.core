618ea764923fba5e1d22d7e22d9d532e6794ebb2
Jonas Baumann <jone@jone.ch>
2013-04-26 15:59:52 +0200

    Use z3c.autoinclude in testing layer.

--- opengever/inbox/testing.py
+++ opengever/inbox/testing.py
@@ -23,13 +23,18 @@ class InboxFunctionalLayer(PloneSandboxLayer):
         from opengever.ogds.base import setuphandlers
         setuphandlers.setup_scriptable_plugin = lambda *a, **kw: None
 
-        import ftw.contentmenu
-        xmlconfig.file('configure.zcml', package=ftw.contentmenu,
-                       context=configurationContext)
 
-        from opengever.inbox import tests
-        xmlconfig.file('testing.zcml', package=tests,
-                       context=configurationContext)
+        xmlconfig.string(
+            '<configure xmlns="http://namespaces.zope.org/zope">'
+
+            '  <include package="z3c.autoinclude" file="meta.zcml" />'
+            '  <includePlugins package="plone" />'
+            '  <includePluginsOverrides package="plone" />'
+
+            '  <include package="opengever.ogds.base" file="tests.zcml" />'
+
+            '</configure>',
+            context=configurationContext)
 
     def setUpPloneSite(self, portal):
 
