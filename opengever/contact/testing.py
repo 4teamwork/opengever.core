7b47088271b9acfdbe9c134392deb1d559ac3478
Jonas Baumann <jone@jone.ch>
2013-05-01 14:59:16 +0200

    Use OPENGEVER_FIXTURE for opengever.contact tests.

--- opengever/contact/testing.py
+++ opengever/contact/testing.py
@@ -1,38 +1,16 @@
-from plone.app.testing import applyProfile
+from opengever.core.testing import OPENGEVER_FIXTURE
 from plone.app.testing import IntegrationTesting, FunctionalTesting
 from plone.app.testing import PloneSandboxLayer
-from plone.app.testing import PLONE_FIXTURE
-from zope.configuration import xmlconfig
-from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
+from plone.app.testing import setRoles
 
 
 class ContactIntegrationLayer(PloneSandboxLayer):
     """Layer for integration tests."""
 
-    defaultBases = (PLONE_FIXTURE,)
-
-    def setUpZope(self, app, configurationContext):
-        # do not install pas plugins (doesnt work in tests)
-        from opengever.ogds.base import setuphandlers
-        setuphandlers.setup_scriptable_plugin = lambda *a, **kw: None
-
-        xmlconfig.string(
-            '<configure xmlns="http://namespaces.zope.org/zope">'
-
-            '  <include package="z3c.autoinclude" file="meta.zcml" />'
-            '  <includePlugins package="plone" />'
-            '  <includePluginsOverrides package="plone" />'
-
-            '  <include package="opengever.ogds.base" file="tests.zcml" />'
-
-            '</configure>',
-            context=configurationContext)
+    defaultBases = (OPENGEVER_FIXTURE,)
 
     def setUpPloneSite(self, portal):
-        applyProfile(portal, 'opengever.ogds.base:default')
-        applyProfile(portal, 'opengever.contact:default')
-        applyProfile(portal, 'opengever.tabbedview:default')
         setRoles(portal, TEST_USER_ID, ['Member', 'Contributor', 'Manager'])
 
 
