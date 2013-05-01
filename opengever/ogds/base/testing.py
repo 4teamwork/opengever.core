78184a33766e9cba59716a0d287e53ed7ebf079d
Jonas Baumann <jone@jone.ch>
2013-05-01 17:15:17 +0200

    Use OPENGEVER_FIXTURE for opengever.ogds.base tests.

--- opengever/ogds/base/testing.py
+++ opengever/ogds/base/testing.py
@@ -1,15 +1,12 @@
-from opengever.globalindex import model as task_model
+from opengever.core.testing import OPENGEVER_FIXTURE
+from opengever.core.testing import setup_sql_tables
+from opengever.core.testing import truncate_sql_tables
 from opengever.ogds.base.interfaces import IClientConfiguration
-from opengever.ogds.base.setuphandlers import create_sql_tables, MODELS
-from opengever.ogds.base.utils import create_session
 from plone.app.testing import IntegrationTesting
-from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
-from plone.app.testing import applyProfile
 from plone.dexterity import utils
 from plone.registry.interfaces import IRegistry
 from zope.component import getUtility
-from zope.configuration import xmlconfig
 
 
 def create_contacts(portal):
@@ -47,52 +44,24 @@ def create_contacts(portal):
 
 class BaseLayer(PloneSandboxLayer):
 
-    defaultBases = (PLONE_FIXTURE, )
-
-    def setUpZope(self, app, configurationContext):
-        # do not install pas plugins (doesnt work in tests)
-        from opengever.ogds.base import setuphandlers
-        setuphandlers.setup_scriptable_plugin = lambda *a, **kw: None
-
-        xmlconfig.string(
-            '<configure xmlns="http://namespaces.zope.org/zope">'
-
-            '  <include package="z3c.autoinclude" file="meta.zcml" />'
-            '  <includePlugins package="plone" />'
-            '  <includePluginsOverrides package="plone" />'
-
-            '  <include package="opengever.ogds.base" file="tests.zcml" />'
-
-            '</configure>',
-            context=configurationContext)
+    defaultBases = (OPENGEVER_FIXTURE, )
 
     def setUpPloneSite(self, portal):
-        # Install into Plone site using portal_setup
-        applyProfile(portal, 'plone.app.registry:default')
-        applyProfile(portal, 'opengever.contact:default')
-        applyProfile(portal, 'opengever.task:default')
-        applyProfile(portal, 'opengever.ogds.base:default')
         # configure client ID
         registry = getUtility(IRegistry, context=portal)
         client = registry.forInterface(IClientConfiguration)
         client.client_id = u'client1'
-        # portal workaround
-        self.portal = portal
 
     def testSetUp(self):
-        # setup the sql tables
-        create_sql_tables()
-        session = create_session()
-        task_model.Base.metadata.create_all(session.bind)
+        super(BaseLayer, self).testSetUp()
+        setup_sql_tables()
 
-    def testTearDown(test):
-        session = create_session()
-        for model in MODELS:
-            getattr(model, 'metadata').drop_all(session.bind)
-        getattr(task_model.Base, 'metadata').drop_all(session.bind)
-        # we may have created custom users and
+    def testTearDown(self):
+        super(BaseLayer, self).testTearDown()
+        truncate_sql_tables()
 
 
 OPENGEVER_OGDS_BASE_FIXTURE = BaseLayer()
 OPENGEVER_OGDS_BASE_TESTING = IntegrationTesting(
-    bases=(OPENGEVER_OGDS_BASE_FIXTURE,), name="OpengeverOgdsBase:Integration")
+    bases=(OPENGEVER_OGDS_BASE_FIXTURE,),
+    name="OpengeverOgdsBase:Integration")
