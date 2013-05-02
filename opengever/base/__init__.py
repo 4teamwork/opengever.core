2270cc11ed73b72bf11dca30f60596a09bea2acb
Philippe Gross <philippe.gross@4teamwork.ch>
2013-05-02 12:11:41 +0200

    Moved gever specific csv dialect, from zugpolicy.base in to the core, and adjust the sources.
    The escaping problems aren't a zug specific problem.

--- opengever/base/__init__.py
+++ opengever/base/__init__.py
@@ -1,5 +1,16 @@
 from zope.i18nmessageid import MessageFactory
+import csv
 
 _ = MessageFactory('opengever.base')
 
 VERSION = 'GEVER Version %(version)s'
+
+
+class OpenGeverCSVDialect(csv.excel):
+    """A CSV dialect based on the standard Excel dialect but with support for
+    escaped characters (double quotes in particular).
+    """
+    escapechar = '\\'
+
+
+csv.register_dialect(u'OpenGeverCSV', OpenGeverCSVDialect)
