fb932525da791fd35be24920d17a4725b50b577f
Yves Senn <yves.senn@gmail.com>
2013-05-03 10:32:05 +0200

    remove `opengever.trash` testing layers.

--- opengever/trash/tests/test_trash.py
+++ opengever/trash/tests/test_trash.py
@@ -1,35 +1,28 @@
-from Products.CMFCore.utils import getToolByName
-from opengever.core.testing import OPENGEVER_FUNCTIONAL_TESTING
-from opengever.trash.testing import OPENGEVER_TRASH_INTEGRATION_TESTING
-from opengever.trash.trash import ITrashable, ITrashed
+from opengever.core.testing import OPENGEVER_INTEGRATION_TESTING
+from opengever.trash.trash import ITrashed
 from plone.app.testing import TEST_USER_ID
-from plone.app.testing import TEST_USER_NAME
-from plone.app.testing import TEST_USER_PASSWORD
 from plone.app.testing import setRoles
 from plone.dexterity.fti import DexterityFTI
 from plone.dexterity.utils import createContentInContainer
-from plone.testing.z2 import Browser
-from zope.component import getUtility
-import transaction
 import unittest2 as unittest
 
 class TestTrash(unittest.TestCase):
 
-    layer = OPENGEVER_TRASH_INTEGRATION_TESTING
+    layer = OPENGEVER_INTEGRATION_TESTING
 
     def setUp(self):
         self.app = self.layer.get('app')
         self.portal = self.layer.get('portal')
-        self.request = self.layer.get('request')
+        setRoles(self.portal, TEST_USER_ID, ['Member', 'Contributor', 'Manager'])
 
     def test_trash(self):
         fti = DexterityFTI('TrashTestFTI',
                            klass="plone.dexterity.content.Container",
                            global_allow=True,
                            filter_content_types=False)
-        fti.behaviors = ('plone.app.content.interfaces.INameFromTitle', 'opengever.trash.trash.ITrashable')
+        fti.behaviors = ('plone.app.content.interfaces.INameFromTitle',
+                         'opengever.trash.trash.ITrashable')
         self.portal.portal_types._setObject('TrashTestFTI', fti)
-        # schema = fti.lookupSchema()
 
         utem1 = createContentInContainer(self.portal, 'TrashTestFTI',
                                          checkConstraints=False,
