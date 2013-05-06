57b33bebead4b7a8413b3b06b6d45e2d653df19a
Jonas Baumann <jone@jone.ch>
2013-05-06 10:57:28 +0200

    document testing: use sql helpers and move creating user to test.

--- opengever/document/testing.py
+++ opengever/document/testing.py
@@ -1,10 +1,7 @@
 from opengever.core.testing import OPENGEVER_FIXTURE
 from opengever.core.testing import truncate_sql_tables
-from opengever.ogds.base.setuphandlers import _create_example_user
-from opengever.ogds.base.utils import create_session
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import PloneSandboxLayer
-from plone.app.testing import TEST_USER_ID
 from plone.registry.interfaces import IRegistry
 from zope.component import getUtility
 
@@ -14,15 +11,6 @@ class DocumentLayer(PloneSandboxLayer):
     defaultBases = (OPENGEVER_FIXTURE, )
 
     def setUpPloneSite(self, portal):
-        session = create_session()
-
-        _create_example_user(session, portal,
-                             TEST_USER_ID,
-                             {'firstname': 'Hugo',
-                              'lastname': 'Boss',
-                              'email': 'hugo@boss.ch'},
-                             ('og_mandant1_users',))
-
         # fix registry
         registry = getUtility(IRegistry)
         registry['ftw.tabbedview.interfaces.ITabbedView.batch_size'] = 50
