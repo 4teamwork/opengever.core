466adcfc1d45d4cd53aedc204dc8ca8e53a40fc8
Yves Senn <yves.senn@gmail.com>
2013-05-10 15:33:43 +0200

    rework test_download_viewlet

--- opengever/base/tests/test_download_viewlet.py
+++ opengever/base/tests/test_download_viewlet.py
@@ -11,62 +11,46 @@ class TestDownloadViewlet(FunctionalTestCase):
         self.grant('Contributor')
         self.ptool = getToolByName(self.portal, 'plone_utils')
 
-    def test_download_viewlet(self):
-        ### Download viewlet
+    def test_without_file(self):
+        test_doc = createContentInContainer(self.portal,
+                                            'opengever.document.document',
+                                            title=u'Doc without file',
+                                            keywords=[])
 
-        ## Trying to call the viewlet on a document with no file
-        # Create a test document WITHOUT a file:
-        test_doc0 = createContentInContainer(self.portal,
-                                             'opengever.document.document',
-                                             title=u'Doc without file',
-                                             keywords=[])
-
-        # Get the file download viewlet and call it:
-        download_viewlet = test_doc0.restrictedTraverse('download_file_version')
-        # Response should be a redirect to context:
         self.assertEquals('http://nohost/plone/document-1',
-                          download_viewlet.render())
+                          self.download(test_doc))
 
-        # with status code 302 (Moved permanently):
-        self.assertResponseStatus(302)
+        self.assertResponseStatus(302) # Moved permanently
 
-        # and a portal status message:
         self.assertEquals(u'No file in in this version',
                           self.ptool.showPortalMessages()[-1].message)
-    
-        ## Calling the viewlet on a document with a file
-        # Create a test document with a file:
-        test_file1 = NamedBlobFile("lorem ipsum", filename=u"foobar.txt")
-        test_doc1 = createContentInContainer(self.portal,
-                                             'opengever.document.document',
-                                             title=u'Foobar',
-                                             keywords=[],
-                                             file=test_file1)
 
-        # Get the file download viewlet and call it:
-        download_viewlet1 = test_doc1.restrictedTraverse('download_file_version')
-        # Response should contain file data:
-        self.assertEquals('lorem ipsum', download_viewlet1.render())
+    def test_with_file(self):
+        test_file = NamedBlobFile("lorem ipsum", filename=u"foobar.txt")
+        test_doc = createContentInContainer(self.portal,
+                                            'opengever.document.document',
+                                            title=u'Foobar',
+                                            keywords=[],
+                                            file=test_file)
 
-        # Check that HTTP response headers are set correctly:
+        self.assertEquals('lorem ipsum', self.download(test_doc))
+
         self.assertResponseHeader('content-disposition', 'attachment; filename="foobar.txt"')
         self.assertResponseHeader('content-length', '11', )
         self.assertResponseHeader('content-type', 'text/plain')
 
+    def test_custom_content_type(self):
+        test_file = NamedBlobFile("some text", filename=u"sometext.txt")
+        test_file.contentType = 'foo/bar'
+        test_doc = createContentInContainer(self.portal,
+                                            'opengever.document.document',
+                                            title=u'Some Text',
+                                            keywords=[],
+                                            file=test_file)
 
-        ## Check that HTTP content-type header gets set correctly
-        # Create a test document with a file and a custom content type:
-        test_file2 = NamedBlobFile("some text", filename=u"sometext.txt")
-        test_file2.contentType = 'foo/bar'
-        test_doc2 = createContentInContainer(self.portal,
-                                             'opengever.document.document',
-                                             title=u'Some Text',
-                                             keywords=[],
-                                             file=test_file2)
-
-        # Get the file download viewlet and call it:
-        download_viewlet2 = test_doc2.restrictedTraverse('download_file_version')
-        download_viewlet2.render()
+        self.download(test_doc)
         self.assertResponseHeader('content-type', 'foo/bar')
 
-        # Check that HTTP content-type header is set correctly:
+    def download(self, document):
+        download_viewlet = document.restrictedTraverse('download_file_version')
+        return download_viewlet.render()
