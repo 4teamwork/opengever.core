fef55a279dcf0c7ecf7a1cf76f1a23112ec5a710
Yves Senn <yves.senn@gmail.com>
2013-05-10 16:24:06 +0200

    rework test_redirector

--- opengever/base/tests/test_redirector.py
+++ opengever/base/tests/test_redirector.py
@@ -6,30 +6,40 @@ class TestRedirector(FunctionalTestCase):
 
     def setUp(self):
         super(TestRedirector, self).setUp()
-        self.grant('Contributor')
         self.prepareSession()
 
-    def test_redirector(self):
+        self.redirector = IRedirector(self.request)
 
-        # Register a redirect:
-        redirector = IRedirector(request)
+    def test_stores_and_retrieves_redirects(self):
+        self.redirector.redirect('http://www.google.ch', target='named-window')
+        self.assertEquals([{'url': 'http://www.google.ch',
+                            'target': 'named-window',
+                            'timeout': 0}],
+                          self.redirector.get_redirects())
+
+    def test_retrieving_a_redirect_removes_it(self):
+        self.redirector.redirect('http://www.ebay.ch', target='named-window')
+
+        self.assertEquals(1, len(self.redirector.get_redirects()))
+        self.assertEquals(0, len(self.redirector.get_redirects()))
+
+    def test_pass_remove_false_to_keeps_redirects(self):
+        self.redirector.redirect('http://www.yahoo.com', target='named-window')
+
+        self.assertEquals(1, len(self.redirector.get_redirects(remove=False)))
+        self.assertEquals(1, len(self.redirector.get_redirects(remove=False)))
+
+class TestRedirectorViewlet(FunctionalTestCase):
+
+    def setUp(self):
+        super(TestRedirectorViewlet, self).setUp()
+        self.prepareSession()
+
+    def test_viewlet_only_redirects_once(self):
+        redirector = IRedirector(self.request)
         redirector.redirect('http://www.google.ch', target='named-window')
 
-        # Getting the redirect:
-        self.assertEquals([{'url': 'http://www.google.ch', 'target': 'named-window', 'timeout': 0}],
-                          redirector.get_redirects(remove=False))
-
-        self.assertEquals([{'url': 'http://www.google.ch', 'target': 'named-window', 'timeout': 0}],
-                          redirector.get_redirects(remove=True))
-
-        self.assertEquals([], redirector.get_redirects())
-
-        # Test the viewlet with another redirect:
-        redirector.redirect('http://www.google.ch', target='named-window')
-
-        # The redirection done with javascript:
-        viewlet = RedirectorViewlet(self.portal, request, {}, {})
+        viewlet = RedirectorViewlet(self.portal, self.request, {}, {})
         self.assertIn("http://www.google.ch", viewlet.render().strip())
 
-        # Only redirect once:
         self.assertEquals("", viewlet.render())
