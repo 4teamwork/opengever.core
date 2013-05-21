cf226931688ab5198aa8b0a4f50d7ae5453b0e2f
Philippe Gross <philippe.gross@4teamwork.ch>
2013-05-21 17:44:18 +0200

    Reworked plugins tests.

--- opengever/ogds/base/tests/test_plugin.py
+++ opengever/ogds/base/tests/test_plugin.py
@@ -1,55 +1,29 @@
 from opengever.ogds.base.Extensions.plugins import authenticate_credentials
 from opengever.ogds.base.Extensions.plugins import extract_user
-from opengever.ogds.base.setuphandlers import _create_example_client
-from opengever.ogds.base.utils import create_session
+from opengever.ogds.base.interfaces import IInternalOpengeverRequestLayer
 from opengever.testing import FunctionalTestCase
+from opengever.testing import create_client
 
 
 class TestRemoteAuthenticationPlugin(FunctionalTestCase):
 
     def setUp(self):
         super(TestRemoteAuthenticationPlugin, self).setUp()
-
         self.request = self.layer['request']
 
-    def test_plugins(self):
-        # ip definitions:
-        client1_ip = '192.168.1.233'
+    def test_plugin_with_valid_request(self):
+        client_ip = '192.168.1.233'
+        create_client('client1', ip_address=client_ip)
 
-        # create_clients:
-        session = create_session()
-        _create_example_client(session, 'client1',
-                                   {'title': 'Client 1',
-                                    'ip_address': client1_ip,
-                                    'site_url': 'http://nohost/client1',
-                                    'public_url': 'http://nohost/client1',
-                                    'group': 'client1_users',
-                                    'inbox_group': 'client1_inbox_users'})
+        # fake a remote request
+        self.request.environ['X_OGDS_AC'] = 'hugo.boss'
+        self.request.environ['X_OGDS_CID'] = 'client1'
+        self.request.environ['REMOTE_HOST'] = 'http://nohost/client1'
+        self.request._client_addr = client_ip
 
-        # the ip_address column should also work with a comma seperated list
+        self.portal.REQUEST = self.request
 
-        _create_example_client(session, 'client3',
-                                   {'title': 'Client 3',
-                                    'ip_address': '192.168.1.53,192.168.1.2',
-                                    'site_url': 'http://nohost/client3',
-                                    'public_url': 'http://nohost/client3',
-                                    'group': 'client3_users',
-                                    'inbox_group': 'client3_inbox_users'})
-
-
-        # extract_user:
-
-        # fake the request
-        self.request.environ['X_OGDS_AC']='hugo.boss'
-        self.request.environ['X_OGDS_CID']='client1'
-        self.request.environ['REMOTE_HOST']='http://nohost/client1'
-        self.request._client_addr=client1_ip
-
-        class Context(object): pass
-        obj = Context()
-        obj.REQUEST = self.request
-
-        creds = extract_user(obj, self.request)
+        creds = extract_user(self.portal, self.request)
         self.assertEquals(
             {'login': 'hugo.boss',
              'remote_host': 'http://nohost/client1',
@@ -58,15 +32,52 @@ class TestRemoteAuthenticationPlugin(FunctionalTestCase):
             creds)
 
         self.assertEquals(
-            ('hugo.boss', 'hugo.boss'), authenticate_credentials(obj, creds))
+            ('hugo.boss', 'hugo.boss'),
+            authenticate_credentials(self.portal, creds))
 
-        # fake a bad request (client != ip)
-        self.request.environ['X_OGDS_CID']='client3'
-        creds = extract_user(obj, self.request)
-        self.assertEquals(None, authenticate_credentials(obj, creds))
+        self.assertTrue(
+            IInternalOpengeverRequestLayer.providedBy(self.request))
 
-        # the plugin should also work with a client with a comma seperated list of ip_adresses
-        self.request._client_addr='192.168.1.2'
-        creds = extract_user(obj, self.request)
+    def test_plugin_with_multiple_valid_ips(self):
+        """The plugin should also work with a client with a comma
+        seperated list of ip_adresses. """
+
+        create_client('client1', ip_address='192.168.1.53,192.168.1.2')
+
+        # fake a remote request
+        self.request.environ['X_OGDS_AC'] = 'hugo.boss'
+        self.request.environ['X_OGDS_CID'] = 'client1'
+        self.request.environ['REMOTE_HOST'] = 'http://nohost/client1'
+        self.request._client_addr = '192.168.1.2'
+        self.portal.REQUEST = self.request
+
+        creds = extract_user(self.portal, self.request)
         self.assertEquals(
-            ('hugo.boss', 'hugo.boss'), authenticate_credentials(obj, creds))
+            ('hugo.boss', 'hugo.boss'),
+            authenticate_credentials(self.portal, creds))
+
+    def test_plugin_from_invalid_ip(self):
+        create_client('client1', ip_address='192.186.1.1')
+
+        # fake a remote request
+        self.request.environ['X_OGDS_AC'] = 'hugo.boss'
+        self.request.environ['X_OGDS_CID'] = 'client1'
+        self.request.environ['REMOTE_HOST'] = 'http://nohost/client1'
+        self.request._client_addr = '192.168.1.233'
+        self.portal.REQUEST = self.request
+
+        creds = extract_user(self.portal, self.request)
+        self.assertEquals(None, authenticate_credentials(self.portal, creds))
+
+    def test_plugin_from_not_existing_client(self):
+        create_client('client1', ip_address='192.186.1.1')
+
+        # fake a remote request
+        self.request.environ['X_OGDS_AC'] = 'hugo.boss'
+        self.request.environ['X_OGDS_CID'] = 'client3'
+        self.request.environ['REMOTE_HOST'] = 'http://nohost/client1'
+        self.request._client_addr = '192.168.1.1'
+        self.portal.REQUEST = self.request
+
+        creds = extract_user(self.portal, self.request)
+        self.assertEquals(None, authenticate_credentials(self.portal, creds))
