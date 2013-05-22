4ac8bd33387568d2835a6ec4f7cb1d8918f0555c
Philippe Gross <philippe.gross@4teamwork.ch>
2013-05-22 10:09:48 +0200

    Extend Builder API with the with_default option.

--- opengever/testing/builders.py
+++ opengever/testing/builders.py
@@ -1,5 +1,9 @@
 from plone.dexterity.utils import createContentInContainer
+from plone.dexterity.utils import iterSchemata
 from plone.namedfile.file import NamedBlobFile
+from z3c.form.interfaces import IValue
+from zope.component import queryMultiAdapter
+from zope.schema import getFieldsInOrder
 import transaction
 
 
@@ -25,6 +29,7 @@ class DexterityBuilder(object):
         self.session = session
         self.container = session.portal
         self.arguments = {"checkConstraints": False}
+        self.set_default_values = False
 
     def within(self, container):
         self.container = container
@@ -42,9 +47,15 @@ class DexterityBuilder(object):
         self.arguments.update(kwargs)
         return self
 
+    def with_default_values(self):
+        self.set_default_values = True
+        return self
+
     def create(self):
         self.before_create()
         obj = self.create_object()
+        if self.set_default_values:
+            self.set_defaults(obj)
         self.after_create()
         return obj
 
@@ -54,6 +65,25 @@ class DexterityBuilder(object):
     def after_create(self):
         if self.session.auto_commit:
             transaction.commit()
+
+    def set_defaults(self, obj):
+        for schemata in iterSchemata(obj):
+            for name, field in getFieldsInOrder(schemata):
+                if not field.get(field.interface(obj)):
+                    default = queryMultiAdapter(
+                        (obj, obj.REQUEST, None, field, None),
+                        IValue,
+                        name='default')
+                    if default is not None:
+                        default = default.get()
+                    if default is None:
+                        default = getattr(field, 'default', None)
+                    if default is None:
+                        try:
+                            default = field.missing_value
+                        except AttributeError:
+                            pass
+                    field.set(field.interface(obj), default)
 
 
 class DossierBuilder(DexterityBuilder):
