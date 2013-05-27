38378045e23b83022f84c6305a4bd0adc8a70619
Philippe Gross <philippe.gross@4teamwork.ch>
2013-05-27 14:56:47 +0200

    Reworked autocomplete widget tests.

--- opengever/ogds/base/tests/test_autocomplete_widget.py
+++ opengever/ogds/base/tests/test_autocomplete_widget.py
@@ -1,99 +1,60 @@
 from opengever.ogds.base.autocomplete_widget import AutocompleteFieldWidget
-from opengever.ogds.base.setuphandlers import _create_example_user, _create_example_client
 from opengever.ogds.base.utils import create_session
 from opengever.task.task import ITask
+from opengever.testing import Builder
 from opengever.testing import FunctionalTestCase
+from opengever.testing import create_client
+from opengever.testing import create_ogds_user
 from opengever.testing import set_current_client_id
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import setRoles
-from plone.indexer.interfaces import IIndexer
 from plone.memoize import volatile
 from zope.component import getUtility
-from zope.component import provideAdapter
-from zope.interface import Interface
 from zope.schema.interfaces import IVocabularyFactory
 
 
 class TestAutoCompleteWidget(FunctionalTestCase):
 
-    def test_widget(self):
+    def setUp(self):
+        super(TestAutoCompleteWidget, self).setUp()
+        self.grant('Member', 'Manager')
+        self.widget = AutocompleteFieldWidget(
+            ITask['issuer'], self.portal.REQUEST)
+
+    def test_initally_no_hidden_terms_are_set(self):
+        create_client(clientid='client1')
+        create_ogds_user('hugo.boss')
+        create_ogds_user('franz.michel')
         set_current_client_id(self.portal)
+        task = Builder('task').create()
 
-        # Lets set up some users and clients for testing the vocabularies with:
-        session = create_session()
+        self.widget.context = task
+        source = self.widget.bound_source
 
-
-        # Mocked containging_dossier indexer
-        class MockedIndexer(object):
-            def __init__(self, context, catalog):
-                pass
-
-            def __call__(self):
-                    return 'Peter'
-        provideAdapter(factory=MockedIndexer, adapts=[Interface, Interface],
-                provides=IIndexer, name='containing_dossier')
-
-        # Users:
-        _create_example_user(session, self.portal,
-                                  'hugo.boss',
-                                  {'firstname': 'Hugo',
-                                  'lastname': 'Boss'},
-                                 ('og_mandant1_users',))
-
-        _create_example_user(session, self.portal,
-                                 'franz.michel',
-                                 {'firstname': 'Franz',
-                                  'lastname': 'Michel'},
-                                 ('og_mandant1_users',
-                                  'og_mandant2_users'))
-
-
-        _create_example_client(session, 'client1',
-                                      {'title': 'client1',
-                                      'ip_address': '127.0.0.1',
-                                      'site_url': 'http://nohost/plone',
-                                      'public_url': 'http://nohost/plone',
-                                      'group': 'og_mandant1_users',
-                                      'inbox_group': 'og_mandant1_inbox'})
-
-
-        # we create example task
-        setRoles(self.portal, TEST_USER_ID, ['Member', 'Manager'])
-        id = self.portal.invokeFactory('opengever.task.task', 'task-1')
-        task1 = self.portal[id]
-
-        # We bind task (as context) to ``AutocompleteFieldWidget`` widget.
-        field = ITask['issuer']
-        widget = AutocompleteFieldWidget(field, self.portal.REQUEST)
-        widget.context = task1
-
-        # Let's clear the cache of the vocabulary first:
-
-        fact = getUtility(IVocabularyFactory,
-                               name='opengever.ogds.base.ContactsAndUsersVocabulary')
-        setattr(fact, volatile.ATTR, {})
-
-
-        # Initialy we dont have any value for our ``ITask.issuer`` field, thats why we
-        # also don't hide anything.
-        source = widget.bound_source
         self.assertEquals(
             [u'hugo.boss', u'franz.michel', u'inbox:client1'],
             [i.value for i in source])
 
         self.assertEquals([], source.hidden_terms)
 
-        # Then we set our example task to some value which is not existing in used
-        # vocabulary and we set ``widget._bound_source`` to None so source will be
-        # initialized again.
-        task1.issuer = u'test1'
-        widget._bound_source = None
+    def test_not_existing_term_is_not_shown_in_listing(self):
+        create_client(clientid='client1')
+        create_ogds_user('hugo.boss')
+        create_ogds_user('franz.michel')
+        set_current_client_id(self.portal)
+        task = Builder('task').with_metadata(issuer='not.existing').create()
 
-        # Now we can see that value set before does not show in listing, but its
-        # added to hidden list of terms.
-        source2 = widget.bound_source
-        self.assertEquals(
-            [u'hugo.boss', u'franz.michel', u'inbox:client1'],
-            [i.value for i in source2])
+        self.widget.context = task
 
-        self.assertEquals([u'test1'], source2.hidden_terms)
+        self.assertNotIn(
+            'not.existing', [i.value for i in self.widget.bound_source])
+
+    def test_not_existing_terms_is_included_in_hidden_terms(self):
+        create_client(clientid='client1')
+        create_ogds_user('hugo.boss')
+        create_ogds_user('franz.michel')
+        set_current_client_id(self.portal)
+        task = Builder('task').with_metadata(issuer='not.existing').create()
+
+        self.widget.context = task
+        self.assertEquals([u'not.existing'], self.widget.bound_source.hidden_terms)
