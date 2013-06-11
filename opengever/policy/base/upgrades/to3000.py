ed68653f21e42f31427004140bd7073169a9e5fa
Julian Infanger <j.infanger@4teamwork.ch>
2013-06-11 17:15:58 +0200

    Added upgradestep.

---
+++ opengever/policy/base/upgrades/to3000.py
@@ -0,0 +1,15 @@
+from ftw.upgrade import UpgradeStep
+
+
+class InstallTeamraumTheme(UpgradeStep):
+
+    def __call__(self):
+        self.setup_install_profile(
+            'profile-opengever.policy.base.upgrades:3000')
+        self.setup_install_profile(
+            'profile-ftw.contentmenu:default')
+        self.setup_install_profile(
+            'profile-collective.mtrsetup:default')
+        self.setup_install_profile(
+            'profile-plonetheme.teamraum:gever')
+            
