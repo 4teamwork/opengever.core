bde2ed4790f9c86009eab8aa167e9a717d2ef280
Lukas Knoepfel <shylux@gmail.com>
2013-09-11 11:59:14 +0200

    Added tests for french title.

--- opengever/repository/tests/test_french_title_behavior.py
+++ opengever/repository/tests/test_french_title_behavior.py
@@ -1,3 +1,5 @@
+from zope.interface import alsoProvides
+from opengever.repository.behaviors.frenchtitle import IFrenchTitleBehavior
 from Products.CMFCore.utils import getToolByName
 from ftw.builder import Builder
 from ftw.builder import create
@@ -7,31 +9,48 @@ from opengever.testing import obj2brain
 from plone.app.contentlisting.interfaces import IContentListingObject
 
 
-class TestFrenchTitleBehavior(FunctionalTestCase):
+class TestTitleAccessor(FunctionalTestCase):
+    layer = OPENGEVER_FUNCTIONAL_TESTING
 
+    def test_returns_title_when_no_language_set(self):
+        """ number + space + title """
+
+
+class TestFrenchTitleAccessor(FunctionalTestCase):
     layer = OPENGEVER_FUNCTIONAL_TESTING
 
     def setUp(self):
-        super(TestFrenchTitleBehavior, self).setUp()
+        super(TestFrenchTitleAccessor, self).setUp()
         getToolByName(self.portal, 'portal_languages').setLanguageBindings()
+        fti = getToolByName(self.portal, 'portal_types').get('opengever.repository.repositoryfolder')
+        fti.behaviors = fti.behaviors + ('opengever.repository.behaviors.frenchtitle.IFrenchTitleBehavior', )
 
-    def test_Title_returns_title_in_the_preferred_language(self):
+    def test_returns_german_title_when_preferred_lang_is_de(self):
+        self.portal.REQUEST.get('LANGUAGE_TOOL').LANGUAGE = 'de'
         repository = create(Builder('repository')
-               .having(title='Weiterbildung',
-                       title_fr='Formation continue'))
-
-        repository.REQUEST.get('LANGUAGE_TOOL').LANGUAGE = 'fr'
+                .titled('Weiterbildung')
+                .having(title_fr='Formation continue'))
 
         self.assertEquals(
-            '1. Formation continue',
-            IContentListingObject(obj2brain(repository)).Title())
+            "1. " + repository.effective_title,
+            repository.Title())
 
-    def test_Title_returns_title_when_title_in_preffered_language_does_not_exist(self):
+    def test_returns_french_title_when_preferred_title_is_fr(self):
+        self.portal.REQUEST.get('LANGUAGE_TOOL').LANGUAGE = 'fr'
         repository = create(Builder('repository')
-               .having(title=u'Weiterbildung', ))
-
-        repository.REQUEST.get('LANGUAGE_TOOL').LANGUAGE = 'it'
+                .titled('Weiterbildung')
+                .having(title_fr='Formation continue'))
 
         self.assertEquals(
-            '1. Weiterbildung',
-            IContentListingObject(obj2brain(repository)).Title())
+            "1. Formation continue",
+            repository.Title())
+
+    def test_returns_title_field_when_preferred_lang_is_not_supported(self):
+        self.portal.REQUEST.get('LANGUAGE_TOOL').LANGUAGE = 'it'
+        repository = create(Builder('repository')
+                .titled('Weiterbildung')
+                .having(title_fr='Formation continue'))
+
+        self.assertEquals(
+            "1. " + repository.effective_title,
+            repository.Title())
