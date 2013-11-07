953779bd3a550da56067770b8119c3d29371fec0
Philippe Gross <philippe.gross@4teamwork.ch>
2013-11-07 14:54:24 +0100

    Added upgradestep wich migrates the filingnumber from the old in to the new field.

---
+++ opengever/dossier/upgrades/filing_to2601.py
@@ -0,0 +1,25 @@
+from ftw.upgrade import ProgressLogger
+from ftw.upgrade import UpgradeStep
+from opengever.dossier.behaviors.dossier import IDossierMarker
+
+
+class MigrateFilingNumbers(UpgradeStep):
+
+    def __call__(self):
+        for dossier in self.get_dossiers(
+                {'object_provides': IDossierMarker.__identifier__},
+                'Migrate filing number in to the new field'):
+
+            self.migrate_filing_number(dossier)
+
+    def get_dossiers(self, query, message):
+        brains = self.catalog_unrestricted_search(query)
+        return ProgressLogger(message, brains)
+
+    def migrate_filing_number(self, brain):
+
+        filing_number = brain.filing_no
+        if filing_number and not filing_number.endswith('-?'):
+
+            dossier = brain.getObject()
+            IFilingNumber(dossier).filing_no = IDossier(dossier).filing_no
