6c82d7345e9676e233562dc119d172b102ccdf15
Philippe Gross <philippe.gross@4teamwork.ch>
2013-11-11 08:36:05 +0100

    Reworked mail byline: use base byline template.

---
+++ opengever/mail/tests/test_mail_byline.py
@@ -0,0 +1,36 @@
+from datetime import date
+from ftw.builder import Builder
+from ftw.builder import create
+from opengever.base.tests.byline_base_test import TestBylineBase
+from opengever.testing import create_ogds_user
+from zope.component import getUtility
+from zope.intid.interfaces import IIntIds
+
+
+class TestMailByline(TestBylineBase):
+
+    use_browser = True
+
+    def setUp(self):
+        super(TestMailByline, self).setUp()
+
+        self.grant('Manager')
+
+        self.intids = getUtility(IIntIds)
+        create_ogds_user('hugo.boss')
+
+        self.mail = create(Builder('mail'))
+        self.browser.open(self.mail.absolute_url())
+
+    def test_dossier_byline_icon_display(self):
+        icon = self.get_byline_element_by_class('byline-icon')
+        self.assertEquals('byline-icon contenttype-ftw-mail-mail',
+                          icon.target.get('class'))
+
+    def test_dossier_byline_sequence_number_display(self):
+        seq_number = self.get_byline_value_by_label('Sequence Number:')
+        self.assertEquals('1', seq_number.text_content())
+
+    def test_dossier_byline_reference_number_display(self):
+        ref_number = self.get_byline_value_by_label('Reference Number:')
+        self.assertEquals('OG / 1', ref_number.text_content())
