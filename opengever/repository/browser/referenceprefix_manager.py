1c040fb8376f181772d6a571ea811d1c0ed777b5
Lukas Knoepfel <shylux@gmail.com>
2013-11-28 12:32:53 +0100

    Restricted reference prefix manager to repository and repositoryroot.

--- opengever/repository/browser/referenceprefix_manager.py
+++ opengever/repository/browser/referenceprefix_manager.py
@@ -1,28 +1,30 @@
-from five import grok
 from opengever.base.adapters import ReferenceNumberPrefixAdpater
 from opengever.repository import _
 from opengever.repository.events import RepositoryPrefixUnlocked
-from opengever.repository.interfaces import IRepositoryFolder
 from Products.statusmessages.interfaces import IStatusMessage
 from zope.event import notify
+from zope.publisher.browser import BrowserView
 
 
-class ReferencePrefixManager(grok.View):
-    grok.context(IRepositoryFolder)
-    grok.name("referenceprefix_manager")
-    grok.require('opengever.repository.UnlockReferencePrefix')
+class ReferencePrefixManager(BrowserView):
 
-    def update(self):
+
+    def __call__(self, *args, **kwargs):
+        prefix_num = self.request.get('prefix')
+        if prefix_num:
+            self.free_reference_prefix(prefix_num)
+
+        return super(ReferencePrefixManager, self).__call__(*args, **kwargs)
+
+    def free_reference_prefix(self, prefix_num):
         refs = ReferenceNumberPrefixAdpater(self.context)
+        refs.free_number(prefix_num)
 
-        if self.request.get('prefix'):
-            refs.free_number(self.request.get('prefix'))
-            notify(RepositoryPrefixUnlocked(self.context,
-                                            self.request.get('prefix')))
-            messages = IStatusMessage(self.request)
-            messages.add(_("statmsg_prefix_unlocked",
-                        default=u"Reference prefix has been unlocked."),
-                        type=u"info")
+        notify(RepositoryPrefixUnlocked(self.context, prefix_num))
+        messages = IStatusMessage(self.request)
+        messages.add(_("statmsg_prefix_unlocked",
+                    default=u"Reference prefix has been unlocked."),
+                    type=u"info")
 
     def prefix_mapping(self):
         refs = ReferenceNumberPrefixAdpater(self.context)
