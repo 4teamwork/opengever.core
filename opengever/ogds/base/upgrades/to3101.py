89b7a62a4764fda6720f0142f5503617b2b463e5
Lukas Graf <lukas.graf@4teamwork.ch>
2013-12-13 10:25:05 +0100

    UpgradeStep: Increase UserID and GroupID column lengths in OGDS.

---
+++ opengever/ogds/base/upgrades/to3101.py
@@ -0,0 +1,14 @@
+from ftw.upgrade import UpgradeStep
+from opengever.ogds.base.utils import create_session
+from opengever.ogds.models.utils import alter_column_length
+
+
+class IncreaseUserAndGroupIDColumnLengths(UpgradeStep):
+    """Increase UserID and GroupID column lengths in OGDS SQL schema."""
+
+    def __call__(self):
+        session = create_session()
+        for tbl_name, col_name, new_length in [('users', 'userid', 255),
+                                               ('groups', 'groupid', 255),
+                                               ]:
+            alter_column_length(session, tbl_name, col_name, new_length)
