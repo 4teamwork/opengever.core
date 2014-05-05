75b534d70268498266ef75d4d8660ab5e7f1733b
Philippe Gross <philippe.gross@4teamwork.ch>
2014-05-05 10:17:10 +0200

    Mail view: use sprite icons in the atachments list.

--- opengever/mail/browser/mail.py
+++ opengever/mail/browser/mail.py
@@ -12,24 +12,41 @@ class View(ftwView):
 
     template = ViewPageTemplateFile('mail_templates/view.pt')
 
+    def __call__(self):
+        self.normalizer = getUtility(IIDNormalizer)
+        self.mtr = getToolByName(self.context, 'mimetypes_registry')
+        return super(View, self).__call__()
+
+    def lookup_mimetype_registry(self, attachment):
+        if attachment.get('content-type') == 'application/octet-stream':
+            lookup = self.mtr.globFilename(attachment.get('filename'))
+        else:
+            lookup = self.mtr.lookup(attachment['content-type'])
+
+        if isinstance(lookup, list) or isinstance(lookup, tuple):
+            lookup = lookup[0]
+
+        return lookup
+
+    def get_css_class(self, lookup):
+        # copied from opengever.base.browser.utils.get_css_class, should be
+        # removed when the sprite problematic is solved complety for opengever
+        icon_path = lookup.icon_path
+        filetype = icon_path[:icon_path.rfind('.')].replace('icon_', '')
+        css_class = 'icon-%s' % self.normalizer.normalize(filetype)
+        return css_class
+
     @instance.memoize
     def attachments(self):
-        normalize = getUtility(IIDNormalizer).normalize
         context = aq_inner(self.context)
         attachments = utils.get_attachments(context.msg)
-        mtr = getToolByName(context, 'mimetypes_registry')
         for attachment in attachments:
-            icon = 'mimetype-plain'
-            type_name = 'File'
-            if attachment.get('content-type') == 'application/octet-stream':
-                lookup = mtr.globFilename(attachment.get('filename'))
-            else:
-                lookup = mtr.lookup(attachment['content-type'])
+            lookup = self.lookup_mimetype_registry(attachment)
             if lookup:
-                if isinstance(lookup, list) or isinstance(lookup, tuple):
-                    lookup = lookup[0]
-                icon = "mimetype-%s" % normalize(lookup.minor())
-                type_name = lookup.name()
-            attachment['icon'] = icon
-            attachment['type-name'] = type_name
+                attachment['class'] = self.get_css_class(lookup)
+                attachment['type-name'] = lookup.name()
+            else:
+                attachment['class'] = ''
+                attachment['type-name'] = 'File'
+
         return attachments
