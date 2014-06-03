d9495fec20ff0035356571ca173c2e725693b7e9
Lukas Graf <lukas.graf@4teamwork.ch>
2014-06-03 16:15:04 +0200

    Add assertion to existing test that didn't have one.

--- opengever/document/tests/test_overview_missing_fields.py
+++ opengever/document/tests/test_overview_missing_fields.py
@@ -1,6 +1,5 @@
 from ftw.builder import create, Builder
 from ftw.testbrowser import browsing
-from opengever.core.testing import OPENGEVER_FUNCTIONAL_TESTING
 from opengever.document.document import IDocumentSchema
 from opengever.testing import FunctionalTestCase
 from plone.autoform.interfaces import OMITTED_KEY
@@ -10,17 +9,27 @@ from zope.interface import Interface
 class TestDocumentOverviewMissingFields(FunctionalTestCase):
 
     def setUp(self):
-        omitted_values = IDocumentSchema.getTaggedValue(OMITTED_KEY)
-        self.org_omitted_values = list(omitted_values)
+        omitted_values = []
+        if OMITTED_KEY in IDocumentSchema.getTaggedValueTags():
+            omitted_values.extend(IDocumentSchema.getTaggedValue(OMITTED_KEY))
+        self.orig_omitted_values = list(omitted_values)
         omitted_values.append((Interface, 'document_type', 'true'))
         IDocumentSchema.setTaggedValue(OMITTED_KEY, omitted_values)
 
         self.document = create(Builder('document'))
 
     def tearDown(self):
-        IDocumentSchema.setTaggedValue(OMITTED_KEY, self.org_omitted_values)
+        IDocumentSchema.setTaggedValue(OMITTED_KEY, self.orig_omitted_values)
 
     @browsing
-    def test_document_overview_without_document_type(self, browser):
+    def test_document_type_can_be_dropped_from_overview(self, browser):
         browser.login()
         browser.visit(self.document, view="tabbedview_view-overview")
+        field_headers = browser.css('table tr th').text
+        self.assertEquals(['Document Date',
+                           'creator',
+                           'Digital Available',
+                           'Preserved as paper',
+                           'Date of receipt',
+                           'Date of delivery'],
+                          field_headers)
