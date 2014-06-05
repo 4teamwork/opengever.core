910c829226d37015e2a1ad49eea2a54046f8f213
David Erni <david.erni@4teamwork.ch>
2014-06-05 17:27:59 +0200

    Fix ogds base tests and transport by selecting ITask IForwarding attributes.

--- opengever/ogds/base/tests/test_autocomplete_widget.py
+++ opengever/ogds/base/tests/test_autocomplete_widget.py
@@ -3,10 +3,8 @@ from ftw.builder import create
 from opengever.ogds.base.autocomplete_widget import AutocompleteFieldWidget
 from opengever.task.task import ITask
 from opengever.testing import FunctionalTestCase
-from opengever.testing import create_client
-from opengever.testing import create_ogds_user
-from opengever.testing import select_current_org_unit
 from plone.app.testing import TEST_USER_ID
+
 
 class TestAutoCompleteWidget(FunctionalTestCase):
 
@@ -16,13 +14,11 @@ class TestAutoCompleteWidget(FunctionalTestCase):
         self.widget = AutocompleteFieldWidget(
             ITask['issuer'], self.portal.REQUEST)
 
-        client = create_client(clientid='client1')
-        create_ogds_user('hugo.boss')
-        create_ogds_user('franz.michel')
-        create_ogds_user(TEST_USER_ID, assigned_client=[client],
-                         firstname="Test", lastname="User")
-
-        select_current_org_unit()
+        self.user, self.org_unit, self.admin_unit, self.hugo = create(
+            Builder('fixture').with_all_unit_setup().with_hugo_boss())
+        create(Builder('ogds_user')
+               .in_group(self.org_unit.users_group())
+               .having(userid='franz.michel'))
 
     def test_initally_no_hidden_terms_are_set(self):
         task = create(Builder('task'))
@@ -30,7 +26,7 @@ class TestAutoCompleteWidget(FunctionalTestCase):
         self.widget.context = task
         source = self.widget.bound_source
 
-        self.assertEquals(
+        self.assertItemsEqual(
             [u'hugo.boss', u'franz.michel', TEST_USER_ID, u'inbox:client1'],
             [i.value for i in source])
 
@@ -41,7 +37,7 @@ class TestAutoCompleteWidget(FunctionalTestCase):
 
         self.widget.context = task
 
-        self.assertEquals(
+        self.assertItemsEqual(
             [u'hugo.boss', u'franz.michel', TEST_USER_ID, u'inbox:client1'],
             [i.value for i in self.widget.bound_source])
 
