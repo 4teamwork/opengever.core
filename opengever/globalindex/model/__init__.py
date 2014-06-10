ff20cfed917ac0b99a3059d00a7175945ddb233d
Philippe Gross <philippe.gross@4teamwork.ch>
2014-06-10 15:03:11 +0200

    Use and configure new query base from `opengever.ogds.models`.
    See https://github.com/4teamwork/opengever.ogds.models/pull/9.

--- opengever/globalindex/model/__init__.py
+++ opengever/globalindex/model/__init__.py
@@ -1,2 +1,6 @@
-from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
+from opengever.ogds.base.utils import Session
+from opengever.ogds.models import BASE
+from opengever.ogds.models.declarative import query_base
+
+BASE.session = Session
+Base = query_base(Session)
