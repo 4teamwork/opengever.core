7951c16a655fd50a9afa6886fdc6461647474c59
Philippe Gross <philippe.gross@4teamwork.ch>
2014-06-13 14:24:18 +0200

    reworked `linked_ogds_author` helper use Actor.lookup.
    Using Actor.get_link instead of `IContactInformation.render_link`.

--- opengever/tabbedview/tests/test_helpers.py
+++ opengever/tabbedview/tests/test_helpers.py
@@ -7,35 +7,6 @@ from opengever.tabbedview.helper import task_id_checkbox_helper
 
 class TestDocumentsUrl(MockTestCase):
 
-    def test_linked_ogds_author(self):
-
-        # mocked links
-        HUGOBOSS = '<a href="http://plone/author/hugo.boss">Hugo Boss</a>'
-        HANSPETER = '<a href="http://plone/author/hans.peter">Hans Peter</a>'
-
-        brain = self.stub()
-
-        info = self.stub()
-        self.mock_utility(info, IContactInformation)
-
-        self.expect(info.is_user('hugo.boss')).result(True)
-        self.expect(info.render_link('hugo.boss')).result(HUGOBOSS)
-
-        self.expect(info.is_user(u'hans.peter')).result(False)
-        self.expect(info.is_contact(u'hans.peter')).result(True)
-        self.expect(info.render_link(u'hans.peter')).result(HANSPETER)
-
-        self.expect(info.is_user('james.bond')).result(False)
-        self.expect(info.is_contact('james.bond')).result(False)
-        self.expect(info.is_inbox('james.bond')).result(False)
-
-        self.replay()
-        self.assertEquals(linked_ogds_author(brain, u'hugo.boss'), HUGOBOSS)
-        self.assertEquals(linked_ogds_author(brain, 'hans.peter'), HANSPETER)
-        self.assertEquals(
-            linked_ogds_author(brain, u'james.bond'), 'james.bond')
-        self.assertEquals(linked_ogds_author(brain, None), '')
-
     def test_task_id_checkbox_helper(self):
 
         brain = self.stub()
