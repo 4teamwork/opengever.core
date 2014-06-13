e1f96c3664999073d94709d7f5d34617719a8a0e
Philippe Gross <philippe.gross@4teamwork.ch>
2014-06-13 12:11:14 +0200

    Reworked ListGroupMembers view, use the ogds_service and Actor class.
    Test is missing for the ListGroupMembers view.

--- opengever/base/browser/list_groupmembers.py
+++ opengever/base/browser/list_groupmembers.py
@@ -1,18 +1,20 @@
+from opengever.ogds.base.actor import Actor
+from opengever.ogds.base.utils import ogds_service
 from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from opengever.ogds.base.interfaces import IContactInformation
-from zope.component import getUtility
 
 
 class ListGroupMembers(BrowserView):
     template = ViewPageTemplateFile("list_groupmembers.pt")
 
     def __call__(self):
-        group_id = self.context.REQUEST.get('group', None)
-        info = getUtility(IContactInformation)
-        if not group_id:
+        self.group_id = self.context.REQUEST.get('group', None)
+        self.members = []
+
+        if not self.group_id:
             return 'no group id'
-        self.group_name = group_id
-        members = info.list_group_users(group_id)
-        self.members = [info.render_link(member) for member in members]
+
+        for user in ogds_service().fetch_group(self.group_id).users:
+            self.members.append(
+                Actor.user(user.userid).get_link)
         return self.template()
