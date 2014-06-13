f8b62a1415fe47fb25446f48bb4be5d779acb874
Philippe Gross <philippe.gross@4teamwork.ch>
2014-06-13 12:31:49 +0200

    DocumentOverview: use Actor.get_link instead of `IContactInformation.render_link`.

--- opengever/document/tests/test_overview_missing_fields.py
+++ opengever/document/tests/test_overview_missing_fields.py
@@ -1,6 +1,6 @@
-from ftw.builder import create, Builder
+from ftw.builder import Builder
+from ftw.builder import create
 from ftw.testbrowser import browsing
-from opengever.core.testing import OPENGEVER_FUNCTIONAL_TESTING
 from opengever.document.document import IDocumentSchema
 from opengever.testing import FunctionalTestCase
 from plone.autoform.interfaces import OMITTED_KEY
@@ -10,6 +10,7 @@ from zope.interface import Interface
 class TestDocumentOverviewMissingFields(FunctionalTestCase):
 
     def setUp(self):
+        create(Builder('ogds_user'))
         omitted_values = IDocumentSchema.getTaggedValue(OMITTED_KEY)
         self.org_omitted_values = list(omitted_values)
         omitted_values.append((Interface, 'document_type', 'true'))
