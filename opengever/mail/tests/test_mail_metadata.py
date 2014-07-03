fdbc8f70944f2d34a10103b8da21aa37b55e96f8
Lukas Graf <lukas.graf@4teamwork.ch>
2014-07-03 15:35:26 +0200

    Refactor mail metadata tests.

--- opengever/mail/tests/test_mail_metadata.py
+++ opengever/mail/tests/test_mail_metadata.py
@@ -9,156 +9,104 @@ from opengever.mail.mail import extract_email
 from opengever.mail.mail import get_author_by_email
 from opengever.testing import FunctionalTestCase
 from opengever.testing.sql import create_ogds_user
+from pkg_resources import resource_string
 from plone.dexterity.interfaces import IDexterityFTI
 from plone.registry.interfaces import IRegistry
 from unittest2 import TestCase
 from zope.component import getUtility
-import os
 
 
-MAIL_DATA = open(
-    os.path.join(os.path.dirname(__file__), 'mail.txt'), 'r').read()
+MAIL_DATA = resource_string('opengever.mail.tests', 'mail.txt')
 
 
-class TestMailMetadata(FunctionalTestCase):
+def get_header_date(mail):
+    return datetime.fromtimestamp(utils.get_date_header(mail.msg, 'Date'))
+
+
+def get_preserved_as_paper_default():
+    registry = getUtility(IRegistry)
+    return registry.forInterface(IDocumentSettings).preserved_as_paper_default
+
+
+class TestMailMetadataWithBuilder(FunctionalTestCase):
+
     use_browser = True
 
     def setUp(self):
-        super(TestMailMetadata, self).setUp()
+        super(TestMailMetadataWithBuilder, self).setUp()
         self.grant('Contributor', 'Editor', 'Member', 'Manager')
 
-    def get_header_date(self, mail):
-        return datetime.fromtimestamp(utils.get_date_header(mail.msg, 'Date'))
+    def create_mail(self):
+        mail = create(Builder("mail").with_message(MAIL_DATA))
+        return mail
 
-    def get_preserved_as_papger_default(self):
-        registry = getUtility(IRegistry)
-        return registry.forInterface(
-            IDocumentSettings).preserved_as_paper_default
+    def create_user(self):
+        user_properties = {'firstname': 'Friedrich',
+                           'lastname': u'H\xf6lderlin',
+                           'email': 'from@example.org'}
+        user = create_ogds_user('someuserid', **user_properties)
+        return user
 
     def test_fill_metadata_of_new_mail(self):
-        mail = create(Builder("mail").with_message(MAIL_DATA))
-        mail_metadata = metadata.IDocumentMetadata(mail)
+        mail = self.create_mail()
+        self.assertIsNone(mail.description,
+                          'Description should have no value')
 
-        self.assertIsNone(mail_metadata.description,
-                          'Description has no value')
+        self.assertEquals((), mail.keywords, 'Expected no keywords')
 
-        self.assertEquals((), mail_metadata.keywords, 'Expect no keywords')
+        self.assertIsNone(mail.foreign_reference,
+                          'Foreign reference should have no value')
 
-        self.assertIsNone(mail_metadata.foreign_reference,
-                          'Foreign reference has no value')
-
-        self.assertEquals(self.get_header_date(mail).date(),
-                          mail_metadata.document_date)
+        self.assertEquals(get_header_date(mail).date(),
+                          mail.document_date)
 
         self.assertEquals(date.today(),
-                          mail_metadata.receipt_date)
+                          mail.receipt_date)
 
-        self.assertIsNone(mail_metadata.delivery_date,
-                          'Delivery date has no value')
+        self.assertIsNone(mail.delivery_date,
+                          'Delivery date should have no value')
 
-        self.assertIsNone(mail_metadata.document_type,
-                          'Document type has no value')
+        self.assertIsNone(mail.document_type,
+                          'Document type should have no value')
 
-        self.assertIsNone(mail_metadata.digitally_available,
-                          'Digitally available has no value')
+        self.assertIsNone(mail.digitally_available,
+                          'Digitally available should have no value')
 
-        self.assertEquals(self.get_preserved_as_papger_default(),
-                          mail_metadata.preserved_as_paper)
+        self.assertEquals(get_preserved_as_paper_default(),
+                          mail.preserved_as_paper)
 
-        self.assertIsNone(mail_metadata.archival_file,
-                          'Archival file date has no value')
+        self.assertIsNone(mail.archival_file,
+                          'Archival file should have no value')
 
-        self.assertIsNone(mail_metadata.thumbnail,
-                          'Thumbnail has no value')
+        self.assertIsNone(mail.thumbnail,
+                          'Thumbnail should have no value')
 
-        self.assertIsNone(mail_metadata.preview,
-                          'Preview has no value')
+        self.assertIsNone(mail.preview,
+                          'Preview should have no value')
 
     def test_fill_mail_author_with_fullname_of_EXISTING_user(self):
-        properties = {'firstname': 'Friedrich',
-                      'lastname': u'H\xf6lderlin',
-                      'email': 'from@example.org'}
-        user = create_ogds_user('someuserid', **properties)
-
-        mail = create(Builder("mail").with_message(MAIL_DATA))
-        mail_metadata = metadata.IDocumentMetadata(mail)
-
+        user = self.create_user()
+        mail = self.create_mail()
         self.assertEquals(
             u'{0} {1}'.format(user.lastname, user.firstname),
             get_author_by_email(mail))
 
         self.assertEquals(
             get_author_by_email(mail),
-            mail_metadata.document_author)
+            mail.document_author)
 
     def test_fill_mail_author_with_FROM_data_if_user_does_not_exist(self):
-
-        mail = create(Builder("mail").with_message(MAIL_DATA))
-        mail_metadata = metadata.IDocumentMetadata(mail)
-
+        mail = self.create_mail()
         self.assertEquals(
             utils.get_header(mail.msg, 'From'),
             get_author_by_email(mail))
 
         self.assertEquals(
             get_author_by_email(mail),
-            mail_metadata.document_author)
-
-    def test_metadata_initialization_upgrade_step(self):
-        fti = getUtility(IDexterityFTI, name=u'ftw.mail.mail')
-        behaviors = list(fti.behaviors)
-        behaviors.remove(
-            u'opengever.document.behaviors.metadata.IDocumentMetadata')
-        fti._updateProperty('behaviors', tuple(behaviors))
-
-        mail = create(Builder("mail").with_message(MAIL_DATA))
-
-        from opengever.mail.upgrades.to2200 import ActivateBehaviors
-        ActivateBehaviors(self.portal.portal_setup)
-
-        assert metadata.IDocumentMetadata.providedBy(mail)
-        mail_metadata = metadata.IDocumentMetadata(mail)
-        # The description is initialized wihtout the behavior, so the default
-        # value is by another behavior with a description field.
-        self.assertEquals(u'',
-                          mail_metadata.description,
-                          'Description has no value')
-
-        self.assertEquals((), mail_metadata.keywords, 'Expect no keywords')
-
-        self.assertIsNone(mail_metadata.foreign_reference,
-                          'Foreign reference has no value')
-
-        self.assertEquals(self.get_header_date(mail).date(),
-                          mail_metadata.document_date)
-
-        self.assertEquals(date.today(),
-                          mail_metadata.receipt_date)
-
-        self.assertIsNone(mail_metadata.delivery_date,
-                          'Delivery date has no value')
-
-        self.assertIsNone(mail_metadata.document_type,
-                          'Document type has no value')
-
-        self.assertIsNone(mail_metadata.digitally_available,
-                          'Digitally available has no value')
-
-        self.assertEquals(self.get_preserved_as_papger_default(),
-                          mail_metadata.preserved_as_paper)
-
-        self.assertIsNone(mail_metadata.archival_file,
-                          'Archive file has no value')
-
-        self.assertIsNone(mail_metadata.thumbnail,
-                          'Thumbnail has no value')
-
-        self.assertIsNone(mail_metadata.preview,
-                          'Preview has no value')
+            mail.document_author)
 
     def test_mail_catalog_metadata(self):
-        mail = create(Builder("mail").with_message(MAIL_DATA))
-
+        mail = self.create_mail()
         brain = self.portal.portal_catalog(portal_type='ftw.mail.mail')[0]
 
         self.assertEquals(mail.document_date,
@@ -173,6 +121,61 @@ class TestMailMetadata(FunctionalTestCase):
         self.assertEquals('',
                           brain.checked_out,
                           'Checked out, should be empty')
+
+
+class TestMailUpgradeStep(FunctionalTestCase):
+
+    def test_metadata_initialization_upgrade_step(self):
+        fti = getUtility(IDexterityFTI, name=u'ftw.mail.mail')
+        behaviors = list(fti.behaviors)
+        behaviors.remove(
+            u'opengever.document.behaviors.metadata.IDocumentMetadata')
+        fti._updateProperty('behaviors', tuple(behaviors))
+
+        mail = create(Builder("mail").with_message(MAIL_DATA))
+
+        from opengever.mail.upgrades.to2200 import ActivateBehaviors
+        ActivateBehaviors(self.portal.portal_setup)
+
+        assert metadata.IDocumentMetadata.providedBy(mail)
+        mail = metadata.IDocumentMetadata(mail)
+        # The description is initialized wihtout the behavior, so the default
+        # value is by another behavior with a description field.
+        self.assertEquals(u'',
+                          mail.description,
+                          'Description has no value')
+
+        self.assertEquals((), mail.keywords, 'Expect no keywords')
+
+        self.assertIsNone(mail.foreign_reference,
+                          'Foreign reference has no value')
+
+        self.assertEquals(get_header_date(mail).date(),
+                          mail.document_date)
+
+        self.assertEquals(date.today(),
+                          mail.receipt_date)
+
+        self.assertIsNone(mail.delivery_date,
+                          'Delivery date has no value')
+
+        self.assertIsNone(mail.document_type,
+                          'Document type has no value')
+
+        self.assertIsNone(mail.digitally_available,
+                          'Digitally available has no value')
+
+        self.assertEquals(get_preserved_as_paper_default(),
+                          mail.preserved_as_paper)
+
+        self.assertIsNone(mail.archival_file,
+                          'Archive file has no value')
+
+        self.assertIsNone(mail.thumbnail,
+                          'Thumbnail has no value')
+
+        self.assertIsNone(mail.preview,
+                          'Preview has no value')
 
 
 class TestEmailRegex(TestCase):
