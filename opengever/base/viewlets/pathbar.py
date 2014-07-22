654b7a653ffdb8c359cbd05092389ade39133003
Philippe Gross <philippe.gross@4teamwork.ch>
2014-07-22 18:12:07 +0200

    Breadcrumb: Replace Home label with curent client label.

--- opengever/base/viewlets/pathbar.py
+++ opengever/base/viewlets/pathbar.py
@@ -1,6 +1,15 @@
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from opengever.ogds.base.utils import get_current_client
 from plone.app.layout.viewlets import common
+from Products.CMFPlone import PloneMessageFactory as pmf
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
 class PathBar(common.PathBarViewlet):
     index = ViewPageTemplateFile('pathbar.pt')
+
+    def client_title(self):
+        try:
+            return get_current_client().title
+        except ValueError:
+            # Current client was not found we return the default home label
+            return pmf('tabs_home', default="Home")
