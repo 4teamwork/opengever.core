bc8740b617aa1597ac910b23c43e7cbb63c3a204
Philippe Gross <philippe.gross@4teamwork.ch>
2014-07-23 09:50:14 +0200

    Dropped automatic title for inboxes.
    With the new implementation, it will exist a inbox for every orgunit, so the title
    could be configured on every subinbox.

--- opengever/inbox/tests/test_tabs.py
+++ opengever/inbox/tests/test_tabs.py
@@ -1,9 +1,6 @@
 from ftw.builder import Builder
 from ftw.builder import create
-from ftw.testbrowser import browsing
-from opengever.testing import create_client
 from opengever.testing import FunctionalTestCase
-from opengever.testing import obj2brain
 from opengever.testing import select_current_org_unit
 from opengever.testing import task2sqltask
 
@@ -17,23 +14,6 @@ class TestInboxTabbedview(FunctionalTestCase):
         self.user, self.org_unit, self.admin_unit = create(
             Builder('fixture').with_all_unit_setup())
 
-    @browsing
-    def test_title_is_inbox_title_suffixed_with_current_orgunit_label(self, browser):
-        create(Builder('org_unit')
-               .id('client2')
-               .having(title='Client 2')
-               .assign_users([self.user]))
-
-        browser.login().open(self.inbox)
-        self.assertEquals('Testinbox: Client1',
-                          browser.css('h1.documentFirstHeading').first.text)
-
-
-        browser.open(self.inbox, view='change_org_unit?unit_id=client2')
-        browser.login().open(self.inbox)
-        self.assertEquals('Testinbox: Client 2',
-                          browser.css('h1.documentFirstHeading').first.text)
-
     def test_trash_listing_does_not_contain_subdossier_and_checked_out_column(self):
         trash = self.inbox.restrictedTraverse('tabbedview_view-trash')
 
@@ -42,8 +22,8 @@ class TestInboxTabbedview(FunctionalTestCase):
         self.assertNotIn('checked_out', columns)
 
     def test_trash_lists_only_documents_from_the_current_admin_unit(self):
-        document_1 =  create(Builder('document').titled('Doc 1')
-                             .trashed().within(self.inbox))
+        document_1 = create(Builder('document').titled('Doc 1')
+                            .trashed().within(self.inbox))
 
         create(Builder('org_unit').id('additional')
                .assign_users([self.user]).as_current_org_unit())
