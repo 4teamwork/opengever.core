af153742ad40e2c8dc659c974512f65a2d0d2f2b
David Erni <david.erni@4teamwork.ch>
2014-07-29 12:04:50 +0200

    Sort groupmembers by label for groupmembers list.
    Closes #360.

---
+++ opengever/base/tests/test_list_groupmembers.py
@@ -0,0 +1,34 @@
+from ftw.builder import Builder
+from ftw.builder import create
+from ftw.testbrowser import browsing
+from opengever.testing import FunctionalTestCase
+from opengever.ogds.base.actor import Actor
+
+
+class TestResolveOGUIDView(FunctionalTestCase):
+
+    def setUp(self):
+        super(TestResolveOGUIDView, self).setUp()
+
+        self.group = create(Builder('ogds_group').having(groupid='group'))
+        self.user1 = create(Builder('ogds_user')
+                            .in_group(self.group))
+        self.user2 = create(Builder('ogds_user')
+                            .in_group(self.group)
+                            .having(userid='x.john.doe',
+                                    lastname='Doe',
+                                    firstname='John'))
+
+        self.actor1 = Actor.user(self.user1.userid)
+        self.actor2 = Actor.user(self.user2.userid)
+
+    @browsing
+    def test_list_groupmembers_view(self, browser):
+        browser.login().open(view='list_groupmembers',
+                             data={'group': self.group.groupid})
+
+        self.assertSequenceEqual(
+            [self.actor2.get_link(), self.actor1.get_link()],
+            [each.normalized_outerHTML for each in
+             browser.css('.member_listing li a')]
+        )
