c04c86158b358ab9695f95c2b61b6c81adeb6adf
Philippe Gross <philippe.gross@4teamwork.ch>
2014-08-12 16:15:50 +0200

    QueryUtility: Dropped no longer used methods

--- opengever/globalindex/interfaces.py
+++ opengever/globalindex/interfaces.py
@@ -4,7 +4,3 @@ from zope.interface import Interface
 class ITaskQuery(Interface):
     """A global utility for querying opengever tasks.
     """
-
-    def get_tasks_for_issuer(issuer):
-        """Returns all tasks issued by the given issuer.
-        """
