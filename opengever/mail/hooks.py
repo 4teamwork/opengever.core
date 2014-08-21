5a01caa1db7b7b436a33d00601bc518656c64bcf
David Erni <david.erni@4teamwork.ch>
2014-08-21 10:25:44 +0200

    Use ftw.profilehook instead of custom import steps for opengever.mail.

--- opengever/mail/hooks.py
+++ opengever/mail/hooks.py
@@ -3,9 +3,5 @@ from opengever.core.catalog import add_catalog_indexes
 from opengever.mail.config import INDEXES
 
 
-def import_various(setup):
-    """Import step for configuration that is not handled in xml files.
-    """
-    if setup.readDataFile('opengever.mail.txt') is None:
-        return
+def installed(site):
     add_catalog_indexes(INDEXES, logging.getLogger('opengever.mail'))
