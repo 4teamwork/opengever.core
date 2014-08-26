e45327aecaf828b664bf0d6a228f35ebbfc285ba
Philippe Gross <philippe.gross@4teamwork.ch>
2014-08-26 11:10:46 +0200

    Pathbar: use current adminunits label instead of the current orgunits one.

--- opengever/base/viewlets/pathbar.py
+++ opengever/base/viewlets/pathbar.py
@@ -1,4 +1,4 @@
-from opengever.ogds.base.utils import get_current_org_unit
+from opengever.ogds.base.utils import get_current_admin_unit
 from plone.app.layout.viewlets import common
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
@@ -6,5 +6,5 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 class PathBar(common.PathBarViewlet):
     index = ViewPageTemplateFile('pathbar.pt')
 
-    def org_unit_label(self):
-        return get_current_org_unit().label()
+    def admin_unit_label(self):
+        return get_current_admin_unit().label()
