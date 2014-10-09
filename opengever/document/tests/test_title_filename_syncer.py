2d39364928f446fa404c461d66c147ccac5c6ce1
David Erni <david.erni@4teamwork.ch>
2014-10-09 09:30:24 +0200

    Fix an unicode issue while syncing document filename and title.
    Fixes #584.

--- opengever/document/tests/test_title_filename_syncer.py
+++ opengever/document/tests/test_title_filename_syncer.py
@@ -1,6 +1,8 @@
 from ftw.builder import Builder
 from ftw.builder import create
 from opengever.testing import FunctionalTestCase
+from zope.event import notify
+from zope.lifecycleevent import ObjectModifiedEvent
 
 
 class TestTitleFilenameSyncer(FunctionalTestCase):
@@ -17,3 +19,12 @@ class TestTitleFilenameSyncer(FunctionalTestCase):
                           .attach_file_containing(u"blup", name=u"wrong.txt"))
         self.assertEqual(document.title, u'My Title')
         self.assertEqual(document.file.filename, u'my-title.txt')
+
+    def test_filename_is_unicode(self):
+        document = create(Builder("document")
+                          .attach_file_containing(u"12", name=u"hmm.txt"))
+        # set non-unicode title bypassing schema checks
+        document.title = 'Ohh'
+        notify(ObjectModifiedEvent(document))
+
+        self.assertEqual(document.file.filename, u'ohh.txt')
