3215eee598670792206c9fff57122b5c127a9fd3
Lukas Graf <lukas.graf@4teamwork.ch>
2014-10-10 10:16:02 +0200

    OGDS sync: Remove old unused shell script.

--- opengever/ogds/base/ldap_import/ogds-updater.sh
+++
@@ -1,5 +0,0 @@
-export ORACLE_HOME=/usr/lib/oracle/11.2/client/lib
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME
-
-/home/zope/clients/plone01-ska-arch/bin/instance0 run /home/zope/ogds-updater/opengever/ogds/base/ldap_import/sync_ldap.py -s ska-arch
-
