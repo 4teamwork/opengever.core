ced6b20a33672fa37a277432b626c435e1fa8b10
David Erni <david.erni@4teamwork.ch>
2014-10-30 14:45:35 +0100

    Add Deployment configuration registry to replace Client/Policy registry.

--- opengever/setup/interfaces.py
+++ opengever/setup/interfaces.py
@@ -1,29 +1,17 @@
 from zope.interface import Interface
 
 
-class IClientConfigurationRegistry(Interface):
-    """A client configuration registry is a utility which.
-    Knows all the registered configurations."""
+class IDeploymentConfigurationRegistry(Interface):
+    """A deployment configuration registry is a utility which knows all the
+    registered configurations.
 
-    def update_clients(id, attr):
-        """Update or add client configuration to the
-        client-configuration registry."""
+    """
+    def update_deployments(id, ident, attr):
+        """Update or add a deployment configuration in the registry."""
 
-    def update_policy(id, attr):
-        """Update or add policy to the client-configuration registry."""
+    def get_deployment(ident):
+        """Returns the deployment configuration (a dict) for the given
+        identifier."""
 
-    def get_configuration(id):
-        """Returns the client configuration (a dict) for the given id. """
-
-    def get_policy(id):
-        """Returns a dict with all policy attributes. Include a list
-        of client configurations on the key `clients`.
-
-        For policies with the multiclient option, it appends ten generated
-        clients, and fill in the client_number if it's necessary."""
-
-    def get_policies():
-        """Returns a generator wiht all registered policy configurations."""
-
-    def list_policies():
-        """Returns a list policy titles."""
+    def list_deployments():
+        """Returns a list of deployment titles."""
--- opengever/setup/registry.py
+++ opengever/setup/registry.py
@@ -1,57 +1,21 @@
-from opengever.setup.interfaces import IClientConfigurationRegistry
+from opengever.setup.interfaces import IDeploymentConfigurationRegistry
 from zope.interface import implements
 
 
-class ClientConfigurationRegistry(object):
-    implements(IClientConfigurationRegistry)
+class DeploymentConfigurationRegistry(object):
+    implements(IDeploymentConfigurationRegistry)
 
     def __init__(self):
-        self.clients = {}
-        self.policies = {}
+        self.deployments = {}
 
-    def update_clients(self, id, attr):
-        if id in self.clients:
-            self.clients[id].update(attr)
+    def update_deployments(self, ident, attr):
+        if id in self.deployments:
+            self.deployments[ident].update(attr)
         else:
-            self.clients[id] = attr
+            self.deployments[ident] = attr
 
-    def update_policy(self, id, kwargs):
-        if id in self.policies:
-            self.policies[id].update(kwargs)
-        else:
-            self.policies[id] = kwargs
+    def list_deployments(self):
+        return self.deployments.keys()
 
-    def get_configuration(self, id):
-        return self.clients.get(id)
-
-    def get_policy(self, id):
-        configuration = self.policies.get(id).copy()
-        configuration['clients'] = []
-
-        if configuration.get('multi_clients'):
-            return self.generate_multi_policies(configuration)
-
-        for client_id in configuration.get('client_ids'):
-            configuration['clients'].append(self.get_configuration(client_id))
-        return configuration
-
-    def generate_multi_policies(self, configuration):
-        configuration['clients'] = []
-        client_id = configuration.get('client_ids')[0]
-        muster_configuration = self.get_configuration(client_id)
-
-        for i in range(1, 11):
-            client = muster_configuration.copy()
-            for key, value in client.items():
-                if isinstance(value, unicode):
-                    client[key] = value.format(client_number=str(i))
-            configuration['clients'].append(client)
-
-        return configuration
-
-    def get_policies(self):
-        for policy_id in self.policies.keys():
-            yield self.get_policy(policy_id)
-
-    def list_policies(self):
-        return self.policies.keys()
+    def get_deployment(self, ident):
+        return self.deployments.get(ident)
