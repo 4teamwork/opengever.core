e359d452e250209bcedb54587a7bc37ff30377a6
David Erni <david.erni@4teamwork.ch>
2014-11-03 11:45:38 +0100

    Configure some more development options for admin-units.

--- opengever/setup/creation/adminunit.py
+++ opengever/setup/creation/adminunit.py
@@ -3,6 +3,9 @@ from opengever.ogds.models.admin_unit import AdminUnit
 from opengever.setup.creation.unit import UnitCreator
 from plone.registry.interfaces import IRegistry
 from zope.component import getUtility
+from opengever.setup import DEVELOPMENT_SERVER_PORT
+from opengever.setup import DEVELOPMENT_SERVER_HOSTNAME
+from opengever.setup import DEVELOPMENT_IP_ADDRESS
 
 
 class AdminUnitCreator(UnitCreator):
@@ -11,6 +14,15 @@ class AdminUnitCreator(UnitCreator):
     item_class = AdminUnit
     required_attributes = ('unit_id', 'ip_address', 'site_url',
                            'public_url', 'abbreviation')
+
+    def apply_development_config(self, item):
+        admin_unit_id = item['unit_id']
+        url = 'http://{}:{}/{}'.format(DEVELOPMENT_SERVER_HOSTNAME,
+                                       DEVELOPMENT_SERVER_PORT,
+                                       admin_unit_id)
+        item['site_url'] = url
+        item['public_url'] = url
+        item['ip_address'] = DEVELOPMENT_IP_ADDRESS
 
     def create_unit(self, item):
         super(AdminUnitCreator, self).create_unit(item)
