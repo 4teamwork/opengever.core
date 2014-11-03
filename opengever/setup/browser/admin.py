96935d78c72d4fcd66fe0fae5df22d3b795296c4
David Erni <david.erni@4teamwork.ch>
2014-11-03 13:52:11 +0100

    Factor out deployment creation into its own class.
    Minor cleanup, remove unused sections and no longer use the term "client".

--- opengever/setup/browser/admin.py
+++ opengever/setup/browser/admin.py
@@ -1,31 +1,13 @@
-from AccessControl.interfaces import IRoleManager
 from datetime import datetime
-from ftw.mail.interfaces import IMailSettings
-from opengever.ogds.base.interfaces import IAdminUnitConfiguration
-from opengever.ogds.base.sync.ogds_updater import sync_ogds
 from opengever.ogds.base.utils import create_session
-from opengever.ogds.models import BASE
-from opengever.setup import DEVELOPMENT_USERS_GROUP
+from opengever.setup.deploy import GeverDeployment
 from opengever.setup.interfaces import IDeploymentConfigurationRegistry
 from opengever.setup.interfaces import ILDAPConfigurationRegistry
-from opengever.setup.ldap_creds import configure_ldap_credentials
-from plone.app.controlpanel.language import ILanguageSelectionSchema
-from plone.registry.interfaces import IRegistry
 from Products.CMFPlone.browser.admin import AddPloneSite
-from Products.CMFPlone.factory import _DEFAULT_PROFILE
-from Products.CMFPlone.factory import addPloneSite
-from Products.CMFPlone.utils import getToolByName
-from Products.PluggableAuthService.interfaces.plugins import IPropertiesPlugin
-from sqlalchemy.exc import NoReferencedTableError
-from zope.component import getAdapter
 from zope.component import getUtility
 from zope.publisher.browser import BrowserView
 import App.config
-import opengever.globalindex.model
 import os
-
-
-SQL_BASES = (BASE, opengever.globalindex.model.Base)
 
 
 class SetupError(Exception):
@@ -33,29 +15,17 @@ class SetupError(Exception):
     """
 
 
-# these profiles will be installed automatically
-EXTENSION_PROFILES = (
-    'plonetheme.classic:default',
-    'plonetheme.sunburst:default',
-)
-
-
-class AddOpengeverClient(AddPloneSite):
-
-    default_extension_profiles = EXTENSION_PROFILES
+class AddDeployment(AddPloneSite):
 
     def __call__(self):
         self.is_development_mode = os.environ.get('IS_DEVELOPMENT_MODE', False)
         return self.index()
 
-    def server_port(self):
-        return self.request.get('SERVER_PORT')
-
     def javascript_src_url(self):
         """returns the url to the javascript. This makes it possible to
         change the URL in debug mode.
-        """
 
+        """
         base_url = '/++resource++addclient.js'
         return base_url + '?x=' + str(datetime.now())
 
@@ -65,9 +35,9 @@ class AddOpengeverClient(AddPloneSite):
         ldap_registry = getUtility(ILDAPConfigurationRegistry)
         return ldap_registry.list_ldaps()
 
-    def get_deployment_options(self):
-        client_registry = getUtility(IDeploymentConfigurationRegistry)
-        return client_registry.list_deployments()
+    def get_deployment_profiles(self):
+        deployment_registry = getUtility(IDeploymentConfigurationRegistry)
+        return deployment_registry.list_deployments()
 
     def get_ogds_config(self):
         """Returns the DSN URL for the OGDS DB connection currently being
@@ -99,172 +69,24 @@ class AddOpengeverClient(AddPloneSite):
         return db_info
 
 
-class CreateOpengeverClient(BrowserView):
+class CreateDeployment(BrowserView):
 
     def __call__(self):
         form = self.request.form
-        session = create_session()
+        db_session = create_session()
 
         policy_id = form['policy']
-        client_registry = getUtility(IDeploymentConfigurationRegistry)
-        config = client_registry.get_deployment(policy_id)
+        deployment_registry = getUtility(IDeploymentConfigurationRegistry)
+        config = deployment_registry.get_deployment(policy_id)
 
         is_development_setup = form.get('dev_mode', False)
         if is_development_setup:
             self.request['unit_creation_dev_mode'] = True
 
-        self.prepare_sql(form, session)
-        site = self.setup_plone_site(config)
-
-        self.setup_ldap(site, form)
-        self.import_ldap_users(site, form)
-        self.configure_admin_unit(config)
-        self.install_policy_profile(site, config)
-        self.configure_plone_site(site, form, config)
-        self.install_additional_profiles(site, config)
-
-        if is_development_setup:
-            self.configure_development_options(site)
-
-    def prepare_sql(self, form, session):
-        if form.get('purge_sql'):
-            self.drop_sql_tables(session)
-
-    def setup_plone_site(self, config):
-        ext_profiles = list(EXTENSION_PROFILES)
-        ext_profiles.append(config['base_profile'])
-
-        return addPloneSite(
-            self.context,
-            config['admin_unit_id'].encode('utf-8'),
-            title=config['title'],
-            profile_id=_DEFAULT_PROFILE,
-            extension_ids=ext_profiles,
-            setup_content=False,
-            default_language=config.get('language', 'de-ch'),
-        )
-
-    def setup_ldap(self, site, form):
-        stool = getToolByName(site, 'portal_setup')
-        if form.get('ldap'):
-            stool = getToolByName(site, 'portal_setup')
-            stool.runAllImportStepsFromProfile('profile-%s' % form.get('ldap'))
-
-            # Configure credentials from JSON file at
-            # ~/.opengever/ldap/{hostname}.json
-            configure_ldap_credentials(site)
-
-            acl_users = getToolByName(site, 'acl_users')
-            plugins = acl_users.plugins
-
-            # disable source_groups when using ldap
-            for ptype in plugins.listPluginTypeInfo():
-                try:
-                    plugins.deactivatePlugin(ptype['interface'],
-                                             'source_groups')
-                except KeyError:
-                    pass
-
-            # deactivate recursive groups
-            for ptype in plugins.listPluginTypeInfo():
-                try:
-                    plugins.deactivatePlugin(ptype['interface'],
-                                             'recursive_groups')
-                except KeyError:
-                    pass
-
-            # move ldap up
-            plugins.movePluginsUp(IPropertiesPlugin, ('ldap',))
-            plugins.movePluginsUp(IPropertiesPlugin, ('ldap',))
-            plugins.movePluginsUp(IPropertiesPlugin, ('ldap',))
-
-    def import_ldap_users(self, site, form):
-        if not form.get('import_users'):
-            return
-
-        print '===== SYNC LDAP ===='
-        # Import LDAP users and groups
-        sync_ogds(site)
-
-    def configure_admin_unit(self, config):
-        registry = getUtility(IRegistry)
-        proxy = registry.forInterface(IAdminUnitConfiguration)
-        proxy.current_unit_id = config['admin_unit_id'].decode('utf-8')
-
-    def install_policy_profile(self, site, config):
-        # import the defaul generic setup profiles if needed
-        policy_profile = config.get('policy_profile')
-        stool = getToolByName(site, 'portal_setup')
-        stool.runAllImportStepsFromProfile(
-            'profile-%s' % policy_profile)
-
-    def configure_plone_site(self, site, form, config):
-        # set the mail domain in the registry
-        client_id = config['title']  # XXX
-        registry = getUtility(IRegistry)
-        proxy = registry.forInterface(IMailSettings)
-        proxy.mail_domain = config['mail_domain'].decode('utf-8')
-        site.manage_changeProperties(
-            {'email_from_address': config['mail_from_address'],
-             'email_from_name': client_id})
-
-        # set global Member role for the client users group
-        # site.acl_users.portal_role_manager.assignRoleToPrincipal(
-        #     'Member', form['group'])
-
-        # set global Member role for readers group
-        if form.get('reader_group'):
-            site.acl_users.portal_role_manager.assignRoleToPrincipal(
-                'Member', form['reader_group'])
-
-        # set Role Manager role for rolemanager group
-        if form.get('rolemanager_group'):
-            site.acl_users.portal_role_manager.assignRoleToPrincipal(
-                'Role Manager', form['rolemanager_group'])
-
-        # set the site title
-        site.manage_changeProperties(title=site_title)
-
-        self.assign_group_to_role(site, config, 'reader_group', 'Member')
-        self.assign_group_to_role(site, config,
-                                  'rolemanager_group', 'Role Manager')
-        self.assign_group_to_role(site, config,
-                                  'administrator_group', 'Administrator')
-
-        # REALLY set the language - the plone4 addPloneSite is really
-        # buggy with languages.
-        langCP = getAdapter(site, ILanguageSelectionSchema)
-        langCP.default_language = 'de-ch'
-
-    def assign_group_to_role(self, site, config, group_name_key, role_name):
-        group_name = config.get(group_name_key)
-        if not group_name:
-            return
-
-        site.acl_users.portal_role_manager.assignRoleToPrincipal(
-            role_name, group_name)
-
-    def install_additional_profiles(self, site, config):
-        # import the defaul generic setup profiles if needed
-        additional_profiles = config.get('additional_profiles')
-        stool = getToolByName(site, 'portal_setup')
-        if additional_profiles:
-            for additional_profile in additional_profiles:
-                stool.runAllImportStepsFromProfile(
-                    'profile-%s' % additional_profile)
-
-    def configure_development_options(self, site):
-        for obj in site.listFolderContents():
-            if IRoleManager.providedBy(obj):
-                obj.manage_addLocalRoles(
-                    DEVELOPMENT_USERS_GROUP,
-                    ["Contributor", "Editor", "Reader"])
-
-    def drop_sql_tables(self, session):
-        """Drops sql tables, usually when creating the first client
-        """
-        for base in SQL_BASES:
-            try:
-                getattr(base, 'metadata').drop_all(session.bind)
-            except NoReferencedTableError:
-                pass
+        deployment = GeverDeployment(
+            self.context, config, db_session,
+            is_development_setup=is_development_setup,
+            has_purge_sql=form.get('purge_sql', False),
+            ldap_profile=form.get('ldap', None),
+            has_ldap_user_import=form.get('import_users', False))
+        deployment.create()
---
+++ opengever/setup/deploy.py
@@ -0,0 +1,192 @@
+from AccessControl.interfaces import IRoleManager
+from ftw.mail.interfaces import IMailSettings
+from opengever.ogds.base.interfaces import IAdminUnitConfiguration
+from opengever.ogds.base.sync.ogds_updater import sync_ogds
+from opengever.ogds.models import BASE
+from opengever.setup import DEVELOPMENT_USERS_GROUP
+from opengever.setup.ldap_creds import configure_ldap_credentials
+from plone.app.controlpanel.language import ILanguageSelectionSchema
+from plone.registry.interfaces import IRegistry
+from Products.CMFPlone.factory import _DEFAULT_PROFILE
+from Products.CMFPlone.factory import addPloneSite
+from Products.CMFPlone.utils import getToolByName
+from Products.PluggableAuthService.interfaces.plugins import IPropertiesPlugin
+from sqlalchemy.exc import NoReferencedTableError
+from zope.component import getAdapter
+from zope.component import getUtility
+import opengever.globalindex.model
+
+
+# these profiles will be installed automatically
+EXTENSION_PROFILES = (
+    'plonetheme.classic:default',
+    'plonetheme.sunburst:default',
+)
+
+
+SQL_BASES = (BASE, opengever.globalindex.model.Base)
+
+
+class GeverDeployment(object):
+
+    def __init__(self, context, config, db_session,
+                 is_development_setup=False,
+                 has_purge_sql=False,
+                 ldap_profile=None,
+                 has_ldap_user_import=False):
+        self.context = context
+        self.config = config
+        self.db_session = db_session
+        self.is_development_setup = is_development_setup
+        self.has_purge_sql = has_purge_sql
+        self.ldap_profile = ldap_profile
+        self.has_ldap_user_import = has_ldap_user_import
+
+    def create(self):
+        self.prepare_sql()
+        self.site = self.setup_plone_site()
+
+        self.setup_ldap()
+        self.import_ldap_users()
+        self.configure_admin_unit()
+        self.install_policy_profile()
+        self.configure_plone_site()
+        self.install_additional_profiles()
+
+        if self.is_development_setup:
+            self.configure_development_options()
+
+    def prepare_sql(self):
+        if not self.has_purge_sql:
+            return
+
+        self.drop_sql_tables(self.db_session)
+
+    def setup_plone_site(self):
+        config = self.config
+        ext_profiles = list(EXTENSION_PROFILES)
+        ext_profiles.append(config['base_profile'])
+
+        return addPloneSite(
+            self.context,
+            config['admin_unit_id'].encode('utf-8'),
+            title=config['title'],
+            profile_id=_DEFAULT_PROFILE,
+            extension_ids=ext_profiles,
+            setup_content=False,
+            default_language=config.get('language', 'de-ch'),
+        )
+
+    def setup_ldap(self):
+        if not self.ldap_profile:
+            return
+
+        stool = getToolByName(self.site, 'portal_setup')
+        stool = getToolByName(self.site, 'portal_setup')
+        stool.runAllImportStepsFromProfile(
+            'profile-{0}'.format(self.ldap_profile))
+
+        # Configure credentials from JSON file at
+        # ~/.opengever/ldap/{hostname}.json
+        configure_ldap_credentials(self.site)
+
+        acl_users = getToolByName(self.site, 'acl_users')
+        plugins = acl_users.plugins
+
+        # disable source_groups when using ldap
+        for ptype in plugins.listPluginTypeInfo():
+            try:
+                plugins.deactivatePlugin(ptype['interface'],
+                                         'source_groups')
+            except KeyError:
+                pass
+
+        # deactivate recursive groups
+        for ptype in plugins.listPluginTypeInfo():
+            try:
+                plugins.deactivatePlugin(ptype['interface'],
+                                         'recursive_groups')
+            except KeyError:
+                pass
+
+        # move ldap up
+        plugins.movePluginsUp(IPropertiesPlugin, ('ldap',))
+        plugins.movePluginsUp(IPropertiesPlugin, ('ldap',))
+        plugins.movePluginsUp(IPropertiesPlugin, ('ldap',))
+
+    def import_ldap_users(self):
+        if not self.has_ldap_user_import:
+            return
+
+        print '===== SYNC LDAP ===='
+        # Import LDAP users and groups
+        sync_ogds(self.site)
+        print '===== Done SYNCING LDAP ===='
+
+    def configure_admin_unit(self):
+        registry = getUtility(IRegistry)
+        proxy = registry.forInterface(IAdminUnitConfiguration)
+        proxy.current_unit_id = self.config['admin_unit_id'].decode('utf-8')
+
+    def install_policy_profile(self):
+        # import the defaul generic setup profiles if needed
+        policy_profile = self.config.get('policy_profile')
+        stool = getToolByName(self.site, 'portal_setup')
+        stool.runAllImportStepsFromProfile(
+            'profile-{}'.format(policy_profile))
+
+    def configure_plone_site(self):
+        # configure mail settings
+        site_title = self.config['title']
+        registry = getUtility(IRegistry)
+        proxy = registry.forInterface(IMailSettings)
+        proxy.mail_domain = self.config['mail_domain'].decode('utf-8')
+        self.site.manage_changeProperties(
+            {'email_from_address': self.config['mail_from_address'],
+             'email_from_name': site_title})
+        # set the site title
+        self.site.manage_changeProperties(title=site_title)
+
+        self.assign_group_to_role(self.site, self.config,
+                                  'reader_group', 'Member')
+        self.assign_group_to_role(self.site, self.config,
+                                  'rolemanager_group', 'Role Manager')
+        self.assign_group_to_role(self.site, self.config,
+                                  'administrator_group', 'Administrator')
+
+        # REALLY set the language - the plone4 addPloneSite is really
+        # buggy with languages.
+        langCP = getAdapter(self.site, ILanguageSelectionSchema)
+        langCP.default_language = 'de-ch'
+
+    def assign_group_to_role(self, site, config, group_name_key, role_name):
+        group_name = config.get(group_name_key)
+        if not group_name:
+            return
+
+        site.acl_users.portal_role_manager.assignRoleToPrincipal(
+            role_name, group_name)
+
+    def install_additional_profiles(self):
+        additional_profiles = self.config.get('additional_profiles')
+        stool = getToolByName(self.site, 'portal_setup')
+        if additional_profiles:
+            for additional_profile in additional_profiles:
+                stool.runAllImportStepsFromProfile(
+                    'profile-%s' % additional_profile)
+
+    def configure_development_options(self):
+        for obj in self.site.listFolderContents():
+            if IRoleManager.providedBy(obj):
+                obj.manage_addLocalRoles(
+                    DEVELOPMENT_USERS_GROUP,
+                    ["Contributor", "Editor", "Reader"])
+
+    def drop_sql_tables(self, session):
+        """Drops all sql tables, usually for a dev-setup.
+        """
+        for base in SQL_BASES:
+            try:
+                getattr(base, 'metadata').drop_all(session.bind)
+            except NoReferencedTableError:
+                pass
