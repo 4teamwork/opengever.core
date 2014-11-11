0e039d9b969845c5b1b7342a54e3b6ef083c81e4
Lukas Graf <lukas.graf@4teamwork.ch>
2014-11-11 12:32:28 +0100

    Add builders for LDAP users and groups.

--- opengever/testing/builders/__init__.py
+++ opengever/testing/builders/__init__.py
@@ -1,3 +1,4 @@
 from opengever.testing.builders.dx import *
 from opengever.testing.builders.fixture import *
+from opengever.testing.builders.ldap import *
 from opengever.testing.builders.sql import *
