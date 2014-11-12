fdde491ce20398791caffc9a214afa39276058e7
Philippe Gross <philippe.gross@4teamwork.ch>
2014-11-12 09:52:05 +0100

    Reworked PasteItem tests (use ftw.testbrowser instead of OGBrowser).

--- opengever/base/tests/test_paste_items.py
+++ opengever/base/tests/test_paste_items.py
@@ -1,37 +1,42 @@
 from ftw.builder import Builder
 from ftw.builder import create
+from ftw.testbrowser import browsing
 from opengever.testing import FunctionalTestCase
 
 
 class TestPasteItems(FunctionalTestCase):
 
-    use_browser = True
-
     def setUp(self):
         super(TestPasteItems, self).setUp()
         self.grant('Manager')
 
-    def test_paste_action_not_displayed_for_contactfolder(self):
+    @browsing
+    def test_paste_action_not_displayed_for_contactfolder(self, browser):
         contactfolder = create(Builder('contactfolder'))
         contact = create(Builder('contact')
                          .within(contactfolder))
 
-        contactfolder.manage_copyObjects(contact.id)
+        paths = ['/'.join(contact.getPhysicalPath())]
+        browser.login().open(contactfolder, {'paths:list': paths},
+                             view='copy_items')
 
-        self.browser.open(contactfolder.absolute_url())
-        self.assertNotIn('actionicon-object_buttons-paste',
-                         self.browser.contents)
+        browser.open(contactfolder)
+        actions = browser.css('#plone-contentmenu-actions li').text
+        self.assertSequenceEqual(['Sharing'], actions)
 
-    def test_paste_action_not_displayed_for_templatedossier(self):
+    @browsing
+    def test_paste_action_not_displayed_for_templatedossier(self, browser):
         templatedossier = create(Builder('templatedossier'))
         document = create(Builder('document')
                          .within(templatedossier))
 
-        templatedossier.manage_copyObjects(document.id)
+        paths = ['/'.join(document.getPhysicalPath())]
+        browser.login().open(templatedossier, {'paths:list': paths},
+                             view='copy_items')
 
-        self.browser.open(templatedossier.absolute_url())
-        self.assertNotIn('actionicon-object_buttons-paste',
-                         self.browser.contents)
+        browser.open(templatedossier)
+        actions = browser.css('#plone-contentmenu-actions li').text
+        self.assertSequenceEqual(['Properties', 'Sharing'], actions)
 
     def test_pasting_copied_document_into_dossier_succeeds(self):
         dossier = create(Builder('dossier'))
