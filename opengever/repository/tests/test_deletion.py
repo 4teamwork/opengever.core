0480d725a8c8858f6c8bd21c2ad4e4efb001585a
Philippe Gross <philippe.gross@4teamwork.ch>
2014-11-21 16:29:09 +0100

    Add repository delete action and confirmation view.

---
+++ opengever/repository/tests/test_deletion.py
@@ -0,0 +1,104 @@
+from ftw.builder import Builder
+from ftw.builder import create
+from ftw.testbrowser import browsing
+from ftw.testbrowser.pages.statusmessages import assert_message
+from opengever.repository.deleter import RepositoryDeleter
+from opengever.testing import FunctionalTestCase
+from zExceptions import Unauthorized
+
+
+class TestRepositoryDeleter(FunctionalTestCase):
+
+    def setUp(self):
+        super(TestRepositoryDeleter, self).setUp()
+
+        self.repository_root = create(Builder('repository_root'))
+        self.repository = create(Builder('repository')
+                                 .within(self.repository_root))
+
+    def test_deletion_is_not_allowed_when_repository_is_not_empty(self):
+        create(Builder('dossier').within(self.repository))
+        deleter = RepositoryDeleter(self.repository)
+
+        self.assertFalse(deleter.is_deletion_allowed())
+
+    def test_deletion_is_allowed_when_repository_is_empty(self):
+        deleter = RepositoryDeleter(self.repository)
+
+        self.assertTrue(deleter.is_deletion_allowed())
+
+    def test_repository_deletion(self):
+        deleter = RepositoryDeleter(self.repository)
+        deleter.delete()
+
+        self.assertEquals([], self.repository_root.listFolderContents())
+
+    def test_repository_deletion_raises_unauthorized_when_preconditions_not_satisfied(self):
+        create(Builder('dossier').within(self.repository))
+        deleter = RepositoryDeleter(self.repository)
+
+        with self.assertRaises(Unauthorized):
+            deleter.delete()
+
+
+class TestRepositoryDeletion(FunctionalTestCase):
+
+    def setUp(self):
+        super(TestRepositoryDeletion, self).setUp()
+
+        self.repository_root = create(Builder('repository_root'))
+        self.repository = create(Builder('repository')
+                                 .within(self.repository_root))
+
+        self.grant('Administrator')
+
+    @browsing
+    def test_delete_action_is_not_available_when_preconditions_not_satisfied(self, browser):
+        create(Builder('dossier').within(self.repository))
+        browser.login().open(self.repository)
+        self.assertEquals(
+            ['Prefix Manager',
+             'Properties',
+             'Sharing',
+             'repositoryfolder-transition-inactivate'],
+            browser.css('#plone-contentmenu-actions .actionMenuContent a').text)
+
+    @browsing
+    def test_delete_action_is_available_when_preconditions_satisfied(self, browser):
+        browser.login().open(self.repository)
+        self.assertEquals(
+            ['Delete',
+             'Prefix Manager',
+             'Properties',
+             'Sharing',
+             'repositoryfolder-transition-inactivate'],
+            browser.css('#plone-contentmenu-actions .actionMenuContent a').text)
+
+    @browsing
+    def test_raise_unauthorized_when_preconditions_not_satisfied(self, browser):
+        create(Builder('dossier').within(self.repository))
+        with self.assertRaises(Unauthorized):
+            browser.login().open(self.repository, view='delete_repository')
+
+    @browsing
+    def test_cancel_redirects_back_to_repository(self, browser):
+        browser.login().open(self.repository, view='delete_repository')
+        browser.css('#form-buttons-cancel').first.click()
+
+        self.assertEquals(self.repository.absolute_url(), browser.url)
+
+    @browsing
+    def test_submit_redirects_to_parent(self, browser):
+        browser.login().open(self.repository, view='delete_repository')
+        browser.css('#form-buttons-delete').first.click()
+
+        self.assertEquals(self.repository_root.absolute_url(), browser.url)
+        assert_message("The repository have been successfully deleted.")
+
+    @browsing
+    def test_form_is_csrf_safe(self, browser):
+        url = '{}/delete_repository?form.buttons.delete=true'.format(
+            self.repository.absolute_url())
+
+        with self.assertRaises(Unauthorized):
+            browser.login().open(url)
