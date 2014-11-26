e6f76632adad579026860f228e85526487b9be7d
David Erni <david.erni@4teamwork.ch>
2014-11-26 15:22:37 +0100

    Add upgrade step to install meeting profile.

---
+++ opengever/policy/base/upgrades/to4000.py
@@ -0,0 +1,20 @@
+from ftw.upgrade import UpgradeStep
+from opengever.core import model
+
+
+class InstallMeeting(UpgradeStep):
+
+    def __call__(self):
+        self.setup_install_profile(
+            'profile-opengever.meeting:default')
+        self.create_meeting_tables()
+
+    def create_meeting_tables(self):
+        """When installing meeting via upgrade-step we need to initialize
+        its database tables.
+
+        If if is instlalled on an new plone-site `opengever.base.hooks` takes
+        care of that.
+
+        """
+        model.Base.metadata.create_all(model.Session().bind, checkfirst=True)
