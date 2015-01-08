efb92893b04a413fbddd5311a3ff09f5e0e6f361
David Erni <david.erni@4teamwork.ch>
2015-01-08 09:57:15 +0100

    Add tests for meeting-feature view.

---
+++ opengever/meeting/tests/test_meetingfeature.py
@@ -0,0 +1,18 @@
+from opengever.core.testing import OPENGEVER_FUNCTIONAL_MEETING_LAYER
+from opengever.testing import FunctionalTestCase
+
+
+class TestDisabledMeetingFeature(FunctionalTestCase):
+
+    def test_meeting_feature_is_enabled(self):
+        view = self.portal.restrictedTraverse('is_meeting_feature_enabled')
+        self.assertFalse(view())
+
+
+class TestEnabledMeetingFeature(FunctionalTestCase):
+
+    layer = OPENGEVER_FUNCTIONAL_MEETING_LAYER
+
+    def test_meeting_feature_is_enabled(self):
+        view = self.portal.restrictedTraverse('is_meeting_feature_enabled')
+        self.assertTrue(view())
