ea42085f726bc20f109dd49281916dc9e54346b5
David Erni <david.erni@4teamwork.ch>
2015-01-21 14:23:52 +0100

    Update meeting schema with participants.

--- opengever/meeting/upgrades/to4202.py
+++ opengever/meeting/upgrades/to4202.py
@@ -1,16 +1,37 @@
 from opengever.core.upgrade import SchemaMigration
 from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
 from sqlalchemy import Text
 
 
-class AddAgendaItemColumns(SchemaMigration):
+class AddProtocolsToMeetings(SchemaMigration):
 
     profileid = 'opengever.meeting'
     upgradeid = 4202
 
     def migrate(self):
         self.create_agenda_item_columns()
+        self.create_meeting_columns()
+        self.create_participants_secondary_table()
 
     def create_agenda_item_columns(self):
         self.op.add_column('agendaitems', Column('discussion', Text()))
         self.op.add_column('agendaitems', Column('decision', Text()))
+
+    def create_meeting_columns(self):
+        self.op.add_column(
+            'meetings',
+            Column('presidency_id', Integer, ForeignKey('members.id')))
+
+        self.op.add_column(
+            'meetings',
+            Column('secretary_id', Integer, ForeignKey('members.id')))
+
+        self.op.add_column('meetings', Column('other_participants', Text()))
+
+    def create_participants_secondary_table(self):
+        self.op.create_table(
+            'meeting_participants',
+            Column('meeting_id', Integer, ForeignKey('meetings.id')),
+            Column('member_id', Integer, ForeignKey('members.id')))
