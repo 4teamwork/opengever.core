0b93a843f7a8e4d740e9a945f3b52a6353b89cd1
David Erni <david.erni@4teamwork.ch>
2015-01-29 10:21:08 +0100

    Make date_from a primary key for memberships.

--- opengever/meeting/upgrades/to4203.py
+++ opengever/meeting/upgrades/to4203.py
@@ -10,6 +10,20 @@ class AddMembershipRole(SchemaMigration):
 
     def migrate(self):
         self.add_role_to_membership()
+        self.make_start_date_primary()
 
     def add_role_to_membership(self):
         self.op.add_column('memberships', Column('role', String(256)))
+
+    def make_start_date_primary(self):
+        """this works only under the assumption that we run the migration
+        with mysql.
+
+        Luckily this is true since it is not in use in production. And a first
+        activation in production will be done by differently, i.e. by creating
+        all tables with metadata.create_all().
+
+        """
+        self.op.execute(
+            "ALTER TABLE `memberships` DROP PRIMARY KEY, "
+            "ADD PRIMARY KEY (`committee_id`, `member_id`, `date_from`);")
