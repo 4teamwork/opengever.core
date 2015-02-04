7fe0bd9b0286955e5b96a0e4954fcfa79cc60e06
Philippe Gross <philippe.gross@4teamwork.ch>
2015-02-04 14:24:04 +0100

    Added first version of NotificationMailer.

---
+++ opengever/activity/mail.py
@@ -0,0 +1,49 @@
+from Acquisition import aq_inner
+from email.header import Header
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from opengever.ogds.base.utils import ogds_service
+from plone import api
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class NotificationMailer(object):
+
+    def __init__(self, context, notification):
+        self.context = aq_inner(context)
+        self.request = self.context.REQUEST
+        self.notification = notification
+
+    def send_mail(self):
+        msg = self.prepare_mail()
+        mailhost = api.portal.get_tool('MailHost')
+        mailhost.send(msg)
+
+    def prepare_mail(self):
+        msg = MIMEMultipart('alternative')
+
+        actor = ogds_service().fetch_user(self.notification.activity.actor_id)
+        msg['From'] = Header(u'{} <{}>'.format(actor.fullname(), actor.email),
+                             'utf-8')
+
+        recipient = ogds_service().fetch_user(
+            self.notification.watcher.user_id)
+        msg['To'] = recipient.email
+        msg['Subject'] = Header(self.notification.activity.title, 'utf-8')
+
+        html = self.prepare_html()
+        msg.attach(MIMEText(html.encode('utf-8'), 'html', 'utf-8'))
+
+        return msg
+
+    def prepare_html(self):
+        template = ViewPageTemplateFile("mail_templates/notification.pt")
+        options = {
+            'subject': self.notification.activity.title,
+            'title': self.notification.activity.title,
+            'kind': self.notification.activity.kind,
+            'summary': self.notification.activity.summary,
+            'description': self.notification.activity.description,
+            'link': self.notification.activity.get_link()}
+
+        return template(self, **options)
---
+++ opengever/activity/tests/test_mail.py
@@ -0,0 +1,61 @@
+from datetime import date
+from ftw.builder import Builder
+from ftw.builder import create
+from ftw.testbrowser import browsing
+from ftw.testing.mailing import Mailing
+from opengever.testing import FunctionalTestCase
+from plone.app.testing import TEST_USER_ID
+import email
+
+
+class TestEmailNotification(FunctionalTestCase):
+
+    def setUp(self):
+        super(TestEmailNotification, self).setUp()
+
+        # setup mail
+        Mailing(self.portal).set_up()
+
+        create(Builder('ogds_user')
+               .assign_to_org_units([self.org_unit])
+               .having(userid='hugo.boss',
+                       firstname='Hugo',
+                       lastname='Boss',
+                       email='hugo.boss@example.org'))
+
+        create(Builder('ogds_user')
+               .assign_to_org_units([self.org_unit])
+               .having(userid='franz.michel',
+                       firstname='Franz',
+                       lastname='Michel',
+                       email='hugo.boss@example.org'))
+
+        self.dossier = create(Builder('dossier').titled(u'Dossier A'))
+
+    def tearDown(self):
+        super(TestEmailNotification, self).tearDown()
+        Mailing(self.layer['portal']).tear_down()
+
+    @browsing
+    def test_subject_is_title(self, browser):
+        browser.login().open(self.dossier, view='++add++opengever.task.task')
+        browser.fill({'Title': 'Test Task',
+                      'Responsible':'hugo.boss',
+                      'Task Type': 'comment'})
+        browser.css('#form-buttons-save').first.click()
+
+        mail = email.message_from_string(Mailing(self.portal).pop())
+
+        self.assertEquals('Test Task', mail.get('Subject'))
+
+    @browsing
+    def test_from_and_to_addresses(self, browser):
+        browser.login().open(self.dossier, view='++add++opengever.task.task')
+        browser.fill({'Title': 'Test Task',
+                      'Responsible':'hugo.boss',
+                      'Task Type': 'comment'})
+        browser.css('#form-buttons-save').first.click()
+
+        mail = email.message_from_string(Mailing(self.portal).pop())
+        self.assertEquals('hugo.boss@example.org', mail.get('To'))
+        self.assertEquals('Test User <test@example.org>', mail.get('From'))
