965ee76adcc1267d38c21ed4396e7dc26a421133
David Erni <david.erni@4teamwork.ch>
2015-02-16 17:18:30 +0100

    Filter non-selectable proposals when submitting additional documents.

--- opengever/meeting/browser/documents/submit.py
+++ opengever/meeting/browser/documents/submit.py
@@ -1,11 +1,26 @@
 from opengever.base.source import DossierPathSourceBinder
 from opengever.document import _
+from opengever.meeting.proposal import IProposal
 from plone import api
 from plone.autoform.form import AutoExtensibleForm
 from plone.directives import form
+from plone.formwidget.contenttree.source import CustomFilter
 from z3c.form.button import buttonAndHandler
 from z3c.form.form import Form
 from z3c.relationfield.schema import RelationChoice
+
+
+class SubmittableProposalFilter(CustomFilter):
+    """Custom selectable filter to only allow selection of proposals which
+    allow submission of additional documents.
+
+    """
+    def __call__(self, brain, index_data):
+        obj = brain.getObject()
+        if IProposal.providedBy(obj):
+            return IProposal(obj).is_submit_additional_documents_allowed()
+        return super(SubmittableProposalFilter, self).__call__(
+            brain, index_data)
 
 
 class ISubmitAdditionalDocument(form.Schema):
@@ -13,6 +28,7 @@ class ISubmitAdditionalDocument(form.Schema):
     proposal = RelationChoice(
         title=_(u'Proposal', default=u'Proposal'),
         source=DossierPathSourceBinder(
+            filter_class=SubmittableProposalFilter,
             portal_type=("opengever.meeting.proposal"),
             navigation_tree_query={
                 'object_provides':
