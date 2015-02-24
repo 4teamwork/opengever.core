5a856482c1961aad856714fc8d154236dfbe68f5
Philippe Gross <philippe.gross@4teamwork.ch>
2015-02-24 08:34:00 +0100

    Activity listing: added translation for the kind column.

--- opengever/activity/browser/listing.py
+++ opengever/activity/browser/listing.py
@@ -7,6 +7,8 @@ from opengever.ogds.base.actor import Actor
 from opengever.tabbedview import _
 from opengever.tabbedview.browser.base import BaseListingTab
 from opengever.tabbedview.browser.base import BaseTableSource
+from zope.globalrequest import getRequest
+from zope.i18n import translate
 from zope.interface import implements
 from zope.interface import Interface
 
@@ -21,6 +23,10 @@ def readable_actor(item, value):
     return Actor.lookup(item.activity.actor_id).get_label()
 
 
+def translated_kind(item, value):
+    return translate(item.activity.kind, domain='plone', context=getRequest())
+
+
 class INotificationTableSourceConfig(ITableSourceConfig):
     """Marker interface for notification table source configs."""
 
@@ -33,7 +39,7 @@ class NotificationListingTab(BaseListingTab):
     columns = (
         {'column': 'kind',
          'column_title': _(u'column_kind', default=u'Kind'),
-         'transform': lambda item, value: item.activity.kind},
+         'transform': translated_kind},
 
         {'column': '',
          'column_title': _(u'column_title', default=u'Title'),
