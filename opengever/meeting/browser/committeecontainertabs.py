c7c8775e1ccf46d3585648e8282210837167aaf7
Philippe Gross <philippe.gross@4teamwork.ch>
2015-02-24 13:50:12 +0100

    Meeting listings: Disable selection links for listins without checkbox column.

--- opengever/meeting/browser/committeecontainertabs.py
+++ opengever/meeting/browser/committeecontainertabs.py
@@ -2,11 +2,14 @@ from five import grok
 from opengever.meeting.committeecontainer import ICommitteeContainer
 from opengever.meeting.tabs.committeelisting import CommitteeListingTab
 from opengever.meeting.tabs.memberlisting import MemberListingTab
+from zope.app.pagetemplate import ViewPageTemplateFile
 
 
 class Committees(CommitteeListingTab):
     grok.name('tabbedview_view-committees')
     grok.context(ICommitteeContainer)
+
+    selection = ViewPageTemplateFile("templates/no_selection.pt")
 
     enabled_actions = []
     major_actions = []
@@ -15,6 +18,8 @@ class Committees(CommitteeListingTab):
 class Members(MemberListingTab):
     grok.name('tabbedview_view-members')
     grok.context(ICommitteeContainer)
+
+    selection = ViewPageTemplateFile("templates/no_selection.pt")
 
     enabled_actions = []
     major_actions = []
