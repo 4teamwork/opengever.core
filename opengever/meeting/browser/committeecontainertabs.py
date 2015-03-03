8acb2cec39f21ab61cd79fc147b1afcf4afff40b
Philippe Gross <philippe.gross@4teamwork.ch>
2015-03-03 11:37:10 +0100

    Replaced committelisting tab with a committe overview listing.

--- opengever/meeting/browser/committeecontainertabs.py
+++ opengever/meeting/browser/committeecontainertabs.py
@@ -1,19 +1,31 @@
 from five import grok
 from opengever.meeting.committeecontainer import ICommitteeContainer
-from opengever.meeting.tabs.committeelisting import CommitteeListingTab
+from opengever.meeting.service import meeting_service
 from opengever.meeting.tabs.memberlisting import MemberListingTab
+from opengever.tabbedview.browser.base import OpengeverTab
 from zope.app.pagetemplate import ViewPageTemplateFile
 
 
-class Committees(CommitteeListingTab):
+class Committees(grok.View, OpengeverTab):
     grok.name('tabbedview_view-committees')
     grok.context(ICommitteeContainer)
+    grok.require('zope2.View')
+    grok.template('committee')
 
-    selection = ViewPageTemplateFile("templates/no_selection.pt")
-    sort_on = 'title'
+    def committees(self):
+        committees = []
 
-    enabled_actions = []
-    major_actions = []
+        for committee in meeting_service().all_committees():
+            committees.append(
+                {'title': committee.title,
+                 'url': committee.get_url(),
+                 'number_unscheduled_proposals': len(
+                     meeting_service().get_submitted_proposals(committee)),
+                 'next_meeting': meeting_service().get_next_meeting(committee),
+                 'last_meeting': meeting_service().get_last_meeting(committee)}
+            )
+
+        return committees
 
 
 class Members(MemberListingTab):
