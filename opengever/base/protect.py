9b93bf63145ace547e7f82de30bece086e6cb42d
Jonas Baumann <jone@jone.ch>
2015-03-04 12:00:59 +0100

    always allow unprotected writes to memberdata cache.

--- opengever/base/protect.py
+++ opengever/base/protect.py
@@ -1,6 +1,8 @@
 from plone.protect.auto import ProtectTransform
+from Products.CMFCore.utils import getToolByName
 from zope.annotation.attribute import AttributeAnnotations
 from zope.component import adapts
+from zope.component.hooks import getSite
 from zope.globalrequest import getRequest
 from zope.interface import Interface
 from zope.publisher.interfaces.browser import IBrowserRequest
@@ -43,6 +45,11 @@ class OGProtectTransform(ProtectTransform):
     adapts(Interface, IBrowserRequest)
 
     def _registered_objects(self):
+        self._global_unprotect()
         objects = super(OGProtectTransform, self)._registered_objects()
         unprotected = _get_unprotected_objects()
         return filter(lambda obj: obj not in unprotected, objects)
+
+    def _global_unprotect(self):
+        # portal_memberdata._members cache will be written sometimes.
+        unprotected_write(getToolByName(getSite(), 'portal_memberdata')._members)
