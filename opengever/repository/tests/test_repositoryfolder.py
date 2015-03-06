d9406210e742828aa95decfd8cabe6cd4dcc4034
Jonas Baumann <jone@jone.ch>
2015-03-06 16:50:16 +0100

    Rewrite repositorfolder test and add workaround for CSRF.
    The problem is that when renering the add form of the very first repository folder,
    it changes the repository root object itself which we do not want to whitelist.

--- opengever/repository/tests/test_repositoryfolder.py
+++ opengever/repository/tests/test_repositoryfolder.py
@@ -1,14 +1,20 @@
 from ftw.builder import Builder
 from ftw.builder import create
+from ftw.testbrowser import browsing
+from ftw.testbrowser.pages import factoriesmenu
+from ftw.testbrowser.pages import plone
+from opengever.base.adapters import ReferenceNumberPrefixAdpater
 from opengever.base.interfaces import IReferenceNumberSettings
 from opengever.repository.behaviors.referenceprefix import IReferenceNumberPrefix
 from opengever.repository.repositoryfolder import IRepositoryFolderSchema
 from opengever.testing import FunctionalTestCase
 from plone.dexterity.interfaces import IDexterityFTI
+from plone.protect import createToken
 from plone.registry.interfaces import IRegistry
 from zope.component import createObject
 from zope.component import getUtility
 from zope.component import queryUtility
+import transaction
 
 
 class TestRepositoryFolder(FunctionalTestCase):
@@ -65,48 +71,34 @@ class TestRepositoryFolderTitleAccessor(FunctionalTestCase):
 
 
 class TestRepositoryFolderWithBrowser(FunctionalTestCase):
-    use_browser = True
 
     def setUp(self):
         super(TestRepositoryFolderWithBrowser, self).setUp()
         self.grant('Manager')
 
-        # Since the Generic Setup profile was imported,
-        # we should have a some FTIs registered::
-        pt = self.portal.portal_types
-        pt.get('opengever.repository.repositoryfolder', None)
-        pt.get('opengever.repository.repositoryroot', None)
+    @browsing
+    def test_repository_folder(self, browser):
+        browser.login().open()
+        factoriesmenu.add('RepositoryRoot')
+        browser.fill({'Title': 'Registraturplan'}).save()
+        self.assertEquals(('tabbed_view', 'opengever-repository-repositoryroot'),
+                          plone.view_and_portal_type())
+        registraturplan = browser.context
 
-    def test_repository_folder(self):
-        # So, lets create the repository-root::
-        self.browser.open('http://nohost/plone/folder_factories')
-        self.assertPageContains('opengever.repository.repositoryroot')
-        self.browser.getControl('RepositoryRoot').click()
-        self.browser.getControl('Add').click()
-        self.browser.getControl('Title').value = 'Registraturplan'
-        self.browser.getControl('Description').value = ''
-        self.browser.getControl('Save').click()
-        self.browser.assert_url('http://nohost/plone/registraturplan/tabbed_view')
+        # This will cause a WRITE to registraturplan the first time it is accessed.
+        browser.open(registraturplan, view='++add++opengever.repository.repositoryfolder',
+                     data={'_authenticator': createToken()})
+        browser.fill({'Title': 'Accounting'}).save()
+        self.assertEquals(('tabbed_view', 'opengever-repository-repositoryfolder'),
+                          plone.view_and_portal_type())
+        accounting = browser.context
+        self.assertEquals(u'1', IReferenceNumberPrefix(accounting).reference_number_prefix)
+        self.assertEquals(('test_user_1_',), accounting.listCreators())
 
-        # Now, create our first repository folder::
-        self.browser.open('./folder_factories')
-        self.assertPageContains('opengever.repository.repositoryfolder')
-        self.browser.getControl('RepositoryFolder').click()
-        self.browser.getControl('Add').click()
-        self.browser.getControl('Title').value = 'Accounting'
-        self.browser.getControl('Save').click()
-        self.browser.assert_url('http://nohost/plone/registraturplan/accounting/tabbed_view')
-
-        # Check some stuff::
-        obj = self.portal.get('registraturplan').get('accounting')
-        self.assertEquals(u'1', IReferenceNumberPrefix(obj).reference_number_prefix)
-        self.assertEquals(('test_user_1_',), obj.listCreators())
-
-        # Add another one::
-        self.browser.open('http://nohost/plone/registraturplan/++add++opengever.repository.repositoryfolder')
-        self.browser.getControl('Title').value = 'Custody'
-        self.browser.getControl('Save').click()
-        self.browser.assert_url('http://nohost/plone/registraturplan/custody/tabbed_view')
-
-        obj = self.portal.get('registraturplan').get('custody')
-        self.assertEquals(u'2', IReferenceNumberPrefix(obj).reference_number_prefix)
+        browser.open(registraturplan)
+        factoriesmenu.add('RepositoryFolder')
+        browser.fill({'Title': 'Custody'}).save()
+        self.assertEquals(('tabbed_view', 'opengever-repository-repositoryfolder'),
+                          plone.view_and_portal_type())
+        custody = browser.context
+        self.assertEquals(u'2', IReferenceNumberPrefix(custody).reference_number_prefix)
