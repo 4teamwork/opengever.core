d1ae8a4edbf3fc3c54ade9495c616fdfaaa5e385
David Erni <david.erni@4teamwork.ch>
2015-03-16 14:05:20 +0100

    Fix an issue with event handlers while deleting a plone-site.

--- opengever/meeting/handlers.py
+++ opengever/meeting/handlers.py
@@ -3,10 +3,21 @@ from OFS.interfaces import IObjectWillBeRemovedEvent
 from opengever.base.model import create_session
 from opengever.document.document import IDocumentSchema
 from opengever.meeting.model import SubmittedDocument
+from zope.component import getUtility
+from zope.component.interfaces import ComponentLookupError
+from zope.intid.interfaces import IIntIds
 
 
 @grok.subscribe(IDocumentSchema, IObjectWillBeRemovedEvent)
 def document_deleted(context, event):
+    # this event is also fired when deleting a plone site. Unfortunately
+    # no deletion-order seems to be guaranteed, so it might happen that the
+    # IntId utility is removed before removing content.
+    try:
+        getUtility(IIntIds)
+    except ComponentLookupError:
+        return
+
     session = create_session()
     for doc in SubmittedDocument.query.by_document(context).all():
         session.delete(doc)
