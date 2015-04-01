f52f7a951802770dc7528c30e79e767fa54b3a41
David Erni <david.erni@4teamwork.ch>
2015-04-01 16:40:47 +0200

    Drop template utility in favour of utility function.

--- opengever/dossier/templatedossier/__init__.py
+++ opengever/dossier/templatedossier/__init__.py
@@ -1,2 +1,13 @@
 from opengever.dossier.templatedossier.interfaces import ITemplateDossier
-from opengever.dossier.templatedossier.interfaces import ITemplateUtility
+from plone import api
+
+
+def get_template_dossier():
+    catalog = api.portal.get_tool('portal_catalog')
+    result = catalog(
+        portal_type="opengever.dossier.templatedossier",
+        sort_on='path')
+
+    if result:
+        return result[0].getObject()
+    return None
