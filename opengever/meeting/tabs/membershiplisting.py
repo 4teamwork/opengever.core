2f3522b799b591dca17d48a65c195da599c97ba2
Philippe Gross <philippe.gross@4teamwork.ch>
2015-04-10 14:46:59 +0200

    Membership listing: link memberships to the member view.

--- opengever/meeting/tabs/membershiplisting.py
+++ opengever/meeting/tabs/membershiplisting.py
@@ -18,26 +18,31 @@ class MembershipListingTab(BaseListingTab):
 
     model = Membership
 
-    columns = (
-        {'column': 'member_id',
-         'column_title': _(u'column_title', default=u'Title'),
-         'transform': lambda item, value: item.title()},
+    @property
+    def columns(self):
+        return (
+            {'column': 'member_id',
+             'column_title': _(u'column_member', default=u'Member'),
+             'transform': self.get_member_link},
 
-        {'column': 'date_from',
-         'column_title': _(u'column_date_from', default=u'Date from'),
-         'transform': lambda item, value: item.format_date_from()},
+            {'column': 'date_from',
+             'column_title': _(u'column_date_from', default=u'Date from'),
+             'transform': lambda item, value: item.format_date_from()},
 
-        {'column': 'date_to',
-         'column_title': _(u'column_date_to', default=u'Date to'),
-         'transform': lambda item, value: item.format_date_to()},
+            {'column': 'date_to',
+             'column_title': _(u'column_date_to', default=u'Date to'),
+             'transform': lambda item, value: item.format_date_to()},
 
-        {'column': 'role',
-         'column_title': _(u'column_role', default=u'Role'),
-         'transform': lambda item, value: item.role},
-    )
+            {'column': 'role',
+             'column_title': _(u'column_role', default=u'Role'),
+             'transform': lambda item, value: item.role},
+        )
 
     def get_base_query(self):
         return Membership.query.filter_by(committee=self.context.load_model())
+
+    def get_member_link(self, item, value):
+        return item.member.get_link(self.context)
 
 
 class MembershipTableSource(SqlTableSource):
