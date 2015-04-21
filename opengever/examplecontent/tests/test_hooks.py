1466c23100c42251f07d1c846f0a2e2ac987e664
David Erni <david.erni@4teamwork.ch>
2015-04-21 15:26:12 +0200

    Create separate profiles, with small and big repository.

--- opengever/examplecontent/tests/test_hooks.py
+++ opengever/examplecontent/tests/test_hooks.py
@@ -6,6 +6,11 @@ from plone.app.testing import applyProfile
 class TestHooks(FunctionalTestCase):
 
     def test_execute_examplecontent_hooks(self):
+        """Test that examplecontent hooks are executed successfully."""
+
         applyProfile(api.portal.get(), 'opengever.setup:default_content')
         applyProfile(api.portal.get(), 'opengever.setup:empty_templates')
+        applyProfile(api.portal.get(), 'opengever.examplecontent:repository_minimal')
         applyProfile(api.portal.get(), 'opengever.examplecontent:init')
+
+
