6be452ef9511d4424d63eb9ef71f98065b25143d
David Erni <david.erni@4teamwork.ch>
2015-04-28 16:20:02 +0200

    Add MySQL implementations for creating/dropping indices.

--- opengever/core/upgrade.py
+++ opengever/core/upgrade.py
@@ -184,14 +184,30 @@ class IdempotentOperations(Operations):
             *args, **kwargs)
 
     @metadata_operation
-    def create_index(self, *args, **kwargs):
+    def create_index(self, name, table_name, columns, schema=None,
+                     unique=False, quote=None, **kw):
+        if self._has_index(name, table_name):
+            logger.log(logging.INFO,
+                       "Skipping create index '{0}' for table '{1}', "
+                       "index already exists."
+                       .format(name, table_name))
+            return
+
         return super(IdempotentOperations, self).create_index(
-            *args, **kwargs)
+            name, table_name, columns,
+            schema=schema, unique=unique, quote=quote, **kw)
 
     @metadata_operation
-    def drop_index(self, *args, **kwargs):
+    def drop_index(self, name, table_name=None, schema=None):
+        if not self._has_index(name, table_name):
+            logger.log(logging.INFO,
+                       "Skipping drop index '{0}' for table '{1}', "
+                       "index no longer exists."
+                       .format(name, table_name))
+            return
+
         return super(IdempotentOperations, self).drop_index(
-            *args, **kwargs)
+            name, table_name=table_name, schema=schema)
 
 
 class DeactivatedFKConstraint(object):
