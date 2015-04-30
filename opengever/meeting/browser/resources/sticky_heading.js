2333d7c4382337f19b1ad65f5e1fa1b6c2e83dd0
Kevin Bieri <k.bieri@4teamwork.ch>
2015-04-30 17:15:53 +0200

    Provide event callbacks in sticky_heading.js
    Provide callback functions for interacting with events.

--- opengever/meeting/browser/resources/sticky_heading.js
+++ opengever/meeting/browser/resources/sticky_heading.js
@@ -1,21 +1,31 @@
 stickyHeading = function($){
+
+  var onStickyCallback = function() {};
+  var onNoStickyCallback = function() {};
+  var onCollisionCallback = function() {};
+
   var layouter = {
     reset: function(headings){
       $.each(headings, function(i, heading) { heading.node.removeClass('sticky').css('top', 0); });
     },
 
     noSticky: function(scrollPositionTop){
-      // no-op
+      onNoStickyCallback(scrollPositionTop);
     },
 
-    sticky: function(heading, scrollPositionTop){
+    sticky: function(heading, pastHeading, scrollPositionTop){
       heading.node.addClass('sticky');
       heading.node.css('top', scrollPositionTop - heading.offset);
+      if(pastHeading) {
+        this.reset([pastHeading]);
+      }
+      onStickyCallback(heading, scrollPositionTop);
     },
 
     collision: function(fadeOutHeading, fadeInHeading, scrollPositionTop) {
       fadeOutHeading.node.addClass('sticky').css('top', fadeInHeading.offset - fadeOutHeading.offset - fadeOutHeading.height);
       fadeInHeading.node.addClass('sticky');
+      onCollisionCallback(fadeOutHeading, fadeInHeading, scrollPositionTop);
     }
   }
 
@@ -36,14 +46,14 @@ stickyHeading = function($){
 
     if(headings.length < 1) { return false; }
 
-    function findStickyHeading(scrollPositionTop){
+    function findPastHeadings(scrollPositionTop){
       var pastHeadings = [];
       $.each(headings, function(i, heading) {
         if (scrollPositionTop >= heading.offset) {
           pastHeadings.push(heading);
         }
       });
-      return pastHeadings[pastHeadings.length - 1];
+      return pastHeadings;
     }
 
     function onResize() {
@@ -57,14 +67,19 @@ stickyHeading = function($){
       }
       layouter.reset(headings);
       var scrollPositionTop = $(window).scrollTop();
-      var stickyHeading = findStickyHeading(scrollPositionTop);
+      var pastHeadings = findPastHeadings(scrollPositionTop);
 
-      if (stickyHeading) {
-        nextHeading = headings[headings.index(stickyHeading) + 1];
+      if (pastHeadings.length > 0) {
+        var stickyHeading = pastHeadings[pastHeadings.length - 1];
+        var nextHeading = headings[headings.index(stickyHeading) + 1];
+        var pastHeading = null;
+        if(pastHeadings.length > 1) {
+          pastHeading = pastHeadings[pastHeadings.length - 2];
+        }
         if (nextHeading && scrollPositionTop >= nextHeading.offset - stickyHeading.height) {
           layouter.collision(stickyHeading, nextHeading, scrollPositionTop);
         } else {
-          layouter.sticky(stickyHeading, scrollPositionTop);
+          layouter.sticky(stickyHeading, pastHeading, scrollPositionTop);
         }
       } else {
         layouter.noSticky(scrollPositionTop);
@@ -73,5 +88,21 @@ stickyHeading = function($){
 
     $(window).scroll(onScroll);
     $(window).resize(onResize);
+
+    return {
+
+      onSticky: function(callback) {
+        onStickyCallback = callback;
+      },
+
+      onNoSticky: function(callback) {
+        onNoStickyCallback = callback;
+      },
+
+      onCollision: function(callback) {
+        onCollisionCallback = callback;
+      }
+
+    }
   }
 }(jQuery);
