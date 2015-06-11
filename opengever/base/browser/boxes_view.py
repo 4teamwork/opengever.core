27bb2f9d2a42d3ac5f717a3c9ffca455da1ba7f8
Lukas Graf <lukas.graf@4teamwork.ch>
2015-06-11 14:08:45 +0200

    Merge pull request #991 from 4teamwork/deif-fix-inbox-overview
    Inbox-Übersicht render-Probleme beheben.

---
+++ opengever/base/browser/boxes_view.py
@@ -0,0 +1,30 @@
+from opengever.base.browser.helper import get_css_class
+from opengever.globalindex.model.task import Task
+from opengever.globalindex.utils import indexed_task_link_helper
+from Products.ZCatalog.interfaces import ICatalogBrain
+from z3c.form.interfaces import IFieldWidget
+
+
+class BoxesViewMixin(object):
+
+    def render_globalindex_task(self, item):
+        return indexed_task_link_helper(item, item.title)
+
+    def get_item_css_classes(self, item):
+        """Return all css classes for item.
+        """
+        return " ".join(["rollover-breadcrumb", get_css_class(item)])
+
+    def get_type(self, item):
+        """differ the object typ and return the type as string"""
+
+        if isinstance(item, dict):
+            return 'dict'
+        elif ICatalogBrain.providedBy(item):
+            return 'brain'
+        elif IFieldWidget.providedBy(item):
+            return 'widget'
+        elif isinstance(item, Task):
+            return 'globalindex_task'
+        else:
+            raise ValueError("Unknown item type: {!r}".format(item))
