5769e32958d00a1bd6a21e5f479c8e58572429e3
Lukas Graf <lukas.graf@4teamwork.ch>
2015-06-11 14:08:45 +0200

    Merge pull request #991 from 4teamwork/deif-fix-inbox-overview
    Inbox-Übersicht render-Probleme beheben.

--- opengever/inbox/tests/test_overview.py
+++ opengever/inbox/tests/test_overview.py
@@ -1,6 +1,7 @@
 from DateTime import DateTime
 from ftw.builder import Builder
 from ftw.builder import create
+from ftw.testbrowser import browsing
 from opengever.globalindex.handlers.task import sync_task
 from opengever.testing import FunctionalTestCase
 
@@ -23,179 +24,195 @@ class TestBaseInboxOverview(FunctionalTestCase):
             Builder('fixture').with_all_unit_setup())
 
         self.inbox = create(Builder('inbox').titled(u'eingangskorb'))
-        self.view = self.inbox.restrictedTraverse('tabbedview_view-overview')
 
 
 class TestInboxOverviewDocumentBox(TestBaseInboxOverview):
 
-    def list_documents_from_the_inbox(self):
-        inbox_document = create(Builder('document')
-                                .titled('inbox document')
-                                .within(self.inbox))
+    @browsing
+    def test_overview(self, browser):
+        browser.login().open(self.inbox, view='tabbedview_view-overview')
+
+    @browsing
+    def test_inbox_documents_are_listed(self, browser):
+        create(Builder('document')
+               .titled('inbox document')
+               .within(self.inbox))
         create(Builder('document')
                .titled('portal document')
                .within(self.portal))
+        create(Builder('mail')
+               .titled('A mail')
+               .within(self.inbox))
 
-        self.assert_documents([inbox_document, ], self.view.documents())
+        browser.login().open(self.inbox, view='tabbedview_view-overview')
 
-    def test_list_only_documents_directly_inside_the_current_inbox(self):
-        inbox_document = create(Builder('document')
-                                .titled('inbox document')
-                                .within(self.inbox))
+        self.assertSequenceEqual(
+            browser.css('#documentsBox li:not(.moreLink) a').text,
+            ['inbox document', 'A mail'])
+
+    @browsing
+    def test_list_only_documents_directly_inside_the_current_inbox(self, browser):
+        create(Builder('document')
+               .titled('inbox document')
+               .within(self.inbox))
         forwarding = create(Builder('forwarding').within(self.inbox))
         create(Builder('document')
                .titled('forwarding document')
                .within(forwarding))
 
-        self.assert_documents([inbox_document, ], self.view.documents())
+        browser.login().open(self.inbox, view='tabbedview_view-overview')
 
-    def test_list_only_documents_in_the_current_inbox(self):
-        sub_inbox_1 = create(Builder('inbox')
-                             .within(self.inbox)
-                             .having(responsible_org_unit='client1'))
+        self.assertSequenceEqual(
+            browser.css('#documentsBox li:not(.moreLink) a').text,
+            ['inbox document'])
 
-        sub_inbox_2 = create(Builder('inbox')
-                             .within(self.inbox)
-                             .having(responsible_org_unit='client2'))
-
-        doc1 = create(Builder('document')
-                       .titled('Doc 1').within(sub_inbox_1))
+    @browsing
+    def test_document_box_items_are_limited_to_ten_and_sorted_by_modified(self, browser):
+        for i in range(1, 11):
+            create(Builder('document')
+                   .within(self.inbox)
+                   .with_modification_date(DateTime(2010, 1, 1) + i)
+                   .titled(u'Document %s' % i))
         create(Builder('document')
-                       .titled('Doc 2').within(sub_inbox_2))
+               .within(self.inbox)
+               .with_modification_date(DateTime(2009, 12, 8))
+               .titled(u'Document 11'))
 
-        sub_inbox_1_view = sub_inbox_1.restrictedTraverse(
-            'tabbedview_view-overview')
-
-        self.assert_documents([doc1, ], sub_inbox_1_view.documents())
-
-    def test_document_box_is_limited_to_ten_documents(self):
-        for i in range(15):
-            create(Builder('document').within(self.inbox))
-
-        self.assertEquals(10, len(self.view.documents()))
-
-    def assert_documents(self, expected, value):
-        self.assertEquals(
-            [obj.Title() for obj in expected],
-            [item.get('Title') for item in value])
+        browser.login().open(self.inbox, view='tabbedview_view-overview')
+        self.assertSequenceEqual(
+            browser.css('#documentsBox li:not(.moreLink) a').text,
+            ['Document 10', 'Document 9', 'Document 8', 'Document 7',
+             'Document 6', 'Document 5', 'Document 4', 'Document 3',
+             'Document 2', 'Document 1'])
 
 
 class TestInboxOverviewAssignedInboxTasks(TestBaseInboxOverview):
 
-    def test_list_tasks_and_forwardings_assigned_to_current_inbox_group(self):
-        task = create(Builder('task')
-                      .with_modification_date(DateTime(2014, 1, 1))
-                      .having(responsible='inbox:client1'))
-        forwarding = create(Builder('forwarding')
-                            .with_modification_date(DateTime(2014, 1, 2))
-                            .having(responsible='inbox:client1'))
-        create(Builder('forwarding').having(responsible='inbox:client2'))
+    @browsing
+    def test_list_tasks_and_forwardings_assigned_to_current_inbox_group(self, browser):
+        create(Builder('task')
+               .within(self.inbox)
+               .with_modification_date(DateTime(2014, 1, 1))
+               .having(responsible='inbox:client1')
+               .titled(u'Task x'))
+        create(Builder('forwarding')
+               .within(self.inbox)
+               .with_modification_date(DateTime(2014, 1, 2))
+               .having(responsible='inbox:client1')
+               .titled(u'Forwarding x'))
+        create(Builder('forwarding').having(responsible='inbox:client2')
+                                    .titled(u'Forwarding Invisible'))
 
+        browser.login().open(self.inbox, view='tabbedview_view-overview')
         self.assertEquals(
-            [forwarding.get_sql_object(), task.get_sql_object()],
-            self.view.assigned_tasks())
+            ['Forwarding x', 'Task x'],
+            browser.css('#assigned_inbox_tasksBox li:not(.moreLink) span span').text)
 
-    def test_is_limited_to_five_entries(self):
-        for i in range(10):
+    @browsing
+    def test_task_box_items_are_limited_to_five_and_sorted_by_modified(self, browser):
+        for i in range(1, 6):
             create(Builder('forwarding')
-                   .having(responsible='inbox:client1'))
+                   .within(self.inbox)
+                   .having(responsible='inbox:client1')
+                   .with_modification_date(DateTime(2010, 1, 1) + i)
+                   .titled(u'Task %s' % i))
+        create(Builder('task')
+               .within(self.inbox)
+               .having(responsible='inbox:client1')
+               .with_modification_date(DateTime(2009, 12, 1))
+               .titled(u'Task 6'))
 
-        self.assertEquals(5, len(self.view.assigned_tasks()))
+        browser.login().open(self.inbox, view='tabbedview_view-overview')
+        self.assertSequenceEqual(
+            browser.css('#assigned_inbox_tasksBox li:not(.moreLink) span span').text,
+            ['Task 5', 'Task 4', 'Task 3', 'Task 2', 'Task 1'])
 
-    def test_lists_only_the_local_one_when_having_predecessor_successor_couples(self):
+    @browsing
+    def test_lists_only_the_local_one_when_having_predecessor_successor_couples(self, browser):
         predecessor = create(Builder('forwarding')
                              .having(responsible='inbox:client2',
-                                     assigned_client='client2'))
-        successor = create(Builder('forwarding')
-                           .having(responsible='inbox:client1',
-                                   assigned_client='client1')
-                           .successor_from(predecessor))
+                                     assigned_client='client2')
+                             .titled(u'Predecessor'))
+        create(Builder('forwarding')
+               .having(responsible='inbox:client1',
+                       assigned_client='client1')
+               .successor_from(predecessor)
+               .titled(u'Successor'))
 
-        self.assertEquals(
-            [successor.get_sql_object()], self.view.assigned_tasks())
+        browser.login().open(self.inbox, view='tabbedview_view-overview')
+        self.assertSequenceEqual(
+            browser.css('#assigned_inbox_tasksBox li:not(.moreLink) span span').text,
+            ['Successor'])
 
-    def test_list_only_active_tasks(self):
-        active = create(Builder('forwarding')
-                        .having(responsible='inbox:client1'))
+    @browsing
+    def test_list_only_active_tasks(self, browser):
+        create(Builder('forwarding')
+               .having(responsible='inbox:client1')
+               .titled('Active'))
         closed = create(Builder('forwarding')
                         .having(responsible='inbox:client1')
-                        .in_state('forwarding-state-closed'))
+                        .in_state('forwarding-state-closed')
+                        .titled('Closed'))
         sync_task(closed, None)
 
-        self.assertEquals(
-            [active.get_sql_object()],
-            self.view.assigned_tasks())
-
-    def test_is_sorted_on_modification_date_last_modified_first(self):
-        task1 = create(Builder('task')
-                       .with_modification_date(DateTime(2014, 1, 2))
-                       .having(responsible='inbox:client1'))
-
-        task2 = create(Builder('task')
-                       .with_modification_date(DateTime(2014, 1, 1))
-                       .having(responsible='inbox:client1'))
-
-        task3 = create(Builder('task')
-                      .with_modification_date(DateTime(2014, 1, 3))
-                      .having(responsible='inbox:client1'))
-
-        self.assertEquals(
-            [task.get_sql_object() for task in [task3, task1, task2]],
-            self.view.assigned_tasks())
+        browser.login().open(self.inbox, view='tabbedview_view-overview')
+        self.assertSequenceEqual(
+            browser.css('#assigned_inbox_tasksBox li:not(.moreLink) span span').text,
+            ['Active'])
 
 
 class TestInboxOverviewIssuedInboxTasks(TestBaseInboxOverview):
 
-    def test_list_tasks_and_forwardings_issued_by_current_inbox_group(self):
-        task = create(Builder('task')
-                      .with_modification_date(DateTime(2014, 1, 1))
-                      .within(self.inbox)
-                      .having(issuer='inbox:client1'))
-        forwarding = create(Builder('forwarding')
-                            .with_modification_date(DateTime(2014, 2, 1))
-                            .within(self.inbox)
-                            .having(issuer='inbox:client1'))
-        create(Builder('forwarding').having(issuer='inbox:client2'))
-
-        self.assertEquals(
-            [forwarding.get_sql_object(), task.get_sql_object()],
-            self.view.issued_tasks())
-
-    def test_is_limited_to_five_entries(self):
-        for i in range(10):
-            create(Builder('forwarding')
-                   .within(self.inbox)
-                   .having(issuer='inbox:client1'))
-
-        self.assertEquals(5, len(self.view.issued_tasks()))
-
-    def test_list_only_active_tasks_and_forwardings(self):
-        active = create(Builder('forwarding')
-                        .within(self.inbox)
-                        .having(issuer='inbox:client1'))
-
-        create(Builder('forwarding')
+    @browsing
+    def test_list_tasks_and_forwardings_issued_by_current_inbox_group(self, browser):
+        create(Builder('task')
+               .with_modification_date(DateTime(2014, 1, 1))
                .within(self.inbox)
                .having(issuer='inbox:client1')
-               .in_state('forwarding-state-closed'))
+               .titled('A Task'))
+        create(Builder('forwarding')
+               .with_modification_date(DateTime(2014, 2, 1))
+               .within(self.inbox)
+               .having(issuer='inbox:client1')
+               .titled('A Forwarding'))
+        create(Builder('forwarding').having(issuer='inbox:client2'))
 
-        self.assertEquals(
-            [active.get_sql_object()],
-            self.view.issued_tasks())
+        browser.login().open(self.inbox, view='tabbedview_view-overview')
+        self.assertSequenceEqual(
+            browser.css('#issued_inbox_tasksBox li:not(.moreLink) span span').text,
+            ['A Forwarding', 'A Task'])
 
-    def test_is_sorted_by_modfied(self):
-        task1 = create(Builder('task')
-                       .with_modification_date(DateTime(2014, 1, 2))
-                       .having(issuer='inbox:client1'))
+    @browsing
+    def test_task_box_items_are_limited_to_five_and_sorted_by_modified(self, browser):
+        for i in range(1, 6):
+            create(Builder('forwarding')
+                   .within(self.inbox)
+                   .having(issuer='inbox:client1')
+                   .with_modification_date(DateTime(2010, 1, 1) + i)
+                   .titled(u'Task %s' % i))
+        create(Builder('task')
+               .within(self.inbox)
+               .having(issuer='inbox:client1')
+               .with_modification_date(DateTime(2009, 12, 1))
+               .titled(u'Task 6'))
 
-        task2 = create(Builder('task')
-                       .with_modification_date(DateTime(2014, 1, 1))
-                       .having(issuer='inbox:client1'))
+        browser.login().open(self.inbox, view='tabbedview_view-overview')
+        self.assertSequenceEqual(
+            browser.css('#issued_inbox_tasksBox li:not(.moreLink) span span').text,
+            ['Task 5', 'Task 4', 'Task 3', 'Task 2', 'Task 1'])
 
-        task3 = create(Builder('task')
-                      .with_modification_date(DateTime(2014, 1, 3))
-                      .having(issuer='inbox:client1'))
+    @browsing
+    def test_list_only_active_tasks(self, browser):
+        create(Builder('forwarding')
+               .having(issuer='inbox:client1')
+               .titled('Active'))
+        closed = create(Builder('forwarding')
+                        .having(issuer='inbox:client1')
+                        .in_state('forwarding-state-closed')
+                        .titled('Closed'))
+        sync_task(closed, None)
 
-        self.assertEquals(
-            [task.get_sql_object() for task in [task3, task1, task2]],
-            self.view.issued_tasks())
+        browser.login().open(self.inbox, view='tabbedview_view-overview')
+        self.assertSequenceEqual(
+            browser.css('#issued_inbox_tasksBox li:not(.moreLink) span span').text,
+            ['Active'])
