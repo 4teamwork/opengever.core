b257c9aaad16f845c01746b4cf41c96ba63ece38
Philippe Gross <philippe.gross@4teamwork.ch>
2015-07-14 11:43:42 +0200

    Include english to the translatable languages.

--- opengever/base/model.py
+++ opengever/base/model.py
@@ -20,7 +20,7 @@ Session = named_scoped_session('opengever')
 BASE.session = Session
 Base = query_base(Session)
 
-make_translatable(options={'locales': ['de', 'fr']})
+make_translatable(options={'locales': ['de', 'fr', 'en']})
 sqlalchemy_utils.i18n.get_locale = get_locale
 
 
