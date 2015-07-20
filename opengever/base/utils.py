ed34ae23a180041fd836313502e1d21f774fafec
Lukas Graf <lukas.graf@4teamwork.ch>
2015-07-20 12:28:30 +0200

    Add `PathFinder` helper class to provide various Zope2 Instance related paths
    that are otherwise cumbersone to access.

--- opengever/base/utils.py
+++ opengever/base/utils.py
@@ -1,10 +1,13 @@
 from Acquisition import aq_inner
 from Acquisition import aq_parent
+from App.config import DefaultConfiguration
 from opengever.dossier.behaviors.dossier import IDossierMarker
 from plone import api
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.interfaces.siteroot import IPloneSiteRoot
 from zope.component import getMultiAdapter
+import App.config
+import os
 
 
 def language_cache_key(method, context, request):
@@ -72,3 +75,39 @@ def get_preferred_language_code():
 
     # Special handling for combined languages, but works for regular ones too.
     return language_code.split('-')[0]
+
+
+class PathFinder(object):
+    """Helper class to provide various Zope2 Instance related paths that are
+    otherwise cumbersone to access.
+    """
+
+    def __init__(self):
+        self._assert_proper_configuration()
+        self._instance_home = os.environ['INSTANCE_HOME']
+        self._client_home = os.environ['CLIENT_HOME']
+
+    def _assert_proper_configuration(self):
+        cfg = App.config._config
+        if cfg is None or isinstance(cfg, DefaultConfiguration):
+            raise RuntimeError(
+                "Zope is not configured properly yet, refusing "
+                "operate on paths that might be wrong!")
+
+    @property
+    def var(self):
+        """Path to {buildout}/var
+        """
+        return os.path.normpath(os.path.join(self._client_home, '..'))
+
+    @property
+    def var_log(self):
+        """Path to {buildout}/var/log
+        """
+        return os.path.join(self.var, 'log')
+
+    @property
+    def buildout(self):
+        """Path to {buildout}
+        """
+        return os.path.normpath(os.path.join(self.var, '..'))
