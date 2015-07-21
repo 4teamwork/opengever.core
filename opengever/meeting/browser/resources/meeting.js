48a5b586e47c2a03172e73036c5cab65e8614c16
Philippe Gross <philippe.gross@4teamwork.ch>
2015-07-21 08:38:12 +0200

    Autosizing textareas in the add and edit form of proposals.

--- opengever/meeting/browser/resources/meeting.js
+++ opengever/meeting/browser/resources/meeting.js
@@ -4,6 +4,10 @@
 
   $(function() {
 
+    // autosizing textareas when add or editing a proposal
+    $('body.template-opengever-meeting-proposal textarea').autosize();
+    $('body.template-edit.portaltype-opengever-meeting-proposal textarea').autosize();
+
     var viewlet = $("#opengever_meeting_meeting");
 
     var listMessages = function(messages) {
