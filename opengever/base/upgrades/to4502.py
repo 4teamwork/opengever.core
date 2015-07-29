6522cb503a058c0fc3dc7a74605a42ee208e089e
Philippe Gross <philippe.gross@4teamwork.ch>
2015-07-29 17:23:09 +0200

    Cleanup and simplify CleanupRelationsCatalog upgradestep.

--- opengever/base/upgrades/to4502.py
+++ opengever/base/upgrades/to4502.py
@@ -10,14 +10,14 @@ TO_REMOVE = ['plone.directives.form.schema.Schema',
 class CleanupRelationsCatalog(UpgradeStep):
 
     def __call__(self):
-        self.relations_catalog = getUtility(ICatalog)
+        self.catalog = getUtility(ICatalog)
 
         for iface_name in TO_REMOVE:
-            self.cleanup_interface(iface_name, 'to_interfaces_flattened')
-            self.cleanup_interface(iface_name, 'from_interfaces_flattened')
+            self.cleanup_to_mapping(iface_name, 'to_interfaces_flattened')
+            self.cleanup_to_mapping(iface_name, 'from_interfaces_flattened')
 
-    def cleanup_interface(self, iface_name, mapping_key):
-        for iface in self.relations_catalog._name_TO_mapping[mapping_key].keys():
+    def cleanup_to_mapping(self, iface_name, mapping_key):
+        for iface in self.catalog._name_TO_mapping[mapping_key].keys():
             if '{}.{}'.format(iface.__module__, iface.__name__) == iface_name:
-                del self.relations_catalog._name_TO_mapping[mapping_key][iface]
+                del self.catalog._name_TO_mapping[mapping_key][iface]
                 break
