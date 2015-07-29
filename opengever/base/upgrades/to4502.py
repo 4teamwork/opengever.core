f41f594adbbf5b57d3b5873d112757c04cddf4bf
Philippe Gross <philippe.gross@4teamwork.ch>
2015-07-29 17:32:15 +0200

    Extend CleanupRelationsCatalog upgradestep with cleanup the objtokenset.
    With 647eddba0a86868215f3a700324a8cb9a39d2371 an upgradestep was added which cleanup no longer interfaces from the zc.relation catalog (quite from the `_name_TO_mapping`).
    
    To fix existing relation values completly, the `_reltoken_name_TO_objtokenset` mapping, has to be cleaned up as well.
    
    For now also copying or moving migrated objects(from Plone 4.2 -> 4.3) with relationvalues should now work well.

--- opengever/base/upgrades/to4502.py
+++ opengever/base/upgrades/to4502.py
@@ -15,9 +15,21 @@ class CleanupRelationsCatalog(UpgradeStep):
         for iface_name in TO_REMOVE:
             self.cleanup_to_mapping(iface_name, 'to_interfaces_flattened')
             self.cleanup_to_mapping(iface_name, 'from_interfaces_flattened')
+            self.cleanup_objtokenset(
+                ['from_interfaces_flattened', 'to_interfaces_flattened'])
 
     def cleanup_to_mapping(self, iface_name, mapping_key):
         for iface in self.catalog._name_TO_mapping[mapping_key].keys():
             if '{}.{}'.format(iface.__module__, iface.__name__) == iface_name:
                 del self.catalog._name_TO_mapping[mapping_key][iface]
                 break
+
+    def cleanup_objtokenset(self, attribute_names):
+        for (key, value) in self.catalog._reltoken_name_TO_objtokenset.items():
+            token, name = key
+            if name in attribute_names:
+                broken_values = [iface for iface in value
+                                 if '{}.{}'.format(iface.__module__, iface.__name__) in TO_REMOVE]
+
+                for broken in broken_values:
+                    value.remove(broken)
