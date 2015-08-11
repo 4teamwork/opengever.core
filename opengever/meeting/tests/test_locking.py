58b9794eb16e296a82837d51e91a4dbb93ef2d98
Philippe Gross <philippe.gross@4teamwork.ch>
2015-08-11 23:16:20 +0200

    Added tests for the meeting locking integration.

---
+++ opengever/meeting/tests/test_locking.py
@@ -0,0 +1,114 @@
+from datetime import datetime
+from ftw.builder import Builder
+from ftw.builder import create
+from ftw.testbrowser import browsing
+from ftw.testbrowser.pages.statusmessages import info_messages
+from opengever.core.testing import OPENGEVER_FUNCTIONAL_MEETING_LAYER
+from opengever.meeting.meeting_wrapper import MeetingWrapper
+from opengever.testing import FunctionalTestCase
+from plone.app.testing import TEST_USER_ID
+from plone.locking.interfaces import ILockable
+from zExceptions import Redirect
+
+
+class TestMeetingLocking(FunctionalTestCase):
+
+    layer = OPENGEVER_FUNCTIONAL_MEETING_LAYER
+
+    def setUp(self):
+        super(TestMeetingLocking, self).setUp()
+        self.admin_unit.public_url = 'http://nohost/plone'
+
+        self.repository_root, self.repository_folder = create(
+            Builder('repository_tree'))
+        self.dossier = create(
+            Builder('dossier').within(self.repository_folder))
+
+        self.templates = create(Builder('templatedossier'))
+        self.sablon_template = create(
+            Builder('sablontemplate')
+            .within(self.templates)
+            .with_asset_file('sablon_template.docx'))
+        container = create(Builder('committee_container').having(
+            pre_protocol_template=self.sablon_template,
+            protocol_template=self.sablon_template,
+            excerpt_template=self.sablon_template))
+
+        self.committee = create(Builder('committee').within(container))
+        self.proposal = create(Builder('proposal')
+                               .within(self.dossier)
+                               .having(title='Mach doch',
+                                       committee=self.committee.load_model())
+                               .as_submitted())
+
+        self.committee_model = self.committee.load_model()
+        self.meeting = create(Builder('meeting')
+                              .having(committee=self.committee_model,
+                                      start=datetime(2013, 1, 1),
+                                      location='There',))
+        self.meeting.execute_transition('pending-held')
+        self.proposal_model = self.proposal.load_model()
+
+        self.agenda_item = create(
+            Builder('agenda_item')
+            .having(meeting=self.meeting,
+                    proposal=self.proposal_model))
+
+    @browsing
+    def test_editing_a_protocol_locks_meeting(self, browser):
+        browser.login().open(self.meeting.get_url('protocol'))
+
+        browser.open(self.meeting.get_url('plone_lock_info/lock_info'))
+        lock_infos = ILockable(MeetingWrapper(self.meeting)).lock_info()
+
+        self.assertEquals(1, len(lock_infos))
+        lock = lock_infos[0]
+        self.assertEquals('Meeting:1', lock.get('token'))
+        self.assertEquals(TEST_USER_ID, lock.get('creator'))
+
+    @browsing
+    def test_saving_protocol_unlock_meeting(self, browser):
+        browser.login().open(self.meeting.get_url('protocol'))
+        lock_infos = ILockable(MeetingWrapper(self.meeting)).lock_info()
+        self.assertEquals(1, len(lock_infos))
+
+        browser.find('Save').click()
+        self.assertEquals(
+            [], ILockable(MeetingWrapper(self.meeting)).lock_info())
+
+    @browsing
+    def test_cancelling_protocol_unlock_meeting(self, browser):
+        browser.login().open(self.meeting.get_url('protocol'))
+        lock_infos = ILockable(MeetingWrapper(self.meeting)).lock_info()
+        self.assertEquals(1, len(lock_infos))
+
+        browser.find('Cancel').click()
+        self.assertEquals(
+            [], ILockable(MeetingWrapper(self.meeting)).lock_info())
+
+    @browsing
+    def test_protocol_raise_redirect_back_to_meeting_view_when_protocol_is_locked(self, browser):
+        user = create(Builder('user').named('Hugo', 'Boss'))
+        browser.login(username='hugo.boss').open(self.meeting.get_url('protocol'))
+
+        with self.assertRaises(Redirect) as cm:
+            browser.login().open(self.meeting.get_url('protocol'))
+
+        self.assertEquals(
+            'http://nohost/plone/opengever-meeting-committeecontainer/committee-1/meeting-1',
+            str(cm.exception))
+
+    @browsing
+    def test_meeting_view_shows_information_when_is_locked(self, browser):
+        user = create(Builder('user').named('Hugo', 'Boss'))
+        browser.login(username='hugo.boss').open(self.meeting.get_url('protocol'))
+
+        browser.login().open(self.meeting.get_url())
+
+        # the unlock link is rendered as a submit button, therefore `unlock`
+        # is missing in the following statement
+        self.assertEquals(
+            ['This item was locked by Boss Hugo 1 minute ago. If you are '
+             'certain this user has abandoned the object, you may the object. '
+             'You will then be able to edit it.'],
+            info_messages())
