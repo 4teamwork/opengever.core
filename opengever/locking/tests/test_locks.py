d18d7bbfb7cd0317736d828a9a63bd1f90c43000
Philippe Gross <philippe.gross@4teamwork.ch>
2015-08-11 22:29:46 +0200

    Addd tests for the SQLObject Locking support from opengever.locking.

---
+++ opengever/locking/tests/test_locks.py
@@ -0,0 +1,49 @@
+from datetime import timedelta
+from opengever.core.testing import MEMORY_DB_LAYER
+from opengever.locking.model import Lock
+from opengever.locking.model.locks import utcnow_tz_aware
+from plone.app.testing import TEST_USER_ID
+from unittest2 import TestCase
+import pytz
+
+
+class TestUnitLocks(TestCase):
+
+    layer = MEMORY_DB_LAYER
+
+    def setUp(self):
+        super(TestUnitLocks, self).setUp()
+        self.session = self.layer.session
+
+    def test_valid_locks_query(self):
+        valid = Lock(object_type='Meeting',
+                     object_id=1,
+                     creator=TEST_USER_ID,
+                     time=utcnow_tz_aware() - timedelta(seconds=300))
+        self.session.add(valid)
+
+        invalid = Lock(object_type='Meeting',
+                       object_id=2,
+                       creator=TEST_USER_ID,
+                       time=utcnow_tz_aware() - timedelta(seconds=800))
+        self.session.add(invalid)
+
+        query = Lock.query.valid_locks('Meeting', 1)
+        self.assertEquals([valid], query.all())
+
+    def test_is_valid(self):
+        lock1 = Lock(object_type='Meeting',
+                     object_id=1,
+                     creator=TEST_USER_ID,
+                     time=utcnow_tz_aware() - timedelta(seconds=300))
+        self.session.add(lock1)
+
+        lock2 = Lock(object_type='Meeting',
+                     object_id=2,
+                     creator=TEST_USER_ID,
+                     time=utcnow_tz_aware() - timedelta(seconds=800))
+        self.session.add(lock2)
+
+
+        self.assertTrue(lock1.is_valid())
+        self.assertFalse(lock2.is_valid())
