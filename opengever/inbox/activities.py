aca3df24c8b1ee756adf414b24aa56dc99998723
Philippe Gross <philippe.gross@4teamwork.ch>
2015-08-17 17:17:06 +0200

    Fixed label and summary for the forwarding added activity.

--- opengever/inbox/activities.py
+++ opengever/inbox/activities.py
@@ -1,5 +1,6 @@
 from opengever.ogds.base.actor import Actor
-from opengever.task import _
+from opengever.inbox import _
+from opengever.task import _ as tmf
 from opengever.task.activities import TaskAddedActivity
 from plone import api
 from Products.CMFPlone import PloneMessageFactory
@@ -16,16 +17,21 @@ class ForwardingAddedActivity(TaskAddedActivity):
     @property
     def summary(self):
         actor = Actor.lookup(self.context.Creator())
-        msg = _('label_forwarding_added', u'New forwarding added by ${user}',
+        msg = _('msg_forwarding_added', u'New forwarding added by ${user}',
                 mapping={'user': actor.get_label(with_principal=False)})
         return self.translate_to_all_languages(msg)
+
+    @property
+    def label(self):
+        return self.translate_to_all_languages(
+            _('label_forwarding_added', u'Forwarding added'))
 
     def collect_description_data(self, language):
         """Returns a list with [label, value] pairs.
         """
         return [
-            [_('label_task_title', u'Task title'), self.context.title],
-            [_('label_deadline', u'Deadline'),
+            [tmf('label_task_title', u'Task title'), self.context.title],
+            [tmf('label_deadline', u'Deadline'),
              api.portal.get_localized_time(str(self.context.deadline))],
-            [_('label_text', u'Text'), self.context.text]
+            [tmf('label_text', u'Text'), self.context.text]
         ]
