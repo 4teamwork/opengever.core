4a51d59e6865aaa60e89833612eaae62afefe5d4
David Erni <david.erni@4teamwork.ch>
2015-08-31 15:05:02 +0200

    Drop mock-tests they are covered by testbrowser tests.

--- opengever/document/tests/test_download.py
+++ opengever/document/tests/test_download.py
@@ -1,93 +1,14 @@
 from ftw.builder import Builder
 from ftw.builder import create
-from ftw.builder import session
 from ftw.testbrowser import browsing
 from ftw.testbrowser.pages.statusmessages import error_messages
 from ftw.testbrowser.pages.statusmessages import warning_messages
-from ftw.testing import MockTestCase
-from mocker import ANY
-from opengever.core.testing import OPENGEVER_FUNCTIONAL_TESTING
-from opengever.document.interfaces import IFileCopyDownloadedEvent
 from opengever.testing import FunctionalTestCase
 from plone.app.testing import login
-from plone.app.testing import setRoles
-from plone.app.testing import TEST_USER_ID
 from plone.app.testing import TEST_USER_NAME
 from plone.namedfile.file import NamedBlobFile
 from Products.CMFCore.utils import getToolByName
 import transaction
-
-
-class TestDocumentDownloadView(MockTestCase):
-
-    layer = OPENGEVER_FUNCTIONAL_TESTING
-
-    def setUp(self):
-        super(TestDocumentDownloadView, self).setUp()
-        self.portal = self.layer['portal']
-
-        create(Builder('fixture').with_admin_unit())
-
-        setRoles(self.portal, TEST_USER_ID, ['Manager'])
-        session.current_session = session.factory()
-        session.current_session.portal = self.portal
-
-    def tearDown(self):
-        super(TestDocumentDownloadView, self).tearDown()
-        session.current_session = None
-
-    def test_download_view(self):
-        doc1 = create(Builder("document").attach_file_containing("bla bla"))
-        doc2 = create(Builder("document")
-                      .attach_file_containing("blub blub", name=u't\xf6st.txt'))
-
-        downloaded_handler = self.mocker.mock()
-        self.mock_handler(downloaded_handler, [IFileCopyDownloadedEvent, ])
-        self.expect(downloaded_handler(ANY)).count(2).result(True)
-
-        self.replay()
-
-        result = doc1.unrestrictedTraverse('download')()
-        result.seek(0)
-        self.assertEquals(result.read(), 'bla bla')
-
-        result = doc2.unrestrictedTraverse('download')()
-        result.seek(0)
-        self.assertEquals(result.read(), 'blub blub')
-
-    def test_download_file_version_view(self):
-        doc1 = create(Builder("document"))
-
-        repo_tool = getToolByName(self.portal, 'portal_repository')
-        repo_tool._recursiveSave(doc1, {},
-                                 repo_tool._prepareSysMetadata('mock'),
-                                 autoapply=repo_tool.autoapply)
-
-        monk_file = NamedBlobFile('bla bla', filename=u'test.txt')
-        doc1.file = monk_file
-
-        # create version
-        repo_tool = getToolByName(self.portal, 'portal_repository')
-        repo_tool._recursiveSave(doc1, {},
-                                 repo_tool._prepareSysMetadata('mock'),
-                                 autoapply=repo_tool.autoapply)
-
-        downloaded_handler = self.mocker.mock()
-        self.mock_handler(downloaded_handler, [IFileCopyDownloadedEvent, ])
-        self.expect(downloaded_handler(ANY)).result(True)
-
-        self.replay()
-
-        # second version with a document
-        doc1.REQUEST['version_id'] = '2'
-        result = doc1.unrestrictedTraverse('download_file_version')()
-        self.assertEquals(result, 'bla bla')
-
-        # first version with a document
-        self.portal.REQUEST['version_id'] = '1'
-        result = doc1.unrestrictedTraverse('download_file_version')()
-        # result should be a redirect back to the document
-        self.assertEquals(result, 'http://nohost/plone/document-1')
 
 
 class TestDocumentDownloadConfirmation(FunctionalTestCase):
