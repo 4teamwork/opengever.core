5ec399d2a23032c2aa2c840c87cea16a94cd29a4
Philippe Gross <philippe.gross@4teamwork.ch>
2015-10-27 16:46:12 +0100

    Revert review_state check when accepting or assigning a tasks/forwarding.
    Because this checks, introduced by #1006, breaks the ConflictError handling for multi adminunit requests and functionalities. See #1251 for more information.
    
    Revert "Merge pull request #1006 from 4teamwork/deif-prevent-accepting-twice"
    
    This reverts commit 558bed1c842da9b0bb8c66c1cd07e0c049877fa2, reversing
    changes made to c86262c53b36cdc9b3f6ee3cb31671de65314188.
    Revert "Merge pull request #1006 from 4teamwork/deif-prevent-accepting-twice"
    
    This reverts commit 558bed1c842da9b0bb8c66c1cd07e0c049877fa2, reversing
    changes made to c86262c53b36cdc9b3f6ee3cb31671de65314188.

--- opengever/task/exceptions.py
+++ opengever/task/exceptions.py
@@ -2,13 +2,5 @@ class TaskException(Exception):
     """Base exception class for custom task exceptions."""
 
 
-class CannotAcceptTaskException(TaskException):
-    pass
-
-
-class CannotAssignForwardingException(TaskException):
-    pass
-
-
 class TaskRemoteRequestError(TaskException):
     pass
