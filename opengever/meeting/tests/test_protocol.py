37fa8a7c5036d50b78472531536167e4c42a322b
David Erni <david.erni@4teamwork.ch>
2015-10-27 14:46:46 +0100

    Add start_page_number to JSON, if it is specified.

--- opengever/meeting/tests/test_protocol.py
+++ opengever/meeting/tests/test_protocol.py
@@ -54,7 +54,8 @@ class TestProtocol(FunctionalTestCase):
         self.meeting = create(Builder('meeting')
                               .having(committee=self.committee_model,
                                       start=datetime(2013, 1, 1),
-                                      location='There',)
+                                      location='There',
+                                      protocol_start_page=42)
                               .link_with(self.meeting_dossier))
         self.meeting.execute_transition('pending-held')
         self.proposal_model = self.proposal.load_model()
