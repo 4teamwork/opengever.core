962d96a3885c253019dc90318786d4ca484d4c69
Thomas Buchberger <t.buchberger@4teamwork.ch>
2015-10-28 18:16:13 +0100

    Add subscriber that disallows access to views on the portal root for anonymous.

---
+++ opengever/base/tests/test_anonymous.py
@@ -0,0 +1,23 @@
+from AccessControl import Unauthorized
+from ftw.testbrowser import browsing
+from opengever.testing import FunctionalTestCase
+
+
+class TestAnonymousAccess(FunctionalTestCase):
+
+    @browsing
+    def test_anonymous_cannot_access_search(self, browser):
+        with self.assertRaises(Unauthorized):
+            browser.visit(self.portal, view='search')
+
+    @browsing
+    def test_anonymous_can_access_login_form(self, browser):
+        browser.visit(self.portal, view='login_form')
+        self.assertEquals(self.portal.absolute_url() + '/login_form',
+                          browser.url)
+
+    @browsing
+    def test_authenticated_can_access_search(self, browser):
+        browser.login().visit(self.portal, view='search')
+        self.assertEquals(self.portal.absolute_url() + '/@@search',
+                          browser.url)
