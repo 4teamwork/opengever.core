ead8e938c31967ade72d4b36ff66f8256392ae21
Philippe Gross <philippe.gross@4teamwork.ch>
2015-11-11 15:46:22 +0100

    ResponseTransporter: fix de- and encoding of deadline changes.
    Fixes #1310.

--- opengever/task/tests/test_responsetransporter.py
+++ opengever/task/tests/test_responsetransporter.py
@@ -1,6 +1,8 @@
+from datetime import date
 from ftw.builder import Builder
 from ftw.builder import create
 from opengever.task.adapters import IResponseContainer
+from opengever.task.interfaces import IDeadlineModifier
 from opengever.task.task import ITask
 from opengever.task.transporter import IResponseTransporter
 from opengever.task.util import add_simple_response
@@ -21,7 +23,8 @@ class TestResponeTransporter(FunctionalTestCase):
                        lastname=u'B\xf6ss'))
 
         self.task = create(Builder('task')
-                           .having(responsible=u'hugo.boss')
+                           .having(responsible=u'hugo.boss',
+                                   deadline=date(2016, 03, 27))
                            .in_state('task-state-in-progress'))
         add_simple_response(self.task,
                             text=u'Ich \xfcbernehme diese Aufgabe',
@@ -69,3 +72,20 @@ class TestResponeTransporter(FunctionalTestCase):
                             u'name': u'label_resonsible_client',
                             u'before': u'client1'}],
                           IResponseContainer(copy)[1].changes)
+
+    def test_deadline_change_synchronisation(self):
+        IDeadlineModifier(self.task).modify_deadline(
+            date(2016, 03, 29),
+            u'Frist wurde verschoben.',
+            u'task-transition-modify-deadline')
+
+        copy = create(Builder('task').in_state('task-state-in-progress'))
+        IResponseTransporter(self.task).send_responses(
+            'client1', copy.get_physical_path())
+
+        self.assertEquals(
+            [{u'after': date(2016, 03, 29),
+              u'id': u'deadline',
+              u'name': u'label_deadline',
+              u'before': date(2016, 03, 27)}],
+            IResponseContainer(copy)[1].changes)
