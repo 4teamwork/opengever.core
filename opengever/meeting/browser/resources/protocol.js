0e83c925be4eb7f2fc351ee3b02204f3d0339176
Kevin Bieri <k.bieri@4teamwork.ch>
2015-11-11 15:30:20 +0100

    Implement autoscrolling protocol navigation.
    When protocol navigation grows the agendaitems overflows the browser viewport.

--- opengever/meeting/browser/resources/protocol.js
+++ opengever/meeting/browser/resources/protocol.js
@@ -1,66 +1,68 @@
-$(document).ready(function() {
-  var scrollSpy = $("#scrollspy");
+(function(global) {
 
-  $("#opengever_meeting_protocol textarea").autosize();
+  "use strict";
 
-  var scrollspy = new Scrollspy({ selectParent: true });
+  $(function() {
 
-  var navigation = $(".protocol-navigation");
+    $("#opengever_meeting_protocol textarea").autosize();
 
-  var headings = new StickyHeading({ selector: "#opengever_meeting_protocol .protocol_title" });
-  var labels = new StickyHeading({ selector: "#opengever_meeting_protocol .agenda_items label", fix: false, dependsOn: headings});
-  var collapsible = StickyHeading({ selector: "#opengever_meeting_protocol .collapsible", clone: false});
+    var scrollspy = new global.Scrollspy({ selector: ".navigation" });
 
-  var currentOffset;
+    var navigation = $(".protocol-navigation");
 
-  collapsible.onSticky(function() {
-    navigation.addClass("sticky");
-  });
+    var headings = new global.StickyHeading({ selector: "#opengever_meeting_protocol .protocol_title" });
+    var labels = new global.StickyHeading({ selector: "#opengever_meeting_protocol .agenda_items label", fix: false, dependsOn: headings});
+    var collapsible = new global.StickyHeading({ selector: "#opengever_meeting_protocol .collapsible", clone: false});
 
-  headings.onNoSticky(function() {
-    navigation.removeClass("sticky");
-    $(".metadata .fields").css("position", "static");
-  });
-
-  headings.onSticky(function(heading) {
-    navigation.addClass("sticky");
-    currentOffset = heading.clone.outerHeight();
-    $(".metadata .fields").css("position", "fixed");
-    scrollspy.expand($("#" + heading.node.attr("id") + "-anchor"));
-    scrollspy.select($("#" + heading.node.attr("id") + "-anchor"));
-  });
-
-  headings.onCollision(function() {
-    navigation.addClass("sticky");
-    $(".metadata .fields").css("position", "fixed");
-  });
-
-  labels.onSticky(function(label) {
-    scrollspy.select($("#" + label.node.attr("for") + "-anchor"));
-  });
-  labels.onCollision(function(fadingIn, fadingOut) {
-    //Manual offset from sticky hading to label
-    scrollspy.offset(currentOffset + 20);
-    scrollspy.select($("#" + fadingOut.node.attr("for") + "-anchor"));
-  });
-
-  scrollspy.onScroll(function(target, toElement) {
-    if(target.hasClass("expandable")) {
-      scrollspy.expand(target);
+    function moveCaretToEnd(el) {
+      if (typeof el.selectionStart === "number") {
+          el.selectionStart = el.selectionEnd = el.value.length;
+      } else if (el.createTextRange) {
+          var range = el.createTextRange();
+          range.collapse(false);
+      }
+      el.focus();
     }
-    moveCaretToEnd(toElement[0])
-    //Trigger Scrolltop event with 3 pixel more for triggering the first item to be selected
-    $(window).scrollTop($(window).scrollTop() + 3)
+
+    scrollspy.onBeforeScroll(function(target) {
+      if(target.hasClass("expandable")) {
+        scrollspy.options.offset = target.outerHeight() + 60;
+      }
+    });
+
+    collapsible.onSticky(function() { navigation.addClass("sticky"); });
+
+    headings.onNoSticky(function() {
+      navigation.removeClass("sticky");
+      $(".metadata .fields").css("position", "static");
+    });
+
+    headings.onSticky(function(heading) {
+      navigation.addClass("sticky");
+      $(".metadata .fields").css("position", "fixed");
+      scrollspy.expand($("#" + heading.node.attr("id") + "-anchor"));
+      scrollspy.select($("#" + heading.node.attr("id") + "-anchor"));
+    });
+
+    headings.onCollision(function() {
+      navigation.addClass("sticky");
+      $(".metadata .fields").css("position", "fixed");
+    });
+
+    labels.onSticky(function(label) {
+      scrollspy.select($("#" + label.node.attr("for") + "-anchor"));
+    });
+    labels.onCollision(function(fadingIn, fadingOut) {
+      scrollspy.select($("#" + fadingOut.node.attr("for") + "-anchor"));
+    });
+
+    scrollspy.onScroll(function(target, toElement) {
+      if(target.hasClass("expandable")) {
+        scrollspy.expand(target);
+      }
+      moveCaretToEnd(toElement[0]);
+    });
+
   });
 
-  function moveCaretToEnd(el) {
-    if (typeof el.selectionStart == "number") {
-        el.selectionStart = el.selectionEnd = el.value.length;
-    } else if (el.createTextRange) {
-        var range = el.createTextRange();
-        range.collapse(false);
-    }
-    el.focus();
-  }
-
-});
+}(window));
