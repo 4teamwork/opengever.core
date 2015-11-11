be73b28943d0a68b4a9db4a1f183b609439acfdc
David Erni <david.erni@4teamwork.ch>
2015-11-11 11:18:07 +0100

    Link meeting from meeting-dossier overview.

---
+++ opengever/meeting/browser/dossier/overview.py
@@ -0,0 +1,35 @@
+from five import grok
+from opengever.dossier.browser.overview import DossierOverview
+from opengever.meeting import _
+from opengever.meeting.interfaces import IMeetingDossier
+
+
+class MeetingDossierOverview(DossierOverview):
+
+    grok.context(IMeetingDossier)
+
+    def boxes(self):
+        """Insert meeting box first in overview's **middle** column."""
+
+        boxes = super(MeetingDossierOverview, self).boxes()
+
+        column = boxes[0]
+        column.insert(0, self.build_linked_meeting_box())
+        return boxes
+
+    def build_linked_meeting_box(self):
+        box = {'id': 'linked_meeting',
+               'content': self.linked_meeting(),
+               'label': _('label_linked_meeting', default='Linked meeting')}
+        return box
+
+    def linked_meeting(self):
+        content = []
+        meeting = self.context.get_meeting()
+        if meeting:
+            content.append({
+                'Title': meeting.get_title(),
+                'getURL': meeting.get_url(),
+                'css_class': meeting.css_class,
+            })
+        return content
--- opengever/meeting/tests/test_meeting_dossier_overview.py
+++ opengever/meeting/tests/test_meeting_dossier_overview.py
@@ -1,5 +1,6 @@
 from ftw.builder import Builder
 from ftw.builder import create
+from ftw.testbrowser import browsing
 from opengever.dossier.tests.test_overview import TestOverview
 
 
@@ -13,4 +14,17 @@ class TestMeetingDossierOverview(TestOverview):
         self.dossier = create(Builder('meeting_dossier')
                               .titled(u'Testdossier')
                               .having(description=u'Hie hesch e beschribig',
-                                      responsible='hugo.boss'))
+                                      responsible='hugo.boss')
+                              .within(self.repo))
+        self.container = create(Builder('committee_container'))
+        self.committee = create(Builder('committee').within(self.container))
+        self.meeting = create(Builder('meeting')
+                              .having(committee=self.committee.load_model())
+                              .link_with(self.dossier))
+
+    @browsing
+    def test_meeting_is_linked_on_overview(self, browser):
+        browser.login().open(self.dossier, view='tabbedview_view-overview')
+
+        link_node = browser.find(u'B\xe4rn, Dec 13, 2011')
+        self.assertEqual(self.meeting.get_url(), link_node.get('href'))
