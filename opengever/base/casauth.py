8f7404218107ac3850a0e403155c469a4a34a105
Lukas Graf <lukas.graf@4teamwork.ch>
2015-12-15 15:02:03 +0100

    Introduce the concept of a base URL for the GEVER cluster.

--- opengever/base/casauth.py
+++ opengever/base/casauth.py
@@ -2,9 +2,7 @@ from ftw.casauth.plugin import CASAuthenticationPlugin
 from opengever.ogds.base.utils import get_current_admin_unit
 from plone import api
 from Products.PluggableAuthService.interfaces.plugins import IChallengePlugin
-
-
-CAS_SERVER_URL_FORMAT = '{admin_unit_public_url}/portal'
+from urlparse import urljoin
 
 
 def is_cas_auth_enabled():
@@ -13,16 +11,18 @@ def is_cas_auth_enabled():
     return isinstance(challenge_plugin[1], CASAuthenticationPlugin)
 
 
-def build_cas_server_url():
-    """This function *guesses* the URL for the CAS server, in order to
-    initially configure the ftw.casauth plugin during setup.
+def get_cluster_base_url():
+    """This function *guesses* the base URL for this GEVER cluster
+    (always ending with a trailing slash).
 
     This is known to be incorrect for deployments with multiple admin units,
     and will be fixed later, once we store the configuration for the
     GEVER cluster in the OGDS.
     """
-    return CAS_SERVER_URL_FORMAT.format(
-        admin_unit_public_url=get_current_admin_unit().public_url.rstrip('/'))
+    base_url = get_current_admin_unit().public_url
+    if not base_url.endswith('/'):
+        base_url = base_url + '/'
+    return base_url
 
 
 def get_cas_server_url():
@@ -32,3 +32,13 @@ def get_cas_server_url():
     cas_auth = acl_users.cas_auth
     url = getattr(cas_auth, 'cas_server_url', None)
     return url
+
+
+def build_cas_server_url():
+    """Build the URL to the CAS server, based on the GEVER cluster URL.
+
+    This URL should only be constructed once during setup, in order to
+    initially configure the ftw.casauth plugin.
+    """
+    base_url = get_cluster_base_url()
+    return urljoin(base_url, 'portal')
--- opengever/base/tests/test_casauth.py
+++ opengever/base/tests/test_casauth.py
@@ -1,7 +1,32 @@
 from opengever.base.casauth import get_cas_server_url
+from opengever.base.casauth import get_cluster_base_url
 from opengever.ogds.base.utils import get_current_admin_unit
 from opengever.testing import FunctionalTestCase
 from plone.app.testing import applyProfile
+
+
+class TestClusterBaseURL(FunctionalTestCase):
+
+    def test_is_equal_to_public_url_for_single_admin_unit_setup(self):
+        admin_unit = get_current_admin_unit()
+        # For setups with a single admin units, the admin unit's ID is
+        # usually omitted from the public_url
+        admin_unit.public_url = 'http://lab.onegovgever.ch'
+
+        # In this case, the cluster base URL is equivalent to public_url
+        self.assertEquals(
+            'http://lab.onegovgever.ch/', get_cluster_base_url())
+
+    def test_is_equal_to_public_url_for_multi_admin_unit_setup(self):
+        admin_unit = get_current_admin_unit()
+        # For setups with multiple admin units, an admin unit's public_url
+        # ends with the admin unit ID
+        admin_unit.public_url = 'http://dev.onegovgever.ch/{}'.format(
+            admin_unit.unit_id)
+
+        # Cluster base URL is equal to admin unit's public_url *for now*
+        self.assertEquals(
+            'http://dev.onegovgever.ch/client1/', get_cluster_base_url())
 
 
 class TestCASServerURL(FunctionalTestCase):
