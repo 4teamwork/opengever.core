467c9a83954b696b30fadd58fd2d00519f7f9a18
Jonas Baumann <jone@jone.ch>
2016-01-05 12:18:27 +0100

    Reduce conflict errors by using uuid4 in CMFEditions.
    Patch for Products.CMFEditions.historyidhandlertool.HistoryIdHandlerTool.register
    
    The default "register" method uses the Products.CMFUid IUniqueIdGenerator
    utility for generating the history ID. This utility uses the auto-increment
    strategy, which generates a lot of conflicts.
    
    In order to reduce the conflicts when generating the history id,
    we switch to the uuid4 implementation, generating a random number instead
    and thus not writing to the same place.

--- opengever/base/monkeypatch.py
+++ opengever/base/monkeypatch.py
@@ -414,3 +414,34 @@ utils.URL_MATCH = re.compile(r'''(url\s*\(\s*['"]?)(?!data:)([^'")]+)(['"]?\s*\)
 
 
 LOGGER.info('Monkey patched Products.ResourceRegistries.utils.URL_MATCH regexp')
+
+
+# --------
+# Patch for Products.CMFEditions.historyidhandlertool
+#           .HistoryIdHandlerTool.register
+#
+# The default "register" method uses the Products.CMFUid IUniqueIdGenerator
+# utility for generating the history ID. This utility uses the auto-increment
+# strategy, which generates a lot of conflicts.
+#
+# In order to reduce the conflicts when generating the history id,
+# we switch to the uuid4 implementation, generating a random number instead
+# and thus not writing to the same place.
+
+from Products.CMFEditions.historyidhandlertool import HistoryIdHandlerTool
+from uuid import uuid4
+
+
+def HistoryIdHandlerTool_register(self, obj):
+    uid = self.queryUid(obj, default=None)
+    if uid is None:
+        # generate a new unique id and set it
+        uid = uuid4().int
+        self._setUid(obj, uid)
+
+    return uid
+
+
+HistoryIdHandlerTool.register = HistoryIdHandlerTool_register
+LOGGER.info('Monkey patched Products.CMFEditions.historyidhandlertool'
+            '.HistoryIdHandlerTool.register')
