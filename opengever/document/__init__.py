f959da0f0e0ee19593d697bca4fc8bf2c1a46eb0
Lukas Graf <lukas.graf@4teamwork.ch>
2016-01-05 20:24:42 +0100

    ZEM download: Include filename of doc in metadata by
    registering a callback for Products.ExternalEditor that
    adds the filename to the metadata during ZEM generation.

--- opengever/document/__init__.py
+++ opengever/document/__init__.py
@@ -1,4 +1,5 @@
 from opengever.document.extra_mimetypes import register_additional_mimetypes
+from opengever.document.officeconnector import register_ee_filename_callback
 from zope.i18nmessageid import MessageFactory
 
 
@@ -8,3 +9,7 @@ _ = MessageFactory('opengever.document')
 # Register some additional well-known MIME types to help server-side file
 # format recognition do a better job.
 register_additional_mimetypes()
+
+# Register a callback function for ZEM generation in Products.ExternalEditor
+# to include a document's filename in the metadata.
+register_ee_filename_callback()
