1b96a91536d44217ef1ea0e5e4702769eb7c2a01
Kevin Bieri <k.bieri@4teamwork.ch>
2016-02-19 15:22:57 +0100

    Add documentation of controller.

--- opengever/base/browser/resources/Controller.js
+++ opengever/base/browser/resources/Controller.js
@@ -1,3 +1,224 @@
+/*
+
+  This is the base class for all controllers in the opengever.meeting
+
+
+  Initializing
+  ============
+
+  You can simply create your own controller by inheriting from the base Controller:
+
+  function MyController() {
+    global.Controller.call(this); // Inherit the base controller
+    this.init(); // Call the init function of the base Controller
+  }
+
+  You have to call the `init` function first before you can use the controller.
+  The init function will compile the Handlebars template, fetch the initial data
+  and register the events.
+
+  The controller accepts the following options:
+
+  - template:            Specifiy the handlebars template as an HTML string
+  - outlet:              Specify the target where the controller has to render the template
+                         by passing a DOM element.
+                         The whole container will be replaced by the rendered HTML.
+  - options.showMessage: Set this option to false to turn off all messages.
+  - options.context:     This option sets the context where all events are going to be attached.
+
+  function MyController(options) {
+    global.Controller.call(this, $("#template").html(), $("#outlet"), options);
+    this.init();
+  }
+
+  To initialize the controller you have to instantiate the controller
+
+  var myController = new MyController();
+
+
+  Fetching
+  ========
+
+  You have to tell your controller how to fetch data by overriding
+  the `fetch` function.
+  Because the `fetch` function in the base class does nothing at this point.
+  To make the `fetch` function work simply return a jQuery Deferred object (https://api.jquery.com/category/deferred-object/).
+
+  Here is an example for fetching using jQuery's `get` function:
+
+  function MyController(options) {
+    global.Controller.call(this, $("#template").html(), $("#outlet"), options);
+
+    this.fetch = function() { return $.get("http://someurl.com") };
+
+    this.init();
+  }
+
+
+  Rendering
+  =========
+
+  You have to tell your controller how to render the received data
+  by overriding the `render` function.
+  Because the `render` function in the base class does nothing at this point.
+  To make the `render` function work you have to simply return some HTML.
+  The rendered HTML will automatically replace the outlet section.
+  You will get the data in the render function as a parameter.
+  The template is bound to the current context as a handlebars function.
+
+  function MyController(options) {
+    global.Controller.call(this, $("#template").html(), $("#outlet"), options);
+
+    this.fetch = function() { return $.get("http://someurl.com") };
+
+    this.render = function(data) { return this.template({ data: data }); }
+
+    this.init();
+  }
+
+
+  Event Registration
+  ==================
+
+  The controller is able to carry a list of events.
+  To pass the action throug the controller chain simply return a jQuery Deferred object.
+  Returning a plain object will always succeed.
+
+  Each event has the following parts.
+
+  - method:          This is the event type of your event e.q. `click`, `mouseover` or some custom events
+                     triggered by yourself.
+  - target:          Pass a CSS selector where the controller should catch the event.
+  - callback:        Define a callback function which will be called when the event gets fired.
+                     You will get the target DOM element and the event as callback parameters.
+  - options.update:  Set this to true to automatically update the handlebars template
+                     after a successful call of your action.
+  - options.prevent: Set this to false so the controller would not prevent the default
+                     Browser behaviour.
+
+  function MyController(options) {
+    global.Controller.call(this, $("#template").html(), $("#outlet"), options);
+
+    this.fetch = function() { return $.get("http://someurl.com") };
+
+    this.render = function(data) { return this.template({ data: data }); }
+
+    this.init();
+
+    this.postSomething = function(target, event) {
+      return $.post("http://someurl.com/post", { data: "something" });
+    }
+
+    this.events = [
+      {
+        method: "click",
+        target: "#action",
+        callback: this.doSomething
+        options: {
+          update: true
+        }
+      }
+    ]
+  }
+
+  Because the update option in the event is set to true the controller will automatically
+  fetch the new generated record of the `postSomething` function as long as the request succeeded.
+
+
+  Messages
+  ========
+
+  Each response can contain a list of messages as JSON.
+
+  The message object looks as follows:
+
+  messages: [
+    {
+      messageClass: 'info',
+      messageTitle: 'Just some Information',
+      message: 'This message is very important'
+    }
+  ]
+
+  The message class sets the severity of the message. It can be `info`, `warn` or `error`.
+
+
+  Validator
+  =========
+
+  The controller is able to pass each response throug a validator by using the internal `request` function.
+  This function wraps jQuery `ajax` function to pipe the validator (http://api.jquery.com/jquery.ajax).
+  The default validator only accepts the response when `proceed` is set to true.
+  But you can simply override the default validator.
+  As callback parameter you will get the returned data from the request.
+  The validator will only pass the whole request chain if the returned value is truthy.
+  Otherwise the request will reject.
+  So you can manipulate the response before reaching the event callback.
+
+  function MyController(options) {
+    global.Controller.call(this, $("#template").html(), $("#outlet"), options);
+
+    this.fetch = function() { return $.get("http://someurl.com") };
+
+    this.render = function(data) { return this.template({ data: data }); }
+
+    this.init();
+
+    var validator = function(data) { return data.isSave; };
+
+    this.doSomethingValidated = function(target, event) {
+      return this.request("http://someurl.com", {
+        method: "POST",
+        data: { data: "something" },
+        validator: validator
+      }).done(function() { console.log("Everything went good."); })
+      .fail(function() { console.log("Something went wrong"); });
+    }
+
+    this.events = [
+      {
+        method: "click",
+        target: "#action",
+        callback: this.doSomething
+        options: {
+          update: true
+        }
+      }
+    ]
+  }
+
+  Connect Controllers
+  ===================
+
+  You can set dependencies between controllers by passing controller objects to the `connected to` option.
+  The connected controllers will automatically update.
+  Circular dependencies are also possible.
+
+  function MyController(options) {
+    global.Controller.call(this);
+    this.init();
+  }
+
+  function OtherController() {
+    global.Controller.call(this);
+    this.init();
+  }
+
+  function AgainController() {
+    global.Controller.call(this);
+    this.init();
+  }
+
+  var myController = new MyController();
+  var otherController = new OtherController();
+  var againController = new AgainController();
+
+  myController.connectedTo = [otherController, againController];
+  otherController.connectedTo = [myController];
+  againController.connectedTo = [myController];
+
+ */
+
 (function(global, $, HBS) {
 
   "use strict";
