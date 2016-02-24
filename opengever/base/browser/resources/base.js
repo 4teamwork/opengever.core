847afecaf5d0eeb7175610b4b6bab7951445b508
David Erni <david.erni@4teamwork.ch>
2016-02-24 13:40:59 +0100

    Show portal directly when tabbed-view request redirected to portal.
    When a session times out but a page with tabbed-view is still open, tabbed-view
    requests are redirected. This custom handler forces a redirect to the portal
    by reloading the browser location.

--- opengever/base/browser/resources/base.js
+++ opengever/base/browser/resources/base.js
@@ -2,3 +2,12 @@ $(function() {
   // set focus on first form field
   $("form#form input:text:visible, form#form textarea:visible").first().focus();
 });
+
+
+$(document).delegate('body', 'tabbedview.unknownresponse', function(event, overview, jqXHR) {
+  if (jqXHR.getResponseHeader('X-GEVER-Service') === 'Portal') {
+    // the response we got originates from a portal, we reload to show the login window
+    location.reload();
+    return false;
+  }
+});
