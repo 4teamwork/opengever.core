c0a8bb9c541e877022c68451da44ca1f151f752f
David Erni <david.erni@4teamwork.ch>
2016-02-29 12:30:32 +0100

    Prevent accepting file-uploads in trix.

--- opengever/base/browser/resources/trix-custom.js
+++ opengever/base/browser/resources/trix-custom.js
@@ -3,3 +3,12 @@ Remove links from trix document model to prevent that links can be copy-pasted
 into trix even though we have disabled the link buttons.
  */
 delete Trix.config.textAttributes.href
+
+/*
+Prevent accepting file-uploads in trix.
+ */
+$(function() {
+    $('trix-editor').on('trix-file-accept', function(event) {
+        event.preventDefault();
+    });
+});
