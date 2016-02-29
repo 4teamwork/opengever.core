4568616172c14e1d42f89fe34aeceafac4827413
David Erni <david.erni@4teamwork.ch>
2016-02-29 14:35:22 +0100

    No longer accept code/quote and attachments of any kind.

--- opengever/base/browser/resources/trix-custom.js
+++ opengever/base/browser/resources/trix-custom.js
@@ -1,14 +1,26 @@
-/*
-Remove links from trix document model to prevent that links can be copy-pasted
-into trix even though we have disabled the link buttons.
- */
-delete Trix.config.textAttributes.href
+(function ($) {
 
-/*
-Prevent accepting file-uploads in trix.
- */
-$(function() {
-    $('trix-editor').on('trix-file-accept', function(event) {
-        event.preventDefault();
+    'use strict';
+
+    /*
+    Remove pre, blockquote and links from trix document model to prevent that they can
+    be inserted with copy-paste.
+     */
+    delete Trix.config.blockAttributes.code;
+    delete Trix.config.blockAttributes.quote;
+    delete Trix.config.textAttributes.href;
+
+    /*
+    Prevent accepting file-uploads and attachments in trix.
+     */
+    $(function () {
+        var trixEditor = $('trix-editor');
+        trixEditor.on('trix-file-accept', function (event) {
+            event.preventDefault();
+        });
+        trixEditor.on("trix-attachment-add", function (event) {
+            event.originalEvent.attachment.remove();
+        });
     });
-});
+
+}(jQuery));
