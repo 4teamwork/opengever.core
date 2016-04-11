208f599053d625ce092999e1281170430b6ce156
Lukas Graf <lukas.graf@4teamwork.ch>
2016-04-11 21:33:28 +0200

    REST API: Customize summary to include translated titles:
    This will update the summary's 'title' to be the translated
    title (based on the negotiated language, coming from the
    request's Accept-Language header) for objects with
    ITranslatedTitleSupport.
    
    Note: Because we have combined language codes enabled in the
    portal_languages tool, language codes sent in the Accept-Language header
    need to be written in the combined form (e.g., 'de-ch') for language
    content negotiation to work properly.

---
+++ opengever/api/tests/test_summary.py
@@ -0,0 +1,72 @@
+from ftw.builder import Builder
+from ftw.builder import create
+from opengever.api.testing import RelativeSession
+from opengever.core.testing import OPENGEVER_FUNCTIONAL_API_ZSERVER_LAYER
+from opengever.testing import FunctionalTestCase
+from plone import api
+from plone.app.testing import SITE_OWNER_NAME
+from plone.app.testing import SITE_OWNER_PASSWORD
+import transaction
+
+
+class TestGeverJSONSummarySerializer(FunctionalTestCase):
+
+    layer = OPENGEVER_FUNCTIONAL_API_ZSERVER_LAYER
+
+    def setUp(self):
+        super(TestGeverJSONSummarySerializer, self).setUp()
+        self.portal = self.layer['portal']
+
+        self.api = RelativeSession(self.portal.absolute_url())
+        self.api.headers.update({'Accept': 'application/json'})
+        self.api.auth = (SITE_OWNER_NAME, SITE_OWNER_PASSWORD)
+
+        self.repo = create(Builder('repository_root')
+                           .having(id='ordnungssystem',
+                                   title_de=u'Ordnungssystem',
+                                   title_fr=u'Syst\xe8me de classement'))
+        self.repofolder = create(Builder('repository')
+                                 .within(self.repo)
+                                 .having(title_de=u'Ordnungsposition',
+                                         title_fr=u'Position'))
+        self.dossier = create(Builder('dossier')
+                              .within(self.repofolder)
+                              .titled(u'Mein Dossier'))
+
+        lang_tool = api.portal.get_tool('portal_languages')
+        lang_tool.setDefaultLanguage('de-ch')
+        lang_tool.supported_langs = ['fr-ch', 'de-ch']
+        transaction.commit()
+
+    def test_translated_title_contained_in_summary_if_obj_translated(self):
+        response = self.api.get(
+            '/ordnungssystem', headers={'Accept-Language': 'de-ch'})
+        repofolder_summary = response.json()['member'][0]
+
+        self.assertDictContainsSubset(
+            {u'title': u'Ordnungsposition'},
+            repofolder_summary)
+
+        response = self.api.get(
+            '/ordnungssystem', headers={'Accept-Language': 'fr-ch'})
+        repofolder_summary = response.json()['member'][0]
+
+        self.assertDictContainsSubset(
+            {u'title': u'Position'},
+            repofolder_summary)
+
+    def test_translated_titles_default_to_german(self):
+        response = self.api.get('/ordnungssystem')
+        repofolder_summary = response.json()['member'][0]
+
+        self.assertDictContainsSubset(
+            {u'title': u'Ordnungsposition'},
+            repofolder_summary)
+
+    def test_regular_title_in_summary_if_obj_not_translated(self):
+        response = self.api.get('/ordnungssystem/ordnungsposition')
+        dossier_summary = response.json()['member'][0]
+
+        self.assertDictContainsSubset(
+            {u'title': u'Mein Dossier'},
+            dossier_summary)
