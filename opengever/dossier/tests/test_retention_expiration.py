935e3bc8ae4af707e5b16dbc2972bb2bc215c0b5
Philippe Gross <philippe.gross@4teamwork.ch>
2016-04-13 06:59:17 +0200

    Add new event handler which updates/calculate the retention expiration_date when resolving a dossier.

--- opengever/dossier/tests/test_retention_expiration.py
+++ opengever/dossier/tests/test_retention_expiration.py
@@ -1,7 +1,12 @@
 from datetime import date
+from datetime import datetime
 from ftw.builder import Builder
 from ftw.builder import create
+from ftw.testbrowser import browsing
+from ftw.testing import freeze
+from opengever.dossier.behaviors.dossier import IDossier
 from opengever.testing import FunctionalTestCase
+import transaction
 
 
 class TestRetentionExpirationDate(FunctionalTestCase):
@@ -15,3 +20,14 @@ class TestRetentionExpirationDate(FunctionalTestCase):
     def test_is_the_retention_period_years_added_to_the_end_date(self):
         self.assertEqual(date(2030, 02, 21),
                          self.dossier.get_retention_expiration_date())
+
+    @browsing
+    def test_is_updated_when_resolving_a_dossier(self, browser):
+        with freeze(datetime(2010, 02, 21)):
+            IDossier(self.dossier).end = date(2010, 02, 25)
+            transaction.commit()
+
+            browser.login().open(self.dossier)
+            browser.find('dossier-transition-deactivate').click()
+            self.assertEqual(date(2030, 02, 25),
+                             self.dossier.get_retention_expiration_date())
