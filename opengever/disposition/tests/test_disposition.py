d121724cdf9d1675f9ce466d682f88019d623abf
Philippe Gross <philippe.gross@4teamwork.ch>
2016-04-14 14:52:27 +0200

    Change id and title format for Dispositions.
    Title: Disposition 234
    id: disposition-234

--- opengever/disposition/tests/test_disposition.py
+++ opengever/disposition/tests/test_disposition.py
@@ -4,6 +4,7 @@ from ftw.testbrowser import browsing
 from ftw.testbrowser.pages import factoriesmenu
 from ftw.testbrowser.pages.statusmessages import info_messages
 from opengever.testing import FunctionalTestCase
+from opengever.testing import obj2brain
 from opengever.testing import obj2paths
 from plone.protect import createToken
 
@@ -18,11 +19,24 @@ class TestDisposition(FunctionalTestCase):
         self.dossier2 = create(Builder('dossier').within(self.repository))
         self.dossier3 = create(Builder('dossier').within(self.repository))
 
+    def test_id_is_sequence_number_prefixed_with_disposition(self):
+        disposition_1 = create(Builder('disposition'))
+        disposition_2 = create(Builder('disposition'))
+
+        self.assertEquals('disposition-1', disposition_1.getId())
+        self.assertEquals('disposition-2', disposition_2.getId())
+
+    def test_title_is_sequence_number_prefixed_with_Disposition_and_space(self):
+        disposition = create(Builder('disposition'))
+
+        self.assertEquals(u'Disposition 1', disposition.title)
+        self.assertEquals('Disposition 1', disposition.Title())
+        self.assertEquals(u'Disposition 1', obj2brain(disposition).Title)
+
     @browsing
     def test_can_be_added(self, browser):
         browser.login().open(self.root)
         factoriesmenu.add('Disposition')
-
         browser.fill({'Reference': 'Ablieferung X29238',
                       'Dossiers': [self.dossier1, self.dossier2]})
         browser.find('Save').click()
