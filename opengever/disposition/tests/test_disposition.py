7ebf604755f601f9c82d7510de721d692dba17bb
Philippe Gross <philippe.gross@4teamwork.ch>
2016-04-15 16:01:38 +0200

    Make sure dossier states gets updated when added to or removed from a disposition.

--- opengever/disposition/tests/test_disposition.py
+++ opengever/disposition/tests/test_disposition.py
@@ -2,12 +2,15 @@ from ftw.builder import Builder
 from ftw.builder import create
 from ftw.testbrowser import browsing
 from ftw.testbrowser.pages import factoriesmenu
-from ftw.testbrowser.pages.statusmessages import error_messages
 from ftw.testbrowser.pages.statusmessages import info_messages
 from opengever.testing import FunctionalTestCase
 from opengever.testing import obj2brain
 from opengever.testing import obj2paths
+from plone import api
 from plone.protect import createToken
+
+
+OFFERED_STATE = 'dossier-state-offered'
 
 
 class TestDisposition(FunctionalTestCase):
@@ -78,3 +81,63 @@ class TestDisposition(FunctionalTestCase):
             ['Error There were some errors.',
              'The retention period of the selected dossiers is not expired.'],
             browser.css('.error').text)
+
+    @browsing
+    def test_attached_dossier_are_set_to_offered_state(self, browser):
+        data = {'paths:list': obj2paths([self.dossier1, self.dossier3]),
+                '_authenticator': createToken()}
+        browser.login().open(self.root,
+                             view='++add++opengever.disposition.disposition',
+                             data=data)
+        browser.find('Save').click()
+
+        self.assertEquals(OFFERED_STATE, api.content.get_state(self.dossier1))
+        self.assertEquals(OFFERED_STATE, api.content.get_state(self.dossier3))
+
+
+class TestDispositionEditForm(FunctionalTestCase):
+
+    def setUp(self):
+        super(TestDispositionEditForm, self).setUp()
+        self.root = create(Builder('repository_root'))
+        self.repository = create(Builder('repository').within(self.root))
+        self.dossier1 = create(Builder('dossier').as_expired()
+                               .within(self.repository))
+        self.dossier2 = create(Builder('dossier')
+                               .as_expired()
+                               .in_state('dossier-state-inactive')
+                               .within(self.repository))
+        self.dossier3 = create(Builder('dossier')
+                               .as_expired()
+                               .within(self.repository))
+
+        self.grant('Contributor', 'Editor', 'Reader', 'Reviewer')
+
+    @browsing
+    def test_set_added_dossiers_to_offered_state(self, browser):
+        disposition = create(Builder('disposition')
+                             .having(dossiers=[self.dossier1])
+                             .within(self.root))
+
+        browser.login().open(disposition, view='edit')
+        browser.fill({'Dossiers': [self.dossier1, self.dossier2, self.dossier3]})
+        browser.find('Save').click()
+
+        self.assertEquals(OFFERED_STATE, api.content.get_state(self.dossier2))
+        self.assertEquals(OFFERED_STATE, api.content.get_state(self.dossier3))
+
+    @browsing
+    def test_set_dropped_dossiers_to_former_state(self, browser):
+        disposition = create(Builder('disposition')
+                             .having(dossiers=[self.dossier1, self.dossier2, self.dossier3])
+                             .within(self.root))
+
+        browser.login().open(disposition, view='edit')
+        browser.fill({'Dossiers': [self.dossier3]})
+        browser.find('Save').click()
+
+        self.assertEquals('dossier-state-resolved',
+                          api.content.get_state(self.dossier1))
+        self.assertEquals('dossier-state-inactive',
+                          api.content.get_state(self.dossier2))
+        self.assertEquals(OFFERED_STATE, api.content.get_state(self.dossier3))
