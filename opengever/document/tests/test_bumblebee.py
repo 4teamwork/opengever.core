460d7e07d6e3ccc3a91fcbeaea949f6f8cd47c50
David Erni <david.erni@4teamwork.ch>
2016-04-18 14:31:19 +0200

    Make sure documents have a primary field.
    This is/will be required by bumblebee.

--- opengever/document/tests/test_bumblebee.py
+++ opengever/document/tests/test_bumblebee.py
@@ -4,6 +4,7 @@ from opengever.testing import FunctionalTestCase
 from ftw.bumblebee.tests.helpers import asset as bumblebee_asset
 from ftw.bumblebee.tests.helpers import DOCX_CHECKSUM
 from ftw.bumblebee import utils as bumblebee_utils
+from plone.rfc822.interfaces import IPrimaryFieldInfo
 
 
 class TestBumblebee(FunctionalTestCase):
@@ -21,3 +22,7 @@ class TestBumblebee(FunctionalTestCase):
         self.assertEquals(
             DOCX_CHECKSUM,
             bumblebee_utils.get_document_checksum(self.document))
+
+    def test_opengever_documents_have_a_primary_field(self):
+        fieldinfo = IPrimaryFieldInfo(self.document)
+        self.assertEqual('file', fieldinfo.fieldname)
