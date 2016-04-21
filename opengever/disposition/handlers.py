6ea4b453d352fa1e3fd1ec88e7caa738aa6e04dd
Philippe Gross <philippe.gross@4teamwork.ch>
2016-04-21 17:39:10 +0200

    Make sure the archival_value gets updated when finalize the appraisal.

---
+++ opengever/disposition/handlers.py
@@ -0,0 +1,9 @@
+from five import grok
+from opengever.disposition.disposition import IDisposition
+from Products.CMFCore.interfaces import IActionSucceededEvent
+
+
+@grok.subscribe(IDisposition, IActionSucceededEvent)
+def disposition_state_changed(context, event):
+    if event.action == 'disposition-transition-appraise':
+        context.finalize_appraisal()
