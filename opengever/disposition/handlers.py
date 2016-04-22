c0f10ac48926b6267c0e6c1e62f0e7036ca8c07f
Philippe Gross <philippe.gross@4teamwork.ch>
2016-04-22 09:48:51 +0200

    Make sure dossiers set to archived state, when disposition is archived.

--- opengever/disposition/handlers.py
+++ opengever/disposition/handlers.py
@@ -7,3 +7,6 @@ from Products.CMFCore.interfaces import IActionSucceededEvent
 def disposition_state_changed(context, event):
     if event.action == 'disposition-transition-appraise':
         context.finalize_appraisal()
+
+    if event.action == 'disposition-transition-archive':
+        context.mark_dossiers_as_archived()
