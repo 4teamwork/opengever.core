9cf4e6093c761b345c2b464346b48642ef414666
Philippe Gross <philippe.gross@4teamwork.ch>
2016-04-25 16:21:06 +0200

    Add new disposition listing tab to the repositoryroot.

---
+++ opengever/repository/upgrades/20160425161154_add_disposition_tab_action/upgrade.py
@@ -0,0 +1,21 @@
+from ftw.upgrade import UpgradeStep
+
+
+class AddDispositionTabAction(UpgradeStep):
+    """Add disposition tab action.
+    """
+
+    def __call__(self):
+        self.install_upgrade_profile()
+
+        self.actions_add_type_action(
+            'opengever.repository.repositoryroot',
+            after='dossiers',
+            action_id='dispositions',
+            **{'title': 'Dispositions',
+               'action': 'string:#',
+               'category': 'tabbedview-tabs',
+               'condition': '',
+               'link_target': '',
+               'visible': True,
+               'permissions': ('View', )})
