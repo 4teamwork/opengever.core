d866e44acb8d6daef31d40c03a7036b65668522c
Philippe Gross <philippe.gross@4teamwork.ch>
2016-04-27 16:30:37 +0200

    Make sure retention_expiration index value gets updated, when resolving a dossier.
    In some cases, the end date of a dossier gets updated during the resolving process (is set to the earliest_possible_end_date). But the workflow tool only updates workflow relevant indexes and the catalog metadata, therefore the indexed value of the `retention_period` index never gets updated (see #1758).
    
    Therefore I add `retention_expiration` to the dossier workflow's catalog relevant variables, to fixes #1758.

--- opengever/dossier/tests/test_retention_expiration.py
+++ opengever/dossier/tests/test_retention_expiration.py
@@ -6,6 +6,7 @@ from ftw.testbrowser import browsing
 from ftw.testing import freeze
 from opengever.dossier.behaviors.dossier import IDossier
 from opengever.testing import FunctionalTestCase
+from opengever.testing import index_data_for
 from opengever.testing import obj2brain
 import transaction
 
@@ -37,6 +38,23 @@ class TestRetentionExpirationDate(FunctionalTestCase):
                              self.dossier.get_retention_expiration_date())
             self.assertEqual(date(2030, 02, 25),
                              obj2brain(self.dossier).retention_expiration)
+
+    @browsing
+    def test_index_is_updated_when_end_date_is_changed_during_resolving(self, browser):
+        self.grant('Reader', 'Contributor',  'Editor', 'Reviewer')
+
+        create(Builder('document')
+               .within(self.dossier)
+               .having(document_date=date(2015, 01, 01)))
+
+        former_indexed_value = index_data_for(self.dossier).get('retention_expiration')
+
+        browser.login().open(self.dossier)
+        browser.find('dossier-transition-resolve').click()
+
+        self.assertNotEqual(
+            former_indexed_value,
+            index_data_for(self.dossier).get('retention_expiration'))
 
     def test_is_expired_when_its_earlier_than_today(self):
         with freeze(datetime(2030, 02, 22)):
