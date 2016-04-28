6f28e2618a09e71f4f53f0a88502bcb5a8c17947
Lukas Graf <lukas.graf@4teamwork.ch>
2016-04-28 17:59:37 +0200

    Refactor monkey patch "PatchCreateMailInContainer".

--- opengever/base/monkey/patches/__init__.py
+++ opengever/base/monkey/patches/__init__.py
@@ -1,9 +1,11 @@
+from .create_mail_defaults import PatchCreateMailInContainer
 from .ldap_userfolder_encoding import PatchLDAPUserFolderEncoding
 from .paste_permission import PatchDXContainerPastePermission
 from .tz_for_log import PatchZ2LogTimezone
 from .webdav_lock_timeout import PatchWebDAVLockTimeout
 
 
+PatchCreateMailInContainer()()
 PatchDXContainerPastePermission()()
 PatchLDAPUserFolderEncoding()()
 PatchWebDAVLockTimeout()()
