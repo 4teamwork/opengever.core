31ea8b670e842467d88f03f044afd069a771c7f0
Elio Schmutz <e.schmutz@4teamwork.ch>
2016-04-28 12:29:06 +0200

    Add bumblebee image in document overview

--- opengever/bumblebee/tests/test_utils.py
+++ opengever/bumblebee/tests/test_utils.py
@@ -1,6 +1,12 @@
+from ftw.builder import Builder
+from ftw.builder import create
+from ftw.bumblebee.tests.helpers import asset as bumblebee_asset
+from opengever.bumblebee import get_representation_url_by_brain
+from opengever.bumblebee import get_representation_url_by_object
 from opengever.bumblebee import is_bumblebee_feature_enabled
 from opengever.bumblebee.interfaces import IGeverBumblebeeSettings
 from opengever.testing import FunctionalTestCase
+from opengever.testing import obj2brain
 from plone import api
 
 
@@ -17,3 +23,47 @@ class TestIsBumblebeeFeatureEnabled(FunctionalTestCase):
             'is_feature_enabled', False, interface=IGeverBumblebeeSettings)
 
         self.assertFalse(is_bumblebee_feature_enabled())
+
+
+class TestGetRepresentationUrlByObject(FunctionalTestCase):
+
+    def test_returns_representation_url_if_checksum_is_available(self):
+        document = create(Builder('document')
+                          .attach_file_containing(
+                              bumblebee_asset('example.docx').bytes(),
+                              u'example.docx'))
+
+        self.assertIn(
+            '/YnVtYmxlYmVl/api/v2/resource/',
+            get_representation_url_by_object('thumbnail', document))
+
+    def test_returns_preserved_as_paper_fallback_image_if_no_ckecksum_is_available(self):
+        document = create(Builder('document'))
+
+        self.assertIn(
+            'preserved_as_paper.png',
+            get_representation_url_by_object('thumbnail', document))
+
+
+class TestGetRepresentationUrlByBrain(FunctionalTestCase):
+
+    def test_returns_representation_url_if_checksum_is_available(self):
+        document = create(Builder('document')
+                          .attach_file_containing(
+                              bumblebee_asset('example.docx').bytes(),
+                              u'example.docx'))
+
+        brain = obj2brain(document)
+
+        self.assertIn(
+            '/YnVtYmxlYmVl/api/v2/resource/',
+            get_representation_url_by_brain('thumbnail', brain))
+
+    def test_returns_preserved_as_paper_fallback_image_if_no_ckecksum_is_available(self):
+        document = create(Builder('document'))
+
+        brain = obj2brain(document)
+
+        self.assertIn(
+            'preserved_as_paper.png',
+            get_representation_url_by_brain('thumbnail', brain))
