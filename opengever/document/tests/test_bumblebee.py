39362e98d0fdcb19eb653724f5d3382b993ee1c2
Elio Schmutz <e.schmutz@4teamwork.ch>
2016-04-28 12:29:06 +0200

    Add bumblebee image in document overview

--- opengever/document/tests/test_bumblebee.py
+++ opengever/document/tests/test_bumblebee.py
@@ -1,17 +1,19 @@
 from ftw.builder import Builder
 from ftw.builder import create
-from opengever.testing import FunctionalTestCase
+from ftw.bumblebee import utils as bumblebee_utils
 from ftw.bumblebee.tests.helpers import asset as bumblebee_asset
 from ftw.bumblebee.tests.helpers import DOCX_CHECKSUM
-from ftw.bumblebee import utils as bumblebee_utils
+from ftw.testbrowser import browsing
+from opengever.core.testing import OPENGEVER_FUNCTIONAL_BUMBLEBEE_LAYER
+from opengever.testing import FunctionalTestCase
 from plone.rfc822.interfaces import IPrimaryFieldInfo
 
 
-class TestBumblebee(FunctionalTestCase):
+class TestBumblebeeIntegrationWithDisabledFeature(FunctionalTestCase):
     """Test integration of ftw.bumblebee."""
 
     def setUp(self):
-        super(TestBumblebee, self).setUp()
+        super(TestBumblebeeIntegrationWithDisabledFeature, self).setUp()
 
         self.document = create(Builder('document')
                                .attach_file_containing(
@@ -26,3 +28,26 @@ class TestBumblebee(FunctionalTestCase):
     def test_opengever_documents_have_a_primary_field(self):
         fieldinfo = IPrimaryFieldInfo(self.document)
         self.assertEqual('file', fieldinfo.fieldname)
+
+    @browsing
+    def test_document_preview_is_hidden(self, browser):
+        dossier = create(Builder('dossier'))
+        document = create(Builder('document').within(dossier))
+
+        browser.login().visit(document, view='tabbedview_view-overview')
+
+        self.assertEqual(0, len(browser.css('.documentPreview')))
+
+
+class TestBumblebeeIntegrationWithEnabledFeature(FunctionalTestCase):
+
+    layer = OPENGEVER_FUNCTIONAL_BUMBLEBEE_LAYER
+
+    @browsing
+    def test_document_preview_is_visible(self, browser):
+        dossier = create(Builder('dossier'))
+        document = create(Builder('document').within(dossier))
+
+        browser.login().visit(document, view='tabbedview_view-overview')
+
+        self.assertEqual(1, len(browser.css('.documentPreview')))
