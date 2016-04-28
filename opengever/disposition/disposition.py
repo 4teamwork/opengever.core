0a72e07374e63e31ff2c3e6d96b45a85b39002db
Philippe Gross <philippe.gross@4teamwork.ch>
2016-04-28 20:20:24 +0200

    Fixed disposition title: contains now the translated disposition label.
    Bsp: Angebot 345.

--- opengever/disposition/disposition.py
+++ opengever/disposition/disposition.py
@@ -19,6 +19,8 @@ from z3c.relationfield.schema import RelationList
 from zope import schema
 from zope.annotation.interfaces import IAnnotations
 from zope.component import getUtility
+from zope.globalrequest import getRequest
+from zope.i18n import translate
 from zope.interface import implements
 from zope.interface import Invalid
 from zope.interface import invariant
@@ -126,7 +128,9 @@ class Disposition(Container):
     @property
     def title(self):
         return u'{} {}'.format(
-            _('Disposition'), getUtility(ISequenceNumber).get_number(self))
+            translate(_('label_disposition', default=u'Disposition'),
+                      context=getRequest()),
+            getUtility(ISequenceNumber).get_number(self))
 
     @title.setter
     def title(self, x):
