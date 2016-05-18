5edf8ecf0a30ae7e007238c48ae80f000760ec91
David Erni <david.erni@4teamwork.ch>
2016-05-18 10:14:55 +0200

    Rewrite upgrade-step to migrate all dossiers.

--- opengever/dossier/upgrades/20160429161120_use_documents_proxy_view/upgrade.py
+++ opengever/dossier/upgrades/20160429161120_use_documents_proxy_view/upgrade.py
@@ -6,20 +6,26 @@ class UseDocumentsProxyView(UpgradeStep):
     """Use documents proxy view.
     """
 
-    dossier_types = [
-        'opengever.dossier.businesscasedossier',
-        'opengever.dossier.templatedossier',
-        'opengever.dossier.projectdossier'
-        ]
-
     old_action_id = 'documents'
     new_action_id = 'documents-proxy'
 
     def __call__(self):
         self.install_upgrade_profile()
-        self.change_documents_tab_to_docuemnts_proxy_tab()
+        self.change_documents_tab_to_documents_proxy_tab()
 
-    def change_documents_tab_to_docuemnts_proxy_tab(self):
+    def _is_dossier_fti(self, fti):
+        """Use the opengever.dossier.behaviors.dossier.IDossier behavior to
+        detect if the FTI is a dossier fti. That behavior is enabled for all
+        currently known dossiers.
+        """
+
+        behaviors = fti.getProperty('behaviors')
+        if not behaviors:
+            return False
+
+        return 'opengever.dossier.behaviors.dossier.IDossier' in behaviors
+
+    def change_documents_tab_to_documents_proxy_tab(self):
         """To choose which template the user has selected before (list or gallery)
         with activated bumblebeefeature, we have to add a proxy view.
 
@@ -27,12 +33,11 @@ class UseDocumentsProxyView(UpgradeStep):
         proxy-view.
         """
         ttool = api.portal.get_tool('portal_types')
-        for dossier_type in self.dossier_types:
-            dossier = ttool.get(dossier_type)
-            if not dossier:
+        for fti in ttool.listTypeInfo():
+            if not self._is_dossier_fti(fti):
                 continue
 
-            for action in dossier._actions:
+            for action in fti._actions:
                 if not action.id == self.old_action_id:
                     continue
 
