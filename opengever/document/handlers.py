9f795e450694a77b793ea7047af7c4581f25f583
Philippe Gross <philippe.gross@4teamwork.ch>
2016-05-18 17:51:33 +0200

    Set archival_file conversion state to manually, when adding/chaning the archival file via the edit form.

--- opengever/document/handlers.py
+++ opengever/document/handlers.py
@@ -1,10 +1,13 @@
 from five import grok
+from opengever.document.archival_file import ARCHIVAL_FILE_STATE_MANUALLY
+from opengever.document.archival_file import ArchivalFileConverter
 from opengever.document.document import IDocumentSchema
 from opengever.document.interfaces import IObjectBeforeCheckInEvent
 from opengever.document.interfaces import IObjectCheckedOutEvent
 from opengever.dossier.docprops import DocPropertyWriter
 from zope.lifecycleevent import IObjectMovedEvent
 from zope.lifecycleevent import IObjectRemovedEvent
+from zope.lifecycleevent.interfaces import IObjectModifiedEvent
 
 
 DISABLE_DOCPROPERTY_UPDATE_FLAG = 'disable_docproperty_update'
@@ -33,3 +36,17 @@ def update_moved_doc_properties(context, event):
 
 def _update_docproperties(document):
     DocPropertyWriter(document).update()
+
+
+@grok.subscribe(IDocumentSchema, IObjectModifiedEvent)
+def set_archival_file_state(context, event):
+    archival_file_changed = False
+
+    for desc in event.descriptions:
+        for attr in desc.attributes:
+            if attr == 'IDocumentMetadata.archival_file':
+                archival_file_changed = True
+                break
+
+    if archival_file_changed:
+        ArchivalFileConverter(context).set_state(ARCHIVAL_FILE_STATE_MANUALLY)
