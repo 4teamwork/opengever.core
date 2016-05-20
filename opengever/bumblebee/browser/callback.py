d15593646850c14deb784e95d0c301c54b5b36b5
Philippe Gross <philippe.gross@4teamwork.ch>
2016-05-20 13:02:50 +0200

    WIP: callback fix.

--- opengever/bumblebee/browser/callback.py
+++ opengever/bumblebee/browser/callback.py
@@ -5,18 +5,12 @@ import base64
 
 class StoreArchivalFile(BaseConvertCallbackView):
 
-    def handle_success(self):
-        ArchivalFileConverter(self.context).store_file(self.get_file_data())
-        super(StoreArchivalFile, self).handle_success()
+    def handle_success(self, mimetype, file_data):
+        ArchivalFileConverter(self.context).store_file(file_data)
 
     def handle_error(self):
         ArchivalFileConverter(self.context).handle_conversion_failure()
-        super(StoreArchivalFile, self).handle_error()
 
     def verify_token(self):
         # TODO: Remove and let the BaseConvertCallbackView check the token.
         return True
-
-    def get_file_data(self):
-        file_info, data = self.get_body().get('data').split(',')
-        return base64.b64decode(data)
