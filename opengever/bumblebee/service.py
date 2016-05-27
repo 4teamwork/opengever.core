98b5eb11270cc7625c13053930007f869f1f6261
Philippe Gross <philippe.gross@4teamwork.ch>
2016-05-27 17:26:17 +0200

    Update Geverbumblebeeservice service with the newest adjustments from ftw.bumblebee.
    See https://github.com/4teamwork/ftw.bumblebee/pull/80

--- opengever/bumblebee/service.py
+++ opengever/bumblebee/service.py
@@ -5,8 +5,9 @@ from opengever.bumblebee import is_bumblebee_feature_enabled
 class GeverBumblebeeService(BumblebeeServiceV3):
     """Only queues storings when the bumblebee feature is enabled."""
 
-    def queue_storing(self, queue):
+    def queue_storing(self, queue, deferred=False):
         if not is_bumblebee_feature_enabled():
             return False
 
-        return super(GeverBumblebeeService, self).queue_storing(queue)
+        return super(GeverBumblebeeService, self).queue_storing(
+            queue, deferred=deferred)
