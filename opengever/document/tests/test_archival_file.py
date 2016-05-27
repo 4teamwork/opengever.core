3660a8b64263fde5b1f14e67a35f7a41a1854141
Philippe Gross <philippe.gross@4teamwork.ch>
2016-05-27 15:47:44 +0200

    Improve eventhandler which checks if the archival_file has changed.
    Unfortunately the file widgets are always marked as changed in the event descriptions of a ObjectModifiedEvent, therefore I check on basis of the request data.

--- opengever/document/tests/test_archival_file.py
+++ opengever/document/tests/test_archival_file.py
@@ -1,10 +1,10 @@
 from ftw.builder import Builder
 from ftw.builder import create
 from ftw.testbrowser import browsing
-from opengever.document.archival_file import STATE_CONVERTED
-from opengever.document.archival_file import STATE_MANUALLY_PROVIDED
-from opengever.document.archival_file import STATE_CONVERTING
 from opengever.document.archival_file import ArchivalFileConverter
+from opengever.document.archival_file import STATE_CONVERTED
+from opengever.document.archival_file import STATE_CONVERTING
+from opengever.document.archival_file import STATE_MANUALLY_PROVIDED
 from opengever.document.behaviors.metadata import IDocumentMetadata
 from opengever.testing import FunctionalTestCase
 
@@ -52,3 +52,114 @@ class TestArchivalFile(FunctionalTestCase):
         self.assertEquals(
             STATE_CONVERTED,
             IDocumentMetadata(self.document).archival_file_state)
+
+
+class TestArchivalFileState(FunctionalTestCase):
+
+    def setUp(self):
+        super(TestArchivalFileState, self).setUp()
+        self.dossier = create(Builder('dossier'))
+
+    @browsing
+    def test_set_to_manually_when_archival_file_is_added_in_edit_form(self, browser):
+        document = create(Builder('document').within(self.dossier))
+
+        browser.login().open(document, view='edit')
+        browser.fill(
+            {'Archival File': ('FILE DATA', 'archival_file.pdf')})
+        browser.click_on('Save')
+
+        self.assertEquals(
+            STATE_MANUALLY_PROVIDED,
+            IDocumentMetadata(document).archival_file_state)
+
+    @browsing
+    def test_is_not_updated_when_archival_file_is_not_changed(self, browser):
+        document = create(Builder('document')
+                          .within(self.dossier)
+                          .attach_archival_file_containing('DATA')
+                          .having(archival_file_state=STATE_CONVERTED))
+
+        browser.login().open(document, view='edit')
+        browser.fill({'Title': u'New document title'})
+        browser.click_on('Save')
+
+        self.assertEquals(
+            STATE_CONVERTED,
+            IDocumentMetadata(document).archival_file_state)
+
+    @browsing
+    def test_set_to_manually_when_archival_file_is_updated_in_edit_form(self, browser):
+        document = create(Builder('document')
+                          .within(self.dossier)
+                          .attach_archival_file_containing('DATA')
+                          .having(archival_file_state=STATE_CONVERTED))
+
+        browser.login().open(document, view='edit')
+        browser.fill(
+            {'Archival File': ('NEW FILE DATA', 'ddd.pdf')})
+        browser.click_on('Save')
+
+        self.assertEquals(
+            STATE_MANUALLY_PROVIDED,
+            IDocumentMetadata(document).archival_file_state)
+
+    @browsing
+    def test_removes_state_when_archival_file_is_removed(self, browser):
+        document = create(Builder('document')
+                          .within(self.dossier)
+                          .attach_archival_file_containing('DATA')
+                          .having(archival_file_state=STATE_CONVERTED))
+
+        browser.login().open(document, view='edit')
+
+        remove_radio_button = browser.css(
+            '#form-widgets-IDocumentMetadata-archival_file-remove').first
+        remove_radio_button.checked = 'checked'
+        browser.find('Save').click()
+
+        self.assertIsNone(IDocumentMetadata(document).archival_file_state)
+
+    @browsing
+    def test_set_to_manually_when_archival_file_is_added_in_archival_file_form(self, browser):
+        document = create(Builder('document').within(self.dossier))
+
+        browser.login().open(document, view='edit_archival_file')
+        browser.fill(
+            {'Archival File': ('FILE DATA', 'archival_file.pdf')})
+        browser.click_on('Save')
+
+        self.assertEquals(
+            STATE_MANUALLY_PROVIDED,
+            IDocumentMetadata(document).archival_file_state)
+
+    @browsing
+    def test_set_to_manually_when_archival_file_is_updated_in_archival_file_form(self, browser):
+        document = create(Builder('document')
+                          .within(self.dossier)
+                          .attach_archival_file_containing('DATA')
+                          .having(archival_file_state=STATE_CONVERTED))
+
+        browser.login().open(document, view='edit_archival_file')
+        browser.fill(
+            {'Archival File': ('NEW FILE DATA', 'ddd.pdf')})
+        browser.click_on('Save')
+
+        self.assertEquals(
+            STATE_MANUALLY_PROVIDED,
+            IDocumentMetadata(document).archival_file_state)
+
+    @browsing
+    def test_removes_state_when_archival_file_is_removed_in_archival_file_form(self, browser):
+        document = create(Builder('document')
+                          .within(self.dossier)
+                          .attach_archival_file_containing('DATA')
+                          .having(archival_file_state=STATE_CONVERTED))
+
+        browser.login().open(document, view='edit_archival_file')
+        remove_radio_button = browser.css(
+            '#form-widgets-archival_file-remove').first
+        remove_radio_button.checked = 'checked'
+        browser.find('Save').click()
+
+        self.assertIsNone(IDocumentMetadata(document).archival_file_state)
