2aad465d815615bc882029e6543239ff6ba2a4c8
Elio Schmutz <e.schmutz@4teamwork.ch>
2016-06-03 18:19:03 +0200

    Add fallback image for email

--- opengever/bumblebee/__init__.py
+++ opengever/bumblebee/__init__.py
@@ -1,6 +1,7 @@
 from ftw.bumblebee.utils import get_representation_url_by_brain as representation_url_by_brain
 from ftw.bumblebee.utils import get_representation_url_by_object as representation_url_by_object
 from opengever.bumblebee.interfaces import IGeverBumblebeeSettings
+from opengever.mail.mail import IOGMailMarker
 from plone import api
 from zope.globalrequest import getRequest
 
@@ -10,7 +11,13 @@ BUMBLEBEE_VIEW_COOKIE_NAME = 'bumblebee-view'
 def get_not_digitally_available_placeholder_image_url():
     return "{}{}".format(
         api.portal.get().absolute_url(),
-        "/++resource++opengever.base/images/not_digitally_available.png")
+        "/++resource++opengever.bumblebee.resources/fallback_not_digitally_available.png")
+
+
+def get_mail_placeholder_image_url():
+    return "{}{}".format(
+        api.portal.get().absolute_url(),
+        "/++resource++opengever.bumblebee.resources/fallback_email.png")
 
 
 def is_bumblebee_feature_enabled():
@@ -29,6 +36,9 @@ def get_representation_url_by_object(format_name, obj, filename=''):
     That means, our obj is only preserved as paper and we have to return
     a special placeholder image for these documents
     """
+    if IOGMailMarker.providedBy(obj):
+        return get_mail_placeholder_image_url()
+
     return representation_url_by_object(format_name, obj, filename) or \
         get_not_digitally_available_placeholder_image_url()
 
@@ -44,6 +54,9 @@ def get_representation_url_by_brain(format_name, brain):
     That means, our brain is only preserved as paper and we have to return
     a special placeholder image for these documents
     """
+    if brain.portal_type in ['ftw.mail.mail']:
+        return get_mail_placeholder_image_url()
+
     return representation_url_by_brain(format_name, brain) or \
         get_not_digitally_available_placeholder_image_url()
 
--- opengever/tabbedview/tests/test_bumblebee_gallery.py
+++ opengever/tabbedview/tests/test_bumblebee_gallery.py
@@ -241,7 +241,7 @@ class TestBumblebeeGalleryMixinPreviews(FunctionalTestCase):
             [{'uid': document.UID(),
               'overlay_url': 'http://nohost/plone/dossier-1/document-1/@@bumblebee-overlay-listing',
               'mime_type_css_class': 'contenttype-opengever-document-document',
-              'preview_image_url': 'http://nohost/plone/++resource++opengever.base/images/not_digitally_available.png',
+              'preview_image_url': 'http://nohost/plone/++resource++opengever.bumblebee.resources/fallback_not_digitally_available.png',
               'title': 'Testdokum\xc3\xa4nt'}],
             list(view.previews()))
 
