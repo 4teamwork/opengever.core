714fc7aaed46cd4b66b0a4c91fa9df0ce9c4abdb
David Erni <david.erni@4teamwork.ch>
2016-06-16 11:50:10 +0200

    Log unexpected changes to trix html to sentry.

--- opengever/base/tests/test_transform.py
+++ opengever/base/tests/test_transform.py
@@ -9,14 +9,17 @@ class TestTrixSablonTransform(FunctionalTestCase):
         super(TestTrixSablonTransform, self).setUp()
         self.transforms = api.portal.get_tool('portal_transforms')
 
+    def apply_transform(self, value):
+        return self.transforms.convert('trix_to_sablon', value).getData()
+
     def test_transform_strips_disallowed_html_tags(self):
         value = (
-            "<div><span>Foo</span><abbr></abbr>"
+            "<div><span>Foo</span><abbr>gnahahah</abbr>"
             "<h1>Title</h1>"
             "</div>"
         )
-        output = self.transforms.convert('trix_to_sablon', value).getData()
-        self.assertEqual("<div>FooTitle</div>", output)
+        self.assertEqual(
+            "<div>FoognahahahTitle</div>", self.apply_transform(value))
 
     def test_transform_doesnt_strip_whitespace_when_all_elements_are_valid(self):
         value = """&nbsp;
@@ -24,5 +27,9 @@ class TestTrixSablonTransform(FunctionalTestCase):
         </div>
         """ + string.whitespace
 
-        output = self.transforms.convert('trix_to_sablon', value).getData()
-        self.assertEqual(value, output)
+        self.assertEqual(value, self.apply_transform(value))
+
+    def test_transform_doesnt_strip_whitespace_when_stripping_invalid_tags(self):
+        value = "\n<span>\n\n\t  Foo!  </span>"
+
+        self.assertEqual("\n\n\n\t  Foo!  ", self.apply_transform(value))
--- opengever/base/transforms/trix_html_to_sablon_html.py
+++ opengever/base/transforms/trix_html_to_sablon_html.py
@@ -1,4 +1,6 @@
+from opengever.base.sentry import log_msg_to_sentry
 from Products.PortalTransforms.transforms.safe_html import SafeHTML
+from zope.globalrequest import getRequest
 
 
 # These are the HTML tags that are supported by trix
@@ -22,10 +24,34 @@ TRIX_HTML_MIME_TYPE = 'text/x-html-trix'
 SABLON_HTML_MIME_TYPE = 'text/x-html-sablon'
 
 
+class TrixToSablon(SafeHTML):
+
+    __name__ = 'trix_to_sablon'
+    inputs = (TRIX_HTML_MIME_TYPE, )
+    output = SABLON_HTML_MIME_TYPE
+
+    def _log_unexpected_conversion_to_sentry(self, data, orig):
+        request = getRequest()
+
+        extra = {
+            'converted_html': data,
+            'source_html': orig
+        }
+
+        log_msg_to_sentry(
+            'Unexpected html during trix/sablon conversion',
+            request=request,
+            url=request.get('ACTUAL_URL', ''),
+            extra=extra)
+
+    def convert(self, orig, data, **kwargs):
+        data = super(TrixToSablon, self).convert(orig, data, **kwargs)
+        if data != orig:
+            self._log_unexpected_conversion_to_sentry(data, orig)
+        return data
+
+
 # This is basically a copy of the safe_html transform, but we allow only the
 # html tags that are supported by sablon/trix.
 def register():
-    return SafeHTML(name='trix_to_sablon',
-                    inputs=(TRIX_HTML_MIME_TYPE, ),
-                    output=SABLON_HTML_MIME_TYPE,
-                    valid_tags=VALID_TAGS)
+    return TrixToSablon(valid_tags=VALID_TAGS)
