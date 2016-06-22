1de08879796a5057f61ba48c783f591ff0364e9d
David Erni <david.erni@4teamwork.ch>
2016-06-22 12:02:42 +0200

    Use new service registration with request layer.

--- opengever/bumblebee/service.py
+++ opengever/bumblebee/service.py
@@ -6,21 +6,21 @@ from opengever.document.document import IDocumentSchema
 class GeverBumblebeeService(BumblebeeServiceV3):
     """Only queues storings when the bumblebee feature is enabled."""
 
-    def queue_storing(self, queue, deferred=False):
+    def queue_storing(self, document, queue, deferred=False):
         if not is_bumblebee_feature_enabled():
             return False
 
         return super(GeverBumblebeeService, self).queue_storing(
-            queue, deferred=deferred)
+            document, queue, deferred=deferred)
 
-    def handle_document_update(self, force=False):
+    def handle_update(self, document, force=False):
         """Do nothing when attempting to update a checked out document.
 
         This prevents that a preview of a working copy is rendered, it would
         be visible to all the other users as well.
         """
-        if IDocumentSchema.providedBy(self.context):
-            if self.context.is_checked_out():
+        if IDocumentSchema.providedBy(document):
+            if document.is_checked_out():
                 return
 
-        return super(GeverBumblebeeService, self).handle_document_update(force=force)
+        return super(GeverBumblebeeService, self).handle_update(document, force=force)
