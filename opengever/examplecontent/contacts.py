b758bf82631778a524d0469f2286780ae4c5b222
Philippe Gross <philippe.gross@4teamwork.ch>
2016-07-01 09:27:15 +0200

    Extend ExampleContactCreator to creating organisations.

--- opengever/examplecontent/contacts.py
+++ opengever/examplecontent/contacts.py
@@ -1,6 +1,8 @@
 from opengever.base.model import create_session
 from opengever.contact.models import Address
 from opengever.contact.models import MailAddress
+from opengever.contact.models import Organization
+from opengever.contact.models import OrgRole
 from opengever.contact.models import Person
 from opengever.contact.models import PhoneNumber
 from pkg_resources import resource_filename
@@ -14,37 +16,69 @@ PHONENUMBER_LABELS = ['Arbeit', 'Privat', 'Mobile']
 
 class ExampleContactCreator(object):
 
-    def __init__(self, site):
-        self.site = site
+    def __init__(self):
         self.db_session = create_session()
 
-    def load_contacts(self):
+    def load_persons(self):
         path = resource_filename(
-            'opengever.examplecontent', 'data/contacts.json')
-        _file = open(path, 'r')
-        return json.loads(_file.read())
+            'opengever.examplecontent', 'data/persons.json')
+        return json.load(open(path, 'r'))
+
+    def load_organizations(self):
+        path = resource_filename(
+            'opengever.examplecontent', 'data/organizations.json')
+        return json.load(open(path, 'r'))
 
     def create(self):
-        items = self.load_contacts()
+        organizations = self.create_organizations()
+        self.create_contacts(organizations)
+
+    def create_organizations(self):
+        organizations = []
+        items = self.load_organizations()
         for item in items:
-            person = Person(academic_title=item['title'],
+            organization = Organization(name=item['name'])
+            self.db_session.add(organization)
+            organizations.append(organization)
+
+            self.add_address(item, organization, ['Hauptsitz', None])
+            self.add_mail(item, organization, ['Info', 'Support', None])
+
+        return organizations
+
+    def create_contacts(self, organizations=[]):
+        items = self.load_persons()
+        for item in items:
+
+            person = Person(salutation=item['salutation'],
                             firstname=item['firstname'],
                             lastname=item['lastname'])
             self.db_session.add(person)
 
-            address = Address(label=random.choice(ADDRESS_LABELS),
-                              street=item['street'],
-                              zip_code=str(item['zip_code']),
-                              city=item['city'],
-                              contact=person)
-            self.db_session.add(address)
+            self.add_address(item, person, ADDRESS_LABELS)
+            self.add_phonenumber(item, person, PHONENUMBER_LABELS)
+            self.add_mail(item, person, ADDRESS_LABELS)
 
-            phonenumber = PhoneNumber(label=random.choice(PHONENUMBER_LABELS),
-                                      number=item['phonenumber'],
-                                      contact=person)
-            self.db_session.add(phonenumber)
+            org_role = OrgRole(person=person,
+                               organization=random.choice(organizations))
+            self.db_session.add(org_role)
 
-            mail = MailAddress(label=random.choice(ADDRESS_LABELS),
-                               address=item['mail'],
-                               contact=person)
-            self.db_session.add(mail)
+    def add_address(self, item, contact, labels):
+        address = Address(label=random.choice(labels),
+                          street=item['street'],
+                          zip_code=item['zip_code'],
+                          city=item['city'],
+                          contact=contact)
+        self.db_session.add(address)
+
+    def add_phonenumber(self, item, contact, labels):
+        phonenumber = PhoneNumber(label=random.choice(labels),
+                                  phone_number=item['phonenumber'],
+                                  contact=contact)
+        self.db_session.add(phonenumber)
+
+    def add_mail(self, item, contact, labels):
+        mail = MailAddress(label=random.choice(labels),
+                           address=item['mail'],
+                           contact=contact)
+        self.db_session.add(mail)
