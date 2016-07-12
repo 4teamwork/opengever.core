118333c151a4f5d0741965b9632f42fbd9fd14b5
Lukas Graf <lukas.graf@4teamwork.ch>
2016-07-12 16:49:51 +0200

    Add comprehensive tests for ILifeCycle.custody_period

--- opengever/base/tests/test_lifecycle.py
+++ opengever/base/tests/test_lifecycle.py
@@ -1,186 +1,285 @@
 from ftw.builder import Builder
 from ftw.builder import create
 from ftw.testbrowser import browsing
-from grokcore.component.testing import grok
-from mocker import ANY
-from mocker import Mocker
+from ftw.testbrowser.pages import factoriesmenu
 from opengever.base.behaviors import lifecycle
+from opengever.base.behaviors.lifecycle import ILifeCycle
 from opengever.base.interfaces import IBaseCustodyPeriods
 from opengever.base.interfaces import IRetentionPeriodRegister
 from opengever.testing import add_languages
 from opengever.testing import FunctionalTestCase
-from plone.mocktestcase import MockTestCase
+from plone import api
+from plone.dexterity.utils import createContentInContainer
 from plone.registry.interfaces import IRegistry
-from Products.CMFCore.interfaces import ISiteRoot
-from unittest2 import TestCase
-from z3c.form.interfaces import IValidator
-from z3c.form.interfaces import IValue
-from zope.component import getMultiAdapter
 from zope.component import getUtility
-from zope.component import provideAdapter
-from zope.component import provideUtility
-from zope.interface import directlyProvides
-from zope.schema.interfaces import ConstraintNotSatisfied
-from zope.schema.interfaces import IVocabularyFactory
-from zope.schema.vocabulary import getVocabularyRegistry
-from zope.schema.vocabulary import VocabularyRegistryError
 import transaction
 
 
-class TestCustodyPeriod(MockTestCase, TestCase):
+class TestCustodyPeriodDefault(FunctionalTestCase):
 
     def setUp(self):
-        super(TestCustodyPeriod, self).setUp()
+        super(TestCustodyPeriodDefault, self).setUp()
+        self.repofolder = create(Builder('repository'))
+        self.field = ILifeCycle['custody_period']
 
-        self.testcase_mocker = Mocker()
-        grok('opengever.base.behaviors.lifecycle')
+    def get_custody_period(self, obj):
+        return self.field.get(self.field.interface(obj))
 
-        # mock the registry, so that we have a static
-        # configuration in our tests. we test functionality,
-        # not configuration..
-        proxy = self.testcase_mocker.mock()
-        proxy.custody_periods
-        self.testcase_mocker.result([u'0', u'10', u'20', u'30'])
-        self.testcase_mocker.count(0, None)
+    def set_custody_period(self, obj, value):
+        self.field.set(self.field.interface(obj), value)
+        transaction.commit()
 
-        registry = self.testcase_mocker.mock()
-        provideUtility(provides=IRegistry, component=registry)
-        registry.forInterface(IBaseCustodyPeriods)
-        self.testcase_mocker.result(proxy)
-        self.testcase_mocker.count(0, None)
+    def get_fti(self, portal_type):
+        types_tool = api.portal.get_tool('portal_types')
+        return types_tool[portal_type]
 
-        # we need to register the vocabulary utility in the
-        # vocabulary registry manually at this point:
-        vocabulary_registry = getVocabularyRegistry()
-        field = lifecycle.ILifeCycle['custody_period']
-        try:
-            vocabulary_registry.get(None, field.vocabularyName)
-        except VocabularyRegistryError:
-            factory = getUtility(IVocabularyFactory,
-                                 name=u'lifecycle_custody_period_vocabulary')
-            vocabulary_registry.register(field.vocabularyName, factory)
+    @browsing
+    def test_custody_period_default(self, browser):
+        browser.login().open()
+        factoriesmenu.add(u'Business Case Dossier')
+        browser.fill({'Title': 'My Dossier'}).save()
+        dossier = browser.context
 
-        # in this stage, only the grok-components (adapaters, utilities)
-        # of the module are registered in the component registry.
+        value = self.get_custody_period(dossier)
+        self.assertEqual(30, value)
 
-        # we need to register any plone.directives.form magic components
-        # from the module manually (they are not grokky):
-        for factory, name in lifecycle.__form_value_adapters__:
-            provideAdapter(factory, name=name)
+    @browsing
+    def test_custody_period_acquired_default(self, browser):
+        browser.login().open(self.repofolder)
+        self.set_custody_period(self.repofolder, 100)
+        factoriesmenu.add(u'Business Case Dossier')
+        browser.fill({'Title': 'My Dossier'}).save()
+        dossier = browser.context
 
-        self.testcase_mocker.replay()
+        value = self.get_custody_period(dossier)
+        self.assertEqual(100, value)
 
-    def tearDown(self):
-        self.testcase_mocker.verify()
-        self.testcase_mocker.restore()
-        super(TestCustodyPeriod, self).tearDown()
+    @browsing
+    def test_intermediate_folder_doesnt_break_default_aq(self, browser):
+        # An intermediate folderish object that doesn't have the respective
+        # field shouldn't break acquisition of the default
+        self.set_custody_period(self.repofolder, 100)
 
-    def _get_term_titles_from_vocabulary(self, voc):
-        return [term.title for term in voc._terms]
+        fti = self.get_fti('opengever.repository.repositoryfolder')
+        fti.allowed_content_types = fti.allowed_content_types + ('Dummy',)
 
-    def test_configured_field_vocabulary_factory_name(self):
-        field = lifecycle.ILifeCycle['custody_period']
-        self.assertEqual(field.vocabularyName,
-                         u'lifecycle_custody_period_vocabulary')
+        dummy = createContentInContainer(self.repofolder, 'Dummy')
+        transaction.commit()
+        browser.login().open(dummy)
 
-    def test_vocabulary(self):
-        vocfactory = getUtility(IVocabularyFactory,
-                                name=u'lifecycle_custody_period_vocabulary')
-        self.assertEqual(vocfactory.option_names, [0, 10, 20, 30])
+        factoriesmenu.add(u'Business Case Dossier')
+        browser.fill({'Title': 'My Dossier'}).save()
+        dossier = browser.context
 
-    def test_vocabulary_in_context(self):
-        vocfactory = getUtility(IVocabularyFactory,
-                                name=u'lifecycle_custody_period_vocabulary')
+        value = self.get_custody_period(dossier)
+        self.assertEqual(100, value)
 
-        request = self.mocker.mock()
-        self.expect(request.get('PATH_INFO', ANY)).result('somepath/++add++type')
 
-        context = self.mocker.mock()
-        self.expect(context.REQUEST).result(request)
-        self.expect(context.custody_period).result(20)
+class TestCustodyPeriodVocabulary(FunctionalTestCase):
 
-        self.replay()
+    def setUp(self):
+        super(TestCustodyPeriodVocabulary, self).setUp()
+        self.repofolder = create(Builder('repository'))
+        self.field = ILifeCycle['custody_period']
 
-        vocabulary = vocfactory(context)
-        self.assertEqual(sorted(self._get_term_titles_from_vocabulary(vocabulary)),
-                         [u'20', u'30'])
+    def get_custody_period(self, obj):
+        return self.field.get(self.field.interface(obj))
 
-    def test_validator(self):
-        request = self.mocker.mock()
-        self.expect(request.get('PATH_INFO', ANY)).result('somepath/++add++type')
+    def set_custody_period(self, obj, value):
+        self.field.set(self.field.interface(obj), value)
+        transaction.commit()
 
-        field = lifecycle.ILifeCycle['custody_period']
+    @browsing
+    def test_custody_period_choices_configurable_via_registry(self, browser):
+        api.portal.set_registry_record(
+            'custody_periods', interface=IBaseCustodyPeriods,
+            value=[u'1', u'2', u'3', u'99']
+        )
+        transaction.commit()
+        browser.login().open(self.repofolder)
+        factoriesmenu.add(u'Business Case Dossier')
+        form_field = browser.find('Custody period (years)')
 
-        context = None
-        view = None
-        widget = None
+        self.assertEqual(
+            [u'1', u'2', u'3', u'99'],
+            form_field.options_values)
 
-        self.replay()
+    @browsing
+    def test_custody_period_default_choices(self, browser):
+        browser.login().open()
+        factoriesmenu.add(u'Business Case Dossier')
+        form_field = browser.find('Custody period (years)')
+        self.assertEqual(
+            ['0', '30', '100', '150'],
+            form_field.options_values)
 
-        validator = getMultiAdapter((context, request, view, field, widget), IValidator)
-        validator.validate(20)
+    @browsing
+    def test_invalid_acquired_value_falls_back_to_all_choices(self, browser):
+        # If vocab is supposed to be restricted, but we find an invalid value
+        # via acquisition, the vocab should fall back to offering all choices
+        invalid_value = 7
+        self.set_custody_period(self.repofolder, invalid_value)
 
-        with TestCase.assertRaises(self, ConstraintNotSatisfied):
-            validator.validate(15)
+        browser.login().open(self.repofolder)
+        factoriesmenu.add(u'Business Case Dossier')
 
-    def test_validator_in_context(self):
-        request = self.mocker.mock()
-        self.expect(request.get('PATH_INFO', ANY)).result(
-            'somepath/++add++type').count(0, None)
+        form_field = browser.find('Custody period (years)')
+        self.assertEqual(
+            ['0', '30', '100', '150'],
+            form_field.options_values)
 
-        context = self.mocker.mock()
-        self.expect(context.REQUEST).result(request).count(0, None)
-        self.expect(context.custody_period).result(20).count(0, None)
-        self.expect(context.aq_inner).result(context).count(0, None)
-        self.expect(context.aq_parent).result(None).count(0, None)
+    @browsing
+    def test_falsy_acquisition_value_falls_back_to_all_choices(self, browser):
+        # If vocab is supposed to be restricted, but we find a  value via
+        # acquisition that is falsy, the vocab should offer all choices
+        # XXX: This probably should check for None instead of falsyness
+        falsy_value = 0
+        self.set_custody_period(self.repofolder, falsy_value)
 
-        field = lifecycle.ILifeCycle['custody_period']
+        browser.login().open(self.repofolder)
+        factoriesmenu.add(u'Business Case Dossier')
 
-        view = None
-        widget = None
+        form_field = browser.find('Custody period (years)')
+        self.assertEqual(
+            ['0', '30', '100', '150'],
+            form_field.options_values)
 
-        self.replay()
+    @browsing
+    def test_aq_value_is_contained_in_choices_if_restricted(self, browser):
+        self.set_custody_period(self.repofolder, 30)
 
-        validator = getMultiAdapter((context, request, view, field, widget), IValidator)
-        validator.validate(20)
-        validator.validate(30)
+        browser.login().open(self.repofolder)
+        factoriesmenu.add(u'Business Case Dossier')
 
-        with TestCase.assertRaises(self, ConstraintNotSatisfied):
-            validator.validate(10)
+        form_field = browser.find('Custody period (years)')
+        self.assertIn('30', form_field.options_values)
 
-    def test_default_value(self):
-        field = lifecycle.ILifeCycle['custody_period']
+    @browsing
+    def test_vocab_is_restricted_if_indicated_by_aq_value(self, browser):
+        self.set_custody_period(self.repofolder, 100)
 
-        portal = self.create_dummy()
-        directlyProvides(portal, ISiteRoot)
+        browser.login().open(self.repofolder)
+        factoriesmenu.add(u'Business Case Dossier')
 
-        default_value = getMultiAdapter(
-            (portal,  # context
-             None,  # request
-             None,  # form
-             field,  # field
-             None,  # Widget
-             ),
-            IValue,
-            name='default')
-        self.assertEqual(default_value.get(), 30)
+        form_field = browser.find('Custody period (years)')
+        self.assertSetEqual(
+            set(['100', '150']),
+            set(form_field.options_values))
 
-    def test_default_value_in_context(self):
-        field = lifecycle.ILifeCycle['custody_period']
+    @browsing
+    def test_acquired_value_is_suggested_as_default(self, browser):
+        self.set_custody_period(self.repofolder, 100)
 
-        context = self.create_dummy(custody_period=10)
-        directlyProvides(context, lifecycle.ILifeCycle)
+        browser.login().open(self.repofolder)
+        factoriesmenu.add(u'Business Case Dossier')
 
-        default_value = getMultiAdapter(
-            (context,  # context
-             None,  # request
-             None,  # form
-             field,  # field
-             None,  # Widget
-             ),
-            IValue,
-            name='default')
-        self.assertEqual(default_value.get(), 10)
+        form_field = browser.find('Custody period (years)')
+
+        self.assertEqual('100', form_field.value)
+        # Default listed first
+        self.assertEqual('100', form_field.options_values[0])
+
+    @browsing
+    def test_restriction_works_in_edit_form(self, browser):
+        self.set_custody_period(self.repofolder, 100)
+
+        browser.login().open(self.repofolder)
+        factoriesmenu.add(u'Business Case Dossier')
+        browser.fill({'Title': 'My Dossier'}).save()
+        transaction.commit()
+
+        browser.click_on('Edit')
+        form_field = browser.find('Custody period (years)')
+        self.assertSetEqual(
+            set(['100', '150']),
+            set(form_field.options_values))
+
+
+class TestCustodyPeriodPropagation(FunctionalTestCase):
+
+    def setUp(self):
+        super(TestCustodyPeriodPropagation, self).setUp()
+        self.repofolder = create(Builder('repository'))
+        self.field = ILifeCycle['custody_period']
+        self.grant('Administrator')
+
+    def get_custody_period(self, obj):
+        return self.field.get(self.field.interface(obj))
+
+    def set_custody_period(self, obj, value):
+        self.field.set(self.field.interface(obj), value)
+        transaction.commit()
+
+    @browsing
+    def test_change_propagates_to_children(self, browser):
+        # Start with a short custody period
+        self.set_custody_period(self.repofolder, 30)
+
+        browser.login().open(self.repofolder)
+        factoriesmenu.add(u'Business Case Dossier')
+        browser.fill({'Title': 'My Dossier'}).save()
+        dossier = browser.context
+
+        value = self.get_custody_period(dossier)
+        # Dossier should have inherited custody period from repofolder
+        self.assertEqual(30, value)
+
+        browser.open(self.repofolder, view='edit')
+        # Increase custody period
+        browser.fill({'Custody period (years)': '100'}).save()
+        transaction.commit()
+
+        value = self.get_custody_period(dossier)
+        # Increased custody period should have propagated to dossier
+        self.assertEqual(100, value)
+
+    @browsing
+    def test_change_doesnt_propagate_if_old_value_still_valid(self, browser):
+        browser.login().open(self.repofolder)
+        factoriesmenu.add(u'Business Case Dossier')
+        browser.fill({
+            'Title': 'My Dossier',
+            'Custody period (years)': '150'}).save()
+        dossier = browser.context
+
+        value = self.get_custody_period(dossier)
+        self.assertEqual(150, value)
+
+        browser.open(self.repofolder, view='edit')
+        browser.fill({'Custody period (years)': '30'}).save()
+        transaction.commit()
+
+        value = self.get_custody_period(dossier)
+        self.assertEqual(150, value)
+
+    @browsing
+    def test_propagation_is_depth_limited(self, browser):
+        """Propagation of custody period is depth limited to 2 levels.
+        Not sure why this was implemented this way, but here we test for it.
+        """
+        # Start with a short custody period
+        self.set_custody_period(self.repofolder, 30)
+        repofolder2 = create(Builder('repository').within(self.repofolder))
+        repofolder3 = create(Builder('repository').within(repofolder2))
+
+        browser.login().open(repofolder3)
+        factoriesmenu.add(u'Business Case Dossier')
+        browser.fill({'Title': 'My Dossier'}).save()
+        dossier = browser.context
+
+        value = self.get_custody_period(dossier)
+        # Dossier should have inherited custody period from repofolder2
+        self.assertEqual(30, value)
+
+        browser.open(self.repofolder, view='edit')
+        # Increase custody period on top level repofolder
+        browser.fill({'Custody period (years)': '100'}).save()
+        transaction.commit()
+
+        # Increased custody period should have propagated to repofolder2, but
+        # not dossier (because of depth limitation)
+        self.assertEqual(100, self.get_custody_period(repofolder2))
+        self.assertEqual(30, self.get_custody_period(dossier))
 
 
 class TestRetentionPeriod(FunctionalTestCase):
