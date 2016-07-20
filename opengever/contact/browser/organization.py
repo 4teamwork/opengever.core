9070c3618db99c73ec511b94d7be0d50668743e5
Philippe Gross <philippe.gross@4teamwork.ch>
2016-07-20 10:09:34 +0200

    Added first version of the organization view.

---
+++ opengever/contact/browser/organization.py
@@ -0,0 +1,21 @@
+from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.interface import implements
+from zope.publisher.interfaces import IPublishTraverse
+from zope.publisher.interfaces.browser import IBrowserView
+
+
+class OrganizationView(BrowserView):
+    """Overview for a Organization SQL object.
+    """
+
+    implements(IBrowserView, IPublishTraverse)
+
+    template = ViewPageTemplateFile('templates/organization.pt')
+
+    def __init__(self, context, request):
+        super(OrganizationView, self).__init__(context, request)
+        self.model = self.context.model
+
+    def __call__(self):
+        return self.template()
