c99ae9bbd0cb589ba7bd12acf751387ae6336a6c
Philippe Gross <philippe.gross@4teamwork.ch>
2016-07-22 20:37:32 +0200

    Add additional tabs persons and organizations to the contactfolder tabbedview.
    The additional tabs are only avaiable when the contact feature flag is enabled.
    This is a temporary solution, because the person and organization tabs will replace the existing local tab, when the contact improvments are finished.

--- opengever/contact/browser/tabbed.py
+++ opengever/contact/browser/tabbed.py
@@ -1,10 +1,35 @@
 from ftw.tabbedview.browser.tabbed import TabbedView
+from opengever.contact import _
+from opengever.contact import is_contact_feature_enabled
 
 
 class ContactFolderTabbedView(TabbedView):
 
     def get_tabs(self):
-        return [
-            {'id': 'local', 'icon': None, 'url': '#', 'class': None},
-            {'id': 'users', 'icon': None, 'url': '#', 'class': None},
+        tabs = [
+            {'id': 'local',
+             'title': _(u'label_local', default=u'Local'),
+             'icon': None,
+             'url': '#',
+             'class': None},
+            {'id': 'users',
+             'title': _(u'label_ogds', default=u'OGDS'),
+             'icon': None,
+             'url': '#',
+             'class': None},
         ]
+
+        if is_contact_feature_enabled():
+            tabs += [
+                {'id': 'persons',
+                 'title': _(u'label_persons', default=u'Persons'),
+                 'icon': None,
+                 'url': '#',
+                 'class': None},
+                {'id': 'organizations',
+                 'title': _(u'label_organizations', default=u'Organizations'),
+                 'icon': None,
+                 'url': '#',
+                 'class': None}]
+
+        return tabs
---
+++ opengever/contact/tests/test_tabbed.py
@@ -0,0 +1,31 @@
+from ftw.builder import Builder
+from ftw.builder import create
+from ftw.testbrowser import browsing
+from opengever.contact.interfaces import IContactSettings
+from opengever.core.testing import toggle_feature
+from opengever.testing import FunctionalTestCase
+
+
+class TestContactFolderTabbedView(FunctionalTestCase):
+
+    def setUp(self):
+        super(TestContactFolderTabbedView, self).setUp()
+        self.contactfolder = create(Builder('contactfolder')
+                                    .titled(u'Kontakte'))
+
+    @browsing
+    def test_shows_local_and_user_tab(self, browser):
+        browser.login().open(self.contactfolder, view='tabbed_view')
+
+        self.assertEquals(
+            ['Local', 'OGDS'],
+            browser.css('.formTab').text)
+
+    @browsing
+    def test_shows_person_and_organization_tab_when_contact_feature_is_enabled(self, browser):
+        toggle_feature(IContactSettings, enabled=True)
+        browser.login().open(self.contactfolder, view='tabbed_view')
+
+        self.assertEquals(
+            ['Local', 'OGDS', 'Persons', 'Organizations'],
+            browser.css('.formTab').text)
