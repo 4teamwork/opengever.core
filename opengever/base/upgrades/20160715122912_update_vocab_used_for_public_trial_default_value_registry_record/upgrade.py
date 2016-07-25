27744f0af8002f641fba066eabd104f93495be32
Lukas Graf <lukas.graf@4teamwork.ch>
2016-07-25 12:16:27 +0200

    Revert back to using a vocabulary factory for public_trial:
    It makes it much easier to keep the vocabs used for the actual field
    and the `public_trial_default` registry record in sync.

--- opengever/base/upgrades/20160715122912_update_vocab_used_for_public_trial_default_value_registry_record/upgrade.py
+++
@@ -1,9 +0,0 @@
-from ftw.upgrade import UpgradeStep
-
-
-class UpdateVocabUsedForPublicTrialDefaultValueRegistryRecord(UpgradeStep):
-    """Update vocab used for public_trial_default_value registry record.
-    """
-
-    def __call__(self):
-        self.install_upgrade_profile()
