cdc156fed993e5c968b02f25d2f0942724182401
David Erni <david.erni@4teamwork.ch>
2016-09-14 09:50:44 +0200

    Add view to determine if officeatwork feature is enabled.

--- opengever/officeatwork/tests/test_feature.py
+++ opengever/officeatwork/tests/test_feature.py
@@ -17,3 +17,22 @@ class TestIsOfficeatworkFeatureEnabled(FunctionalTestCase):
             'is_feature_enabled', False, interface=IOfficeatworkSettings)
 
         self.assertFalse(is_officeatwork_feature_enabled())
+
+
+class TestIsOfficeatworkFeatureEnabledView(FunctionalTestCase):
+
+    def test_true_if_registry_entry_is_true(self):
+        api.portal.set_registry_record(
+            'is_feature_enabled', True, interface=IOfficeatworkSettings)
+
+        feature_view = self.portal.restrictedTraverse(
+            '@@is_officeatwork_feature_enabled')
+        self.assertTrue(feature_view())
+
+    def test_false_if_registry_entry_is_false(self):
+        api.portal.set_registry_record(
+            'is_feature_enabled', False, interface=IOfficeatworkSettings)
+
+        feature_view = self.portal.restrictedTraverse(
+            '@@is_officeatwork_feature_enabled')
+        self.assertFalse(feature_view())
