e9615521a0bca4442d0aae4519d8ee00ca6c056f
David Erni <david.erni@4teamwork.ch>
2016-10-31 11:37:11 +0100

    Implement copy support for participations.

--- opengever/contact/tests/test_participation_unit.py
+++ opengever/contact/tests/test_participation_unit.py
@@ -18,6 +18,22 @@ class TestContactParticipation(unittest2.TestCase):
         create(Builder('contact_participation')
                .having(contact=self.contact, dossier_oguid=Oguid('foo', 1234)))
 
+    def test_copy_participation(self):
+        participation = create(Builder('contact_participation').having(
+            contact=self.contact,
+            dossier_oguid=Oguid('foo', 1234)))
+        role = create(Builder('participation_role').having(
+            participation=participation,
+            role=u'Sch\xf6ff'))
+
+        copied_participation = participation.copy()
+        self.assertEqual(self.contact, copied_participation.contact)
+        self.assertEqual('foo', copied_participation.dossier_admin_unit_id)
+        self.assertEqual(1234, copied_participation.dossier_int_id)
+        self.assertEqual(1, len(copied_participation.roles))
+        copied_role = copied_participation.roles[0]
+        self.assertEqual(u'Sch\xf6ff', copied_role.role)
+
     def test_participation_can_have_multiple_roles(self):
         participation = create(Builder('contact_participation').having(
             contact=self.contact,
@@ -75,6 +91,18 @@ class TestOgdsUserParticipation(unittest2.TestCase):
             ogds_user=peter,
             dossier_oguid=Oguid('foo', 1234)))
 
+    def test_copy_participation(self):
+        peter = create(Builder('ogds_user').id('peter').as_contact_adapter())
+        participation = create(Builder('ogds_user_participation').having(
+            ogds_user=peter,
+            dossier_oguid=Oguid('foo', 1234)))
+
+        copied_participation = participation.copy()
+        self.assertEqual(peter, copied_participation.ogds_user)
+        self.assertEqual('foo', copied_participation.dossier_admin_unit_id)
+        self.assertEqual(1234, copied_participation.dossier_int_id)
+        self.assertEqual(0, len(copied_participation.roles))
+
 
 class TestOrgRoleParticipation(unittest2.TestCase):
 
@@ -110,3 +138,19 @@ class TestOrgRoleParticipation(unittest2.TestCase):
             role=u'Hanswutscht'))
 
         self.assertEquals([role1, role2], participation.roles)
+
+    def test_copy_participation(self):
+        person = create(Builder('person').having(
+            firstname=u'peter', lastname=u'hans'))
+        organization = create(Builder('organization').named('ACME'))
+        orgrole = create(Builder('org_role').having(
+            person=person, organization=organization, function=u'cheffe'))
+        participation = create(Builder('org_role_participation').having(
+            org_role=orgrole,
+            dossier_oguid=Oguid('foo', 1234)))
+
+        copied_participation = participation.copy()
+        self.assertEqual(orgrole, copied_participation.org_role)
+        self.assertEqual('foo', copied_participation.dossier_admin_unit_id)
+        self.assertEqual(1234, copied_participation.dossier_int_id)
+        self.assertEqual(0, len(copied_participation.roles))
