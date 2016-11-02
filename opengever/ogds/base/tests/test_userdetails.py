f0427c73a554b909f4832b065a4db6490f87225d
Philippe Gross <philippe.gross@4teamwork.ch>
2016-11-02 08:59:32 +0100

    No longer use OGBrowser in UserDetails tests, use ftw.testbrowser instead.

--- opengever/ogds/base/tests/test_userdetails.py
+++ opengever/ogds/base/tests/test_userdetails.py
@@ -1,24 +1,24 @@
 from opengever.testing import FunctionalTestCase
+from ftw.testbrowser import browsing
 from opengever.testing import create_ogds_user
 from zExceptions import NotFound
 
 
 class TestUserDetails(FunctionalTestCase):
 
-    use_browser = True
-
-    def test_user_details(self):
+    @browsing
+    def test_user_details(self, browser):
         create_ogds_user('hugo.boss', lastname='Boss', firstname='Hugo',
                          groups=('group_a', 'group_b'))
 
-        self.browser.open('%s/@@user-details/hugo.boss' % self.portal.absolute_url())
+        browser.open(self.portal, view='@@user-details/hugo.boss')
 
-        self.assertEquals('Boss Hugo (hugo.boss)',
-            self.browser.locate('h1.documentFirstHeading').text)
-
+        self.assertEquals(['Boss Hugo (hugo.boss)'],
+                          browser.css('h1.documentFirstHeading').text)
         self.assertEquals(['group_a', 'group_b'],
-            [node.text for node in self.browser.css('.groups a')])
+                          browser.css('.groups a').text)
 
-    def test_user_details_return_not_found_for_not_exisiting_user(self):
+    @browsing
+    def test_user_details_return_not_found_for_not_exisiting_user(self, browser):
         with self.assertRaises(NotFound):
-            self.browser.open('%s/@@user-details/hugo.boss' % self.portal.absolute_url())
+            browser.open(self.portal, view='@@user-details/hugo.boss')
