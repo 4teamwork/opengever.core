7043579f45e377e177d0fa0eeadf524130b4aa62
David Erni <david.erni@4teamwork.ch>
2016-11-17 10:08:44 +0100

    Use correct dossier byline for meeting dossiers.

---
+++ opengever/meeting/tests/test_meeting_dossier_byline.py
@@ -0,0 +1,15 @@
+from datetime import date
+from ftw.builder import Builder
+from ftw.builder import create
+from opengever.dossier.tests.test_dossier_byline import TestDossierByline
+
+
+class TestMeetingDossierByline(TestDossierByline):
+
+    def create_dossier(self):
+        return create(Builder('meeting_dossier')
+                      .in_state('dossier-state-active')
+                      .having(reference_number_prefix='5',
+                              responsible='hugo.boss',
+                              start=date(2013, 11, 6),
+                              end=date(2013, 11, 7)))
