1622264bc0c6804a5cc628fe7ba00f833e82ac65
Lukas Graf <lukas.graf@4teamwork.ch>
2016-11-25 10:34:52 +0100

    Unify schema dumping: Have the dump_schemas bin/instance zopectl handler
    dump both regular JSON schemas and OGGBundle JSON schemas.

--- opengever/base/schemadump/__init__.py
+++ opengever/base/schemadump/__init__.py
@@ -9,10 +9,4 @@ def dump_schemas_zopectl_handler(app, args):
     """
     setup_plone(get_first_plone_site(app))
     dump_schemas()
-
-
-def dump_oggbundle_schemas_zopectl_handler(app, args):
-    """Handler for the 'bin/instance dump_oggbundle_schemas' zopectl command.
-    """
-    setup_plone(get_first_plone_site(app))
     dump_oggbundle_schemas()
