240a73ff65ed3121f7f4ce36e1c2c812a6c5f199
Philippe Gross <philippe.gross@4teamwork.ch>
2016-11-28 10:03:11 +0100

    Allow archivist to see offered and archived dossiers.

---
+++ opengever/dossier/upgrades/20161125103319_let_archivist_see_offered_and_archived_dossiers/upgrade.py
@@ -0,0 +1,20 @@
+from ftw.upgrade import UpgradeStep
+from opengever.dossier.behaviors.dossier import IDossierMarker
+from plone import api
+
+
+class LetArchivistSeeOfferedAndArchivedDossiers(UpgradeStep):
+    """Let archivist see offered and archived dossiers.
+    """
+
+    def __call__(self):
+        self.install_upgrade_profile()
+
+        self.update_workflow_security(
+            ['opengever_dossier_workflow'],
+            reindex_security=self.has_offered_or_archived_dossiers())
+
+    def has_offered_or_archived_dossiers(self):
+        return bool(api.content.find(
+            object_provides=IDossierMarker,
+            review_state=['dossier-state-offered', 'dossier-state-archived']))
