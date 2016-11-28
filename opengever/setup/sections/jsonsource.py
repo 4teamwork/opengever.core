b51297b4511f8a78e9e99098779b14a686397604
David Erni <david.erni@4teamwork.ch>
2016-11-28 17:02:47 +0100

    Add JSON source section that loads json from a file into the pipeline.

---
+++ opengever/setup/sections/jsonsource.py
@@ -0,0 +1,45 @@
+from collective.transmogrifier.interfaces import ISection
+from collective.transmogrifier.interfaces import ISectionBlueprint
+from collective.transmogrifier.utils import defaultMatcher
+from zope.interface import classProvides
+from zope.interface import implements
+import json
+import logging
+import os.path
+
+
+logger = logging.getLogger('opengever.setup.jsonsource')
+
+
+class JSONSourceSection(object):
+    """Injects items from a JSON file into the pipeline."""
+
+    classProvides(ISectionBlueprint)
+    implements(ISection)
+
+    def __init__(self, transmogrifier, name, options, previous):
+        self.previous = previous
+        self.transmogrifier = transmogrifier
+
+        self.key = defaultMatcher(options, 'key', name)
+        self.json_dir = options.get('json_dir')
+        self.filename = options.get('filename')
+
+        self.filepath = os.path.join(self.json_dir, self.filename)
+
+    def read_json_file(self):
+        try:
+            with open(self.filepath, "rb") as file_:
+                return json.load(file_)
+        except IOError:
+            logger.info("Could not read file '{}'. Skipping...".format(
+                self.filepath))
+            return []
+
+    def __iter__(self):
+        for item in self.previous:
+            yield item
+
+        for item in self.read_json_file():
+            item[u'_source'] = self.filename
+            yield item
---
+++ opengever/setup/tests/test_section_jsonsource.py
@@ -0,0 +1,38 @@
+from collective.transmogrifier.interfaces import ISection
+from collective.transmogrifier.interfaces import ISectionBlueprint
+from opengever.setup.sections.jsonsource import JSONSourceSection
+from pkg_resources import resource_filename
+from unittest2 import TestCase
+from zope.interface.verify import verifyClass
+from zope.interface.verify import verifyObject
+
+
+class TestJSONSource(TestCase):
+
+    def setup_source(self, options, previous=None):
+        previous = previous or []
+        options.setdefault('blueprint', 'opengever.setup.jsonsource')
+        return JSONSourceSection(None, '', options, previous)
+
+    def test_implements_interface(self):
+        self.assertTrue(ISection.implementedBy(JSONSourceSection))
+        verifyClass(ISection, JSONSourceSection)
+
+        self.assertTrue(ISectionBlueprint.providedBy(JSONSourceSection))
+        verifyObject(ISectionBlueprint, JSONSourceSection)
+
+    def test_skips_missing_files(self):
+        directory = resource_filename('opengever.setup.tests', 'nope')
+        source = self.setup_source({'json_dir': directory,
+                                    'filename': 'nope.json'},
+                                   previous=['Foo'])
+
+        self.assertEqual(['Foo'], list(source))
+
+    def test_json_file_parsing(self):
+        directory = resource_filename('opengever.setup.tests', 'assets')
+        source = self.setup_source({'json_dir': directory,
+                                    'filename': 'simple.json'})
+        self.assertEqual(
+            [{u'_source': 'simple.json', u'item': u'b\xe4r'}], list(source)
+        )
