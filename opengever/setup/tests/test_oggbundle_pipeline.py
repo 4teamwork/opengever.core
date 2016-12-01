e177c10235e532440e75bc2a913aafb4aca49c95
David Erni <david.erni@4teamwork.ch>
2016-12-01 15:43:15 +0100

    Add tests for repo root creation.

--- opengever/setup/tests/test_oggbundle_pipeline.py
+++ opengever/setup/tests/test_oggbundle_pipeline.py
@@ -1,4 +1,5 @@
 from collective.transmogrifier.transmogrifier import Transmogrifier
+from datetime import date
 from ftw.builder import Builder
 from ftw.builder import create
 from opengever.testing import FunctionalTestCase
@@ -8,15 +9,16 @@ from plone import api
 
 class TestOggBundlePipeline(FunctionalTestCase):
 
+    use_default_fixture = False
+
     def test_oggbundle_transmogrifier(self):
         # this is a bit hackish, but since builders currently don't work in
         # layer setup/teardown and isolation of database content is ensured
         # on a per test level we abuse just one test to setup the pipeline and
         # test its data.
-        # i'd like to this in a fixture instead, but as things are for now
-        # we depend on content
 
         # load pipeline
+        # XXX move this to a layer
         self.grant("Manager")
         user, org_unit, admin_unit = create(
             Builder('fixture').with_all_unit_setup())
@@ -26,3 +28,15 @@ class TestOggBundlePipeline(FunctionalTestCase):
             'opengever.setup.tests', 'assets/oggbundle')
         transmogrifier(u'opengever.setup.oggbundle')
 
+        # test content creation
+        # XXX use separate test-cases based on a layer
+        self.assert_repo_root_created()
+
+    def assert_repo_root_created(self):
+        root = self.portal.get('ordnungssystem')
+        self.assertEqual('Ordnungssystem', root.Title())
+        self.assertEqual(u'Ordnungssystem', root.title_de)
+        self.assertEqual(u'', root.title_fr)
+        self.assertEqual('ordnungssystem', root.getId())
+        self.assertEqual(date(2000, 1, 1), root.valid_from)
+        self.assertEqual(date(2099, 12, 31), root.valid_until)
