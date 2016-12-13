5331ad5ecf80f0fc5e6424cc5666e7e4eb37a9f6
Lukas Graf <lukas.graf@4teamwork.ch>
2016-12-13 16:25:13 +0100

    WIP: BundleLoader: Validate schemas upon loading bundles.

--- opengever/bundle/loader.py
+++ opengever/bundle/loader.py
@@ -1,10 +1,12 @@
 from collections import OrderedDict
+from jsonschema import FormatChecker
+from jsonschema import validate
 import codecs
 import json
 import os
 
 
-PORTAL_TYPES = OrderedDict([
+BUNDLE_JSON_TYPES = OrderedDict([
     ('reporoots.json', 'opengever.repository.repositoryroot'),
     ('repofolders.json', 'opengever.repository.repositoryfolder'),
     ('dossiers.json', 'opengever.dossier.businesscasedossier'),
@@ -13,15 +15,39 @@ PORTAL_TYPES = OrderedDict([
 
 
 class BundleLoader(object):
-    """Loads an OGGBundle from the filesystem, and yields the containing items
-    in their proper order.
+    """Loads an OGGBundle from the filesystem, validates its JSON files against
+    the provided schemas, and yields the containing items in proper order.
 
     The loader also takes care of inserting the appropriate portal_type for
     each item.
+
+    Expects a dictionary of JSON schemas in the form of a {json_name: schema}
+    mapping.
     """
 
-    def __init__(self, bundle_path):
+    def __init__(self, bundle_path, json_schemas):
         self.bundle_path = bundle_path
+        self.json_schemas = json_schemas
+
+        self.load_items()
+
+    def load_items(self):
+        self.items = []
+        for json_name, portal_type in BUNDLE_JSON_TYPES.items():
+            json_path = os.path.join(self.bundle_path, json_name)
+
+            with codecs.open(json_path, 'r', 'utf-8-sig') as json_file:
+                items = json.load(json_file)
+
+            self.validate_schema(items, json_name)
+            for item in items:
+                item['_type'] = self.determine_portal_type(json_name, item)
+                self.items.append(item)
+
+    def validate_schema(self, items, json_name):
+        schema = self.json_schemas[json_name]
+        # May raise jsonschema.ValidationError
+        validate(items, schema, format_checker=FormatChecker())
 
     def determine_portal_type(self, json_name, item):
         """Determine what portal_type an item should be, based on the name of
@@ -32,17 +58,10 @@ class BundleLoader(object):
             if filepath is not None and filepath.endswith('.eml'):
                 return 'ftw.mail.mail'
             return 'opengever.document.document'
-        return PORTAL_TYPES[json_name]
+        return BUNDLE_JSON_TYPES[json_name]
 
     def __iter__(self):
         """Yield all items of the bundle in order.
         """
-        for json_name, portal_type in PORTAL_TYPES.items():
-            json_path = os.path.join(self.bundle_path, json_name)
-
-            with codecs.open(json_path, 'r', 'utf-8-sig') as json_file:
-                items = json.load(json_file)
-
-            for item in items:
-                item['_type'] = self.determine_portal_type(json_name, item)
-                yield item
+        for item in self.items:
+            yield item
