05b7a3fadd2c75685d27bd75eacecf63f46b3a4b
Philippe Gross <philippe.gross@4teamwork.ch>
2016-12-13 14:40:05 +0100

    Creates successor-tasks of forwardings with a default task type.
    To avoid tasks with an invalid task_type.

---
+++ opengever/inbox/tests/test_accept.py
@@ -0,0 +1,36 @@
+from ftw.builder import Builder
+from ftw.builder import create
+from opengever.task.browser.accept.utils import accept_forwarding_with_successor
+from opengever.task.browser.accept.utils import assign_forwarding_to_dossier
+from opengever.testing import FunctionalTestCase
+from plone.app.testing import TEST_USER_ID
+
+
+class TestForwardingAccepting(FunctionalTestCase):
+
+    def test_successor_task_is_created_with_information_task_type(self):
+        inbox = create(Builder('inbox'))
+        dossier = create(Builder('dossier'))
+        forwarding = create(Builder('forwarding')
+                            .having(responsible=TEST_USER_ID,
+                                    issuer='hugo.boss')
+                            .within(inbox))
+        task = accept_forwarding_with_successor(
+            self.portal, forwarding.oguid.id, 'OK, thx.', dossier=dossier)
+
+        self.assertEquals('information', task.task_type)
+
+
+class TestAssignToDossier(FunctionalTestCase):
+
+    def test_successor_task_is_created_with_information_task_type(self):
+        inbox = create(Builder('inbox'))
+        dossier = create(Builder('dossier'))
+        forwarding = create(Builder('forwarding')
+                            .having(responsible=TEST_USER_ID,
+                                    issuer='hugo.boss')
+                            .within(inbox))
+        task = assign_forwarding_to_dossier(
+            self.portal, forwarding.oguid.id, dossier,  'OK, thx.')
+
+        self.assertEquals('information', task.task_type)
