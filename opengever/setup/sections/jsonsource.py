ea85f8e76470b46cc5010521706b12be04dfda77
Lukas Graf <lukas.graf@4teamwork.ch>
2016-12-15 15:09:45 +0100

    Change JSONSource to use BundleLoader.

--- opengever/setup/sections/jsonsource.py
+++ opengever/setup/sections/jsonsource.py
@@ -1,17 +1,10 @@
 from collective.transmogrifier.interfaces import ISection
 from collective.transmogrifier.interfaces import ISectionBlueprint
-from collective.transmogrifier.utils import defaultMatcher
-from jsonschema import FormatChecker
-from jsonschema import validate
-from opengever.base.schemadump import config
-from opengever.base.schemadump.schema import OGGBundleJSONSchemaBuilder
+from opengever.bundle.loader import BundleLoader
+from zope.annotation.interfaces import IAnnotations
 from zope.interface import classProvides
 from zope.interface import implements
-import codecs
-import json
 import logging
-import os.path
-from zope.annotation.interfaces import IAnnotations
 
 
 logger = logging.getLogger('opengever.setup.jsonsource')
@@ -23,13 +16,7 @@ JSON_STATS_KEY = 'opengever.setup.json_stats'
 
 
 class JSONSourceSection(object):
-    """Injects items from a JSON file into the pipeline.
-
-    If a portal_type is specified, also sets the _type for all items.
-
-    If a jsonschema is registered for the portal_type also validates the
-    file content against the schema.
-
+    """Injects items from an OGGBundle into the pipeline.
     """
 
     classProvides(ISectionBlueprint)
@@ -39,15 +26,10 @@ class JSONSourceSection(object):
         self.previous = previous
         self.transmogrifier = transmogrifier
 
-        self.key = defaultMatcher(options, 'key', name)
-        self.filename = options.get('filename')
         if hasattr(transmogrifier, 'bundle_path'):
             self.bundle_path = transmogrifier.bundle_path
         else:
             self.bundle_path = options.get('bundle_path')
-
-        if not os.path.exists(self.bundle_path):
-            raise Exception("Bundle %s not found" % self.bundle_path)
 
         # Store the bundle path in global annotations on the
         # transmogrifier object for use by later sections
@@ -55,47 +37,7 @@ class JSONSourceSection(object):
 
         IAnnotations(transmogrifier)[JSON_STATS_KEY] = {'errors': {}}
 
-        self.portal_type = options.get('portal_type')
-        self.json_schema = self.get_content_type_json_schema()
-
-        self.filepath = os.path.join(self.bundle_path, self.filename)
-
-    def get_content_type_json_schema(self):
-        if not self.portal_type:
-            return None
-        if self.portal_type not in config.GEVER_TYPES:
-            return None
-
-        builder = OGGBundleJSONSchemaBuilder()
-        return builder.build_schema(self.portal_type)
-
-    def read_json_file(self):
-        try:
-            with codecs.open(self.filepath, 'r', 'utf-8-sig') as file_:
-                return json.load(file_)
-        except IOError:
-            logger.info("Could not read file '{}'. Skipping...".format(
-                self.filepath))
-            return []
-
-    def validate(self, data):
-        if not self.json_schema:
-            return
-
-        validate(data, self.json_schema, format_checker=FormatChecker())
-
-    def get_data(self):
-        data = self.read_json_file()
-        self.validate(data)
-        return data
+        self.bundle = BundleLoader(self.bundle_path)
 
     def __iter__(self):
-        logger.info('Yielding items from %s' % self.filename)
-        for item in self.previous:
-            yield item
-
-        for item in self.get_data():
-            item[u'_source'] = self.filename
-            if self.portal_type:
-                item[u'_type'] = self.portal_type
-            yield item
+        return iter(self.bundle)
--- opengever/setup/tests/test_section_jsonsource.py
+++ opengever/setup/tests/test_section_jsonsource.py
@@ -1,6 +1,5 @@
 from collective.transmogrifier.interfaces import ISection
 from collective.transmogrifier.interfaces import ISectionBlueprint
-from jsonschema.exceptions import ValidationError
 from opengever.setup.sections.jsonsource import JSONSourceSection
 from opengever.setup.tests import MockTransmogrifier
 from opengever.testing import FunctionalTestCase
@@ -14,7 +13,7 @@ class TestJSONSource(FunctionalTestCase):
     def setup_source(self, options, bundle_path=None, previous=None):
         previous = previous or []
         bundle_path = bundle_path or resource_filename(
-            'opengever.setup.tests', 'assets')
+            'opengever.bundle.tests', 'assets/basic.oggbundle')
         transmogrifier = MockTransmogrifier()
         transmogrifier.bundle_path = bundle_path
 
@@ -28,50 +27,3 @@ class TestJSONSource(FunctionalTestCase):
 
         self.assertTrue(ISectionBlueprint.providedBy(JSONSourceSection))
         verifyObject(ISectionBlueprint, JSONSourceSection)
-
-    def test_skips_missing_files(self):
-        directory = resource_filename('opengever.setup.tests', 'nope')
-        source = self.setup_source({'bundle_path': directory,
-                                    'filename': 'nope.json'},
-                                   previous=['Foo'])
-
-        self.assertEqual(['Foo'], list(source))
-
-    def test_json_file_parsing(self):
-        source = self.setup_source({'filename': 'simple.json'})
-        self.assertEqual(
-            [{u'_source': 'simple.json', u'item': u'b\xe4r'}], list(source)
-        )
-
-    def test_inserts_portal_type_if_specified(self):
-        source = self.setup_source(
-            {'filename': 'simple.json',
-             'portal_type': 'foo.bar.qux'})
-        self.assertEqual(
-            [{u'item': u'b\xe4r',
-              u'_source': 'simple.json',
-              u'_type': 'foo.bar.qux'}],
-            list(source)
-        )
-
-    def test_validates_portal_type_schema_if_in_supported_types(self):
-        source = self.setup_source(
-            {'filename': 'reporoot.json',
-             'portal_type': 'opengever.repository.repositoryroot'})
-        self.assertEqual([{
-            '_source': 'reporoot.json',
-            '_type': 'opengever.repository.repositoryroot',
-            'guid': '754daf80623f45659004a8c38c78dc3e',
-            'review_state': 'repositoryroot-state-active',
-            'title_de': 'Ordnungssystem',
-            'valid_from': '2000-01-01',
-            'valid_until': '2099-12-31',
-            }],
-            list(source))
-
-    def test_validation_fails_when_files_does_not_meet_schema_spec(self):
-        source = self.setup_source(
-            {'filename': 'simple.json',
-             'portal_type': 'opengever.repository.repositoryroot'})
-        with self.assertRaises(ValidationError):
-            list(source)
