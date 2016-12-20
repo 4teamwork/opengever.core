caba4656104cff5f0f69620c4d7617d1bdefd576
David Erni <david.erni@4teamwork.ch>
2016-12-20 11:37:00 +0100

    Add doc-property ogg.document.document_type for documents.

--- opengever/dossier/tests/test_default_doc_property_adapters.py
+++ opengever/dossier/tests/test_default_doc_property_adapters.py
@@ -32,6 +32,7 @@ class TestDocProperties(FunctionalTestCase):
             .titled("My Document")
             .having(document_date=datetime(2010, 1, 3),
                     document_author=TEST_USER_ID,
+                    document_type='contract',
                     receipt_date=datetime(2010, 1, 3),
                     delivery_date=datetime(2010, 1, 3))
             .with_asset_file('with_gever_properties.docx'))
