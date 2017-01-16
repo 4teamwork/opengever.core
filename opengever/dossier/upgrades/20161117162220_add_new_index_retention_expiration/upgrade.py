58e98793f86663cfc2cadf571e47b4e6274d67d5
Philippe Gross <philippe.gross@4teamwork.ch>
2017-01-16 23:11:31 +0100

    Adjust dossiers retention expiration calculation.
    The retention period of a dossier should expires at the begining of the year after the retention period.

--- opengever/dossier/upgrades/20161117162220_add_new_index_retention_expiration/upgrade.py
+++ opengever/dossier/upgrades/20161117162220_add_new_index_retention_expiration/upgrade.py
@@ -9,12 +9,18 @@ class AddNewIndexRetentionExpiration(UpgradeStep):
     def __call__(self):
         self.install_upgrade_profile()
         self.catalog_add_index('retention_expiration', 'DateIndex')
-        self.reindex_resolved_dossiers()
 
-    def reindex_resolved_dossiers(self):
-        for obj in self.objects(
-                {'object_provides': IDossierMarker.__identifier__,
-                 'review_state': 'dossier-state-resolved'},
-                'Enable exclude from navigation for files'):
 
-            obj.reindexObject(idxs=['retention_expiration'])
+    # The calculation of the retention period has been changed, because of
+    # that there is another upgrade step `UpdateRetentionPeriodCalculation`
+    # which recalculate the retention expiration for every resolved dossier.
+    # Therefore the reindex for those dossiers is not necessary ann will be
+    # done by the newer upgradestep `UpdateRetentionPeriodCalculation`
+
+    # def reindex_resolved_dossiers(self):
+    #     for obj in self.objects(
+    #             {'object_provides': IDossierMarker.__identifier__,
+    #              'review_state': 'dossier-state-resolved'},
+    #             'Enable exclude from navigation for files'):
+
+    #         obj.reindexObject(idxs=['retention_expiration'])
