b7b3d940b9a7d5abb6ffcf19ef2b648efa4ad6e3
Lukas Graf <lukas.graf@4teamwork.ch>
2017-01-16 17:55:45 +0100

    WIP: Fix tests.

--- opengever/bundle/tests/test_guid_resolver.py
+++ opengever/bundle/tests/test_guid_resolver.py
@@ -7,33 +7,21 @@ from unittest2 import TestCase
 
 class TestGUIDResolver(TestCase):
 
-    def setup_resolver(self):
-        resolver = GUIDResolver()
+    def setup_resolver(self, items):
+        resolver = GUIDResolver(items)
         return resolver
 
     def test_requires_guid(self):
-        resolver = self.setup_resolver(
-            items=[{'foo': 1234}]
-        )
-
         with self.assertRaises(MissingGUID):
-            list(resolver)
+            self.setup_resolver(items=[{'foo': 1234}])
 
     def test_prevents_duplicate_guid(self):
-        resolver = self.setup_resolver(
-            items=[{'guid': 1234}, {'guid': 1234}]
-        )
-
         with self.assertRaises(DuplicateGUID):
-            list(resolver)
+            self.setup_resolver(items=[{'guid': 1234}, {'guid': 1234}])
 
     def test_validates_parent_guid(self):
-        resolver = self.setup_resolver(
-            items=[{'guid': 1234, 'parent_guid': 1337}]
-        )
-
         with self.assertRaises(MissingParent):
-            list(resolver)
+            self.setup_resolver(items=[{'guid': 1234, 'parent_guid': 1337}])
 
     def test_reorders_items_parents_before_children(self):
         resolver = self.setup_resolver(items=[
@@ -53,10 +41,7 @@ class TestGUIDResolver(TestCase):
             list(resolver)
         )
 
-    def test_defines_guid_mapping_on_transmogrifier(self):
+    def test_builds_guid_mapping(self):
         item = {'guid': 'marvin'}
         resolver = self.setup_resolver(items=[item])
-        transmogrifier = resolver.transmogrifier
-
-        list(resolver)
-        self.assertEqual(item, transmogrifier.item_by_guid['marvin'])
+        self.assertEqual(item, resolver.item_by_guid['marvin'])
