91a6e853b58809a055b34af98b10f239f1836709
Philippe Gross <philippe.gross@4teamwork.ch>
2017-01-30 15:08:37 +0100

    Reworked appraisal update functionality.
    To makes it easier to implement updating per repository.

--- opengever/disposition/browser/resources/disposition.js
+++ opengever/disposition/browser/resources/disposition.js
@@ -4,7 +4,10 @@
     Controller.call(this);
 
     this.update_appraisal = function(target) {
-      return $.post(target.data('url')).done(function(){
+      var data = {'dossier-id': target.data('intid'),
+                  'should_be_archived': target.data('archive')};
+      var url = $('#disposition_overview').data('appraisal_update_url');
+      return $.post(url, data).done(function(){
         target.toggleClass("active");
         target.siblings().toggleClass("active");
       });
