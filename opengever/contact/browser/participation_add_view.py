5f425812a397398e2f0ac8eb976242f1864dc0b6
David Erni <david.erni@4teamwork.ch>
2017-02-06 15:02:13 +0100

    Use absolute urls for redirections.

--- opengever/contact/browser/participation_add_view.py
+++ opengever/contact/browser/participation_add_view.py
@@ -11,6 +11,11 @@ class ParticipationAddView(BrowserView):
 
     def __call__(self):
         if is_contact_feature_enabled():
-            return self.request.RESPONSE.redirect('add-sql-participation')
+            return self.request.RESPONSE.redirect(
+                self.get_url('add-sql-participation'))
 
-        return self.request.response.redirect('add-plone-participation')
+        return self.request.response.redirect(
+            self.get_url('add-plone-participation'))
+
+    def get_url(self, action):
+        return u"{}/{}".format(self.context.absolute_url(), action)
