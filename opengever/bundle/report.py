b48c267db2077144508e62f89bdf4ec220fcb85a
Lukas Graf <lukas.graf@4teamwork.ch>
2017-02-14 00:10:06 +0100

    OGGBundle reports: Implement basic XLSX report.

--- opengever/bundle/report.py
+++ opengever/bundle/report.py
@@ -1,6 +1,10 @@
 from collections import OrderedDict
+from opengever.base.behaviors.translated_title import ITranslatedTitle
+from opengever.base.behaviors.translated_title import ITranslatedTitleSupport
 from opengever.bundle.loader import BUNDLE_JSON_TYPES
 from opengever.bundle.sections.constructor import BUNDLE_GUID_KEY
+from openpyxl import Workbook
+from openpyxl.styles import Font
 from plone import api
 from zope.annotation import IAnnotations
 import logging
@@ -51,7 +55,11 @@ class DataCollector(object):
     def get_item_info(self, brain, guid):
         obj = brain.getObject()
         path = '/'.join(obj.getPhysicalPath())
-        title = brain.Title
+
+        if ITranslatedTitleSupport.providedBy(obj):
+            title = ITranslatedTitle(obj).title_de
+        else:
+            title = obj.title
         item_info = OrderedDict(
             [('guid', guid), ('path', path), ('title', title)])
         return item_info
@@ -74,3 +82,55 @@ class ASCIISummaryBuilder(object):
             report.append(line)
 
         return '\n'.join(report)
+
+
+class XLSXReportBuilder(object):
+    """Build a detailed report in XLSX format based on report data.
+    """
+
+    def __init__(self, report_data):
+        self.report_data = {}
+        self.report_data.update(report_data)
+
+        # Include mails in documents by moving them over
+        docs = self.report_data.get('opengever.document.document', [])
+        mails = self.report_data.get('ftw.mail.mail', [])
+        self.report_data['opengever.document.document'] = docs + mails
+        self.report_data.pop('ftw.mail.mail', None)
+
+    def build_and_save(self, report_path):
+        workbook = self.build_report()
+        self.save_report(workbook, report_path)
+
+    def save_report(self, workbook, path):
+        with open(path, 'w') as report_xlsx:
+            workbook.save(report_xlsx)
+            log.info("Wrote report to %s" % path)
+        return path
+
+    def _write_row(self, sheet, rownum, values, bold=False):
+        for col_num, value in enumerate(values, 1):
+            cell = sheet.cell(row=rownum + 1, column=col_num)
+            cell.value = value
+            if bold:
+                cell.font = Font(bold=True)
+
+    def build_report(self):
+        workbook = Workbook()
+        workbook.remove_sheet(workbook.active)
+
+        for json_name, portal_type in BUNDLE_JSON_TYPES.items():
+            short_name = json_name.replace('.json', '')
+            log.info("Creating sheet %s" % short_name)
+            sheet = workbook.create_sheet(short_name)
+            sheet.title = short_name
+
+            # Label Row
+            self._write_row(
+                sheet, 0, self.report_data[portal_type][0].keys(), bold=True)
+
+            # Data rows
+            for rownum, info in enumerate(self.report_data[portal_type], 1):
+                self._write_row(sheet, rownum, info.values())
+
+        return workbook
--- opengever/bundle/sections/report.py
+++ opengever/bundle/sections/report.py
@@ -1,12 +1,16 @@
 from collective.transmogrifier.interfaces import ISection
 from collective.transmogrifier.interfaces import ISectionBlueprint
+from opengever.base.pathfinder import PathFinder
 from opengever.bundle.report import ASCIISummaryBuilder
 from opengever.bundle.report import DataCollector
+from opengever.bundle.report import XLSXReportBuilder
 from opengever.bundle.sections.bundlesource import BUNDLE_KEY
 from zope.annotation import IAnnotations
 from zope.interface import classProvides
 from zope.interface import implements
 import logging
+import os
+import tempfile
 
 
 log = logging.getLogger('opengever.bundle.report')
@@ -34,5 +38,22 @@ class ReportSection(object):
         report_data = DataCollector(self.bundle)()
         self.bundle.report_data = report_data
 
+        self.build_ascii_summary(report_data)
+        self.build_xlsx_report(report_data)
+
+    def build_ascii_summary(self, report_data):
         summary = ASCIISummaryBuilder(report_data).build()
         log.info('\n\n%s\n' % summary)
+
+    def build_xlsx_report(self, report_data):
+        try:
+            report_path = os.path.join(PathFinder().var, 'report.xlsx')
+        except RuntimeError:
+            # During tests
+            tmp_file = tempfile.NamedTemporaryFile(
+                delete=False, suffix='.xlsx')
+            tmp_file.close()
+            report_path = tmp_file.name
+
+        xlsx_builder = XLSXReportBuilder(report_data)
+        xlsx_builder.build_and_save(report_path)
