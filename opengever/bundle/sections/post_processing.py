b3ec16329feb2f13a4598aad8d960da8f505729e
Lukas Graf <lukas.graf@4teamwork.ch>
2017-02-13 16:18:49 +0100

    Do intermediate commits during creation of initial versions. This addresses
    a performance issue where the time to write initial versions increases
    exponentially with the number of previous initial versions created in the
    same transaction.

--- opengever/bundle/sections/post_processing.py
+++ opengever/bundle/sections/post_processing.py
@@ -4,10 +4,14 @@ from opengever.document.checkout.handlers import create_initial_version
 from zope.interface import classProvides
 from zope.interface import implements
 import logging
+import transaction
 
 
 log = logging.getLogger('opengever.bundle.post_processing')
 log.setLevel(logging.INFO)
+
+
+INTERMEDIATE_COMMIT_INTERVAL = 1000
 
 
 class PostProcessingSection(object):
@@ -29,6 +33,9 @@ class PostProcessingSection(object):
         self.context = transmogrifier.context
 
     def __iter__(self):
+        self.commit_and_log("Committing transaction before post-processing.")
+        log.info("Transaction committed.")
+
         # Yield all items and collect them, so we can apply post-processing
         # steps *after* all the other sections have been executed
         items_to_post_process = []
@@ -38,6 +45,20 @@ class PostProcessingSection(object):
 
         # Any operations performed here will be applied after all the previous
         # sections have been run for all the items
-        for item in items_to_post_process:
+        for count, item in enumerate(items_to_post_process, start=1):
             log.info("Creating initial version: %s" % item['_path'])
             create_initial_version(item['_object'])
+
+            if count % INTERMEDIATE_COMMIT_INTERVAL == 0:
+                self.commit_and_log(
+                    "Intermediate commit during OGGBundle post-processing. "
+                    "%s of %s items." % (count, len(items_to_post_process)))
+
+        self.commit_and_log(
+            "Final commit after OGGBundle post-processing. "
+            "%s of %s items." % (count, len(items_to_post_process)))
+
+    def commit_and_log(self, msg):
+        transaction.get().note(msg)
+        log.info(msg)
+        transaction.commit()
