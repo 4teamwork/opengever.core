a3168718eb02948bf94987524819bb82bd1b1f7f
Elio Schmutz <e.schmutz@4teamwork.ch>
2017-02-14 17:40:02 +0100

    Move templatedossier forms to templatefolder

--- opengever/dossier/templatedossier/templatedossier.py
+++ opengever/dossier/templatedossier/templatedossier.py
@@ -1,17 +1,5 @@
-from five import grok
 from opengever.base.behaviors.translated_title import TranslatedTitleMixin
-from opengever.base.browser.translated_title import TranslatedTitleAddForm
-from opengever.base.browser.translated_title import TranslatedTitleEditForm
 from opengever.dossier.base import DossierContainer
-from opengever.dossier.templatedossier import ITemplateDossier
-
-
-class TemplateDossierAddForm(TranslatedTitleAddForm):
-    grok.name('opengever.dossier.templatedossier')
-
-
-class TemplateDossierEditForm(TranslatedTitleEditForm):
-    grok.context(ITemplateDossier)
 
 
 class TemplateDossier(DossierContainer, TranslatedTitleMixin):
--- opengever/dossier/templatefolder/templatefolder.py
+++ opengever/dossier/templatefolder/templatefolder.py
@@ -1,5 +1,17 @@
+from five import grok
 from opengever.base.behaviors.translated_title import TranslatedTitleMixin
+from opengever.base.browser.translated_title import TranslatedTitleAddForm
+from opengever.base.browser.translated_title import TranslatedTitleEditForm
 from opengever.dossier.base import DossierContainer
+from opengever.dossier.templatefolder import ITemplateFolder
+
+
+class TemplateFolderAddForm(TranslatedTitleAddForm):
+    grok.name('opengever.dossier.templatefolder')
+
+
+class TemplateFolderEditForm(TranslatedTitleEditForm):
+    grok.context(ITemplateFolder)
 
 
 class TemplateFolder(DossierContainer, TranslatedTitleMixin):
