fcf59d66eb4b0ba0a5facc854714b9e293a85857
Elio Schmutz <e.schmutz@4teamwork.ch>
2017-02-14 09:39:06 +0100

    Rename workflow "opengever_templatedossier_workflow" to "opengever_templatefolder_workflow".

--- opengever/dossier/upgrades/20170214083741_rename_template_dossier_to_template_folder/upgrade.py
+++ opengever/dossier/upgrades/20170214083741_rename_template_dossier_to_template_folder/upgrade.py
@@ -1,4 +1,5 @@
 from ftw.upgrade import UpgradeStep
+from ftw.upgrade.workflow import WorkflowChainUpdater
 from opengever.dossier.interfaces import ITemplateDossierProperties
 from opengever.dossier.interfaces import ITemplateFolderProperties
 from plone import api
@@ -9,11 +10,20 @@ class RenameTemplateDossierToTemplateFolder(UpgradeStep):
     """
 
     def __call__(self):
+        query = {'portal_type': 'opengever.dossier.templatedossier'}
+        objects = self.catalog_unrestricted_search(query, full_objects=True)
+        review_state_mapping = {
+            ('opengever_templatedossier_workflow', 'opengever_templatefolder_workflow'): {
+                'templatedossier-state-active': 'templatefolder-state-active'}
+            }
+
         # Save current ITemplateDossierProperties settings
         create_doc_properties = api.portal.get_registry_record(
             'create_doc_properties', ITemplateDossierProperties)
 
-        self.install_upgrade_profile()
+        with WorkflowChainUpdater(objects, review_state_mapping,
+                                  migrate_workflow_history=False):
+            self.install_upgrade_profile()
 
         # Restore current ITemplateDossierProperties settings
         api.portal.set_registry_record(
