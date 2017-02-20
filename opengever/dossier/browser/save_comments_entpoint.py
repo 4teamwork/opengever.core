299da802ffaf2af60458cd9b88271ba201ae1ed9
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2017-02-20 16:20:45 +0100

    Dossier Note Overlay: Enabnle CSRF protection by optimizing a test case.

--- opengever/dossier/browser/save_comments_entpoint.py
+++ opengever/dossier/browser/save_comments_entpoint.py
@@ -1,14 +1,12 @@
 from opengever.dossier.behaviors.dossier import IDossier
-from plone.protect.interfaces import IDisableCSRFProtection
 from Products.Five.browser import BrowserView
-from zope.interface import alsoProvides
 import json
 
 
 class SaveCommentsEndpoint(BrowserView):
 
     def __call__(self):
-        alsoProvides(self.request, IDisableCSRFProtection)
+        self.request.response.setHeader('Content-Type', 'application/json')
         self.request.response.setHeader('X-Theme-Disabled', 'True')
         self.request.response.setStatus(204)
 
