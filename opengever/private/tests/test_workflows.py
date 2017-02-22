ea485069937c73ee3c9d63c826547bb63b44c19e
David Erni <david.erni@4teamwork.ch>
2017-02-22 15:48:40 +0100

    Allow Administrator to view and update private content.
    Adds view and edit permissions for Administrator to private dossier
    and folder. He already has them for document since that is an
    opengever.document.document and uses the document workflow.

--- opengever/private/tests/test_workflows.py
+++ opengever/private/tests/test_workflows.py
@@ -32,12 +32,23 @@ class TestPrivateFolderWorkflow(FunctionalTestCase):
             browser.login(self.hugo).open(self.folder)
 
     @browsing
-    def test_owner_and_admin_can_add_private_dossiers(self, browser):
+    def test_owner_can_add_private_dossiers(self, browser):
         browser.login().open(self.folder)
         self.assertIn('Private Dossier', factoriesmenu.addable_types())
 
-        browser.login(self.admin).open(self.folder)
-        self.assertIn('Private Dossier', factoriesmenu.addable_types())
+    @browsing
+    def test_admin_cant_add_in_private_content(self, browser):
+        dossier = create(Builder('private_dossier')
+                         .within(self.folder)
+                         .titled(u'Zuz\xfcge'))
+
+        browser.login(self.admin)
+
+        browser.open(self.folder)
+        self.assertFalse(factoriesmenu.visible())
+
+        browser.open(dossier)
+        self.assertFalse(factoriesmenu.visible())
 
     @browsing
     def test_only_owner_and_admin_can_see_private_dossiers(self, browser):
