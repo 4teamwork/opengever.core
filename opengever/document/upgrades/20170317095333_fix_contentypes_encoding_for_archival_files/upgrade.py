56412705cb1aafd42f67bc7f1448873e527b73e2
Philippe Gross <philippe.gross@4teamwork.ch>
2017-03-17 16:27:43 +0100

    Fixed contenttype encoding for archival files.
    The contenttype of the archival_file, automatically generated by Bumblebee, was added by with a wrong contenttype encoding (unicode instead of utf-8). This lead to an ValidationError when editing the document.

---
+++ opengever/document/upgrades/20170317095333_fix_contentypes_encoding_for_archival_files/upgrade.py
@@ -0,0 +1,34 @@
+from ftw.upgrade import UpgradeStep
+from opengever.document.behaviors.metadata import IDocumentMetadata
+from opengever.dossier.interfaces import IDossierResolveProperties
+from plone import api
+
+
+class FixContentypesEncodingForArchivalFiles(UpgradeStep):
+    """Fix contentypes encoding for archival files.
+    """
+
+    def __call__(self):
+        # Skip contentype fixing for installation where the conversion
+        # has not be enabled yet.
+        if not self.is_conversion_enabled():
+            return
+
+        for obj in self.objects(
+                {'object_provides': IDocumentMetadata.__identifier__},
+                'Fix contenttype encoding for archival files'):
+
+            self.fix_contenttype_encoding(obj)
+
+    def is_conversion_enabled(self):
+        return api.portal.get_registry_record(
+            'archival_file_conversion_enabled',
+            interface=IDossierResolveProperties)
+
+    def fix_contenttype_encoding(self, document):
+        if not IDocumentMetadata(document).archival_file:
+            return
+
+        content_type = document.archival_file.contentType
+        if isinstance(content_type, unicode):
+            document.archival_file.contentType = content_type.encode('utf-8')
