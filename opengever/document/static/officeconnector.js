24cbf1107df330d9f9b324ba2dd1f777fed22674
Joni Orponen <j.orponen@4teamwork.ch>
2017-03-25 10:49:07 +0100

    Do not use a js global variable for the OC API JQuery AJAX config.

--- opengever/document/static/officeconnector.js
+++ opengever/document/static/officeconnector.js
@@ -3,17 +3,22 @@ function openOfficeconnector(data) {
     window.location = data['url'];
 }
 
-var officeconnector_config = {
-    headers: { Accept: 'application/json' },
-    url: ''
-}
-
 function officeConnectorCheckout(url) {
+    var officeconnector_config = {}
+
     officeconnector_config.url = url;
+    officeconnector_config.headers = {};
+    officeconnector_config.headers['Accept'] = 'application/json';
+
     $.ajax(officeconnector_config).done(openOfficeconnector);
 }
 
 function officeConnectorAttach(url) {
+    var officeconnector_config = {}
+
     officeconnector_config.url = url;
+    officeconnector_config.headers = {};
+    officeconnector_config.headers['Accept'] = 'application/json';
+
     $.ajax(officeconnector_config).done(openOfficeconnector);
 }
