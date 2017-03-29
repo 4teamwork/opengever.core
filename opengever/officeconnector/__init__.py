1eda9aabe209a2f4fc5267aa9c92e432406dded8
Joni Orponen <j.orponen@4teamwork.ch>
2017-03-29 12:14:49 +0200

    Add/fix/move missing/unnecessary/lost translations.

--- opengever/officeconnector/__init__.py
+++ opengever/officeconnector/__init__.py
@@ -1,5 +1,1 @@
 """A service for newstyle officeconnector URL integrations."""
-
-from zope.i18nmessageid import MessageFactory
-
-_ = MessageFactory('opengever.officeconnector')
--- opengever/officeconnector/interfaces.py
+++ opengever/officeconnector/interfaces.py
@@ -1,4 +1,3 @@
-from opengever.officeconnector import _
 from zope import schema
 from zope.interface import Interface
 
@@ -6,19 +5,15 @@ from zope.interface import Interface
 class IOfficeConnectorSettings(Interface):
 
     attach_to_outlook_enabled = schema.Bool(
-        title=_(u'label_enable_officeconnector_attach_to_outlook',
-                default=u'OfficeConnector Outlook support'),
-        description=_(u'label_enable_officeconnector_attach_to_outlook_description', # noqa
-                      default=(u'Enable attach to Outlook with the new style '
-                               u'OfficeConnector URLs')),
+        title=u'OfficeConnector Outlook support',
+        description=u'Enable attach to Outlook with the new style '
+        u'OfficeConnector URLs',
         default=False)
 
     direct_checkout_and_edit_enabled = schema.Bool(
-        title=_(u'label_enable_officeconnector_direct_checkout_and_edit',
-                default=u'OfficeConnector direct checkout suppport'),
-        description=_(u'label_enable_officeconnector_direct_checkout_and_edit_description', # noqa
-                      default=(u'Enable direct checkout and edit with the '
-                               u'new style OfficeConnector URLs')),
+        title=u'OfficeConnector direct checkout suppport',
+        description=u'Enable direct checkout and edit with the new style '
+        u'OfficeConnector URLs',
         default=False)
 
 
