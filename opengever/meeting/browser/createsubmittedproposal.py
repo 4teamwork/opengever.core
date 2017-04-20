b78af573e1d8363537142f8666c28d3b493dd8ab
Jonas Baumann <jone@jone.ch>
2017-04-20 14:11:01 +0200

    copy file to submitted proposal.

--- opengever/meeting/browser/createsubmittedproposal.py
+++ opengever/meeting/browser/createsubmittedproposal.py
@@ -2,9 +2,11 @@ from five import grok
 from opengever.base.oguid import Oguid
 from opengever.base.security import elevated_privileges
 from opengever.base.transport import REQUEST_KEY
+from opengever.meeting.proposal import ISubmittedProposal
 from opengever.meeting.proposal import SubmittedProposal
 from opengever.meeting.service import meeting_service
 from Products.CMFPlone.interfaces import IPloneSiteRoot
+import base64
 import json
 
 
@@ -22,6 +24,11 @@ class CreateSubmittedProposal(grok.View):
 
         with elevated_privileges():
             submitted_proposal = SubmittedProposal.create(proposal, committee)
+            ISubmittedProposal(submitted_proposal).file = (
+                ISubmittedProposal['file']._type(
+                    filename=data['file']['filename'],
+                    contentType=data['file']['contentType'],
+                    data=base64.decodestring(data['file']['data'])))
 
             self.request.response.setHeader("Content-type", "application/json")
             return json.dumps(
