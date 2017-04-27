a50a406df9b788477e63c488268b31cd6db6097f
Philippe Gross <philippe.gross@4teamwork.ch>
2017-04-27 07:56:59 +0200

    Move portlet configuration for Contactfolder to initial content json
    instead of using a setuphandler.

--- opengever/setup/hooks.py
+++ opengever/setup/hooks.py
@@ -17,7 +17,6 @@ def default_installed(site):
 def default_content_installed(site):
     assign_default_navigation_portlet(site, 'eingangskorb')
     assign_default_navigation_portlet(site, 'vorlagen')
-    assign_default_navigation_portlet(site, 'kontakte')
 
 
 def set_global_roles(site):
