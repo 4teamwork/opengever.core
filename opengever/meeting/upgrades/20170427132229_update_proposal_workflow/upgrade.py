722349ce654425e03f9d80e15fe95f4a5ddf2869
Jonas Baumann <jone@jone.ch>
2017-04-28 10:21:15 +0200

    update proposal workflow upgrade step to change state.
    Change worfklow state of existing proposals when they are not pending.
    This is needed so that old objects are consistent with the new behavior.
    This can be applied regardless of the word meeting feature state.

--- opengever/meeting/upgrades/20170427132229_update_proposal_workflow/upgrade.py
+++ opengever/meeting/upgrades/20170427132229_update_proposal_workflow/upgrade.py
@@ -1,4 +1,6 @@
 from ftw.upgrade import UpgradeStep
+from ftw.upgrade.helpers import update_security_for
+from plone import api
 
 
 class UpdateProposalWorkflow(UpgradeStep):
@@ -7,5 +9,14 @@ class UpdateProposalWorkflow(UpgradeStep):
 
     def __call__(self):
         self.install_upgrade_profile()
-        self.update_workflow_security(['opengever_proposal_workflow'],
-                                      reindex_security=False)
+        map(self.update_object,
+            self.objects({'portal_type': 'opengever.meeting.proposal'},
+                         'Update proposal workflow security.'))
+
+    def update_object(self, proposal):
+        model = proposal.load_model()
+        if model.get_state() == model.STATE_PENDING:
+            update_security_for(proposal, reindex_security=False)
+        else:
+            api.content.transition(obj=proposal,
+                                   to_state='proposal-state-submitted')
