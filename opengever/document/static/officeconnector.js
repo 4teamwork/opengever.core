14976b746db8d1116008e3764be0552731f47dbf
Kevin Bieri <k.bieri@4teamwork.ch>
2017-05-03 15:21:36 +0200

    Improve UX for office connector interactions.
    Closes https://github.com/4teamwork/opengever.core/issues/2876
    
    In order to give the user a feedback poll the document state to determine
    if the office connector has completely loaded.

--- opengever/document/static/officeconnector.js
+++ opengever/document/static/officeconnector.js
@@ -1,3 +1,71 @@
+(function($) {
+
+    "use strict";
+
+    function poll(fn, options) {
+
+        options = $.extend({
+            interval: 2000,
+            timeout: 15 * 1000
+        }, options);
+
+        var promise = $.Deferred();
+        var endTime = new Date().getTime() + options.timeout;
+
+        function checkCondition() {
+            $.when(fn()).done(function(proceed) {
+                if(proceed) {
+                    promise.resolve();
+                } else if(new Date().getTime() < endTime) {
+                    setTimeout(checkCondition, options.interval);
+                } else {
+                    promise.reject(new Error("Polling timed out using timeout of: " + options.timeout + "ms"));
+                }
+            });
+        }
+
+        checkCondition();
+
+        return promise;
+    }
+
+    function requestJSON(url) {
+        return $.ajax(url, {
+            headers: {
+                Accept: "application/json",
+            }
+        });
+    }
+
+    function isDocumentCheckedOut(documentURL) {
+        return requestJSON(documentURL + "/status").pipe(function(data) {
+            return JSON.parse(data)["checked_out"];
+        });
+    }
+
+    function checkout(trigger, documentURL) {
+        if(trigger.hasClass("oc-loading")) {
+            return false;
+        }
+        trigger.addClass("oc-loading");
+        return poll(function() {
+            return isDocumentCheckedOut(documentURL)
+        });
+    }
+
+    $(document).on("click", ".oc-checkout", function(event) {
+        var checkoutButton = $(event.currentTarget);
+        var documentURL = checkoutButton.data("document-url");
+        var checkoutRequest = checkout(checkoutButton, documentURL);
+        checkoutRequest.done(function() {
+            location.reload();
+        }).always(function() {
+            checkoutButton.removeClass("oc-loading");
+        });
+    });
+
+})(window.jQuery);
+
 function openOfficeconnector(data) {
     // URLs the browser does not handle get passed onto the OS
     window.location = data['url'];
