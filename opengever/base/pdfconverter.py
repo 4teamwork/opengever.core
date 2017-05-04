512880a8296d84bcced91b1aa5a09a470018616b
Lukas Graf <lukas.graf@4teamwork.ch>
2017-05-04 21:57:21 +0200

    TEMP: Adding more debug output to debug flaky tests

--- opengever/base/pdfconverter.py
+++ opengever/base/pdfconverter.py
@@ -5,10 +5,16 @@ from opengever.bumblebee import is_bumblebee_feature_enabled
 try:
     pkg_resources.get_distribution('opengever.pdfconverter')
 except pkg_resources.DistributionNotFound:
+    print("Failed to import opengever.pdfconverter, setting "
+          "PDFCONVERTER_AVAILABLE to False")
     PDFCONVERTER_AVAILABLE = False
 else:
+    print("Successfully imported opengever.pdfconverter, setting "
+          "PDFCONVERTER_AVAILABLE to True")
+
     PDFCONVERTER_AVAILABLE = True
 
 
 def is_pdfconverter_enabled():
+    print ("PDFCONVERTER_AVAILABLE: %r") % PDFCONVERTER_AVAILABLE
     return PDFCONVERTER_AVAILABLE and not is_bumblebee_feature_enabled()
