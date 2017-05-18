9ccf1035c3b0bf3bb264a4d4b2ea95b5acb21a03
David Erni <david.erni@4teamwork.ch>
2017-05-18 14:11:39 +0200

    Extract query class for Notification.

--- opengever/activity/model/query.py
+++ opengever/activity/model/query.py
@@ -1,8 +1,28 @@
-from opengever.ogds.models.query import BaseQuery
 from opengever.activity.model import Activity
+from opengever.activity.model import Notification
+from opengever.activity.model.subscription import Subscription
+from opengever.ogds.models.query import BaseQuery
 
 
 class ActivityQuery(BaseQuery):
     pass
 
 Activity.query_cls = ActivityQuery
+
+
+class NotificationQuery(BaseQuery):
+
+    def by_user(self, userid):
+        return self.filter_by(userid=userid)
+
+    def by_subscription_roles(self, roles, activity):
+        subscriptions = Subscription.query.by_resource_and_role(
+            activity.resource, roles)
+        user_ids = []
+        for subscription in subscriptions:
+            user_ids += subscription.watcher.get_user_ids()
+
+        return self.filter_by(activity_id=activity.id).filter(
+            Notification.userid.in_(user_ids))
+
+Notification.query_cls = NotificationQuery
