84f7634302a0ff982e91809a8be4e23786105f0f
David Erni <david.erni@4teamwork.ch>
2017-06-14 14:42:02 +0200

    Extract functional part from agenda item tests.

--- opengever/meeting/tests/test_unit_agenda_item.py
+++ opengever/meeting/tests/test_unit_agenda_item.py
@@ -1,15 +1,68 @@
 from ftw.builder import Builder
 from ftw.builder import create
+from opengever.core.testing import OPENGEVER_FUNCTIONAL_MEETING_LAYER
+from opengever.testing import FunctionalTestCase
 from opengever.testing import MEMORY_DB_LAYER
 from unittest2 import TestCase
 
 
-class TestUnitAgendaItem(TestCase):
+class TestProposalAgendaItem(FunctionalTestCase):
+
+    layer = OPENGEVER_FUNCTIONAL_MEETING_LAYER
+
+    def setUp(self):
+        super(TestProposalAgendaItem, self).setUp()
+        self.admin_unit.public_url = 'http://nohost/plone'
+
+        self.repository_root, self.repository_folder = create(
+            Builder('repository_tree'))
+        self.dossier = create(
+            Builder('dossier').within(self.repository_folder))
+        self.meeting_dossier = create(
+            Builder('meeting_dossier').within(self.repository_folder))
+        self.container = create(Builder('committee_container'))
+        self.committee = create(Builder('committee').within(self.container))
+        self.meeting = create(Builder('meeting')
+                              .having(committee=self.committee.load_model())
+                              .link_with(self.meeting_dossier))
+        self.proposal = create(Builder('proposal')
+                               .within(self.dossier)
+                               .having(title=u'Pr\xf6posal',
+                                       committee=self.committee.load_model()))
+        self.submitted_proposal = create(
+            Builder('submitted_proposal').submitting(self.proposal))
+        self.agenda_item = create(
+            Builder('agenda_item')
+            .having(proposal=self.proposal.load_model(),
+                    meeting=self.meeting))
+
+    def test_title_is_proposal_title_when_proposal_is_available(self):
+        self.assertEqual(
+            self.submitted_proposal.title,
+            self.agenda_item.get_title())
+
+    def test_proposal_title_correctly_contains_number(self):
+        self.assertEqual(
+            u'1. Pr\xf6posal',
+            self.agenda_item.get_title(include_number=True))
+
+    def test_serialize_proposal_agenda_item(self):
+        self.assertEqual(
+            {'css_class': 'proposal',
+             'number': '1.',
+             'id': 1,
+             'title': u'Pr\xf6posal',
+             'has_proposal': True,
+             'link': u'<a href="http://nohost/plone/opengever-meeting-committeecontainer/committee-1/submitted-proposal-1" title="Pr\xf6posal">Pr\xf6posal</a>'}, # noqa
+            self.agenda_item.serialize())
+
+
+class TestSimpleAgendaItem(TestCase):
 
     layer = MEMORY_DB_LAYER
 
     def setUp(self):
-        super(TestUnitAgendaItem, self).setUp()
+        super(TestSimpleAgendaItem, self).setUp()
         self.session = self.layer.session
 
         self.committee = create(Builder('committee_model'))
@@ -24,23 +77,10 @@ class TestUnitAgendaItem(TestCase):
                 submitted_physical_path='meetings/proposal-1'))
 
         self.proposal.submitted_admin_unit_id = 'fd'
-        self.proposal_agenda_item = create(
-            Builder('agenda_item')
-            .having(proposal=self.proposal,
-                    meeting=self.meeting))
         self.simple_agenda_item = create(
             Builder('agenda_item')
             .having(title=u'Simple',
                     meeting=self.meeting))
-
-    def test_test_title_is_proposal_title_when_proposal_is_available(self):
-        self.assertEqual(
-            self.proposal.title, self.proposal_agenda_item.get_title())
-
-    def test_proposal_title_correctly_contains_number(self):
-        self.assertEqual(
-            u'1. Pr\xf6posal',
-            self.proposal_agenda_item.get_title(include_number=True))
 
     def test_title_falls_back_to_agenda_item_title(self):
         self.assertEqual(u'Simple', self.simple_agenda_item.get_title())
@@ -69,15 +109,6 @@ class TestUnitAgendaItem(TestCase):
                           'link': u'Simple',
                           },
                          self.simple_agenda_item.serialize())
-
-    def test_serialize_proposal_agenda_item(self):
-        self.assertEqual({'css_class': 'proposal',
-                          'number': '1.',
-                          'id': 1,
-                          'title': u'Pr\xf6posal',
-                          'has_proposal': True,
-                          'link': u'<a href="http://example.com/public/meetings/proposal-1" title="Pr\xf6posal">Pr\xf6posal</a>'},
-                         self.proposal_agenda_item.serialize())
 
     def test_reopen_is_only_possible_if_agenda_item_is_decided(self):
         self.assertFalse(
