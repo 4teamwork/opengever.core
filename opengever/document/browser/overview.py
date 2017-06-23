f1ad6abecae17831fb6589b5e6c01e3d5d7ee11a
Pablo Vergés <pablo.verges@4teamwork.ch>
2017-06-23 15:14:07 +0200

    Add related documents method to document overview
    ... also improve overview's `get_css_class` method.
    
    When adding / editing a document (let's say A), related documents
    (sat B and C) can be selected. These are listed under "related
    documents" in the document overview (A lists B and C)..
    Those related objects should list the documents which are related to
    them as well (B and C should display A as well).
    
    See also: #1498

--- opengever/document/browser/overview.py
+++ opengever/document/browser/overview.py
@@ -1,4 +1,5 @@
 from AccessControl import getSecurityManager
+from Acquisition import aq_inner
 from five import grok
 from ftw import bumblebee
 from opengever.base import _ as ogbmf
@@ -23,9 +24,11 @@ from plone.i18n.normalizer.interfaces import IIDNormalizer
 from Products.CMFCore.utils import getToolByName
 from Products.MimetypesRegistry.common import MimeTypeException
 from z3c.form.browser.checkbox import SingleCheckBoxWidget
+from zc.relation.interfaces import ICatalog
 from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile
 from zope.component import getUtility
 from zope.component import queryMultiAdapter
+from zope.intid.interfaces import IIntIds
 
 
 class BaseRow(object):
@@ -133,6 +136,8 @@ class Overview(DisplayForm, GeverTabMixin, ActionButtonRendererMixin):
     show_searchform = False
 
     file_template = ViewPageTemplateFile('templates/file.pt')
+    related_documents_template = ViewPageTemplateFile(
+        'templates/related_documents.pt')
     archival_file_template = ViewPageTemplateFile('templates/archiv_file.pt')
     public_trial_template = ViewPageTemplateFile('templates/public_trial.pt')
     submitted_with_template = ViewPageTemplateFile('templates/submitted_with.pt')  # noqa
@@ -155,7 +160,8 @@ class Overview(DisplayForm, GeverTabMixin, ActionButtonRendererMixin):
             FieldRow('IDocumentMetadata.preserved_as_paper'),
             FieldRow('IDocumentMetadata.receipt_date'),
             FieldRow('IDocumentMetadata.delivery_date'),
-            FieldRow('IRelatedDocuments.relatedItems'),
+            TemplateRow(self.related_documents_template, label=_(
+                u'label_related_documents', default=u'Related Documents')),
             FieldRow('IClassification.classification'),
             FieldRow('IClassification.privacy_layer'),
             TemplateRow(self.public_trial_template,
@@ -193,6 +199,22 @@ class Overview(DisplayForm, GeverTabMixin, ActionButtonRendererMixin):
                     submitted_document.document_id
                     )
 
+    def linked_documents(self):
+        """Returns a list documents related to the context document."""
+        catalog = getUtility(ICatalog)
+        doc_id = getUtility(IIntIds).getId(aq_inner(self.context))
+        related_objects = self.context.related_items() + [
+            r.from_object
+            for r in catalog.findRelations(
+                {'to_id': doc_id, 'from_attribute': 'relatedItems'}
+            )
+        ]
+        return [{
+            'class': self.get_css_class(obj),
+            'title': obj.Title(),
+            'url': obj.absolute_url(),
+        } for obj in sorted(related_objects, key=lambda obj: obj.Title())]
+
     def is_outdated(self, submitted_document):
         return not submitted_document.is_up_to_date(self.context)
 
@@ -220,8 +242,8 @@ class Overview(DisplayForm, GeverTabMixin, ActionButtonRendererMixin):
             return Actor.user(manager.get_checked_out_by()).get_link()
         return ''
 
-    def get_css_class(self):
-        return get_css_class(self.context)
+    def get_css_class(self, context=None):
+        return get_css_class(context or self.context)
 
     def show_modfiy_public_trial_link(self):
         try:
