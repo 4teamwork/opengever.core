46a18c023249840a50c5f4aa4fba3396778ff473
Elio Schmutz <e.schmutz@4teamwork.ch>
2017-06-27 10:44:06 +0200

    Activate relateddocuments-proxy for opengever.task.task

---
+++ opengever/task/upgrades/20170627091144_use_task_related_documents_proxy_view/upgrade.py
@@ -0,0 +1,32 @@
+from ftw.upgrade import UpgradeStep
+from plone import api
+
+
+class UseRelatedDocumentsProxyView(UpgradeStep):
+    """Use related documents proxy view.
+    """
+
+    old_action_id = 'relateddocuments'
+    new_action_id = 'relateddocuments-proxy'
+
+    type_names = ['opengever.task.task']
+
+    def __call__(self):
+        self.install_upgrade_profile()
+        self.change_relateddocuments_tab_to_relateddocuments_proxy_tab()
+
+    def change_relateddocuments_tab_to_relateddocuments_proxy_tab(self):
+        """To choose which template the user has selected before (list or gallery)
+        with activated bumblebeefeature, we have to add a proxy view.
+
+        The tabs are stored in actions. So we have to update them to the new
+        proxy-view.
+        """
+        ttool = api.portal.get_tool('portal_types')
+        for type_name in self.type_names:
+            fti = ttool[type_name]
+            for action in fti._actions:
+                if not action.id == self.old_action_id:
+                    continue
+
+                action.id = self.new_action_id
