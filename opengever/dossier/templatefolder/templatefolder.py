0f56c20bb539b79de1b1901db69d741a4853aa7b
Elio Schmutz <e.schmutz@4teamwork.ch>
2017-06-29 17:05:47 +0200

    Refactor allowedContentTypes for dossiers and templatefolder

--- opengever/dossier/templatefolder/templatefolder.py
+++ opengever/dossier/templatefolder/templatefolder.py
@@ -1,11 +1,14 @@
+from Acquisition import aq_inner
+from Acquisition import aq_parent
 from five import grok
 from opengever.base.behaviors.translated_title import TranslatedTitleMixin
 from opengever.base.browser.translated_title import TranslatedTitleAddForm
 from opengever.base.browser.translated_title import TranslatedTitleEditForm
+from opengever.dossier.dossiertemplate import is_dossier_template_feature_enabled
 from opengever.dossier.templatefolder import ITemplateFolder
+from opengever.meeting import is_meeting_feature_enabled
+from opengever.meeting import is_word_meeting_implementation_enabled
 from plone.dexterity.content import Container
-from Acquisition import aq_parent
-from Acquisition import aq_inner
 
 
 class TemplateFolderAddForm(TranslatedTitleAddForm):
@@ -24,3 +27,21 @@ class TemplateFolder(Container, TranslatedTitleMixin):
     def is_subtemplatefolder(self):
         parent = aq_parent(aq_inner(self))
         return bool(ITemplateFolder.providedBy(parent))
+
+    def allowedContentTypes(self, *args, **kwargs):
+        types = super(TemplateFolder, self).allowedContentTypes(*args, **kwargs)
+
+        def filter_type(fti):
+            factory_type = fti.id
+            if factory_type in [u'opengever.meeting.sablontemplate']:
+                return is_meeting_feature_enabled()
+
+            if factory_type in [u'opengever.meeting.proposaltemplate']:
+                return is_word_meeting_implementation_enabled()
+
+            if factory_type in [u'opengever.dossier.dossiertemplate']:
+                return is_dossier_template_feature_enabled()
+
+            return True
+
+        return filter(filter_type, types)
