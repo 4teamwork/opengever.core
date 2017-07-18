1ef598881faf0d7397d9c6214f82b0506a31e9f5
Jonas Baumann <jone@jone.ch>
2017-07-18 10:28:00 +0200

    Rewrite dossier/test_deactivate as integration test.

--- opengever/dossier/tests/test_deactivate.py
+++ opengever/dossier/tests/test_deactivate.py
@@ -3,120 +3,88 @@ from datetime import datetime
 from ftw.builder import Builder
 from ftw.builder import create
 from ftw.testbrowser import browsing
-from ftw.testbrowser.pages.statusmessages import error_messages
+from ftw.testbrowser.pages import editbar
+from ftw.testbrowser.pages import statusmessages
 from ftw.testing import freeze
+from opengever.document.interfaces import ICheckinCheckoutManager
 from opengever.dossier.behaviors.dossier import IDossier
-from opengever.testing import FunctionalTestCase
-from plone import api
-from plone.protect import createToken
+from opengever.testing import IntegrationTestCase
+from zope.component import getMultiAdapter
 
 
-class TestDossierDeactivation(FunctionalTestCase):
-
-    def setUp(self):
-        super(TestDossierDeactivation, self).setUp()
-        self.dossier = create(Builder('dossier'))
+class TestDossierDeactivation(IntegrationTestCase):
 
     @browsing
     def test_fails_with_resolved_subdossier(self, browser):
-        create(Builder('dossier')
-               .within(self.dossier)
-               .titled(u'b\xe4\xe4\xe4h')
-               .in_state('dossier-state-resolved'))
-
-        browser.login().open(self.dossier, view='transition-deactivate',
-                             data={'_authenticator': createToken()})
-        self.assertEqual(
-            u"The Dossier can't be deactivated, the subdossier b\xe4\xe4\xe4h is already resolved",
-            error_messages()[0])
+        self.login(self.dossier_responsible, browser)
+        self.set_workflow_state('dossier-state-resolved', self.subdossier)
+        browser.open(self.dossier, view='transition-deactivate',
+                     send_authenticator=True)
+        self.assert_workflow_state('dossier-state-active', self.dossier)
+        statusmessages.assert_message(
+            u"The Dossier can\'t be deactivated,"
+            u" the subdossier 2016 is already resolved")
 
     @browsing
     def test_fails_with_checked_out_documents(self, browser):
-        create(Builder('document')
-               .within(self.dossier)
-               .checked_out())
-
-        browser.login().open(self.dossier, view='transition-deactivate',
-                             data={'_authenticator': createToken()})
-        self.assertEqual(
-            u"The Dossier can't be deactivated, not all containeddocuments "
-            "are checked in.",
-            error_messages()[0])
+        self.login(self.dossier_responsible, browser)
+        getMultiAdapter((self.document, self.request),
+                        ICheckinCheckoutManager).checkout()
+        browser.open(self.dossier, view='transition-deactivate',
+                     send_authenticator=True)
+        self.assert_workflow_state('dossier-state-active', self.dossier)
+        statusmessages.assert_message(
+            u"The Dossier can't be deactivated, not all containeddocuments"
+            u" are checked in.")
 
     @browsing
     def test_not_possible_with_not_closed_tasks(self, browser):
-        create(Builder('task')
-               .within(self.dossier)
-               .in_state('task-state-in-progress'))
-
-        browser.login().open(self.dossier,
-                             view='transition-deactivate',
-                             data={'_authenticator': createToken()})
-
-        self.assertEqual('dossier-state-active',
-                         api.content.get_state(self.dossier))
-
-        self.assertEqual(
-            u"The Dossier can't be deactivated, not all contained "
-            "tasks are in a closed state.",
-            error_messages()[0])
+        self.login(self.dossier_responsible, browser)
+        self.assert_workflow_state('task-state-in-progress', self.task)
+        browser.open(self.dossier, view='transition-deactivate',
+                     send_authenticator=True)
+        self.assert_workflow_state('dossier-state-active', self.dossier)
+        statusmessages.assert_message(
+            u"The Dossier can't be deactivated, not all contained"
+            u" tasks are in a closed state.")
 
     @browsing
     def test_not_possible_with_active_proposals(self, browser):
-        repo = create(Builder('repository'))
-        dossier = create(Builder('dossier').within(repo))
-        create(Builder('proposal').within(dossier))
-
-        browser.login().open(dossier,
-                             view='transition-deactivate',
-                             data={'_authenticator': createToken()})
-
-        self.assertEqual('dossier-state-active',
-                         api.content.get_state(dossier))
-        self.assertEqual(
-            u"The Dossier can't be deactivated, it contains active proposals.",
-            error_messages()[0])
+        self.login(self.dossier_responsible, browser)
+        self.assert_workflow_state('proposal-state-active', self.proposal)
+        browser.open(self.dossier, view='transition-deactivate',
+                     send_authenticator=True)
+        self.assert_workflow_state('dossier-state-active', self.dossier)
+        statusmessages.assert_message(
+            u"The Dossier can't be deactivated,"
+            u" not all contained tasks are in a closed state.")
 
     @browsing
     def test_recursively_deactivate_subdossier(self, browser):
-        subdossier = create(Builder('dossier').within(self.dossier))
-        subsubdossier = create(Builder('dossier').within(subdossier))
-        create(Builder('task')
-               .within(self.dossier)
-               .in_state('task-state-tested-and-closed'))
-
-        browser.login().open(self.dossier, view='transition-deactivate',
-                             data={'_authenticator': createToken()})
-
-        self.assertEqual('dossier-state-inactive',
-                         api.content.get_state(self.dossier))
-        self.assertEqual('dossier-state-inactive',
-                         api.content.get_state(subdossier))
-        self.assertEqual('dossier-state-inactive',
-                         api.content.get_state(subsubdossier))
+        self.login(self.secretariat_user, browser)
+        subdossier = create(Builder('dossier').within(self.empty_dossier))
+        browser.open(self.empty_dossier)
+        editbar.menu_option('Actions', 'dossier-transition-deactivate').click()
+        statusmessages.assert_no_error_messages()
+        self.assert_workflow_state('dossier-state-inactive', self.empty_dossier)
+        self.assert_workflow_state('dossier-state-inactive', subdossier)
 
     @browsing
     def test_already_inactive_subdossier_will_be_ignored(self, browser):
-        subdossier1 = create(Builder('dossier').within(self.dossier))
-        subdossier2 = create(Builder('dossier')
-                             .within(self.dossier)
-                             .in_state('dossier-state-inactive'))
-
-        browser.login().open(self.dossier, view='transition-deactivate',
-                             data={'_authenticator': createToken()})
-
-        self.assertEqual('dossier-state-inactive',
-                         api.content.get_state(self.dossier))
-        self.assertEqual('dossier-state-inactive',
-                         api.content.get_state(subdossier1))
-        self.assertEqual('dossier-state-inactive',
-                         api.content.get_state(subdossier2))
+        self.login(self.secretariat_user, browser)
+        subdossier = create(Builder('dossier').within(self.empty_dossier)
+                            .in_state('dossier-state-inactive'))
+        browser.open(self.empty_dossier)
+        editbar.menu_option('Actions', 'dossier-transition-deactivate').click()
+        statusmessages.assert_no_error_messages()
+        self.assert_workflow_state('dossier-state-inactive', self.empty_dossier)
+        self.assert_workflow_state('dossier-state-inactive', subdossier)
 
     @browsing
     def test_sets_end_date_to_current_date(self, browser):
+        self.login(self.secretariat_user, browser)
         with freeze(datetime(2016, 3, 29)):
-            browser.login().open(self.dossier,
-                                 view='transition-deactivate',
-                                 data={'_authenticator': createToken()})
+            browser.open(self.empty_dossier)
+            editbar.menu_option('Actions', 'dossier-transition-deactivate').click()
 
-        self.assertEqual(date(2016, 3, 29), IDossier(self.dossier).end)
+        self.assertEqual(date(2016, 3, 29), IDossier(self.empty_dossier).end)
