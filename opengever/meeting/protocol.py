e82008927cfdb3c083ba95159f8acb30382dc2b4
Elio Schmutz <e.schmutz@4teamwork.ch>
2017-08-09 09:05:32 +0200

    Remove unused member-sorting in protocol since we always sort members by fullname

--- opengever/meeting/protocol.py
+++ opengever/meeting/protocol.py
@@ -69,7 +69,6 @@ class ProtocolData(object):
                 "email": participant.email,
                 "role": membership.role if membership else None
             })
-        members.sort(key=lambda item: item['fullname'])
         participants = {
             'members': members
         }
