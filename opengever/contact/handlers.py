a17cb7644a6b449bbf0fd57fbeb6a362c6ea5466
Elio Schmutz <e.schmutz@4teamwork.ch>
2017-08-10 15:37:04 +0200

    Remove grok from opengever.contact.

--- opengever/contact/handlers.py
+++ opengever/contact/handlers.py
@@ -1,12 +1,8 @@
-from five import grok
 from opengever.base.model import create_session
 from opengever.base.portlets import block_context_portlet_inheritance
 from opengever.contact.models import Participation
-from opengever.dossier.behaviors.dossier import IDossierMarker
-from zope.lifecycleevent.interfaces import IObjectCopiedEvent
 
 
-@grok.subscribe(IDossierMarker, IObjectCopiedEvent)
 def copy_participations(copied_dossier, event):
     """Make sure that participations are copied as well when copying a dossier.
     """
