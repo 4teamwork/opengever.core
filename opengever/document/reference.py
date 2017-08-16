95adaa375f58b3f8e1ec2414c2a9bf7d06260d4b
Elio Schmutz <e.schmutz@4teamwork.ch>
2017-08-16 13:34:13 +0200

    Ungrok BasicReferenceNumber adapter

--- opengever/document/reference.py
+++ opengever/document/reference.py
@@ -1,17 +1,14 @@
-from five import grok
-from opengever.base.interfaces import IReferenceNumber
 from opengever.base.interfaces import ISequenceNumber
 from opengever.base.reference import BasicReferenceNumber
 from opengever.document.behaviors import IBaseDocument
+from zope.component import adapter
 from zope.component import getUtility
 
 
+@adapter(IBaseDocument)
 class DocumentReferenceNumber(BasicReferenceNumber):
     """ Reference number for documents
     """
-    grok.provides(IReferenceNumber)
-    grok.context(IBaseDocument)
-
     ref_type = 'document'
 
     def get_local_number(self):
