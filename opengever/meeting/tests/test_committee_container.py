b7fbef769c00515a60ecd287cce366ecb3d083ee
David Erni <david.erni@4teamwork.ch>
2017-08-16 17:16:41 +0200

    Convert TestCommitteesTab to an integration test.

--- opengever/meeting/tests/test_committee_container.py
+++ opengever/meeting/tests/test_committee_container.py
@@ -2,9 +2,10 @@ from datetime import timedelta
 from ftw.builder import Builder
 from ftw.builder import create
 from ftw.testbrowser import browsing
+from ftw.testbrowser.pages import editbar
 from ftw.testbrowser.pages import factoriesmenu
+from opengever.base.date_time import utcnow_tz_aware
 from opengever.testing import add_languages
-from opengever.testing import FunctionalTestCase
 from opengever.testing import IntegrationTestCase
 
 
@@ -49,220 +50,186 @@ class TestCommitteeContainer(IntegrationTestCase):
             'plone.leftcolumn', self.committee_container)
 
 
-class TestCommitteesTab(FunctionalTestCase):
+class TestCommitteesTab(IntegrationTestCase):
 
-    def setUp(self):
-        super(TestCommitteesTab, self).setUp()
-        self.container = create(Builder('committee_container'))
-        self.grant('MeetingUser', on=self.container)
-        self.committee = create(Builder('committee')
-                                .within(self.container)
-                                .titled(u'Kleiner Burgerrat'))
-        self.committee_model = self.committee.load_model()
+    features = ('meeting',)
 
     @browsing
     def test_shows_albhabetically_sorted_committees_in_boxes(self, browser):
-        create(Builder('committee')
-               .within(self.container)
-               .titled(u'Wirtschafts Kommission'))
-
-        create(Builder('committee')
-               .within(self.container)
-               .titled(u'Gew\xe4sser Kommission'))
-
-        browser.login().open(self.container, view='tabbedview_view-committees')
+        self.login(self.meeting_user, browser)
+        browser.open(self.committee_container,
+                     view='tabbedview_view-committees')
 
         self.assertEquals(
-            [u'Gew\xe4sser Kommission', u'Kleiner Burgerrat', u'Wirtschafts Kommission'],
+            [u'Kommission f\xfcr Verkehr',
+             u'Rechnungspr\xfcfungskommission'],
             browser.css('#committees_view .committee_box h2').text)
 
     @browsing
     def test_can_only_see_committees_for_corresponding_group(self, browser):
-        user = create(Builder('user')
-                      .with_userid('hugo.boss'))
-        create(Builder('group')
-               .with_groupid('kom_wirtschaft')
-               .with_members(user))
-        self.grant('MeetingUser', user_id='kom_wirtschaft', on=self.container)
+        with self.login(self.administrator):
+            create(Builder('committee')
+                   .within(self.committee_container)
+                   .titled(u'Invisible'))
 
-        create(Builder('committee')
-               .within(self.container)
-               .having(group_id='kom_wirtschaft')
-               .titled(u'Wirtschafts Kommission'))
-        create(Builder('committee')
-               .within(self.container)
-               .titled(u'Gew\xe4sser Kommission'))
-
-        browser.login('hugo.boss').open(self.container,
-                                        view='tabbedview_view-committees')
+        self.login(self.meeting_user, browser)
+        browser.open(self.committee_container,
+                     view='tabbedview_view-committees')
         self.assertEquals(
-            [u'Wirtschafts Kommission'],
+            [u'Kommission f\xfcr Verkehr',
+             u'Rechnungspr\xfcfungskommission'],
             browser.css('#committees_view .committee_box h2').text)
 
     @browsing
     def test_tabbedview_text_filter(self, browser):
-        create(Builder('committee')
-               .within(self.container)
-               .titled(u'Wirtschafts Kommission'))
-
-        create(Builder('committee')
-               .within(self.container)
-               .titled(u'Gew\xe4sser Kommission'))
-
-        browser.login().open(self.container,
-                             view='tabbedview_view-committees',
-                             data={'searchable_text': 'Wirtschaf Komm'})
+        self.login(self.meeting_user, browser)
+        browser.open(self.committee_container,
+                     view='tabbedview_view-committees',
+                     data={'searchable_text': 'Rechnung'})
 
         self.assertEquals(
-            [u'Wirtschafts Kommission'],
+            [u'Rechnungspr\xfcfungskommission'],
             browser.css('#committees_view .committee_box h2').text)
 
     @browsing
     def test_text_filter_ignores_trailing_asterisk(self, browser):
-        create(Builder('committee')
-               .within(self.container)
-               .titled(u'Wirtschafts Kommission'))
-
-        create(Builder('committee')
-               .within(self.container)
-               .titled(u'Gew\xe4sser Kommission'))
-
-        browser.login().open(self.container,
-                             view='tabbedview_view-committees',
-                             data={'searchable_text': 'Wirtschaft*'})
+        self.login(self.meeting_user, browser)
+        browser.open(self.committee_container,
+                     view='tabbedview_view-committees',
+                     data={'searchable_text': 'Rechnung*'})
 
         self.assertEquals(
-            [u'Wirtschafts Kommission'],
+            [u'Rechnungspr\xfcfungskommission'],
             browser.css('#committees_view .committee_box h2').text)
 
     @browsing
     def test_list_only_active_committees_by_default(self, browser):
-        committee_b = create(Builder('committee')
-                             .within(self.container)
-                             .titled(u'Wasserkommission'))
+        with self.login(self.committee_responsible, browser):
+            browser.open(self.empty_committee)
+            editbar.menu_option('Actions', 'deactivate').click()
 
-        browser.login().open(committee_b, view='deactivate')
-        browser.open(self.container, view='tabbedview_view-committees')
-
+        self.login(self.meeting_user, browser)
+        browser.open(self.committee_container,
+                     view='tabbedview_view-committees')
         self.assertEquals(
-            [u'Kleiner Burgerrat'],
+            [u'Rechnungspr\xfcfungskommission'],
             browser.css('#committees_view .committee_box h2').text)
 
     @browsing
     def test_list_all_committees_when_selecting_all_filter(self, browser):
-        committee_b = create(Builder('committee')
-                             .within(self.container)
-                             .titled(u'Wasserkommission'))
+        with self.login(self.committee_responsible, browser):
+            browser.open(self.empty_committee)
+            editbar.menu_option('Actions', 'deactivate').click()
 
-        browser.login().open(committee_b, view='deactivate')
-        browser.open(self.container, view='tabbedview_view-committees',
+        self.login(self.meeting_user, browser)
+        browser.open(self.committee_container,
+                     view='tabbedview_view-committees',
                      data={'committee_state_filter': 'filter_all'})
 
         self.assertEquals(
-            [u'Kleiner Burgerrat', u'Wasserkommission'],
+            [u'Kommission f\xfcr Verkehr',
+             u'Rechnungspr\xfcfungskommission'],
             browser.css('#committees_view .committee_box h2').text)
 
     @browsing
     def test_committe_box_has_state_class(self, browser):
-        committee_b = create(Builder('committee')
-                             .within(self.container)
-                             .titled(u'Wasserkommission'))
+        with self.login(self.committee_responsible, browser):
+            browser.open(self.empty_committee)
+            editbar.menu_option('Actions', 'deactivate').click()
 
-        browser.login().open(committee_b, view='deactivate')
-        browser.open(self.container, view='tabbedview_view-committees',
+        self.login(self.meeting_user, browser)
+        browser.open(self.committee_container,
+                     view='tabbedview_view-committees',
                      data={'committee_state_filter': 'filter_all'})
 
         self.assertEquals(
-            'committee_box active',
+            'committee_box inactive',
             browser.css('#committees_view .committee_box')[0].get('class'))
         self.assertEquals(
-            'committee_box inactive',
+            'committee_box active',
             browser.css('#committees_view .committee_box')[1].get('class'))
 
     @browsing
-    def test_commitee_is_linked_correctly(self, browser):
-        browser.login().open(self.container, view='tabbedview_view-committees')
+    def test_committee_is_linked_correctly(self, browser):
+        self.login(self.meeting_user, browser)
+        browser.open(self.committee_container,
+                     view='tabbedview_view-committees')
+
         title = browser.css('#committees_view .committee_box a').first
 
-        self.assertEquals(u'Kleiner Burgerrat', title.text)
+        self.assertEquals(u'Kommission f\xfcr Verkehr', title.text)
         self.assertEquals(
-            'http://example.com/opengever-meeting-committeecontainer/committee-1',
+            'http://nohost/plone/opengever-meeting-committeecontainer/committee-2',
             title.get('href'))
 
     @browsing
     def test_unscheduled_proposal_number(self, browser):
-        repo, repo_folder = create(Builder('repository_tree'))
-        dossier = create(Builder('dossier').within(repo_folder))
-
-        for i in range(0, 5):
-            create(Builder('proposal')
-                   .within(dossier)
-                   .having(committee=self.committee_model)
-                   .as_submitted())
-
-        browser.login().open(self.container, view='tabbedview_view-committees')
+        self.login(self.meeting_user, browser)
+        browser.open(self.committee_container,
+                     view='tabbedview_view-committees')
 
         self.assertEquals(
-            'New unschedulded proposals: 5',
-            browser.css('#committees_view .unscheduled_proposals').first.text)
+            ['New unschedulded proposals: 0',
+             'New unschedulded proposals: 2'],
+            browser.css('#committees_view .unscheduled_proposals').text)
 
     @browsing
     def test_unscheduled_proposal_number_link(self, browser):
-        repo, repo_folder = create(Builder('repository_tree'))
-        dossier = create(Builder('dossier').within(repo_folder))
+        self.login(self.meeting_user, browser)
+        browser.open(self.committee_container,
+                     view='tabbedview_view-committees')
 
-        create(Builder('proposal')
-               .within(dossier)
-               .having(committee=self.committee_model)
-               .as_submitted())
+        link = browser.css('#committees_view .unscheduled_proposals a')[1]
 
-        browser.login().open(self.container, view='tabbedview_view-committees')
-        link = browser.css('#committees_view .unscheduled_proposals a').first
-
-        self.assertEquals('1', link.text)
+        self.assertEquals('2', link.text)
         self.assertEquals(
-            'http://example.com/opengever-meeting-committeecontainer/committee-1#submittedproposals',
+            'http://nohost/plone/opengever-meeting-committeecontainer/committee-1#submittedproposals',
             link.get('href'))
 
     @browsing
     def test_unscheduled_proposal_number_class(self, browser):
-        repo, repo_folder = create(Builder('repository_tree'))
-        dossier = create(Builder('dossier').within(repo_folder))
-        create(Builder('proposal')
-               .within(dossier)
-               .having(committee=self.committee_model)
-               .as_submitted())
+        self.login(self.meeting_user, browser)
+        browser.open(self.committee_container,
+                     view='tabbedview_view-committees')
 
-        create(Builder('committee').within(self.container)
-               .titled(u'Xenophoben-Kommission'))
-
-        browser.login().open(self.container, view='tabbedview_view-committees')
         links = browser.css('#committees_view .unscheduled_proposals a')
 
-        self.assertEquals('1', links[0].text)
-        self.assertEquals('number unscheduled_number', links[0].get('class'))
-        self.assertEquals('0', links[1].text)
-        self.assertEquals('number', links[1].get('class'))
+        self.assertEquals('0', links[0].text)
+        self.assertEquals('number', links[0].get('class'))
+        self.assertEquals('2', links[1].text)
+        self.assertEquals('number unscheduled_number', links[1].get('class'))
 
     @browsing
     def test_meetings_display(self, browser):
-        meeting1 = create(Builder('meeting')
-                          .having(committee=self.committee_model,
-                                  start=self.localized_datetime(2015, 01, 01)))
+        with self.login(self.committee_responsible):
+            start = utcnow_tz_aware() + timedelta(days=5)
 
-        meeting2 = create(Builder('meeting')
-                          .having(committee=self.committee_model,
-                                  start=self.localized_datetime() + timedelta(days=1)))
+            meeting_dossier = create(
+                Builder('meeting_dossier')
+                .within(self.leaf_repofolder)
+                .titled(u'Sitzungsdossier f')
+                .having(start=start.date(),
+                        responsible=self.committee_responsible.getId()))
+            meeting = create(
+                Builder('meeting')
+                .having(title=u'f. Sitzung der Rechnungspr\xfcfungskommission',
+                        committee=self.committee.load_model(),
+                        location=u'B\xfcren an der Aare',
+                        start=start)
+                .link_with(meeting_dossier))
 
-        browser.login().open(self.container, view='tabbedview_view-committees')
+        self.login(self.meeting_user, browser)
+        browser.open(self.committee_container,
+                     view='tabbedview_view-committees')
 
         self.assertEquals(
-            ['Last Meeting: Jan 01, 2015',
-             'Next Meeting: {}'.format(meeting2.get_date())],
+            ['Last Meeting: Sep 12, 2016',
+             'Next Meeting: {}'.format(meeting.get_date())],
             browser.css('#committees_view .meetings li').text)
 
         last_meeting = browser.css('#committees_view .meetings li a')[0]
         next_meeting = browser.css('#committees_view .meetings li a')[1]
 
-        self.assertEquals(meeting1.get_url(), last_meeting.get('href'))
-        self.assertEquals(meeting2.get_url(), next_meeting.get('href'))
+        self.assertEquals(self.meeting.model.get_url(),
+                          last_meeting.get('href'))
+        self.assertEquals(meeting.get_url(), next_meeting.get('href'))
