524252761df6557086a5aa8c66c0797b6237eae7
David Erni <david.erni@4teamwork.ch>
2017-08-17 10:36:33 +0200

    Convert TestPathBar to an integration test.
    Also document the test a bit better.
    Also use new breadcrumbs page object.

--- opengever/meeting/tests/test_pathbar.py
+++ opengever/meeting/tests/test_pathbar.py
@@ -2,69 +2,63 @@ from datetime import date
 from ftw.builder import Builder
 from ftw.builder import create
 from ftw.testbrowser import browsing
+from opengever.base.model import create_session
 from opengever.meeting.wrapper import MemberWrapper
-from opengever.testing import FunctionalTestCase
+from opengever.testing import IntegrationTestCase
+from opengever.testing.pages import breadcrumbs
 
 
-class TestPathBar(FunctionalTestCase):
+class TestPathBar(IntegrationTestCase):
 
-    def setUp(self):
-        super(TestPathBar, self).setUp()
-        self.admin_unit.public_url = 'http://nohost/plone'
-
-        self.root = create(Builder('repository_root').titled(u'Repository'))
-        self.repo = create(Builder('repository')
-                           .within(self.root)
-                           .titled(u'Testposition'))
-        self.dossier = create(Builder('dossier')
-                              .within(self.repo)
-                              .titled(u'Dossier 1'))
-        self.meeting_dossier = create(
-            Builder('meeting_dossier').within(self.repo))
+    features = ('meeting',)
 
     @browsing
     def test_committee_pathbar_is_correct(self, browser):
-        container = create(Builder('committee_container'))
-        committee = create(Builder('committee').within(container))
-        meeting = create(Builder('meeting')
-                         .having(committee=committee.load_model())
-                         .link_with(self.meeting_dossier))
+        self.login(self.meeting_user, browser)
 
-        browser.login().open(meeting.get_url())
+        browser.open(self.meeting.model.get_url())
         self.assertEqual(
-            ['Client1', 'opengever-meeting-committeecontainer',
-             'My Committee', u'C\xf6mmunity meeting'],
-            browser.css('#portal-breadcrumbs a').text)
+            ['Hauptmandant',
+             'Sitzungen',
+             u'Rechnungspr\xfcfungskommission',
+             u'9. Sitzung der Rechnungspr\xfcfungskommission'],
+            breadcrumbs.text_items())
 
     @browsing
     def test_member_pathbar_is_correct(self, browser):
-        container = create(Builder('committee_container'))
-        member = create(Builder('member'))
+        with self.login(self.administrator):
+            member = create(Builder('member'))
+            create_session().flush()  # XXX maybe auto-flush here ...
 
-        browser.login().open(member.get_url(container))
+        self.login(self.meeting_user, browser)
+        browser.open(member.get_url(self.committee_container))
         self.assertEqual(
-            [u'Client1',
-             u'opengever-meeting-committeecontainer',
-             u'M\xfcller Peter'],
-            browser.css('#portal-breadcrumbs a').text)
+            [u'Hauptmandant', u'Sitzungen', u'M\xfcller Peter'],
+            breadcrumbs.text_items())
 
     @browsing
     def test_membership_pathbar_is_correct(self, browser):
-        container = create(Builder('committee_container'))
-        committee = create(Builder('committee').within(container))
-        committee_model = committee.load_model()
-        member = create(Builder('member'))
-        membership = create(Builder('membership')
-                            .having(member=member,
-                                    committee=committee_model,
-                                    date_from=date(2014, 1, 1),
-                                    date_to=date(2015, 1, 1)))
+        with self.login(self.administrator):
+            committee_model = self.committee.load_model()
+            member = create(Builder('member'))
+            membership = create(Builder('membership')
+                                .having(member=member,
+                                        committee=committee_model,
+                                        date_from=date(2014, 1, 1),
+                                        date_to=date(2015, 1, 1)))
+            create_session().flush()  # XXX maybe auto-flush here ...
 
-        wrapped_member = MemberWrapper.wrap(container, member)
-        browser.login().open(membership.get_url(wrapped_member))
+        # XXX the permissions currently don't work well. this checks
+        # modify poral content on committee-container, so we need to be
+        # an admin for now. see #2829.
+        self.login(self.administrator, browser)
+        wrapped_member = MemberWrapper.wrap(self.committee_container, member)
+        # NOTE: this opens the edit view! there is no separate view for
+        # a single membership, they are all listed on the member view.
+        browser.open(membership.get_url(wrapped_member))
         self.assertEqual(
-            [u'Client1',
-             u'opengever-meeting-committeecontainer',
+            [u'Hauptmandant',
+             u'Sitzungen',
              u'M\xfcller Peter',
              u'M\xfcller Peter, Jan 01, 2014 - Jan 01, 2015'],
-            browser.css('#portal-breadcrumbs a').text)
+            breadcrumbs.text_items())
