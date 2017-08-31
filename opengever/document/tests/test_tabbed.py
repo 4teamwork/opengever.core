7bc7e2f3c466917c09ea647bea4572a60cb55dbb
Philippe Gross <philippe.gross@4teamwork.ch>
2017-08-31 09:31:49 +0200

    Convert TestDocumentQuickupload to an IntergrationTest.

--- opengever/document/tests/test_tabbed.py
+++ opengever/document/tests/test_tabbed.py
@@ -1,60 +1,59 @@
-from ftw.builder import Builder
-from ftw.builder import create
 from ftw.testbrowser import browsing
-from opengever.testing import FunctionalTestCase
-from plone.app.testing import TEST_USER_ID
+from opengever.document.interfaces import ICheckinCheckoutManager
+from opengever.testing import IntegrationTestCase
 from plone.locking.interfaces import IRefreshableLockable
+from zope.component import queryMultiAdapter
 
 
-class TestDocumentQuickupload(FunctionalTestCase):
-
-    def setUp(self):
-        super(TestDocumentQuickupload, self).setUp()
-        self.dossier = create(Builder('dossier'))
+class TestDocumentQuickupload(IntegrationTestCase):
 
     @browsing
     def test_upload_box_is_hidden_when_document_is_not_checked_out(self, browser):
-        document = create(Builder('document').within(self.dossier))
-
-        browser.login().open(document)
+        self.login(self.regular_user, browser)
+        browser.open(self.document)
         self.assertEquals([], browser.css('#uploadbox'),
                           'uploadbox is wrongly displayed')
 
     @browsing
     def test_upload_box_is_hidden_when_document_is_locked(self, browser):
-        document = create(Builder('document').within(self.dossier))
-        IRefreshableLockable(document).lock()
+        self.login(self.regular_user, browser)
 
-        browser.login().open(document)
+        manager = queryMultiAdapter(
+            (self.document, self.request), ICheckinCheckoutManager)
+        manager.checkout()
+        IRefreshableLockable(self.document).lock()
+
+        browser.open(self.document)
         self.assertEquals([], browser.css('#uploadbox'),
                           'uploadbox is wrongly displayed')
 
     @browsing
     def test_upload_box_is_shown_when_document_is_checked_out_and_not_locked(self, browser):
-        document = create(Builder('document')
-                          .within(self.dossier)
-                          .checked_out())
+        self.login(self.regular_user, browser)
+        browser.open(self.document)
 
-        browser.login().open(document)
-        self.assertNotEquals(0, len(browser.css('#uploadbox')),
-                             'Uploadbox is not displayed, but should.')
+        manager = queryMultiAdapter(
+            (self.document, self.request), ICheckinCheckoutManager)
+        manager.checkout()
+
+        browser.open(self.document)
+        self.assertGreater(len(browser.css('#uploadbox')), 0,
+                           'Uploadbox is not displayed, but should.')
 
     @browsing
     def test_upload_box_is_also_shown_in_a_resolved_task(self, browser):
-        create(Builder('ogds_user').id(u'hugo.boss'))
-        task = create(Builder('task')
-                      .having(responsible='hugo.boss', issuer='hugo.boss')
-                      .within(self.dossier)
-                      .in_state('task-state-tested-and-closed'))
-        document = create(Builder('document')
-                          .within(task)
-                          .checked_out())
+        self.login(self.regular_user, browser)
 
-        self.grant('Reader')
+        self.set_workflow_state('task-state-tested-and-closed', self.task)
         self.dossier.manage_setLocalRoles(
-            TEST_USER_ID, ['Reader', 'Contributor', 'Editor'])
+            'kathi.barfuss', ['Reader', 'Contributor', 'Editor'])
         self.dossier.reindexObjectSecurity()
 
-        browser.login().open(document)
-        self.assertNotEquals(0, len(browser.css('#uploadbox')),
-                             'Uploadbox is not displayed, but should.')
+        manager = queryMultiAdapter(
+            (self.taskdocument, self.request), ICheckinCheckoutManager)
+        manager.checkout()
+
+        browser.open(self.taskdocument)
+
+        self.assertGreater(len(browser.css('#uploadbox')), 0,
+                           'Uploadbox is not displayed, but should.')
