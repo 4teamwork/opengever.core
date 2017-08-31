eeb1bbdcd34b4f1689ab8799a9f15e0c90a7f52e
Philippe Gross <philippe.gross@4teamwork.ch>
2017-08-31 14:14:10 +0200

    Convert breadcrumb viewlet tests to integration tests.

--- opengever/base/tests/test_pathbar.py
+++ opengever/base/tests/test_pathbar.py
@@ -1,65 +1,53 @@
-from ftw.builder import Builder
-from ftw.builder import create
 from ftw.testbrowser import browsing
-from opengever.testing import FunctionalTestCase
+from opengever.testing import IntegrationTestCase
 
 
-class TestPathBar(FunctionalTestCase):
-
-    def setUp(self):
-        super(TestPathBar, self).setUp()
-        self.admin_unit.public_url = 'http://nohost/plone'
-
-        self.root = create(Builder('repository_root').titled(u'Repository'))
-        self.repo = create(Builder('repository')
-                           .within(self.root)
-                           .titled(u'Testposition'))
-        self.subrepo = create(Builder('repository')
-                              .within(self.repo)
-                              .titled(u'Subposition'))
-        self.dossier = create(Builder('dossier')
-                              .within(self.subrepo)
-                              .titled(u'Dossier 1'))
-        self.meeting_dossier = create(
-            Builder('meeting_dossier').within(self.subrepo))
+class TestPathBar(IntegrationTestCase):
 
     @browsing
     def test_first_part_is_org_unit_title(self, browser):
-        browser.login().open(self.dossier)
+        self.login(self.regular_user, browser)
+        browser.open(self.dossier)
 
         first_part = browser.css('#portal-breadcrumbs li a')[0]
-        self.assertEquals('Client1', first_part.text)
+        self.assertEquals('Hauptmandant', first_part.text)
         self.assertEquals(self.portal.absolute_url(), first_part.get('href'))
 
     @browsing
     def test_contains_contenttype_icon_class(self, browser):
-        browser.login().open(self.dossier)
+        self.login(self.regular_user, browser)
+        browser.open(self.document)
 
         self.assertEquals(
             ['contenttype-plone-site',
              'contenttype-opengever-repository-repositoryfolder',
              'contenttype-opengever-repository-repositoryfolder',
              'contenttype-opengever-repository-repositoryroot',
-             'contenttype-opengever-dossier-businesscasedossier'],
+             'contenttype-opengever-dossier-businesscasedossier',
+             'contenttype-opengever-document-document'],
             [crumb.get('class') for crumb in
              browser.css('#portal-breadcrumbs li a i')])
 
     @browsing
     def test_repositories_are_grouped_in_a_sublist(self, browser):
-        browser.login().open(self.dossier)
+        self.login(self.regular_user, browser)
+        browser.open(self.dossier)
 
         self.assertEquals(
-            ['Client1',
-             '1.1. Subposition', '1. Testposition', 'Repository',
-             'Dossier 1'],
+            [u'Hauptmandant',
+             u'1.1. Vertr\xe4ge und Vereinbarungen',
+             u'1. F\xfchrung',
+             u'Ordnungssystem',
+             u'Vertr\xe4ge mit der kantonalen Finanzverwaltung'],
             browser.css('#portal-breadcrumbs li a').text)
 
-        self.assertEquals(['1. Testposition', 'Repository'],
+        self.assertEquals([u'1. F\xfchrung', 'Ordnungssystem'],
                           browser.css('#portal-breadcrumbs li ul a').text)
 
     @browsing
     def test_last_part_is_linked(self, browser):
-        browser.login().open(self.dossier)
+        self.login(self.regular_user, browser)
+        browser.open(self.dossier)
 
         last_link = browser.css('#portal-breadcrumbs a')[-1]
         self.assertEqual(self.dossier.absolute_url(),
@@ -67,30 +55,22 @@ class TestPathBar(FunctionalTestCase):
 
     @browsing
     def test_proposal_is_linked_with_title(self, browser):
-        proposal = create(Builder('proposal')
-                          .within(self.dossier)
-                          .titled('My Proposal'))
+        self.login(self.regular_user, browser)
+        browser.open(self.proposal)
 
-        browser.login().open(proposal)
         last_link = browser.css('#portal-breadcrumbs a')[-1]
-        self.assertEqual(proposal.absolute_url(),
+        self.assertEqual(self.proposal.absolute_url(),
                          last_link.node.attrib['href'])
-        self.assertEqual('My Proposal', last_link.text)
+        self.assertEqual(self.proposal.title, last_link.text)
 
     @browsing
     def test_meeting_is_linked_with_title(self, browser):
-        container = create(Builder('committee_container'))
-        committee = create(Builder('committee').within(container))
-        meeting = create(Builder('meeting')
-                         .having(committee=committee.load_model())
-                         .link_with(self.meeting_dossier))
+        self.login(self.meeting_user, browser)
+        browser.open(self.meeting)
 
-        self.grant('MeetingUser', on=container)
-        self.grant('CommitteeMember', on=committee)
-        browser.login().open(meeting.get_url())
         last_link = browser.css('#portal-breadcrumbs a')[-1]
         self.assertEqual(
             'http://nohost/plone/opengever-meeting-committeecontainer/committee-1/meeting-1',
             last_link.get('href'))
 
-        self.assertEqual(meeting.get_title(), last_link.text)
+        self.assertEqual(self.meeting.get_title(), last_link.text)
