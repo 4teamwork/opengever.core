4acb1eae9f45a1368a46ead2a75f6d0593c39617
Lukas Graf <lukas.graf@4teamwork.ch>
2017-09-04 14:21:57 +0200

    Add content stats provider for file mimetypes.

--- opengever/contentstats/providers.py
+++ opengever/contentstats/providers.py
@@ -1,4 +1,7 @@
+from collections import Counter
 from ftw.contentstats.interfaces import IStatsProvider
+from opengever.document.document import Document
+from opengever.mail.mail import OGMail
 from plone import api
 from Products.CMFPlone.interfaces import IPloneSiteRoot
 from zope.component import adapter
@@ -41,4 +44,48 @@ class CheckedOutDocsProvider(object):
             else:
                 counts['checked_out'] += num_docs
 
+        return counts
+
+
+@implementer(IStatsProvider)
+@adapter(IPloneSiteRoot, Interface)
+class FileMimetypesProvider(object):
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def title(self):
+        """Human readable title
+        """
+        return u'File mimetype statistics'
+
+    def get_display_names(self):
+        return None
+
+    def get_raw_stats(self):
+        """Return a dictionary with counts of file mimetypes.
+        """
+        counts = {}
+        catalog = api.portal.get_tool('portal_catalog')
+
+        # Documents
+        docs_query = {
+            'portal_type': 'opengever.document.document',
+            'review_state': Document.active_state,
+            'trashed': False,
+        }
+        docs = catalog.unrestrictedSearchResults(docs_query)
+
+        docs_counts = Counter([b.getContentType for b in docs])
+        counts.update(docs_counts)
+
+        # Mails
+        mails_query = {
+            'portal_type': 'ftw.mail.mail',
+            'review_state': OGMail.active_state,
+            'trashed': False,
+        }
+        mails = catalog.unrestrictedSearchResults(mails_query)
+        counts['message/rfc822'] = len(mails)
         return counts
