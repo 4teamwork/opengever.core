702d4a2845c86cc2d1cfb4e01efb8ab6f04797e4
Pablo Vergés <pablo.verges@4teamwork.ch>
2017-09-27 17:35:35 +0200

    Remove excerpt template from forms
    The excerpt template is not available when the word-meetin feature
    enabled, and is therefore hidden.
    Since the field is required (when the word-meeting feature is not
    activated) the require-argument is changed by a constraint.
    The constraint checks if there's a given value, which is (almost)
    equivalent to setting required=True.

--- opengever/meeting/browser/committeecontainer_forms.py
+++ opengever/meeting/browser/committeecontainer_forms.py
@@ -18,6 +18,8 @@ class AddForm(TranslatedTitleAddForm):
         if not is_word_meeting_implementation_enabled():
             self.widgets['ad_hoc_template'].mode = HIDDEN_MODE
             self.widgets['paragraph_template'].mode = HIDDEN_MODE
+        else:
+            self.widgets['excerpt_template'].mode = HIDDEN_MODE
 
 
 @adapter(IFolderish, IDefaultBrowserLayer, IDexterityFTI)
@@ -34,3 +36,5 @@ class EditForm(TranslatedTitleEditForm):
         if not is_word_meeting_implementation_enabled():
             self.widgets['ad_hoc_template'].mode = HIDDEN_MODE
             self.widgets['paragraph_template'].mode = HIDDEN_MODE
+        else:
+            self.widgets['excerpt_template'].mode = HIDDEN_MODE
