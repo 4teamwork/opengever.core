053f4f75a99905d6614daa7be93a8018bdc32898
Philippe Gross <philippe.gross@4teamwork.ch>
2017-10-04 11:22:03 +0200

    Remove the creating initial version on added event handler.
    The creation of an initial version is now handled by the document file
    setter and does not need to be triggered by an event handler.

--- opengever/document/checkout/handlers.py
+++ opengever/document/checkout/handlers.py
@@ -1,38 +1,3 @@
-from five import grok
-from opengever.document.document import IDocumentSchema
-from opengever.document.interfaces import INoAutomaticInitialVersion
-from zope.globalrequest import getRequest
-from zope.interface import alsoProvides
-from zope.interface import noLongerProvides
-from zope.lifecycleevent.interfaces import IObjectAddedEvent
-
-
-class NoAutomaticInitialVersion(object):
-    """Contextmanager that temporarily disables automatic creation of
-    initial versions via event handler.
-
-    This is sometimes necessary when a transmogrifier pipeline is involved,
-    i.e. during setup or migration.
-    """
-
-    def __enter__(self):
-        alsoProvides(getRequest(), INoAutomaticInitialVersion)
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        noLongerProvides(getRequest(), INoAutomaticInitialVersion)
-
-
-@grok.subscribe(IDocumentSchema, IObjectAddedEvent)
-def handle_document_added(context, event):
-    """Event handler which creates a initial version when adding a new
-    document. Since we use manual versioning for the document, the initial
-    version doesn't get created automatically.
-
-    """
-    if INoAutomaticInitialVersion.providedBy(context.REQUEST):
-        return
-
-    create_initial_version(context)
 
 
 def create_initial_version(context):
