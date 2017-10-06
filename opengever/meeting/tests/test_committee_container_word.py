32fee5576cdaf2cb9ace68ff44182844c239cd39
Jonas Baumann <jone@jone.ch>
2017-10-06 11:43:55 +0200

    Refactor tests for field visibility in committee container and committee forms.

--- opengever/meeting/tests/test_committee_container_word.py
+++ opengever/meeting/tests/test_committee_container_word.py
@@ -71,34 +71,23 @@ class TestCommitteeContainer(IntegrationTestCase):
                          browser.context.get_paragraph_template())
 
     @browsing
-    def test_paragraph_template_for_nonword_in_add_form_is_hidden(self, browser):
-        """The "Paragraph template" field only makes sense when
-        the word-meeting feature is enabled.
-        Make sure that it does not appear in the forms when disabling
-        the word-meeting feature.
+    def test_visible_fields_in_forms(self, browser):
+        """Some fields should only be displayed when the word feature is
+        enabled.
+        Therefore we test the appearance of all fields.
         """
+        fields = [u'Title',
+                  u'Protocol header template',
+                  u'Protocol suffix template',
+                  u'Agendaitem list template',
+                  u'Table of contents template',
+                  u'Ad hoc agenda item template',
+                  u'Paragraph template']
         self.login(self.manager, browser)
+
         browser.open()
         factoriesmenu.add('Committee Container')
-        field_label = 'Paragraph template'
-        self.assertTrue(browser.find(field_label))
+        self.assertEquals(fields, browser.css('form#form > div.field > label').text)
 
-        self.deactivate_feature('word-meeting')
-        browser.reload()
-        self.assertFalse(browser.find(field_label))
-
-    @browsing
-    def test_paragraph_template_for_nonword_in_edit_form_is_hidden(self, browser):
-        """The "Paragraph template" field only makes sense when
-        the word-meeting feature is enabled.
-        Make sure that it does not appear in the forms when disabling
-        the word-meeting feature.
-        """
-        self.login(self.manager, browser)
         browser.open(self.committee_container, view='edit')
-        field_label = 'Paragraph template'
-        self.assertTrue(browser.find(field_label))
-
-        self.deactivate_feature('word-meeting')
-        browser.reload()
-        self.assertFalse(browser.find(field_label))
+        self.assertEquals(fields, browser.css('form#form > div.field > label').text)
