ea95e0610a5b86fb2b3ab5671a1caa544c376233
Pablo Verges <pablo.verges@4teamwork.ch>
2017-10-10 08:40:54 +0200

    Add protocol generation with suffix template tests
    ... to test if a protocol can be generated when the protocol suffix
    template is available either on the committee.

--- opengever/meeting/tests/test_protocol_word.py
+++ opengever/meeting/tests/test_protocol_word.py
@@ -37,6 +37,36 @@ class TestProtocolWithWord(IntegrationTestCase):
         self.assertEqual(1, meeting.protocol_document.generated_version)
 
     @browsing
+    def test_word_protocols_with_suffix_template_in_committee_can_be_created_and_updated(self, browser):
+        self.login(self.committee_responsible, browser)
+        self.committee.protocol_suffix_template = self.committee.protocol_header_template
+        self.schedule_paragraph(self.meeting, u'A-Gesch\xe4fte')
+        self.schedule_proposal(self.meeting, self.submitted_word_proposal)
+        meeting = self.meeting.model
+
+        self.assertIsNone(meeting.protocol_document)
+
+        browser.open(meeting.get_url())
+        # generate first protocol
+        browser.css('.generate-protocol').first.click()
+
+        statusmessages.assert_message(
+            u'Protocol for meeting 9. Sitzung der '
+            u'Rechnungspr\xfcfungskommission has been generated '
+            u'successfully.')
+        self.assertIsNotNone(meeting.protocol_document)
+        self.assertEqual(0, meeting.protocol_document.generated_version)
+
+        # update already generated protocol
+        browser.css('.refresh-protocol').first.click()
+
+        statusmessages.assert_message(
+            u'Protocol for meeting 9. Sitzung der '
+            u'Rechnungspr\xfcfungskommission has been updated successfully.')
+        self.assertIsNotNone(meeting.protocol_document)
+        self.assertEqual(1, meeting.protocol_document.generated_version)
+
+    @browsing
     def test_protocol_is_generated_when_closing_meetings(self, browser):
         self.login(self.committee_responsible, browser)
         self.schedule_proposal(self.meeting, self.submitted_word_proposal).decide()
