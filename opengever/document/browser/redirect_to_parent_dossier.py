e161a6896a7236a09e9658d30e7dca9467c1a510
Elio Schmutz <e.schmutz@4teamwork.ch>
2017-10-10 17:10:29 +0200

    Ungrok browser/redirect_to_parent_dossier

--- opengever/document/browser/redirect_to_parent_dossier.py
+++ opengever/document/browser/redirect_to_parent_dossier.py
@@ -1,15 +1,10 @@
-from five import grok
-from opengever.document.behaviors import IBaseDocument
+from Products.Five import BrowserView
 
 
-class RedirectToParentDossier(grok.View):
+class RedirectToParentDossier(BrowserView):
     """Redirects to the containing subdossier of a document.
     """
-    grok.context(IBaseDocument)
-    grok.name('redirect_to_parent_dossier')
-    grok.require('zope2.View')
-
-    def render(self):
+    def __call__(self):
         dossier = self.context.get_parent_dossier()
         assert dossier, ('the redirect view should only be called when a '
                          'parent dossier is available')
