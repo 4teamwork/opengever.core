88db195e55f1f0125d6c997c6ab206da80238d87
Joni Orponen <j.orponen@4teamwork.ch>
2017-10-13 13:43:43 +0200

    Implement HTTP DELETE for shadow documents.

--- opengever/document/service.py
+++ opengever/document/service.py
@@ -1,4 +1,5 @@
 from opengever.document import _
+from plone import api
 from plone.rest import Service
 from zExceptions import Forbidden
 from zope.i18n import translate
@@ -97,3 +98,48 @@ class ShadowDocumentDeleteConfirmMessage(Service):
         # Fail per default
         raise Forbidden
 
+
+class DeleteShadowDocument(Service):
+    """Provide a HTTP DELETE endpoint for shadow documents."""
+
+    def render(self):
+        """Delete a shadow document.
+
+        Preconditions:
+        * Only the document owner or a Manager may delete a shadow document
+        * Only a document still in the shadow state may be deleted
+        * Only a document without a file may be deleted
+        * Only a document without versions beyond the initial version may be deleted
+        """
+        document_owner_id = self.context.owner_info().get('id', None)
+
+        current_user = api.user.get_current()
+        current_user_id = current_user.id
+
+        # The creator or someone with a Manager role is trying to delete the document
+        if (document_owner_id and current_user_id == document_owner_id) or api.user.has_permission('Manage portal', obj=self.context):
+            # The document is still in a shadow state
+            if self.context.is_shadow_document():
+                # The document does not yet have a file
+                if not self.context.file:
+                    # The document does not yet have versions
+                    if self.context.version_id == 0:
+                        parent_dossier = self.context.get_parent_dossier()
+                        parent_dossier_url = parent_dossier.absolute_url() if parent_dossier else None
+
+                        parent_inbox = self.context.get_parent_inbox()
+                        parent_inbox_url = parent_inbox.absolute_url() if parent_inbox else None
+
+                        parent_url = parent_dossier_url if parent_dossier_url else parent_inbox_url
+
+                        api.content.delete(obj=self.context)
+
+                        success_message = _(u'label_succesful_shadow_document_deletion', default='Succesfully deleted the shadow document.')
+                        api.portal.show_message(message=success_message, type='info', request=self.request)
+
+                        return json.dumps(dict(parent_url=parent_url))
+
+        # Fail per default
+        failure_message = _(u'label_failed_shadow_document_deletion', default='Failed to delete the shadow document.')
+        api.portal.show_message(message=failure_message, type='error', request=self.request)
+        raise Forbidden
--- opengever/document/tests/test_api.py
+++ opengever/document/tests/test_api.py
@@ -1,5 +1,10 @@
 from ftw.testbrowser import browsing
+from opengever.document.document import Document
+from opengever.document.document import IDocumentSchema
+from opengever.document.interfaces import ICheckinCheckoutManager
 from opengever.testing import IntegrationTestCase
+from plone.namedfile.file import NamedBlobFile
+from zope.component import getMultiAdapter
 
 
 class TestDocumentAPI(IntegrationTestCase):
@@ -132,3 +137,144 @@ class TestDocumentAPI(IntegrationTestCase):
                     'Accept': 'application/json',
                 },
             )
+
+    @browsing
+    def test_shadow_document_can_be_deleted_by_owner(self, browser):
+        self.login(self.dossier_responsible, browser)
+
+        deletion_url = '/'.join((
+            self.shadow_document.absolute_url(),
+            'delete_shadow_document',
+            ))
+
+        browser.open(
+            deletion_url,
+            method='DELETE',
+            headers={
+                'Accept': 'application/json',
+            },
+        )
+
+        self.assertFalse([doc for doc in self.dossier.objectValues() if isinstance(doc, Document) and doc.is_shadow_document()])
+
+    @browsing
+    def test_shadow_document_cannot_be_deleted_by_another_user(self, browser):
+        self.login(self.dossier_responsible, browser)
+
+        deletion_url = '/'.join((
+            self.shadow_document.absolute_url(),
+            'delete_shadow_document',
+            ))
+
+        self.login(self.regular_user, browser)
+
+        with browser.expect_http_error(code=401, reason='Unauthorized'):
+            browser.open(
+                deletion_url,
+                method='DELETE',
+                headers={
+                    'Accept': 'application/json',
+                },
+            )
+
+        self.login(self.dossier_responsible, browser)
+        self.assertTrue(self.shadow_document)
+
+    @browsing
+    def test_shadow_document_can_be_deleted_by_portal_manager(self, browser):
+        self.login(self.dossier_responsible, browser)
+
+        deletion_url = '/'.join((
+            self.shadow_document.absolute_url(),
+            'delete_shadow_document',
+            ))
+
+        self.login(self.manager, browser)
+
+        browser.open(
+            deletion_url,
+            method='DELETE',
+            headers={
+                'Accept': 'application/json',
+            },
+        )
+
+        self.assertFalse([doc for doc in self.dossier.objectValues() if isinstance(doc, Document) and doc.is_shadow_document()])
+
+    @browsing
+    def test_normal_document_cannot_be_deleted(self, browser):
+        self.login(self.dossier_responsible, browser)
+
+        deletion_url = '/'.join((
+            self.document.absolute_url(),
+            'delete_shadow_document',
+            ))
+
+        self.login(self.regular_user, browser)
+
+        with browser.expect_http_error(code=403, reason='Forbidden'):
+            browser.open(
+                deletion_url,
+                method='DELETE',
+                headers={
+                    'Accept': 'application/json',
+                },
+            )
+
+        self.assertTrue(self.document)
+
+    @browsing
+    def test_shadow_document_with_a_file_cannot_be_deleted(self, browser):
+        self.login(self.dossier_responsible, browser)
+
+        field = IDocumentSchema['file']
+        uploaded_file = NamedBlobFile('bla bla', filename=u'test.txt')
+        field.set(self.shadow_document, uploaded_file)
+
+        self.assertEquals(0, self.shadow_document.version_id)
+
+        deletion_url = '/'.join((
+            self.shadow_document.absolute_url(),
+            'delete_shadow_document',
+            ))
+
+        with browser.expect_http_error(code=403, reason='Forbidden'):
+            browser.open(
+                deletion_url,
+                method='DELETE',
+                headers={
+                    'Accept': 'application/json',
+                },
+            )
+
+        self.assertTrue(self.shadow_document)
+
+    @browsing
+    def test_shadow_document_with_versions_cannot_be_deleted(self, browser):
+        self.login(self.dossier_responsible, browser)
+
+        getMultiAdapter((self.shadow_document, self.shadow_document.REQUEST), ICheckinCheckoutManager).checkout()
+
+        field = IDocumentSchema['file']
+        uploaded_file = NamedBlobFile('bla bla', filename=u'test.txt')
+        field.set(self.shadow_document, uploaded_file)
+
+        getMultiAdapter((self.shadow_document, self.shadow_document.REQUEST), ICheckinCheckoutManager).checkin()
+
+        self.assertNotEquals(0, self.shadow_document.version_id)
+
+        deletion_url = '/'.join((
+            self.shadow_document.absolute_url(),
+            'delete_shadow_document',
+            ))
+
+        with browser.expect_http_error(code=403, reason='Forbidden'):
+            browser.open(
+                deletion_url,
+                method='DELETE',
+                headers={
+                    'Accept': 'application/json',
+                },
+            )
+
+        self.assertTrue(self.shadow_document)
