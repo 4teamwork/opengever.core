cc944f120ab51adc537103cc13095ba41fd450a4
Joni Orponen <j.orponen@4teamwork.ch>
2017-10-16 14:27:10 +0200

    Optimize the integration testing feature flags for diffs.

--- opengever/testing/integration_test_case.py
+++ opengever/testing/integration_test_case.py
@@ -30,16 +30,15 @@ import timeit
 
 
 FEATURE_FLAGS = {
-    'meeting': 'opengever.meeting.interfaces.IMeetingSettings.is_feature_enabled',
-    'word-meeting': 'opengever.meeting.interfaces.IMeetingSettings.is_word_implementation_enabled',
-    'dossiertemplate': ('opengever.dossier.dossiertemplate'
-                        '.interfaces.IDossierTemplateSettings.is_feature_enabled'),
+    'activity': 'opengever.activity.interfaces.IActivitySettings.is_feature_enabled',
+    'bumblebee': 'opengever.bumblebee.interfaces.IGeverBumblebeeSettings.is_feature_enabled',
+    'dossiertemplate': 'opengever.dossier.dossiertemplate.interfaces.IDossierTemplateSettings.is_feature_enabled',
     'ech0147-export': 'opengever.ech0147.interfaces.IECH0147Settings.ech0147_export_enabled',
     'ech0147-import': 'opengever.ech0147.interfaces.IECH0147Settings.ech0147_import_enabled',
     'extjs': 'ftw.tabbedview.interfaces.ITabbedView.extjs_enabled',
-    'bumblebee': 'opengever.bumblebee.interfaces.IGeverBumblebeeSettings.is_feature_enabled',
-    'activity': 'opengever.activity.interfaces.IActivitySettings.is_feature_enabled'
-}
+    'meeting': 'opengever.meeting.interfaces.IMeetingSettings.is_feature_enabled',
+    'word-meeting': 'opengever.meeting.interfaces.IMeetingSettings.is_word_implementation_enabled',
+    }
 
 FEATURE_PROFILES = {
     'filing_number': 'opengever.dossier:filing',
