a601c72fcbc9d45c8fbe6b0e05a87d7bd33e24c4
Elio Schmutz <e.schmutz@4teamwork.ch>
2017-10-17 09:51:38 +0200

    Ungrok base/behaviors/sequence

--- opengever/base/behaviors/sequence.py
+++ opengever/base/behaviors/sequence.py
@@ -1,9 +1,7 @@
-from five import grok
 from zope.component import getUtility
 from plone.indexer import indexer
 from plone.directives import form
 from opengever.base.interfaces import ISequenceNumber
-from zope.lifecycleevent.interfaces import IObjectCopiedEvent
 
 
 class ISequenceNumberBehavior(form.Schema):
@@ -17,10 +15,8 @@ def sequence_number(obj):
     """ Indexer for the sequence_number """
     seqNumb = getUtility(ISequenceNumber)
     return seqNumb.get_number(obj)
-grok.global_adapter(sequence_number, name='sequence_number')
 
 
-@grok.subscribe(ISequenceNumberBehavior, IObjectCopiedEvent)
 def new_sequence_number(obj, event):
     """When a object was copied, the sequence number,
     would also be copied. This Event handler fix that problem,
