0f739de8d781f6f190c80c4e14f5fdcca9a20078
Elio Schmutz <e.schmutz@4teamwork.ch>
2017-10-17 10:24:01 +0200

    Ungrok base/indexes

--- opengever/base/indexes.py
+++ opengever/base/indexes.py
@@ -1,4 +1,3 @@
-from five import grok
 from opengever.base.behaviors.translated_title import ITranslatedTitle
 from opengever.base.behaviors.translated_title import ITranslatedTitleSupport
 from opengever.base.interfaces import IReferenceNumber
@@ -10,7 +9,6 @@ from plone.indexer import indexer
 def referenceIndexer(obj):
     ref_number = IReferenceNumber(obj).get_number()
     return ref_number
-grok.global_adapter(referenceIndexer, name="reference")
 
 
 @indexer(IDexterityContent)
@@ -19,13 +17,9 @@ def title_de_indexer(obj):
         return ITranslatedTitle(obj).title_de
     return None
 
-grok.global_adapter(title_de_indexer, name="title_de")
-
 
 @indexer(IDexterityContent)
 def title_fr_indexer(obj):
     if ITranslatedTitleSupport.providedBy(obj):
         return ITranslatedTitle(obj).title_fr
     return None
-
-grok.global_adapter(title_fr_indexer, name="title_fr")
