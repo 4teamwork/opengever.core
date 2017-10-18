5a7966e2c66b5ea9a4c892d363cda007f78dfd0d
Elio Schmutz <e.schmutz@4teamwork.ch>
2017-10-18 10:18:46 +0200

    Ungrok meeting/browser/meetingfeatureenabled

--- opengever/meeting/browser/meetingfeatureenabled.py
+++ opengever/meeting/browser/meetingfeatureenabled.py
@@ -1,12 +1,8 @@
-from five import grok
 from opengever.meeting import is_meeting_feature_enabled
-from zope.interface import Interface
+from Products.Five.browser import BrowserView
 
 
-class MeetingFeatureEnabledView(grok.View):
-    grok.context(Interface)
-    grok.name('is_meeting_feature_enabled')
-    grok.require('zope2.View')
+class MeetingFeatureEnabledView(BrowserView):
 
-    def render(self):
+    def __call__(self):
         return is_meeting_feature_enabled()
