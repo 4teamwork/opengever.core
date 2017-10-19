6a163910942289066c15da8231380a65610f5ec5
Lukas Graf <lukas.graf@4teamwork.ch>
2017-10-19 08:26:05 +0200

    Add indexer for bundle GUIDs and create index upon bundle import.

--- opengever/bundle/console.py
+++ opengever/bundle/console.py
@@ -2,10 +2,12 @@ from collective.transmogrifier.transmogrifier import Transmogrifier
 from opengever.base.interfaces import INoSeparateConnectionForSequenceNumbers
 from opengever.base.interfaces import IOpengeverBaseLayer
 from opengever.bundle.ldap import DisabledLDAP
+from opengever.bundle.loader import GUID_INDEX_NAME
 from opengever.bundle.sections.bundlesource import BUNDLE_KEY
 from opengever.bundle.sections.bundlesource import BUNDLE_PATH_KEY
 from opengever.core.debughelpers import get_first_plone_site
 from opengever.core.debughelpers import setup_plone
+from plone import api
 from zope.annotation import IAnnotations
 from zope.interface import alsoProvides
 import logging
@@ -33,6 +35,9 @@ def import_oggbundle(app, args):
     # order to avoid conflict errors during OGGBundle import
     alsoProvides(plone.REQUEST, INoSeparateConnectionForSequenceNumbers)
 
+    # Add index to track imported GUIDs (if it doesn't exist yet)
+    add_guid_index()
+
     transmogrifier = Transmogrifier(plone)
     IAnnotations(transmogrifier)[BUNDLE_PATH_KEY] = bundle_path
 
@@ -50,6 +55,25 @@ def import_oggbundle(app, args):
     transaction.get().note("Finished import of OGGBundle %r" % bundle_path)
     transaction.commit()
     log.info("Done.")
+
+
+def add_guid_index():
+    """Adds a FieldIndex 'bundle_guid' if it doesn't exist yet.
+
+    This index is only used by OGGBundle imports, and is used to track GUIDs
+    of already imported objects and efficiently query them. This is necessary
+    to enable partial / delta imports, and to accurately query the catalog to
+    build reports about imported objects.
+
+    This index will likely be removed after successful migrations, so code
+    outside the bundle import MUST NOT rely on it being present.
+    """
+    catalog = api.portal.get_tool('portal_catalog')
+    if GUID_INDEX_NAME not in catalog.indexes():
+        log.info("Adding GUID index %r" % GUID_INDEX_NAME)
+        catalog.addIndex(GUID_INDEX_NAME, 'FieldIndex')
+    else:
+        log.info("GUID index %r already exists" % GUID_INDEX_NAME)
 
 
 def setup_logging():
