1d656e52820c9fcc9a3b83eba601d2e1f362e9c7
Joni Orponen <j.orponen@4teamwork.ch>
2017-11-03 15:50:17 +0100

    Port bumblebee overlay listing tests to integration tests.

--- opengever/bumblebee/tests/test_overlay_view.py
+++ opengever/bumblebee/tests/test_overlay_view.py
@@ -3,169 +3,152 @@ from ftw.builder import create
 from ftw.bumblebee.tests.helpers import asset as bumblebee_asset
 from ftw.testbrowser import browsing
 from opengever.bumblebee.browser.overlay import BumblebeeOverlayBaseView
-from opengever.bumblebee.interfaces import IGeverBumblebeeSettings
 from opengever.core.testing import OPENGEVER_FUNCTIONAL_BUMBLEBEE_LAYER
+from opengever.document.interfaces import ICheckinCheckoutManager
 from opengever.testing import FunctionalTestCase
+from opengever.testing import IntegrationTestCase
 from opengever.testing.helpers import create_document_version
-from plone import api
 from plone.namedfile.file import NamedBlobFile
 from zExceptions import NotFound
+from zope.component import getMultiAdapter
 import transaction
 
 
-class TestBumblebeeOverlayListing(FunctionalTestCase):
+class TestBumblebeeOverlayListing(IntegrationTestCase):
     """Test Bumblebee in overlay listings."""
 
-    layer = OPENGEVER_FUNCTIONAL_BUMBLEBEE_LAYER
+    features = (
+        'bumblebee',
+        )
 
     @browsing
     def test_render_bumblebee_overlay_listing(self, browser):
-        dossier = create(Builder('dossier'))
-        document = create(Builder('document').within(dossier))
+        self.login(self.regular_user, browser)
 
-        browser.login().visit(document, view="bumblebee-overlay-listing")
-
+        browser.open(self.document, view='bumblebee-overlay-listing')
         self.assertEqual(1, len(browser.css('#file-preview')))
 
     @browsing
     def test_open_pdf_in_a_new_window_disabled(self, browser):
-        dossier = create(Builder('dossier'))
-        document = create(Builder('document')
-                          .within(dossier)
-                          .attach_file_containing(
-                              bumblebee_asset('example.docx').bytes(),
-                              u'example.docx'))
+        self.login(self.regular_user, browser)
 
-        browser.login().visit(document, view="bumblebee-overlay-listing")
+        browser.open(self.document, view='bumblebee-overlay-listing')
 
         self.assertNotIn(
-            'target=', browser.css('.function-pdf-preview').first.outerHTML)
+            'target=',
+            browser.css('.function-pdf-preview').first.outerHTML,
+            )
 
     @browsing
     def test_open_pdf_in_a_new_window_enabled(self, browser):
-        api.portal.set_registry_record('open_pdf_in_a_new_window',
-                                       True,
-                                       interface=IGeverBumblebeeSettings)
+        self.activate_feature('bumblebee-open-pdf-new-tab')
+        self.login(self.regular_user, browser)
 
-        dossier = create(Builder('dossier'))
-        document = create(Builder('document')
-                          .within(dossier)
-                          .attach_file_containing(
-                              bumblebee_asset('example.docx').bytes(),
-                              u'example.docx'))
-
-        browser.login().visit(document, view='bumblebee-overlay-listing')
+        browser.open(self.document, view='bumblebee-overlay-listing')
 
         self.assertIn(
-            'target=', browser.css('.function-pdf-preview').first.outerHTML)
+            'target=',
+            browser.css('.function-pdf-preview').first.outerHTML,
+            )
 
     @browsing
     def test_actions_with_file(self, browser):
-        dossier = create(Builder('dossier'))
-        document = create(Builder('document')
-                          .within(dossier)
-                          .attach_file_containing(
-                              bumblebee_asset('example.docx').bytes(),
-                              u'example.docx'))
+        self.login(self.regular_user, browser)
 
-        browser.login().visit(document, view="bumblebee-overlay-listing")
+        browser.open(self.document, view='bumblebee-overlay-listing')
 
         self.assertEqual(
-            ['Edit metadata',
-             'Checkout and edit',
-             'Download copy',
-             'Open as PDF',
-             'Open detail view'],
-            browser.css('.file-action-buttons a').text)
+            [
+                'Edit metadata',
+                'Checkout and edit',
+                'Download copy',
+                'Open as PDF',
+                'Open detail view',
+                ],
+            browser.css('.file-action-buttons a').text,
+            )
 
     @browsing
     def test_actions_without_file(self, browser):
-        dossier = create(Builder('dossier'))
-        document = create(Builder('document').within(dossier))
+        self.login(self.regular_user, browser)
+        self.document.file = None
 
-        browser.login().visit(document, view="bumblebee-overlay-listing")
+        browser.open(self.document, view='bumblebee-overlay-listing')
 
         self.assertEqual(
-            ['Edit metadata', 'Open detail view'],
-            browser.css('.file-action-buttons a').text)
+            [
+                'Edit metadata',
+                'Open detail view',
+                ],
+            browser.css('.file-action-buttons a').text,
+            )
 
     @browsing
     def test_actions_with_checked_out_file(self, browser):
-        dossier = create(Builder('dossier'))
-        document = create(Builder('document')
-                          .within(dossier)
-                          .attach_file_containing(
-                              bumblebee_asset('example.docx').bytes(),
-                              u'example.docx')
-                          .checked_out())
+        self.login(self.regular_user, browser)
+        getMultiAdapter((self.document, self.request), ICheckinCheckoutManager).checkout()
 
-        browser.login().visit(document, view="bumblebee-overlay-listing")
+        browser.open(self.document, view='bumblebee-overlay-listing')
 
         self.assertEqual(
-            ['Edit metadata',
-             'Checkout and edit',
-             'Checkin without comment',
-             'Checkin with comment',
-             'Download copy',
-             'Open as PDF',
-             'Open detail view'],
-            browser.css('.file-action-buttons a').text)
+            [
+                'Edit metadata',
+                'Checkout and edit',
+                'Checkin without comment',
+                'Checkin with comment',
+                'Download copy',
+                'Open as PDF',
+                'Open detail view',
+                ],
+            browser.css('.file-action-buttons a').text,
+            )
 
     @browsing
     def test_actions_with_file_checked_out_by_another_user(self, browser):
-        create(Builder('user')
-               .with_userid('hans')
-               .with_roles('Contributor', 'Editor', 'Reader'))
-        dossier = create(Builder('dossier'))
-        document = create(Builder('document')
-                          .within(dossier)
-                          .attach_file_containing(
-                              bumblebee_asset('example.docx').bytes(),
-                              u'example.docx')
-                          .checked_out_by('hans'))
+        self.login(self.dossier_responsible, browser)
+        getMultiAdapter((self.document, self.request), ICheckinCheckoutManager).checkout()
+        self.login(self.regular_user, browser)
 
-        browser.login().visit(document, view="bumblebee-overlay-listing")
+        browser.open(self.document, view='bumblebee-overlay-listing')
 
         self.assertEqual(
-            ['Open as PDF',
-             'Open detail view'],
-            browser.css('.file-action-buttons a').text)
+            [
+                'Open as PDF',
+                'Open detail view',
+                ],
+            browser.css('.file-action-buttons a').text,
+            )
 
     @browsing
     def test_actions_with_mail(self, browser):
-        dossier = create(Builder('dossier'))
-        mail = create(Builder('mail')
-                      .with_dummy_message()
-                      .within(dossier))
+        self.login(self.regular_user, browser)
 
-        browser.login().visit(mail, view="bumblebee-overlay-listing")
+        browser.open(self.mail, view='bumblebee-overlay-listing')
 
         self.assertEqual(
-            ['Edit metadata',
-             'Download copy',
-             'Open as PDF',
-             'Open detail view'],
-            browser.css('.file-action-buttons a').text)
+            [
+                'Edit metadata',
+                'Download copy',
+                'Open as PDF',
+                'Open detail view',
+                ],
+            browser.css('.file-action-buttons a').text,
+            )
 
     @browsing
     def test_actions_with_versioned_document(self, browser):
-        dossier = create(Builder('dossier'))
-        document = create(Builder('document')
-                          .within(dossier)
-                          .attach_file_containing(
-                              bumblebee_asset('example.docx').bytes(),
-                              u'example.docx'))
+        self.login(self.regular_user, browser)
+        create_document_version(self.document, version_id=0)
 
-        document.file = NamedBlobFile(data='new', filename=u'example.docx')
-        create_document_version(document, version_id=1)
-        transaction.commit()
-
-        browser.login()
-        browser.open(document, view="bumblebee-overlay-document?version_id=1")
+        browser.open(self.document, view='bumblebee-overlay-document?version_id=0')
 
         self.assertEqual(
-            ['Download copy', 'Revert document'],
-            browser.css('.file-action-buttons a').text)
+            [
+                'Download copy',
+                'Revert document',
+                ],
+            browser.css('.file-action-buttons a').text,
+            )
 
 
 class TestBumblebeeOverlayDocument(FunctionalTestCase):
