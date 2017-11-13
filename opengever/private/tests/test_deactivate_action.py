16aef692b81f1ac0c63e090d39941c6c26d23acf
Pablo Verges <pablo.verges@4teamwork.ch>
2017-11-13 14:35:35 +0100

    Test if deactivate action is displayed

---
+++ opengever/private/tests/test_deactivate_action.py
@@ -0,0 +1,27 @@
+import unittest
+from ftw.testbrowser import browsing
+from ftw.testbrowser.pages import statusmessages
+from opengever.testing import IntegrationTestCase
+
+
+class TestDeleteActionInPrivateFolderTabbedViews(IntegrationTestCase):
+
+    @browsing
+    def test_deactivate_action_is_not_displayed_for_administrator(self, browser):
+        self.login(self.administrator, browser)
+        browser.open(self.private_dossier)
+        statusmessages.assert_no_error_messages()
+
+        self.assertEquals(
+            0,
+            len(browser.css('#workflow-transition-dossier-transition-deactivate')))
+
+    @browsing
+    def test_deactivate_action_is_not_displayed_for_owner(self, browser):
+        self.login(self.regular_user, browser)
+        browser.open(self.private_dossier)
+        statusmessages.assert_no_error_messages()
+
+        self.assertEquals(
+            0,
+            len(browser.css('#workflow-transition-dossier-transition-deactivate')))
