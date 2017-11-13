683c794c8dda267fc7c2a9c28d2011646fd30471
David Erni <david.erni@4teamwork.ch>
2017-11-13 14:24:12 +0100

    Rewrite tests to integration test cases.

--- opengever/dossier/tests/test_tabbed.py
+++ opengever/dossier/tests/test_tabbed.py
@@ -1,37 +1,29 @@
-from ftw.builder import Builder
-from ftw.builder import create
 from ftw.testbrowser import browsing
-from opengever.contact.interfaces import IContactSettings
-from opengever.testing import FunctionalTestCase
-from plone import api
-import transaction
+from opengever.testing import IntegrationTestCase
 
 
-class TestParticipationTab(FunctionalTestCase):
-
-    def setUp(self):
-        super(TestParticipationTab, self).setUp()
-        self.dossier = create(Builder('dossier'))
+class TestParticipationTab(IntegrationTestCase):
 
     @browsing
     def test_is_plone_object_implementation_when_contact_feature_is_disabled(self, browser):
-        api.portal.set_registry_record(
-            'is_feature_enabled', False, interface=IContactSettings)
-        transaction.commit()
+        self.login(self.dossier_responsible, browser)
 
-        browser.login().open(self.dossier, view='tabbed_view')
+        browser.open(self.dossier, view='tabbed_view')
         browser.click_on('Participants')
 
         self.assertEqual('http://nohost/plone/dossier-1/tabbed_view#participants',
                          browser.url)
 
+
+class TestContactParticipationTab(IntegrationTestCase):
+
+    features = ('contact',)
+
     @browsing
     def test_is_sql_object_implementation_when_contact_feature_is_enabled(self, browser):
-        api.portal.set_registry_record(
-            'is_feature_enabled', True, interface=IContactSettings)
-        transaction.commit()
+        self.login(self.dossier_responsible, browser)
 
-        browser.login().open(self.dossier, view='tabbed_view')
+        browser.open(self.dossier, view='tabbed_view')
         browser.click_on('Participations')
 
         self.assertEqual(
