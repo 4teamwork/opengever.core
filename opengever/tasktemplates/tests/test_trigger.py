ef8f11eef2f5e3f55445382cac753997f2e44b64
Philippe Gross <philippe.gross@4teamwork.ch>
2017-11-20 10:08:32 +0100

    Fixed and convert TaskTemplateTrigger tests to integration tests.

--- opengever/tasktemplates/tests/test_trigger.py
+++ opengever/tasktemplates/tests/test_trigger.py
@@ -4,64 +4,38 @@ from ftw.builder import Builder
 from ftw.builder import create
 from ftw.testbrowser import browsing
 from ftw.testbrowser.pages.statusmessages import error_messages
+from opengever.tasktemplates.content.tasktemplate import ITaskTemplate
 from opengever.tasktemplates.interfaces import IFromTasktemplateGenerated
-from opengever.testing import FunctionalTestCase
+from opengever.testing import IntegrationTestCase
 from plone import api
-from plone.app.testing import TEST_USER_ID
-import transaction
 
 
-class TestTriggeringTaskTemplate(FunctionalTestCase):
+class TestTriggeringTaskTemplate(IntegrationTestCase):
 
-    def setUp(self):
-        super(TestTriggeringTaskTemplate, self).setUp()
+    def trigger_tasktemplatefolder(
+            self, browser, folder=u'Verfahren Neuanstellung',
+            templates=None, documents=None):
 
-        create(Builder('ogds_user').id(u'hugo.boss'))
-        create(Builder('ogds_user').id(u'peter.meier'))
-        self.dossier = create(Builder('dossier')
-                              .having(responsible=u'peter.meier'))
-        self.templatefolder = create(Builder('templatefolder'))
-
-        self.folder1 = create(Builder('tasktemplatefolder')
-                         .within(self.templatefolder)
-                         .titled(u'Mitberichtsverfahren')
-                         .in_state('tasktemplatefolder-state-activ'))
-
-        create(Builder('tasktemplate')
-               .within(self.folder1)
-               .titled(u'Mitbericht FD')
-               .having(preselected=True, deadline=17, issuer=u'current_user',
-                       responsible_client=u'interactive_users',
-                       responsible=u'current_user'))
-        create(Builder('tasktemplate')
-               .within(self.folder1)
-               .titled(u'Mitbericht DI')
-               .having(preselected=False, deadline=3, issuer=u'hugo.boss',
-                       responsible_client=u'client1',
-                       responsible=u'hugo.boss'))
-        create(Builder('tasktemplate')
-               .within(self.folder1)
-               .titled(u'Mitbericht SD')
-               .having(preselected=False, deadline=5, issuer=u'responsible',
-                       responsible_client=u'interactive_users',
-                       responsible=u'responsible'))
-
-    def trigger_tasktemplatefolder(self, browser, folder='Mitberichtsverfahren', templates=[]):
-        browser.login().open(self.dossier, view='add-tasktemplate')
+        browser.open(self.dossier, view='add-tasktemplate')
         browser.fill({'Tasktemplatefolder': folder})
+        if documents:
+            browser.fill({'Related documents': documents})
         browser.click_on('Continue')
 
-        browser.fill({'Tasktemplates': templates})
+        if templates:
+            browser.fill({'Tasktemplates': templates})
+
         browser.click_on('Trigger')
 
     @browsing
     def test_redirects_back_and_show_statusmessage_when_no_active_tasktemplatefolder_exists(self, browser):
-        api.content.transition(
-            self.folder1,
-            transition='tasktemplatefolder-transition-activ-inactiv')
-        transaction.commit()
 
-        browser.login().open(self.dossier, view='add-tasktemplate')
+        self.login(self.regular_user, browser=browser)
+
+        self.set_workflow_state(
+            'tasktemplatefolder-state-inactiv', self.tasktemplatefolder)
+
+        browser.open(self.dossier, view='add-tasktemplate')
         self.assertEquals(self.dossier.absolute_url(), browser.url)
         self.assertEquals(
             ['Currently there are no active task template folders registered.'],
@@ -69,66 +43,94 @@ class TestTriggeringTaskTemplate(FunctionalTestCase):
 
     @browsing
     def test_all_active_tasktemplates_are_listed(self, browser):
+        self.login(self.regular_user, browser=browser)
+
         create(Builder('tasktemplatefolder')
                .titled(u'Einsprache abarbeiten'))
         create(Builder('tasktemplatefolder')
                .titled(u'Einb\xfcrgerungsverfahren')
                .in_state('tasktemplatefolder-state-activ'))
 
-        browser.login().open(self.dossier, view='add-tasktemplate')
+        browser.open(self.dossier, view='add-tasktemplate')
 
         self.assertEquals(
-            [u'Einb\xfcrgerungsverfahren', 'Mitberichtsverfahren'],
+            [u'Einb\xfcrgerungsverfahren', 'Verfahren Neuanstellung'],
             browser.css('#formfield-form-widgets-tasktemplatefolder').first.options)
 
     @browsing
     def test_step2_list_all_tasktemplates_of_the_selected_folder_and_preselects_them_correctly(self, browser):
-        browser.login().open(self.dossier, view='add-tasktemplate')
-        browser.fill({'Tasktemplatefolder': 'Mitberichtsverfahren'})
+        self.login(self.regular_user, browser=browser)
+
+        create(Builder('tasktemplate')
+               .titled(u'Notebook einrichten.')
+               .having(issuer='responsible',
+                       responsible_client='fa',
+                       responsible='robert.ziegler',
+                       deadline=10,
+                       preselected=True)
+               .within(self.tasktemplatefolder))
+
+        create(Builder('tasktemplate')
+               .titled(u'User Accounts erstellen.')
+               .having(issuer='responsible',
+                       responsible_client='fa',
+                       responsible='robert.ziegler',
+                       deadline=10,
+                       preselected=False)
+        .within(self.tasktemplatefolder))
+
+        browser.open(self.dossier, view='add-tasktemplate')
+        browser.fill({'Tasktemplatefolder': 'Verfahren Neuanstellung'})
         browser.click_on('Continue')
 
-        self.assertEquals(
-            ['Mitbericht FD', 'Mitbericht DI', 'Mitbericht SD'],
+        self.assertItemsEqual(
+            ['Notebook einrichten.', 'User Accounts erstellen.',
+             'Arbeitsplatz einrichten.'],
             browser.css('#formfield-form-widgets-tasktemplates .option').text)
 
-        self.assertEquals(
-            ['Mitbericht FD'],
+        self.assertItemsEqual(
+            ['Notebook einrichten.'],
             browser.css('#formfield-form-widgets-tasktemplates input[checked]').getparents().text)
 
     @browsing
     def test_creates_main_task_assigned_to_current_user(self, browser):
+        self.login(self.regular_user, browser=browser)
         self.trigger_tasktemplatefolder(
-            browser, templates=['Mitbericht FD', 'Mitbericht DI'])
+            browser, templates=[u'Arbeitsplatz einrichten.'])
 
-        main_task = self.dossier.get('task-1')
-        self.assertEquals(u'Mitberichtsverfahren', main_task.title)
-        self.assertEquals(TEST_USER_ID, main_task.responsible)
-        self.assertEquals(TEST_USER_ID, main_task.issuer)
+        main_task = self.dossier.listFolderContents()[-1]
+        self.assertEquals(u'Verfahren Neuanstellung', main_task.title)
+        self.assertEquals(self.regular_user.getId(), main_task.responsible)
+        self.assertEquals(self.regular_user.getId(), main_task.issuer)
         self.assertEquals('direct-execution', main_task.task_type)
 
     @browsing
     def test_sets_main_task_to_in_progress_state(self, browser):
+        self.login(self.regular_user, browser=browser)
         self.trigger_tasktemplatefolder(
-            browser, templates=['Mitbericht FD', 'Mitbericht DI'])
+            browser, templates=['Arbeitsplatz einrichten.'])
 
-        main_task = self.dossier.get('task-1')
+        main_task = self.dossier.listFolderContents()[-1]
         self.assertEquals('task-state-in-progress',
                           api.content.get_state(main_task))
 
     @browsing
     def test_main_task_deadline_is_the_highest_template_deadline_plus_five(self, browser):
+        self.login(self.regular_user, browser=browser)
         self.trigger_tasktemplatefolder(
-            browser, templates=['Mitbericht FD', 'Mitbericht DI'])
+            browser, templates=['Arbeitsplatz einrichten.'])
 
-        self.assertEquals(date.today() + timedelta(days=17 + 5),
-                          self.dossier.get('task-1').deadline)
+        main_task = self.dossier.listFolderContents()[-1]
+        self.assertEquals(
+            date.today() + timedelta(days=10 + 5), main_task.deadline)
 
     @browsing
     def test_all_tasks_are_marked_with_marker_interface(self, browser):
+        self.login(self.regular_user, browser=browser)
         self.trigger_tasktemplatefolder(
-            browser, templates=['Mitbericht FD', 'Mitbericht DI'])
+            browser, templates=['Arbeitsplatz einrichten.'])
 
-        main_task = self.dossier.get('task-1')
+        main_task = self.dossier.listFolderContents()[-1]
         self.assertTrue(IFromTasktemplateGenerated.providedBy(main_task))
 
         for subtask in main_task.listFolderContents():
@@ -136,70 +138,72 @@ class TestTriggeringTaskTemplate(FunctionalTestCase):
 
     @browsing
     def test_creates_a_subtask_for_each_selected_template(self, browser):
-        self.trigger_tasktemplatefolder(
-            browser, templates=['Mitbericht FD', 'Mitbericht SD'])
+        self.login(self.regular_user, browser=browser)
+        create(Builder('tasktemplate')
+               .titled(u'Notebook einrichten.')
+               .having(issuer='responsible',
+                       responsible_client='fa',
+                       responsible='robert.ziegler',
+                       deadline=10,
+                       preselected=True)
+        .within(self.tasktemplatefolder))
 
-        main_task = self.dossier.get('task-1')
+        create(Builder('tasktemplate')
+               .titled(u'User Accounts erstellen.')
+               .having(issuer='responsible',
+                       responsible_client='fa',
+                       responsible='robert.ziegler',
+                       deadline=10,
+                       preselected=False)
+        .within(self.tasktemplatefolder))
+
+        self.trigger_tasktemplatefolder(
+            browser, templates=['Arbeitsplatz einrichten.',
+                                u'User Accounts erstellen.'])
+
+        main_task = self.dossier.listFolderContents()[-1]
         self.assertEquals(2, len(main_task.listFolderContents()))
 
-        subtask1, subtask2 = main_task.listFolderContents()
-        self.assertEquals('Mitbericht FD', subtask1.title)
-        self.assertEquals('Mitbericht SD', subtask2.title)
+        self.assertItemsEqual(
+            [u'Arbeitsplatz einrichten.', u'User Accounts erstellen.'],
+            [item.title for item in main_task.listFolderContents()])
 
     @browsing
     def test_replace_interactive_issuer(self, browser):
+        self.login(self.regular_user, browser=browser)
         self.trigger_tasktemplatefolder(
-            browser,
-            templates=['Mitbericht FD', 'Mitbericht DI', 'Mitbericht SD'])
+            browser, templates=['Arbeitsplatz einrichten.'])
 
-        main_task = self.dossier.get('task-1')
-        subtask1, subtask2, subtask3 = main_task.listFolderContents()
+        main_task = self.dossier.listFolderContents()[-1]
+        subtask = main_task.listFolderContents()[0]
 
-        # current_user
-        self.assertEquals(TEST_USER_ID, subtask1.issuer)
-
-        # not interactive
-        self.assertEquals('hugo.boss', subtask2.issuer)
-
-        # responsible
-        self.assertEquals('peter.meier', subtask3.issuer)
+        self.assertEquals(self.dossier_responsible.getId(), subtask.issuer)
 
     @browsing
-    def test_replace_interactive_responsibles(self, browser):
+    def test_replace_interactive_responsible(self, browser):
+        self.login(self.regular_user, browser=browser)
+
+        ITaskTemplate(self.tasktemplate).responsible = 'current_user'
         self.trigger_tasktemplatefolder(
-            browser,
-            templates=['Mitbericht FD', 'Mitbericht DI', 'Mitbericht SD'])
+            browser, templates=['Arbeitsplatz einrichten.'])
 
-        main_task = self.dossier.get('task-1')
-        subtask1, subtask2, subtask3 = main_task.listFolderContents()
-
-        # current_user
-        self.assertEquals(TEST_USER_ID, subtask1.responsible)
-        self.assertEquals('client1', subtask1.responsible_client)
-
-        # not interactive
-        self.assertEquals('hugo.boss', subtask2.responsible)
-        self.assertEquals('client1', subtask1.responsible_client)
-
-        # responsible
-        self.assertEquals('peter.meier', subtask3.responsible)
-        self.assertEquals('client1', subtask1.responsible_client)
+        main_task = self.dossier.listFolderContents()[-1]
+        subtask = main_task.listFolderContents()[0]
+        self.assertEquals(self.regular_user.getId(), subtask.responsible)
 
     @browsing
-    def set_relateditems_on_every_subtask_when_selected(self, browser):
-        doc1 = create(Builder('document')
-                      .within(self.dossier)
-                      .titled(u'Doc A'))
-        doc2 = create(Builder('document')
-                      .within(self.dossier)
-                      .titled(u'Doc B'))
+    def test_set_relateditems_on_every_subtask_when_selected(self, browser):
+        self.login(self.regular_user, browser=browser)
+
+        ITaskTemplate(self.tasktemplate).responsible = 'current_user'
 
         self.trigger_tasktemplatefolder(
-            browser, templates=['Mitbericht FD', 'Mitbericht SD'],
-            documents=['Doc A', 'Doc B'])
+            browser, templates=['Arbeitsplatz einrichten.'],
+            documents=[self.document])
 
         main_task = self.dossier.get('task-1')
-        subtask1, subtask2 = main_task.listFolderContents()
+        subtask = main_task.listFolderContents()[0]
 
-        self.assertEquals([doc1, doc2], subtask1.relatedItems)
-        self.assertEquals([doc1, doc2], subtask2.relatedItems)
+        self.assertEquals(
+            [self.document],
+            [relation.to_object for relation in subtask.relatedItems])
