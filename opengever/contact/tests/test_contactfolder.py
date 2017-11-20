84f0122bebc9929293973af60a82365e463dce46
Philippe Gross <philippe.gross@4teamwork.ch>
2017-11-20 10:45:08 +0100

    Convert contact folder tests to integration tests cases.

--- opengever/contact/tests/test_contactfolder.py
+++ opengever/contact/tests/test_contactfolder.py
@@ -1,29 +1,27 @@
 # -*- coding: utf-8 -*-
 
-from ftw.builder import Builder
-from ftw.builder import create
 from ftw.testbrowser import browsing
 from ftw.testbrowser.pages import factoriesmenu
 from opengever.contact.interfaces import IContactFolder
 from opengever.testing import add_languages
-from opengever.testing import FunctionalTestCase
+from opengever.testing import IntegrationTestCase
 
 
-class TestContactFolder(FunctionalTestCase):
+class TestContactFolder(IntegrationTestCase):
 
     def setUp(self):
         super(TestContactFolder, self).setUp()
-        self.grant('Manager')
-
         add_languages(['de-ch', 'fr-ch'])
 
     def test_provides_marker_interface(self):
-        contactfolder = create(Builder('contactfolder'))
-        self.assertTrue(IContactFolder.providedBy(contactfolder))
+        self.login(self.manager)
+        self.assertTrue(IContactFolder.providedBy(self.contactfolder))
 
     @browsing
     def test_supports_translated_title(self, browser):
-        browser.login().open()
+        self.login(self.manager, browser=browser)
+
+        browser.open()
         factoriesmenu.add('ContactFolder')
         browser.fill({'Title (German)': u'Kontakte',
                       'Title (French)': u'Contacts'})
@@ -35,13 +33,8 @@ class TestContactFolder(FunctionalTestCase):
         browser.find('Deutsch').click()
         self.assertEquals("Kontakte", browser.css('h1').first.text)
 
-    @browsing
-    def test_portlets_inheritance_is_blocked(self, browser):
-        browser.login().open()
-        factoriesmenu.add('ContactFolder')
-        browser.fill({'Title (German)': u'Kontakte',
-                      'Title (French)': u'Contacts'})
-        browser.find('Save').click()
+    def test_portlets_inheritance_is_blocked(self):
+        self.login(self.manager)
 
         self.assert_portlet_inheritance_blocked(
-            'plone.leftcolumn', browser.context)
+            'plone.leftcolumn', self.contactfolder)
