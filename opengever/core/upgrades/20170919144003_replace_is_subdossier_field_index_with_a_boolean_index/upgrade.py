be6db7dcf60666c3d3efbc41097262d45a98bd06
David Erni <dave.erni@gmail.com>
2017-11-21 16:03:21 +0100

    Merge pull request #3642 from 4teamwork/mle-optimize-upgrade-steps
    Upgradestep optimizations

--- opengever/core/upgrades/20170919144003_replace_is_subdossier_field_index_with_a_boolean_index/upgrade.py
+++ opengever/core/upgrades/20170919144003_replace_is_subdossier_field_index_with_a_boolean_index/upgrade.py
@@ -1,15 +1,17 @@
 from ftw.upgrade import UpgradeStep
-from opengever.dossier.behaviors.dossier import IDossierMarker
 
 
 INDEX_NAME = 'is_subdossier'
 
 
 class ReplaceIsSubdossierFieldIndexWithABooleanIndex(UpgradeStep):
-    """Replace is_subdossier field index with a boolean index.
+    """Replace is_subdossier field index with a boolean index if
+    necessary.
     """
 
     def __call__(self):
-        self.catalog_remove_index(INDEX_NAME)
-        self.catalog_add_index(INDEX_NAME, 'BooleanIndex')
-        self.catalog_rebuild_index(INDEX_NAME)
+        index = self.portal.portal_catalog.Indexes.get(INDEX_NAME)
+        if index.__class__.__name__ != 'BooleanIndex':
+            self.catalog_remove_index(INDEX_NAME)
+            self.catalog_add_index(INDEX_NAME, 'BooleanIndex')
+            self.catalog_rebuild_index(INDEX_NAME)
