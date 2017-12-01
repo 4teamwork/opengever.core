44fe4453e03e73af89438ddd55e20fc6d116fee2
Philippe Gross <philippe.gross@4teamwork.ch>
2017-12-01 21:00:25 +0100

    Convert TestBreadcrumbByUIDView tests to integration tests.

--- opengever/base/tests/test_breadcrumb_by_uid.py
+++ opengever/base/tests/test_breadcrumb_by_uid.py
@@ -1,34 +1,31 @@
-from ftw.builder import Builder
-from ftw.builder import create
-from opengever.testing import FunctionalTestCase
+from opengever.testing import IntegrationTestCase
 from plone.uuid.interfaces import IUUID
 
 
-class TestBreadcrumbByUIDView(FunctionalTestCase):
+class TestBreadcrumbByUIDView(IntegrationTestCase):
 
-    def setUp(self):
-        super(TestBreadcrumbByUIDView, self).setUp()
-
-        self.root = create(Builder('repository_root').titled(u'Repository'))
-        self.repo = create(Builder('repository')
-                           .within(self.root)
-                           .titled(u'Testposition'))
-        self.dossier = create(Builder('dossier')
-                              .within(self.repo)
-                              .titled(u'Dossier 1'))
-
-        self.view = self.portal.restrictedTraverse('breadcrumb_by_uid')
 
     def test_resolve_dossier(self):
+        self.login(self.regular_user)
+
         self.portal.REQUEST['ploneuid'] = IUUID(self.dossier)
+
         view = self.portal.restrictedTraverse('breadcrumb_by_uid')
-        self.assertEquals('Repository > 1. Testposition > Dossier 1', view())
+        self.assertEquals(
+            'Ordnungssystem > 1. F\xc3\xbchrung > 1.1. Vertr\xc3\xa4ge '
+            'und Vereinbarungen > Vertr\xc3\xa4ge mit der kantonalen '
+            'Finanzverwaltung',
+            view())
 
     def test_raise_attributerror_if_no_uid_is_provided(self):
+        self.login(self.regular_user)
+
         with self.assertRaises(AttributeError):
             self.portal.restrictedTraverse('breadcrumb_by_uid')()
 
     def test_raise_attributerror_if_uid_is_unkown(self):
+        self.login(self.regular_user)
+
         self.portal.REQUEST['ploneuid'] = 'doesnotexists'
         view = self.portal.restrictedTraverse('breadcrumb_by_uid')
         with self.assertRaises(AttributeError):
