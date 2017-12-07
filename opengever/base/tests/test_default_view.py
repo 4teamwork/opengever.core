f01d06b66f8a557b900bc759a74ec6cebeaef220
Philippe Gross <philippe.gross@4teamwork.ch>
2017-12-08 00:29:00 +0100

    Convert TestOGDefaultView to integration tests.

--- opengever/base/tests/test_default_view.py
+++ opengever/base/tests/test_default_view.py
@@ -1,14 +1,14 @@
 from ftw.builder import Builder
 from ftw.builder import create
 from ftw.testbrowser import browsing
-from opengever.testing import FunctionalTestCase
+from opengever.testing import IntegrationTestCase
 
 
-class TestOGDefaultView(FunctionalTestCase):
+class TestOGDefaultView(IntegrationTestCase):
 
     @browsing
     def tests_default_view_does_not_show_help_messages(self, browser):
-        task = create(Builder('task'))
-        browser.login().open(task, view='view')
+        self.login(self.regular_user, browser=browser)
 
+        browser.open(self.task, view='view')
         self.assertEquals([], browser.css('.formHelp'))
