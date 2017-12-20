81766413c00bd6193add858d0b0df7702e6d532d
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2017-12-20 18:57:06 +0100

    Add workspace root tabbed_view with one tab listing workspaces.

---
+++ opengever/workspace/browser/tabs.py
@@ -0,0 +1,36 @@
+from ftw.table import helper
+from opengever.tabbedview.browser.tabs import Dossiers
+from opengever.tabbedview.helper import linked
+from opengever.tabbedview.helper import readable_ogds_author
+from opengever.tabbedview import _
+
+
+class Workspaces(Dossiers):
+
+    filterlist_available = False
+
+    columns = (
+
+        {'column': '',
+         'column_title': '',
+         'transform': helper.path_checkbox,
+         'sortable': False,
+         'groupable': False,
+         'width': 30},
+
+        {'column': 'reference',
+         'column_title': _(u'label_reference', default=u'Reference Number'),
+         'width': 120},
+
+        {'column': 'Title',
+         'column_title': _(u'label_title', default=u'Title'),
+         'sort_index': 'sortable_title',
+         'transform': linked,
+         'width': 500},
+
+        {'column': 'responsible',
+         'column_title': _(u'label_dossier_responsible',
+                           default=u"Responsible"),
+         'transform': readable_ogds_author,
+         'width': 300}
+    )
