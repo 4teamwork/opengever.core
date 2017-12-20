9ce2308cbfa68810b28b953e256374488e66fb22
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2017-12-20 15:54:32 +0100

    Remove workspace specific sequence key from dossier implementation.

--- opengever/dossier/sequence.py
+++ opengever/dossier/sequence.py
@@ -1,6 +1,5 @@
 from opengever.base.sequence import DefaultSequenceNumberGenerator
 from opengever.dossier.behaviors.dossier import IDossierMarker
-from opengever.workspace.interfaces import IWorkspace
 from zope.component import adapter
 
 
@@ -9,12 +8,4 @@ class DossierSequenceNumberGenerator(DefaultSequenceNumberGenerator):
     """ All dossier-types should use the same range/key of sequence numbers.
     """
 
-    @property
-    def key(self):
-        if IWorkspace.providedBy(self.context):
-            # Workspaces provide the IWorkspace interface, but also
-            # the IDossierMarker interface (behavior).
-            # Registering the adapter for IWorkspace does not override the
-            # IDossierMarker adapter, since they do not subclass each other.
-            return 'WorkspaceSequenceNumberGenerator'
-        return 'DossierSequenceNumberGenerator'
+    key = 'DossierSequenceNumberGenerator'
