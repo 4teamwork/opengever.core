46c4dc615c76d8cbef7ae86f026707d985fbcbfd
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2018-01-12 10:59:05 +0100

    Avoid circular imports.

--- opengever/workspace/__init__.py
+++ opengever/workspace/__init__.py
@@ -1,4 +1,3 @@
-from opengever.workspace.interfaces import IWorkspaceSettings
 from plone import api
 from zope.i18nmessageid import MessageFactory
 
@@ -6,5 +5,6 @@ _ = MessageFactory('opengever.workspace')
 
 
 def is_workspace_feature_enabled():
+    from opengever.workspace.interfaces import IWorkspaceSettings
     return api.portal.get_registry_record(
         'is_feature_enabled', interface=IWorkspaceSettings)
