9390554d6b466ce2a468e10c747afe1a3849e34b
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2018-01-17 16:45:48 +0100

    Also list pending invitations.

--- opengever/workspace/participation/browser/manage_participants.py
+++ opengever/workspace/participation/browser/manage_participants.py
@@ -1,3 +1,4 @@
+from opengever.workspace.participation.storage import IInvitationStorage
 from plone import api
 from Products.Five.browser import BrowserView
 import json
@@ -6,7 +7,9 @@ import json
 class ManageParticipants(BrowserView):
 
     def __call__(self):
-        return self.json_response(self.get_participants())
+        return self.json_response(
+            self.get_participants() + self.get_pending_invitations()
+        )
 
     def json_response(self, data):
         response = self.request.response
@@ -26,27 +29,48 @@ class ManageParticipants(BrowserView):
         for userid, roles in self.context.get_local_roles():
             member = api.user.get(userid=userid)
             if member is not None:
-                email = member.getProperty('email', '').decode('utf-8')
-                name = member.getProperty('fullname', '').decode('utf-8')
                 item = dict(userid=userid,
                             roles=roles,
                             can_manage=self.can_manage_member(member),
-                            type_='user')
-
-                if name and email:
-                    item['name'] = u'{} ({})'.format(name, email)
-                elif name:
-                    item['name'] = name
-                else:
-                    item['name'] = userid.decode('utf-8')
+                            type_='user',
+                            name=self.get_full_user_info(member=member))
                 entries.append(item)
         return entries
 
-    def can_manage_member(self, member):
+    def can_manage_member(self, member=None):
 
-        if member.getId() == api.user.get_current().getId():
+        if member and member.getId() == api.user.get_current().getId():
             return False
         else:
             return api.user.has_permission(
                 'Sharing page: Delegate WorkspaceAdmin role',
                 obj=self.context)
+
+    def get_pending_invitations(self):
+        storage = IInvitationStorage(self.context)
+        entries = []
+
+        for invitation in storage.get_invitations_for_context(self.context):
+            item = dict(name=self.get_full_user_info(userid=invitation.userid),
+                        roles=invitation.role,
+                        inviter=self.get_full_user_info(userid=invitation.inviter),
+                        can_manage=self.can_manage_member(),
+                        type_='invitation',
+                        iid=invitation.iid)
+            entries.append(item)
+
+        return entries
+
+    def get_full_user_info(self, userid=None, member=None):
+        if member is None:
+            member = api.user.get(userid=userid)
+
+        email = member.getProperty('email', '').decode('utf-8')
+        name = member.getProperty('fullname', '').decode('utf-8')
+
+        if name and email:
+            return u'{} ({})'.format(name, email)
+        elif name:
+            return name
+        else:
+            return userid.decode('utf-8')
--- opengever/workspace/tests/test_workspace_manage_participants.py
+++ opengever/workspace/tests/test_workspace_manage_participants.py
@@ -1,5 +1,9 @@
+from ftw.builder import Builder
+from ftw.builder import create
 from ftw.testbrowser import browsing
 from opengever.testing import IntegrationTestCase
+from opengever.workspace.participation.invitation import Invitation
+from opengever.workspace.participation.storage import IInvitationStorage
 
 
 def get_entry_by_userid(entries, userid):
@@ -13,7 +17,6 @@ class TestWorkspaceManageParticipants(IntegrationTestCase):
 
     def setUp(self):
         super(TestWorkspaceManageParticipants, self).setUp()
-        # self.storage = InvitationStorage(self.workspace)
 
     @browsing
     def test_list_all_current_participants(self, browser):
@@ -50,10 +53,47 @@ class TestWorkspaceManageParticipants(IntegrationTestCase):
     def test_current_logged_in_admin_cannot_manage_himself(self, browser):
         self.login(self.workspace_admin, browser=browser)
         browser.visit(self.workspace, view='manage-participants')
-
         self.assertFalse(
-            get_entry_by_userid(browser.json, 'fridolin.hugentobler')['can_manage'],
+            get_entry_by_userid(browser.json, 'fridolin.hugentobler')[
+                'can_manage'],
             'The admin should not be able to manage himself')
         self.assertTrue(
             get_entry_by_userid(browser.json, 'hans.peter')['can_manage'],
             'The admin should be able to manage hans.peter')
+
+    @browsing
+    def test_user_without_sharing_permission_cannot_manage(self, browser):
+        self.login(self.workspace_member, browser=browser)
+        browser.visit(self.workspace, view='manage-participants')
+        self.assertFalse(
+            get_entry_by_userid(browser.json, 'gunther.frohlich')[
+                'can_manage'],
+            'The admin should not be able to manage himself')
+
+    @browsing
+    def test_list_all_pending_invitations(self, browser):
+        self.login(self.workspace_admin, browser=browser)
+        storage = IInvitationStorage(self.workspace)
+        workspace2 = create(Builder('workspace')
+                            .within(self.workspace_root)
+                            .titled(u'Second workspace'))
+        invitation = Invitation(workspace2, self.regular_user.getId(),
+                                self.workspace_admin.getId(), 'WorkspacesUser')
+        storage.add_invitation(invitation)
+
+        browser.visit(workspace2, view='manage-participants')
+        self.assertEquals(
+            [
+                {u'can_manage': False,
+                 u'userid': u'fridolin.hugentobler',
+                 u'type_': u'user',
+                 u'roles': [u'WorkspaceOwner'],
+                 u'name': u'Hugentobler Fridolin (fridolin.hugentobler@gever.local)'},
+                {u'name': u'B\xe4rfuss K\xe4thi (kathi.barfuss@gever.local)',
+                 u'roles': u'WorkspacesUser',
+                 u'can_manage': True,
+                 u'iid': invitation.iid,
+                 u'type_': u'invitation',
+                 u'inviter': u'Hugentobler Fridolin (fridolin.hugentobler@gever.local)'}
+            ],
+            browser.json)
