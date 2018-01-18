21d8f89a8169855903eeb937b71e19f0c5ba620b
Philippe Gross <philippe.gross@4teamwork.ch>
2018-01-18 16:18:16 +0100

    Extract base functionality to send a mail in to a mixin.

--- opengever/activity/mail.py
+++ opengever/activity/mail.py
@@ -1,12 +1,6 @@
-from email.header import Header
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 from opengever.activity.browser import resolve_notification_url
 from opengever.activity.dispatcher import NotificationDispatcher
-from opengever.base.model import get_locale
-from opengever.mail.utils import make_addr_header
-from opengever.ogds.base.utils import ogds_service
-from plone import api
+from opengever.activity.mailer import Mailer
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from zope.i18n import translate
 from zope.i18nmessageid import MessageFactory
@@ -16,7 +10,7 @@ from zope.i18nmessageid import MessageFactory
 _ = MessageFactory("opengever.activity")
 
 
-class PloneNotificationMailer(NotificationDispatcher):
+class PloneNotificationMailer(NotificationDispatcher, Mailer):
     """The PloneNotificationMailer is a notification dispatcher,
     which generates mail(s) for passed notification(s) and send them to
     the corresponding watcher.
@@ -24,58 +18,29 @@ class PloneNotificationMailer(NotificationDispatcher):
 
     _id = 'mail'
     roles_key = 'mail_notification_roles'
-
-    def __init__(self):
-        self.mailhost = api.portal.get_tool('MailHost')
-
-        # This is required by ViewPageTemplateFile for
-        # the html mail-template
-        self.context = api.portal.get()
-        self.request = self.context.REQUEST
+    template = ViewPageTemplateFile("templates/notification.pt")
 
     def dispatch_notification(self, notification):
-        msg = self.prepare_mail(notification)
+        msg = self.prepare_mail(
+            subject=self.get_subject(notification),
+            to_userid=notification.userid,
+            from_userid=notification.activity.actor_id,
+            data=self.get_data(notification)
+        )
         self.send_mail(msg)
-
-    def send_mail(self, msg):
-        self.mailhost.send(msg)
-
-    def prepare_mail(self, notification):
-        msg = MIMEMultipart('alternative')
-
-        actor = ogds_service().fetch_user(notification.activity.actor_id)
-        msg['From'] = make_addr_header(actor.fullname(), actor.email, 'utf-8')
-
-        recipient = ogds_service().fetch_user(notification.userid)
-        msg['To'] = recipient.email
-        msg['Subject'] = self.get_subject(notification)
-
-        html = self.prepare_html(notification)
-        msg.attach(MIMEText(html.encode('utf-8'), 'html', 'utf-8'))
-
-        return msg
 
     def get_subject(self, notification):
         prefix = translate(_(u'subject_prefix', default=u'GEVER Task'),
                            context=self.request)
         title = notification.activity.translations[self.get_users_language()].title
-        return Header(u'{}: {}'.format(prefix, title), 'utf-8')
+        return u'{}: {}'.format(prefix, title)
 
-    def get_users_language(self):
-        # XXX TODO Right now there is no support to store users preferred
-        # language. Therefore we send the mails always in the current selected
-        # language.
-        return get_locale()
-
-    def prepare_html(self, notification):
-        template = ViewPageTemplateFile("templates/notification.pt")
+    def get_data(self, notification):
         language = self.get_users_language()
-        options = {
+        return {
             'title': notification.activity.translations[language].title,
             'label': notification.activity.translations[language].label,
             'summary': notification.activity.translations[language].summary,
             'description': notification.activity.translations[language].description,
             'link': resolve_notification_url(notification)
         }
-
-        return template(self, **options)
---
+++ opengever/activity/mailer.py
@@ -0,0 +1,52 @@
+from email.header import Header
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from opengever.base.model import get_locale
+from opengever.mail.utils import make_addr_header
+from opengever.ogds.base.utils import ogds_service
+from plone import api
+
+
+class Mailer(object):
+    """Simple mixin, which helps to send mails, used by notification
+    and digest mails.
+    """
+
+    def __init__(self):
+        self.mailhost = api.portal.get_tool('MailHost')
+
+        # This is required by ViewPageTemplateFile for
+        # the html mail-template
+        self.context = api.portal.get()
+        self.request = self.context.REQUEST
+
+    def send_mail(self, msg):
+        self.mailhost.send(msg)
+
+    def prepare_mail(self, subject=u'', to_userid=None, from_userid=None, data={}):
+        msg = MIMEMultipart('alternative')
+
+        if from_userid:
+            actor = ogds_service().fetch_user(from_userid)
+            msg['From'] = make_addr_header(actor.fullname(),
+                                           actor.email, 'utf-8')
+        else:
+            msg['From'] = make_addr_header(
+                u'OneGov GEVER', api.portal.get().email_from_address, 'utf-8')
+
+        recipient = ogds_service().fetch_user(to_userid)
+        msg['To'] = recipient.email
+        msg['Subject'] = Header(subject, 'utf-8')
+
+        html = self.prepare_html(data)
+        msg.attach(MIMEText(html.encode('utf-8'), 'html', 'utf-8'))
+        return msg
+
+    def get_users_language(self):
+        # XXX TODO Right now there is no support to store users preferred
+        # language. Therefore we send the mails always in the current selected
+        # language.
+        return get_locale()
+
+    def prepare_html(self, data):
+        return self.template(self, **data)
