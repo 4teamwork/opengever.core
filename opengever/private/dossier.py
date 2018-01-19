0b1c0fc7e4177dc53d6b14a66f9bd70b652d6da9
Joni Orponen <j.orponen@4teamwork.ch>
2018-01-19 15:31:45 +0100

    Prefix the reference numbers on private dossiers with a P.

--- opengever/private/dossier.py
+++ opengever/private/dossier.py
@@ -1,6 +1,10 @@
+from opengever.base.interfaces import IReferenceNumberFormatter
+from opengever.base.interfaces import IReferenceNumberSettings
 from opengever.dossier.base import DossierContainer
 from opengever.dossier.businesscase import IBusinessCaseDossier
 from opengever.private.interfaces import IPrivateContainer
+from plone import api
+from zope.component import queryAdapter
 
 
 class IPrivateDossier(IBusinessCaseDossier, IPrivateContainer):
@@ -12,3 +16,23 @@ class PrivateDossier(DossierContainer):
     """A specific dossier type, only addable to a PrivateFolder in the
     private Area and only visible for the Owner and Managers.
     """
+
+    def get_reference_number(self):
+        """Prefix reference numbers of private dossiers with a 'P'."""
+        active_formatter_name = api.portal.get_registry_record(
+            name='formatter',
+            interface=IReferenceNumberSettings,
+            )
+
+        formatter = queryAdapter(
+            api.portal.get(),
+            IReferenceNumberFormatter,
+            name=active_formatter_name,
+            )
+
+        separator = getattr(formatter, 'repository_dossier_separator', u' ')
+
+        return separator.join((
+            'P',
+            super(PrivateDossier, self).get_reference_number(),
+            ))
