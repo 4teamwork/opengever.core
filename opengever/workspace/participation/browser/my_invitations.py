09fba4e9ff15365d82b1a173ee6eedcfa2fe6ee6
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2018-01-22 12:24:08 +0100

    Implement accept endpoint for my-invitations view.

--- opengever/workspace/participation/browser/my_invitations.py
+++ opengever/workspace/participation/browser/my_invitations.py
@@ -1,9 +1,11 @@
 from opengever.base.security import elevated_privileges
+from opengever.workspace import _
 from opengever.workspace.participation.storage import IInvitationStorage
 from plone import api
 from plone.app.uuid.utils import uuidToObject
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zExceptions import BadRequest
 from zope.component import getUtility
 
 
@@ -18,10 +20,43 @@ class MyWorkspaceInvitations(BrowserView):
     def get_invitations(self):
         storage = getUtility(IInvitationStorage)
         userid = api.user.get_current().getId()
+        target_title = _(u'Deleted Workspace')
+
+        entries = list(storage.iter_invitions_for_recipient(userid))
+        entries.sort(key=lambda item: item['created'])
+
         for entry in storage.iter_invitions_for_recipient(userid):
             with elevated_privileges():
                 target = uuidToObject(entry['target_uuid'])
+                if target:
+                    target_title = target.Title()
 
             if target:
                 yield {'inviter': entry['inviter'],
-                       'target_title': target.Title()}
+                       'target_title': target_title,
+                       'iid': entry['iid']}
+
+    def accept(self):
+        """Accept a invitation by setting the role and redirect the user
+        to the workspace.
+        """
+        iid = self.request.get('iid', None)
+
+        if iid is None:
+            raise BadRequest('No iid given')
+
+        storage = getUtility(IInvitationStorage)
+        invitation = storage.get_invitation(iid)
+
+        if api.user.get_current().getId() != invitation['recipient']:
+            raise BadRequest('Wrong invitation')
+
+        else:
+            with elevated_privileges():
+                target = uuidToObject(invitation['target_uuid'])
+                target.manage_setLocalRoles(invitation['recipient'],
+                                            [invitation['role']])
+                target.reindexObjectSecurity()
+                storage.remove_invitation(iid)
+
+            return self.request.RESPONSE.redirect(target.absolute_url())
--- opengever/workspace/tests/test_my_invitations.py
+++ opengever/workspace/tests/test_my_invitations.py
@@ -13,32 +13,57 @@ class TestMyInvitationsView(IntegrationTestCase):
         self.login(self.workspace_admin)
         self.storage = getUtility(IInvitationStorage)
 
-    @browsing
-    def test_list_invitations_of_logged_in_user(self, browser):
-        self.login(self.workspace_admin, browser=browser)
-        workspace2 = create(Builder('workspace')
-                            .within(self.workspace_root)
-                            .titled(u'Second workspace'))
+        self.workspace2 = create(Builder('workspace')
+                                 .within(self.workspace_root)
+                                 .titled(u'Second workspace'))
 
         self.storage.add_invitation(
             self.workspace, self.regular_user.getId(),
             self.workspace_admin.getId(), 'WorkspaceGuest')
-
         self.storage.add_invitation(
-            workspace2, self.regular_user.getId(),
+            self.workspace2, self.regular_user.getId(),
             self.workspace_admin.getId(), 'WorkspaceGuest')
-
         self.storage.add_invitation(
-            workspace2, self.workspace_guest.getId(),
+            self.workspace2, self.workspace_guest.getId(),
             self.workspace_admin.getId(), 'WorkspaceGuest')
 
         self.workspace_root.manage_setLocalRoles(self.regular_user.getId(),
                                                  ['WorkspacesUser'])
 
+    @browsing
+    def test_list_invitations_of_logged_in_user(self, browser):
         self.login(self.regular_user, browser=browser)
         browser.visit(self.workspace_root, view='my-invitations')
-
         self.assertItemsEqual(
             ['fridolin.hugentobler Second workspace Accept Decline',
              'fridolin.hugentobler A Workspace Accept Decline'],
             browser.css('table.listing').first.body_rows.text)
+
+    @browsing
+    def test_accept_invition(self, browser):
+        self.login(self.regular_user, browser=browser)
+        browser.visit(self.workspace_root, view='my-invitations')
+        browser.css('a.AcceptInvitation').first.click()
+        self.assertEquals(self.workspace, browser.context)
+        self.assertEquals(1,
+                          len(tuple(self.storage.iter_invitions_for_recipient(
+                              self.regular_user.getId()))),
+                          'Expect only one item')
+
+    @browsing
+    def test_cannot_accept_invitation_of_other_users(self, browser):
+        self.login(self.regular_user, browser=browser)
+        foreign_iid = tuple(self.storage.iter_invitions_for_context(
+            self.workspace2))[0]['iid']
+
+        with browser.expect_http_error(400):
+            browser.open(self.workspace_root.absolute_url() + '/my-invitations/accept',
+                         data={'iid': foreign_iid})
+
+    @browsing
+    def test_cannot_accept_invalid_invitation(self, browser):
+        self.login(self.regular_user, browser=browser)
+
+        with browser.expect_http_error(503):
+            browser.open(self.workspace_root.absolute_url() + '/my-invitations/accept',
+                         data={'iid': 'someinvalidiid'})
