a9c1c23206097d7c8d1657c275124eeadab795b5
Philippe Gross <philippe.gross@4teamwork.ch>
2018-01-23 17:38:34 +0100

    Convert forwarding test to integration tests.

--- opengever/inbox/tests/test_forwarding.py
+++ opengever/inbox/tests/test_forwarding.py
@@ -3,103 +3,74 @@ from ftw.builder import create
 from ftw.testbrowser import browsing
 from ftw.testbrowser.pages import statusmessages
 from ftw.testbrowser.pages.factoriesmenu import addable_types
-from opengever.testing import FunctionalTestCase
+from opengever.testing import IntegrationTestCase
 
 
-class TestForwarding(FunctionalTestCase):
-
-    def setUp(self):
-        super(TestForwarding, self).setUp()
-        self.inbox = create(Builder('inbox'))
+class TestForwarding(IntegrationTestCase):
 
     @browsing
     def test_forwarding_is_not_addable_over_the_factory_menu(self, browser):
-        browser.login().open(self.inbox)
+        self.login(self.secretariat_user, browser=browser)
+        browser.open(self.inbox)
 
         self.assertEqual(['Document'], addable_types(browser))
 
     @browsing
     def test_at_least_one_document_references_is_required(self, browser):
-        browser.login().open(
-            self.inbox,
-            view='++add++opengever.inbox.forwarding',
-            )
+        self.login(self.secretariat_user, browser=browser)
+        browser.open(self.inbox,
+                     view='++add++opengever.inbox.forwarding')
 
         messages = statusmessages.messages()
         self.assertEqual(
             ['Error: Please select at least one document to forward.'],
-            messages.get('error'),
-            )
+            messages.get('error'))
 
     @browsing
     def test_creation_moves_document_in_to_the_forwarding(self, browser):
+        self.login(self.secretariat_user, browser=browser)
+
         doc1 = create(Builder('document').within(self.inbox).titled(u'Doc 1'))
         doc2 = create(Builder('document').within(self.inbox).titled(u'Doc 2'))
         doc3 = create(Builder('document').within(self.inbox).titled(u'Doc 3'))
 
         data = {
-            'paths': ['/'.join(doc.getPhysicalPath()) for doc in [doc1, doc3]],
-            }
+            'paths': ['/'.join(doc.getPhysicalPath()) for doc in [doc1, doc3]]}
 
-        browser.login().open(
-            self.inbox,
-            data,
-            view='++add++opengever.inbox.forwarding',
-            )
-
-        browser.fill({
-            'Title': u'Test forwarding',
-            'Responsible': 'inbox:client1',
-            })
-
+        browser.open(
+            self.inbox, data, view='++add++opengever.inbox.forwarding')
+        browser.fill({'Title': u'Test forwarding',
+                      'Responsible': 'inbox:fa'})
         browser.css('#form-buttons-save').first.click()
 
-        forwarding = self.inbox.get('forwarding-1')
+        forwarding = self.inbox.objectValues()[-1]
 
-        self.assertEqual(
-            ['Item created'],
-            statusmessages.messages().get('info'),
-            )
-
+        self.assertEqual(['Item created'],
+                         statusmessages.messages().get('info'))
         self.assertEqual([doc1, doc3], forwarding.listFolderContents())
-
-        self.assertEqual(
-            [doc2],
-            self.inbox.listFolderContents({
-                'portal_type': 'opengever.document.document',
-                }),
-            )
+        self.assertIn(doc2, self.inbox.listFolderContents())
 
     @browsing
     def test_forwarding_add_form_does_not_render_empty_fieldset_additional(
-            self,
-            browser,
-        ):
-        doc = create(Builder('document').within(self.inbox).titled(u'Doc 1'))
-        data = dict(paths=[doc.getPhysicalPath()])
+            self, browser):
 
-        browser.login().open(
-            self.inbox,
-            data,
-            view='++add++opengever.inbox.forwarding',
-            )
+        self.login(self.secretariat_user, browser=browser)
+
+        data = self.make_path_param(self.inbox_document)
+        browser.open(
+            self.inbox, data, view='++add++opengever.inbox.forwarding')
 
         fieldsets = browser.css('form#form fieldset')
-
         self.assertEqual(1, len(fieldsets))
         self.assertEqual('Common', fieldsets.first.css('legend').first.text)
 
     @browsing
     def test_forwarding_edit_form_does_not_render_empty_fieldset_additional(
-            self,
-            browser,
-        ):
-        forwarding = create(Builder('forwarding').within(self.inbox))
+            self, browser):
 
-        self.grant('Manager')
-        browser.login().open(forwarding, view='edit')
+        self.login(self.manager, browser=browser)
+        browser.open(self.inbox_forwarding, view='edit')
 
         fieldsets = browser.css('form#form fieldset')
-
         self.assertEqual(1, len(fieldsets))
         self.assertEqual('Common', fieldsets.first.css('legend').first.text)
