2f5937f9e3a5df7ad18d6f7be2f6abe0454d5e45
Joni Orponen <j.orponen@4teamwork.ch>
2018-01-24 15:53:19 +0100

    Add a helper to get the path to a test asset.

--- opengever/testing/assets/__init__.py
+++ opengever/testing/assets/__init__.py
@@ -1,5 +1,10 @@
+from pkg_resources import resource_filename
 from pkg_resources import resource_string
 
 
 def load(asset_filename):
     return resource_string('opengever.testing.assets', asset_filename)
+
+
+def path_to_asset(asset_filename):
+    return resource_filename('opengever.testing.assets', asset_filename)
