f7d099c41a969e9ab2be5ad763fcfd97db43167c
Philippe Gross <philippe.gross@4teamwork.ch>
2018-01-31 12:40:31 +0100

    Adjust PrivateDossier and PrivateFolder workflows.
    Disallow access for Administrators on foreign private folders or
    dossiers, but allow MemberAreaAdministrators instead.

--- opengever/private/tests/test_workflows.py
+++ opengever/private/tests/test_workflows.py
@@ -22,18 +22,35 @@ class TestPrivateFolderWorkflow(FunctionalTestCase):
         self.admin = create(Builder('user')
                             .named('Ad', 'Min')
                             .with_roles('Member', 'Administrator'))
+        self.member_admin = create(Builder('user')
+                            .named('MemberAreaAdmin', 'MAdm')
+                            .with_roles('Member',
+                                        'MemberAreaAdministrator'))
 
     @browsing
-    def test_only_owner_and_admin_can_see_private_folder(self, browser):
+    def test_only_owner_and_memberareaadmin_can_see_private_folder(self, browser):
         browser.login().open(self.folder)
-        browser.login(self.admin).open(self.folder)
+        browser.login(self.member_admin).open(self.folder)
         with browser.expect_unauthorized():
             browser.login(self.hugo).open(self.folder)
+
+        with browser.expect_unauthorized():
+            browser.login(self.admin).open(self.folder)
 
     @browsing
     def test_owner_can_add_private_dossiers(self, browser):
         browser.login().open(self.folder)
         self.assertIn('Private Dossier', factoriesmenu.addable_types())
+
+
+    @browsing
+    def test_admin_cant_access_private_dossier(self, browser):
+        dossier = create(Builder('private_dossier')
+                         .within(self.folder)
+                         .titled(u'Zuz\xfcge'))
+
+        with browser.expect_unauthorized():
+            browser.login(self.admin).open(dossier)
 
     @browsing
     def test_admin_cant_add_in_private_content(self, browser):
@@ -41,7 +58,7 @@ class TestPrivateFolderWorkflow(FunctionalTestCase):
                          .within(self.folder)
                          .titled(u'Zuz\xfcge'))
 
-        browser.login(self.admin)
+        browser.login(self.member_admin)
 
         browser.open(self.folder)
         self.assertFalse(factoriesmenu.visible())
@@ -50,13 +67,13 @@ class TestPrivateFolderWorkflow(FunctionalTestCase):
         self.assertFalse(factoriesmenu.visible())
 
     @browsing
-    def test_only_owner_and_admin_can_see_private_dossiers(self, browser):
+    def test_only_owner_and_memberareaadmin_can_see_private_dossiers(self, browser):
         dossier = create(Builder('private_dossier')
                          .within(self.folder)
                          .titled(u'Zuz\xfcge'))
 
         browser.login().visit(dossier)
-        browser.login(self.admin).visit(dossier)
+        browser.login(self.member_admin).visit(dossier)
         with browser.expect_unauthorized():
             browser.login(self.hugo).visit(dossier)
 
