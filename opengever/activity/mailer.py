4c14604f6752252423bbaee0b3a43a214958b906
Philippe Gross <philippe.gross@4teamwork.ch>
2018-02-04 20:05:48 +0100

    Improve digest-template and include the logo as a separate part.

--- opengever/activity/mailer.py
+++ opengever/activity/mailer.py
@@ -1,9 +1,11 @@
 from email.header import Header
+from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from opengever.base.model import get_locale
 from opengever.mail.utils import make_addr_header
 from opengever.ogds.base.utils import ogds_service
+from pkg_resources import resource_filename
 from plone import api
 
 
@@ -24,7 +26,7 @@ class Mailer(object):
         self.mailhost.send(msg)
 
     def prepare_mail(self, subject=u'', to_userid=None, from_userid=None, data={}):
-        msg = MIMEMultipart('alternative')
+        msg = MIMEMultipart('related')
 
         if from_userid:
             actor = ogds_service().fetch_user(from_userid)
@@ -40,6 +42,7 @@ class Mailer(object):
 
         html = self.prepare_html(data)
         msg.attach(MIMEText(html.encode('utf-8'), 'html', 'utf-8'))
+        msg.attach(self.get_logo_part())
         return msg
 
     def get_users_language(self):
@@ -47,6 +50,15 @@ class Mailer(object):
         # language. Therefore we send the mails always in the current selected
         # language.
         return get_locale()
+
+    def get_logo_part(self):
+        path = resource_filename(
+            'opengever.activity.browser.resources', 'gever.png')
+        fp = open(path, 'r')
+        msg_image = MIMEImage(fp.read())
+        fp.close()
+        msg_image.add_header('Content-ID', '<image1>')
+        return msg_image
 
     def prepare_html(self, data):
         return self.template(self, **data)
