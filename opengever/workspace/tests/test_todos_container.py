81ba9ba4663355d62d5b78c4153c824d15221197
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2018-02-16 13:59:23 +0100

    Fix adding todo container test.

--- opengever/workspace/tests/test_todos_container.py
+++ opengever/workspace/tests/test_todos_container.py
@@ -8,5 +8,5 @@ class TestTodosContainer(IntegrationTestCase):
     def test_can_be_added(self):
         self.login(self.administrator)
         container = create(Builder('todos container').within(self.workspace))
-        self.assertEquals('/plone/workspaces/workspace/todos',
+        self.assertEquals('/plone/workspaces/workspace-1/todos',
                           '/'.join(container.getPhysicalPath()))
