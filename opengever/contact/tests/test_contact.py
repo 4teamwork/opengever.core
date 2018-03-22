0a23eb970c8bba1575e331c101ae96c0615372e9
Niklaus Johner <niklaus.johner@4teamwork.ch>
2018-03-22 11:00:57 +0100

    Test that contacts cannot be added when contact feature is enabled

--- opengever/contact/tests/test_contact.py
+++ opengever/contact/tests/test_contact.py
@@ -1,11 +1,14 @@
 from ftw.builder import Builder
 from ftw.builder import create
 from ftw.testbrowser import browsing
+from ftw.testbrowser import InsufficientPrivileges
 from ftw.testbrowser.pages import factoriesmenu
+from opengever.contact.interfaces import IContactSettings
 from opengever.testing import add_languages
 from opengever.testing import FunctionalTestCase
 from opengever.testing import obj2brain
 from plone import api
+import transaction
 
 
 class TestContact(FunctionalTestCase):
@@ -30,6 +33,23 @@ class TestContact(FunctionalTestCase):
             'http://nohost/plone/opengever-contact-contactfolder/durr-hanspeter/contact_view',
             browser.url)
         self.assertEquals(u'D\xfcrr Hanspeter', browser.css('h1').first.text)
+
+    @browsing
+    def test_cannot_add_a_contact_with_contact_feature_enabled(self, browser):
+        contactfolder = create(Builder('contactfolder'))
+
+        browser.login().open(contactfolder)
+        self.assertIn('Contact', factoriesmenu.addable_types())
+        browser.visit(contactfolder, view="++add++opengever.contact.contact")
+
+        api.portal.set_registry_record(
+          'is_feature_enabled', True, interface=IContactSettings)
+        transaction.commit()
+
+        browser.open(contactfolder)
+        self.assertNotIn('Contact', factoriesmenu.addable_types())
+        with self.assertRaises(InsufficientPrivileges):
+            browser.visit(contactfolder, view="++add++opengever.contact.contact")
 
     @browsing
     def test_edit_a_contact(self, browser):
