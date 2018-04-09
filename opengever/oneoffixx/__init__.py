8a24f9e9d12dbf2f67ae6fe7585af628d5bed9c7
Niklaus Johner <niklaus.johner@4teamwork.ch>
2018-04-09 16:22:01 +0200

    Create document from a oneoffixx template

---
+++ opengever/oneoffixx/__init__.py
@@ -0,0 +1,15 @@
+from opengever.officeconnector.interfaces import IOfficeConnectorSettings
+from opengever.oneoffixx.interfaces import IOneoffixxSettings
+from plone import api
+from zope.i18nmessageid import MessageFactory
+
+_ = MessageFactory("opengever.oneoffixx")
+
+
+def is_oneoffixx_feature_enabled():
+    office_connector_enabled = api.portal.get_registry_record(
+        "direct_checkout_and_edit_enabled",
+        interface=IOfficeConnectorSettings)
+    oneoffixx_enabled = api.portal.get_registry_record(
+        'is_feature_enabled', interface=IOneoffixxSettings)
+    return office_connector_enabled and oneoffixx_enabled
