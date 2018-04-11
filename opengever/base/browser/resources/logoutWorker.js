fd286349adde05615487833856744121e16dea4f
Philippe Gross <gross.philippe@gmail.com>
2018-04-11 13:15:52 +0200

    Merge pull request #4145 from 4teamwork/pg_kb_fix_sso_logout_worker
    Fix logout worker when using SSO

--- opengever/base/browser/resources/logoutWorker.js
+++ opengever/base/browser/resources/logoutWorker.js
@@ -5,8 +5,8 @@ onconnect = function(e) {
   var port = e.ports[0];
   clients.add(port);
   port.start();
-  port.onmessage = function() {
+  port.onmessage = function(e) {
     // Broadcast logout message to all open gever tabs
-    clients.forEach(c => { c.postMessage('logout'); });
+    clients.forEach(c => { c.postMessage(e.data); });
   };
 };
