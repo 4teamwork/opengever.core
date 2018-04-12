fd2eba66cedd124ecf85b67fc8a70a57220fead8
Philippe Gross <philippe.gross@4teamwork.ch>
2018-04-12 15:59:15 +0200

    Add favorite eTag adapter
    to make sure the favorite viewlet is not cached, after setting a favorite.

--- opengever/base/viewlets/favorite_action.py
+++ opengever/base/viewlets/favorite_action.py
@@ -1,9 +1,13 @@
 from opengever.base.favorite import FavoriteManager
 from opengever.base.oguid import Oguid
 from plone import api
+from plone.app.caching.interfaces import IETagValue
 from plone.app.layout.viewlets import common
 from plone.dexterity.interfaces import IDexterityContent
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.component import adapts
+from zope.interface import implements
+from zope.interface import Interface
 
 
 class FavoriteAction(common.ViewletBase):
@@ -23,3 +27,21 @@ class FavoriteAction(common.ViewletBase):
 
     def available(self):
         return IDexterityContent.providedBy(self.context) and self.oguid()
+
+
+class FavoriteETagValue(object):
+    implements(IETagValue)
+    adapts(Interface, Interface)
+
+    def __init__(self, published, request):
+        self.published = published
+        self.request = request
+
+    def __call__(self):
+        favorite = FavoriteManager().get_favorite(
+            self.published.context, api.user.get_current())
+
+        if not favorite:
+            return None
+
+        return str(favorite.favorite_id)
