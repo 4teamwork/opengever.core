4184b6596fc46b31a75b301ae775aa25f81bf644
Joni Orponen <j.orponen@4teamwork.ch>
2018-05-08 14:51:37 +0200

    Add a retry button for oneoffixx on shadow documents.

--- opengever/oneoffixx/tests/test_oneoffixx.py
+++ opengever/oneoffixx/tests/test_oneoffixx.py
@@ -29,6 +29,18 @@ class TestCreateDocFromOneoffixxTemplate(IntegrationTestCase):
         self.assertTrue(browser.context.is_shadow_document())
 
     @browsing
+    def test_retry_button_visible_on_shadow_doc(self, browser):
+        self.login(self.regular_user, browser)
+        browser.open(self.dossier)
+        factoriesmenu.add('document_with_oneoffixx_template')
+        node = browser.css("#form-widgets-template-2574d08d-95ea-4639-beab-3103fe4c3bc7").first
+        browser.fill({'Title': 'A doc'})
+        browser.fill({'Template': node.get("title")})
+        browser.find('Save').click()
+        browser.open(browser.context, view='tabbedview_view-overview')
+        self.assertEqual(['Oneoffixx retry'], browser.css('a.oc-checkout').text)
+
+    @browsing
     def test_template_id_stored_in_annotations(self, browser):
         self.login(self.regular_user, browser)
         browser.open(self.dossier)
