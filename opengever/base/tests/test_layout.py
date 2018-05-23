7b9a0d516de8d3c50e11b8e1af3a7c8869526075
David Erni <david.erni@4teamwork.ch>
2018-05-23 18:21:07 +0200

    Always have word meeting css class in layout.

--- opengever/base/tests/test_layout.py
+++ opengever/base/tests/test_layout.py
@@ -20,16 +20,16 @@ class TestGeverLayoutPolicy(IntegrationTestCase):
     @browsing
     def test_word_meeting_feature_presence(self, browser):
         self.login(self.regular_user, browser)
-        self.activate_feature('meeting')
+
         feature_class = 'feature-word-meeting'
 
-        self.deactivate_feature('word-meeting')
-        browser.open()
-        self.assertNotIn(feature_class, browser.css('body').first.classes)
-
-        self.activate_feature('word-meeting')
+        self.activate_feature('meeting')
         browser.open()
         self.assertIn(feature_class, browser.css('body').first.classes)
+
+        self.deactivate_feature('meeting')
+        browser.open()
+        self.assertNotIn(feature_class, browser.css('body').first.classes)
 
     @browsing
     def test_no_model_class_on_regular_content(self, browser):
@@ -42,6 +42,6 @@ class TestGeverLayoutPolicy(IntegrationTestCase):
     @browsing
     def test_model_class_on_sql_wrapper(self, browser):
         self.login(self.committee_responsible, browser)
-        map(self.activate_feature, ('meeting', 'word-meeting'))
+        self.activate_feature('meeting')
         browser.open(self.meeting)
         self.assertIn('model-meeting', browser.css('body').first.classes)
