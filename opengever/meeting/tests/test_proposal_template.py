42dd71b5256d4a7e6a291a235022d532a399cb72
Joni Orponen <j.orponen@4teamwork.ch>
2018-05-26 20:43:28 +0200

    Port proposal template tests to integration tests.

--- opengever/meeting/tests/test_proposal_template.py
+++ opengever/meeting/tests/test_proposal_template.py
@@ -1,65 +1,53 @@
-from ftw.builder import Builder
-from ftw.builder import create
 from ftw.testbrowser import browsing
 from ftw.testbrowser.pages import factoriesmenu
 from ftw.testbrowser.pages import statusmessages
-from opengever.core.testing import activate_meeting_word_implementation
 from opengever.meeting.command import MIME_DOCX
-from opengever.meeting.interfaces import IMeetingSettings
-from opengever.testing import FunctionalTestCase
-from plone import api
-import transaction
+from opengever.testing import IntegrationTestCase
 
 
-class TestProposalTemplate(FunctionalTestCase):
+class TestProposalTemplateWordEnabled(IntegrationTestCase):
+    features = (
+        'meeting',
+        'word-meeting',
+        )
 
-    def setUp(self):
-        super(TestProposalTemplate, self).setUp()
-        activate_meeting_word_implementation()
+    @browsing
+    def test_proposal_templates_addable_when_feature_enabled(self, browser):
+        self.login(self.administrator, browser)
+        browser.open(self.templates)
+        self.assertIn('Proposal Template', factoriesmenu.addable_types(browser))
 
     @browsing
     def test_adding_new_proposal_template(self, browser):
-        vorlagen = create(Builder('templatefolder').titled(u'Vorlagen'))
-        browser.login().open(vorlagen)
-        factoriesmenu.add('Proposal Template')
-
-        browser.fill({
-            'Title': 'Baugesuch',
-            'File': ('Binary Data', 'Baugesuch.docx', MIME_DOCX)}).save()
+        self.login(self.administrator, browser)
+        browser.open(self.templates, view='++add++opengever.meeting.proposaltemplate')
+        browser.fill({'Title': 'Baugesuch', 'File': ('Binary Data', 'Baugesuch.docx', MIME_DOCX)}).save()
         statusmessages.assert_no_error_messages()
-
-        baugesuch, = vorlagen.objectValues()
+        baugesuch = self.templates.objectValues()[-1]
         browser.open(baugesuch, view='tabbedview_view-overview')
-        self.assertDictContainsSubset(
-            {'Title': 'Baugesuch'},
-            dict(browser.css('.documentMetadata table').first.lists()))
-
-        self.assertEquals(
-            'baugesuch.docx',
-            browser.css('.documentMetadata span.filename').first.text)
+        self.assertDictContainsSubset({'Title': 'Baugesuch'}, dict(browser.css('.documentMetadata table').first.lists()))
+        self.assertEquals('baugesuch.docx', browser.css('.documentMetadata span.filename').first.text)
 
     @browsing
     def test_uploading_non_docx_files_is_not_allowed(self, browser):
-        vorlagen = create(Builder('templatefolder').titled(u'Vorlagen'))
-        browser.login().open(vorlagen)
-        factoriesmenu.add('Proposal Template')
-
-        browser.fill({
-            'Title': u'Geb\xfchren',
-            'File': ('DATA', 'Wrong.txt', 'text/plain')}).save()
+        self.login(self.administrator, browser)
+        browser.open(self.templates, view='++add++opengever.meeting.proposaltemplate')
+        browser.fill({'Title': u'Geb\xfchren', 'File': ('DATA', 'Wrong.txt', 'text/plain')}).save()
         statusmessages.assert_message('There were some errors.')
         self.assertEquals(
             ['Only word files (.docx) can be added here.'],
-            browser.css('#formfield-form-widgets-file .error').text)
+            browser.css('#formfield-form-widgets-file .error').text,
+            )
+
+
+class TestProposalTemplateWordDisabled(IntegrationTestCase):
+
+    features = (
+        'meeting',
+        )
 
     @browsing
     def test_proposal_templates_not_addable_when_feature_disabled(self, browser):
-        vorlagen = create(Builder('templatefolder').titled(u'Vorlagen'))
-        browser.login().open(vorlagen)
-        self.assertIn('Proposal Template', factoriesmenu.addable_types())
-
-        api.portal.set_registry_record('is_word_implementation_enabled', False,
-                                       interface=IMeetingSettings)
-        transaction.commit()
-        browser.reload()
-        self.assertNotIn('Proposal Template', factoriesmenu.addable_types())
+        self.login(self.administrator, browser)
+        browser.open(self.templates)
+        self.assertNotIn('Proposal Template', factoriesmenu.addable_types(browser))
