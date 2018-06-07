c254a45c11a6fd8c811563ceb19c8a7a6e9b56ae
David Erni <david.erni@4teamwork.ch>
2018-06-07 13:49:28 +0200

    Merge committeeontainer tests into same case.

--- opengever/meeting/tests/test_committee_container.py
+++ opengever/meeting/tests/test_committee_container.py
@@ -11,48 +11,6 @@ from opengever.testing import IntegrationTestCase
 from unittest import skip
 
 
-class TestCommitteeContainer(IntegrationTestCase):
-
-    features = ('meeting',)
-
-    @skip("This test currently fails in a flaky way on CI."
-          "See https://github.com/4teamwork/opengever.core/issues/3995")
-    @browsing
-    def test_supports_translated_title(self, browser):
-        self.login(self.manager, browser)
-        add_languages(['de-ch', 'fr-ch'])
-        browser.open()
-        factoriesmenu.add('Committee Container')
-        browser.fill({'Title (German)': u'Sitzungen',
-                      'Title (French)': u's\xe9ance',
-                      'Protocol template': self.sablon_template,
-                      'Excerpt template': self.sablon_template,
-                      'Table of contents template': self.sablon_template}).save()
-        statusmessages.assert_no_error_messages()
-
-        browser.find(u'Fran\xe7ais').click()
-        self.assertEquals(u's\xe9ance', browser.css('h1').first.text)
-
-        browser.find('Deutsch').click()
-        self.assertEquals(u'Sitzungen', browser.css('h1').first.text)
-
-    def test_get_toc_template(self):
-        self.login(self.manager)
-
-        self.assertIsNone(self.committee_container.toc_template)
-        self.assertIsNone(self.committee_container.get_toc_template())
-
-        self.committee_container.toc_template = self.as_relation_value(
-            self.sablon_template)
-
-        self.assertEqual(
-            self.sablon_template, self.committee_container.get_toc_template())
-
-    def test_portlets_inheritance_is_blocked(self):
-        self.login(self.manager)
-        self.assert_portlet_inheritance_blocked(
-            'plone.leftcolumn', self.committee_container)
-
 
 class TestCommitteesTab(IntegrationTestCase):
 
