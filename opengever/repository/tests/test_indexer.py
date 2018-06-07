0515ea5fbe82ff8d642a23d1083611104bc7f8d1
Pablo Vergés <pablo.verges@4teamwork.ch>
2018-06-07 09:52:02 +0200

    Add sortable_title indexer for repository folders

--- opengever/repository/tests/test_indexer.py
+++ opengever/repository/tests/test_indexer.py
@@ -1,10 +1,33 @@
+from ftw.builder import Builder
+from ftw.builder import create
 from opengever.testing import IntegrationTestCase
-from zope.event import notify
 from opengever.sharing.events import LocalRolesAcquisitionActivated
 from opengever.sharing.events import LocalRolesAcquisitionBlocked
+from opengever.repository.indexers import sortable_title
+from zope.event import notify
 
 
 class TestRepositoryFolderIndexers(IntegrationTestCase):
+
+    def test_sortable_title_index_ignores_refernce_number_length(self):
+        self.login(self.secretariat_user)
+
+        # create a 5 fold nested folder
+        repofolder = reduce(
+            lambda repofolder, level: create(
+                Builder('repository')
+                .within(repofolder)
+                .having(
+                    title_de=u'Vertr\xe4ge {}'.format(level),
+                    title_fr=u'Contrats {}'.format(level),
+                )),
+            range(5),
+            self.leaf_repofolder,
+        )
+
+        self.assertEquals(
+            '0001.0001.0001.0001.0001.0001.0001. vertrage 0004',
+            sortable_title(repofolder)())
 
     def test_blocked_local_roles(self):
         self.login(self.regular_user)
