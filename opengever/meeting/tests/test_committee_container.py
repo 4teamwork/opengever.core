ee4ffe24fbd6fcaa714d48a2a6dd8bc8f72ebc0b
Joni Orponen <j.orponen@4teamwork.ch>
2018-06-12 21:20:12 +0200

    Restabilize committee container tests.

--- opengever/meeting/tests/test_committee_container.py
+++ opengever/meeting/tests/test_committee_container.py
@@ -8,15 +8,12 @@ from ftw.testbrowser.pages import statusmessages
 from opengever.base.date_time import utcnow_tz_aware
 from opengever.testing import add_languages
 from opengever.testing import IntegrationTestCase
-from unittest import skip
 
 
 class TestCommitteeContainer(IntegrationTestCase):
 
     features = ('meeting',)
 
-    @skip("This test currently fails in a flaky way on CI."
-          "See https://github.com/4teamwork/opengever.core/issues/3995")
     @browsing
     def test_supports_translated_title(self, browser):
         self.login(self.manager, browser)
@@ -25,8 +22,8 @@ class TestCommitteeContainer(IntegrationTestCase):
         factoriesmenu.add('Committee Container')
         browser.fill({'Title (German)': u'Sitzungen',
                       'Title (French)': u's\xe9ance',
-                      'Protocol template': self.sablon_template,
-                      'Excerpt template': self.sablon_template,
+                      'Protocol header template': self.sablon_template,
+                      'Excerpt header template': self.sablon_template,
                       'Table of contents template': self.sablon_template}).save()
         statusmessages.assert_no_error_messages()
 
