2f5c887def317b391aab121677084c52cbfa90b4
Elio Schmutz <e.schmutz@4teamwork.ch>
2018-06-20 14:01:39 +0200

    Restrict SelectAutocomplete to the desired proposal-fields

--- opengever/meeting/browser/resources/proposal.js
+++ opengever/meeting/browser/resources/proposal.js
@@ -2,7 +2,8 @@
 
   $(function() {
     if($(".template-add-membership, .template-opengever-meeting-proposal, .portaltype-opengever-meeting-proposal.template-edit").length) {
-      new SelectAutocomplete();
+      new SelectAutocomplete({
+          target: 'select#form-widgets-committee, select#form-widgets-language, select#form-widgets-member'});
     }
   });
 
