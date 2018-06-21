066ad575bd46233732562a4ddbdfad26c80531d3
Joni Orponen <j.orponen@4teamwork.ch>
2018-06-21 16:00:03 +0200

    Add a no value option to the table radio widget when it's not required.

--- opengever/base/tests/test_widget.py
+++ opengever/base/tests/test_widget.py
@@ -62,31 +62,38 @@ class TestRadioTableWidget(FunctionalTestCase):
 
         self.assertEqual(
             # checkbox-column title/text is empty and that's ok.
-            [{'': '', 'Title': 'foo'},
-             {'': '', 'Title': 'bar'}],
-            browser.css('#formfield-form-widgets-default_radio_table_field '
-                        'table').first.dicts()
-        )
+            [{'': '', 'Title': 'None'}, {'': '', 'Title': 'foo'}, {'': '', 'Title': 'bar'}],
+            browser.css('#formfield-form-widgets-default_radio_table_field table').first.dicts(),
+            )
 
     @browsing
     def test_renders_custom_table_columns(self, browser):
         browser.login().visit(view='test-z3cform-widget')
-
+        # checkbox-column title/text is empty and that's ok.
+        expected_columns = [
+            {'': '', 'Description': ''},
+            {'': '', 'Description': 'Odi et amo.'},
+            {'': '', 'Description': 'Quare id faciam fortasse requiris?'},
+            ]
         self.assertEqual(
-            # checkbox-column title/text is empty and that's ok.
-            [{'': '', 'Description': 'Odi et amo.'},
-             {'': '', 'Description': 'Quare id faciam fortasse requiris?'}],
-            browser.css('#formfield-form-widgets-custom_radio_table_field '
-                        'table').first.dicts()
-        )
+            expected_columns,
+            browser.css('#formfield-form-widgets-custom_radio_table_field table').first.dicts(),
+            )
+
+    @browsing
+    def test_does_not_render_none_choice_when_a_required_field(self, browser):
+        browser.login()
+        browser.open(view='test-z3cform-required-widget')
+        self.assertNotIn(
+            {'': '', 'Description': ''},
+            browser.css('#formfield-form-widgets-required_radio_table_field table').first.dicts(),
+            )
 
     @browsing
     def test_fill_table_choice_field(self, browser):
         browser.login().visit(view='test-z3cform-widget')
         browser.fill({'form.widgets.default_radio_table_field': '2'}).submit()
-
-        self.assertEqual(
-            {u'default_radio_table_field': [2, u'bar']}, browser.json)
+        self.assertEqual({u'default_radio_table_field': [2, u'bar']}, browser.json)
 
     @browsing
     def test_renders_table_choice_input_fields(self, browser):
@@ -94,12 +101,10 @@ class TestRadioTableWidget(FunctionalTestCase):
         inputs = browser.css(
             '#formfield-form-widgets-default_radio_table_field input')
         expected_inputs = [
-            u'<input type="text" name="searchable_text" class="inputLabel tableradioSearchbox" autocomplete="false" '
-            u'placeholder="Filter" title="Filter">',
-            u'<input id="form-widgets-default_radio_table_field-1" '
-            u'name="form.widgets.default_radio_table_field" value="1" title="foo" type="radio">',
-            u'<input id="form-widgets-default_radio_table_field-2" '
-            u'name="form.widgets.default_radio_table_field" value="2" title="bar" type="radio">',
+            u'<input type="text" name="searchable_text" class="inputLabel tableradioSearchbox" autocomplete="false" placeholder="Filter" title="Filter">',  # noqa
+            u'<input id="form-widgets-default_radio_table_field-empty-marker" name="form.widgets.default_radio_table_field" value="--NOVALUE--" title="label_none" type="radio">',  # noqa
+            u'<input id="form-widgets-default_radio_table_field-1" name="form.widgets.default_radio_table_field" value="1" title="foo" type="radio">',  # noqa
+            u'<input id="form-widgets-default_radio_table_field-2" name="form.widgets.default_radio_table_field" value="2" title="bar" type="radio">',  # noqa
             u'<input name="form.widgets.default_radio_table_field-empty-marker" type="hidden" value="1">',
             ]
         self.assertEqual(expected_inputs, [each.normalized_outerHTML for each in inputs])
