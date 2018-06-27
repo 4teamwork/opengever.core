00d60f63aa7c9dc8c137642a7fa9855284a3b03e
David Erni <david.erni@4teamwork.ch>
2018-06-27 10:54:11 +0200

    Revert "Merge pull request #4495 from 4teamwork/pv-ms_publisher-4481"
    This reverts commit b25ccdb99442b384fcc73c27bec7074e31151d48, reversing
    changes made to ca1ab87f9858f06ac2d8df2a76f63dd538cd0d68.

--- opengever/core/upgrades/20180625101451_add_ms_publisher_to_oc_editables/upgrade.py
+++
@@ -1,9 +0,0 @@
-from ftw.upgrade import UpgradeStep
-
-
-class AddMSPublisherToOCEditables(UpgradeStep):
-    """Add MS Publisher to OC editables.
-    """
-
-    def __call__(self):
-        self.install_upgrade_profile()
