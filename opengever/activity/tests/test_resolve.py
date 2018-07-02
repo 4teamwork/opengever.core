42273ed8a12b6e8459e58fa228733a9adabbd9ad
Elio Schmutz <e.schmutz@4teamwork.ch>
2018-07-02 14:02:32 +0200

    Fix resolve object in activity for deleted objects

--- opengever/activity/tests/test_resolve.py
+++ opengever/activity/tests/test_resolve.py
@@ -4,8 +4,11 @@ from ftw.testbrowser import browsing
 from opengever.activity import notification_center
 from opengever.activity.browser import resolve_notification_url
 from opengever.base.oguid import Oguid
+from opengever.base.security import elevated_privileges
 from opengever.testing import FunctionalTestCase
+from plone import api
 from plone.app.testing import TEST_USER_ID
+import transaction
 
 
 class TestResolveNotificationView(FunctionalTestCase):
@@ -78,6 +81,27 @@ class TestResolveNotificationView(FunctionalTestCase):
                              data={'notification_id': notification.notification_id})
 
         self.assertEquals(task.absolute_url(), browser.url)
+
+    @browsing
+    def test_raises_notfound_when_oguid_does_no_longer_exists(self, browser):
+        task = create(Builder('task'))
+        oguid = Oguid.for_object(task)
+
+        resource = create(Builder('resource').oguid(oguid.id))
+        activity = create(Builder('activity').having(resource=resource))
+        notification = create(Builder('notification')
+                              .having(activity=activity, userid=TEST_USER_ID))
+
+        with elevated_privileges():
+            # This happens on rejecting a submitte proposal within the
+            # opengever.meeting-module. A submtited proposal will be deleted
+            # on rejecting and is no longer resolveable.
+            api.content.delete(task)
+            transaction.commit()
+
+        with browser.expect_http_error(reason='Not Found'):
+            browser.login().open(self.portal, view='resolve_notification',
+                                 data={'notification_id': notification.notification_id})
 
     @browsing
     def test_redirects_to_the_resolveoguid_on_admin_unit_for_foreign_ressources(self, browser):
