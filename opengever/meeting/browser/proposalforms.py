c45dad8e4d55983fcbd32bcad97576f3015485ae
Niklaus Johner <niklaus.johner@4teamwork.ch>
2018-07-02 17:54:57 +0200

    Hide Excerpts field in edit form for SubmittedProposals.

--- opengever/meeting/browser/proposalforms.py
+++ opengever/meeting/browser/proposalforms.py
@@ -64,6 +64,7 @@ class SubmittedProposalEditForm(ModelProxyEditForm,
 
     def updateWidgets(self):
         super(SubmittedProposalEditForm, self).updateWidgets()
+        self.widgets['excerpts'].mode = HIDDEN_MODE
         self.widgets['relatedItems'].mode = HIDDEN_MODE
 
         if self.context.get_state() is not self.context.load_model().STATE_PENDING:
