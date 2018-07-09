b2676b09e96c531c9897bfe6e212a642046dc131
David Erni <david.erni@4teamwork.ch>
2018-07-09 17:35:33 +0200

    Fix re-submitting previously rejected proposals.
    There is an issue with mail attachments as the event handler was only registered
    for documents, and not for mails. Thus rows in the submitteddocuments table
    were not removed correctly.

--- opengever/meeting/handlers.py
+++ opengever/meeting/handlers.py
@@ -19,9 +19,15 @@ from zope.intid.interfaces import IIntIds
 
 
 def document_deleted(context, event):
-    # this event is also fired when deleting a plone site. Unfortunately
-    # no deletion-order seems to be guaranteed, so it might happen that the
-    # IntId utility is removed before removing content.
+    """Fired when a document or a mail is deleted.
+
+    This happens while rejecting proposals. The entry that tracks the submitted
+    documents must also be removed to avoid issues when re-submitting.
+
+    this event is also fired when deleting a plone site. Unfortunately
+    no deletion-order seems to be guaranteed, so it might happen that the
+    IntId utility is removed before removing content.
+    """
     try:
         getUtility(IIntIds)
     except ComponentLookupError:
