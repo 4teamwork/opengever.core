57518785370d3ae54e268f6f9fea0972d218d256
Lukas Graf <lukas.graf@4teamwork.ch>
2018-07-10 15:39:39 +0200

    Rewrite TestHelpers as IntegrationTestCase.

--- opengever/tabbedview/tests/test_helpers.py
+++ opengever/tabbedview/tests/test_helpers.py
@@ -1,23 +1,20 @@
 from datetime import date
 from ftw.testing import MockTestCase
-from opengever.tabbedview.helper import task_id_checkbox_helper
 from opengever.tabbedview.helper import readable_date
+from opengever.tabbedview.helper import task_id_checkbox_helper
+from opengever.testing import IntegrationTestCase
 
 
-class TestHelpers(MockTestCase):
+class TestHelpers(IntegrationTestCase):
 
     def test_task_id_checkbox_helper(self):
-
-        brain = self.stub()
-        self.expect(brain.title).result('The brain of a task')
-        self.expect(brain.task_id).result('123')
-
-        self.replay()
+        self.login(self.regular_user)
+        sql_task = self.task.get_sql_object()
 
         self.assertEquals(
-            task_id_checkbox_helper(brain, ''),
-            '<input class="noborder selectable" id="123" name="task_ids:list" '
-            'title="Select The brain of a task" type="checkbox" value="123" />')
+            task_id_checkbox_helper(sql_task, ''),
+            u'<input class="noborder selectable" id="2" name="task_ids:list" '
+            u'title="Select Vertragsentwurf \xdcberpr\xfcfen" type="checkbox" value="2" />')
 
     def test_readable_date_from_datetime_string(self):
         self.assertEqual(
@@ -30,3 +27,5 @@ class TestHelpers(MockTestCase):
     def test_readable_date_from_date_object(self):
         self.assertEqual(
             readable_date({}, date(2017, 12, 31)), '31.12.2017')
+
+
