ed8eba39734f464381e39a23745d00193673b5c4
Lukas Graf <lukas.graf@4teamwork.ch>
2018-07-10 17:52:55 +0200

    Tests: Rewrite TestSecurity as IntegrationTestCase.

--- opengever/base/tests/test_security.py
+++ opengever/base/tests/test_security.py
@@ -1,22 +1,19 @@
 from opengever.base.security import elevated_privileges
-from opengever.testing import FunctionalTestCase
+from opengever.testing import IntegrationTestCase
 from plone import api
-from plone.app.testing import TEST_USER_ID
 
 
-class TestSecurity(FunctionalTestCase):
-
-    def setUp(self):
-        super(TestSecurity, self).setUp()
-        self.test_user = api.user.get(userid=TEST_USER_ID)
+class TestSecurity(IntegrationTestCase):
 
     def test_elevated_privileges_sets_priviledged_user_and_restores_old(self):
-        self.assertEqual(TEST_USER_ID, api.user.get_current().getId())
+        self.login(self.regular_user)
+
+        self.assertEqual('kathi.barfuss', api.user.get_current().getId())
         self.assertNotIn('manage', api.user.get_current().getRoles())
 
         with elevated_privileges():
-            self.assertEqual(TEST_USER_ID, api.user.get_current().getId())
+            self.assertEqual('kathi.barfuss', api.user.get_current().getId())
             self.assertIn('manage', api.user.get_current().getRoles())
 
         self.assertNotIn('manage', api.user.get_current().getRoles())
-        self.assertEqual(TEST_USER_ID, api.user.get_current().getId())
+        self.assertEqual('kathi.barfuss', api.user.get_current().getId())
