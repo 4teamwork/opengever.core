5b65798e98d6a8faabe470bb78b593e3f2a706da
Elio Schmutz <e.schmutz@4teamwork.ch>
2018-07-18 16:07:07 +0200

    Add TaskReminder to handle reminders in annotations and sql.

---
+++ opengever/task/reminder/reminder.py
@@ -0,0 +1,99 @@
+from opengever.base.model import create_session
+from opengever.globalindex.model.reminder_settings import ReminderSetting
+from opengever.task.reminder import TASK_REMINDER_OPTIONS
+from persistent.dict import PersistentDict
+from plone import api
+from zope.annotation import IAnnotations
+
+
+TASK_REMINDER_ANNOTATIONS_KEY = 'opengever.task.task_reminder'
+
+
+def get_task_reminder():
+    return TaskReminder()
+
+
+class TaskReminder(object):
+
+    def __init__(self):
+        self.session = create_session()
+
+    def set_reminder(self, obj, option, user_id=None):
+        """Sets a reminder for the given object for a specific user or for the
+        current logged in user.
+
+        A previously set reminder for the given user for the given object will
+        be overridden by the new reminder-setting.
+
+        arguments:
+        obj -- the object for which the reminder should be set
+        option -- a TASK_REMINDER_OPTIONS option
+        """
+        user_id = user_id or api.user.get_current().getId()
+        self._set_reminder_setting_in_annotation(obj, user_id, option)
+        self._set_reminder_setting_in_globalindex(obj, user_id, option)
+
+    def get_reminder(self, obj, user_id=None):
+        """Get the reminder-option object for the given object for a specific
+        user or for the current logged in user.
+
+        Returns None, if no reminder is set.
+        """
+        user_id = user_id or api.user.get_current().getId()
+        return TASK_REMINDER_OPTIONS.get(
+            self._get_user_annotation(obj, user_id))
+
+    def get_sql_reminder(self, obj, user_id=None):
+        """Get the sql-reminder for the given object for a specific user or
+        for the current logged in user.
+
+        Returns None, if no reminder is set.
+        """
+        user_id = user_id or api.user.get_current().getId()
+        return ReminderSetting.query.filter(
+            ReminderSetting.actor_id == user_id,
+            ReminderSetting.task.has(task_id=obj.get_sql_object().task_id)).first()
+
+    def clear_reminder(self, obj, user_id=None):
+        """Removes a registered reminder for the given object for a specific
+        user or for the current logged in user.
+        """
+        user_id = user_id or api.user.get_current().getId()
+        self._clear_reminder_setting_in_annotation(obj, user_id)
+        self._clear_reminder_setting_in_sql(obj, user_id)
+
+    def _set_reminder_setting_in_annotation(self, obj, user_id, option):
+        self._set_user_annotation(obj, user_id, option.option_type)
+
+    def _set_reminder_setting_in_globalindex(self, obj, user_id, option):
+        self._clear_reminder_setting_in_sql(obj, user_id)
+        self.session.add(ReminderSetting(
+            task_id=obj.get_sql_object().task_id,
+            actor_id=user_id,
+            option_type=option.option_type,
+            remind_day=option.calculate_remind_on(obj.deadline)
+            ))
+
+    def _clear_reminder_setting_in_annotation(self, obj, user_id):
+        storage = self._annotation_storage(obj)
+        if user_id in storage:
+            del storage[user_id]
+
+    def _clear_reminder_setting_in_sql(self, obj, user_id):
+        current_reminder = self.get_sql_reminder(obj, user_id)
+        if current_reminder:
+            self.session.delete(current_reminder)
+
+    def _annotation_storage(self, obj):
+        annotations = IAnnotations(obj)
+        if TASK_REMINDER_ANNOTATIONS_KEY not in annotations:
+            annotations[TASK_REMINDER_ANNOTATIONS_KEY] = PersistentDict()
+
+        return annotations.get(TASK_REMINDER_ANNOTATIONS_KEY)
+
+    def _set_user_annotation(self, obj, user_id, value):
+        storage = self._annotation_storage(obj)
+        storage[user_id] = value
+
+    def _get_user_annotation(self, obj, user_id):
+        return self._annotation_storage(obj).get(user_id)
--- opengever/task/tests/test_reminder.py
+++ opengever/task/tests/test_reminder.py
@@ -3,6 +3,7 @@ from opengever.task.reminder import TASK_REMINDER_SAME_DAY
 from opengever.task.reminder import TASK_REMINDER_ONE_DAY_BEFORE
 from opengever.task.reminder import TASK_REMINDER_ONE_WEEK_BEFORE
 from opengever.task.reminder import TASK_REMINDER_BEGINNING_OF_WEEK
+from opengever.task.reminder.reminder import TaskReminder
 from opengever.testing import IntegrationTestCase
 
 
@@ -30,3 +31,92 @@ class TestTaskReminderOptions(IntegrationTestCase):
         self.assertEqual(
             date(2018, 7, 2),  # Monday
             TASK_REMINDER_BEGINNING_OF_WEEK.calculate_remind_on(deadline))
+
+
+class TestTaskReminder(IntegrationTestCase):
+
+    def test_set_reminder_creates_annotation_entry_for_current_user(self):
+        self.login(self.regular_user)
+        task_reminder = TaskReminder()
+        task_reminder.set_reminder(self.task, TASK_REMINDER_ONE_DAY_BEFORE)
+
+        self.assertEqual(
+            TASK_REMINDER_ONE_DAY_BEFORE.option_type,
+            task_reminder.get_reminder(self.task).option_type)
+
+        self.assertIsNone(task_reminder.get_reminder(
+            self.task, user_id=self.dossier_responsible.getId()))
+
+    def test_set_reminder_updates_annotations_entry_if_already_exists(self):
+        self.login(self.regular_user)
+        task_reminder = TaskReminder()
+        task_reminder.set_reminder(self.task, TASK_REMINDER_ONE_DAY_BEFORE)
+        task_reminder.set_reminder(self.task, TASK_REMINDER_ONE_WEEK_BEFORE)
+
+        self.assertEqual(
+            TASK_REMINDER_ONE_WEEK_BEFORE.option_type,
+            task_reminder.get_reminder(self.task).option_type)
+
+    def test_set_reminder_creates_sql_entry_for_current_user(self):
+        self.login(self.regular_user)
+        task_reminder = TaskReminder()
+        task_reminder.set_reminder(self.task, TASK_REMINDER_ONE_DAY_BEFORE)
+
+        reminder = task_reminder.get_sql_reminder(self.task)
+
+        self.assertEqual(
+            TASK_REMINDER_ONE_DAY_BEFORE.option_type,
+            reminder.option_type
+            )
+
+        self.assertEqual(
+            TASK_REMINDER_ONE_DAY_BEFORE.calculate_remind_on(self.task.deadline),
+            reminder.remind_day
+            )
+
+        self.assertIsNone(task_reminder.get_sql_reminder(
+            self.task, user_id=self.dossier_responsible.getId()))
+
+    def test_set_reminder_updates_sql_entry_if_already_exists(self):
+        self.login(self.regular_user)
+        task_reminder = TaskReminder()
+        task_reminder.set_reminder(self.task, TASK_REMINDER_ONE_DAY_BEFORE)
+        task_reminder.set_reminder(self.task, TASK_REMINDER_ONE_WEEK_BEFORE)
+
+        self.assertEqual(
+            TASK_REMINDER_ONE_WEEK_BEFORE.option_type,
+            task_reminder.get_sql_reminder(self.task).option_type)
+
+    def test_clear_reminder_removes_annotation_reminder_for_current_user(self):
+        self.login(self.regular_user)
+        task_reminder = TaskReminder()
+
+        task_reminder.set_reminder(self.task, TASK_REMINDER_ONE_DAY_BEFORE)
+        task_reminder.set_reminder(
+            self.task, TASK_REMINDER_ONE_DAY_BEFORE,
+            user_id=self.dossier_responsible.getId())
+
+        task_reminder.clear_reminder(self.task)
+        self.assertIsNone(task_reminder.get_reminder(self.task))
+
+        self.assertEqual(
+            TASK_REMINDER_ONE_DAY_BEFORE.option_type,
+            task_reminder.get_reminder(
+                self.task, user_id=self.dossier_responsible.getId()).option_type)
+
+    def test_clear_reminder_removes_sql_reminder_for_current_user(self):
+        self.login(self.regular_user)
+        task_reminder = TaskReminder()
+
+        task_reminder.set_reminder(self.task, TASK_REMINDER_ONE_DAY_BEFORE)
+        task_reminder.set_reminder(
+            self.task, TASK_REMINDER_ONE_DAY_BEFORE,
+            user_id=self.dossier_responsible.getId())
+
+        task_reminder.clear_reminder(self.task)
+        self.assertIsNone(task_reminder.get_sql_reminder(self.task))
+
+        self.assertEqual(
+            TASK_REMINDER_ONE_DAY_BEFORE.option_type,
+            task_reminder.get_sql_reminder(
+                self.task, user_id=self.dossier_responsible.getId()).option_type)
