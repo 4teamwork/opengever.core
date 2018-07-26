3db471793dc5ea85e36b1e4c2bbec767c3f11b0f
Pablo Vergés <pablo.verges@4teamwork.ch>
2018-07-26 10:33:46 +0200

    Update workflow security

--- opengever/core/upgrades/20180716085827_meeting_templates/upgrade.py
+++ opengever/core/upgrades/20180716085827_meeting_templates/upgrade.py
@@ -7,3 +7,5 @@ class MeetingTemplates(UpgradeStep):
 
     def __call__(self):
         self.install_upgrade_profile()
+        self.update_workflow_security(['opengever_templatefolder_workflow'],
+                                      reindex_security=False)
