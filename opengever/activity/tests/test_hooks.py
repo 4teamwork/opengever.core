8f36799c10886af635eb3221c089c1372eb8e5fd
Pablo Vergés <pablo.verges@4teamwork.ch>
2018-07-30 16:56:27 +0200

    Minor cleanup on activity hook tests.

--- opengever/activity/tests/test_hooks.py
+++ opengever/activity/tests/test_hooks.py
@@ -1,4 +1,3 @@
-from opengever.activity.hooks import DEFAULT_SETTINGS
 from opengever.activity.model.settings import NotificationDefault
 from opengever.testing import IntegrationTestCase
 
@@ -8,8 +7,6 @@ class TestNotificationDefaultsHook(IntegrationTestCase):
     def test_notification_default(self):
         # only check task-added default as a sample
         default = NotificationDefault.query.by_kind('task-added').first()
-        self.assertEquals(
-            frozenset([u'task_issuer', u'task_responsible']),
-            default.badge_notification_roles)
-        self.assertEquals(frozenset([u'task_responsible']),
-                          default.mail_notification_roles)
+        expected_notifications = frozenset([u'task_issuer', u'task_responsible'])
+        self.assertEquals(expected_notifications, default.badge_notification_roles)
+        self.assertEquals(frozenset([u'task_responsible']), default.mail_notification_roles)
