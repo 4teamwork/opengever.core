b928feb7d39685cf89fc6bd8a50aee373e0503eb
Pablo Vergés <pablo.verges@4teamwork.ch>
2018-07-30 17:40:33 +0200

    Refactor activity notification resolve view tests.

--- opengever/activity/tests/test_resolve.py
+++ opengever/activity/tests/test_resolve.py
@@ -1,125 +1,89 @@
 from ftw.builder import Builder
 from ftw.builder import create
 from ftw.testbrowser import browsing
-from opengever.activity import notification_center
 from opengever.activity.browser import resolve_notification_url
+from opengever.activity.model import Notification
 from opengever.base.oguid import Oguid
-from opengever.base.security import elevated_privileges
-from opengever.testing import FunctionalTestCase
+from opengever.testing import IntegrationTestCase
 from plone import api
-from plone.app.testing import TEST_USER_ID
-import transaction
 
 
-class TestResolveNotificationView(FunctionalTestCase):
+class TestResolveNotificationView(IntegrationTestCase):
+
+    def setUp(self):
+        super(TestResolveNotificationView, self).setUp()
+        # XXX - Cannot fixturise SQL objects yet.
+        with self.login(self.regular_user):
+            oguid = Oguid.for_object(self.task)
+            resource = create(Builder('resource').oguid(oguid.id))
+            activity = create(Builder('activity').having(resource=resource))
+            self.notification_id = 123
+            create(
+                Builder('notification')
+                .id(self.notification_id)
+                .having(activity=activity, userid=self.regular_user.id),
+            )
+            # XXX - Refresh the SQL object
+            self.notification = Notification.query.get(self.notification_id)
 
     def test_resolve_notification_url(self):
-        notification = create(Builder('notification')
-                              .id('123')
-                              .having(userid='peter'))
-        url = resolve_notification_url(notification)
         self.assertEquals(
-            'http://example.com/@@resolve_notification?notification_id=123',
-            url)
+            'http://nohost/plone/@@resolve_notification?notification_id=123',
+            resolve_notification_url(self.notification),
+        )
 
     @browsing
     def test_raises_notfound_when_notification_id_is_missing(self, browser):
+        self.login(self.regular_user, browser)
         with browser.expect_http_error(reason='Not Found'):
-            browser.login().open(self.portal, view='resolve_notification')
+            browser.open(self.portal, view='resolve_notification')
 
     @browsing
     def test_raises_notfound_when_notification_id_is_invalid(self, browser):
+        self.login(self.regular_user, browser)
         with browser.expect_http_error(reason='Not Found'):
-            browser.login().open(self.portal, view='resolve_notification',
-                                 data={'notification_id': '123'})
-
-    @browsing
-    def test_raises_unauthorized_when_notification_is_not_for_the_current_user(self, browser):
-        task = create(Builder('task'))
-        oguid = Oguid.for_object(task)
-
-        resource = create(Builder('resource').oguid(oguid.id))
-        activity = create(Builder('activity').having(resource=resource))
-        notification = create(Builder('notification')
-                              .having(activity=activity, userid='hugo.boss'))
-
-        with browser.expect_unauthorized():
-            browser.login().open(
-                self.portal,
-                view='resolve_notification?notification_id={}'.format(
-                    notification.notification_id))
-
-    @browsing
-    def test_mark_notification_as_read(self, browser):
-        task = create(Builder('task'))
-        oguid = Oguid.for_object(task)
-
-        resource = create(Builder('resource').oguid(oguid.id))
-        activity = create(Builder('activity').having(resource=resource))
-        notification = create(Builder('notification')
-                              .having(activity=activity, userid=TEST_USER_ID))
-
-        browser.login().open(self.portal, view='resolve_notification',
-                             data={'notification_id': notification.notification_id})
-
-        notification = notification_center().get_notification(
-            notification.notification_id)
-
-        self.assertTrue(notification.is_read)
-
-    @browsing
-    def test_redirects_to_the_object_if_its_on_the_current_admin_unit(self, browser):
-        task = create(Builder('task'))
-        oguid = Oguid.for_object(task)
-
-        resource = create(Builder('resource').oguid(oguid.id))
-        activity = create(Builder('activity').having(resource=resource))
-        notification = create(Builder('notification')
-                              .having(activity=activity, userid=TEST_USER_ID))
-
-        browser.login().open(self.portal, view='resolve_notification',
-                             data={'notification_id': notification.notification_id})
-
-        self.assertEquals(task.absolute_url(), browser.url)
+            browser.open(self.portal, view='resolve_notification', data={'notification_id': '1234'})
 
     @browsing
     def test_raises_notfound_when_oguid_does_no_longer_exists(self, browser):
-        task = create(Builder('task'))
-        oguid = Oguid.for_object(task)
-
-        resource = create(Builder('resource').oguid(oguid.id))
-        activity = create(Builder('activity').having(resource=resource))
-        notification = create(Builder('notification')
-                              .having(activity=activity, userid=TEST_USER_ID))
-
-        with elevated_privileges():
-            # This happens on rejecting a submitte proposal within the
-            # opengever.meeting-module. A submtited proposal will be deleted
-            # on rejecting and is no longer resolveable.
-            api.content.delete(task)
-            transaction.commit()
-
+        # This happens on rejecting a submitte proposal within the
+        # opengever.meeting-module. A submtited proposal will be deleted
+        # on rejecting and is no longer resolveable.
+        self.login(self.manager)
+        api.content.delete(self.task)
         with browser.expect_http_error(reason='Not Found'):
-            browser.login().open(self.portal, view='resolve_notification',
-                                 data={'notification_id': notification.notification_id})
+            self.login(self.regular_user, browser)
+            data = {'notification_id': self.notification.notification_id}
+            browser.open(self.portal, view='resolve_notification', data=data)
 
     @browsing
     def test_redirects_to_the_resolveoguid_on_admin_unit_for_foreign_ressources(self, browser):
-
-        create(Builder('admin_unit')
-               .having(public_url='http://example.com/additional')
-               .id('additional'))
-
-        resource = create(Builder('resource').oguid('additional:123'))
-        activity = create(Builder('activity').having(resource=resource))
-        notification = create(Builder('notification')
-                              .having(activity=activity, userid=TEST_USER_ID))
-
         # Calling the resolve_notification view for a foreign object should
         # raise an NotFound exception,  because the view redirects to the
         # foreign admin_unit. But we can't setup a second plone site
         # in this test.
+        resource = create(Builder('resource').oguid('additional:{}'.format(self.notification_id)))
+        activity = create(Builder('activity').having(resource=resource))
+        notification = create(Builder('notification').having(activity=activity, userid=self.regular_user.id))
         with browser.expect_http_error(reason='Not Found'):
-            browser.login().open(
-                self.portal, view='resolve_notification',
-                data={'notification_id': notification.notification_id})
+            self.login(self.regular_user, browser)
+            browser.open(self.portal, view='resolve_notification', data={'notification_id': notification.notification_id})
+
+    @browsing
+    def test_mark_notification_as_read(self, browser):
+        self.login(self.regular_user, browser)
+        browser.open(self.portal, view='resolve_notification', data={'notification_id': self.notification.notification_id})
+        self.assertTrue(self.notification.is_read)
+
+    @browsing
+    def test_raises_unauthorized_when_notification_is_not_for_the_current_user(self, browser):
+        self.login(self.dossier_responsible, browser)
+        with browser.expect_unauthorized():
+            view = 'resolve_notification?notification_id={}'.format(self.notification.notification_id)
+            browser.open(self.portal, view=view)
+
+    @browsing
+    def test_redirects_to_the_object_if_its_on_the_current_admin_unit(self, browser):
+        self.login(self.regular_user, browser)
+        browser.open(self.portal, view='resolve_notification', data={'notification_id': self.notification.notification_id})
+        self.assertEquals(self.task.absolute_url(), browser.url)
