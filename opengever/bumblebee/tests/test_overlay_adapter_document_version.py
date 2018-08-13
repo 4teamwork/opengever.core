d816f5cf8fba1f63a0e5d400193fd5098327c073
Joni Orponen <j.orponen@4teamwork.ch>
2018-08-13 18:12:55 +0200

    Port bumblebee versioned document overlay adapter tests to integration tests.

--- opengever/bumblebee/tests/test_overlay_adapter_document_version.py
+++ opengever/bumblebee/tests/test_overlay_adapter_document_version.py
@@ -1,27 +1,22 @@
-from ftw.builder import Builder
-from ftw.builder import create
 from opengever.bumblebee.browser.overlay import BumblebeeDocumentVersionOverlay
 from opengever.bumblebee.interfaces import IBumblebeeOverlay
 from opengever.bumblebee.interfaces import IVersionedContextMarker
-from opengever.core.testing import OPENGEVER_FUNCTIONAL_BUMBLEBEE_LAYER
-from opengever.testing import FunctionalTestCase
+from opengever.testing import IntegrationTestCase
 from zope.component import getMultiAdapter
 from zope.interface import alsoProvides
 from zope.interface.verify import verifyClass
 
 
-class TestAdapterRegisteredProperly(FunctionalTestCase):
+class TestAdapterRegisteredProperly(IntegrationTestCase):
 
-    layer = OPENGEVER_FUNCTIONAL_BUMBLEBEE_LAYER
+    features = (
+        'bumblebee',
+        )
 
     def test_get_overlay_adapter_for_documents(self):
-        dossier = create(Builder('dossier'))
-        document = create(Builder('document').within(dossier))
-
+        self.login(self.regular_user)
         alsoProvides(self.request, IVersionedContextMarker)
-
-        adapter = getMultiAdapter((document, self.request), IBumblebeeOverlay)
-
+        adapter = getMultiAdapter((self.document, self.request), IBumblebeeOverlay)
         self.assertIsInstance(adapter, BumblebeeDocumentVersionOverlay)
 
     def test_verify_implemented_interfaces(self):
