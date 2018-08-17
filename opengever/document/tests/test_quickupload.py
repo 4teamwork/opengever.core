ec7c6f18f0ab54299f5ecf6824782c59605aa8bf
Joni Orponen <j.orponen@4teamwork.ch>
2018-08-17 16:36:02 +0200

    Port document quickupload tests to integration tests.

--- opengever/document/tests/test_quickupload.py
+++ opengever/document/tests/test_quickupload.py
@@ -1,47 +1,39 @@
 from ftw.builder import Builder
 from ftw.builder import create
-from opengever.testing import FunctionalTestCase
+from opengever.testing import IntegrationTestCase
 from plone.locking.interfaces import IRefreshableLockable
 from zExceptions import Unauthorized
 
 
-class TestDocumentQuickupload(FunctionalTestCase):
-
-    def setUp(self):
-        super(TestDocumentQuickupload, self).setUp()
-        self.dossier = create(Builder('dossier'))
-        self.document = create(Builder('document')
-                               .titled('Anfrage Herr Meier')
-                               .checked_out()
-                               .attach_file_containing('OLD DATA')
-                               .within(self.dossier))
+class TestDocumentQuickupload(IntegrationTestCase):
 
     def test_raises_unauthorized_when_document_is_not_checked_out(self):
-        document = create(Builder('document'))
+        self.login(self.regular_user)
         with self.assertRaises(Unauthorized):
             create(Builder('quickuploaded_document')
-                   .within(document)
+                   .within(self.document)
                    .with_data('text'))
 
     def test_raises_unauthorized_when_document_is_locked(self):
+        self.login(self.regular_user)
         IRefreshableLockable(self.document).lock()
-
         with self.assertRaises(Unauthorized):
             create(Builder('quickuploaded_document')
                    .within(self.document)
                    .with_data('text'))
 
     def test_file_is_updated(self):
+        self.login(self.regular_user)
+        self.checkout_document(self.document)
         create(Builder('quickuploaded_document')
                .within(self.document)
                .with_data('NEW DATA'))
-
         self.assertEquals('NEW DATA', self.document.file.data)
 
     def test_uses_existing_filename_but_new_extension(self):
+        self.login(self.regular_user)
+        self.checkout_document(self.document)
         create(Builder('quickuploaded_document')
                .within(self.document)
                .with_data('NEW DATA', filename='test.pdf'))
-
-        self.assertEquals('Anfrage Herr Meier.pdf',
-                          self.document.file.filename)
+        self.assertEquals('Vertraegsentwurf.pdf', self.document.file.filename)
