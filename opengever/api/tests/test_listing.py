669fef8b56715e482b38b10963b002e8b4e2c546
Philippe Gross <philippe.gross@4teamwork.ch>
2018-08-22 14:45:16 +0200

    Add minimal tests for the @listing endpoint.

---
+++ opengever/api/tests/test_listing.py
@@ -0,0 +1,50 @@
+from opengever.testing import IntegrationTestCase
+from ftw.testbrowser import browsing
+import requests
+
+
+class TestListingEndpoint(IntegrationTestCase):
+
+    @browsing
+    def test_dossier_listing(self, browser):
+        self.login(self.regular_user, browser=browser)
+
+        view = '@listing?name=dossiers&columns=reference&columns=title&columns=review_state&columns=responsible&sort_on=created'
+        browser.open(self.repository_root, view=view, headers={'Accept': 'application/json'})
+
+        self.assertEquals(
+            {u'review_state': u'dossier-state-active',
+             u'responsible': u'Ziegler Robert (robert.ziegler)',
+             u'@id': u'http://nohost/plone/ordnungssystem/fuhrung/vertrage-und-vereinbarungen/dossier-1',
+             u'reference': u'Client1 1.1 / 1',
+             u'title': u'Vertr\xe4ge mit der kantonalen Finanzverwaltung'},
+            browser.json['items'][-1])
+
+    @browsing
+    def test_document_listing(self, browser):
+        self.login(self.regular_user, browser=browser)
+
+        view = '@listing?name=documents&columns=reference&columns=title&columns=modified&columns=document_author&columns=containing_dossier&sort_on=created'
+        browser.open(self.dossier, view=view, headers={'Accept': 'application/json'})
+
+        self.assertEquals(
+            {u'reference': u'Client1 1.1 / 1 / 12',
+             u'title': u'Vertr\xe4gsentwurf',
+             u'document_author': u'test_user_1_',
+             u'@id': u'http://nohost/plone/ordnungssystem/fuhrung/vertrage-und-vereinbarungen/dossier-1/document-12',
+             u'modified': u'2016-08-31T13:07:33+00:00',
+             u'containing_dossier': u'Vertr\xe4ge mit der kantonalen Finanzverwaltung'},
+            browser.json['items'][-1])
+
+    @browsing
+    def test_file_information(self, browser):
+        self.login(self.regular_user, browser=browser)
+
+        view = '@listing?name=documents&columns=filename&columns=filesize&sort_on=created'
+        browser.open(self.dossier, view=view, headers={'Accept': 'application/json'})
+
+        self.assertEquals(
+            {u'@id': self.document.absolute_url(),
+             u'filesize': self.document.file.size,
+             u'filename': u'Vertraegsentwurf.docx'},
+            browser.json['items'][-1])
