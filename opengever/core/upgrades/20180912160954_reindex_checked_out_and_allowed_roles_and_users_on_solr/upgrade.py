92f34ab6ebb687067e5601a13528696716fae332
Philippe Gross <philippe.gross@4teamwork.ch>
2018-09-12 16:19:14 +0200

    Add upgradestep which reindexes checked_out and allowedRolesAndUsers,
    on solr only. Because those could be affected by the reindexing
    problem (see previous commit). The upgradestep is registered as deferrable.

---
+++ opengever/core/upgrades/20180912160954_reindex_checked_out_and_allowed_roles_and_users_on_solr/upgrade.py
@@ -0,0 +1,24 @@
+from ftw.upgrade import UpgradeStep
+from opengever.base.interfaces import ISearchSettings
+from plone import api
+from zope.component import queryMultiAdapter
+
+
+class ReindexCheckedOutAndAllowedRolesAndUsersOnSolr(UpgradeStep):
+    """Reindex checked_out and allowedRolesAndUsers on solr.
+    """
+
+    deferrable = True
+
+    def __call__(self):
+        solr_enabled = api.portal.get_registry_record(
+            name='use_solr', interface=ISearchSettings)
+        if not solr_enabled:
+            return
+
+        portal = api.portal.get()
+        solr_maintenance = queryMultiAdapter(
+            (portal, portal.REQUEST), name=u'solr-maintenance')
+
+        solr_maintenance.reindex(
+            idxs=['checked_out', 'allowedRolesAndUsers'], doom=False)
