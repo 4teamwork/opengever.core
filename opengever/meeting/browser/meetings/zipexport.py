fd8af97f488817b1d307cab4b4c145d3438527bf
David Erni <david.erni@4teamwork.ch>
2018-10-02 17:01:46 +0200

    Add view and vue app to demand meeting zip.

--- opengever/meeting/browser/meetings/zipexport.py
+++ opengever/meeting/browser/meetings/zipexport.py
@@ -1,18 +1,137 @@
 from ftw.zipexport.generation import ZipGenerator
 from ftw.zipexport.utils import normalize_path
+from opengever.base.handlebars import get_handlebars_template
+from opengever.base.utils import disable_edit_bar
+from opengever.meeting import _
 from opengever.meeting.command import AgendaItemListOperations
 from opengever.meeting.command import CreateGeneratedDocumentCommand
 from opengever.meeting.command import MergeDocxProtocolCommand
 from opengever.meeting.command import ProtocolOperations
 from opengever.meeting.exceptions import AgendaItemListMissingTemplate
 from opengever.meeting.interfaces import IMeetingWrapper
+from opengever.meeting.zipexport import MeetingZipExporter
+from pkg_resources import resource_filename
+from plone.protect.interfaces import IDisableCSRFProtection
 from Products.CMFPlone.utils import safe_unicode
 from Products.Five.browser import BrowserView
 from StringIO import StringIO
+from zope.i18n import translate
+from zope.interface import alsoProvides
 from ZPublisher.Iterators import filestream_iterator
 import json
 import os
 import pytz
+
+
+class DemandMeetingZip(BrowserView):
+    """Display a view where the zip can be downloaded eventually."""
+
+    def __init__(self, context, request):
+        super(DemandMeetingZip, self).__init__(context, request)
+        self.model = self.context.model
+
+    def __call__(self):
+        disable_edit_bar()
+        alsoProvides(self.request, IDisableCSRFProtection)
+
+        committee_model = self.model.committee
+        committee = committee_model.oguid.resolve_object()
+
+        public_id = self.request.get('public_id', None)
+        if public_id:
+            # XXX validate id
+            self.public_id = public_id
+            return super(DemandMeetingZip, self).__call__()
+
+        else:
+            public_id = MeetingZipExporter(self.model, committee).demand_pdfs()
+            self.public_id = str(public_id)
+            url = "{}/@@demand_meeting_zip?public_id={}".format(
+                self.context.absolute_url(), self.public_id)
+            return self.request.RESPONSE.redirect(url)
+
+    @property
+    def vuejs_template(self):
+        return get_handlebars_template(
+            resource_filename('opengever.meeting.browser.meetings',
+                              'templates/demand_zip.html'))
+
+    def get_zip_export_title(self):
+        return _(u'title_zip_export',
+                 default=u'Zip export: ${title}',
+                 mapping={'title': self.model.get_title()})
+
+    def translations(self):
+        return json.dumps({
+            'label_info': translate(
+                _(u'label_info', default=u'Information'),
+                context=self.request),
+            'label_warning': translate(
+                _(u'label_warning', default=u'Warning'),
+                context=self.request),
+            'label_error': translate(
+                _(u'label_error', default=u'Error'),
+                context=self.request),
+            'msg_some_documents_remain_uncoverted': translate(
+                _(u'msg_some_documents_remain_uncoverted',
+                  default=u'Some documents could not be converted to PDF, '
+                           'their original files will be included in the '
+                           'Zip.'),
+                context=self.request),
+            'msg_zip_ready_for_download': translate(
+                _(u'msg_zip_ready_for_download',
+                  default=u'The zip file is ready for download.'),
+                context=self.request),
+            'msg_zip_timeout': translate(
+                _(u'msg_zip_timeout',
+                  default=u'Generating the Zip took too long.'),
+                context=self.request),
+            'msg_zip_error': translate(
+                _(u'msg_zip_error',
+                  default=u'There was an error while generating the Zip.'),
+                context=self.request),
+            'msg_zip_creation_in_progress': translate(
+                _(u'msg_zip_creation_in_progress',
+                  default=u'A Zip file with PDFs for all documents of this '
+                           'meeting is being generated.'),
+                context=self.request),
+            'desc_pdf_zip_contents': translate(
+                _(u'desc_pdf_zip_contents',
+                  default=u'The zip contains a PDF file for each document in '
+                           'this meeting and a JSON file which allows it to '
+                           'be imported by the meeting app.'),
+                context=self.request),
+            'label_button_download_zip': translate(
+                _(u'label_button_download_zip',
+                  default=u'Download Zip'),
+                context=self.request),
+            'label_button_error': translate(
+                _(u'label_button_error',
+                  default=u'Error Generating Zip'),
+                context=self.request),
+            'label_button_creation_in_progress': translate(
+                _(u'label_button_creation_in_progress',
+                  default=u'Generating Zip ...'),
+                context=self.request),
+            'label_backlink_to_meeting': translate(
+                _(u'label_backlink_to_meeting',
+                  default=u'Back to the meeting'),
+                context=self.request),
+        })
+
+    def poll_url(self):
+        return "{}/@@poll_meeting_zip?public_id={}".format(
+            self.context.absolute_url(), self.public_id)
+
+    def download_url(self):
+        return "{}/@@download_meeting_zip?public_id={}".format(
+            self.context.absolute_url(), self.public_id)
+
+    def meeting_url(self):
+        return self.model.get_url()
+
+    def zip_export_url(self):
+        return self.model.get_url(view='export-meeting-zip')
 
 
 class MeetingZipExport(BrowserView):
---
+++ opengever/meeting/browser/resources/zip_export_app.js
@@ -0,0 +1,74 @@
+$(init);
+function init() {
+  var FIRST_POLL_INTERVAL = 500;
+  var POLL_INTERVAL = 2000;
+  var POLL_TIMEOUT = 60000;
+
+  var app = new Vue({
+    template: '#meeting-zip-export',
+    el: '#meeting-zip-export-vue-app',
+    data: {
+      i18n: {},
+      isFinished: false,
+      isTimeout: false,
+      isError: false,
+      isDocumentSkipped: false,
+      downloadurl: null,
+      pollurl: null,
+      meetingurl: null,
+    },
+
+    beforeMount: function () {
+      var portalurl = this.$el.attributes['data-portalurl'].value;
+      this.pollurl = this.$el.attributes['data-pollurl'].value;
+      this.downloadurl = this.$el.attributes['data-downloadurl'].value;
+      this.meetingurl = this.$el.attributes['data-meetingurl'].value;
+      this.i18n = JSON.parse(this.$el.attributes['data-i18n'].value);
+
+      var requester = axios.create();
+      requester.defaults.headers.common['Accept'] = 'application/json';
+      requester.defaults.headers.common['Content-Type'] = 'application/json';
+      this.requester = requester;
+
+      this.pollingInitiated = Date.now();
+    },
+
+    mounted: function () {
+      setTimeout(function () {
+        this.fetchData();
+      }.bind(this), FIRST_POLL_INTERVAL);
+    },
+
+    methods: {
+      fetchData: function () {
+        // Make sure IE 11 does not cache the fetch request
+        var url = this.pollurl + '&_t=' + Date.now().toString();
+
+        // abort if timeout exceeded
+        if (Date.now() - this.pollingInitiated > POLL_TIMEOUT) {
+          this.isTimeout = true;
+          this.isError = true;
+          return;
+        }
+
+        // poll for status
+        return this.requester.get(url)
+          .then(function (response) {
+            if (response.data['converting'] <= 0) {
+              this.isFinished = true;
+              if (response.data['skipped'] > 0) {
+                this.isDocumentSkipped = true;
+              }
+            } else {
+              setTimeout(function () {
+                this.fetchData();
+              }.bind(this), POLL_INTERVAL)
+            }
+        }.bind(this))
+        .catch(function (error) {
+          this.isError = true;
+        }.bind(this));
+      }
+    }
+  });
+}
