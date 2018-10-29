64bcc0cc2d06e7ecc76437516f0c36a1d9fd0e44
Joni Orponen <j.orponen@4teamwork.ch>
2018-10-29 22:50:29 +0100

    Port opengever.api content creation tests to use the @restapi decorator.

--- opengever/api/tests/test_content_creation.py
+++ opengever/api/tests/test_content_creation.py
@@ -1,30 +1,22 @@
 from ftw.bumblebee.interfaces import IBumblebeeDocument
-from ftw.testbrowser import browsing
+from ftw.testbrowser import restapi
 from opengever.base.behaviors.lifecycle import ILifeCycle
 from opengever.dossier.behaviors.dossier import IDossier
 from opengever.testing import IntegrationTestCase
 from plone import api
-import json
 
 
 class TestContentCreation(IntegrationTestCase):
 
-    api_headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-    }
-
     def setUp(self):
         super(TestContentCreation, self).setUp()
-        self.portal = self.layer['portal']
-
         lang_tool = api.portal.get_tool('portal_languages')
         lang_tool.setDefaultLanguage('de-ch')
         lang_tool.supported_langs = ['fr-ch', 'de-ch']
 
-    @browsing
-    def test_dossier_creation(self, browser):
-        self.login(self.regular_user, browser)
+    @restapi
+    def test_dossier_creation(self, api_client):
+        self.login(self.regular_user, api_client)
         payload = {
             u'@type': u'opengever.dossier.businesscasedossier',
             u'title': u'Sanierung B\xe4rengraben 2016',
@@ -33,15 +25,10 @@ class TestContentCreation(IntegrationTestCase):
             u'archival_value': u'unchecked',
             u'retention_period': 5,
         }
-        response = browser.open(
-            self.leaf_repofolder.absolute_url(),
-            data=json.dumps(payload),
-            method='POST',
-            headers=self.api_headers)
+        api_client.open(self.leaf_repofolder, data=payload)
+        self.assertEqual(201, api_client.status_code)
 
-        self.assertEqual(201, response.status_code)
-
-        new_object_id = str(response.json['id'])
+        new_object_id = str(api_client.contents['id'])
         dossier = self.leaf_repofolder.restrictedTraverse(new_object_id)
 
         self.assertEqual(u'Sanierung B\xe4rengraben 2016', dossier.title)
@@ -50,9 +37,9 @@ class TestContentCreation(IntegrationTestCase):
         self.assertEqual(u'unchecked', ILifeCycle(dossier).archival_value)
         self.assertEqual(5, ILifeCycle(dossier).retention_period)
 
-    @browsing
-    def test_document_creation(self, browser):
-        self.login(self.regular_user, browser)
+    @restapi
+    def test_document_creation(self, api_client):
+        self.login(self.regular_user, api_client)
         payload = {
             u'@type': u'opengever.document.document',
             u'title': u'Sanierung B\xe4rengraben 2016',
@@ -62,15 +49,10 @@ class TestContentCreation(IntegrationTestCase):
                 u'filename': u'b\xe4rengraben.txt',
                 u'content-type': u'text/plain'},
         }
-        response = browser.open(
-            self.dossier.absolute_url(),
-            data=json.dumps(payload),
-            method='POST',
-            headers=self.api_headers)
+        api_client.open(self.dossier, data=payload)
+        self.assertEqual(201, api_client.status_code)
 
-        self.assertEqual(201, response.status_code)
-
-        new_object_id = str(response.json['id'])
+        new_object_id = str(api_client.contents['id'])
         doc = self.dossier.restrictedTraverse(new_object_id)
         self.assertEqual(u'Sanierung B\xe4rengraben 2016', doc.title)
         self.assertEqual(u'Sanierung Baerengraben 2016.txt', doc.file.filename)
