f0042b8a1bcdd7e5bf206671f504d711c1b361d2
Joni Orponen <j.orponen@4teamwork.ch>
2018-10-29 22:32:33 +0100

    Port opengever.api bumblebee tests to use the @restapi decorator.

--- opengever/api/tests/test_bumblebee.py
+++ opengever/api/tests/test_bumblebee.py
@@ -1,16 +1,12 @@
-from ftw.testbrowser import browsing
+from ftw.testbrowser import restapi
 from opengever.testing import IntegrationTestCase
 
 
 class TestBumblebeeSession(IntegrationTestCase):
 
-    @browsing
-    def test_create_bumblebee_session(self, browser):
-        self.login(self.regular_user, browser)
-        browser.open(
-            self.portal.absolute_url() + '/@preview-session',
-            method='POST',
-            headers={'Accept': 'application/json'},
-        )
-        self.assertEqual(browser.status_code, 204)
-        self.assertIn('bumblebee-local', browser.cookies)
+    @restapi
+    def test_create_bumblebee_session(self, api_client):
+        self.login(self.regular_user, api_client)
+        api_client.open(self.portal, endpoint='@preview-session', method='POST')
+        self.assertEqual(api_client.status_code, 204)
+        self.assertIn('bumblebee-local', api_client.cookies)
