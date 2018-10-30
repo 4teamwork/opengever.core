655f0265588bb660fc7540fecca6f6e5dbc96b4d
Joni Orponen <j.orponen@4teamwork.ch>
2018-10-30 15:05:05 +0100

    Port opengever.api move tests to use the @restapi decorator.

--- opengever/api/tests/test_move.py
+++ opengever/api/tests/test_move.py
@@ -1,24 +1,13 @@
-from ftw.testbrowser import browsing
-from plone.app.testing import setRoles
+from ftw.testbrowser import restapi
 from opengever.testing import IntegrationTestCase
-import json
 
 
 class TestMove(IntegrationTestCase):
 
-    def setUp(self):
-        super(TestMove, self).setUp()
-
-    @browsing
-    def test_regular_user_can_move_document(self, browser):
-        self.login(self.regular_user, browser)
+    @restapi
+    def test_regular_user_can_move_document(self, api_client):
+        self.login(self.regular_user, api_client)
         doc_id = self.document.getId()
-        browser.open(
-            self.subdossier.absolute_url() + '/@move',
-            data=json.dumps({"source": self.document.absolute_url()}),
-            method='POST',
-            headers={'Accept': 'application/json',
-                     'Content-Type': 'application/json'},
-        )
-        self.assertEqual(200, browser.status_code)
+        api_client.open(self.subdossier, endpoint='@move', data={"source": self.document.absolute_url()})
+        self.assertEqual(200, api_client.status_code)
         self.assertIn(doc_id, self.subdossier)
