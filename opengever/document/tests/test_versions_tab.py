08e05770e36879ecc0161373e9a878b08827a9c6
Niklaus Johner <niklaus.johner@4teamwork.ch>
2018-11-02 13:28:18 +0100

    Add column in version tab to save the PDF of that version as document.

--- opengever/document/tests/test_versions_tab.py
+++ opengever/document/tests/test_versions_tab.py
@@ -176,7 +176,8 @@ class TestVersionsTabWithBubmelbeeActivated(BaseVersionsTab):
              'Kommentar',
              'Kopie herunterladen',
              u'Zur\xfccksetzen',
-             'Vorschau'],
+             'Vorschau',
+             'PDF speichern unter'],
             column_headers)
 
     @browsing
@@ -213,6 +214,7 @@ class TestVersionsTabForDocumentWithoutInitialVersion(FunctionalTestCase):
               'Download copy': 'Download copy',
               'Preview': 'Preview',
               'Revert': '',
+              'Save PDF': 'Save PDF',
               'Version': '0',
               'Date': 'Nov 06, 2016 12:00 AM',
               'Changed by': 'Test User (test_user_1_)'}],
