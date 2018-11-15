c5743ea95f093abfd7e758caa1a82e81b4298df7
Joni Orponen <j.orponen@4teamwork.ch>
2018-11-15 11:41:14 +0100

    dump

--- opengever/journal/tab.py
+++ opengever/journal/tab.py
@@ -23,30 +23,21 @@ from zope.interface import Interface
 import cPickle
 
 
-def title_helper(item, value):
-    return translate(item['action'].get('title'), context=getRequest())
-
-
-def empty_template_helper(self, *args, **kwargs):
-    return ''
+def title_helper(*args):
+    """Translate item action titles. All values we'd ever get are empty."""
+    return translate(args[0]['action']['title'], context=getRequest())
 
 
 class IJournalSourceConfig(ITableSourceConfig):
-    """Marker interface for journal table source config.
-    """
+    """Marker interface for journal table source config."""
 
 
 class JournalTab(BaseListingTab):
-    """Journal tab implementing IJorunalConfig.
-    """
+    """Journal tab implementing IJorunalConfig."""
 
     implements(IJournalSourceConfig)
 
-    reference_template = ViewPageTemplateFile(
-        'templates/journal_references.pt')
-
-    # This is a bound method on purpose so it can be called down the line
-    select_all_template = empty_template_helper
+    reference_template = ViewPageTemplateFile('templates/journal_references.pt')
 
     sort_on = 'time'
     sort_reverse = True
@@ -57,66 +48,53 @@ class JournalTab(BaseListingTab):
     major_actions = []
     selection = ViewPageTemplateFile("templates/journal_selection.pt")
 
+    def __init__(self, context, request):
+        super(JournalTab, self).__init__(context, request)
+        self.documents = []
+        self.contacts = []
+        self.users = []
+
     @property
     def columns(self):
         return (
-            {'column': 'time',
-             'column_title': _(u'label_time', default=u'Time'),
-             'transform': helper.readable_date_time},
-
-            {'column': 'title',
-             'column_title': _(u'label_title', 'Title'),
-             'transform': title_helper},
-
-            {'column': 'actor',
-             'column_title': _(u'label_actor', default=u'Changed by'),
-             'transform': linked_ogds_author},
-
-            {'column': 'comments',
-             'column_title': _(u'label_comments', default=u'Comments'),
-             'transform': tooltip_helper,
-             'sortable': False},
-
-            {'column': 'references',
-             'column_title': _(u'label_references', default=u'References'),
-             'transform': self.journal_references,
-             'sortable': False},
+            {'column': 'time', 'column_title': _(u'label_time', default=u'Time'), 'transform': helper.readable_date_time},
+            {'column': 'title', 'column_title': _(u'label_title', 'Title'), 'transform': title_helper},
+            {'column': 'actor', 'column_title': _(u'label_actor', default=u'Changed by'), 'transform': linked_ogds_author},
+            {'column': 'comments', 'column_title': _(u'label_comments', default=u'Comments'), 'transform': tooltip_helper, 'sortable': False},  # noqa: E501
+            {'column': 'references', 'column_title': _(u'label_references', default=u'References'), 'transform': self.journal_references, 'sortable': False},  # noqa: E501
         )
 
-    def journal_references(self, item, value):
-        """
-        """
+    def journal_references(self, *args):
+        item = args[0]
         self.documents = item.get('action').get('documents', [])
         self.contacts = item.get('action').get('contacts', [])
         self.users = item.get('action').get('users', [])
         return self.reference_template(self)
 
-    def get_contactfolder_url(self):
+    @staticmethod
+    def get_contactfolder_url():
         return get_contactfolder_url()
 
     def get_base_query(self):
         return None
 
     def manual_entry_allowed(self):
-        return api.user.has_permission('Modify portal content',
-                                       user=api.user.get_current(),
-                                       obj=self.context)
+        return api.user.has_permission('Modify portal content', user=api.user.get_current(), obj=self.context)
 
 
 def time_column_sorter(a, b):
+    """Sort by timestamp."""
     return cmp(a.get('time', ''), b.get('time', ''))
 
 
 def title_column_sorter(a, b):
-    return cmp(a.get('action', {}).get('title'),
-               b.get('action', {}).get('title'))
+    """Sort by title."""
+    return cmp(a.get('action', {}).get('title'), b.get('action', {}).get('title'))
 
 
 def actor_column_sorter(a, b):
-    """Sort on the on users label (`lastname firstname (userid)`).
-    """
-    return cmp(readable_ogds_user(a, a.get('actor')),
-               readable_ogds_user(b, b.get('actor')))
+    """Sort by user label (`lastname firstname (userid)`)."""
+    return cmp(readable_ogds_user(a, a.get('actor')), readable_ogds_user(b, b.get('actor')))
 
 
 @implementer(ITableSource)
@@ -124,9 +102,11 @@ def actor_column_sorter(a, b):
 class JournalTableSource(GeverTableSource):
     """Generate a table to display in the journal tab view."""
 
-    sorter = {'time': time_column_sorter,
-              'title': title_column_sorter,
-              'actor': actor_column_sorter}
+    sorter = {
+        'actor': actor_column_sorter,
+        'time': time_column_sorter,
+        'title': title_column_sorter,
+    }
 
     batching_enabled = True
 
@@ -136,7 +116,7 @@ class JournalTableSource(GeverTableSource):
             annotations = IAnnotations(context)
             data = annotations.get(JOURNAL_ENTRIES_ANNOTATIONS_KEY, [])
         elif IWorkflowHistoryJournalizable.providedBy(context):
-            raise NotImplemented
+            raise NotImplementedError
 
         # XXX - a performance hack to replace deepcopy(data)
         # This only works as persistent objects are guaranteed to be picklable
@@ -154,7 +134,7 @@ class JournalTableSource(GeverTableSource):
         return results
 
     def extend_query_with_textfilter(self, results, text):
-        if not len(text):
+        if not text:
             return results
 
         if text.endswith('*'):
