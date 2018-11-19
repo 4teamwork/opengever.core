d689926a6dcd52eb8a0498bf2968e25bd9ce4461
Joni Orponen <j.orponen@4teamwork.ch>
2018-11-19 18:22:54 +0100

    Port opengever.api scanin tests to use the @restapi decorator.

--- opengever/api/tests/test_scanin.py
+++ opengever/api/tests/test_scanin.py
@@ -1,6 +1,6 @@
 from ftw.builder import Builder
 from ftw.builder import create
-from ftw.testbrowser import browsing
+from ftw.testbrowser import restapi
 from opengever.base.role_assignments import RoleAssignmentManager
 from opengever.base.role_assignments import SharingRoleAssignment
 from opengever.testing import IntegrationTestCase
@@ -52,160 +52,130 @@ class TestScanIn(IntegrationTestCase):
             'Accept': 'application/json',
         }
 
-    @browsing
-    def test_scanin_to_inbox(self, browser):
-        self.login(self.regular_user, browser)
+    @restapi
+    def test_scanin_to_inbox(self, api_client):
+        self.login(self.regular_user, api_client)
         self.create_single_inbox()
         body, headers = self.prepare_request()
 
-        browser.open(self.portal.absolute_url() + '/@scan-in',
-                     method='POST',
-                     headers=headers,
-                     data=body)
+        api_client.open(endpoint='@scan-in', method='POST', headers=headers, data=body)
 
-        self.assertEqual(201, browser.status_code)
-        self.login(self.administrator, browser)
+        self.assertEqual(201, api_client.status_code)
+        self.login(self.administrator, api_client)
 
         doc = self.inbox.objectValues()[-1]
         self.assertEqual('mydocument', doc.Title())
 
-    @browsing
-    def test_scanin_to_inbox_without_permission(self, browser):
-        self.login(self.administrator, browser)
-        body, headers = self.prepare_request(
-            userid=self.dossier_responsible.getId())
-        browser.exception_bubbling = True
-        with browser.expect_http_error(code=403, reason='Forbidden'):
-            browser.open(self.portal.absolute_url() + '/@scan-in',
-                         method='POST',
-                         headers=headers,
-                         data=body)
+    @restapi
+    def test_scanin_to_inbox_without_permission(self, api_client):
+        self.login(self.administrator, api_client)
+        body, headers = self.prepare_request(userid=self.dossier_responsible.getId())
+        api_client.exception_bubbling = True
+        with api_client.expect_http_error(code=403, reason='Forbidden'):
+            api_client.open(endpoint='@scan-in', method='POST', headers=headers, data=body)
 
         expected_message = 'The user does not have the required permissions to perform a scan-in via the API.'
-        self.assertEqual(expected_message, browser.json.get('error').get('message'))
+        self.assertEqual(expected_message, api_client.contents.get('error').get('message'))
 
-    @browsing
-    def test_scanin_to_org_unit_inbox(self, browser):
-        self.login(self.regular_user, browser)
+    @restapi
+    def test_scanin_to_org_unit_inbox(self, api_client):
+        self.login(self.regular_user, api_client)
         inbox = self.create_org_unit_inbox()
 
         body, headers = self.prepare_request(org_unit='fa')
-        browser.open(self.portal.absolute_url() + '/@scan-in',
-                     method='POST',
-                     headers=headers,
-                     data=body)
+        api_client.open(endpoint='@scan-in', method='POST', headers=headers, data=body)
 
-        self.assertEqual(201, browser.status_code)
+        self.assertEqual(201, api_client.status_code)
         doc = inbox.objectValues()[0]
         self.assertEqual('mydocument', doc.Title())
 
-    @browsing
-    def test_scanin_to_org_unit_inbox_by_title(self, browser):
-        self.login(self.regular_user, browser)
+    @restapi
+    def test_scanin_to_org_unit_inbox_by_title(self, api_client):
+        self.login(self.regular_user, api_client)
         inbox = self.create_org_unit_inbox()
 
         body, headers = self.prepare_request(org_unit='Finanzamt')
-        browser.open(self.portal.absolute_url() + '/@scan-in',
-                     method='POST',
-                     headers=headers,
-                     data=body)
+        api_client.open(endpoint='@scan-in', method='POST', headers=headers, data=body)
 
-        self.assertEqual(201, browser.status_code)
+        self.assertEqual(201, api_client.status_code)
         doc = inbox.objectValues()[0]
         self.assertEqual('mydocument', doc.Title())
 
-    @browsing
-    def test_scanin_to_new_private_dossier(self, browser):
-        self.login(self.manager, browser)
+    @restapi
+    def test_scanin_to_new_private_dossier(self, api_client):
+        self.login(self.regular_user, api_client)
         private_folder = self.create_private_folder()
-
         body, headers = self.prepare_request(destination='private')
-        browser.open(self.portal.absolute_url() + '/@scan-in',
-                     method='POST',
-                     headers=headers,
-                     data=body)
+        api_client.open(endpoint='@scan-in', method='POST', headers=headers, data=body)
 
-        self.assertEqual(201, browser.status_code)
+        self.assertEqual(201, api_client.status_code)
         dossier = private_folder.objectValues()[0]
         self.assertEqual('Scaneingang', dossier.Title())
         doc = dossier.objectValues()[0]
         self.assertEqual('mydocument', doc.Title())
 
-    @browsing
-    def test_scanin_to_existing_private_dossier(self, browser):
-        self.login(self.manager, browser)
+    @restapi
+    def test_scanin_to_existing_private_dossier(self, api_client):
+        self.login(self.regular_user, api_client)
         private_folder = self.create_private_folder()
         dossier = create(Builder('private_dossier')
                          .within(private_folder)
                          .titled(u'Scaneingang'))
 
         body, headers = self.prepare_request(destination='private')
-        browser.open(self.portal.absolute_url() + '/@scan-in',
-                     method='POST',
-                     headers=headers,
-                     data=body)
+        api_client.open(endpoint='@scan-in', method='POST', headers=headers, data=body)
 
-        self.assertEqual(201, browser.status_code)
+        self.assertEqual(201, api_client.status_code)
         self.assertEqual(1, len(private_folder.objectIds()))
         doc = dossier.objectValues()[0]
         self.assertEqual('mydocument', doc.Title())
 
-    @browsing
-    def test_scanin_to_missing_private_dossier(self, browser):
-        self.login(self.manager, browser)
+    @restapi
+    def test_scanin_to_missing_private_dossier(self, api_client):
+        self.login(self.manager, api_client)
 
         body, headers = self.prepare_request(destination='private')
-        with browser.expect_http_error(code=404, reason='Not Found'):
-            browser.open(self.portal.absolute_url() + '/@scan-in',
-                         method='POST',
-                         headers=headers,
-                         data=body)
+        with api_client.expect_http_error(code=404, reason='Not Found'):
+            api_client.open(endpoint='@scan-in', method='POST', headers=headers, data=body)
 
         expected_message = 'The scan-in destination does not exist.'
-        self.assertIn(expected_message, browser.json.get('error').get('message'))
+        self.assertIn(expected_message, api_client.contents.get('error').get('message'))
 
-    @browsing
-    def test_scanin_with_missing_userid(self, browser):
-        self.login(self.regular_user, browser)
-        with browser.expect_http_error(code=400, reason='Bad Request'):
-            browser.open(self.portal.absolute_url() + '/@scan-in',
-                         method='POST',
-                         headers={'Accept': 'application/json'},
-                         data={})
-        self.assertIn('Missing userid.', browser.contents)
+    @restapi
+    def test_scanin_with_missing_userid(self, api_client):
+        self.login(self.regular_user, api_client)
+        with api_client.expect_http_error(code=400, reason='Bad Request'):
+            api_client.open(endpoint='@scan-in', data={})
+        self.assertEqual({u'error': {u'message': u'Missing userid.', u'type': u'BadRequest'}}, api_client.contents)
 
-    @browsing
-    def test_scanin_with_unkown_userid(self, browser):
-        self.login(self.regular_user, browser)
+    @restapi
+    def test_scanin_with_unkown_userid(self, api_client):
+        self.login(self.regular_user, api_client)
 
         body, headers = self.prepare_request(userid='chief')
-        with browser.expect_http_error(code=404, reason='Not Found'):
-            browser.open(self.portal.absolute_url() + '/@scan-in',
-                         method='POST',
-                         headers=headers,
-                         data=body)
-        self.assertIn('Unknown user.', browser.contents)
+        with api_client.expect_http_error(code=404, reason='Not Found'):
+            api_client.open(endpoint='@scan-in', method='POST', headers=headers, data=body)
+        self.assertEqual({u'error': {u'message': u'Unknown user.', u'type': u'BadRequest'}}, api_client.contents)
 
-    @browsing
-    def test_scanin_without_file(self, browser):
-        self.login(self.regular_user, browser)
-        with browser.expect_http_error(code=400, reason='Bad Request'):
-            browser.open(self.portal.absolute_url() + '/@scan-in',
-                         method='POST',
-                         headers={'Accept': 'application/json'},
-                         data={'userid': self.regular_user.getId()})
-        self.assertIn('Missing file.', browser.contents)
+    @restapi
+    def test_scanin_without_file(self, api_client):
+        self.login(self.regular_user, api_client)
+        with api_client.expect_http_error(code=400, reason='Bad Request'):
+            api_client.open(
+                endpoint='@scan-in',
+                method='POST',
+                headers={'Accept': 'application/json'},
+                data={'userid': self.regular_user.getId()},
+            )
+        self.assertEqual({u'error': {u'message': u'Missing file.', u'type': u'BadRequest'}}, api_client.contents)
 
-    @browsing
-    def test_scanin_to_unknown_destination(self, browser):
-        self.login(self.regular_user, browser)
+    @restapi
+    def test_scanin_to_unknown_destination(self, api_client):
+        self.login(self.regular_user, api_client)
 
         body, headers = self.prepare_request(destination='unknown')
-        with browser.expect_http_error(code=404, reason='Not Found'):
-            browser.open(self.portal.absolute_url() + '/@scan-in',
-                         method='POST',
-                         headers=headers,
-                         data=body)
+        with api_client.expect_http_error(code=404, reason='Not Found'):
+            api_client.open(endpoint='@scan-in', method='POST', headers=headers, data=body)
 
         expected_message = 'The scan-in destination does not exist.'
-        self.assertIn(expected_message, browser.json.get('error').get('message'))
+        self.assertIn(expected_message, api_client.contents.get('error').get('message'))
