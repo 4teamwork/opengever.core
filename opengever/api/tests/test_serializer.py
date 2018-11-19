2bbfaed20755d338de44133c205f37b6d23627a1
Joni Orponen <j.orponen@4teamwork.ch>
2018-11-19 18:39:43 +0100

    Port opengever.api serializer tests to use the @restapi decorator.

--- opengever/api/tests/test_serializer.py
+++ opengever/api/tests/test_serializer.py
@@ -1,136 +1,124 @@
 from ftw.bumblebee.interfaces import IBumblebeeDocument
 from ftw.bumblebee.tests.helpers import DOCX_CHECKSUM
-from ftw.testbrowser import browsing
+from ftw.testbrowser import restapi
 from opengever.testing import IntegrationTestCase
 from plone import api
 
 
-class TestRepositoryFolderSerializer(IntegrationTestCase):
+class TestApiSerializer(IntegrationTestCase):
 
     def setUp(self):
-        super(TestRepositoryFolderSerializer, self).setUp()
+        super(TestApiSerializer, self).setUp()
         lang_tool = api.portal.get_tool('portal_languages')
         lang_tool.setDefaultLanguage('de-ch')
 
-    @browsing
-    def test_repofolder_serialization_contains_reference_number(self, browser):
-        self.login(self.regular_user, browser)
-        browser.open(
-            self.leaf_repofolder, headers={'Accept': 'application/json'})
-        self.assertEqual(browser.status_code, 200)
-        self.assertEqual(browser.json.get(u'reference_number'), u'Client1 1.1')
 
-    @browsing
-    def test_repofolder_serialization_contains_is_leafnode(self, browser):
-        self.login(self.regular_user, browser)
-        browser.open(
-            self.leaf_repofolder, headers={'Accept': 'application/json'})
-        self.assertEqual(browser.status_code, 200)
-        self.assertEqual(browser.json.get(u'is_leafnode'), True)
+class TestRepositoryFolderSerializer(TestApiSerializer):
 
-    @browsing
-    def test_repofolder_serialization_contains_relative_path(self, browser):
-        self.login(self.regular_user, browser)
-        browser.open(
-            self.leaf_repofolder, headers={'Accept': 'application/json'})
-        self.assertEqual(browser.status_code, 200)
+    @restapi
+    def test_repofolder_serialization_contains_reference_number(self, api_client):
+        self.login(self.regular_user, api_client)
+        api_client.open(self.leaf_repofolder)
+        self.assertEqual(200, api_client.status_code)
+        self.assertEqual(u'Client1 1.1', api_client.contents.get(u'reference_number'))
+
+    @restapi
+    def test_repofolder_serialization_contains_is_leafnode(self, api_client):
+        self.login(self.regular_user, api_client)
+        api_client.open(self.leaf_repofolder)
+        self.assertEqual(200, api_client.status_code)
+        self.assertEqual(True, api_client.contents.get(u'is_leafnode'))
+
+    @restapi
+    def test_repofolder_serialization_contains_relative_path(self, api_client):
+        self.login(self.regular_user, api_client)
+        api_client.open(self.leaf_repofolder)
+        self.assertEqual(200, api_client.status_code)
+        self.assertEqual(u'ordnungssystem/fuhrung/vertrage-und-vereinbarungen', api_client.contents.get(u'relative_path'))
+
+
+class TestDossierSerializer(TestApiSerializer):
+
+    @restapi
+    def test_dossier_serialization_contains_reference_number(self, api_client):
+        self.login(self.regular_user, api_client)
+        api_client.open(self.dossier)
+        self.assertEqual(200, api_client.status_code)
+        self.assertEqual(u'Client1 1.1 / 1', api_client.contents.get(u'reference_number'))
+
+    @restapi
+    def test_dossier_serialization_contains_email(self, api_client):
+        self.login(self.regular_user, api_client)
+        api_client.open(self.dossier)
+        self.assertEqual(200, api_client.status_code)
+        self.assertEqual(u'1014013300@example.org', api_client.contents.get(u'email'))
+
+    @restapi
+    def test_dossier_serialization_contains_responsible_fullname(self, api_client):
+        self.login(self.regular_user, api_client)
+        api_client.open(self.dossier)
+        self.assertEqual(200, api_client.status_code)
+        self.assertEqual(u'Ziegler Robert', api_client.contents.get(u'responsible_fullname'))
+
+    @restapi
+    def test_dossier_serialization_contains_relative_path(self, api_client):
+        self.login(self.regular_user, api_client)
+        api_client.open(self.dossier)
+        self.assertEqual(200, api_client.status_code)
         self.assertEqual(
-            browser.json.get(u'relative_path'),
-            u'ordnungssystem/fuhrung/vertrage-und-vereinbarungen')
+            u'ordnungssystem/fuhrung/vertrage-und-vereinbarungen/dossier-1',
+            api_client.contents.get(u'relative_path'),
+        )
 
 
-class TestDossierSerializer(IntegrationTestCase):
+class TestDocumentSerializer(TestApiSerializer):
 
-    def setUp(self):
-        super(TestDossierSerializer, self).setUp()
-        lang_tool = api.portal.get_tool('portal_languages')
-        lang_tool.setDefaultLanguage('de-ch')
+    @restapi
+    def test_document_serialization_contains_reference_number(self, api_client):
+        self.login(self.regular_user, api_client)
+        api_client.open(self.document)
+        self.assertEqual(200, api_client.status_code)
+        self.assertEqual(u'Client1 1.1 / 1 / 12', api_client.contents.get(u'reference_number'))
 
-    @browsing
-    def test_dossier_serialization_contains_reference_number(self, browser):
-        self.login(self.regular_user, browser)
-        browser.open(self.dossier, headers={'Accept': 'application/json'})
-        self.assertEqual(browser.status_code, 200)
-        self.assertEqual(browser.json.get(u'reference_number'), u'Client1 1.1 / 1')
+    @restapi
+    def test_document_serialization_contains_bumblebee_checksum(self, api_client):
+        self.login(self.regular_user, api_client)
+        api_client.open(self.document)
+        self.assertEqual(200, api_client.status_code)
+        self.assertEqual(DOCX_CHECKSUM, api_client.contents.get(u'bumblebee_checksum'))
 
-    @browsing
-    def test_dossier_serialization_contains_email(self, browser):
-        self.login(self.regular_user, browser)
-        browser.open(self.dossier, headers={'Accept': 'application/json'})
-        self.assertEqual(browser.status_code, 200)
-        self.assertEqual(browser.json.get(u'email'), u'1014013300@example.org')
-
-    @browsing
-    def test_dossier_serialization_contains_responsible_fullname(self, browser):
-        self.login(self.regular_user, browser)
-        browser.open(self.dossier, headers={'Accept': 'application/json'})
-        self.assertEqual(browser.status_code, 200)
+    @restapi
+    def test_document_serialization_contains_relative_path(self, api_client):
+        self.login(self.regular_user, api_client)
+        api_client.open(self.document)
+        self.assertEqual(200, api_client.status_code)
         self.assertEqual(
-            browser.json.get(u'responsible_fullname'), u'Ziegler Robert')
+            u'ordnungssystem/fuhrung/vertrage-und-vereinbarungen/dossier-1/{}'.format(self.document.getId()),
+            api_client.contents.get(u'relative_path'),
+        )
 
-    @browsing
-    def test_dossier_serialization_contains_relative_path(self, browser):
-        self.login(self.regular_user, browser)
-        browser.open(self.dossier, headers={'Accept': 'application/json'})
-        self.assertEqual(browser.status_code, 200)
-        self.assertEqual(
-            browser.json.get(u'relative_path'),
-            u'ordnungssystem/fuhrung/vertrage-und-vereinbarungen/dossier-1')
+    @restapi
+    def test_mail_serialization_contains_reference_number(self, api_client):
+        self.login(self.regular_user, api_client)
+        api_client.open(self.mail_eml)
+        self.assertEqual(200, api_client.status_code)
+        self.assertEqual(u'Client1 1.1 / 1 / 24', api_client.contents.get(u'reference_number'))
 
-
-class TestDocumentSerializer(IntegrationTestCase):
-
-    def setUp(self):
-        super(TestDocumentSerializer, self).setUp()
-        lang_tool = api.portal.get_tool('portal_languages')
-        lang_tool.setDefaultLanguage('de-ch')
-
-    @browsing
-    def test_document_serialization_contains_reference_number(self, browser):
-        self.login(self.regular_user, browser)
-        browser.open(self.document, headers={'Accept': 'application/json'})
-        self.assertEqual(browser.status_code, 200)
-        self.assertEqual(browser.json.get(u'reference_number'), u'Client1 1.1 / 1 / 12')
-
-    @browsing
-    def test_document_serialization_contains_bumblebee_checksum(self, browser):
-        self.login(self.regular_user, browser)
-        browser.open(self.document, headers={'Accept': 'application/json'})
-        self.assertEqual(browser.status_code, 200)
-        self.assertEqual(browser.json.get(u'bumblebee_checksum'), DOCX_CHECKSUM)
-
-    @browsing
-    def test_document_serialization_contains_relative_path(self, browser):
-        self.login(self.regular_user, browser)
-        browser.open(self.document, headers={'Accept': 'application/json'})
-        self.assertEqual(browser.status_code, 200)
-        self.assertEqual(
-            browser.json.get(u'relative_path'),
-            u'ordnungssystem/fuhrung/vertrage-und-vereinbarungen/dossier-1/{}'.format(
-                self.document.getId()))
-
-    @browsing
-    def test_mail_serialization_contains_reference_number(self, browser):
-        self.login(self.regular_user, browser)
-        browser.open(self.mail_eml, headers={'Accept': 'application/json'})
-        self.assertEqual(browser.status_code, 200)
-        self.assertEqual(browser.json.get(u'reference_number'), u'Client1 1.1 / 1 / 24')
-
-    @browsing
-    def test_mail_serialization_contains_bumblebee_checksum(self, browser):
-        self.login(self.regular_user, browser)
-        browser.open(self.mail_eml, headers={'Accept': 'application/json'})
-        self.assertEqual(browser.status_code, 200)
+    @restapi
+    def test_mail_serialization_contains_bumblebee_checksum(self, api_client):
+        self.login(self.regular_user, api_client)
+        api_client.open(self.mail_eml)
+        self.assertEqual(200, api_client.status_code)
 
         checksum = IBumblebeeDocument(self.mail_eml).get_checksum()
-        self.assertEqual(browser.json.get(u'bumblebee_checksum'), checksum)
+        self.assertEqual(checksum, api_client.contents.get(u'bumblebee_checksum'))
 
-    @browsing
-    def test_mail_serialization_contains_relative_path(self, browser):
-        self.login(self.regular_user, browser)
-        browser.open(self.mail_eml, headers={'Accept': 'application/json'})
-        self.assertEqual(browser.status_code, 200)
+    @restapi
+    def test_mail_serialization_contains_relative_path(self, api_client):
+        self.login(self.regular_user, api_client)
+        api_client.open(self.mail_eml)
+        self.assertEqual(200, api_client.status_code)
         self.assertEqual(
-            browser.json.get(u'relative_path'),
-            u'ordnungssystem/fuhrung/vertrage-und-vereinbarungen/dossier-1/{}'.format(
-                self.mail_eml.getId()))
+            u'ordnungssystem/fuhrung/vertrage-und-vereinbarungen/dossier-1/{}'.format(self.mail_eml.getId()),
+            api_client.contents.get(u'relative_path'),
+        )
