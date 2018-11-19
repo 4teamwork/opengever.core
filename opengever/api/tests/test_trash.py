21032c83dcf9d6b901295848188a19ee57dd2bc3
Joni Orponen <j.orponen@4teamwork.ch>
2018-11-19 18:56:08 +0100

    Port opengever.api trash tests to use the @restapi decorator.

--- opengever/api/tests/test_trash.py
+++ opengever/api/tests/test_trash.py
@@ -1,4 +1,4 @@
-from ftw.testbrowser import browsing
+from ftw.testbrowser import restapi
 from opengever.testing import IntegrationTestCase
 from opengever.trash.trash import ITrashable
 from opengever.trash.trash import ITrashed
@@ -6,74 +6,56 @@ from opengever.trash.trash import ITrashed
 
 class TestTrashAPI(IntegrationTestCase):
 
-    def setUp(self):
-        super(TestTrashAPI, self).setUp()
-
-    @browsing
-    def test_trash_document(self, browser):
-        self.login(self.regular_user, browser)
-        browser.open(self.document.absolute_url() + '/@trash',
-                     method='POST', headers={'Accept': 'application/json'})
-        self.assertEqual(204, browser.status_code)
+    @restapi
+    def test_trash_document(self, api_client):
+        self.login(self.regular_user, api_client)
+        api_client.open(self.document, endpoint='@trash', method='POST')
+        self.assertEqual(204, api_client.status_code)
         self.assertTrue(ITrashed.providedBy(self.document))
 
-    @browsing
-    def test_trash_trashed_document_gives_bad_request(self, browser):
-        self.login(self.regular_user, browser)
-        browser.open(self.document.absolute_url() + '/@trash',
-                     method='POST', headers={'Accept': 'application/json'})
+    @restapi
+    def test_trash_trashed_document_gives_bad_request(self, api_client):
+        self.login(self.regular_user, api_client)
+        api_client.open(self.document, endpoint='@trash', method='POST')
 
-        with browser.expect_http_error(code=400, reason='Bad Request'):
-            browser.open(self.document.absolute_url() + '/@trash',
-                         method='POST', headers={'Accept': 'application/json'})
-        self.assertEqual(
-            browser.json[u'error'][u'message'], u'Already trashed')
+        with api_client.expect_http_error(code=400, reason='Bad Request'):
+            api_client.open(self.document, endpoint='@trash', method='POST')
 
-    @browsing
-    def test_trash_checked_out_document_gives_bad_request(self, browser):
-        self.login(self.regular_user, browser)
-        browser.open(self.document.absolute_url() + '/@checkout',
-                     method='POST', headers={'Accept': 'application/json'})
+        self.assertEqual(u'Already trashed', api_client.contents[u'error'][u'message'])
 
-        with browser.expect_http_error(code=400, reason='Bad Request'):
-            browser.open(self.document.absolute_url() + '/@trash',
-                         method='POST', headers={'Accept': 'application/json'})
-        self.assertEqual(
-            browser.json[u'error'][u'message'],
-            u'Can not trash a checked-out document')
+    @restapi
+    def test_trash_checked_out_document_gives_bad_request(self, api_client):
+        self.login(self.regular_user, api_client)
+        api_client.open(self.document, endpoint='@checkout', method='POST')
 
-    @browsing
-    def test_trash_document_without_permission_gives_401(self, browser):
-        self.login(self.regular_user, browser)
+        with api_client.expect_http_error(code=400, reason='Bad Request'):
+            api_client.open(self.document, endpoint='@trash', method='POST')
 
-        self.document.aq_parent.manage_permission(
-            'opengever.trash: Trash content', roles=[], acquire=0)
-        with browser.expect_http_error(code=401, reason='Unauthorized'):
-            browser.open(self.document.absolute_url() + '/@trash',
-                         method='POST', headers={'Accept': 'application/json'})
-        self.assertEqual(
-            browser.json[u'message'],
-            u'You are not authorized to access this resource.')
+        self.assertEqual(u'Can not trash a checked-out document', api_client.contents[u'error'][u'message'])
 
-    @browsing
-    def test_untrash_document(self, browser):
-        self.login(self.regular_user, browser)
+    @restapi
+    def test_trash_document_without_permission_gives_401(self, api_client):
+        self.login(self.regular_user, api_client)
+        self.document.aq_parent.manage_permission('opengever.trash: Trash content', roles=[], acquire=0)
+        with api_client.expect_http_error(code=401, reason='Unauthorized'):
+            api_client.open(self.document, endpoint='@trash', method='POST')
+
+        self.assertEqual(u'You are not authorized to access this resource.', api_client.contents[u'message'])
+
+    @restapi
+    def test_untrash_document(self, api_client):
+        self.login(self.regular_user, api_client)
         trasher = ITrashable(self.document)
         trasher.trash()
-        browser.open(self.document.absolute_url() + '/@untrash',
-                     method='POST', headers={'Accept': 'application/json'})
-        self.assertEqual(204, browser.status_code)
+        api_client.open(self.document, endpoint='@untrash', method='POST')
+        self.assertEqual(204, api_client.status_code)
         self.assertFalse(ITrashed.providedBy(self.document))
 
-    @browsing
-    def test_untrash_document_without_permission_gives_401(self, browser):
-        self.login(self.regular_user, browser)
+    @restapi
+    def test_untrash_document_without_permission_gives_401(self, api_client):
+        self.login(self.regular_user, api_client)
+        self.document.aq_parent.manage_permission('opengever.trash: Untrash content', roles=[], acquire=0)
+        with api_client.expect_http_error(code=401, reason='Unauthorized'):
+            api_client.open(self.document, endpoint='@untrash', method='POST')
 
-        self.document.aq_parent.manage_permission(
-            'opengever.trash: Untrash content', roles=[], acquire=0)
-        with browser.expect_http_error(code=401, reason='Unauthorized'):
-            browser.open(self.document.absolute_url() + '/@untrash',
-                         method='POST', headers={'Accept': 'application/json'})
-        self.assertEqual(
-            browser.json[u'message'],
-            u'You are not authorized to access this resource.')
+        self.assertEqual(u'You are not authorized to access this resource.', api_client.contents[u'message'])
