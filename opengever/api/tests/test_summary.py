a8ae4455a4185a034f85603d2846a809814ed3e2
Joni Orponen <j.orponen@4teamwork.ch>
2018-11-19 18:50:39 +0100

    Port opengever.api summary tests to use the @restapi decorator.

--- opengever/api/tests/test_summary.py
+++ opengever/api/tests/test_summary.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-from ftw.testbrowser import browsing
+from ftw.testbrowser import restapi
 from opengever.api.testing import RelativeSession
 from opengever.testing import IntegrationTestCase
 from plone import api
@@ -20,126 +19,77 @@ class TestGeverJSONSummarySerializer(IntegrationTestCase):
         lang_tool.setDefaultLanguage('de-ch')
         lang_tool.supported_langs = ['fr-ch', 'de-ch']
 
-    @browsing
-    def test_portal_type_is_included(self, browser):
-        self.login(self.regular_user, browser)
-        browser.open(
-            self.repository_root.absolute_url(),
-            headers={'Accept': 'application/json'})
-        repofolder_summary = browser.json['items'][0]
+    @restapi
+    def test_portal_type_is_included(self, api_client):
+        self.login(self.regular_user, api_client)
+        api_client.open(self.repository_root)
+        repofolder_summary = api_client.contents['items'][0]
+        self.assertDictContainsSubset({u'@type': u'opengever.repository.repositoryfolder'}, repofolder_summary)
 
+        api_client.open(self.leaf_repofolder)
+        dossier_summary = api_client.contents['items'][0]
+        self.assertDictContainsSubset({u'@type': u'opengever.dossier.businesscasedossier'}, dossier_summary)
+
+    @restapi
+    def test_translated_title_contained_in_summary_if_obj_translated(self, api_client):
+        self.login(self.regular_user, api_client)
+
+        api_client.open(self.repository_root, headers={'Accept': 'application/json', 'Accept-Language': 'de-ch'})
+        self.assertDictContainsSubset({u'title': u'1. F\xfchrung'}, api_client.contents['items'][0])
+
+        api_client.open(self.repository_root, headers={'Accept': 'application/json', 'Accept-Language': 'fr-ch'})
+        self.assertDictContainsSubset({u'title': u'1. Direction'}, api_client.contents['items'][0])
+
+    @restapi
+    def test_translated_titles_default_to_german(self, api_client):
+        self.login(self.regular_user, api_client)
+        api_client.open(self.repository_root)
+        self.assertDictContainsSubset({u'title': u'1. F\xfchrung'}, api_client.contents['items'][0])
+
+    @restapi
+    def test_regular_title_in_summary_if_obj_not_translated(self, api_client):
+        self.login(self.regular_user, api_client)
+        api_client.open(self.leaf_repofolder)
         self.assertDictContainsSubset(
-            {u'@type': u'opengever.repository.repositoryfolder'},
-            repofolder_summary)
+            {u'title': u'Vertr\xe4ge mit der kantonalen Finanzverwaltung'},
+            api_client.contents['items'][0],
+        )
 
-        browser.open(
-            self.leaf_repofolder.absolute_url(),
-            headers={'Accept': 'application/json'})
-        dossier_summary = browser.json['items'][0]
+    @restapi
+    def test_summary_with_custom_field_list(self, api_client):
+        self.login(self.regular_user, api_client)
+        api_client.open(self.dossier, endpoint='?items.fl=filesize,filename,modified,created,mimetype,creator,changed')
+        expected_summary = {
+            u'@id': u'http://nohost/plone/ordnungssystem/fuhrung/vertrage-und-vereinbarungen/dossier-1/document-12',
+            u'changed': u'2016-08-31T14:07:33+00:00',
+            u'created': u'2016-08-31T14:07:33+00:00',
+            u'creator': u'robert.ziegler',
+            u'filename': u'Vertraegsentwurf.docx',
+            u'filesize': 27413,
+            u'mimetype': u'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+            u'modified': u'2016-08-31T14:07:33+00:00',
+        }
+        self.assertEqual(expected_summary, api_client.contents['items'][0])
 
-        self.assertDictContainsSubset(
-            {u'@type': u'opengever.dossier.businesscasedossier'},
-            dossier_summary)
+    @restapi
+    def test_summary_with_reference_number(self, api_client):
+        self.login(self.regular_user, api_client)
+        api_client.open(self.dossier, endpoint='?items.fl=reference_number')
 
-    @browsing
-    def test_translated_title_contained_in_summary_if_obj_translated(
-            self, browser):
-        self.login(self.regular_user, browser)
-        browser.open(
-            self.repository_root.absolute_url(),
-            headers={'Accept': 'application/json', 'Accept-Language': 'de-ch'})
-        repofolder_summary = browser.json['items'][0]
+        expected_summary = {
+            u'@id': u'http://nohost/plone/ordnungssystem/fuhrung/vertrage-und-vereinbarungen/dossier-1/document-12',
+            u'reference_number': u'Client1 1.1 / 1 / 12',
+        }
+        self.assertEqual(expected_summary, api_client.contents['items'][0])
 
-        self.assertDictContainsSubset(
-            {u'title': u'1. Führung'},
-            repofolder_summary)
-
-        browser.open(
-            self.repository_root.absolute_url(),
-            headers={'Accept': 'application/json', 'Accept-Language': 'fr-ch'})
-        repofolder_summary = browser.json['items'][0]
-
-        self.assertDictContainsSubset(
-            {u'title': u'1. Direction'},
-            repofolder_summary)
-
-    @browsing
-    def test_translated_titles_default_to_german(self, browser):
-        self.login(self.regular_user, browser)
-        browser.open(
-            self.repository_root.absolute_url(),
-            headers={'Accept': 'application/json'})
-        repofolder_summary = browser.json['items'][0]
-
-        self.assertDictContainsSubset(
-            {u'title': u'1. Führung'},
-            repofolder_summary)
-
-    @browsing
-    def test_regular_title_in_summary_if_obj_not_translated(self, browser):
-        self.login(self.regular_user, browser)
-        browser.open(
-            self.leaf_repofolder.absolute_url(),
-            headers={'Accept': 'application/json'})
-        dossier_summary = browser.json['items'][0]
-
-        self.assertDictContainsSubset(
-            {u'title': u'Verträge mit der kantonalen Finanzverwaltung'},
-            dossier_summary)
-
-    @browsing
-    def test_summary_with_custom_field_list(self, browser):
-        self.login(self.regular_user, browser)
-        browser.open(
-            self.dossier.absolute_url() +
-            '?items.fl=filesize,filename,modified,created,mimetype,creator,changed',
-            headers={'Accept': 'application/json'})
-
-        summary = browser.json['items'][0]
-        self.assertEqual(
-            {
-                u'@id': u'http://nohost/plone/ordnungssystem/fuhrung/vertrage-'
-                u'und-vereinbarungen/dossier-1/document-12',
-                u'changed': u'2016-08-31T14:07:33+00:00',
-                u'created': u'2016-08-31T14:07:33+00:00',
-                u'creator': u'robert.ziegler',
-                u'filename': u'Vertraegsentwurf.docx',
-                u'filesize': 27413,
-                u'mimetype': u'application/vnd.openxmlformats-officedocument.'
-                u'wordprocessingml.document',
-                u'modified': u'2016-08-31T14:07:33+00:00'},
-            summary)
-
-    @browsing
-    def test_summary_with_reference_number(self, browser):
-        self.login(self.regular_user, browser)
-        browser.open(
-            self.dossier.absolute_url() +
-            '?items.fl=reference_number',
-            headers={'Accept': 'application/json'})
-
-        summary = browser.json['items'][0]
-        self.assertEqual(
-            summary,
-            {
-                u'@id': u'http://nohost/plone/ordnungssystem/fuhrung/vertrage-'
-                        u'und-vereinbarungen/dossier-1/document-12',
-                u'reference_number': u'Client1 1.1 / 1 / 12',
-            })
-
-    @browsing
-    def test_summary_with_filename_on_dossiers_containing_tasks(self, browser):
-        self.login(self.regular_user, browser)
-
+    @restapi
+    def test_summary_with_filename_on_dossiers_containing_tasks(self, api_client):
+        self.login(self.regular_user, api_client)
         self.task.text = u'Sample description'
-
-        browser.open(self.dossier.absolute_url() +
-                     '?items.fl=filename,filesize',
-                     headers={'Accept': 'application/json'})
-
-        summary = browser.json['items'][10]
-        self.assertEqual(
-            {u'@id': u'http://nohost/plone/ordnungssystem/fuhrung/'
-             'vertrage-und-vereinbarungen/dossier-1/task-1',
-             u'filename': None,
-             u'filesize': 0}, summary)
+        api_client.open(self.dossier, endpoint='?items.fl=filename,filesize')
+        expected_summary = {
+            u'@id': u'http://nohost/plone/ordnungssystem/fuhrung/vertrage-und-vereinbarungen/dossier-1/task-1',
+            u'filename': None,
+            u'filesize': 0,
+        }
+        self.assertEqual(expected_summary, api_client.contents['items'][10])
