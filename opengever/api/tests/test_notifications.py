1b64f305d90e7e73a9ec84b3de5ce2cf20671dd9
Joni Orponen <j.orponen@4teamwork.ch>
2018-11-19 16:55:25 +0100

    Port opengever.api notification tests to use the @restapi decorator.

--- opengever/api/tests/test_notifications.py
+++ opengever/api/tests/test_notifications.py
@@ -1,11 +1,10 @@
 from datetime import datetime
-from ftw.testbrowser import browsing
+from ftw.testbrowser import restapi
 from ftw.testing import freeze
 from opengever.activity import notification_center
 from opengever.activity.model import Notification
 from opengever.task.activities import TaskAddedActivity
 from opengever.testing import IntegrationTestCase
-import json
 import pytz
 
 
@@ -13,13 +12,11 @@ class TestNotificationsGet(IntegrationTestCase):
 
     features = ('activity', )
 
-    @browsing
-    def test_list_all_notifications_for_the_given_userid(self, browser):
-        self.login(self.administrator, browser=browser)
-
+    @restapi
+    def test_list_all_notifications_for_the_given_userid(self, api_client):
+        self.login(self.administrator, api_client)
         center = notification_center()
-
-        self.assertEqual(0,  Notification.query.count())
+        self.assertEqual(0, Notification.query.count())
 
         with freeze(datetime(2017, 10, 16, 0, 0, tzinfo=pytz.utc)):
             TaskAddedActivity(self.task, self.request, self.task.__parent__).record()
@@ -31,202 +28,170 @@ class TestNotificationsGet(IntegrationTestCase):
             TaskAddedActivity(self.task, self.request, self.task.__parent__).record()
 
         self.assertEqual(2, len(center.get_watchers(self.task)))
-
         # two notifications for each watcher, the responsible and the issuer
-        self.assertEqual(4,  Notification.query.count())
+        self.assertEqual(4, Notification.query.count())
 
-        self.login(self.regular_user, browser=browser)
+        self.login(self.regular_user, api_client)
+        endpoint = '@notifications/{}'.format(self.regular_user.getId())
+        api_client.open(endpoint=endpoint)
+        self.assertEqual(200, api_client.status_code)
 
-        url = '{}/@notifications/{}'.format(self.portal.absolute_url(),
-                                            self.regular_user.getId())
-        browser.open(url, method='GET', headers={'Accept': 'application/json'})
+        expected_items = [
+            {
+                u'@id': u'http://nohost/plone/@notifications/kathi.barfuss/3',
+                u'created': u'2018-10-16T00:00:00+00:00',
+                u'label': u'Task opened',
+                u'link': u'http://nohost/plone/@@resolve_notification?notification_id=3',
+                u'notification_id': 3,
+                u'read': False,
+                u'summary': u'New task opened by Ziegler Robert',
+                u'title': u'Vertragsentwurf \xdcberpr\xfcfen',
+            },
+            {
+                u'@id': u'http://nohost/plone/@notifications/kathi.barfuss/1',
+                u'created': u'2017-10-16T00:00:00+00:00',
+                u'label': u'Task opened',
+                u'link': u'http://nohost/plone/@@resolve_notification?notification_id=1',
+                u'notification_id': 1,
+                u'read': True,
+                u'summary': u'New task opened by Ziegler Robert',
+                u'title': u'Vertragsentwurf \xdcberpr\xfcfen',
+            },
+        ]
+        self.assertEqual(expected_items, api_client.contents.get('items'))
 
-        self.assertEqual(200, browser.status_code)
-
-        self.assertEquals(
-            [{u'@id': u'http://nohost/plone/@notifications/kathi.barfuss/3',
-              u'created': u'2018-10-16T00:00:00+00:00',
-              u'label': u'Task opened',
-              u'link': u'http://nohost/plone/@@resolve_notification?notification_id=3',
-              u'notification_id': 3,
-              u'read': False,
-              u'summary': u'New task opened by Ziegler Robert',
-              u'title': u'Vertragsentwurf \xdcberpr\xfcfen'},
-             {u'@id': u'http://nohost/plone/@notifications/kathi.barfuss/1',
-              u'created': u'2017-10-16T00:00:00+00:00',
-              u'label': u'Task opened',
-              u'link': u'http://nohost/plone/@@resolve_notification?notification_id=1',
-              u'notification_id': 1,
-              u'read': True,
-              u'summary': u'New task opened by Ziegler Robert',
-              u'title': u'Vertragsentwurf \xdcberpr\xfcfen'}],
-            browser.json.get('items'))
-
-    @browsing
-    def test_batch_notifications(self, browser):
-        self.login(self.administrator, browser=browser)
-
-        center = notification_center()
-
-        self.assertEqual(0,  Notification.query.count())
+    @restapi
+    def test_batch_notifications(self, api_client):
+        self.login(self.administrator, api_client)
+        self.assertEqual(0, Notification.query.count())
 
         with freeze(datetime(2017, 10, 16, 0, 0, tzinfo=pytz.utc)):
-            for i in range(5):
+            for _i in range(5):
                 TaskAddedActivity(self.task, self.request, self.task.__parent__).record()
 
-        self.login(self.regular_user, browser=browser)
-
+        self.login(self.regular_user, api_client)
         batch_size = 2
-        url = '{}/@notifications/{}?b_size={}'.format(
-            self.portal.absolute_url(),
-            self.regular_user.getId(),
-            batch_size)
+        endpoint = '@notifications/{}?b_size={}'.format(self.regular_user.getId(), batch_size)
+        api_client.open(endpoint=endpoint)
 
-        browser.open(url, method='GET', headers={'Accept': 'application/json'})
+        self.assertEqual(200, api_client.status_code)
+        self.assertEqual(5, api_client.contents.get('items_total'))
+        self.assertEqual(2, len(api_client.contents.get('items')))
 
-        self.assertEqual(200, browser.status_code)
-
-        self.assertEquals(5, browser.json.get('items_total'))
-        self.assertEquals(2, len(browser.json.get('items')))
-
-        url = browser.json.get('batching').get('last')
-        browser.open(url, method='GET', headers={'Accept': 'application/json'})
+        url = api_client.contents.get('batching').get('last')
+        api_client.open(url)
 
         # 5 notifications with a batchsize of 2 will display only 1 notification
         # on the last batch
-        self.assertEquals(1, len(browser.json.get('items')))
+        self.assertEqual(1, len(api_client.contents.get('items')))
 
-    @browsing
-    def test_returns_serialized_notifications_for_the_given_userid_and_notification_id(self, browser):
-        self.login(self.dossier_responsible, browser=browser)
-
-        self.assertEqual(0,  Notification.query.count())
+    @restapi
+    def test_returns_serialized_notifications_for_the_given_userid_and_notification_id(self, api_client):
+        self.login(self.dossier_responsible, api_client)
+        self.assertEqual(0, Notification.query.count())
 
         with freeze(datetime(2017, 10, 16, 0, 0, tzinfo=pytz.utc)):
             TaskAddedActivity(self.task, self.request, self.task.__parent__).record()
 
-        url = '{}/@notifications/{}/1'.format(self.portal.absolute_url(),
-                                              self.regular_user.getId())
+        endpoint = '@notifications/{}/1'.format(self.regular_user.getId())
 
-        self.login(self.regular_user, browser=browser)
-        browser.open(url, method='GET', headers={'Accept': 'application/json'})
+        self.login(self.regular_user, api_client)
+        api_client.open(endpoint=endpoint)
 
-        self.assertEqual(200, browser.status_code)
+        self.assertEqual(200, api_client.status_code)
+        expected_notification = {
+            u'@id': u'http://nohost/plone/@notifications/kathi.barfuss/1',
+            u'created': u'2017-10-16T00:00:00+00:00',
+            u'label': u'Task opened',
+            u'link': u'http://nohost/plone/@@resolve_notification?notification_id=1',
+            u'notification_id': 1,
+            u'read': False,
+            u'summary': u'New task opened by Ziegler Robert',
+            u'title': u'Vertragsentwurf \xdcberpr\xfcfen',
+        }
+        self.assertEqual(expected_notification, api_client.contents)
 
-        self.assertEquals(
-            {u'@id': u'http://nohost/plone/@notifications/kathi.barfuss/1',
-             u'created': u'2017-10-16T00:00:00+00:00',
-             u'label': u'Task opened',
-             u'link': u'http://nohost/plone/@@resolve_notification?notification_id=1',
-             u'notification_id': 1,
-             u'read': False,
-             u'summary': u'New task opened by Ziegler Robert',
-             u'title': u'Vertragsentwurf \xdcberpr\xfcfen'},
-            browser.json)
+    @restapi
+    def test_raises_bad_request_when_userid_is_missing(self, api_client):
+        self.login(self.regular_user, api_client)
 
-    @browsing
-    def test_raises_bad_request_when_userid_is_missing(self, browser):
-        self.login(self.regular_user, browser=browser)
+        with api_client.expect_http_error(400):
+            api_client.open(endpoint='@notifications')
 
-        with browser.expect_http_error(400):
-            url = '{}/@notifications'.format(self.portal.absolute_url())
-            browser.open(url, method='GET',
-                         headers={'Accept': 'application/json'})
+        expected_error = {
+            "message": "Must supply user ID as URL and optional the notification id as path parameter.",
+            "type": "BadRequest",
+        }
+        self.assertEqual(expected_error, api_client.contents)
 
-        self.assertEqual(
-            {"message": "Must supply user ID as URL and optional the "
-             "notification id as path parameter.",
-             "type": "BadRequest"},
-            browser.json)
+    @restapi
+    def test_raises_not_found_when_accessing_not_exisiting_notification(self, api_client):
+        self.login(self.dossier_responsible, api_client)
+        endpoint = '@notifications/{}/1'.format(self.dossier_responsible.getId())
 
-    @browsing
-    def test_raises_not_found_when_accessing_not_exisiting_notification(self, browser):
-        self.login(self.dossier_responsible, browser=browser)
+        with api_client.expect_http_error(404):
+            api_client.open(endpoint=endpoint)
 
-        url = '{}/@notifications/{}/1'.format(self.portal.absolute_url(),
-                                              self.dossier_responsible.getId())
-
-        with browser.expect_http_error(404):
-            browser.open(url, method='GET', headers={'Accept': 'application/json'})
-
-    @browsing
-    def test_raises_unauthorized_when_accessing_notification_of_other_user(self, browser):
-        self.login(self.dossier_responsible, browser=browser)
+    @restapi
+    def test_raises_unauthorized_when_accessing_notification_of_other_user(self, api_client):
+        self.login(self.dossier_responsible, api_client)
 
         with freeze(datetime(2017, 10, 16, 0, 0, tzinfo=pytz.utc)):
             TaskAddedActivity(self.task, self.request, self.task.__parent__).record()
 
         # Different username in path
-        with browser.expect_http_error(401):
-            url = '{}/@notifications/{}/1'.format(
-                self.portal.absolute_url(), self.regular_user.getId())
-            browser.open(url, method='GET',
-                         headers={'Accept': 'application/json'})
+        with api_client.expect_http_error(401):
+            endpoint = '@notifications/{}/1'.format(self.regular_user.getId())
+            api_client.open(endpoint=endpoint)
 
         # Own username but foreign notification-id
-        with browser.expect_http_error(401):
-            url = '{}/@notifications/{}/1'.format(
-                self.portal.absolute_url(), self.dossier_responsible.getId())
-            browser.open(url, method='GET',
-                         headers={'Accept': 'application/json'})
+        with api_client.expect_http_error(401):
+            endpoint = '@notifications/{}/1'.format(self.dossier_responsible.getId())
+            api_client.open(endpoint=endpoint)
 
 
 class TestNotificationsPatch(IntegrationTestCase):
 
     features = ('activity', )
 
-    @browsing
-    def test_mark_notification_as_read(self, browser):
-        self.login(self.dossier_responsible, browser=browser)
-
+    @restapi
+    def test_mark_notification_as_read(self, api_client):
+        self.login(self.dossier_responsible, api_client)
         TaskAddedActivity(self.task, self.request, self.task.__parent__).record()
-
-        url = '{}/@notifications/{}/{}'.format(self.portal.absolute_url(),
-                                               self.regular_user.getId(), 1)
-
-        self.login(self.regular_user, browser=browser)
-
-        browser.open(url, method='GET', headers={'Accept': 'application/json'})
+        endpoint = '@notifications/{}/1'.format(self.regular_user.getId())
+        self.login(self.regular_user, api_client)
+        api_client.open(endpoint=endpoint)
 
         self.assertFalse(Notification.query.first().is_read)
 
-        data = json.dumps({'read': True})
-        browser.open(url, data=data, method='PATCH',
-                     headers={'Accept': 'application/json'})
+        data = {'read': True}
+        api_client.open(endpoint=endpoint, data=data, method='PATCH')
 
-        self.assertEqual(204, browser.status_code)
+        self.assertEqual(204, api_client.status_code)
         self.assertTrue(Notification.query.first().is_read)
 
-    @browsing
-    def test_raises_not_found_when_accessing_not_exisiting_notification(self, browser):
-        self.login(self.dossier_responsible, browser=browser)
+    @restapi
+    def test_raises_not_found_when_accessing_not_exisiting_notification(self, api_client):
+        self.login(self.dossier_responsible, api_client)
+        self.assertEqual(0, Notification.query.count())
 
-        url = '{}/@notifications/{}/1'.format(self.portal.absolute_url(),
-                                              self.dossier_responsible.getId())
+        with api_client.expect_http_error(404):
+            endpoint = '@notifications/{}/1'.format(self.dossier_responsible.getId())
+            api_client.open(endpoint=endpoint, data={}, method='PATCH')
 
-        self.assertEqual(0,  Notification.query.count())
-
-        with browser.expect_http_error(404):
-            browser.open(url, method='PATCH', data=json.dumps({}),
-                         headers={'Accept': 'application/json'})
-
-    @browsing
-    def test_raises_unauthorized_when_accessing_notification_of_other_user(self, browser):
-        self.login(self.dossier_responsible, browser=browser)
+    @restapi
+    def test_raises_unauthorized_when_accessing_notification_of_other_user(self, api_client):
+        self.login(self.dossier_responsible, api_client)
 
         with freeze(datetime(2017, 10, 16, 0, 0, tzinfo=pytz.utc)):
             TaskAddedActivity(self.task, self.request, self.task.__parent__).record()
 
         # Different username in path
-        with browser.expect_http_error(401):
-            url = '{}/@notifications/{}/1'.format(
-                self.portal.absolute_url(), self.regular_user.getId())
-            browser.open(url, data=json.dumps({}), method='PATCH',
-                         headers={'Accept': 'application/json'})
+        with api_client.expect_http_error(401):
+            endpoint = '@notifications/{}/1'.format(self.regular_user.getId())
+            api_client.open(endpoint=endpoint, data={}, method='PATCH')
 
         # Own username but foreign notification-id
-        with browser.expect_http_error(401):
-            url = '{}/@notifications/{}/1'.format(
-                self.portal.absolute_url(), self.dossier_responsible.getId())
-            browser.open(url, data=json.dumps({}), method='PATCH',
-                         headers={'Accept': 'application/json'})
+        with api_client.expect_http_error(401):
+            endpoint = '@notifications/{}/1'.format(self.dossier_responsible.getId())
+            api_client.open(endpoint=endpoint, data={}, method='PATCH')
