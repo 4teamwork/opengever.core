00a696022d1539ee94de99add0f3a3a735a21869
Joni Orponen <j.orponen@4teamwork.ch>
2018-11-19 17:28:45 +0100

    Port opengever.api repository favorites tests to use the @restapi decorator.

--- opengever/api/tests/test_repository_favorites.py
+++ opengever/api/tests/test_repository_favorites.py
@@ -1,17 +1,23 @@
 from ftw.builder import Builder
 from ftw.builder import create
-from ftw.testbrowser import browsing
+from ftw.testbrowser import restapi
 from opengever.base.model.favorite import Favorite
 from opengever.testing import IntegrationTestCase
 from plone.uuid.interfaces import IUUID
-import json
+
+
+class TestRepositoryFavorites(IntegrationTestCase):
+
+    @staticmethod
+    def uuid(obj):
+        return IUUID(obj, None)
 
 
 class TestRepositoryFavoritesGet(IntegrationTestCase):
 
-    @browsing
-    def test_returns_list_of_uids(self, browser):
-        self.login(self.regular_user, browser=browser)
+    @restapi
+    def test_returns_list_of_uids(self, api_client):
+        self.login(self.regular_user, api_client)
 
         favorite1 = create(Builder('favorite')
                            .for_user(self.regular_user)
@@ -21,17 +27,15 @@ class TestRepositoryFavoritesGet(IntegrationTestCase):
                            .for_user(self.regular_user)
                            .for_object(self.leaf_repofolder))
 
-        url = '{}/@repository-favorites/{}'.format(
-            self.portal.absolute_url(), self.regular_user.getId())
-        browser.open(url, method='GET', headers={'Accept': 'application/json'})
+        endpoint = '@repository-favorites/{}'.format(self.regular_user.getId())
+        api_client.open(endpoint=endpoint)
 
-        self.assertEqual(200, browser.status_code)
-        self.assertEqual([favorite1.plone_uid, favorite2.plone_uid],
-                         browser.json)
+        self.assertEqual(200, api_client.status_code)
+        self.assertEqual([favorite1.plone_uid, favorite2.plone_uid], api_client.contents)
 
-    @browsing
-    def test_list_only_favorites_for_given_user(self, browser):
-        self.login(self.regular_user, browser=browser)
+    @restapi
+    def test_list_only_favorites_for_given_user(self, api_client):
+        self.login(self.regular_user, api_client)
 
         favorite1 = create(Builder('favorite')
                            .for_user(self.regular_user)
@@ -41,16 +45,15 @@ class TestRepositoryFavoritesGet(IntegrationTestCase):
                .for_user(self.dossier_responsible)
                .for_object(self.leaf_repofolder))
 
-        url = '{}/@repository-favorites/{}'.format(
-            self.portal.absolute_url(), self.regular_user.getId())
-        browser.open(url, method='GET', headers={'Accept': 'application/json'})
+        endpoint = '@repository-favorites/{}'.format(self.regular_user.getId())
+        api_client.open(endpoint=endpoint)
 
-        self.assertEqual(200, browser.status_code)
-        self.assertEqual([favorite1.plone_uid], browser.json)
+        self.assertEqual(200, api_client.status_code)
+        self.assertEqual([favorite1.plone_uid], api_client.contents)
 
-    @browsing
-    def test_list_only_repositoryfolder_favorites(self, browser):
-        self.login(self.regular_user, browser=browser)
+    @restapi
+    def test_list_only_repositoryfolder_favorites(self, api_client):
+        self.login(self.regular_user, api_client)
 
         favorite1 = create(Builder('favorite')
                            .for_user(self.regular_user)
@@ -60,16 +63,15 @@ class TestRepositoryFavoritesGet(IntegrationTestCase):
                .for_user(self.regular_user)
                .for_object(self.dossier))
 
-        url = '{}/@repository-favorites/{}'.format(
-            self.portal.absolute_url(), self.regular_user.getId())
-        browser.open(url, method='GET', headers={'Accept': 'application/json'})
+        endpoint = '@repository-favorites/{}'.format(self.regular_user.getId())
+        api_client.open(endpoint=endpoint)
 
-        self.assertEqual(200, browser.status_code)
-        self.assertEqual([favorite1.plone_uid], browser.json)
+        self.assertEqual(200, api_client.status_code)
+        self.assertEqual([favorite1.plone_uid], api_client.contents)
 
-    @browsing
-    def test_list_only_favorites_from_current_admin_unit(self, browser):
-        self.login(self.regular_user, browser=browser)
+    @restapi
+    def test_list_only_favorites_from_current_admin_unit(self, api_client):
+        self.login(self.regular_user, api_client)
 
         favorite1 = create(Builder('favorite')
                            .for_user(self.regular_user)
@@ -80,150 +82,106 @@ class TestRepositoryFavoritesGet(IntegrationTestCase):
                .for_object(self.leaf_repofolder)
                .having(admin_unit_id='sk'))
 
-        url = '{}/@repository-favorites/{}'.format(
-            self.portal.absolute_url(), self.regular_user.getId())
-        browser.open(url, method='GET', headers={'Accept': 'application/json'})
+        endpoint = '@repository-favorites/{}'.format(self.regular_user.getId())
+        api_client.open(endpoint=endpoint)
 
-        self.assertEqual(200, browser.status_code)
-        self.assertEqual([favorite1.plone_uid], browser.json)
+        self.assertEqual(200, api_client.status_code)
+        self.assertEqual([favorite1.plone_uid], api_client.contents)
 
-    @browsing
-    def test_raises_when_userid_is_missing(self, browser):
-        self.login(self.regular_user, browser=browser)
+    @restapi
+    def test_raises_when_userid_is_missing(self, api_client):
+        self.login(self.regular_user, api_client)
 
-        with browser.expect_http_error(400):
-            url = '{}/@repository-favorites'.format(self.portal.absolute_url())
-            browser.open(url, method='GET',
-                         headers={'Accept': 'application/json'})
+        with api_client.expect_http_error(400):
+            api_client.open(endpoint='@repository-favorites')
 
-        self.assertEqual(
-            {"message": "Must supply exactly one parameter (user id)",
-             "type": "BadRequest"},
-            browser.json)
+        expected_error = {
+            "message": "Must supply exactly one parameter (user id)",
+            "type": "BadRequest",
+        }
+        self.assertEqual(expected_error, api_client.contents)
 
 
-class TestRepositoryFavoritesPost(IntegrationTestCase):
+class TestRepositoryFavoritesPost(TestRepositoryFavorites):
 
-    def uuid(self, obj):
-        return IUUID(obj, None)
-
-    @browsing
-    def test_adding_favorite(self, browser):
-        self.login(self.regular_user, browser=browser)
-
-        uuid = self.uuid(self.document)
-
-        url = '{}/@repository-favorites/{}'.format(
-            self.portal.absolute_url(), self.regular_user.getId())
-
+    @restapi
+    def test_adding_favorite(self, api_client):
+        self.login(self.regular_user, api_client)
         self.assertEqual(0, Favorite.query.count())
 
-        data = json.dumps({'uuid': uuid})
-        browser.open(url, data=data, method='POST',
-                     headers={'Accept': 'application/json',
-                              'Content-Type': 'application/json'})
+        endpoint = '@repository-favorites/{}'.format(self.regular_user.getId())
+        api_client.open(endpoint=endpoint, data={'uuid': self.uuid(self.document)})
 
-        self.assertEqual(201, browser.status_code)
+        self.assertEqual(201, api_client.status_code)
+        self.assertEqual(u'http://nohost/plone/@favorites/kathi.barfuss/1', api_client.headers.get('location'))
 
-        self.assertEqual(u'http://nohost/plone/@favorites/kathi.barfuss/1',
-                         browser.headers.get('location'))
-
-        browser.open(browser.headers.get('location'), method='GET',
-                     headers={'Accept': 'application/json',
-                              'Content-Type': 'application/json'})
-
+        api_client.open(api_client.headers.get('location'))
         self.assertEqual(1, Favorite.query.count())
 
-    @browsing
-    def test_raises_with_missing_uuid(self, browser):
-        self.login(self.regular_user, browser=browser)
+    @restapi
+    def test_raises_with_missing_uuid(self, api_client):
+        self.login(self.regular_user, api_client)
 
-        url = '{}/@repository-favorites/{}'.format(
-            self.portal.absolute_url(), self.regular_user.getId())
+        with api_client.expect_http_error(400):
+            endpoint = '@repository-favorites/{}'.format(self.regular_user.getId())
+            api_client.open(endpoint=endpoint, method='POST')
 
-        with browser.expect_http_error(400):
-            browser.open(url, method='POST', data="{}",
-                         headers={'Accept': 'application/json',
-                                  'Content-Type': 'application/json'})
+        self.assertEquals({u'message': u'Missing parameter uuid', u'type': u'BadRequest'}, api_client.contents)
 
-        self.assertEquals(
-            {u'message': u'Missing parameter uuid', u'type': u'BadRequest'},
-            browser.json)
+    @restapi
+    def test_raises_unauthorized_when_accessing_favorites_of_other_user(self, api_client):
+        self.login(self.regular_user, api_client)
 
-    @browsing
-    def test_raises_unauthorized_when_accessing_favorites_of_other_user(self, browser):
-        self.login(self.regular_user, browser=browser)
+        with api_client.expect_http_error(401):
+            endpoint = '@repository-favorites/{}'.format(self.dossier_responsible.getId())
+            api_client.open(endpoint=endpoint, data={'uuid': self.uuid(self.document)})
 
-        uuid = self.uuid(self.document)
-        url = '{}/@repository-favorites/{}'.format(
-            self.portal.absolute_url(), self.dossier_responsible.getId())
+        expected_error = {u'message': u"It's not allowed to add favorites for other users.", u'type': u'Unauthorized'}
+        self.assertEqual(expected_error, api_client.contents)
 
-        data = json.dumps({'uuid': uuid})
-        with browser.expect_http_error(401):
-            browser.open(url, data=data, method='POST',
-                         headers={'Accept': 'application/json',
-                                  'Content-Type': 'application/json'})
-
-        self.assertEqual(
-            {u'message': u"It's not allowed to add favorites for other users.",
-             u'type': u'Unauthorized'},
-            browser.json)
-
-    @browsing
-    def test_adding_already_existing_favorite_returns_409_and_existing_representation(self, browser):
-        self.login(self.administrator, browser=browser)
+    @restapi
+    def test_adding_already_existing_favorite_returns_409_and_existing_representation(self, api_client):
+        self.login(self.administrator, api_client)
 
         create(Builder('favorite')
                .for_user(self.administrator)
                .for_object(self.document))
 
-        uuid = self.uuid(self.document)
-        url = '{}/@repository-favorites/{}'.format(
-            self.portal.absolute_url(), self.administrator.getId())
-        data = json.dumps({'uuid': uuid})
+        with api_client.expect_http_error(409):
+            endpoint = '@repository-favorites/{}'.format(self.administrator.getId())
+            api_client.open(endpoint=endpoint, data={'uuid': self.uuid(self.document)})
 
-        with browser.expect_http_error(409):
-            browser.open(url, data=data, method='POST',
-                         headers={'Accept': 'application/json',
-                                  'Content-Type': 'application/json'})
-
-        self.assertEqual(
-            {u'@id': u'http://nohost/plone/@favorites/nicole.kohler/1',
-             u'admin_unit': u'Hauptmandant',
-             u'favorite_id': 1,
-             u'icon_class': u'icon-docx',
-             u'oguid': u'plone:1014073300',
-             u'position': None,
-             u'target_url': u'http://nohost/plone/resolve_oguid/plone:1014073300',
-             u'title': u'Vertr\xe4gsentwurf',
-             u'tooltip_url': u'http://nohost/plone/resolve_oguid/plone:1014073300/tooltip'},
-            browser.json)
-
+        expected_favorite = {
+            u'@id': u'http://nohost/plone/@favorites/nicole.kohler/1',
+            u'admin_unit': u'Hauptmandant',
+            u'favorite_id': 1,
+            u'icon_class': u'icon-docx',
+            u'oguid': u'plone:1014073300',
+            u'position': None,
+            u'target_url': u'http://nohost/plone/resolve_oguid/plone:1014073300',
+            u'title': u'Vertr\xe4gsentwurf',
+            u'tooltip_url': u'http://nohost/plone/resolve_oguid/plone:1014073300/tooltip',
+        }
+        self.assertEqual(expected_favorite, api_client.contents)
         self.assertEqual(1, Favorite.query.count())
 
 
-class TestFavoritesDelete(IntegrationTestCase):
+class TestFavoritesDelete(TestRepositoryFavorites):
 
-    def uuid(self, obj):
-        return IUUID(obj, None)
+    @restapi
+    def test_raises_when_id_is_missing(self, api_client):
+        self.login(self.regular_user, api_client)
 
-    @browsing
-    def test_raises_when_id_is_missing(self, browser):
-        self.login(self.regular_user, browser=browser)
+        with api_client.expect_http_error(400):
+            endpoint = '@repository-favorites/{}'.format(self.regular_user.getId())
+            api_client.open(endpoint=endpoint, method='DELETE')
 
-        with browser.expect_http_error(400):
-            url = '{}/@repository-favorites/{}'.format(
-                self.portal.absolute_url(), self.regular_user.getId())
-            browser.open(url, method='DELETE',
-                         headers={'Accept': 'application/json'})
+        expected_error = {u'message': u'Must supply exactly two parameters (user id and uuid)', u'type': u'BadRequest'}
+        self.assertEqual(expected_error, api_client.contents)
 
-        self.assertEqual(
-            {u'message': u'Must supply exactly two parameters (user id and uuid)',
-             u'type': u'BadRequest'}, browser.json)
-
-    @browsing
-    def test_raises_when_favorite_is_not_owned_by_the_given_user(self, browser):
-        self.login(self.regular_user, browser=browser)
+    @restapi
+    def test_raises_when_favorite_is_not_owned_by_the_given_user(self, api_client):
+        self.login(self.regular_user, api_client)
 
         favorite = create(Builder('favorite')
                           .for_user(self.dossier_responsible)
@@ -231,22 +189,19 @@ class TestFavoritesDelete(IntegrationTestCase):
 
         self.assertEqual(1, Favorite.query.count())
 
-        with browser.expect_http_error(404):
-            url = '{}/@repository-favorites/{}/{}'.format(
-                self.portal.absolute_url(),
-                self.regular_user.getId(),
-                favorite.favorite_id)
+        with api_client.expect_http_error(404):
+            endpoint = '@repository-favorites/{}/{}'.format(self.regular_user.getId(), favorite.favorite_id)
+            api_client.open(endpoint=endpoint, method='DELETE')
 
-            browser.open(url, method='DELETE',
-                         headers={'Accept': 'application/json'})
+        expected_error = {
+            u"message": u'Resource not found: http://nohost/plone/@repository-favorites/kathi.barfuss/1',
+            u"type": u"NotFound",
+        }
+        self.assertEqual(expected_error, api_client.contents)
 
-        self.assertEqual(
-            {u"message": u'Resource not found: http://nohost/plone/@repository-favorites/kathi.barfuss/1',
-             u"type": u"NotFound"}, browser.json)
-
-    @browsing
-    def test_removes_favorite_when_already_exists_for_user(self, browser):
-        self.login(self.regular_user, browser=browser)
+    @restapi
+    def test_removes_favorite_when_already_exists_for_user(self, api_client):
+        self.login(self.regular_user, api_client)
 
         create(Builder('favorite')
                .for_user(self.regular_user)
@@ -255,17 +210,15 @@ class TestFavoritesDelete(IntegrationTestCase):
 
         self.assertEqual(1, Favorite.query.count())
 
-        url = '{}/@repository-favorites/{}/{}'.format(
-            self.portal.absolute_url(), self.regular_user.getId(),
-            self.uuid(self.dossier))
-        browser.open(url, method='DELETE', headers={'Accept': 'application/json'})
+        endpoint = '@repository-favorites/{}/{}'.format(self.regular_user.getId(), self.uuid(self.dossier))
+        api_client.open(endpoint=endpoint, method='DELETE')
 
-        self.assertEqual(204, browser.status_code)
+        self.assertEqual(204, api_client.status_code)
         self.assertEqual(0, Favorite.query.count())
 
-    @browsing
-    def test_raises_unauthorized_when_removing_a_favorite_of_a_other_user(self, browser):
-        self.login(self.regular_user, browser=browser)
+    @restapi
+    def test_raises_unauthorized_when_removing_a_favorite_of_a_other_user(self, api_client):
+        self.login(self.regular_user, api_client)
 
         create(Builder('favorite')
                .for_user(self.dossier_responsible)
@@ -273,14 +226,9 @@ class TestFavoritesDelete(IntegrationTestCase):
 
         self.assertEqual(1, Favorite.query.count())
 
-        url = '{}/@favorites/{}/{}'.format(
-            self.portal.absolute_url(), self.dossier_responsible.getId(),
-            self.uuid(self.dossier))
+        with api_client.expect_http_error(401):
+            endpoint = '@favorites/{}/{}'.format(self.dossier_responsible.getId(), self.uuid(self.dossier))
+            api_client.open(endpoint=endpoint, method='DELETE')
 
-        with browser.expect_http_error(401):
-            browser.open(url, method='DELETE', headers={'Accept': 'application/json'})
-
-        self.assertEqual(
-            {u'message': u"It's not allowed to delete favorites of other users.",
-             u'type': u'Unauthorized'},
-            browser.json)
+        expected_error = {u'message': u"It's not allowed to delete favorites of other users.", u'type': u'Unauthorized'}
+        self.assertEqual(expected_error, api_client.contents)
