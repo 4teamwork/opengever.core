c27e0e5e3a4a7ad5f437799af8b6dd613d4592fe
Joni Orponen <j.orponen@4teamwork.ch>
2018-11-19 17:02:33 +0100

    Port opengever.api recently touched tests to use the @restapi decorator.

--- opengever/api/tests/test_recently_touched.py
+++ opengever/api/tests/test_recently_touched.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from ftw.testbrowser import browsing
+from ftw.testbrowser import restapi
 from ftw.testing import freeze
 from opengever.base.interfaces import IRecentlyTouchedSettings
 from opengever.base.touched import ObjectTouchedEvent
@@ -17,136 +17,123 @@ class TestRecentlyModifiedGet(IntegrationTestCase):
     def _clear_recently_touched_log(self, user_id):
         del IAnnotations(self.portal)[RECENTLY_TOUCHED_KEY][user_id][:]
 
-    @browsing
-    def test_lists_recently_modified_objs_for_given_user(self, browser):
-        self.login(self.regular_user, browser=browser)
-
+    @restapi
+    def test_lists_recently_modified_objs_for_given_user(self, api_client):
+        self.login(self.regular_user, api_client)
         self._clear_recently_touched_log(self.regular_user.getId())
 
         with freeze(datetime(2018, 4, 30)):
             notify(ObjectTouchedEvent(self.document))
 
-        url = '%s/@recently-touched/%s' % (
-            self.portal.absolute_url(), self.regular_user.getId())
-        browser.open(url, method='GET', headers={'Accept': 'application/json'})
+        endpoint = '@recently-touched/%s' % self.regular_user.getId()
+        api_client.open(endpoint=endpoint)
 
-        self.assertEqual(200, browser.status_code)
-        self.assertEquals(
-            {'checked_out': [],
-             'recently_touched': [{
-                 'icon_class': 'icon-docx',
-                 'last_touched': '2018-04-30T00:00:00',
-                 'target_url': self.document.absolute_url(),
-                 'title': u'Vertr\xe4gsentwurf'}]},
-            browser.json)
+        self.assertEqual(200, api_client.status_code)
+        expected_item = {
+            'checked_out': [],
+            'recently_touched': [{
+                'icon_class': 'icon-docx',
+                'last_touched': '2018-04-30T00:00:00',
+                'target_url': self.document.absolute_url(),
+                'title': u'Vertr\xe4gsentwurf',
+            }],
+        }
+        self.assertEquals(expected_item, api_client.contents)
 
-    @browsing
-    def test_lists_checked_out_docs_for_given_user(self, browser):
-        self.login(self.regular_user, browser=browser)
-
+    @restapi
+    def test_lists_checked_out_docs_for_given_user(self, api_client):
+        self.login(self.regular_user, api_client)
         self._clear_recently_touched_log(self.regular_user.getId())
 
         with freeze(datetime(2018, 4, 30)):
-            manager = queryMultiAdapter(
-                (self.document, self.request), ICheckinCheckoutManager)
+            manager = queryMultiAdapter((self.document, self.request), ICheckinCheckoutManager)
             manager.checkout()
 
-        url = '%s/@recently-touched/%s' % (
-            self.portal.absolute_url(), self.regular_user.getId())
-        browser.open(url, method='GET', headers={'Accept': 'application/json'})
+        endpoint = '@recently-touched/%s' % self.regular_user.getId()
+        api_client.open(endpoint=endpoint)
 
-        self.assertEqual(200, browser.status_code)
-        self.assertEquals(
-            {'checked_out': [{
+        self.assertEqual(200, api_client.status_code)
+        expected_item = {
+            'checked_out': [{
                 'icon_class': 'icon-docx is-checked-out-by-current-user',
                 'last_touched': '2018-04-30T00:00:00+02:00',
                 'target_url': self.document.absolute_url(),
-                'title': u'Vertr\xe4gsentwurf'}],
-             'recently_touched': []},
-            browser.json)
+                'title': u'Vertr\xe4gsentwurf',
+            }],
+            'recently_touched': [],
+        }
+        self.assertEquals(expected_item, api_client.contents)
 
-    @browsing
-    def test_checked_out_docs_arent_listed_twice(self, browser):
-        self.login(self.regular_user, browser=browser)
-
+    @restapi
+    def test_checked_out_docs_arent_listed_twice(self, api_client):
+        self.login(self.regular_user, api_client)
         self._clear_recently_touched_log(self.regular_user.getId())
 
         with freeze(datetime(2018, 4, 30)):
-            manager = queryMultiAdapter(
-                (self.document, self.request), ICheckinCheckoutManager)
+            manager = queryMultiAdapter((self.document, self.request), ICheckinCheckoutManager)
             manager.checkout()
             notify(ObjectTouchedEvent(self.document))
 
-        url = '%s/@recently-touched/%s' % (
-            self.portal.absolute_url(), self.regular_user.getId())
-        browser.open(url, method='GET', headers={'Accept': 'application/json'})
+        endpoint = '@recently-touched/%s' % self.regular_user.getId()
+        api_client.open(endpoint=endpoint)
 
         # If a document is both in the log for recently touched objects as
         # well as checked out, it must only be listed once, in the
         # checked out documents section.
 
-        self.assertEqual(200, browser.status_code)
-        self.assertEquals(
-            {'checked_out': [{
+        self.assertEqual(200, api_client.status_code)
+        expected_item = {
+            'checked_out': [{
                 'icon_class': 'icon-docx is-checked-out-by-current-user',
                 'last_touched': '2018-04-30T00:00:00+02:00',
                 'target_url': self.document.absolute_url(),
-                'title': u'Vertr\xe4gsentwurf'}],
-             'recently_touched': []},
-            browser.json)
+                'title': u'Vertr\xe4gsentwurf',
+            }],
+            'recently_touched': [],
+        }
+        self.assertEquals(expected_item, api_client.contents)
 
-    @browsing
-    def test_limits_recently_touched_items(self, browser):
-        self.login(self.regular_user, browser=browser)
+    @restapi
+    def test_limits_recently_touched_items(self, api_client):
+        self.login(self.regular_user, api_client)
         user_id = self.regular_user.getId()
-
         self._clear_recently_touched_log(user_id)
 
         # Touch a couple documents (more than the current limit)
-        docs = [self.document, self.private_document, self.expired_document,
-                self.subdocument, self.taskdocument]
+        docs = [self.document, self.private_document, self.expired_document, self.subdocument, self.taskdocument]
 
         with freeze(datetime(2018, 4, 30)) as freezer:
             for doc in docs:
                 freezer.forward(minutes=1)
                 notify(ObjectTouchedEvent(doc))
 
-        api.portal.set_registry_record(
-            'limit', 3, IRecentlyTouchedSettings)
+        api.portal.set_registry_record('limit', 3, IRecentlyTouchedSettings)
 
-        url = '%s/@recently-touched/%s' % (
-            self.portal.absolute_url(), self.regular_user.getId())
-        browser.open(
-            url, method='GET', headers={'Accept': 'application/json'})
+        endpoint = '@recently-touched/%s' % self.regular_user.getId()
+        api_client.open(endpoint=endpoint)
 
         # Even though the storage contains more logged touched entries, the
         # API endpoint should truncate them to the currently defined limit.
 
-        self.assertEqual(200, browser.status_code)
-        recently_touched_list = browser.json['recently_touched']
+        self.assertEqual(200, api_client.status_code)
+        recently_touched_list = api_client.contents['recently_touched']
         self.assertEqual(3, len(recently_touched_list))
 
-    @browsing
-    def test_rejects_request_for_other_user(self, browser):
-        self.login(self.regular_user, browser=browser)
+    @restapi
+    def test_rejects_request_for_other_user(self, api_client):
+        self.login(self.regular_user, api_client)
+        with api_client.expect_unauthorized():
+            api_client.open(endpoint='@recently-touched/other-user')
+        self.assertEqual(401, api_client.status_code)
 
-        url = '%s/@recently-touched/%s' % (
-            self.portal.absolute_url(), 'other-user')
-        with browser.expect_unauthorized():
-            browser.open(
-                url, method='GET', headers={'Accept': 'application/json'})
-        self.assertEqual(401, browser.status_code)
+    @restapi
+    def test_raises_when_userid_is_missing(self, api_client):
+        self.login(self.regular_user, api_client)
+        with api_client.expect_http_error(400):
+            api_client.open(endpoint='@recently-touched')
 
-    @browsing
-    def test_raises_when_userid_is_missing(self, browser):
-        self.login(self.regular_user, browser=browser)
-
-        with browser.expect_http_error(400):
-            url = '%s/@recently-touched' % self.portal.absolute_url()
-            browser.open(url, method='GET',
-                         headers={'Accept': 'application/json'})
-
-        self.assertEqual(
-            {'type': 'BadRequest',
-             'message': 'Must supply user ID as path parameter'},
-            browser.json)
+        expected_error = {
+            'type': 'BadRequest',
+            'message': 'Must supply user ID as path parameter',
+        }
+        self.assertEqual(expected_error, api_client.contents)
