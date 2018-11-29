0d14d81861afea3b7b72f7600319bb317d9fd83b
Jonas Baumann <jone@jone.ch>
2018-11-29 16:34:49 +0100

    Provide GEVER testserver.

---
+++ opengever/core/testserver.py
@@ -0,0 +1,68 @@
+from contextlib import contextmanager
+from datetime import datetime
+from ftw.builder import session
+from ftw.testing import freeze
+from ftw.testing import staticuid
+from ftw.testing.layer import COMPONENT_REGISTRY_ISOLATION
+from opengever.base.model import create_session
+from opengever.core.testing import OpengeverFixture
+from opengever.testing.helpers import incrementing_intids
+from plone import api
+from plone.app.testing import applyProfile
+from plone.app.testing import FunctionalTesting
+from plone.testing import z2
+from zope.globalrequest import setRequest
+import pytz
+import transaction
+
+
+class TestserverLayer(OpengeverFixture):
+    defaultBases = (COMPONENT_REGISTRY_ISOLATION,)
+
+    def setUpPloneSite(self, portal):
+        session.current_session = session.BuilderSession()
+        session.current_session.session = create_session()
+        super(TestserverLayer, self).setUpPloneSite(portal)
+
+        applyProfile(portal, 'plonetheme.teamraum:gever')
+
+        portal.portal_languages.use_combined_language_codes = True
+        portal.portal_languages.addSupportedLanguage('de-ch')
+
+        from opengever.testing.fixtures import OpengeverContentFixture
+        setRequest(portal.REQUEST)
+        print 'Installing fixture. Have patience.'
+        OpengeverContentFixture()()
+        print 'Finished installing fixture.'
+        setRequest(None)
+
+    def setupLanguageTool(self, portal):
+        lang_tool = api.portal.get_tool('portal_languages')
+        lang_tool.setDefaultLanguage('de')
+        lang_tool.supported_langs = ['de-ch']
+
+
+class TestServerFunctionalTesting(FunctionalTesting):
+
+    @contextmanager
+    def isolation(self):
+        start = datetime(2018, 11, 22, 14, 29, 33, tzinfo=pytz.UTC)
+        with freeze(start, ignore_modules=['ftw.tokenauth.oauth2.jwt_grants']):
+            with staticuid('testserver-session'):
+                with incrementing_intids():
+                    yield
+
+    def testSetUp(self):
+        super(TestServerFunctionalTesting, self).testSetUp()
+        self.context_manager = self.isolation()
+        self.context_manager.__enter__()
+        transaction.commit()
+
+    def testTearDown(self):
+        self.context_manager.__exit__(None, None, None)
+        super(TestServerFunctionalTesting, self).testTearDown()
+
+
+OPENGEVER_TESTSERVER = TestServerFunctionalTesting(
+    bases=(TestserverLayer(), z2.ZSERVER_FIXTURE),
+    name="opengever.core:testserver")
