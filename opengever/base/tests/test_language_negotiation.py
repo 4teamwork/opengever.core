3448d632e847f93bc9083691a531a410e14991e8
Lukas Graf <lukas.graf@4teamwork.ch>
2018-12-04 18:11:53 +0100

    Supply date format locale settings for fr-ch.

--- opengever/base/tests/test_language_negotiation.py
+++ opengever/base/tests/test_language_negotiation.py
@@ -38,3 +38,27 @@ class TestLanguageNegotiation(IntegrationTestCase):
 
         self.assertIn('language-fr-ch',
                       browser.css('.currentLanguage').first.classes)
+
+
+class TestLanguageDependentDateFormat(IntegrationTestCase):
+
+    def setUp(self):
+        super(TestLanguageDependentDateFormat, self).setUp()
+        lang_tool = api.portal.get_tool('portal_languages')
+        lang_tool.setDefaultLanguage('de-ch')
+        lang_tool.supported_langs = ['fr-ch', 'de-ch']
+
+    def get_byline_value_by_label(self, label, browser):
+        byline_elements = browser.css(".documentByLine li")
+
+        for element in byline_elements:
+            if element.css('.label').first.text == label:
+                return element.css('> *')[1]
+
+    @browsing
+    def test_french_date_format(self, browser):
+        self.login(self.regular_user, browser=browser)
+
+        browser.open(self.dossier, headers={'Accept-Language': 'fr'})
+        start_date = self.get_byline_value_by_label(u'D\xe9but:', browser)
+        self.assertEqual('01.01.2016', start_date.text)
