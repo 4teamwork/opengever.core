e86e59891b8dfd8fad695c2a08288419c52e0392
Philippe Gross <gross.philippe@gmail.com>
2018-12-10 13:08:07 +0100

    Merge pull request #5174 from 4teamwork/pg_fix_task_revoke_permissions
    Fix task revoking permissions on close/reassign

--- opengever/task/tests/test_response.py
+++ opengever/task/tests/test_response.py
@@ -5,6 +5,7 @@ from opengever.testing import IntegrationTestCase
 from persistent import Persistent
 from persistent.list import PersistentList
 from persistent.mapping import PersistentMapping
+from plone import api
 
 
 class TestTaskResponses(IntegrationTestCase):
@@ -45,3 +46,33 @@ class TestTaskResponseForm(IntegrationTestCase):
         browser.click_on('task-transition-in-progress-resolved')
 
         self.assertEquals([], info_messages())
+
+    @browsing
+    def test_redirects_to_task_if_user_has_access_rights(self, browser):
+        self.login(self.dossier_responsible, browser=browser)
+        browser.open(self.subtask, view='tabbedview_view-overview')
+        browser.click_on('task-transition-resolved-tested-and-closed')
+        browser.click_on('Save')
+
+        self.assertEquals(['Review state successfully changed.'],
+                          info_messages())
+        self.assertEquals(self.subtask, browser.context)
+
+    @browsing
+    def test_redirects_to_portal_if_user_has_no_longer_access_rights(self, browser):
+        self.login(self.administrator)
+
+        self.set_workflow_state('task-state-open', self.task_in_protected_dossier)
+        self.task_in_protected_dossier.task_type = 'information'
+        self.task_in_protected_dossier.sync()
+
+        self.login(self.regular_user, browser=browser)
+        browser.open(self.task_in_protected_dossier, view='tabbedview_view-overview')
+        browser.click_on('task-transition-open-tested-and-closed')
+        browser.click_on('Save')
+
+        self.assertEquals(
+            ['Review state successfully changed, you are no longer permitted '
+             'to access the task.'],
+            info_messages())
+        self.assertEquals(api.portal.get(), browser.context)
