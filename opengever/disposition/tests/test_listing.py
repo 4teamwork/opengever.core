d3b751371551dcd13d04cd768de7cb0dcfe4cc4d
Niklaus Johner <niklaus.johner@4teamwork.ch>
2018-12-12 08:54:39 +0100

    Port disposition removal protocol tests to integration layer.

--- opengever/disposition/tests/test_listing.py
+++ opengever/disposition/tests/test_listing.py
@@ -3,7 +3,14 @@ from ftw.builder import Builder
 from ftw.builder import create
 from ftw.testbrowser import browsing
 from ftw.testing import freeze
+from lxml.cssselect import CSSSelector
+from opengever.base.security import elevated_privileges
+from opengever.disposition.interfaces import IHistoryStorage
+from opengever.latex.listing import ILaTexListing
 from opengever.testing import IntegrationTestCase
+from plone import api
+from zope.component import getMultiAdapter
+import lxml
 
 
 class TestDispositionListing(IntegrationTestCase):
@@ -101,3 +108,91 @@ class TestDispositionListing(IntegrationTestCase):
         self.assertItemsEqual(
             ['In Progress', 'Appraised', 'Disposed', 'Disposed', 'Closed'],
             [row.get('Title') for row in rows])
+
+
+class BaseLatexListingTest(IntegrationTestCase):
+
+    def assert_row_values(self, expected, row):
+        self.assertEquals(
+            expected,
+            [value.text_content().strip() for value in
+             row.xpath(CSSSelector('td').path)])
+
+    def get_listing_rows(self, obj, listing_name, items):
+        listing = getMultiAdapter(
+            (obj, obj.REQUEST, self),
+            ILaTexListing, name=listing_name)
+        listing.items = items
+
+        table = lxml.html.fromstring(listing.template())
+        rows = table.xpath(CSSSelector('tbody tr').path)
+        return rows
+
+
+class TestDestroyedDossierListing(BaseLatexListingTest):
+
+    def test_listing(self):
+        self.login(self.regular_user)
+        with elevated_privileges():
+            api.content.transition(obj=self.disposition, transition='disposition-transition-appraise')
+            api.content.transition(obj=self.disposition, transition='disposition-transition-dispose')
+            api.content.transition(obj=self.disposition, transition='disposition-transition-archive')
+            api.content.transition(obj=self.disposition, transition='disposition-transition-close')
+
+        rows = self.get_listing_rows(self.disposition,
+                                     'destroyed_dossiers',
+                                     self.disposition.get_dossier_representations())
+
+        self.assert_row_values(
+            ['Client1 1.1 / 11', 'Hannah Baufrau', 'Yes'], rows[0])
+        self.assert_row_values(
+            ['Client1 1.1 / 12', 'Hans Baumann', 'No'], rows[1])
+
+
+class TestDispositionHistoryListing(BaseLatexListingTest):
+
+    def test_listing(self):
+        self.login(self.records_manager)
+
+        with freeze(datetime(2016, 11, 1, 11, 0)):
+            storage = IHistoryStorage(self.disposition)
+            storage.add('edited', api.user.get_current().getId(), [])
+
+        with freeze(datetime(2016, 11, 6, 12, 33)), elevated_privileges():
+            api.content.transition(self.disposition,
+                                   'disposition-transition-appraise')
+            api.content.transition(self.disposition,
+                                   'disposition-transition-dispose')
+
+        with freeze(datetime(2016, 11, 16, 8, 12)), elevated_privileges():
+            api.content.transition(self.disposition,
+                                   'disposition-transition-archive')
+
+        rows = self.get_listing_rows(self.disposition,
+                                     'disposition_history',
+                                     self.disposition.get_history())
+
+        expected_rows = [
+            ['Nov 16, 2016 08:12 AM', 'Flucht Ramon (ramon.flucht)', 'disposition-transition-archive'],
+            ['Nov 06, 2016 12:33 PM', 'Flucht Ramon (ramon.flucht)', 'disposition-transition-dispose'],
+            ['Nov 06, 2016 12:33 PM', 'Flucht Ramon (ramon.flucht)', 'disposition-transition-appraise'],
+            ['Nov 01, 2016 11:00 AM', 'Flucht Ramon (ramon.flucht)', 'Disposition edited'],
+            ['Aug 31, 2016 07:05 PM', 'Flucht Ramon (ramon.flucht)', 'Disposition added']]
+
+        for row, expected_row in zip(rows, expected_rows):
+            self.assert_row_values(expected_row, row)
+
+    def test_transition_label_for_added_and_edited_entries_is_translated_correctly(self):
+        self.login(self.records_manager)
+
+        storage = IHistoryStorage(self.disposition)
+        storage.add('edited', api.user.get_current().getId(), [])
+
+        rows = self.get_listing_rows(self.disposition,
+                                     'disposition_history',
+                                     self.disposition.get_history())
+
+        self.assert_row_values(
+            ['Disposition edited'], rows[0][2])
+        self.assert_row_values(
+            ['Disposition added'], rows[1][2])
