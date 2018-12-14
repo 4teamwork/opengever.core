0d0feb9548f342ace1ebef07ba8568d5dfbeddf2
Niklaus Johner <niklaus.johner@4teamwork.ch>
2018-12-14 10:35:25 +0100

    Port disposition byline test to integration layer.

--- opengever/disposition/tests/test_byline.py
+++ opengever/disposition/tests/test_byline.py
@@ -1,34 +1,16 @@
-from DateTime import DateTime
-from ftw.builder import Builder
-from ftw.builder import create
 from ftw.testbrowser import browsing
-from opengever.testing import FunctionalTestCase
+from opengever.testing import IntegrationTestCase
 
 
-class TestDispositionByline(FunctionalTestCase):
-
-    def setUp(self):
-        super(TestDispositionByline, self).setUp()
-        self.root = create(Builder('repository_root'))
-        self.repository = create(Builder('repository').within(self.root))
-        self.dossier1 = create(Builder('dossier')
-                               .as_expired()
-                               .within(self.repository))
-
-        self.grant('Records Manager')
+class TestDispositionByline(IntegrationTestCase):
 
     @browsing
     def test_shows_current_review_state_creation_and_modification_date(self, browser):
-        disposition = create(Builder('disposition')
-                             .having(dossiers=[self.dossier1])
-                             .with_creation_date(DateTime(2016, 4, 10, 7, 25))
-                             .with_modification_date(DateTime(2016, 4, 22, 22))
-                             .within(self.root))
-
-        browser.login().open(disposition)
+        self.login(self.records_manager, browser)
+        browser.open(self.disposition)
 
         self.assertEquals(
-            ['Created: Apr 10, 2016 07:25 AM',
+            ['Created: Aug 31, 2016 09:05 PM',
              'State: disposition-state-in-progress',
-             'Last modified: Apr 22, 2016 10:00 PM'],
+             'Last modified: Aug 31, 2016 09:05 PM'],
             browser.css('#plone-document-byline li').text)
