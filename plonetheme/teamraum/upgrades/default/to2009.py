fca827bebb80019f44e9118d81e5d21e122d3c30
Philippe Gross <philippe.gross@4teamwork.ch>
2018-12-19 10:57:09 +0100

    Remove plonetheme.teamraum upgradesteps.
    Those upgrades are outdated and no longer used, but can lead to
    problems, because the plonetheme.teamraum upgrades are run after the
    opengever.core ones. For example the upgradestep which removes the
    `++theme++plonetheme.teamraum/js/dropdown.js` (`RegisterMissingDropdownJS`)
    is run before the older plonetheme.teamraum upgradestep which adds the
    `dropdown.js`. So after an upgrade a wrong and not working dropdown.js
    is registered.

--- plonetheme/teamraum/upgrades/default/to2009.py
+++
@@ -1,8 +0,0 @@
-from ftw.upgrade import UpgradeStep
-from plonetheme.teamraum.hooks import remove_screen_media_types
-
-
-class RemoveScreenMediaType(UpgradeStep):
-
-    def __call__(self):
-        remove_screen_media_types(self.portal)
