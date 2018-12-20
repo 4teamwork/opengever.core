788cff246d39be7fc53144dbace9392a587b8bb3
Philippe Gross <philippe.gross@4teamwork.ch>
2018-12-20 23:06:02 +0100

    Convert Edit public trial form tests to integration tests.

--- opengever/base/tests/test_edit_public_trial_form.py
+++ opengever/base/tests/test_edit_public_trial_form.py
@@ -1,89 +1,77 @@
-from ftw.builder import Builder
-from ftw.builder import create
 from ftw.testbrowser import browsing
-from opengever.base.browser import edit_public_trial
-from opengever.testing import FunctionalTestCase
 from opengever.base.behaviors.classification import PUBLIC_TRIAL_PRIVATE
+from opengever.base.browser import edit_public_trial
+from opengever.testing import IntegrationTestCase
+from plone import api
+from zExceptions import Unauthorized
 
 
-class TestEditPublicTrialHelperFunction(FunctionalTestCase):
+class TestEditPublicTrialHelperFunction(IntegrationTestCase):
 
     def setUp(self):
         super(TestEditPublicTrialHelperFunction, self).setUp()
-
-        dossier = create(Builder('dossier').in_state('dossier-state-resolved'))
-        self.document = create(Builder('document')
-                               .with_dummy_content()
-                               .within(dossier))
-
         self.can_edit = edit_public_trial.can_access_public_trial_edit_form
 
     def test_parent_of_content_needs_to_be_a_dossier(self):
-        user = self.portal.portal_membership.getAuthenticatedMember()
+        self.login(self.regular_user)
+        self.set_workflow_state('dossier-state-resolved', self.dossier)
 
+        user = api.user.get_current()
         self.assertTrue(self.can_edit(user, self.document))
 
         with self.assertRaises(AssertionError):
             self.can_edit(user, self.document.aq_parent)
 
     def test_one_of_the_parents_needs_to_be_a_dossier(self):
-        user = self.portal.portal_membership.getAuthenticatedMember()
-        dossier = create(Builder('dossier').in_state('dossier-state-resolved'))
-        task = create(Builder('task').within(dossier))
-        document = create(Builder('document')
-                          .with_dummy_content()
-                          .within(task))
+        self.login(self.regular_user)
+        self.set_workflow_state('dossier-state-resolved', self.dossier)
 
-        self.assertTrue(self.can_edit(user, document))
+        self.assertTrue(self.can_edit(api.user.get_current(), self.taskdocument))
 
     def test_user_CANNOT_edit_when_parent_dossier_is_inactive(self):
-        user = self.portal.portal_membership.getAuthenticatedMember()
-        dossier = create(Builder('dossier').in_state('dossier-state-inactive'))
-        document = create(Builder('document')
-                          .with_dummy_content()
-                          .within(dossier))
+        self.login(self.regular_user)
+        self.set_workflow_state('dossier-state-inactive', self.dossier)
 
-        self.assertFalse(self.can_edit(user, document))
+        self.assertFalse(self.can_edit(api.user.get_current(), self.document))
 
     def test_user_WITH_modify_permission_in_open_state_CAN_edit(self):
-        user = self.portal.portal_membership.getAuthenticatedMember()
+        self.login(self.regular_user)
+        self.set_workflow_state('dossier-state-resolved', self.dossier)
 
-        self.assertTrue(self.can_edit(user, self.document))
+        self.assertTrue(self.can_edit(api.user.get_current(), self.document))
 
     def test_user_WITHOUT_modify_permission_in_open_state_CANNOT_edit(self):
-        user = create(Builder('user').with_roles('Contributor'))
-        self.assertFalse(self.can_edit(user, self.document))
+        self.login(self.regular_user)
+        self.set_workflow_state('dossier-state-resolved', self.dossier)
+
+        self.assertFalse(self.can_edit(self.reader_user, self.document))
 
     def test_does_not_work_on_subdossier(self):
-        user = self.portal.portal_membership.getAuthenticatedMember()
-        dossier = create(Builder('dossier'))
-        subdossier = create(Builder('dossier').within(dossier))
+        self.login(self.regular_user)
+        self.set_workflow_state('dossier-state-resolved', self.dossier)
 
         with self.assertRaises(AssertionError):
-            self.can_edit(user, subdossier)
+            self.can_edit(api.user.get_current(), self.subdossier)
 
 
-class TestEditPublicTrialForm(FunctionalTestCase):
-
-    def setUp(self):
-        super(TestEditPublicTrialForm, self).setUp()
-
-        dossier = create(Builder('dossier').in_state('dossier-state-resolved'))
-        self.document = create(Builder('document')
-                               .with_dummy_content()
-                               .within(dossier))
+class TestEditPublicTrialForm(IntegrationTestCase):
 
     @browsing
     def test_raise_aunauthorized_if_the_user_CANNOT_modify(self, browser):
-        user = create(Builder('user').with_roles('Contributor'))
+        self.login(self.regular_user, browser=browser)
+        self.set_workflow_state('dossier-state-resolved', self.dossier)
 
-        with browser.expect_unauthorized():
-            browser.login(user.getId()).visit(self.document,
-                                              view='edit_public_trial')
+        self.login(self.reader_user, browser=browser)
+        with self.assertRaises(Unauthorized):
+            browser.visit(self.document, view='edit_public_trial')
 
     @browsing
     def test_user_CAN_access_edit_form(self, browser):
-        browser.login().visit(self.document, view='edit_public_trial')
+        self.login(self.regular_user, browser=browser)
+        self.login(self.administrator, browser=browser)
+
+        self.set_workflow_state('dossier-state-resolved', self.dossier)
+        browser.visit(self.document, view='edit_public_trial')
 
         self.assertEquals(
             '{0}/edit_public_trial'.format(self.document.absolute_url()),
@@ -94,14 +82,20 @@ class TestEditPublicTrialForm(FunctionalTestCase):
 
     @browsing
     def test_user_CAN_modify_public_trial_information(self, browser):
-        browser.login().visit(self.document, view='edit_public_trial')
+        self.login(self.regular_user, browser=browser)
+        self.set_workflow_state('dossier-state-resolved', self.dossier)
+
+        browser.open(self.document, view='edit_public_trial')
         browser.fill({'Public Trial': PUBLIC_TRIAL_PRIVATE}).submit()
 
         self.assertEquals('private', self.document.public_trial)
 
     @browsing
     def test_user_CAN_modify_public_trial_statement(self, browser):
-        browser.login().visit(self.document, view='edit_public_trial')
+        self.login(self.regular_user, browser=browser)
+        self.set_workflow_state('dossier-state-resolved', self.dossier)
+
+        browser.visit(self.document, view='edit_public_trial')
         browser.fill({'Public trial statement': 'Foo statement'}).submit()
 
         self.assertEquals('Foo statement',
@@ -109,6 +103,9 @@ class TestEditPublicTrialForm(FunctionalTestCase):
 
     @browsing
     def test_user_submit_cancel_button(self, browser):
-        browser.login().visit(self.document, view='edit_public_trial')
+        self.login(self.regular_user, browser=browser)
+        self.set_workflow_state('dossier-state-resolved', self.dossier)
+
+        browser.visit(self.document, view='edit_public_trial')
         browser.find('Cancel').click()
         self.assertEquals(self.document.absolute_url(), browser.url)
