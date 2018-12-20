ca6913db6646cbbb98972f10adc4bec79ee7bcf1
Philippe Gross <philippe.gross@4teamwork.ch>
2018-12-20 12:40:57 +0100

    Convert all opengever.advancedsearch tests to integration test.

--- opengever/advancedsearch/tests/test_search.py
+++ opengever/advancedsearch/tests/test_search.py
@@ -2,45 +2,43 @@ from ftw.builder import Builder
 from ftw.builder import create
 from ftw.testbrowser import browsing
 from ftw.testbrowser.exceptions import FormFieldNotFound
-from opengever.core.testing import activate_filing_number
-from opengever.core.testing import inactivate_filing_number
-from opengever.testing import FunctionalTestCase
+from opengever.dossier.behaviors.dossier import IDossierMarker
+from opengever.testing import IntegrationTestCase
 import urllib
+import urlparse
 
 
-class TestSearchForm(FunctionalTestCase):
+class TestSearchForm(IntegrationTestCase):
 
     @browsing
     def test_filing_number_fields_is_hidden_in_site_without_filing_number_support(self, browser):
-        browser.login().open(self.portal, view='advanced_search')
+        self.login(self.regular_user, browser=browser)
+        browser.open(self.portal, view='advanced_search')
 
         with self.assertRaises(FormFieldNotFound):
             browser.fill({'Filing number': 'Test'})
 
 
-class TestSearchFormWithFilingNumberSupport(FunctionalTestCase):
+class TestSearchFormWithFilingNumberSupport(IntegrationTestCase):
 
-    def setUp(self):
-        super(TestSearchFormWithFilingNumberSupport, self).setUp()
-        activate_filing_number(self.portal)
-
-    def tearDown(self):
-        super(TestSearchFormWithFilingNumberSupport, self).tearDown()
-        inactivate_filing_number(self.portal)
+    features = ('filing_number', )
 
     @browsing
     def test_filing_number_field_is_displayed_in_a_filing_number_supported_site(self, browser):
-        browser.login().open(view='advanced_search')
-        browser.fill({'Filing number': 'Test'})
+        self.login(self.regular_user, browser=browser)
+        browser.open(self.portal, view='advanced_search')
+
+        browser.fill({'Filing number': u'FD-112'})
 
 
-class TestSearchFormObjectProvidesDescription(FunctionalTestCase):
+class TestSearchFormObjectProvidesDescription(IntegrationTestCase):
 
     @browsing
     def test_contains_special_info_in_a_multi_client_setup(self, browser):
-        create(Builder('admin_unit'))
+        create(Builder('admin_unit').id("additional"))
 
-        browser.login().open(self.portal, view='advanced_search')
+        self.login(self.regular_user, browser=browser)
+        browser.open(self.portal, view='advanced_search')
 
         self.assertEquals(
             ['Select the contenttype to be searched for.It searches '
@@ -49,125 +47,191 @@ class TestSearchFormObjectProvidesDescription(FunctionalTestCase):
 
     @browsing
     def test_not_contains_client_info_in_a_single_client_setup(self, browser):
-        browser.login().open(self.portal, view='advanced_search')
+        self.login(self.regular_user, browser=browser)
+        browser.open(self.portal, view='advanced_search')
 
         self.assertEquals(
             ['Select the contenttype to be searched for.'],
             browser.css('#formfield-form-widgets-object_provides span.formHelp').text)
 
 
-class TestSearchWithContent(FunctionalTestCase):
-
-    def setUp(self):
-        super(TestSearchWithContent, self).setUp()
-
-        self.dossier1 = create(Builder("dossier").titled(u"Dossier1"))
-        self.dossier2 = create(Builder("dossier").titled(u"Dossier2"))
+class TestSearchWithContent(IntegrationTestCase):
 
     @browsing
     def test_search_dossiers(self, browser):
-        browser.login().open(self.dossier1, view='advanced_search')
+        self.login(self.regular_user, browser=browser)
+
+        browser.open(self.portal, view='advanced_search')
         browser.fill({
-            'Text': "dossier1",
+            'Text': "kantonalen Finanzverwaltung",
             'Type': ['opengever.dossier.behaviors.dossier.IDossierMarker']})
         browser.css('#form-buttons-button_search').first.click()
 
         self.assertEquals(['1'], browser.css('#search-results-number').text)
         self.assertEquals(
-            'http://nohost/plone/@@search?object_provides=opengever.dossier.behaviors.dossier.IDossierMarker&SearchableText=dossier1',
-            browser.url)
+            'object_provides=opengever.dossier.behaviors.dossier.IDossierMarker'
+            '&SearchableText=kantonalen+Finanzverwaltung',
+            urlparse.urlparse(browser.url).query)
+        self.assertEquals(
+            [u'Vertr\xe4ge mit der kantonalen Finanzverwaltung'],
+            browser.css('.searchResults .searchItem dt').text)
 
     @browsing
     def test_search_documents(self, browser):
-        create(Builder("document").within(self.dossier1).titled("Document1"))
-        create(Builder("document").within(self.dossier2).titled("Document2"))
+        self.login(self.regular_user, browser=browser)
 
-        # search documents (we can't find the document because we must
-        # change the content-type)
-        browser.login().open(self.dossier1, view='advanced_search')
+        browser.open(self.portal, view='advanced_search')
         browser.fill({
-            'Text': "document1",
-            'Type': ['opengever.dossier.behaviors.dossier.IDossierMarker']})
-        browser.css('#form-buttons-button_search').first.click()
-        self.assertEquals(['0'], browser.css('#search-results-number').text)
-
-        # search documents with the right content-type
-        browser.open(self.dossier1, view='advanced_search')
-        browser.fill({
-            'Text': "document1",
+            'Text': u'Vertr\xe4gsentwurf',
             'Type': ['opengever.document.behaviors.IBaseDocument']})
         browser.css('#form-buttons-button_search').first.click()
 
-        self.assertEquals(['1'], browser.css('#search-results-number').text)
-        self.assertEquals(['Document1'], browser.css('.searchItem dt').text)
+        self.assertEquals(['3'], browser.css('#search-results-number').text)
+        self.assertEquals(
+            [self.document.title, self.taskdocument.title,
+             self.proposaldocument.title],
+            browser.css('.searchResults .searchItem dt').text)
 
     @browsing
     def test_search_tasks(self, browser):
-        create(Builder("task").within(self.dossier1).titled("Task1"))
-        create(Builder("task").within(self.dossier2).titled("Task2"))
-
-        # search tasks (we can't find the task because we must change
-        # the content-type)
-        browser.login().open(self.dossier1, view='advanced_search')
+        self.login(self.regular_user, browser=browser)
+        browser.open(self.portal, view='advanced_search')
         browser.fill({
-            'Text': "task1",
-            'Type': ['opengever.dossier.behaviors.dossier.IDossierMarker']})
-        browser.css('#form-buttons-button_search').first.click()
-        self.assertEquals(['0'], browser.css('#search-results-number').text)
-
-        # search tasks with the right content-type
-        browser.login().open(self.dossier1, view='advanced_search')
-        browser.fill({
-            'Text': "task1",
+            'Text': "Rechtliche Grundlagen",
             'Type': ['opengever.task.task.ITask']})
         browser.css('#form-buttons-button_search').first.click()
+
         self.assertEquals(['1'], browser.css('#search-results-number').text)
-        self.assertEquals(['Task1'], browser.css('.searchItem dt').text)
+        self.assertEquals([self.subtask.title],
+                          browser.css('.searchResults .searchItem dt').text)
+
+    @browsing
+    def test_date_min_queries(self, browser):
+        self.login(self.regular_user, browser=browser)
+        browser.open(self.portal, view='advanced_search')
+
+        browser.fill({
+            'Text': "kantonalen Finanzverwaltung",
+            'form.widgets.object_provides': IDossierMarker.__identifier__,
+            'form.widgets.start_1': "10.10.2015"})
+        browser.css('#form-buttons-button_search').first.click()
+
+        self.assertEquals([self.dossier.title],
+                          browser.css('.searchResults .searchItem dt').text)
+
+        browser.open(self.portal, view='advanced_search')
+        browser.fill({
+            'Text': "kantonalen Finanzverwaltung",
+            'form.widgets.object_provides': IDossierMarker.__identifier__,
+            'form.widgets.start_1': "10.01.2017"})
+        browser.css('#form-buttons-button_search').first.click()
+        self.assertEquals([],
+                          browser.css('.searchResults .searchItem dt').text)
+
+    @browsing
+    def test_date_max_queries(self, browser):
+        self.login(self.regular_user, browser=browser)
+        browser.open(self.portal, view='advanced_search')
+
+        browser.fill({
+            'Text': u'Abgeschlossene Vertr\xe4ge',
+            'form.widgets.object_provides': IDossierMarker.__identifier__,
+            'form.widgets.end_2': "10.10.2002"})
+        browser.css('#form-buttons-button_search').first.click()
+
+        self.assertEquals([self.expired_dossier.title],
+                          browser.css('.searchResults .searchItem dt').text)
+
+        browser.open(self.portal, view='advanced_search')
+        browser.fill({
+            'Text': u'Abgeschlossene Vertr\xe4ge',
+            'form.widgets.object_provides': IDossierMarker.__identifier__,
+            'form.widgets.end_2': "10.10.1998"})
+        browser.css('#form-buttons-button_search').first.click()
+        self.assertEquals([],
+                          browser.css('.searchResults .searchItem dt').text)
+
+    @browsing
+    def test_can_select_dossier_responsible_from_inactive_ou_in_widget(self, browser):
+        self.create_inactive_user()
+        self.login(self.regular_user, browser=browser)
+
+        # We manually do a request on the widget here because
+        # ftw.keywordwidget.tests.widget.AsyncKeywordWidget
+        # doesn't support .query() yet
+        widget_url = '/'.join((
+            self.portal.absolute_url(),
+            'advanced_search',
+            '++widget++form.widgets.responsible'))
+        browser.open(widget_url + '/search?q=without')
+
+        self.assertIn(
+            {'id': 'user.without.orgunit',
+             'text': 'Orgunit Without (user.without.orgunit)',
+             '_resultId': 'user.without.orgunit'},
+            browser.json['results'])
+
+    @browsing
+    def test_can_select_task_issuer_from_inactive_ou_in_widget(self, browser):
+        self.create_inactive_user()
+        self.login(self.regular_user, browser=browser)
+
+        # We manually do a request on the widget here because
+        # ftw.keywordwidget.tests.widget.AsyncKeywordWidget
+        # doesn't support .query() yet
+        widget_url = '/'.join((
+            self.portal.absolute_url(),
+            'advanced_search',
+            '++widget++form.widgets.issuer'))
+        browser.open(widget_url + '/search?q=without')
+
+        self.assertIn(
+            {'id': 'user.without.orgunit',
+             'text': 'Orgunit Without (user.without.orgunit)',
+             '_resultId': 'user.without.orgunit'},
+            browser.json['results'])
+
+    @browsing
+    def test_can_select_doc_checked_out_from_inactive_ou_in_widget(self, browser):
+        self.create_inactive_user()
+        self.login(self.regular_user, browser=browser)
+
+        # We manually do a request on the widget here because
+        # ftw.keywordwidget.tests.widget.AsyncKeywordWidget
+        # doesn't support .query() yet
+        widget_url = '/'.join((
+            self.portal.absolute_url(),
+            'advanced_search',
+            '++widget++form.widgets.checked_out'))
+        browser.open(widget_url + '/search?q=without')
+
+        self.assertIn(
+            {'id': 'user.without.orgunit',
+             'text': 'Orgunit Without (user.without.orgunit)',
+             '_resultId': 'user.without.orgunit'},
+            browser.json['results'])
+
+    @browsing
+    def test_disable_unload_protection(self, browser):
+        self.login(self.regular_user, browser=browser)
+        browser.open(self.portal, view='advanced_search')
+
+        self.assertNotIn('enableUnloadProtection', browser.contents)
 
 
-class TestSearchWithoutContent(FunctionalTestCase):
+class TestQueryStrings(IntegrationTestCase):
 
-    def setUp(self):
-        super(TestSearchWithoutContent, self).setUp()
-
-        activate_filing_number(self.portal)
-
-        self.dossier1 = create(Builder("dossier"))
-
-    def tearDown(self):
-        super(TestSearchWithoutContent, self).tearDown()
-
-        inactivate_filing_number(self.portal)
+    features = ('filing_number', )
 
     def assertBrowserUrlContainsSearchParams(self, browser, params):
         url = "http://nohost/plone/@@search?{}".format(urllib.urlencode(params))
         self.assertEqual(browser.url, url)
 
     @browsing
-    def test_date_min_or_max_range_is_queried(self, browser):
-        browser.login()
-        browser.open(view='advanced_search')
-        browser.fill({'form.widgets.object_provides': 'opengever.dossier.behaviors.dossier.IDossierMarker',
-                      'form.widgets.start_1': "01.01.2010",
-                      'form.widgets.end_2': "01.04.2010"})
-        browser.css('#form-buttons-button_search').first.click()
-
-        self.assertBrowserUrlContainsSearchParams(browser, [
-            ('object_provides', 'opengever.dossier.behaviors.dossier.IDossierMarker'),
-            ('start.range:record', 'min'),
-            ('start.query:record:list:date', '2010-01-01'),
-            ('end.range:record', 'max'),
-            ('end.query:record:list:date', '2010-04-02'),
-        ])
-
-    @browsing
     def test_validate_searchstring_for_dossiers(self, browser):
-        create(Builder('ogds_user')
-               .having(firstname='Foo', lastname='Boss',
-                       userid='foo@example.com')
-               .assign_to_org_units([self.org_unit]))
+        self.login(self.regular_user, browser=browser)
+        browser.open(view='advanced_search')
 
-        browser.login().open(view='advanced_search')
         browser.fill({'form.widgets.searchableText': "dossier1",
                       'form.widgets.object_provides': ['opengever.dossier.behaviors.dossier.IDossierMarker'],
                       'form.widgets.start_1': "01.01.2010",
@@ -180,8 +244,7 @@ class TestSearchWithoutContent(FunctionalTestCase):
                       'form.widgets.dossier_review_state:list': 'dossier-state-active'})
 
         form = browser.find_form_by_field('Responsible')
-        form.find_widget('Responsible').fill('foo@example.com')
-
+        form.find_widget('Responsible').fill(self.regular_user.id)
         browser.css('#form-buttons-button_search').first.click()
 
         self.assertBrowserUrlContainsSearchParams(browser, [
@@ -196,76 +259,15 @@ class TestSearchWithoutContent(FunctionalTestCase):
             ('reference', 'OG 14.2'),
             ('sequence_number:int', '5'),
             ('searchable_filing_no', '14'),
-            ('responsible', 'foo@example.com'),
+            ('responsible', self.regular_user.id),
             ('review_state:list', 'dossier-state-active'),
         ])
 
     @browsing
-    def test_can_select_dossier_responsible_from_inactive_ou_in_widget(self, browser):
-        create(Builder('ogds_user')
-               .having(firstname='Without', lastname='Orgunit',
-                       userid='user.without.orgunit'))
-
-        # We manually do a request on the widget here because
-        # ftw.keywordwidget.tests.widget.AsyncKeywordWidget
-        # doesn't support .query() yet
-        widget_url = '/'.join((
-            self.portal.absolute_url(),
-            'advanced_search',
-            '++widget++form.widgets.responsible'))
-        browser.login().open(widget_url + '/search?q=without')
-
-        self.assertIn(
-            {'id': 'user.without.orgunit',
-             'text': 'Orgunit Without (user.without.orgunit)',
-             '_resultId': 'user.without.orgunit'},
-            browser.json['results'])
-
-    @browsing
-    def test_can_select_task_issuer_from_inactive_ou_in_widget(self, browser):
-        create(Builder('ogds_user')
-               .having(firstname='Without', lastname='Orgunit',
-                       userid='user.without.orgunit'))
-
-        # We manually do a request on the widget here because
-        # ftw.keywordwidget.tests.widget.AsyncKeywordWidget
-        # doesn't support .query() yet
-        widget_url = '/'.join((
-            self.portal.absolute_url(),
-            'advanced_search',
-            '++widget++form.widgets.issuer'))
-        browser.login().open(widget_url + '/search?q=without')
-
-        self.assertIn(
-            {'id': 'user.without.orgunit',
-             'text': 'Orgunit Without (user.without.orgunit)',
-             '_resultId': 'user.without.orgunit'},
-            browser.json['results'])
-
-    @browsing
-    def test_can_select_doc_checked_out_from_inactive_ou_in_widget(self, browser):
-        create(Builder('ogds_user')
-               .having(firstname='Without', lastname='Orgunit',
-                       userid='user.without.orgunit'))
-
-        # We manually do a request on the widget here because
-        # ftw.keywordwidget.tests.widget.AsyncKeywordWidget
-        # doesn't support .query() yet
-        widget_url = '/'.join((
-            self.portal.absolute_url(),
-            'advanced_search',
-            '++widget++form.widgets.checked_out'))
-        browser.login().open(widget_url + '/search?q=without')
-
-        self.assertIn(
-            {'id': 'user.without.orgunit',
-             'text': 'Orgunit Without (user.without.orgunit)',
-             '_resultId': 'user.without.orgunit'},
-            browser.json['results'])
-
-    @browsing
     def test_validate_searchstring_for_documents(self, browser):
-        browser.login().open(view='advanced_search')
+        self.login(self.regular_user, browser=browser)
+        browser.open(view='advanced_search')
+
         browser.fill({'form.widgets.searchableText': "document1",
                       'form.widgets.object_provides': 'opengever.document.behaviors.IBaseDocument',
                       'form.widgets.receipt_date_1': "01.01.2010",
@@ -293,7 +295,9 @@ class TestSearchWithoutContent(FunctionalTestCase):
 
     @browsing
     def test_validate_searchstring_for_tasks(self, browser):
-        browser.login().open(view='advanced_search')
+        self.login(self.regular_user, browser=browser)
+        browser.open(view='advanced_search')
+
         browser.fill({'form.widgets.searchableText': "task1",
                       'form.widgets.object_provides': 'opengever.task.task.ITask',
                       'form.widgets.deadline_1': "01.01.2010",
@@ -310,8 +314,3 @@ class TestSearchWithoutContent(FunctionalTestCase):
             ('deadline.query:record:list:date', '2010-02-02'),
             ('task_type', 'information'),
         ])
-
-    @browsing
-    def test_disable_unload_protection(self, browser):
-        browser.login().open(view='advanced_search')
-        self.assertNotIn('enableUnloadProtection', browser.contents)
