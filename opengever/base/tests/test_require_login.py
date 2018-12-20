7301811ed31be9f524d646a01381a38b785b2252
Philippe Gross <philippe.gross@4teamwork.ch>
2018-12-20 18:44:06 +0100

    Convert TestRequireLoginScript to integrationtests.

--- opengever/base/tests/test_require_login.py
+++ opengever/base/tests/test_require_login.py
@@ -1,63 +1,58 @@
-from ftw.builder import Builder
-from ftw.builder import create
 from ftw.testbrowser import browsing
 from ftw.testbrowser.pages import plone
-from opengever.testing import FunctionalTestCase
+from opengever.testing import IntegrationTestCase
 from plone.app.testing import TEST_USER_NAME
 from plone.app.testing import TEST_USER_PASSWORD
 import transaction
 import urllib
 
 
-class TestRequireLoginScript(FunctionalTestCase):
-
-    def setUp(self):
-        super(TestRequireLoginScript, self).setUp()
-
-        self.repo, self.repo_folder = create(Builder('repository_tree'))
-        self.dossier = create(Builder('dossier').within(self.repo_folder))
-        self.document = create(
-            Builder('document')
-            .within(self.dossier)
-            .titled(u'Document1')
-            .with_dummy_content())
+class TestRequireLoginScript(IntegrationTestCase):
 
     @browsing
     def test_require_login_redirects_to_came_from_if_already_logged_in(self, browser):
-        browser.login().open(
-            view='require_login?came_from={}'.format(
-                urllib.quote(self.document.absolute_url())))
-        self.assertEqual(self.document.absolute_url(),
-                         browser.url.split('?')[0])
+        self.login(self.regular_user, browser=browser)
+
+        browser.open(self.portal, view='require_login?came_from={}'.format(
+            urllib.quote(self.document.absolute_url())))
+
+        self.assertEqual(self.document.absolute_url(), browser.url.split('?')[0])
 
     @browsing
     def test_require_login_displays_login_form_and_redirecs_upon_login(self, browser):
+        self.login(self.regular_user, browser=browser)
+        document_url = self.document.absolute_url()
+        self.logout(browser=browser)
+
         # Disable the `cookie only over https flag, activated
         # by `opengever.core.hooks.installed`.
         self.portal.acl_users.session.secure = False
-        transaction.commit()
 
         with browser.expect_unauthorized():
             browser.open(
                 view='require_login?came_from={}'.format(
-                    urllib.quote(self.document.absolute_url())))
+                    urllib.quote(document_url)))
+
         self.assertTrue(
             browser.url.startswith('http://nohost/plone/require_login'),
             'Unexpected URL {}'.format(browser.url))
 
-        browser.fill({'Login Name': TEST_USER_NAME,
+        browser.fill({'Login Name': self.regular_user.id,
                       'Password': TEST_USER_PASSWORD}).submit()
-        self.assertEqual(self.document.absolute_url(), browser.url)
+        self.assertEqual(document_url, browser.url)
 
     @browsing
     def test_unauthorized_visible_when_raised_in_traversal(self, browser):
+        self.login(self.regular_user, browser=browser)
+
         with browser.expect_unauthorized():
-            browser.login().open(view='test-traversal-unauthorized')
+            browser.open(view='test-traversal-unauthorized')
 
     @browsing
     def test_unauthorized_visible_when_raised_in_publishing(self, browser):
+        self.login(self.regular_user, browser=browser)
         with browser.expect_unauthorized():
-            browser.login().open(view='test-publishing-unauthorized')
+            browser.open(view='test-publishing-unauthorized')
         self.assertEquals('Insufficient Privileges', plone.first_heading())
 
     @browsing
@@ -66,6 +61,9 @@ class TestRequireLoginScript(FunctionalTestCase):
         # page to appear when logged in.
         url = ('{0}/acl_users/credentials_cookie_auth/require_login'
                '?came_from={0}/does/not/exist').format(self.portal.absolute_url())
+
+        self.login(self.regular_user, browser=browser)
         with browser.expect_unauthorized():
-            browser.login().open(url)
+            browser.open(url)
+
         self.assertEquals('Insufficient Privileges', plone.first_heading())
