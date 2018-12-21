e4c5e1a054a4a5a78819e656a6e06c9e05024353
Philippe Gross <philippe.gross@4teamwork.ch>
2018-12-21 11:14:20 +0100

    Convert trashed index test to integration layer.

--- opengever/trash/tests/test_catalog.py
+++ opengever/trash/tests/test_catalog.py
@@ -1,12 +1,8 @@
-from opengever.testing import FunctionalTestCase
+from opengever.testing import IntegrationTestCase
 from plone import api
 
 
-class TestCatalog(FunctionalTestCase):
-
-    def setUp(self):
-        super(TestCatalog, self).setUp()
-        self.catalog = api.portal.get_tool('portal_catalog')
+class TestCatalog(IntegrationTestCase):
 
     def test_trashed_index_registered(self):
-        self.assertIn('trashed', self.catalog.indexes())
+        self.assertIn('trashed', api.portal.get_tool('portal_catalog').indexes())
