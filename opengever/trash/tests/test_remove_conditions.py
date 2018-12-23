22b7b4ef7aaba24230793401a8e310d0343c84c7
Philippe Gross <philippe.gross@4teamwork.ch>
2018-12-24 00:44:33 +0100

    Convert and improved RemoveConditions test

--- opengever/trash/tests/test_remove_conditions.py
+++ opengever/trash/tests/test_remove_conditions.py
@@ -1,29 +1,26 @@
 from ftw.builder import Builder
 from ftw.builder import create
+from ftw.testbrowser import browsing
 from opengever.base.security import elevated_privileges
-from opengever.testing import FunctionalTestCase
+from opengever.testing import IntegrationTestCase
 from opengever.trash.remover import RemoveConditionsChecker
+from opengever.trash.remover import Remover
 from plone import api
-from zope.event import notify
 from zope.i18n import translate
-from zope.lifecycleevent import ObjectModifiedEvent
 
 
-class TestRemoveConditionsChecker(FunctionalTestCase):
-
-    def setUp(self):
-        super(TestRemoveConditionsChecker, self).setUp()
-        self.grant('Administrator')
+class TestRemoveConditionsChecker(IntegrationTestCase):
 
     def assert_error_messages(self, expected, msgs):
         self.assertEquals(
             expected, [translate(msg) for msg in msgs])
 
     def test_document_must_not_have_relations(self):
-        document_a = create(Builder('document'))
+        self.login(self.manager)
+
         document_b = create(Builder('document')
-                            .trashed()
-                            .relate_to([document_a]))
+                            .relate_to([self.document])
+                            .trashed())
 
         checker = RemoveConditionsChecker(document_b)
 
@@ -31,57 +28,65 @@ class TestRemoveConditionsChecker(FunctionalTestCase):
         self.assert_error_messages(
             [u'The document contains relations.'], checker.error_msg)
 
-    def test_document_must_not_have_backreferences(self):
-        document_a = create(Builder('document')
-                            .trashed())
-        document_b = create(Builder('document')
-                            .titled('Doc b')
-                            .relate_to([document_a]))
+    @browsing
+    def test_document_must_not_have_backreferences(self, browser):
+        self.login(self.manager, browser=browser)
+        self.trash_documents(self.subdocument)
 
-        notify(ObjectModifiedEvent(document_b))
+        browser.open(self.taskdocument, view='edit')
+        browser.fill({'Related Documents': [self.subdocument]})
+        browser.find('Save').click()
 
-        checker = RemoveConditionsChecker(document_a)
+        checker = RemoveConditionsChecker(self.subdocument)
 
         self.assertFalse(checker.removal_allowed())
         self.assert_error_messages(
-            [u'The document is referred by the document(s) <a href=http://nohost/plone/document-2>Doc b</a>.'],
+            [u'The document is referred by the document(s) <a href={}>{}</a>.'.format(
+                self.taskdocument.absolute_url(), self.taskdocument.title)],
             checker.error_msg)
 
-    def test_check_does_not_fail_if_document_has_no_longer_existent_backrefs(self):
-        document_a = create(Builder('document')
-                            .trashed())
-        document_b = create(Builder('document')
-                            .titled('Doc b')
-                            .relate_to([document_a]))
+    @browsing
+    def test_check_does_not_fail_if_document_has_no_longer_existent_backrefs(self, browser):
+        self.login(self.manager, browser=browser)
 
-        checker = RemoveConditionsChecker(document_a)
+        browser.open(self.taskdocument, view='edit')
+        browser.fill({'Related Documents': [self.subdocument]})
+        browser.find('Save').click()
+
+        self.trash_documents(self.subdocument)
+
+        checker = RemoveConditionsChecker(self.subdocument)
         self.assertFalse(checker.removal_allowed())
 
         with elevated_privileges():
-            api.content.delete(obj=document_b)
+            api.content.delete(obj=self.taskdocument)
 
-        checker = RemoveConditionsChecker(document_a)
+        checker = RemoveConditionsChecker(self.subdocument)
         self.assertTrue(checker.removal_allowed())
 
     def test_document_must_be_trashed(self):
-        document = create(Builder('document'))
-        checker = RemoveConditionsChecker(document)
+        self.login(self.manager)
+
+        checker = RemoveConditionsChecker(self.empty_document)
         self.assertFalse(checker.removal_allowed())
         self.assert_error_messages(
-            [u'The document is not trashed.'],
-            checker.error_msg)
+            [u'The document is not trashed.'], checker.error_msg)
 
     def test_document_must_not_already_be_removed(self):
-        document = create(Builder('document')
-                          .trashed()
-                          .removed())
-        checker = RemoveConditionsChecker(document)
+        self.login(self.manager)
+
+        self.trash_documents(self.empty_document)
+        Remover([self.empty_document]).remove()
+
+        checker = RemoveConditionsChecker(self.empty_document)
         self.assertFalse(checker.removal_allowed())
         self.assert_error_messages(
             [u'The document is already removed.'],
             checker.error_msg)
 
     def test_removal_allowed(self):
-        document = create(Builder('document').trashed())
-        checker = RemoveConditionsChecker(document)
+        self.login(self.manager)
+        self.trash_documents(self.empty_document)
+
+        checker = RemoveConditionsChecker(self.empty_document)
         self.assertTrue(checker.removal_allowed())
