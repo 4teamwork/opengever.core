59208050567e9d98ce0269d6da0b6fd87da1854c
Joni Orponen <j.orponen@4teamwork.ch>
2019-01-15 18:25:49 +0100

    Unify group member listing HTTP behaviour with team member listing.

--- opengever/base/browser/list_groupmembers.py
+++ opengever/base/browser/list_groupmembers.py
@@ -2,17 +2,27 @@ from opengever.ogds.base.actor import Actor
 from opengever.ogds.base.utils import ogds_service
 from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zExceptions import BadRequest
 
 
 class ListGroupMembers(BrowserView):
+    """Fetch group members by group id."""
+
     template = ViewPageTemplateFile("list_groupmembers.pt")
+
+    def __init__(self, context, request):
+        super(ListGroupMembers, self).__init__(context, request)
+
+        self.active = None
+        self.members = None
+        self.group_id = None
 
     def __call__(self):
         self.group_id = self.context.REQUEST.get('group', None)
         self.members = []
 
         if not self.group_id:
-            return 'no group id'
+            BadRequest('no group id')
 
         group = ogds_service().fetch_group(self.group_id)
         self.active = getattr(group, 'active', None)
