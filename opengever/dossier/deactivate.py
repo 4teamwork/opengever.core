3e6b606392a54c2422e9ffa29afa186a9aee7310
Joni Orponen <j.orponen@4teamwork.ch>
2019-02-06 22:05:00 +0100

    Prevent deactivating dossiers with undeactivatable subdossiers.

--- opengever/dossier/deactivate.py
+++ opengever/dossier/deactivate.py
@@ -1,4 +1,5 @@
 from datetime import date
+from opengever.base.security import elevated_privileges
 from opengever.dossier import _
 from opengever.dossier.behaviors.dossier import IDossier
 from plone import api
@@ -6,9 +7,15 @@ from Products.Five.browser import BrowserView
 
 
 class DossierDeactivateView(BrowserView):
-    """ Recursively deactivate the dossier and his subdossiers.
-    If some subdossiers are already resolved we return a status err msg.
-    If some subdossiers are already deactivated we ignore them."""
+    """Recursively deactivate the dossier and its subdossiers.
+
+    Preconditions:
+    * All contained documents are checked in.
+    * Some subdossiers are already resolved.
+    * Some subdossiers cannot be deactivated by the user.
+    * The user has no access to some of the subdossiers.
+    * The dossier contains active tasks.
+    """
 
     def __call__(self):
         if not self.check_preconditions():
@@ -54,15 +61,56 @@ class DossierDeactivateView(BrowserView):
                   "proposals."), self.request, type='error')
             satisfied = False
 
-        # check for resolved subdossiers
-        for subdossier in self.context.get_subdossiers():
-            state = api.content.get_state(obj=subdossier.getObject())
-            if state == 'dossier-state-resolved':
-                msg = _(u"The Dossier can't be deactivated, the subdossier "
-                       "${dossier} is already resolved",
-                       mapping=dict(dossier=subdossier.Title.decode('utf-8')))
-                api.portal.show_message(msg, self.request, type='error')
+        # Check for subdossiers the user cannot deactivate
+        for subdossier in self.context.get_subdossiers(unrestricted=True):
+            with elevated_privileges():
+                subdossier = subdossier.getObject()
 
+            wftool = api.portal.get_tool('portal_workflow')
+            user_can_deactivate = any(
+                transition['name'] == 'dossier-transition-deactivate'
+                for transition in wftool.getTransitionsFor(subdossier)
+            )
+            state = api.content.get_state(subdossier)
+
+            # A subdossier already being inactive is not a blocker
+            if not user_can_deactivate and state != 'dossier-state-inactive':
+                if api.user.has_permission('View', obj=subdossier):
+                    subdossier_title = subdossier.title_or_id()
+                    # We cannot deactivate if some of the subdossiers are already resolved
+                    if state == 'dossier-state-resolved':
+                        msg = _(
+                            u"The Dossier can't be deactivated, the subdossier ${dossier} is already resolved.",
+                            mapping=dict(dossier=subdossier_title),
+                        )
+                    else:
+                        # The deactvation of this subdossier is most likely not allowed by role
+                        msg = _(
+                            u"The Dossier ${dossier} can't be deactivated by the user.",
+                            mapping=dict(dossier=subdossier_title),
+                        )
+                # Inform the user which of the parents contains the blocking subdossier they cannot see
+                else:
+                    # We are guaranteed to hit a dossier the user can view
+                    parent_title = ''
+                    parents = [subdossier.get_parent_dossier()]
+
+                    # Traverse up the parents until we hit a non-dossier
+                    while parents[-1].get_parent_dossier():
+                        parents.append(parents[-1].get_parent_dossier())
+
+                    # Grab the title of the first parent we do have access to
+                    for parent in parents:
+                        if api.user.has_permission('View', obj=parent):
+                            parent_title = parent.title_or_id()
+                            break
+
+                    msg = _(
+                        u"The Dossier ${dossier} contains a subdossier which can't be deactivated by the user.",
+                        mapping=dict(dossier=parent_title),
+                    )
+
+                api.portal.show_message(msg, self.request, type='error')
                 satisfied = False
 
         if self.context.has_active_tasks():
