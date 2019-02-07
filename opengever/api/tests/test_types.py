ce2dfd6645d138c3bc7be33491c140f9dc771ced
Elio Schmutz <e.schmutz@4teamwork.ch>
2019-02-07 13:37:27 +0100

    Update test to use a black list instead a whitelist

--- opengever/api/tests/test_types.py
+++ opengever/api/tests/test_types.py
@@ -1,47 +1,33 @@
 from ftw.testbrowser import browsing
 from opengever.testing import IntegrationTestCase
+from plone import api
 
-GEVER_TYPES = ['ftw.mail.mail',
-               'opengever.contact.contact',
-               'opengever.contact.contactfolder',
-               'opengever.disposition.disposition',
-               'opengever.document.document',
-               'opengever.dossier.businesscasedossier',
-               'opengever.dossier.dossiertemplate',
-               'opengever.dossier.templatefolder',
-               'opengever.inbox.container',
-               'opengever.inbox.forwarding',
-               'opengever.inbox.inbox',
-               'opengever.inbox.yearfolder',
-               'opengever.meeting.committee',
-               'opengever.meeting.committeecontainer',
-               'opengever.meeting.meetingdossier',
-               'opengever.meeting.meetingtemplate',
-               'opengever.meeting.paragraphtemplate',
-               'opengever.meeting.proposal',
-               'opengever.meeting.proposaltemplate',
-               'opengever.meeting.sablontemplate',
-               'opengever.meeting.submittedproposal',
-               'opengever.private.dossier',
-               'opengever.private.folder',
-               'opengever.private.root',
-               'opengever.repository.repositoryfolder',
-               'opengever.repository.repositoryroot',
-               'opengever.task.task',
-               'opengever.tasktemplates.tasktemplate',
-               'opengever.tasktemplates.tasktemplatefolder',
-               'opengever.workspace.folder',
-               'opengever.workspace.root',
-               'opengever.workspace.workspace',
-               'Plone%20Site']
+
+BLACKLIST = []
 
 
 class TestPloneRestAPI(IntegrationTestCase):
 
+    def whitelisted_portal_type_ids(self):
+        types_tool = api.portal.get_tool('portal_types')
+        portal_type_ids = [typeinfo.id for typeinfo in types_tool.listTypeInfo()]
+        return set(portal_type_ids) - set(BLACKLIST)
+
     @browsing
     def test_rest_api(self, browser):
+        broken_types = []
         self.login(self.regular_user, browser)
-        for content_type in GEVER_TYPES:
-            browser.open(self.portal.absolute_url() + '/@types/{}'.format(content_type),
-                         method='GET', headers={'Accept': 'application/json'})
-            self.assertEqual(200, browser.status_code)
+        for portal_type_id in self.whitelisted_portal_type_ids():
+            try:
+                # Performing the request within a try-except block will ensure
+                # that the traceback will be logged and that we can continue
+                # testing the remaining types.
+                browser.open(
+                    '{}/@types/{}'.format(self.portal.absolute_url(), portal_type_id),
+                    method='GET', headers={'Accept': 'application/json'})
+            except:
+                broken_types.append(portal_type_id)
+
+        self.assertEqual(
+            [], broken_types,
+            "There was an error on requesting these types")
