7c88e73223b8950d409f69199026501b6885e306
Niklaus Johner <niklaus.johner@4teamwork.ch>
2019-04-01 16:00:37 +0200

    Add webaction edit form.

--- opengever/webactions/tests/test_webaction_forms.py
+++ opengever/webactions/tests/test_webaction_forms.py
@@ -1,5 +1,10 @@
+from datetime import datetime
+from ftw.builder import Builder
+from ftw.builder import create
 from ftw.testbrowser import browsing
 from ftw.testbrowser.pages.statusmessages import error_messages
+from ftw.testbrowser.pages.statusmessages import info_messages
+from ftw.testing import freeze
 from opengever.testing import IntegrationTestCase
 from opengever.webactions.storage import get_storage
 from plone import api
@@ -113,3 +118,109 @@ class TestWebactionsAddForm(IntegrationTestCase):
         self.assertEqual("Display location 'title-buttons' requires an icon.",
                          errors.first.text)
         self.assertEqual(0, len(storage.list()))
+
+
+class TestWebactionsEditForm(IntegrationTestCase):
+
+    def setUp(self):
+        super(TestWebactionsEditForm, self).setUp()
+
+        with self.login(self.webaction_manager):
+            with freeze(datetime(2018, 4, 20)):
+                create(Builder('webaction')
+                       .titled(u'Action 1')
+                       .having(order=5))
+
+            with freeze(datetime(2018, 4, 21)):
+                create(Builder('webaction')
+                       .titled(u'Action 2')
+                       .having(order=10))
+
+    def get_edit_url(self, action_id=0):
+        return "/".join([api.portal.getSite().absolute_url(),
+                         "manage-webactions-edit?action_id={}".format(action_id)])
+
+    @browsing
+    def test_edit_form_available_for_managers(self, browser):
+        self.login(self.regular_user, browser)
+
+        with browser.expect_unauthorized():
+            browser.open(self.get_edit_url())
+
+        self.login(self.manager, browser)
+        browser.open(self.get_edit_url())
+
+    @browsing
+    def test_edit_form_available_for_webaction_managers(self, browser):
+        self.login(self.regular_user, browser)
+
+        with browser.expect_unauthorized():
+            browser.open(self.get_edit_url())
+
+        self.login(self.webaction_manager, browser)
+        browser.open(self.get_edit_url())
+
+    @browsing
+    def test_no_modifications(self, browser):
+        self.login(self.webaction_manager, browser)
+
+        browser.open(self.get_edit_url())
+        browser.click_on("Save")
+
+        webaction = get_storage().list()[0]
+        self.assertEqual(['No changes were applied.'], info_messages())
+        self.assertEqual(datetime(2018, 4, 20), webaction.get("modified"))
+
+    @browsing
+    def test_only_saves_modified_fields(self, browser):
+        self.login(self.webaction_manager, browser)
+
+        webaction_before = get_storage().list()[0]
+        with freeze(datetime(2018, 4, 22)):
+            browser.open(self.get_edit_url())
+            browser.fill({"Title": "changed title"})
+            browser.click_on("Save")
+
+        webaction = get_storage().list()[0]
+        self.assertEqual(['Data successfully updated.'], info_messages())
+        self.assertNotEqual(webaction_before.pop("title"), webaction.get("title"))
+        self.assertEqual("changed title", webaction.pop("title"))
+        self.assertNotEqual(webaction_before.pop("modified"), webaction.get("modified"))
+        self.assertEqual(datetime(2018, 4, 22), webaction.pop("modified"))
+        self.assertDictEqual(webaction_before, webaction)
+
+    @browsing
+    def test_validates_individual_fields(self, browser):
+        self.login(self.webaction_manager, browser)
+        browser.open(self.get_edit_url())
+
+        browser.fill({"Target URL": "not an url"})
+        browser.click_on("Save")
+
+        self.assertEqual(['There were some errors.'], error_messages())
+        errors = browser.css("div.error")
+        # The error class is added on the field itself and on the div
+        # containing the error text
+        self.assertEqual(2, len(errors))
+        self.assertEqual('formfield-form-widgets-target_url', errors[0].get("id"))
+        self.assertEqual('The specified URI is not valid.', errors[1].text)
+
+        webaction = get_storage().list()[0]
+        self.assertEqual(datetime(2018, 4, 20), webaction.pop("modified"))
+
+    @browsing
+    def test_missing_required_icon_invariant(self, browser):
+        self.login(self.webaction_manager, browser)
+        browser.open(self.get_edit_url())
+
+        browser.fill({"Display Location": "title-buttons"})
+        browser.click_on("Save")
+
+        self.assertEqual(['There were some errors.'], error_messages())
+        errors = browser.css("div.error")
+        self.assertEqual(1, len(errors))
+        self.assertEqual("Display location 'title-buttons' requires an icon.",
+                         errors.first.text)
+
+        webaction = get_storage().list()[0]
+        self.assertEqual(datetime(2018, 4, 20), webaction.pop("modified"))
