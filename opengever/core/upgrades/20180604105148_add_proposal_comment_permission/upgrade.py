77ff4fe708b6ed26fc6baa48e10f2029d2e89731
Philippe Gross <philippe.gross@4teamwork.ch>
2019-04-25 07:48:20 +0200

    Merge upgradesteps AddNewDossierManagerRole and AddProposalCommentPermission
    to avoid reindexing workflow_security twice for dossiers.

--- opengever/core/upgrades/20180604105148_add_proposal_comment_permission/upgrade.py
+++ opengever/core/upgrades/20180604105148_add_proposal_comment_permission/upgrade.py
@@ -5,6 +5,9 @@ class AddProposalCommentPermission(UpgradeStep):
     """Add proposal comment permission.
     """
 
+    # Reindexes the security for all dossiers also for the
+    # `20171026172342_add_new_dossier_manager_role` upgradestep.
+
     def __call__(self):
         self.install_upgrade_profile()
         self.update_workflow_security(
