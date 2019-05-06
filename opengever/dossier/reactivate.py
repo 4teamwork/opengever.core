cc687dcf0bb4d1271c296818572f57cae7bad94e
Niklaus Johner <niklaus.johner@4teamwork.ch>
2019-05-06 17:17:32 +0200

    Refactor DossierReactivator.
    We change the precondition check to return the precondition
    violations in prevision of using the reactivator for the
    transition via the RESTAPI.

--- opengever/dossier/reactivate.py
+++ opengever/dossier/reactivate.py
@@ -1,8 +1,11 @@
 from opengever.dossier import _
 from opengever.dossier.base import DOSSIER_STATES_OPEN
 from opengever.dossier.behaviors.dossier import IDossier
+from plone import api
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
+
+MSG_SUBDOSSIER = _("It isn't possible to reactivate a sub dossier.")
 
 
 class Reactivator(object):
@@ -11,15 +14,16 @@ class Reactivator(object):
         self.context = context
         self.wft = getToolByName(self.context, 'portal_workflow')
 
-    def is_reactivate_possible(self):
+    def get_precondition_violations(self):
+        errors = []
         parent = self.context.get_parent_dossier()
         if parent:
             if self.wft.getInfoFor(parent, 'review_state') not in DOSSIER_STATES_OPEN:
-                return False
-        return True
+                errors.append(MSG_SUBDOSSIER)
+        return errors
 
     def reactivate(self):
-        if not self.is_reactivate_possible():
+        if self.get_precondition_violations():
             raise TypeError
         self._recursive_reactivate(self.context)
 
@@ -41,18 +45,23 @@ class Reactivator(object):
 class DossierReactivateView(BrowserView):
 
     def __call__(self):
-        ptool = getToolByName(self, 'plone_utils')
-
         reactivator = Reactivator(self.context)
 
         # check preconditions
-        if reactivator.is_reactivate_possible():
-            reactivator.reactivate()
-            ptool.addPortalMessage(_('Dossiers successfully reactivated.'),
-                                   type="info")
-            self.request.RESPONSE.redirect(self.context.absolute_url())
-        else:
-            ptool.addPortalMessage(
-                _("It isn't possible to reactivate a sub dossier."),
-                type="warning")
-            self.request.RESPONSE.redirect(self.context.absolute_url())
+        errors = reactivator.get_precondition_violations()
+        if errors:
+            return self.show_errors(errors)
+
+        reactivator.reactivate()
+        api.portal.show_message(message=_('Dossiers successfully reactivated.'),
+                                request=self.request, type='info')
+        return self.redirect()
+
+    def redirect(self):
+        return self.request.RESPONSE.redirect(self.context.absolute_url())
+
+    def show_errors(self, errors):
+        for msg in errors:
+            api.portal.show_message(
+                message=msg, request=self.request, type='warning')
+        return self.redirect()
