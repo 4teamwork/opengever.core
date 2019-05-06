9578cfead1f3956d8e6b1c8da5e7dabd3ca036f3
Niklaus Johner <niklaus.johner@4teamwork.ch>
2019-05-06 16:08:40 +0200

    Refactor dossier activate view.

--- opengever/dossier/activate.py
+++ opengever/dossier/activate.py
@@ -1,29 +1,41 @@
 from opengever.dossier import _
 from plone import api
 from Products.Five.browser import BrowserView
-from Products.statusmessages.interfaces import IStatusMessage
+
+
+MAIN_DOSSIER_ACTIVE = _("This subdossier can't be activated,"
+                        "because the main dossiers is inactive")
 
 
 class DossierActivateView(BrowserView):
     """View which activates the dossier including its subdossiers."""
 
     def __call__(self):
-        if self.check_preconditions():
-            self.activate()
+        errors = self.check_preconditions()
+        if errors:
+            self.show_errors(errors)
+            return self.redirect()
 
+        self.activate()
+        api.portal.show_message(_("The Dossier has been activated"),
+                                self.request, type='info')
+        return self.redirect()
+
+    def show_errors(self, errors):
+        for msg in errors:
+            api.portal.show_message(
+                message=msg, request=self.request, type='error')
+
+    def redirect(self):
         return self.request.RESPONSE.redirect(self.context.absolute_url())
 
     def check_preconditions(self):
-        satisfied = True
+        errors = []
         if self.context.is_subdossier():
             state = api.content.get_state(self.context.get_parent_dossier())
             if state == 'dossier-state-inactive':
-                satisfied = False
-                IStatusMessage(self.request).add(
-                    _("This subdossier can't be activated,"
-                      "because the main dossiers is inactive"), type='error')
-
-        return satisfied
+                errors.append(MAIN_DOSSIER_ACTIVE)
+        return errors
 
     def activate(self):
         # subdossiers
@@ -32,6 +44,3 @@ class DossierActivateView(BrowserView):
 
         # main dossier
         self.context.activate()
-
-        IStatusMessage(self.request).add(_("The Dossier has been activated"),
-                                         type='info')
