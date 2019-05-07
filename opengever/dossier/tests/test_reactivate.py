2d99280c3510936e12ef4766366340ee758524b1
Niklaus Johner <niklaus.johner@4teamwork.ch>
2019-05-07 09:20:11 +0200

    Refactor reactivate dossier tests and port to integration layer.

--- opengever/dossier/tests/test_reactivate.py
+++ opengever/dossier/tests/test_reactivate.py
@@ -2,86 +2,97 @@ from datetime import date
 from ftw.builder import Builder
 from ftw.builder import create
 from ftw.testbrowser import browsing
-from ftw.testbrowser.pages.statusmessages import info_messages
+from ftw.testbrowser.pages import statusmessages
 from ftw.testbrowser.pages.statusmessages import error_messages
+from ftw.testbrowser.pages.statusmessages import info_messages
 from opengever.dossier.behaviors.dossier import IDossier
-from opengever.testing import FunctionalTestCase
-from plone import api
+from opengever.testing import IntegrationTestCase
 from plone.protect import createToken
 
 
-class TestReactivating(FunctionalTestCase):
+class TestReactivating(IntegrationTestCase):
 
     def setUp(self):
         super(TestReactivating, self).setUp()
-        self.grant('Reader', 'Contributor', 'Editor', 'Reviewer', 'Publisher')
+        with self.login(self.regular_user):
+            self.set_workflow_state('dossier-state-resolved',
+                                    self.resolvable_dossier,
+                                    self.resolvable_subdossier)
+
+    def reactivate(self, dossier, browser):
+        return browser.open(dossier,
+                            view='transition-reactivate',
+                            data={'_authenticator': createToken()})
+
+    def assert_errors(self, dossier, browser, error_msgs):
+        self.assertEquals(dossier.absolute_url(), browser.url)
+        self.assertEquals(error_msgs, error_messages())
+
+    def assert_success(self, dossier, browser, info_msgs=None):
+        self.assertEquals(dossier.absolute_url(), browser.url)
+        statusmessages.assert_no_error_messages()
+        self.assertEquals(info_msgs, info_messages())
 
     @browsing
     def test_reactivating_a_resolved_dossier_succesfully(self, browser):
-        dossier = create(Builder('dossier')
-                         .in_state('dossier-state-resolved'))
+        self.login(self.secretariat_user, browser)
 
-        browser.login().open(dossier)
-        browser.click_on('dossier-transition-reactivate')
+        self.reactivate(self.resolvable_dossier, browser)
 
-        self.assertEquals('dossier-state-active',
-                          api.content.get_state(dossier))
-        self.assertEquals(['Dossiers successfully reactivated.'],
-                          info_messages())
+        self.assert_workflow_state('dossier-state-active', self.resolvable_dossier)
+        self.assert_success(self.resolvable_dossier, browser,
+                            ['Dossiers successfully reactivated.'])
 
     @browsing
     def test_reactivating_a_main_dossier_reactivates_subdossiers_recursively(self, browser):
-        dossier = create(Builder('dossier')
-                         .in_state('dossier-state-resolved'))
-        sub = create(Builder('dossier')
-                     .within(dossier)
-                     .in_state('dossier-state-resolved'))
+        self.login(self.secretariat_user, browser)
         subsub = create(Builder('dossier')
-                        .within(sub)
+                        .within(self.resolvable_subdossier)
                         .in_state('dossier-state-resolved'))
 
-        browser.login().open(dossier, view=u'transition-reactivate',
-                             data={'_authenticator': createToken()})
-        self.assertEquals('dossier-state-active',
-                          api.content.get_state(dossier))
-        self.assertEquals('dossier-state-active',
-                          api.content.get_state(sub))
-        self.assertEquals('dossier-state-active',
-                          api.content.get_state(subsub))
+        self.reactivate(self.resolvable_dossier, browser)
+
+        self.assert_workflow_state('dossier-state-active', self.resolvable_dossier)
+        self.assert_workflow_state('dossier-state-active', self.resolvable_subdossier)
+        self.assert_workflow_state('dossier-state-active', subsub)
 
     @browsing
     def test_reactivating_a_subdossier_of_a_resolved_dossier_is_not_possible(self, browser):
-        dossier = create(Builder('dossier')
-                         .in_state('dossier-state-resolved'))
-        sub = create(Builder('dossier')
-                     .within(dossier)
-                     .in_state('dossier-state-resolved'))
+        self.login(self.secretariat_user, browser)
 
-        browser.login().open(sub, view=u'transition-reactivate',
-                             data={'_authenticator': createToken()})
+        self.reactivate(self.resolvable_subdossier, browser)
 
-        self.assertEquals(
-            ["It isn't possible to reactivate a sub dossier."],
-            error_messages())
-        self.assertEquals('dossier-state-resolved', api.content.get_state(sub))
+        self.assert_errors(self.resolvable_subdossier, browser,
+                           ["It isn't possible to reactivate a sub dossier."])
+        self.assert_workflow_state('dossier-state-resolved',
+                                   self.resolvable_subdossier)
 
     @browsing
     def test_resets_end_dates_recursively(self, browser):
-        dossier = create(Builder('dossier')
-                         .having(end=date(2013, 2, 21))
-                         .in_state('dossier-state-resolved'))
-        sub = create(Builder('dossier')
-                     .within(dossier)
-                     .having(end=date(2013, 2, 21))
-                     .in_state('dossier-state-resolved'))
+        end_date = date(2013, 2, 21)
+        end_date_index = self.dateindex_value_from_datetime(end_date)
+
+        self.login(self.secretariat_user, browser)
+        IDossier(self.resolvable_dossier).end = end_date
+        IDossier(self.resolvable_subdossier).end = end_date
+        self.resolvable_dossier.reindexObject(idxs=['end'])
+        self.resolvable_subdossier.reindexObject(idxs=['end'])
         subsub = create(Builder('dossier')
-                        .within(sub)
-                        .having(end=date(2013, 2, 21))
+                        .within(self.resolvable_subdossier)
+                        .having(end=end_date)
                         .in_state('dossier-state-resolved'))
 
-        browser.login().open(dossier, view=u'transition-reactivate',
-                             data={'_authenticator': createToken()})
+        self.assert_index_value(end_date_index, 'end', self.resolvable_dossier,
+                                self.resolvable_subdossier, subsub)
+        self.assert_metadata_value(end_date, 'end', self.resolvable_dossier,
+                                   self.resolvable_subdossier, subsub)
 
-        self.assertIsNone(IDossier(dossier).end)
-        self.assertIsNone(IDossier(sub).end)
+        self.reactivate(self.resolvable_dossier, browser)
+
+        self.assert_index_value('', 'end', self.resolvable_dossier,
+                                self.resolvable_subdossier, subsub)
+        self.assert_metadata_value(None, 'end', self.resolvable_dossier,
+                                   self.resolvable_subdossier, subsub)
+        self.assertIsNone(IDossier(self.resolvable_dossier).end)
+        self.assertIsNone(IDossier(self.resolvable_subdossier).end)
         self.assertIsNone(IDossier(subsub).end)
