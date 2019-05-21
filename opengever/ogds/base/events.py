44cdde52e6e50d9265963feb3af1bfb969ae7351
Lukas Graf <lukas.graf@4teamwork.ch>
2019-05-21 14:44:26 +0200

    WIP: Put SQL in readonly mode when ZODB connection is readonly.

--- opengever/ogds/base/events.py
+++ opengever/ogds/base/events.py
@@ -1,6 +1,7 @@
 from sqlalchemy import exc
 from sqlalchemy.event import listen
 from sqlalchemy.pool import Pool
+from zope.component.hooks import getSite
 
 
 def alter_session_on_connect(dbapi_connection, connection_record):
@@ -18,6 +19,11 @@ def setup_isolation_level_on_connect(dbapi_connection, connection_record):
     # using-savepoints-in-python-sqlite3
 
     dbapi_connection.isolation_level = None
+
+
+def set_sql_connection_to_readonly_on_begin(conn):
+    # Only tested for PostgreSQL
+    conn.execute("SET TRANSACTION READ ONLY")
 
 
 def make_like_case_sensitive(dbapi_connection, connection_record):
@@ -46,6 +52,12 @@ def setup_engine_options(event):
         - transaction isolation level for SQLite engines
     """
     engine = event.engine
+
+    site = getSite()
+    conn = site._p_jar
+    if conn.isReadOnly():
+        listen(engine, 'begin', set_sql_connection_to_readonly_on_begin)
+
     if engine.name == 'oracle':
         listen(engine, 'connect', alter_session_on_connect)
 
