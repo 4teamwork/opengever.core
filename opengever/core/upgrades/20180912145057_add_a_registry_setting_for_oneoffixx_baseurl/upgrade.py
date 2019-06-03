f188b43aea9884c2b803b5fd1b1411f237ab234c
David Erni <david.erni@4teamwork.ch>
2019-06-03 13:42:33 +0200

    Merge pull request #5698 from 4teamwork/deif-drop-defect-upgrade
    Drop no longer working oneoffixx upgrade.

--- opengever/core/upgrades/20180912145057_add_a_registry_setting_for_oneoffixx_baseurl/upgrade.py
+++
@@ -1,9 +0,0 @@
-from ftw.upgrade import UpgradeStep
-
-
-class AddARegistrySettingForOneoffixxBaseurl(UpgradeStep):
-    """Add a registry setting for Oneoffixx baseurl.
-    """
-
-    def __call__(self):
-        self.install_upgrade_profile()
