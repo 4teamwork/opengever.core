d003e8884112f8ee12f2cafefff40e30327aa0e8
Elio Schmutz <e.schmutz@4teamwork.ch>
2019-07-08 17:23:51 +0200

    Extend workspace POST endpoint to directly assign a todo to a todolist

--- opengever/api/tests/test_workspace.py
+++ opengever/api/tests/test_workspace.py
@@ -1,5 +1,7 @@
 from ftw.testbrowser import browsing
 from opengever.testing import IntegrationTestCase
+from opengever.workspace.todos.storage import ToDoListStorage
+import json
 
 
 class TestWorkspaceSerializer(IntegrationTestCase):
@@ -17,3 +19,47 @@ class TestWorkspaceSerializer(IntegrationTestCase):
             self.workspace, headers={'Accept': 'application/json'}).json
 
         self.assertTrue(response.get(u'can_manage_participants'))
+
+
+class TestWorkspacePost(IntegrationTestCase):
+
+    @browsing
+    def test_added_todo_will_not_be_added_to_a_todo_list_by_default(self, browser):
+        self.login(self.workspace_owner, browser)
+        storage = ToDoListStorage(self.workspace)
+
+        browser.open(
+            self.workspace.absolute_url(),
+            method='POST',
+            headers=self.api_headers,
+            data=json.dumps({
+                '@type': 'opengever.workspace.todo',
+                'title': u'Important todo'}),
+        )
+
+        self.assertEqual([], storage.list())
+
+    @browsing
+    def test_add_todo_to_a_todo_list(self, browser):
+        self.login(self.workspace_owner, browser)
+        storage = ToDoListStorage(self.workspace)
+        todo_list = storage.create_todo_list('01 - General')
+
+        todo = browser.open(
+            self.workspace.absolute_url(),
+            method='POST',
+            headers=self.api_headers,
+            data=json.dumps({
+                '@type': 'opengever.workspace.todo',
+                'title': u'Important todo',
+                'todo_list_id': todo_list.get('list_id')}),
+        ).json
+
+        self.assertEqual(
+            [
+                {
+                    'todo_uids': [todo.get('UID')],
+                    'list_id': todo_list.get('list_id'),
+                    'title': '01 - General'
+                }
+            ], storage.list())
--- opengever/api/workspace.py
+++ opengever/api/workspace.py
@@ -1,7 +1,10 @@
 from opengever.api.serializer import GeverSerializeFolderToJson
 from opengever.workspace.interfaces import IWorkspace
 from opengever.workspace.participation.browser.manage_participants import ManageParticipants
+from opengever.workspace.todos.storage import ToDoListStorage
+from plone.restapi.deserializer import json_body
 from plone.restapi.interfaces import ISerializeToJson
+from plone.restapi.services.content.add import FolderPost
 from zope.component import adapter
 from zope.interface import implementer
 from zope.interface import Interface
@@ -17,5 +20,25 @@ class SerializeWorkspaceToJson(GeverSerializeFolderToJson):
         manager = ManageParticipants(self.context, self.request)
 
         result[u"can_manage_participants"] = manager.can_manage_member()
+
+        return result
+
+
+class WorkspacePost(FolderPost):
+
+    def reply(self):
+        result = super(WorkspacePost, self).reply()
+        data = json_body(self.request)
+
+        if result.get('@type') == 'opengever.workspace.todo':
+            result = self.handle_add_todo(data, result)
+
+        return result
+
+    def handle_add_todo(self, data, result):
+        todo_list_id = data.get('todo_list_id')
+        if todo_list_id:
+            ToDoListStorage(self.context).add_todo_to_list(todo_list_id,
+                                                           result.get('UID'))
 
         return result
