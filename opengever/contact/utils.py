f66788444084aff0d6cb97e472e5194d71b694e3
Philippe Gross <philippe.gross@4teamwork.ch>
2019-07-30 11:45:00 +0200

    Switch from `elevated_privileges` to an unrestricted catalog query,
    to fetch contactsfolder url.

--- opengever/contact/utils.py
+++ opengever/contact/utils.py
@@ -1,4 +1,3 @@
-from opengever.base.security import elevated_privileges
 from opengever.base.utils import get_hostname
 from opengever.contact.interfaces import IContactFolder
 from plone import api
@@ -11,13 +10,13 @@ def hostname_cache_key(m, *args, **kwargs):
 
 
 @ram.cache(hostname_cache_key)
-def get_contactfolder_url(elevate_privileges=False):
-    if elevate_privileges:
-
-        with elevated_privileges():
-            result = api.content.find(object_provides=IContactFolder)
+def get_contactfolder_url(unrestricted=False):
+    catalog = api.portal.get_tool('portal_catalog')
+    if unrestricted:
+        result = catalog.unrestrictedSearchResults(
+            object_provides=IContactFolder.__identifier__)
     else:
-        result = api.content.find(object_provides=IContactFolder)
+        result = catalog(object_provides=IContactFolder.__identifier__)
 
     if not result:
         raise Exception('Contactfolder is missing, GEVER deployment was not '
