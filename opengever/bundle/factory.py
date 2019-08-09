eda1ff6617d10b39f929a83507000c5544b18c25
Niklaus Johner <niklaus.johner@4teamwork.ch>
2019-08-09 17:27:29 +0200

    Refactor BundleFactory.

--- opengever/bundle/factory.py
+++ opengever/bundle/factory.py
@@ -11,27 +11,28 @@ import argparse
 import errno
 import json
 import os
+from collections import namedtuple
 
 
 DEFAULT_RESPONSIBLE = 'lukas.graf'
-DEFAULT_USERS_GROUP = 'og_demo-ftw_users'
 
 # List of fnmatch() patterns to specify which "documents" to ignore
 IGNORES = ['*.DS_Store']
 
 
+DirectoryNode = namedtuple('Node', ['path', 'guid', 'parent_guid', 'level'])
+FileNode = namedtuple('Node', ['path', 'guid', 'parent_guid'])
+
+
 class FilesystemWalker(object):
 
-    def __init__(self, top, followlinks=False,
-                 repo_depth=3, users_group=DEFAULT_USERS_GROUP):
+    def __init__(self, top, followlinks=False):
         self.top = top
         self.followlinks = followlinks
-        self.repo_depth = repo_depth
-        self.users_group = users_group
 
     def __iter__(self):
         return self.walk(
-            self.top, followlinks=self.followlinks, repo_depth=self.repo_depth)
+            self.top, followlinks=self.followlinks)
 
     def make_guid(self, path):
         """Create a unique, but human readable GUID, like
@@ -53,36 +54,10 @@ class FilesystemWalker(object):
         names = listdir(top)
 
         # Folderish item
-        container = {
-            'path': top,
-            'guid': self.make_guid(top),
-            'parent_guid': parent_guid,
-            'title': basename(top.rstrip(os.path.sep)),
-        }
-
-        if level == 0:
-            container['type'] = 'reporoot'
-            container.pop('parent_guid')
-            container['review_state'] = 'repositoryroot-state-active'
-            container['title_de'] = container.pop('title')
-            container['_permissions'] = {
-                'read': [self.users_group],
-                'add': [self.users_group],
-                'edit': [self.users_group],
-                'close': [],
-                'reactivate': [],
-            }
-
-        elif level <= repo_depth:
-            container['type'] = 'repofolder'
-            container['review_state'] = 'repositoryfolder-state-active'
-            container['title_de'] = container.pop('title')
-
-        elif level > repo_depth:
-            container['type'] = 'dossier'
-            container['responsible'] = DEFAULT_RESPONSIBLE
-            container['review_state'] = 'dossier-state-active'
-
+        container = DirectoryNode(path=top,
+                                  guid=self.make_guid(top),
+                                  parent_guid=parent_guid,
+                                  level=level)
         yield container
 
         dirs = []
@@ -97,24 +72,112 @@ class FilesystemWalker(object):
                     continue
 
                 # Non-folderish item (i.e. document)
-                item = {
-                    'path': path,
-                    'guid': self.make_guid(path),
-                    'parent_guid': container['guid'],
-                    'title': basename(name),
-                    'type': 'document',
-                    'review_state': 'document-state-draft',
-                    'filepath': path,
-                }
-                yield item
+                node = FileNode(path=path,
+                                guid=self.make_guid(path),
+                                parent_guid=container.guid)
+                yield node
 
         for name in dirs:
             new_path = pjoin(top, name)
             if followlinks or not islink(new_path):
                 for node in self.walk(
                         new_path, followlinks, level=level + 1,
-                        parent_guid=container['guid'], repo_depth=repo_depth):
+                        parent_guid=container.guid):
                     yield node
+
+
+class OGGBundleItemCreator(object):
+
+    def __init__(self, repo_depth, responsible, user_group):
+        self.repo_depth = repo_depth
+        self.responsible = responsible
+        self.user_group = user_group
+
+    def __call__(self, node):
+        if not self.isdir(node):
+            return OGGBundleDocument(node)
+        elif node.level == 0:
+            return OGGBundleRepoRoot(node, self.user_group)
+        elif node.level <= self.repo_depth:
+            return OGGBundleRepoFolder(node)
+        else:
+            return OGGBundleDossier(node, self.responsible)
+
+    @staticmethod
+    def isdir(node):
+        return isinstance(node, DirectoryNode)
+
+
+class OGGBundleItemBase(object):
+
+    item_type = None
+    review_state = None
+
+    def __init__(self, node):
+        self.node = node
+        self.path = node.path
+        self._data = {
+            'guid': node.guid,
+            'review_state': self.review_state,
+            'title': self.title
+            }
+
+    def as_dict(self):
+        return self._data
+
+    @property
+    def title(self):
+        return basename(self.path.rstrip(os.path.sep))
+
+
+class OGGBundleRepoRoot(OGGBundleItemBase):
+
+    item_type = 'reporoot'
+    review_state = 'repositoryroot-state-active'
+
+    def __init__(self, node, users_group):
+        super(OGGBundleRepoRoot, self).__init__(node)
+        self._data['title_de'] = self._data.pop('title')
+        self._data['_permissions'] = {
+                'read': [users_group],
+                'add': [users_group],
+                'edit': [users_group],
+                'close': [],
+                'reactivate': [],
+            }
+
+
+class OGGBundleRepoFolder(OGGBundleItemBase):
+
+    item_type = 'repofolder'
+    review_state = 'repositoryfolder-state-active'
+
+    def __init__(self, node):
+        super(OGGBundleRepoFolder, self).__init__(node)
+        self._data['title_de'] = self._data.pop('title')
+        self._data['parent_guid'] = self.node.parent_guid
+
+
+class OGGBundleDossier(OGGBundleItemBase):
+
+    item_type = 'dossier'
+    review_state = 'dossier-state-active'
+
+    def __init__(self, node, responsible):
+        super(OGGBundleDossier, self).__init__(node)
+        self._data['parent_guid'] = self.node.parent_guid
+        self._data['responsible'] = responsible
+
+
+class OGGBundleDocument(OGGBundleItemBase):
+
+    item_type = 'document'
+    review_state = 'document-state-draft'
+
+    def __init__(self, node):
+        super(OGGBundleDocument, self).__init__(node)
+        self._data['parent_guid'] = self.node.parent_guid
+        self._data['filepath'] = self.node.path
 
 
 class BundleFactory(object):
@@ -128,6 +191,10 @@ class BundleFactory(object):
 
         self.repofolder_paths = []
         self.items = []
+
+        self.item_creator = OGGBundleItemCreator(self.repo_nesting_depth,
+                                                 DEFAULT_RESPONSIBLE,
+                                                 self.users_group)
 
     @property
     def bundle_name(self):
@@ -146,15 +213,10 @@ class BundleFactory(object):
             'document': [],
         }
 
-        walker = FilesystemWalker(self.source_dir, followlinks=True,
-                                  repo_depth=self.repo_nesting_depth,
-                                  users_group=self.users_group)
-        for item in walker:
-            items[item['type']].append(item)
-
-            # Strip keys not allowed in OGGBundle items
-            for key in ('type', 'path'):
-                item.pop(key)
+        walker = FilesystemWalker(self.source_dir, followlinks=True)
+        for node in walker:
+            item = self.item_creator(node)
+            items[item.item_type].append(item.as_dict())
 
         self.dump_items(items, bundle_dir)
 
