c4dfe2c1a7715755e1619a089a3d285166901d4a
Philippe Gross <philippe.gross@4teamwork.ch>
2019-08-21 08:04:10 +0200

    Add @response GET endpoint.

--- opengever/api/response.py
+++ opengever/api/response.py
@@ -1,12 +1,17 @@
 from opengever.base.response import IResponse
+from opengever.base.response import IResponseContainer
 from plone.restapi.interfaces import IFieldSerializer
 from plone.restapi.interfaces import ISerializeToJson
 from plone.restapi.serializer.converters import json_compatible
 from plone.restapi.serializer.dxcontent import SerializeFolderToJson
+from plone.restapi.services import Service
+from zExceptions import NotFound
 from zope.component import adapter
+from zope.component import getMultiAdapter
 from zope.component import queryMultiAdapter
 from zope.interface import implementer
 from zope.interface import Interface
+from zope.publisher.interfaces import IPublishTraverse
 from zope.schema import getFields
 from zope.schema.interfaces import IField
 
@@ -36,7 +41,10 @@ class SerializeResponseToJson(SerializeFolderToJson):
         self.request = request
 
     def __call__(self, *args, **kwargs):
-        result = {}
+        response_url = '{}/@responses/{}'.format(
+            kwargs['container'].absolute_url(), kwargs['response_id'])
+
+        result = {'@id': response_url}
         for name, field in getFields(IResponse).items():
             serializer = queryMultiAdapter(
                 (field, self.context, self.request), IFieldSerializer)
@@ -44,3 +52,35 @@ class SerializeResponseToJson(SerializeFolderToJson):
             result[json_compatible(name)] = value
 
         return result
+
+
+@implementer(IPublishTraverse)
+class ResponseGet(Service):
+    """Representation of a single response.
+    """
+
+    def __init__(self, context, request):
+        super(ResponseGet, self).__init__(context, request)
+        self.params = []
+
+    def publishTraverse(self, request, name):
+        # Consume any path segments after /@responses as parameters
+        self.params.append(name)
+        return self
+
+    @property
+    def _get_response_id(self):
+        if len(self.params) != 1:
+            raise Exception(
+                "Must supply exactly one parameter (the response id)")
+        return self.params[0]
+
+    def reply(self):
+        response_container = IResponseContainer(self.context)
+        if self._get_response_id not in response_container:
+            raise NotFound
+
+        response = response_container[self._get_response_id]
+        serializer = getMultiAdapter((response, self.request), ISerializeToJson)
+        return serializer(
+            container=self.context, response_id=self._get_response_id)
--- opengever/api/tests/test_response.py
+++ opengever/api/tests/test_response.py
@@ -36,10 +36,41 @@ class TestResponseGETSerialization(IntegrationTestCase):
 
         browser.open(self.todo, method="GET", headers=self.api_headers)
         self.assertEquals(
-            [{u'created': u'2016-12-09T09:40:00',
+            [{u'@id': u'http://nohost/plone/workspaces/workspace-1/todo-1/@responses/1481272800000000',
+              u'created': u'2016-12-09T09:40:00',
               u'creator': self.workspace_member.id,
               u'text': u'Ich bin hier anderer Meinung!'},
-             {u'created': u'2016-12-24T08:23:00',
+             {u'@id': u'http://nohost/plone/workspaces/workspace-1/todo-1/@responses/1482564180000000',
+              u'created': u'2016-12-24T08:23:00',
               u'creator': self.workspace_admin.id,
               u'text': u'Ok, Fanke f\xfcr dein Feedback'}],
             browser.json['responses'])
+
+
+class TestResponseGET(IntegrationTestCase):
+
+    @browsing
+    def test_raises_not_found_when_response_id_does_not_exist(self, browser):
+        self.login(self.workspace_member, browser=browser)
+
+        with browser.expect_http_error(404):
+            url = '{}/@responses/111'.format(self.todo.absolute_url())
+            browser.open(url, method="GET", headers=self.api_headers)
+
+    @browsing
+    def test_returns_the_serialized_response_object(self, browser):
+        self.login(self.workspace_member, browser=browser)
+
+        with freeze(datetime(2016, 12, 9, 9, 40)):
+            IResponseContainer(self.todo).add(
+                Response('Ich bin hier anderer Meinung!'))
+
+        url = '{}/@responses/1481272800000000'.format(self.todo.absolute_url())
+        browser.open(url, method="GET", headers=self.api_headers)
+
+        self.assertEquals(
+            {u'@id': url,
+             u'created': u'2016-12-09T09:40:00',
+             u'creator': self.workspace_member.id,
+             u'text': u'Ich bin hier anderer Meinung!'},
+            browser.json)
