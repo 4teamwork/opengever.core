ee1d6984e5de2c888c05ffe637c5b8a663f487ab
Philippe Gross <philippe.gross@4teamwork.ch>
2019-09-12 11:38:23 +0200

    Another fix.

--- opengever/task/deadline_modifier.py
+++ opengever/task/deadline_modifier.py
@@ -35,7 +35,7 @@ class DeadlineModifier(object):
             return checker.current_user.is_issuer or checker.current_user.is_responsible
         else:
             return (checker.current_user.is_issuer
-                    or checker.current_user.current_user.is_responsible
+                    or checker.current_user.is_responsible
                     or checker.current_user.in_issuing_orgunits_inbox_group
                     or checker.current_user.is_administrator)
 
