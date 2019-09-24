ae653c7aca138433ed9a69eece34d57fe4469d7d
Niklaus Johner <niklaus.johner@4teamwork.ch>
2019-09-24 18:03:48 +0200

    Handle proposal creation through REST-API.

--- opengever/api/add.py
+++ opengever/api/add.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from Acquisition import aq_base
 from Acquisition.interfaces import IAcquirer
+from opengever.meeting.browser.proposalforms import get_selected_template
+from opengever.meeting.browser.proposalforms import IAddProposalSupplementaryFields
 from plone.restapi.deserializer import json_body
 from plone.restapi.exceptions import DeserializationError
 from plone.restapi.interfaces import IDeserializeFromJson
+from plone.restapi.interfaces import IFieldDeserializer
 from plone.restapi.interfaces import ISerializeToJson
 from plone.restapi.services import Service
 from plone.restapi.services.content.utils import add
@@ -16,8 +19,11 @@ from zope.component import getMultiAdapter
 from zope.component import queryMultiAdapter
 from zope.event import notify
 from zope.interface import alsoProvides
+from zope.interface.exceptions import Invalid
 from zope.lifecycleevent import ObjectCreatedEvent
-
+from zope.schema import getFieldsInOrder
+from zope.schema import ValidationError
+from zope.schema.interfaces import RequiredMissing
 import plone.protect.interfaces
 
 
@@ -35,6 +41,7 @@ class FolderPost(Service):
 
         if not self.type:
             raise BadRequest("Property '@type' is required")
+        return data
 
     def deserialize_object(self):
         # Acquisition wrap temporarily to satisfy things like vocabularies
@@ -103,3 +110,137 @@ class FolderPost(Service):
         serialized_obj = self.serialize_object()
 
         return serialized_obj
+
+
+class GeverFolderPost(FolderPost):
+    """Contains specific treatment for creation of certain portal types
+    """
+
+    def extract_data(self):
+        data = super(GeverFolderPost, self).extract_data()
+        if self.type == 'opengever.meeting.proposal':
+            data.update(self.extract_additional_data(data, IAddProposalSupplementaryFields))
+            self.validate_additional_schema(data, IAddProposalSupplementaryFields)
+        self.data = data
+        return data
+
+    def extract_additional_data(self, data, schema):
+        """ Deserializes the values corresponding to the passed schema found in
+        data. Note that validation of the value happens during deserialization.
+        """
+        for name, field in getFieldsInOrder(schema):
+            if name not in data:
+                continue
+            validation_data = SchemaValidationData(schema, data, self.context)
+            deserializer = queryMultiAdapter((field, validation_data, self.request),
+                                             IFieldDeserializer)
+            value = deserializer(data[name])
+            data[name] = value
+        return data
+
+    def validate_additional_schema(self, data, schema):
+        """This will validate the values in data corresponding to
+        the fields from schema, check that all required fields are
+        in data, and validate the invariants.
+        """
+        errors = get_validation_errors(self.context, data, schema)
+        if errors:
+            raise BadRequest(errors)
+
+    def add_object_to_context(self):
+        super(GeverFolderPost, self).add_object_to_context()
+        if self.obj.portal_type == 'opengever.meeting.proposal':
+            # For proposals we also need to create the proposal document
+            data = SchemaValidationData(IAddProposalSupplementaryFields,
+                                        self.data,
+                                        self.context)
+            proposal_template = get_selected_template(data)
+            self.obj.create_proposal_document(
+                title=self.obj.title_or_id(),
+                source_blob=proposal_template.file,
+            )
+
+
+class SchemaValidationData(object):
+    """To validate a field, it needs to be bound to its context, as for example
+    certain vocabularies are context-dependent. In the case of content creation,
+    the object does not exist yet and the container is used as context, so we
+    need to provide access to the data that will be set on the object being
+    created (contained either in data or as default values on the schema).
+    For example the ProposalTemplatesForCommitteeVocabulary used for the
+    proposal_template field needs access to the predeessor_proposal and
+    committee_oguid both stored in data.
+
+    This class therefore wraps the context (container) together with data and
+    schema defaults to allow correct validation. This is similar to the
+    z3c.form.validator.Data class used during form invariants validation.
+    """
+    def __init__(self, schema, data, context):
+        self.context = context
+        self.data = data
+        self.schema = schema
+
+    def __getattr__(self, name):
+        if name in self.data:
+            return self.data.get(name)
+        if hasattr(self.context, name):
+            return getattr(self.context, name)
+        if name in self.schema:
+            return self.schema.get(name).default
+        return None
+
+
+def get_schema_validation_errors(context, data, schema):
+    """Validate a dict against a schema.
+
+    Return a list of basic schema validation errors (required fields,
+    constraints, but doesn't check invariants yet).
+
+    Loosely based on zope.schema.getSchemaValidationErrors, but:
+
+    - Processes fields in schema order
+    - Handles dict subscription access instead of object attribute access
+    - Respects required / optional fields
+    - Raises RequiredMissing instead of SchemaNotFullyImplemented
+    """
+    validation_data = SchemaValidationData(schema, data, context)
+    errors = []
+    for name, field in getFieldsInOrder(schema):
+        try:
+            value = data[name]
+        except KeyError:
+            # property for the given name is not implemented
+            if not field.required:
+                continue
+            errors.append((name, RequiredMissing(name)))
+        else:
+            try:
+                field.bind(validation_data).validate(value)
+            except ValidationError as e:
+                errors.append((name, e))
+
+    return errors
+
+
+def get_validation_errors(context, data, schema):
+    """Validate a dict against a schema and invariants.
+
+    Return a list of all validation errors, including invariants.
+    Based on zope.schema.getValidationErrors.
+    """
+    errors = get_schema_validation_errors(context, data, schema)
+    if errors:
+        return errors
+
+    # Only validate invariants if there were no previous errors. Previous
+    # errors could be missing attributes which would most likely make an
+    # invariant raise an AttributeError.
+    invariant_errors = []
+    try:
+        schema.validateInvariants(SchemaValidationData(schema, data, context),
+                                  invariant_errors)
+    except Invalid:
+        # Just collect errors
+        pass
+    errors = [(None, e) for e in invariant_errors]
+    return errors
--- opengever/api/tests/test_content_creation.py
+++ opengever/api/tests/test_content_creation.py
@@ -1,13 +1,17 @@
 from ftw.bumblebee.interfaces import IBumblebeeDocument
 from ftw.testbrowser import browsing
 from opengever.base.behaviors.lifecycle import ILifeCycle
+from opengever.base.oguid import Oguid
 from opengever.dossier.behaviors.dossier import IDossier
 from opengever.testing import IntegrationTestCase
 from plone import api
+from plone.uuid.interfaces import IUUID
 import json
 
 
 class TestContentCreation(IntegrationTestCase):
+
+    features = ('meeting',)
 
     def setUp(self):
         super(TestContentCreation, self).setUp()
@@ -72,3 +76,144 @@ class TestContentCreation(IntegrationTestCase):
 
         checksum = IBumblebeeDocument(doc).get_checksum()
         self.assertIsNotNone(checksum)
+
+    @browsing
+    def test_proposal_creation(self, browser):
+        self.login(self.meeting_user, browser)
+
+        committee_oguid = Oguid.for_object(self.committee).id
+        payload = {
+            u'@type': u'opengever.meeting.proposal',
+            u'title': u'Sanierung B\xe4rengraben 2016',
+            u'proposal_template': IUUID(self.proposal_template),
+            u"committee_oguid": committee_oguid,
+            u'issuer': self.meeting_user.getId(),
+        }
+        response = browser.open(
+            self.dossier.absolute_url(),
+            data=json.dumps(payload),
+            method='POST',
+            headers=self.api_headers)
+
+        self.assertEqual(201, response.status_code)
+
+        new_object_id = str(response.json['id'])
+        proposal = self.dossier.restrictedTraverse(new_object_id)
+        self.assertEqual(u'Sanierung B\xe4rengraben 2016', proposal.title)
+        self.assertEqual(self.meeting_user.getId(), proposal.issuer)
+
+        proposal_doc = proposal.get_proposal_document()
+        self.assertIsNotNone(proposal_doc)
+        self.assertEqual(u'Sanierung B\xe4rengraben 2016',
+                         proposal_doc.title)
+        self.assertEqual(u'Sanierung Baerengraben 2016.docx',
+                         proposal_doc.get_filename())
+
+        checksum = IBumblebeeDocument(proposal_doc).get_checksum()
+        self.assertIsNotNone(checksum)
+        self.assertEqual(IBumblebeeDocument(self.proposal_template).get_checksum(),
+                         checksum)
+
+    @browsing
+    def test_template_is_mandatory_for_proposal_creation(self, browser):
+        self.login(self.meeting_user, browser)
+
+        committee_oguid = Oguid.for_object(self.committee).id
+        payload = {
+            u'@type': u'opengever.meeting.proposal',
+            u'title': u'Sanierung B\xe4rengraben 2016',
+            u"committee_oguid": committee_oguid,
+            u'issuer': self.meeting_user.getId(),
+        }
+
+        with browser.expect_http_error():
+            browser.open(
+                self.dossier.absolute_url(),
+                data=json.dumps(payload),
+                method='POST',
+                headers=self.api_headers)
+
+        self.assertEqual(400, browser.status_code)
+        self.assertDictEqual(
+            {u'message': u"[(None, Invalid(u'error_template_or_document_required_for_creation',))]",
+             u'type': u'BadRequest'},
+            browser.json)
+
+    @browsing
+    def test_proposal_creation_from_docx_template(self, browser):
+        self.login(self.meeting_user, browser)
+
+        committee_oguid = Oguid.for_object(self.committee).id
+        payload = {
+            u'@type': u'opengever.meeting.proposal',
+            u'title': u'Sanierung B\xe4rengraben 2016',
+            u'proposal_document_type': 'existing',
+            u'proposal_document': IUUID(self.document),
+            u"committee_oguid": committee_oguid,
+            u'issuer': self.meeting_user.getId(),
+        }
+
+        response = browser.open(
+            self.dossier.absolute_url(),
+            data=json.dumps(payload),
+            method='POST',
+            headers=self.api_headers)
+
+        self.assertEqual(201, response.status_code)
+
+        new_object_id = str(response.json['id'])
+        proposal = self.dossier.restrictedTraverse(new_object_id)
+        self.assertEqual(u'Sanierung B\xe4rengraben 2016', proposal.title)
+        self.assertEqual(self.meeting_user.getId(), proposal.issuer)
+
+        proposal_doc = proposal.get_proposal_document()
+        self.assertIsNotNone(proposal_doc)
+        self.assertEqual(u'Sanierung B\xe4rengraben 2016',
+                         proposal_doc.title)
+        self.assertEqual(u'Sanierung Baerengraben 2016.docx',
+                         proposal_doc.get_filename())
+
+        checksum = IBumblebeeDocument(proposal_doc).get_checksum()
+        self.assertIsNotNone(checksum)
+        self.assertEqual(IBumblebeeDocument(self.document).get_checksum(),
+                         checksum)
+
+    @browsing
+    def test_proposal_creation_from_predecessor_proposal_document(self, browser):
+        self.login(self.meeting_user, browser)
+
+        committee_oguid = Oguid.for_object(self.committee).id
+        payload = {
+            u'@type': u'opengever.meeting.proposal',
+            u'title': u'Sanierung B\xe4rengraben 2016',
+            u'proposal_template': IUUID(self.proposal.get_proposal_document()),
+            u'predecessor_proposal': IUUID(self.proposal),
+            u"committee_oguid": committee_oguid,
+            u'issuer': self.meeting_user.getId(),
+        }
+
+        response = browser.open(
+            self.dossier.absolute_url(),
+            data=json.dumps(payload),
+            method='POST',
+            headers=self.api_headers)
+
+        self.assertEqual(201, response.status_code)
+
+        new_object_id = str(response.json['id'])
+        proposal = self.dossier.restrictedTraverse(new_object_id)
+        self.assertEqual(u'Sanierung B\xe4rengraben 2016', proposal.title)
+        self.assertEqual(self.meeting_user.getId(), proposal.issuer)
+
+        proposal_doc = proposal.get_proposal_document()
+        self.assertIsNotNone(proposal_doc)
+        self.assertEqual(u'Sanierung B\xe4rengraben 2016',
+                         proposal_doc.title)
+        self.assertEqual(u'Sanierung Baerengraben 2016.docx',
+                         proposal_doc.get_filename())
+
+        checksum = IBumblebeeDocument(proposal_doc).get_checksum()
+        self.assertIsNotNone(checksum)
+        self.assertEqual(
+            IBumblebeeDocument(self.proposal.get_proposal_document()).get_checksum(),
+            checksum)
