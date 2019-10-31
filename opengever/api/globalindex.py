474df8fe81d6d1b2a6469110d6710a0be136a297
Philippe Gross <philippe.gross@4teamwork.ch>
2019-10-31 14:56:46 +0100

    Add globalindex endpoint to query the task globalindex.

---
+++ opengever/api/globalindex.py
@@ -0,0 +1,53 @@
+from opengever.api.listing import DEFAULT_SORT_INDEX
+from opengever.globalindex.model.task import Task
+from plone.restapi.batching import HypermediaBatch
+from plone.restapi.serializer.converters import json_compatible
+from plone.restapi.services import Service
+from sqlalchemy import asc
+from sqlalchemy import desc
+
+
+class GlobalIndexGet(Service):
+
+    METADATA = [
+        'task_id', 'title', 'review_state', 'responsible', 'issuer',
+        'task_type', 'is_private', 'is_subtask', 'assigned_org_unit',
+        'issuing_org_unit', 'deadline', 'modified', 'created',
+        'predecessor_id']
+
+    def reply(self):
+
+        sort_on = self.request.form.get('sort_on', DEFAULT_SORT_INDEX)
+        if sort_on not in Task.__table__.columns:
+            sort_on = DEFAULT_SORT_INDEX
+
+        sort_order = self.request.form.get('sort_order', 'descending')
+
+        start = self.request.form.get('b_start', '0')
+        rows = self.request.form.get('b_size', '25')
+
+        try:
+            start = int(start)
+            rows = int(rows)
+        except ValueError:
+            start = 0
+            rows = 25
+
+        if sort_order == 'ascending':
+            order = asc(getattr(Task, sort_on))
+        else:
+            order = desc(getattr(Task, sort_on))
+
+        tasks = Task.query.order_by(order).all()
+        items = []
+        batch = HypermediaBatch(self.request, tasks)
+        for task in batch:
+            items.append(
+                {key: json_compatible(getattr(task, key)) for (key) in self.METADATA})
+
+        result = {}
+        result['items'] = items
+        result['batching'] = batch.links
+        result['items_total'] = batch.items_total
+
+        return result
---
+++ opengever/api/tests/test_globalindex.py
@@ -0,0 +1,81 @@
+from ftw.testbrowser import browsing
+from opengever.testing import IntegrationTestCase
+
+
+class TestGlobalIndexGet(IntegrationTestCase):
+
+    @browsing
+    def test_lists_all_task(self, browser):
+        self.login(self.regular_user, browser=browser)
+        browser.open(self.portal, view='@globalindex?sort_on=title',
+                     headers=self.api_headers)
+
+        self.assertEqual(15, browser.json['items_total'])
+        self.assertEqual(15, len(browser.json['items']))
+        self.assertEqual(
+            {u'title': u're: Diskr\xe4te Dinge',
+             u'task_type': u'direct-execution',
+             u'task_id': 14,
+             u'created': u'2016-08-31T18:27:33',
+             u'issuing_org_unit': u'fa',
+             u'responsible': u'inbox:fa',
+             u'modified': u'2016-08-31T18:27:33',
+             u'is_subtask': False,
+             u'deadline': u'2020-01-01',
+             u'review_state': u'task-state-in-progress',
+             u'assigned_org_unit': u'fa',
+             u'is_private': True,
+             u'predecessor_id': None,
+             u'issuer': u'robert.ziegler'},
+            browser.json['items'][0])
+
+        # default row size is 25
+        self.assertIsNone(browser.json['batching'])
+
+    @browsing
+    def test_respect_batching_parameters(self, browser):
+        self.login(self.regular_user, browser=browser)
+        browser.open(self.portal, view='@globalindex?b_size=3',
+                     headers=self.api_headers)
+
+        self.assertEqual(15, browser.json['items_total'])
+        self.assertEqual(3, len(browser.json['items']))
+        self.assertEqual(
+            {u'@id': u'http://nohost/plone/@globalindex?b_size=3',
+             u'first': u'http://nohost/plone/@globalindex?b_start=0&b_size=3',
+             u'last': u'http://nohost/plone/@globalindex?b_start=12&b_size=3',
+             u'next': u'http://nohost/plone/@globalindex?b_start=3&b_size=3'},
+            browser.json['batching'])
+
+    @browsing
+    def test_respect_sort_parameter(self, browser):
+        self.login(self.regular_user, browser=browser)
+        browser.open(self.portal, view='@globalindex?sort_on=title&b_size=3',
+                     headers=self.api_headers)
+
+        self.assertEqual(
+            [u're: Diskr\xe4te Dinge',
+             u'Vorstellungsrunde bei anderen Mitarbeitern',
+             u'Vertr\xe4ge abschliessen'],
+            [item['title'] for item in browser.json['items']])
+
+        browser.open(
+            self.portal,
+            view='@globalindex?sort_on=title&b_size=3&sort_order=ascending',
+            headers=self.api_headers)
+
+        self.assertEqual(
+            [u'Arbeitsplatz vorbereiten',
+             u'Diskr\xe4te Dinge',
+             u'Ein notwendiges \xdcbel'],
+            [item['title'] for item in browser.json['items']])
+
+    @browsing
+    def test_fallback_to_default_sort_if_sort_on_does_not_exist(self, browser):
+        self.login(self.regular_user, browser=browser)
+        browser.open(self.portal, view='@globalindex?sort_on=notexisting&b_size=3',
+                     headers=self.api_headers)
+
+        self.assertEqual(15, browser.json['items_total'])
+        self.assertEqual(u'Ein notwendiges \xdcbel', browser.json['items'][0]['title'])
+        self.assertEqual(u'2016-08-31T20:25:33', browser.json['items'][0]['modified'])
