747ef0e5f2e3891f02aebd73700227148f410af4
Lukas Graf <lukas.graf@4teamwork.ch>
2019-11-07 09:09:41 +0100

    Revert "Cast the userid to bytestring so that validation to ASCIILine passes"

--- opengever/webactions/storage.py
+++ opengever/webactions/storage.py
@@ -92,9 +92,7 @@ class WebActionsStorage(object):
         new_action = PersistentMapping(action_data)
         self._actions[action_id] = new_action
 
-        # This is a workaround: the returned userid is of type unicode. As the "owner" field is of type ASCIILine, it is
-        # simply casted to a bytestring.
-        userid = str(api.user.get_current().getId())
+        userid = api.user.get_current().getId()
         now = datetime.now()
 
         # The `action_id` is stored redundantly here in the actual
