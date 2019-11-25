4346947ec8aefd678fc6fdcb81cf95cf7843728b
David Erni <david.erni@4teamwork.ch>
2019-11-25 14:51:57 +0100

    Add description to period manager fields.

--- opengever/meeting/behaviors/period.py
+++ opengever/meeting/behaviors/period.py
@@ -63,6 +63,9 @@ class IPeriodSchema(model.Schema):
     decision_sequence_number = schema.Int(
         title=_(u'label_decision_sequence_number',
                 default=u'Sequence number for decisions'),
+        description=_(u'description_decision_sequence_number',
+                     default=u'Only visible for managers. Only change this if '
+                              'you know what you are doing!'),
         required=True,
         default=0,
     )
@@ -71,6 +74,9 @@ class IPeriodSchema(model.Schema):
     meeting_sequence_number = schema.Int(
         title=_(u'label_meeting_sequence_number',
                 default=u'Sequence number for meetings'),
+        description=_(u'description_meeting_sequence_number',
+                     default=u'Only visible for managers. Only change this if '
+                              'you know what you are doing!'),
         required=True,
         default=0,
     )
