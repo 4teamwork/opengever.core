8b5702ed89e474b772e301d1851f3bc9b2a7cf9a
David Erni <david.erni@4teamwork.ch>
2019-12-11 16:14:05 +0100

    Rename endpoint to WorkspaceInvitationsPost.
    This frees up the classname InvitationsPost for a POST to the
    @invitations endpoint which makes more sense. The @workspace-invitations
    thing is weird and may go away in that form eventually.

--- opengever/api/invitations.py
+++ opengever/api/invitations.py
@@ -55,7 +55,7 @@ class MyInvitationsGet(Service):
         return result
 
 
-class InvitationsPost(ParticipationTraverseService):
+class WorkspaceInvitationsPost(ParticipationTraverseService):
     """API Endpoint for accepting or declining invitations.
 
     POST /@workspace-invitations/{invitation_id}/{action} HTTP/1.1
