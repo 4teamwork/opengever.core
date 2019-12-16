5a6bec1832ed056a7a3c5d33dbbc0a209bfd1700
Niklaus Johner <niklaus.johner@4teamwork.ch>
2019-12-16 15:53:25 +0100

    Handle different scenarios when accepting an invitation.

--- opengever/workspace/participation/browser/my_invitations.py
+++ opengever/workspace/participation/browser/my_invitations.py
@@ -1,16 +1,23 @@
+from itsdangerous import URLSafeTimedSerializer
+from opengever.base.casauth import get_gever_portal_url
 from opengever.base.role_assignments import InvitationRoleAssignment
 from opengever.base.role_assignments import RoleAssignmentManager
 from opengever.base.security import elevated_privileges
 from opengever.ogds.base.actor import PloneUserActor
+from opengever.ogds.base.utils import ogds_service
 from opengever.workspace import _
 from opengever.workspace import is_workspace_feature_enabled
 from opengever.workspace.activities import WorkspaceWatcherManager
+from opengever.workspace.config import workspace_config
 from opengever.workspace.participation.storage import IInvitationStorage
 from plone import api
 from plone.app.uuid.utils import uuidToObject
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.LDAPMultiPlugins.interfaces import ILDAPMultiPlugin
+from urllib import urlencode
 from zExceptions import BadRequest
+from zExceptions import InternalError
 from zope.component import getUtility
 
 
@@ -69,15 +76,76 @@ class MyWorkspaceInvitations(BrowserView):
         except KeyError:
             return None
 
-        if api.user.get_current().getId() != invitation['recipient']:
-            return None
         return invitation
 
+    def _get_orgunit_group_dn(self):
+        """get the dn of the group associated with the current orgunit
+        """
+        portal = api.portal.get()
+        org_units = ogds_service().all_org_units()
+        if len(org_units) > 1:
+            raise InternalError("Workspace installation can only have a "
+                                "single enabled org_unit")
+        group_id = org_units[0].users_group_id
+        groups = []
+        for item in portal['acl_users'].objectValues():
+            if ILDAPMultiPlugin.providedBy(item):
+                user_folder = item.acl_users
+                key = user_folder.groupid_attr
+                groups.extend(user_folder.searchGroups(
+                    **{key: group_id, 'exactMatch': True})
+                )
+        if len(groups) > 1:
+            raise InternalError("group {} is not unique".format(group_id))
+        return groups[0]['dn']
+
+    def _serialize_and_sign_payload(self, payload):
+        """Serialize and sign the payload for the portal
+        """
+        secret = workspace_config.secret
+        serializer = URLSafeTimedSerializer(secret)
+        return serializer.dumps(payload)
+
     def accept(self):
-        """Accept a invitation by setting the role and redirect the user
-        to the workspace.
+        """Accept an invitation. There are 3 different scenarios here:
+        1. The user is not logged in and his E-mail address does not match any
+           user. In that case we redirect to the portal registration.
+        2. The user is not logged in but his E-mail address matches that of an
+           existing user. In that case we redirect to the portal login.
+        3. The user is logged-in. In that case we accept the invitation by
+           setting the role and redirect the user to the workspace.
         """
         invitation = self.get_invitation_and_validate_payload()
+        with elevated_privileges():
+            target_workspace = uuidToObject(invitation['target_uuid'])
+
+        if api.user.is_anonymous():
+            accept_url = "{}/@@my-invitations/accept".format(
+                target_workspace.absolute_url())
+            accept_params = {'iid': invitation['iid']}
+
+            if not self.storage()._find_user_id_for_email(invitation['recipient_email']):
+                accept_params['new_user'] = 1
+                accept_url = "{}?{}".format(
+                    accept_url, urlencode(accept_params))
+                group_dn = self._get_orgunit_group_dn()
+
+                params = {'email': invitation['recipient_email'],
+                          'callback': accept_url,
+                          'group': group_dn
+                          }
+                payload = self._serialize_and_sign_payload(params)
+                redirect_url = "{}/registration?invitation={}".format(
+                    get_gever_portal_url(), payload)
+
+            else:
+                accept_url = "{}?{}".format(
+                    accept_url, urlencode(accept_params))
+                params = {'redirect_url': accept_url}
+                redirect_url = "{}/login?{}".format(
+                    get_gever_portal_url(), urlencode(params))
+
+            return self.request.RESPONSE.redirect(redirect_url)
         target = self._accept(invitation)
         return self.request.RESPONSE.redirect(target.absolute_url())
 
