7571b1bca7a8d0756058d8959d080d849d9f515f
Lukas Graf <lukas.graf@4teamwork.ch>
2020-02-18 12:11:52 +0100

    Add smoke tests for rewrite rules and VHost configs.

---
+++ rewrite_rule_smoketests/config_entities.py
@@ -0,0 +1,101 @@
+class AdminUnit(object):
+    """AdminUnit configuration entity.
+
+    Is able to build its most commonly used URLs, and track whether
+    it is a classic GEVER admin unit, or one that is used for a dedicated
+    Teamraum setup.
+
+    Must be linked to a cluster before being used.
+    """
+
+    def __init__(self, unit_id, is_dedicated_teamraum=False):
+        self.unit_id = unit_id
+        self.is_dedicated_teamraum = is_dedicated_teamraum
+
+        self.cluster = None
+
+    def __repr__(self):
+        return "<AdminUnit '%s'>" % self.unit_id
+
+    @property
+    def front_page_url(self):
+        """The topmost URL of GEVER deployment.
+
+        Is usually the URL of the AdminUnit, except in the case where we
+        have a Teamraum redirect.
+        """
+        if self.is_dedicated_teamraum:
+            return self.url + '/workspaces'
+        return self.url
+
+    @property
+    def url(self):
+        """The AdminUnit's URL.
+        """
+        if self.cluster.single_unit_setup:
+            # https://lab.onegovgever.ch
+            return self.cluster.url
+
+        # https://dev.onegovgever.ch/fd
+        return '/'.join((self.cluster.url, self.unit_id))
+
+
+class Cluster(object):
+    """Cluster configuration entity.
+
+    Represents a GEVER cluster (a bunch of deployments that share the same
+    OGDS as well as portal).
+
+    The cluster's primary identifier is its base URL. From this URL, all
+    other URLs are derived.
+    """
+
+    def __init__(self, url, new_portal=False, gever_ui_is_default=False,
+                 admin_units=None):
+        self.url = url
+        self.new_portal = new_portal
+        self.gever_ui_is_default = gever_ui_is_default
+        self.admin_units = admin_units
+
+        # Link admin units to their containing cluster
+        for admin_unit in self.admin_units:
+            admin_unit.cluster = self
+
+        self.validate()
+
+    def __repr__(self):
+        return "<Cluster '%s'>" % self.url
+
+    def validate(self):
+        # We want a homogenous URL style to start building other URLs from
+        # in order to get predictable results.
+        assert not self.url.endswith('/'), \
+            "Cluster URLs must not end with trailing slash."
+
+        assert self.url.startswith('https://'), \
+            "Cluster URLs be the https:// URLs"
+
+    @property
+    def single_unit_setup(self):
+        """Whether this cluster is single-unit setup or not.
+        """
+        return len(self.admin_units) == 1
+
+    @property
+    def portal_url(self):
+        """The base URL to this cluster's portal.
+        """
+        return '/'.join((self.url, 'portal'))
+
+    @property
+    def portal_landingpage_url(self):
+        """The portal's landing page URL.
+
+        This differs slightly for the old vs. new portal.
+        """
+        if self.new_portal:
+            # https://dev.teamraum.ch/portal
+            return self.portal_url
+
+        # https://dev.onegovgever.ch/portal/landingpage
+        return '/'.join((self.portal_url, 'landingpage'))
---
+++ rewrite_rule_smoketests/smoketests.py
@@ -0,0 +1,197 @@
+from os.path import abspath
+from os.path import dirname
+from requests.cookies import create_cookie
+from urllib import quote
+from urlparse import urlparse
+from urlparse import urlunparse
+
+
+# Hack to allow script to use relative imports when run with bin/zopepy
+import sys
+sys.path.append(abspath(dirname(__file__)))
+
+from assertions import assert_equal  # noqa
+from assertions import assert_in  # noqa
+from auth import login_and_store  # noqa
+from auth import login_to_portal  # noqa
+from colors import green  # noqa
+from colors import red  # noqa
+from config import AdminUnit  # noqa
+from config import Cluster  # noqa
+from config import CLUSTERS_TO_TEST  # noqa
+from registry import logged_in  # noqa
+from registry import on_admin_unit  # noqa
+from registry import on_cluster  # noqa
+from registry import tests_by_type  # noqa
+from runner import SmokeTestRunner  # noqa
+
+
+@on_cluster
+@on_admin_unit
+def test_canonical_https_redirect(browser, entity):
+    """Tests for presence of canonical HTTPS redirect.
+
+    All URLs that start with a http:// scheme should be redirected to their
+    https equivalent.
+    """
+    expect_trailing_slash = False
+
+    if isinstance(entity, Cluster):
+        expect_trailing_slash = True
+
+    # In single unit setups canonical HTTPS redirect adds a trailing
+    # slash to the admin unit URL, otherwise it doesn't
+    if isinstance(entity, AdminUnit):
+        if entity.cluster.single_unit_setup:
+            expect_trailing_slash = True
+
+    parsed = urlparse(entity.url)
+    http_url = urlunparse(('http', ) + tuple(parsed)[1:])
+    https_url = urlunparse(('https', ) + tuple(parsed)[1:])
+
+    browser.allow_redirects = False
+    browser.open(http_url)
+
+    expected = https_url
+    if expect_trailing_slash:
+        expected += '/'
+
+    assert_equal(301, browser.status_code)
+    assert_equal(expected, browser.headers['Location'])
+
+
+@on_admin_unit
+def test_teamraum_redirect(browser, admin_unit):
+    if not admin_unit.is_dedicated_teamraum:
+        return
+
+    browser.allow_redirects = False
+    browser.open(admin_unit.url)
+    assert_equal(302, browser.status_code)
+    assert_equal(admin_unit.front_page_url, browser.headers['Location'])
+    assert_equal(True, browser.headers['Location'].endswith('/workspaces'))
+
+
+@on_admin_unit
+def test_anonymous_cas_redirect(browser, admin_unit):
+    """Tests that requests to GEVER without authentication cause a redirect
+
+    to the CAS portal.
+    """
+    browser.allow_redirects = False
+    browser.open(admin_unit.front_page_url)
+
+    cluster = admin_unit.cluster
+
+    if cluster.gever_ui_is_default:
+        # When the new UI is the default, we won't see a CAS redirect
+        # performed by Plone for Anonymous requests. Instead, the frontend
+        # app's index.html is served, and the frontend will trigger the CAS
+        # redirect.
+        assert_equal(200, browser.status_code)
+        assert_in('/geverui/assets/', browser.contents)
+    else:
+        assert_equal(302, browser.status_code)
+
+        service_url = quote(admin_unit.front_page_url + '/')
+        cas_login_url = '%s/login?service=%s' % (cluster.portal_url, service_url)
+        assert_equal(cas_login_url, browser.headers['Location'])
+
+
+@on_cluster
+def test_portal(browser, cluster):
+    browser.open(cluster.portal_url)
+    assert_equal(200, browser.status_code)
+
+    if cluster.new_portal:
+        # New portal
+        assert_equal(cluster.portal_url, browser.url)
+        assert_equal(200, browser.status_code)
+        assert_in('<script src=/portal/assets/js/app', browser.contents)
+    else:
+        # Old portal
+        username_field = browser.find('Benutzername')
+        assert_equal('username', username_field.attrib['name'])
+
+        password_field = browser.find('Passwort')
+        assert_equal('password', password_field.attrib['name'])
+
+
+def test_portal_login(browser, cluster):
+    """TODO: Figure out how to conveniently run this test.
+
+    It's currently unregistered because it needs an actual password to test
+    the login, and can therefore only run in --prompt-credentials mode.
+
+    It therefore is unregistered for the moment, and doesn't run.
+    """
+    username = 'foo'
+    password = 'bar'
+
+    logged_in_user, cookies = login_to_portal(browser, cluster, username, password)
+
+    if cluster.new_portal:
+        assert_equal(200, browser.status_code)
+        assert_equal({
+            'username': username,
+            'state': 'logged_in',
+            'invitation_callback': ''},
+            browser.json
+        )
+        assert_in('sessionid', browser.cookies)
+
+    else:
+        assert_equal(200, browser.status_code)
+        assert_equal(cluster.portal_landingpage_url, browser.url)
+
+
+@logged_in
+@on_cluster
+def test_portal_landingpage(browser, cluster):
+    browser.allow_redirects = False
+    browser.open(cluster.portal_landingpage_url)
+    assert_equal(200, browser.status_code)
+
+    if cluster.new_portal:
+        # Can't really assert on much more for the new portal.
+        assert_in('csrftoken', browser.cookies)
+    else:
+        logged_in_msg = browser.css('.current-user-info').first.text
+        logged_in_msg.startswith('Angemeldet als')
+
+
+@logged_in
+@on_admin_unit
+def test_gever_front_page_logged_in_old_ui(browser, admin_unit):
+    driver = browser.get_driver()
+
+    cookie_data = {'name': 'geverui', 'value': '0'}
+    cookie = create_cookie(**cookie_data)
+    driver.requests_session.cookies.set_cookie(cookie)
+
+    browser.open(admin_unit.front_page_url)
+    first_heading = browser.css('.documentFirstHeading').first.text
+
+    if admin_unit.is_dedicated_teamraum:
+        marker = u'Teamr\xe4ume'
+    else:
+        marker = u'Pers\xf6nliche \xdcbersicht'
+
+    assert_equal(True, first_heading.startswith(marker))
+
+    logo = browser.css('a#portal-logo').first
+    assert_equal(admin_unit.url, logo.attrib['href'])
+
+
+def main():
+    prompt_credentials = False
+    if '--prompt-credentials' in sys.argv:
+        prompt_credentials = True
+
+    runner = SmokeTestRunner(CLUSTERS_TO_TEST, tests_by_type,
+                             prompt_credentials=prompt_credentials)
+    runner.run_tests()
+
+
+if __name__ == '__main__':
+    main()
