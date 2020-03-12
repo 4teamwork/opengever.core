e597cf7f162d769baa491fb3d2739a555e3c85c2
Niklaus Johner <niklaus.johner@4teamwork.ch>
2020-03-12 17:35:30 +0100

    Reorganise OGDSService tests.

--- opengever/ogds/models/tests/test_service.py
+++ opengever/ogds/models/tests/test_service.py
@@ -2,12 +2,10 @@ from opengever.ogds.models.exceptions import RecordNotFound
 from opengever.ogds.models.tests.base import OGDSTestCase
 
 
-class TestOGDSService(OGDSTestCase):
+class TestOGDSServiceUserMehtods(OGDSTestCase):
+
     def test_fetch_user_by_id(self):
         self.assertEqual(self.john, self.service.fetch_user('john'))
-
-    def test_fetch_group_by_id(self):
-        self.assertEqual(self.members_a, self.service.fetch_group('members_a'))
 
     def test_fetch_user_returns_none_when_no_user_found(self):
         self.assertEqual(None, self.service.fetch_user('foobar'))
@@ -40,18 +38,29 @@ class TestOGDSService(OGDSTestCase):
         self.assertEqual([self.john, self.jack], self.service.filter_users(['smith']).all())
 
 
-class TestOrgUnitCounters(OGDSTestCase):
-    def test_has_multiple_org_units(self):
-        self.assertTrue(self.service.has_multiple_org_units())
+class TestOGDSServiceGroupMethods(OGDSTestCase):
 
-    def test_falsy_multiple_org_units(self):
-        self.session.delete(self.org_unit_b)
-        self.session.delete(self.org_unit_c)
-        self.session.delete(self.org_unit_d)
-        self.assertFalse(self.service.has_multiple_org_units())
+    def test_fetch_group_by_id(self):
+        self.assertEqual(self.members_a, self.service.fetch_group('members_a'))
+
+    def test_all_groups_returns_active_groups_only_by_default(self):
+        expected_groups = [self.members_a, self.members_b, self.members_c, self.inbox_members]
+        self.assertItemsEqual(expected_groups, self.service.all_groups())
+
+    def test_all_groups_includes_inactive_groups_when_active_flag_is_false(self):
+        expected_groups = [self.members_a, self.members_b, self.members_c, self.inbox_members, self.inactive_members]
+        self.assertItemsEqual(expected_groups, self.service.all_groups(active_only=False))
+
+    def test_assigned_groups_returns_empty_list_if_no_groups_are_assigned(self):
+        self.assertEqual([], self.service.assigned_groups('bob'))
+
+    def test_assigned_groups_returns_a_list_of_multiple_groups(self):
+        groups = self.service.assigned_groups('john')
+        self.assertSequenceEqual([self.members_a, self.inbox_members], groups)
 
 
-class TestServiceOrgUnitMethods(OGDSTestCase):
+class TestOGDSServiceAdminUnitMethods(OGDSTestCase):
+
     def test_has_multiple_admin_units(self):
         self.assertTrue(self.service.has_multiple_admin_units())
 
@@ -59,12 +68,6 @@ class TestServiceOrgUnitMethods(OGDSTestCase):
         self.admin_unit_1.enabled = False
         self.admin_unit_2.enabled = False
         self.assertFalse(self.service.has_multiple_admin_units())
-
-    def test_fetch_org_unit_by_unit_id(self):
-        self.assertEqual(self.org_unit_c, self.service.fetch_org_unit('unitc'))
-
-    def test_fetch_org_unit_returns_none_when_no_org_unit_is_found(self):
-        self.assertIsNone(self.service.fetch_org_unit('not-existing-unit'))
 
     def test_fetch_admin_unit_by_unit_id(self):
         self.assertEqual(self.admin_unit_1, self.service.fetch_admin_unit('canton_1'))
@@ -75,16 +78,34 @@ class TestServiceOrgUnitMethods(OGDSTestCase):
     def test_fetch_not_existing_admin_unit_returns_none(self):
         self.assertIsNone(self.service.fetch_admin_unit('admin_xx'))
 
+    def test_all_admin_units_returns_a_list_of_all_enabled_admin_units(self):
+        self.assertSequenceEqual([self.admin_unit_1, self.admin_unit_2], self.service.all_admin_units())
+
+    def test_all_admin_units_includes_disabled_orgunits_when_flag_is_set(self):
+        expected_admin_units = [self.admin_unit_1, self.admin_unit_2, self.admin_unit_3]
+        self.assertSequenceEqual(expected_admin_units, self.service.all_admin_units(enabled_only=False))
+
+
+class TestOGDSServiceOrgUnitMethods(OGDSTestCase):
+
+    def test_has_multiple_org_units(self):
+        self.assertTrue(self.service.has_multiple_org_units())
+
+    def test_falsy_multiple_org_units(self):
+        self.session.delete(self.org_unit_b)
+        self.session.delete(self.org_unit_c)
+        self.session.delete(self.org_unit_d)
+        self.assertFalse(self.service.has_multiple_org_units())
+
+    def test_fetch_org_unit_by_unit_id(self):
+        self.assertEqual(self.org_unit_c, self.service.fetch_org_unit('unitc'))
+
+    def test_fetch_org_unit_returns_none_when_no_org_unit_is_found(self):
+        self.assertIsNone(self.service.fetch_org_unit('not-existing-unit'))
+
     def test_assigned_org_units_returns_a_list_of_orgunit(self):
         units = self.service.assigned_org_units('hugo')
         self.assertSequenceEqual([self.org_unit_a, self.org_unit_b], units)
-
-    def test_assigned_groups_returns_empty_list_if_no_groups_are_assigned(self):
-        self.assertEqual([], self.service.assigned_groups('bob'))
-
-    def test_assigned_groups_returns_a_list_of_multiple_groups(self):
-        groups = self.service.assigned_groups('john')
-        self.assertSequenceEqual([self.members_a, self.inbox_members], groups)
 
     def test_all_org_units_returns_list_of_all_orgunits(self):
         units = self.service.all_org_units()
@@ -93,18 +114,3 @@ class TestServiceOrgUnitMethods(OGDSTestCase):
     def test_all_org_units_includes_disabled_orgunits_when_flag_is_set(self):
         units = self.service.all_org_units(enabled_only=False)
         self.assertSequenceEqual([self.org_unit_a, self.org_unit_b, self.org_unit_c, self.org_unit_d], units)
-
-    def test_all_admin_units_returns_a_list_of_all_enabled_admin_units(self):
-        self.assertSequenceEqual([self.admin_unit_1, self.admin_unit_2], self.service.all_admin_units())
-
-    def test_all_admin_units_includes_disabled_orgunits_when_flag_is_set(self):
-        expected_admin_units = [self.admin_unit_1, self.admin_unit_2, self.admin_unit_3]
-        self.assertSequenceEqual(expected_admin_units, self.service.all_admin_units(enabled_only=False))
-
-    def test_all_groups_returns_active_groups_only_by_default(self):
-        expected_groups = [self.members_a, self.members_b, self.members_c, self.inbox_members]
-        self.assertItemsEqual(expected_groups, self.service.all_groups())
-
-    def test_all_groups_includes_inactive_groups_when_active_flag_is_false(self):
-        expected_groups = [self.members_a, self.members_b, self.members_c, self.inbox_members, self.inactive_members]
-        self.assertItemsEqual(expected_groups, self.service.all_groups(active_only=False))
