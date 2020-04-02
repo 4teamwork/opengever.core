0cfb34644d8c45d5e9fbaa906a9505fea8d0dbdb
Niklaus Johner <niklaus.johner@4teamwork.ch>
2020-04-02 19:36:55 +0200

    Fix CreateDocumentFromTemplateCommand.
    The CreateDocumentFromTemplateCommand changes the docproperties
    just after creating the object. This seems to break the indexing
    of the blob in solr.

--- opengever/dossier/tests/test_command.py
+++ opengever/dossier/tests/test_command.py
@@ -1,12 +1,17 @@
+from docx import Document
+from docxcompose.properties import CustomProperties
 from ftw.builder import Builder
 from ftw.builder import create
-from opengever.testing import IntegrationTestCase
+from opengever.document.docprops import TemporaryDocFile
 from opengever.dossier.behaviors.dossier import IDossierMarker
-from opengever.dossier.command import CreateDossierFromTemplateCommand
 from opengever.dossier.command import CreateDocumentFromTemplateCommand
+from opengever.dossier.command import CreateDossierFromTemplateCommand
+from opengever.testing import IntegrationTestCase
 
 
 class TestCreateDocumentFromTemplateCommand(IntegrationTestCase):
+
+    features = ('doc-properties',)
 
     def test_create_document_from_template(self):
         expected_title = 'My title'
@@ -26,6 +31,33 @@ class TestCreateDocumentFromTemplateCommand(IntegrationTestCase):
         document = command.execute()
         self.assertEqual(expected_title, document.title)
         self.assertIsNone(document.file)
+
+    def test_create_document_from_template_updates_docproperties(self):
+        expected_title = 'My title'
+        self.login(self.regular_user)
+        template = create(Builder('document')
+                          .within(self.dossiertemplate)
+                          .with_asset_file('with_gever_properties.docx'))
+
+        with TemporaryDocFile(template.file) as tmpfile:
+            template_properties = CustomProperties(Document(tmpfile.path)).items()
+        self.assertItemsEqual(
+            [('ogg.dossier.reference_number', 'Client1 / 2')],
+            template_properties)
+
+        command = CreateDocumentFromTemplateCommand(
+            self.dossier, template, expected_title)
+        document = command.execute()
+
+        with TemporaryDocFile(document.file) as tmpfile:
+            properties = CustomProperties(Document(tmpfile.path))
+
+        self.assertEquals(
+            'Client1 1.1 / 1',
+            properties['ogg.dossier.reference_number'])
+        self.assertEquals(
+            u'B\xe4rfuss K\xe4thi',
+            properties['ogg.user.title'])
 
 
 class TestCreateDossierFromTemplateCommand(IntegrationTestCase):
