1ddc476029c7046b70e937c97520e5bb93c9923c
Niklaus Johner <niklaus.johner@4teamwork.ch>
2020-04-02 17:35:52 +0200

    Add is_emm_environment to config endpoint.

--- opengever/api/config.py
+++ opengever/api/config.py
@@ -1,4 +1,5 @@
 from opengever.base.interfaces import IGeverSettings
+from opengever.officeconnector.helpers import is_client_ip_in_office_connector_disallowed_ip_range
 from plone.restapi.services import Service
 
 
@@ -6,7 +7,12 @@ class Config(Service):
     """GEVER configuration"""
 
     def reply(self):
-        return IGeverSettings(self.context).get_config()
+        config = IGeverSettings(self.context).get_config()
+        self.add_additional_infos(config)
+        return config
 
     def check_permission(self):
         return
+
+    def add_additional_infos(self, config):
+        config['is_emm_environment'] = is_client_ip_in_office_connector_disallowed_ip_range()
