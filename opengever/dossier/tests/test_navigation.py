299f8d7434c87eda29c6276347f61aa5efb4666c
Thomas Buchberger <t.buchberger@4teamwork.ch>
2020-06-02 18:42:56 +0200

    Use Solr for dossier navigation in overview

--- opengever/dossier/tests/test_navigation.py
+++ opengever/dossier/tests/test_navigation.py
@@ -1,121 +1,88 @@
 from ftw.builder import Builder
 from ftw.builder import create
 from ftw.testbrowser import browsing
-from opengever.testing import FunctionalTestCase
+from opengever.testing import SolrIntegrationTestCase
 from plone.uuid.interfaces import IUUID
 
 
-class TestNavigation(FunctionalTestCase):
-
-    def setUp(self):
-        super(TestNavigation, self).setUp()
-        self.root = create(Builder('repository_root'))
-        self.folder = create(Builder('repository').within(self.root))
-        self.main_dossier = create(Builder('dossier')
-                                   .titled(u'Main')
-                                   .having(description=u'The main dossier')
-                                   .within(self.folder))
+class TestNavigation(SolrIntegrationTestCase):
 
     @browsing
     def test_dossier_navigation_json_is_valid(self, browser):
-        subdossier = create(Builder('dossier')
-                            .titled(u'Subdossier')
-                            .having(description=u'The sub-dossier')
-                            .within(self.main_dossier))
+        self.login(self.regular_user, browser=browser)
+        browser.visit(self.subdossier, view='dossier_navigation.json')
 
-        browser.login().visit(self.main_dossier,
-                              view='dossier_navigation.json')
-        self.assert_json_equal(
-            [{"text": "Main",
-              "description": "The main dossier",
-              "uid": IUUID(self.main_dossier),
-              "url": self.main_dossier.absolute_url(),
-              "nodes": [{"text": "Subdossier",
-                         "description": "The sub-dossier",
+        self.assertEqual(
+            [{"text": self.subdossier.Title(),
+              "description": self.subdossier.Description(),
+              "uid": IUUID(self.subdossier),
+              "url": self.subdossier.absolute_url(),
+              "nodes": [{"text": self.subsubdossier.Title(),
+                         "description": self.subsubdossier.Description(),
                          "nodes": [],
-                         "uid": IUUID(subdossier),
-                         "url": subdossier.absolute_url(),
+                         "uid": IUUID(self.subsubdossier),
+                         "url": self.subsubdossier.absolute_url(),
                          }],
               }],
             browser.json)
 
     @browsing
     def test_empty_dossier_navigation(self, browser):
-        browser.login().visit(self.main_dossier, view='dossier_navigation.json')
+        self.login(self.regular_user, browser=browser)
+        browser.visit(self.empty_dossier, view='dossier_navigation.json')
         self.assertEqual([], browser.json)
 
     @browsing
     def test_dossiers_are_sorted_by_title(self, browser):
-        sub1 = create(Builder('dossier')
-                      .titled(u'XXX')
-                      .having(description=u'The XXX sub-dossier')
-                      .within(self.main_dossier))
+        self.login(self.regular_user, browser=browser)
+        browser.visit(self.dossier, view='dossier_navigation.json')
 
-        sub2 = create(Builder('dossier')
-                      .titled(u'AAA')
-                      .having(description=u'The AAA sub-dossier')
-                      .within(self.main_dossier))
-
-        browser.login().visit(self.main_dossier,
-                              view='dossier_navigation.json')
-        self.assert_json_equal(
-            [{"text": "Main",
-              "description": "The main dossier",
-              "uid": IUUID(self.main_dossier),
-              "url": self.main_dossier.absolute_url(),
-              "nodes": [{"text": "AAA",
-                         "description": "The AAA sub-dossier",
-                         "nodes": [],
-                         "uid": IUUID(sub2),
-                         "url": sub2.absolute_url(),
-                         },
-                        {"text": "XXX",
-                         "description": "The XXX sub-dossier",
-                         "nodes": [],
-                         "uid": IUUID(sub1),
-                         "url": sub1.absolute_url(),
-                         }],
-              }],
-            browser.json)
+        nodes = browser.json[0]['nodes']
+        self.assertEqual(
+            [n['text'] for n in nodes], sorted([n['text'] for n in nodes]))
 
     @browsing
     def test_only_show_active_subdossiers(self, browser):
+        self.login(self.regular_user, browser=browser)
+
         sub1 = create(Builder('dossier')
                       .titled(u'active')
                       .having(description=u'The active sub-dossier')
-                      .within(self.main_dossier))
+                      .within(self.empty_dossier))
 
         create(Builder('dossier')
                .titled(u'archived')
                .having(description=u'The archived sub-dossier')
                .in_state('dossier-state-archived')
-               .within(self.main_dossier))
+               .within(self.empty_dossier))
 
         create(Builder('dossier')
                .titled(u'inactive(storniert)')
                .having(description=u'The inactive sub-dossier')
                .in_state('dossier-state-inactive')
-               .within(self.main_dossier))
+               .within(self.empty_dossier))
 
         create(Builder('dossier')
                .titled(u'resolved(abgeschlossen)')
                .having(description=u'The resolved sub-dossier')
                .in_state('dossier-state-resolved')
-               .within(self.main_dossier))
+               .within(self.empty_dossier))
 
-        browser.login().visit(self.main_dossier,
-                              view='dossier_navigation.json')
-        self.assert_json_equal(
-            [{"text": "Main",
-              "description": "The main dossier",
-              "uid": IUUID(self.main_dossier),
-              "url": self.main_dossier.absolute_url(),
-              "nodes": [
-                        {"text": "active",
-                         "description": "The active sub-dossier",
-                         "nodes": [],
-                         "uid": IUUID(sub1),
-                         "url": sub1.absolute_url(),
-                         }]
-              }],
+        self.commit_solr()
+        browser.visit(self.empty_dossier, view='dossier_navigation.json')
+
+        self.assertEqual(
+            [{
+                "text": self.empty_dossier.Title(),
+                "description": self.empty_dossier.Description(),
+                "uid": IUUID(self.empty_dossier),
+                "url": self.empty_dossier.absolute_url(),
+                "nodes": [{
+                    "text": "active",
+                    "description": "The active sub-dossier",
+                    "nodes": [],
+                    "uid": IUUID(sub1),
+                    "url": sub1.absolute_url(),
+                }],
+            }],
             browser.json)
