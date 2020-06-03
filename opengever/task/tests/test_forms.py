39ab988dbd2369dd9580c500eb786ee46f99efde
Niklaus Johner <niklaus.johner@4teamwork.ch>
2020-06-03 08:51:24 +0200

    Omit informed_principals field when activity feature is disabled.

--- opengever/task/tests/test_forms.py
+++ opengever/task/tests/test_forms.py
@@ -26,9 +26,16 @@ class TestFormFields(IntegrationTestCase):
             browser.css('input#form-widgets-date_of_completion').first.type)
 
     @browsing
-    def test_informed_principals_is_only_shown_in_add_form(self, browser):
+    def test_informed_principals_is_only_shown_in_add_form_with_activity_enabled(self, browser):
         self.login(self.dossier_responsible, browser=browser)
 
+        browser.open(self.dossier, view='++add++opengever.task.task')
+        self.assertEqual(0, len(browser.css('select#form-widgets-informed_principals')))
+
+        browser.open(self.seq_subtask_1, view='edit')
+        self.assertEqual(0, len(browser.css('select#form-widgets-informed_principals')))
+
+        self.activate_feature('activity')
         browser.open(self.dossier, view='++add++opengever.task.task')
         self.assertEqual(1, len(browser.css('select#form-widgets-informed_principals')))
 
