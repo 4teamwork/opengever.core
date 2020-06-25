7d87766afe62fcbdcbbdbecd03e90bb89d839596
Lukas Graf <lukas.graf@4teamwork.ch>
2020-06-25 14:32:55 +0200

    Merge pull request #6522 from 4teamwork/lg-fix-populate-filename-favorites-upgrade
    Make "populate_filename_column_in_favorites" UpgradeStep more robust:

--- opengever/core/upgrades/20200508163417_populate_filename_column_in_favorites/upgrade.py
+++ opengever/core/upgrades/20200508163417_populate_filename_column_in_favorites/upgrade.py
@@ -1,4 +1,5 @@
 from opengever.core.upgrade import SQLUpgradeStep
+from os.path import splitext
 from plone import api
 from sqlalchemy.sql.expression import column
 from sqlalchemy.sql.expression import select
@@ -15,6 +16,13 @@ favorites_table = table(
     column('admin_unit_id'),
     column('filename'),
 )
+
+MAX_FILENAME_BASE_LENGTH = 100
+
+
+def safe_unicode(value):
+    if isinstance(value, str):
+        return value.decode('utf-8')
 
 
 class PopulateFilenameColumnInFavorites(SQLUpgradeStep):
@@ -35,7 +43,13 @@ class PopulateFilenameColumnInFavorites(SQLUpgradeStep):
         query = {"object_provides": "opengever.document.behaviors.IBaseDocument",
                  "UID": favorite_uids}
         for brain in self.catalog_unrestricted_search(query=query):
-            filename = brain.filename or u""
+            # Defensively truncate filename to max 105 chars total
+            filename = safe_unicode(brain.filename or u"")
+            if filename:
+                basename, ext = splitext(filename)
+                basename = basename[:MAX_FILENAME_BASE_LENGTH]
+                filename = u''.join((basename, ext))
+
             self.execute(
                 favorites_table.update()
                 .values(filename=filename)
