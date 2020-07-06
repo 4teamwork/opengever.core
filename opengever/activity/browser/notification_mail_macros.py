2e0a9ca0022c6d0e6d634cc3ea31e8bc113caaca
Martin Bächtold <martin@baechtold.me>
2020-07-06 17:00:00 +0200

    Remove empty class.
    The template was defined twice, once in ZCML and once in the class. Since the class does nothing special, it may be removed.
    
    If we don't do that, we cannot reuse the macro.

--- opengever/activity/browser/notification_mail_macros.py
+++
@@ -1,7 +0,0 @@
-from Products.Five.browser import BrowserView
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-
-
-class View(BrowserView):
-
-    template = ViewPageTemplateFile('templates/notification_mail_macros.pt')
