7e5d5c3e36f7ecb9e3d34a7ac197f117ea0f3931
Niklaus Johner <niklaus.johner@4teamwork.ch>
2020-07-10 09:41:12 +0200

    Add TeasersSettings to registry to allow hiding teasers.

--- opengever/base/tests/test_teaser.py
+++ opengever/base/tests/test_teaser.py
@@ -1,6 +1,7 @@
 from ftw.testbrowser import browsing
-from opengever.testing import IntegrationTestCase
 from opengever.ogds.models.user_settings import UserSettings
+from opengever.testing import IntegrationTestCase
+from plone import api
 
 
 class TestTeaser(IntegrationTestCase):
@@ -27,6 +28,23 @@ class TestTeaser(IntegrationTestCase):
     @browsing
     def test_new_frontend_teaser_is_not_shown_if_gever_ui_is_disabled(self, browser):
         self.deactivate_feature('gever_ui')
+        self.login(self.regular_user, browser=browser)
+        browser.visit(self.portal)
+        self.assertEqual([], browser.css('#new-frontend-teaser'))
+
+    @browsing
+    def test_new_frontend_teaser_is_not_shown_if_teasers_are_disabled(self, browser):
+        api.portal.set_registry_record(
+            'opengever.base.interfaces.ITeasersSettings.show_teasers', False)
+        self.login(self.regular_user, browser=browser)
+        browser.visit(self.portal)
+        self.assertEqual([], browser.css('#new-frontend-teaser'))
+
+    @browsing
+    def test_new_frontend_teaser_is_not_shown_if_in_teasers_to_hide(self, browser):
+        api.portal.set_registry_record(
+            'opengever.base.interfaces.ITeasersSettings.teasers_to_hide',
+            [u'be_new_frontend_teaser'])
         self.login(self.regular_user, browser=browser)
         browser.visit(self.portal)
         self.assertEqual([], browser.css('#new-frontend-teaser'))
--- opengever/base/viewlets/teaser.py
+++ opengever/base/viewlets/teaser.py
@@ -1,4 +1,5 @@
 from opengever.base.interfaces import IGeverUI
+from opengever.base.interfaces import ITeasersSettings
 from opengever.ogds.models.user import User
 from plone import api
 from plone.app.layout.viewlets.common import ViewletBase
@@ -11,16 +12,28 @@ class TeaserViewlet(ViewletBase):
 
     tour_keys = ['be_new_frontend_teaser']
 
-    def unseen_tours(self):
+    def tours_to_show(self):
         userid = api.user.get_current().id
         ogds_user = User.query.filter_by(userid=userid).one_or_none()
-        unseen_tours = []
+
+        tours_to_show = []
         if ogds_user:
             seen_tours = ogds_user.user_settings.seen_tours if ogds_user.user_settings else []
             for key in self.tour_keys:
-                if key not in seen_tours:
-                    unseen_tours.append(key)
-        return unseen_tours
+                if key not in seen_tours and key not in self.teasers_to_hide:
+                    tours_to_show.append(key)
+
+        return tours_to_show
 
     def is_ui_feature_enabled(self):
-        return api.portal.get_registry_record('is_feature_enabled', interface=IGeverUI)
+        return api.portal.get_registry_record(
+            'is_feature_enabled', interface=IGeverUI)
+
+    def show_viewlet(self):
+        return api.portal.get_registry_record(
+            'show_teasers', interface=ITeasersSettings)
+
+    @property
+    def teasers_to_hide(self):
+        return api.portal.get_registry_record(
+            'teasers_to_hide', interface=ITeasersSettings)
