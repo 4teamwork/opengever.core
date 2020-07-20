d7027db708c9e46084bb318623a57d8f0aeb5ebc
Niklaus Johner <niklaus.johner@4teamwork.ch>
2020-07-20 11:58:31 +0200

    Add tests that content creation commands respect behaviors.

--- opengever/base/tests/test_command.py
+++ opengever/base/tests/test_command.py
@@ -1,6 +1,11 @@
 from opengever.base.command import CreateDocumentCommand
 from opengever.base.command import CreateEmailCommand
+from opengever.document.behaviors.related_docs import IRelatedDocuments
 from opengever.testing import IntegrationTestCase
+from z3c.relationfield.relation import RelationValue
+from zope.annotation.interfaces import IAnnotations
+from zope.component import getUtility
+from zope.intid.interfaces import IIntIds
 
 
 class MockMsg2MimeTransform(object):
@@ -37,3 +42,16 @@ class TestCreateDocumentCommand(IntegrationTestCase):
         self.assertEqual(u'\xdcnicode', document.title)
         self.assertEqual('buh!', document.file.data)
         self.assertEqual('text/plain', document.file.contentType)
+
+    def test_create_document_from_command_respects_behaviors(self):
+        self.login(self.regular_user)
+
+        relation = RelationValue(getUtility(IIntIds).getId(self.subdocument))
+        command = CreateDocumentCommand(
+            self.dossier, 'testm\xc3\xa4il.txt', 'buh!', relatedItems=[relation])
+        document = command.execute()
+
+        self.assertIn(
+            'opengever.document.behaviors.related_docs.IRelatedDocuments.relatedItems',
+            IAnnotations(document))
+        self.assertIn(relation, IRelatedDocuments(document).relatedItems)
