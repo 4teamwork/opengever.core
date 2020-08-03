67110a759fd6bc083412756aedd62a2e5e8b6ae1
David Erni <d.erni@4teamwork.ch>
2020-08-03 17:19:25 +0200

    Add basic smoke-tests for solr-maintenance sync.

--- opengever/base/tests/test_solr_maintenance.py
+++ opengever/base/tests/test_solr_maintenance.py
@@ -22,29 +22,18 @@ class TestSolrMaintenanceView(SolrIntegrationTestCase):
     def _get_solr_index_handler(self, obj):
         return getMultiAdapter((obj, self.solr_manager), ISolrIndexHandler)
 
-    def test_default_fixture_has_empty_diff(self):
-        self.login(self.manager)
-
-        not_in_catalog, not_in_solr, not_in_sync = self.solr_maintenance.diff()
-        self.assertEqual(set(), not_in_catalog)
-        self.assertEqual(set(), not_in_solr)
-        self.assertEqual([], not_in_sync)
-
-    def test_solr_diff_picks_up_docs_with_empty_created_fields(self):
-        self.login(self.manager)
-
-        document_uid = IUUID(self.document)
-        solr_query = 'UID:{}'.format(document_uid)
-
-        handler = self._get_solr_index_handler(self.document)
+    def _create_broken_partial_solr_doc(self, obj):
+        connection = self.get_solr_connection()
+        obj_uid = IUUID(obj)
+        solr_query = 'UID:{}'.format(obj_uid)
+        handler = self._get_solr_index_handler(obj)
 
         # make sure doc is in solr as expected
         resp = self.solr_search.search(query=solr_query)
         self.assertEqual(1, resp.num_found)
 
         # remove document from solr
-        connection = self.get_solr_connection()
-        connection.delete(IUUID(self.document))
+        connection.delete(obj_uid)
         self.commit_solr()
 
         resp = self.solr_search.search(query=solr_query)
@@ -58,7 +47,22 @@ class TestSolrMaintenanceView(SolrIntegrationTestCase):
         connection.add(atomic_data)
         self.commit_solr()
 
+    def test_default_fixture_has_empty_diff(self):
+        self.login(self.manager)
+
+        not_in_catalog, not_in_solr, not_in_sync = self.solr_maintenance.diff()
+        self.assertEqual(set(), not_in_catalog)
+        self.assertEqual(set(), not_in_solr)
+        self.assertEqual([], not_in_sync)
+
+    def test_solr_diff_picks_up_docs_with_empty_created_fields(self):
+        self.login(self.manager)
+
+        self._create_broken_partial_solr_doc(self.document)
+        document_uid = IUUID(self.document)
+
         # test solr query detects documents with missing created field
+        connection = self.get_solr_connection()
         incomplete = connection.search({
             u'query': u'-created:[* TO *]',
             u'limit': 10000000,
@@ -72,6 +76,19 @@ class TestSolrMaintenanceView(SolrIntegrationTestCase):
         self.assertEqual(set(), not_in_catalog)
         self.assertEqual(set(), not_in_solr)
         self.assertEqual([document_uid], not_in_sync)
+
+    def test_solr_sync_fixes_docs_with_empty_created_fields(self):
+        self.login(self.manager)
+
+        self._create_broken_partial_solr_doc(self.document)
+
+        not_in_catalog, not_in_solr, not_in_sync = self.solr_maintenance.diff()
+        self.assertEqual(1, len(not_in_sync))
+
+        self.solr_maintenance.sync()
+
+        not_in_catalog, not_in_solr, not_in_sync = self.solr_maintenance.diff()
+        self.assertEqual(0, len(not_in_sync))
 
     def test_solr_diff_picks_up_docs_not_in_solr(self):
         self.login(self.manager)
