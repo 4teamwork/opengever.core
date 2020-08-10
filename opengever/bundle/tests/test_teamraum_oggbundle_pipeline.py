a51d12333555f5cf0c05751bc4d0a855c4b19c4e
Lukas Graf <lukas.graf@4teamwork.ch>
2020-08-10 12:57:21 +0200

    Extend teamraum bundle tests with document and mail.

--- opengever/bundle/tests/test_teamraum_oggbundle_pipeline.py
+++ opengever/bundle/tests/test_teamraum_oggbundle_pipeline.py
@@ -1,6 +1,10 @@
 from Acquisition import aq_parent
 from collective.transmogrifier.transmogrifier import Transmogrifier
+from datetime import date
 from datetime import datetime
+from DateTime import DateTime
+from ftw.testing import freeze
+from opengever.base.behaviors.classification import IClassification
 from opengever.bundle.console import add_guid_index
 from opengever.bundle.loader import GUID_INDEX_NAME
 from opengever.bundle.sections.bundlesource import BUNDLE_INGESTION_SETTINGS_KEY  # noqa
@@ -15,6 +19,7 @@ from plone import api
 from Products.CMFPlone.utils import safe_hasattr
 from zope.annotation import IAnnotations
 import pytz
+import tzlocal
 
 
 FROZEN_NOW = datetime(2016, 12, 20, 9, 40)
@@ -37,6 +42,22 @@ class TestTeamraumOggBundlePipeline(IntegrationTestCase):
         del seq_counters['WorkspaceSequenceNumberGenerator']
         del seq_counters['WorkspaceFolderSequenceNumberGenerator']
 
+    def find_by_title(self, parent, title):
+        matching_children = [child for child in parent.objectValues()
+                             if getattr(child, "title", None) == title]
+        for child in parent.objectValues():
+            object_title = getattr(child, "title", None)
+            if object_title == title:
+                return child
+
+        self.assertTrue(
+            matching_children,
+            msg="did not find object with title: {}".format(title))
+        self.assertEqual(
+            1, len(matching_children),
+            msg="found more than one object with title: {}".format(title))
+        return matching_children[0]
+
     def test_oggbundle_transmogrifier(self):
         # this is a bit hackish, but since builders currently don't work in
         # layer setup/teardown and isolation of database content is ensured
@@ -57,12 +78,15 @@ class TestTeamraumOggBundlePipeline(IntegrationTestCase):
         annotations[BUNDLE_PATH_KEY] = resource_filename(
             'opengever.bundle.tests', 'assets/teamraum.oggbundle')
 
-        transmogrifier(u'opengever.bundle.oggbundle')
+        with freeze(FROZEN_NOW):
+            transmogrifier(u'opengever.bundle.oggbundle')
         bundle = annotations[BUNDLE_KEY]
 
         workspaceroot = self.assert_workspaceroot_created()
         workspace = self.assert_workspace_created(workspaceroot)
         workspacefolder = self.assert_workspacefolder_created(workspace)
+        self.assert_document_created(workspacefolder)
+        self.assert_mail_created(workspace)
 
         self.assert_report_data_collected(bundle)
 
@@ -147,6 +171,93 @@ class TestTeamraumOggBundlePipeline(IntegrationTestCase):
             index_data_for(workspacefolder)[GUID_INDEX_NAME])
         return workspacefolder
 
+    def assert_document_created(self, parent):
+        document1 = self.find_by_title(parent, u'Bewerbung Hanspeter M\xfcller')
+
+        self.assertTrue(document1.digitally_available)
+        self.assertIsNotNone(document1.file)
+        self.assertEqual(22198, len(document1.file.data))
+
+        self.assertEqual(
+            u'david.erni',
+            document1.document_author)
+        self.assertEqual(
+            date(2007, 1, 1),
+            document1.document_date)
+        self.assertEqual(
+            document1.modified(),
+            DateTime("2019-12-05T14:09:59.240726"))
+        self.assertEqual(
+            document1.changed,
+            datetime(2019, 12, 5, 14, 9, 59, 240726, tzinfo=tzlocal.get_localzone()))
+
+        self.assertEqual(
+            tuple(),
+            document1.keywords)
+        self.assertTrue(
+            document1.preserved_as_paper)
+        self.assertEqual(
+            [],
+            document1.relatedItems)
+        self.assertEqual(
+            'document-state-draft',
+            api.content.get_state(document1))
+        self.assertEqual(
+            IAnnotations(document1)[BUNDLE_GUID_KEY],
+            index_data_for(document1)[GUID_INDEX_NAME])
+
+    def assert_mail_created(self, parent):
+        mail = self.find_by_title(parent, u'Ein Mail')
+
+        self.assertTrue(mail.digitally_available)
+        self.assertIsNotNone(mail.message)
+        self.assertEqual(920, len(mail.message.data))
+
+        self.assertEqual(
+            u'Peter Muster <from@example.org>',
+            mail.document_author)
+
+        # Setting the document date in the bundle for an e-mail has no effect
+        # The date from the E-mail is used instead
+        self.assertNotEqual(
+            date(2011, 1, 1),
+            mail.document_date)
+        self.assertEqual(
+            date(2013, 1, 1),
+            mail.document_date)
+
+        self.assertEqual(
+            mail.modified(),
+            DateTime("2019-12-05"))
+        self.assertEqual(
+            mail.changed,
+            datetime(2019, 12, 5, tzinfo=tzlocal.get_localzone()))
+
+        self.assertEqual(
+            None,
+            mail.document_type)
+        self.assertEqual(
+            tuple(),
+            mail.keywords)
+        self.assertTrue(
+            mail.preserved_as_paper)
+        self.assertEqual(
+            u'unchecked',
+            IClassification(mail).public_trial)
+        self.assertEqual(
+            u'',
+            IClassification(mail).public_trial_statement)
+        self.assertEqual(
+            FROZEN_NOW.date(),
+            mail.receipt_date)
+        self.assertEqual(
+            'mail-state-active',
+            api.content.get_state(mail))
+
+        self.assertEqual(
+            IAnnotations(mail)[BUNDLE_GUID_KEY],
+            index_data_for(mail)[GUID_INDEX_NAME])
+
     def assert_report_data_collected(self, bundle):
         report_data = bundle.report_data
         metadata = report_data['metadata']
@@ -178,5 +289,5 @@ class TestTeamraumOggBundlePipeline(IntegrationTestCase):
         self.assertEqual(0, len(reporoots))
         self.assertEqual(0, len(repofolders))
         self.assertEqual(0, len(dossiers))
-        self.assertEqual(0, len(documents))
-        self.assertEqual(0, len(mails))
+        self.assertEqual(1, len(documents))
+        self.assertEqual(1, len(mails))
