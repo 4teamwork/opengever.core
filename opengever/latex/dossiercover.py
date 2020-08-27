8a4411ea7ed1c51466e39c3f7d85ac1f6133625d
2e12 <gabriel.beutler@4teamwork.ch>
2020-08-27 10:51:28 +0200

    Make dossiercover export translatable
    The labels in the dossiercover export were hardcoded and therefore translations were implemented to provide a German/French version of the export.

--- opengever/latex/dossiercover.py
+++ opengever/latex/dossiercover.py
@@ -6,10 +6,12 @@ from ftw.pdfgenerator.utils import provide_request_layer
 from ftw.pdfgenerator.view import MakoLaTeXView
 from opengever.base.interfaces import IReferenceNumber
 from opengever.dossier.behaviors.dossier import IDossier
+from opengever.latex import _
 from opengever.ogds.base.utils import get_current_admin_unit
 from opengever.repository.repositoryroot import IRepositoryRoot
 from zope.component import adapter
 from zope.component import getAdapter
+from zope.i18n import translate
 from zope.interface import Interface
 
 
@@ -33,24 +35,37 @@ class DossierCoverLaTeXView(MakoLaTeXView):
 
     template_directories = ['templates']
     template_name = 'dossiercover.tex'
+    translatable_labels = {
+        'label_responsible': _('label_responsible', default='Responsible'),
+        'label_repository_version': _('repository_version', default='Repository version'),
+        'label_reference_number': _('label_reference_number', default='Reference number'),
+        'label_description': _('label_description', default='Description'),
+        'label_start': _('label_start', default='Start'),
+        'label_end': _('label_end', default='End'),
+    }
 
     def get_render_arguments(self):
-        args = {
+        args = self.get_dossier_metadata()
+        args.update(self.get_translated_labels())
+        return args
+
+    def get_dossier_metadata(self):
+        return {
             'clienttitle': self.convert_plain(self.get_current_admin_unit_label()),
-            'repositoryversion': self.convert_plain(
-                self.get_repository_version()),
+            'repositoryversion': self.convert_plain(self.get_repository_version()),
             'referencenr': self.convert_plain(self.get_referencenumber()),
             'title': self.convert_plain(self.context.Title()),
             'description': self.convert(self.get_description()),
             'responsible': self.convert_plain(self.get_responsible()),
+            'start': self.convert_plain(self.context.toLocalizedTime(str(IDossier(self.context).start)) or '-'),
+            'end': self.convert_plain(self.context.toLocalizedTime(str(IDossier(self.context).end)) or '-'),
+        }
 
-            'start': self.convert_plain(self.context.toLocalizedTime(
-                    str(IDossier(self.context).start)) or '-'),
-            'end': self.convert_plain(self.context.toLocalizedTime(
-                    str(IDossier(self.context).end)) or '-'),
-            }
-
-        return args
+    def get_translated_labels(self):
+        labels = {}
+        for key, label in self.translatable_labels.items():
+            labels[key] = translate(label, context=self.request)
+        return labels
 
     def get_description(self):
         return self.context.Description().replace('\n', '<br />')
