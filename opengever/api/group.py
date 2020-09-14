61baff1f57cbee20d7943deae66147ee956d6833
Niklaus Johner <niklaus.johner@4teamwork.ch>
2020-09-14 13:42:47 +0200

    Add copy of GroupsDelete endpoint for customization.

--- opengever/api/group.py
+++ opengever/api/group.py
@@ -7,6 +7,7 @@ from plone.restapi.interfaces import ISerializeToJson
 from plone.restapi.services import Service
 from Products.CMFCore.utils import getToolByName
 from zExceptions import BadRequest
+from zExceptions import NotFound
 from zope.component import queryMultiAdapter
 from zope.component.hooks import getSite
 from zope.interface import alsoProvides
@@ -211,3 +212,43 @@ class GeverGroupsPatch(Service):
         self.update_ogds_group(title, description, self.group.getGroupMemberIds() if users.items() else None)
 
         return self.reply_no_content()
+
+
+@implementer(IPublishTraverse)
+class GeverGroupsDelete(Service):
+    """Copy of plone.restapi.services.groups.delete.GroupsDelete
+    """
+
+    def __init__(self, context, request):
+        super(GeverGroupsDelete, self).__init__(context, request)
+        self.params = []
+
+    def publishTraverse(self, request, name):
+        # Consume any path segments after /@groups as parameters
+        self.params.append(name)
+        return self
+
+    @property
+    def _get_group_id(self):
+        if len(self.params) != 1:
+            raise Exception("Must supply exactly one parameter (group id)")
+        return self.params[0]
+
+    def _get_group(self, group_id):
+        portal = getSite()
+        portal_groups = getToolByName(portal, "portal_groups")
+        return portal_groups.getGroupById(group_id)
+
+    def reply(self):
+
+        portal_groups = getToolByName(self.context, "portal_groups")
+        group = self._get_group(self._get_group_id)
+
+        if not group:
+            raise NotFound("Trying to delete a non-existing group.")
+
+        delete_successful = portal_groups.removeGroup(self._get_group_id)
+        if delete_successful:
+            return self.reply_no_content()
+        else:
+            return self.reply_no_content(status=404)
