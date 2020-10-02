d38ba633a16d385c419004e370fd8e6f81d7e9fe
Thomas Buchberger <t.buchberger@4teamwork.ch>
2020-10-02 14:29:47 +0200

    Add Cadwork mimetypes
    Register mimetypes for Cadwork in mimetypes registry and add them to the
    editable types of Office Connector.
    
    These are not official IANA mimetypes.

---
+++ opengever/core/upgrades/20200930131957_add_cadwork_mime_types/upgrade.py
@@ -0,0 +1,39 @@
+from ftw.upgrade import UpgradeStep
+
+CADWORK_EXTENSIONS = [
+    '.2d',
+    '.2dc',
+    '.2dl',
+    '.2dm',
+    '.2dr',
+    '.3d',
+    '.3dc',
+    '.iv',
+    '.ivx',
+    '.ivz',
+    '.l2d',
+    '.lx2d',
+    '.lxz',
+]
+
+
+class AddCadworkMimeTypes(UpgradeStep):
+    """Add cadwork mime types.
+    """
+
+    def __call__(self):
+        self.install_upgrade_profile()
+
+        mtr = self.getToolByName('mimetypes_registry')
+
+        for obj in self.objects(
+            {
+                'portal_type': 'opengever.document.document',
+                'file_extension': CADWORK_EXTENSIONS,
+            },
+            'Set Cadwork mimetypes'
+        ):
+            mt = mtr.lookupExtension(obj.file.filename)
+            obj.file.contentType = mt.mimetypes[0]
+            # Update catalog metadata (getContentType)
+            obj.reindexObject(idxs=['filename'])
