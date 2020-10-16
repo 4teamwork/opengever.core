70d0c85cbe36d585127a7878ddd8cee6ba25ee80
Lukas Graf <lukas.graf@4teamwork.ch>
2020-10-16 15:25:56 +0200

    Add failing test for plone.protect login handler during readonly mode.

--- opengever/readonly/tests/test_login_during_readonly.py
+++ opengever/readonly/tests/test_login_during_readonly.py
@@ -6,6 +6,7 @@ from opengever.testing.readonly import ZODBStorageInReadonlyMode
 from plone import api
 from plone.app.contentrules import handlers as contentrules_handlers
 from plone.app.testing import TEST_USER_ID
+from plone.protect import subscribers as plone_protect_subscribers
 from Products.PlonePAS.events import UserLoggedInEvent
 import transaction
 
@@ -35,6 +36,22 @@ class TestLoginDuringReadOnlyMode(FunctionalTestCase):
             user = getSecurityManager().getUser()
             event = UserLoggedInEvent(user)
             contentrules_handlers.user_logged_in(event)
+
+            transaction.commit()
+
+        member = membership_tool.getAuthenticatedMember()
+        self.assertEqual(TEST_USER_ID, member.getId())
+
+    @browsing
+    def test_plone_protect_doesnt_prevent_login_during_readonly(self, browser):
+        membership_tool = api.portal.get_tool('portal_membership')
+
+        with ZODBStorageInReadonlyMode():
+            browser.login()
+
+            user = getSecurityManager().getUser()
+            event = UserLoggedInEvent(user)
+            plone_protect_subscribers.onUserLogsIn(event)
 
             transaction.commit()
 
