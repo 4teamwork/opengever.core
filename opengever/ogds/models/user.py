3059cfcc7e77c52c5df2af4bba60fe0fec8c5f2a
Thomas Buchberger <t.buchberger@4teamwork.ch>
2020-11-13 08:19:02 +0100

    Optimize OGDS Sync
    No longer modify every user and group when syncing. Instead determine
    changes and only apply those in OGDS.

--- opengever/ogds/models/user.py
+++ opengever/ogds/models/user.py
@@ -55,11 +55,12 @@ class User(Base):
 
     last_login = Column(Date, index=True)
 
-    column_names_to_sync = {'active', 'firstname', 'lastname', 'directorate',
-                            'directorate_abbr', 'department', 'department_abbr',
-                            'email', 'email2', 'url', 'phone_office', 'phone_fax',
-                            'phone_mobile', 'salutation', 'description', 'address1',
-                            'address2', 'zip_code', 'city', 'country'}
+    column_names_to_sync = {
+        'userid', 'active', 'firstname', 'lastname', 'directorate',
+        'directorate_abbr', 'department', 'department_abbr', 'email', 'email2',
+        'url', 'phone_office', 'phone_fax', 'phone_mobile', 'salutation',
+        'description', 'address1', 'address2', 'zip_code', 'city', 'country',
+    }
 
     # A classmethod property needs to be defined on the metaclass
     class __metaclass__(type(Base)):
