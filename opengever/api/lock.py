57e35c1ea2e09d69eaddac2aab563a0823467d93
Tina Gerber <tina.gerber@4teamwork.ch>
2020-12-10 16:54:27 +0100

    Copy @unlock endpoint from plone.restapi

--- opengever/api/lock.py
+++ opengever/api/lock.py
@@ -1,9 +1,15 @@
 from opengever.base.interfaces import IOpengeverBaseLayer
+from plone.locking.interfaces import ILockable
+from plone.locking.interfaces import INonStealableLock
+from plone.protect.interfaces import IDisableCSRFProtection
 from plone.restapi.interfaces import IExpandableElement
+from plone.restapi.services import Service
 from plone.restapi.services.locking.locking import lock_info
 from zope.component import adapter
+from zope.interface import alsoProvides
 from zope.interface import implementer
 from zope.interface import Interface
+from zope.interface import noLongerProvides
 
 
 @implementer(IExpandableElement)
@@ -22,3 +28,20 @@ class Lock(object):
         if expand:
             result['lock'].update(lock_info(self.context) or {})
         return result
+
+
+class Unlock(Service):
+    """Unlock an object"""
+
+    def reply(self):
+        lockable = ILockable(self.context)
+        if lockable.can_safely_unlock():
+            lockable.unlock()
+
+            if INonStealableLock.providedBy(self.context):
+                noLongerProvides(self.context, INonStealableLock)
+
+            # Disable CSRF protection
+            alsoProvides(self.request, IDisableCSRFProtection)
+
+        return lock_info(self.context)
