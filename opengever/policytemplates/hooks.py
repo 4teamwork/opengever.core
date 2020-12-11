fac7c0c478c0e22d7f7e05e4a540bb5cff88596a
Niklaus Johner <niklaus.johner@4teamwork.ch>
2020-12-11 15:37:19 +0100

    Add invitation_group_dn to teamraum policy template.

--- opengever/policytemplates/hooks.py
+++ opengever/policytemplates/hooks.py
@@ -42,6 +42,7 @@ IGNORED_QUESTIONS = {
         'deployment.workspace_administrators_group',
         'deployment.workspace_creators_group',
         'deployment.workspace_users_group',
+        'setup.invitation_group_dn',
         'base.apps_endpoint_url',
         'base.workspace_secret',
         'setup.enable_todo_feature'
