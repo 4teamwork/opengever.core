fb92def014ecc4c2d83479bf6a4d4993fd68109c
Elio Schmutz <e.schmutz@4teamwork.ch>
2021-01-04 17:29:13 +0100

    Use the new solr index to sort tasktemplates by objPositionInParent.

--- opengever/tasktemplates/browser/tasktemplates.py
+++ opengever/tasktemplates/browser/tasktemplates.py
@@ -1,6 +1,5 @@
 from ftw.table import helper
 from ftw.table.interfaces import ITableSource
-from opengever.base import ISearchSettings
 from opengever.tabbedview import BaseCatalogListingTab
 from opengever.tabbedview import GeverCatalogTableSource
 from opengever.tabbedview.helper import linked
@@ -12,10 +11,8 @@ from opengever.tasktemplates.browser.helper import interactive_user_helper
 from opengever.tasktemplates.content.tasktemplate import ITaskTemplate
 from plone import api
 from plone.dexterity.browser.view import DefaultView
-from plone.registry.interfaces import IRegistry
 from Products.CMFPlone.utils import safe_unicode
 from zope.component import adapter
-from zope.component._api import getUtility
 from zope.component.hooks import getSite
 from zope.i18n import translate
 from zope.interface import implementer
@@ -42,22 +39,12 @@ class TaskTemplatesCatalogTableSource(GeverCatalogTableSource):
 
     def search_results(self, query):
         """
-        After fetching all the task templates we reorder them by their position in
-        the parent. This is needed because the task templates can be ordered manually
-        by the user (drag'n'drop in the table) but Solr does not know about the position
-        of the task templates within their container.
-
-        We also disable batching of the search results by fetching all task templates because
+        We disable batching of the search results by fetching all task templates because
         it is not very useful to enable batching when the items can be ordered manually (i.e.
         moving items between) pages is very cumbersome. Otherwise we could not reliably order
         the items when we only have a batch. Since we don't expect our clients to create lots
         of task templates, we assume this is a reasonable choice for this special use case.
         """
-        # Backup the sort parameter because it will be gone after the call to the
-        # the super class and we need it in order to determine if we need to do
-        # the sorting.
-        sort_on = query.get('sort_on')
-
         # Get a lot of task templates on the first try in order to prevent batching.
         self.config.pagesize = 5000
         search_results = super(TaskTemplatesCatalogTableSource, self).search_results(query)
@@ -68,15 +55,6 @@ class TaskTemplatesCatalogTableSource(GeverCatalogTableSource):
             # because we assume not many client will have that many task templates.
             self.config.pagesize = search_results.actual_result_count
             search_results = super(TaskTemplatesCatalogTableSource, self).search_results(query)
-
-        # Manually sort the Solr search results by their position in the parent because
-        # Solr does not know about the position of the task templates in their parent.
-        if self.use_solr and sort_on == 'getObjPositionInParent':
-            parent = api.content.get(query['path']['query'])
-            search_results.docs = sorted(
-                search_results.docs,
-                key=lambda doc: parent.objectIds().index(doc['id'])
-            )
 
         return search_results
 
