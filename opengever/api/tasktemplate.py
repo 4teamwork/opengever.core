83c863e2e10d2961b4279d8fb8929d8d6da8b5db
Elio Schmutz <e.schmutz@4teamwork.ch>
2021-01-08 16:48:17 +0100

    Adding tasktemplates over the restapi properly separates the responsible user and client.

---
+++ opengever/api/tasktemplate.py
@@ -0,0 +1,15 @@
+from opengever.tasktemplates.content.tasktemplate import ITaskTemplate
+from plone.restapi.interfaces import IDeserializeFromJson
+from zope.component import adapter
+from zope.interface import implementer
+from zope.interface import Interface
+from opengever.api.task import TaskDeserializeFromJson
+
+
+@implementer(IDeserializeFromJson)
+@adapter(ITaskTemplate, Interface)
+class TaskTemplateDeserializeFromJson(TaskDeserializeFromJson):
+    """A tasktemplate specific deserializer which allows to pass in the
+    responsible_client and responsible value in a combined string.
+    In the same way as it is exposed by the API's querysoure endpoint.
+    """
