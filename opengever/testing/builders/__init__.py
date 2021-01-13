50673173398dee5659f02e0f919327445272e434
David Erni <d.erni@4teamwork.ch>
2021-01-13 13:25:12 +0100

    Define simple property sheet definition builder.
    Just a thin wrapper around the already existing factory method and
    storage for better readability and more convenience in tests.

--- opengever/testing/builders/__init__.py
+++ opengever/testing/builders/__init__.py
@@ -3,6 +3,7 @@ from opengever.testing.builders.dx import *
 from opengever.testing.builders.fixture import *
 from opengever.testing.builders.ldap import *
 from opengever.testing.builders.portlets import *
+from opengever.testing.builders.propertysheets import *
 from opengever.testing.builders.qickupload import *
 from opengever.testing.builders.repositorytree import *
 from opengever.testing.builders.sql import *
---
+++ opengever/testing/builders/propertysheets.py
@@ -0,0 +1,49 @@
+from ftw.builder import builder_registry
+from opengever.propertysheets.storage import PropertySheetSchemaStorage
+from opengever.propertysheets.definition import PropertySheetSchemaDefinition
+
+
+class PropertySheetSchemaBuilder(object):
+    """Create a property sheet schema definition and persist it in storage."""
+    def __init__(self, session):
+        self.session = session
+        self.arguments = {
+            'name': 'schema',
+            'assignments': None
+        }
+        self.field_defs = []
+        self.storage = PropertySheetSchemaStorage()
+
+    def having(self, **kwargs):
+        self.arguments.update(kwargs)
+        return self
+
+    def named(self, name):
+        self.arguments['name'] = name
+        return self
+
+    def assigned_to_slots(self, *args):
+        self.arguments['assignments'] = args
+        return self
+
+    def with_simple_boolean_field(self):
+        return self.with_field("bool", u"yesorno", u"y/n", u"", True)
+
+    def with_field(self, field_type, name, title, description, required, values=None):
+        self.field_defs.append(
+            (field_type, name, title, description, required, values)
+        )
+        return self
+
+    def create(self, **kwargs):
+        definition = PropertySheetSchemaDefinition.create(
+            self.arguments['name'],
+            self.arguments['assignments']
+        )
+        for field_def in self.field_defs:
+            definition.add_field(*field_def)
+        self.storage.save(definition)
+        return self.storage.get(definition.name)
+
+
+builder_registry.register('property_sheet_schema', PropertySheetSchemaBuilder)
