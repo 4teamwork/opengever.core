94ebdcf271a7cfa9969b732718e70ff85fde115b
Thomas Buchberger <t.buchberger@4teamwork.ch>
2021-01-22 11:03:18 +0100

    Create PDF of meeting minutes
    Uses WeasyPrint for PDF generation from HTML.
    WeasyPrint is integrated as dockerized a web service.

---
+++ opengever/workspace/browser/meeting_pdf.py
@@ -0,0 +1,50 @@
+from DateTime import DateTime
+from logging import getLogger
+from opengever.base.helpers import display_name
+from os import environ
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.component import getMultiAdapter
+import requests
+
+
+logger = getLogger('opengever.workspace')
+
+
+class MeetingMinutesPDFView(BrowserView):
+
+    template = ViewPageTemplateFile("templates/meeting_minutes.pt")
+
+    def __call__(self):
+        weasyprint_url = environ.get('WEASYPRINT_URL')
+        if not weasyprint_url:
+            logger.error('Weasyprint url not configured.')
+            self.request.response.setStatus(500)
+            return 'PDF generation failed.'
+
+        resp = None
+        try:
+            resp = requests.post(
+                weasyprint_url, files={'html': self.meeting_minutes_html()})
+            resp.raise_for_status()
+        except requests.exceptions.RequestException:
+            details = resp.content[:200] if resp is not None else ''
+            logger.exception('PDF generation failed. %s', details)
+            self.request.response.setStatus(500)
+            return 'PDF generation failed.'
+        else:
+            self.request.response.setHeader('Content-Type', 'application/pdf')
+            return resp.content
+
+    def meeting_minutes_html(self):
+        data = {}
+        portal_state = getMultiAdapter((self.context, self.request),
+                                       name=u'plone_portal_state')
+        data['generator'] = portal_state.portal_title()
+        data['print_date'] = DateTime()
+        data['title'] = 'Protokoll: {}'.format(self.context.Title())
+        data['responsible'] = display_name(self.context.responsible)
+        data['agenda_items'] = self.context.agenda_items_json.get('items', [])
+        for i, agenda_item in enumerate(data['agenda_items']):
+            agenda_item[u'number'] = '{}. '.format(i + 1)
+        return self.template(self, **data)
