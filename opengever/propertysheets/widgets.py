aab034e76c009f9c012881cba97450a9d82b8b11
David Erni <d.erni@4teamwork.ch>
2021-02-01 18:46:01 +0100

    Fix prefilling widgets when active sheet changes.

--- opengever/propertysheets/widgets.py
+++ opengever/propertysheets/widgets.py
@@ -57,6 +57,7 @@ class PropertySheetWiget(Widget):
             widget.name = identifier
             widget.id = identifier
             widget.mode = self.mode
+
             widget.update()  # update is required to set up terms for sequences
             self.widgets.append(widget)
 
@@ -90,6 +91,9 @@ class PropertySheetWiget(Widget):
         sheet_values = {}
         found_request_value = False
 
+        obj = self.value or dict()
+        sheet_values = obj.get(slot_name, {})
+
         for name, widget in zip(definition.get_fieldnames(), self.widgets):
             value = widget.field.missing_value
             try:
@@ -98,6 +102,10 @@ class PropertySheetWiget(Widget):
                 if raw is not default:
                     found_request_value = True
                     value = IDataConverter(widget).toFieldValue(raw)
+                else:
+                    # if there is no request value try falling back to the
+                    # existing value or then the default missing value
+                    value = sheet_values.get(name, widget.field.missing_value)
                 validator = getMultiAdapter(
                     (
                         self.context,
@@ -126,6 +134,7 @@ class PropertySheetWiget(Widget):
                     widget.error = view
                 errors += (view,)
             else:
+                widget.value = IDataConverter(widget).toWidgetValue(value)
                 sheet_values[name] = value
 
         if self.setErrors and errors:
