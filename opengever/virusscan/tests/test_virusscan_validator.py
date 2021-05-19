964b6a17b8ab5a373bed48abb3ba46949da9b139
Niklaus Johner <niklaus.johner@4teamwork.ch>
2021-05-19 13:53:26 +0200

    Scan for viruses in quickupload.

--- opengever/virusscan/tests/test_virusscan_validator.py
+++ opengever/virusscan/tests/test_virusscan_validator.py
@@ -1,12 +1,13 @@
+from collective.quickupload.interfaces import IQuickUploadFileFactory
 from ftw.testbrowser import browsing
 from ftw.testbrowser.pages import factoriesmenu
+from ftw.testbrowser.pages.dexterity import erroneous_fields
 from ftw.testbrowser.pages.statusmessages import error_messages
+from opengever.testing import IntegrationTestCase
 from opengever.virusscan.interfaces import IAVScannerSettings
 from opengever.virusscan.testing import EICAR
 from opengever.virusscan.testing import register_mock_av_scanner
 from plone import api
-from opengever.testing import IntegrationTestCase
-from ftw.testbrowser.pages.dexterity import erroneous_fields
 
 
 class TestVirusScanValidator(IntegrationTestCase):
@@ -117,3 +118,28 @@ class TestVirusScanValidator(IntegrationTestCase):
         browser.fill({'Archival file': ("No virus", 'file.txt', 'text/plain')}).save()
         self.assertEqual([], error_messages())
         self.assertIsNotNone(self.document.archival_file)
+
+    def test_quickupload_scans_file_for_viruses_when_enabled(self):
+        self.login(self.regular_user)
+        factory = IQuickUploadFileFactory(self.dossier)
+        result = factory(filename='file.txt',
+                         title=None,  # ignored by adapter
+                         description=None,  # ignored by adapter
+                         content_type='text/plain',
+                         data=EICAR,
+                         portal_type='opengever.document.document')
+
+        self.assertIsNone(result['success'])
+        self.assertEqual(
+             u'Validation failed, file is virus-infected. (Eicar-Test-Signature FOUND)',
+             result['error'])
+
+        result = factory(filename='file.txt',
+                         title=None,  # ignored by adapter
+                         description=None,  # ignored by adapter
+                         content_type='text/plain',
+                         data='No virus',
+                         portal_type='opengever.document.document')
+
+        self.assertIsNotNone(result['success'])
+        self.assertEqual('No virus', result['success'].file.data)
