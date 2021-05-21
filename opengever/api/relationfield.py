14daa751c464f37fcdab57c0dfcb0df216d8074b
Philippe Gross <philippe.gross@4teamwork.ch>
2021-05-21 18:20:08 +0200

    Fix a problem in relation deserializer when not all path elements are accessible.
    plone.restapi's RelationChoiceFieldDeserializer uses restrictedTraverse to fetch relations given by URL or path. But this only works if every single path element is accesssible by the current user. Therefore I customized the Relation deseralizer and fetch the element in the same manner the ZCatalog does.

---
+++ opengever/api/relationfield.py
@@ -0,0 +1,79 @@
+from opengever.base.interfaces import IOpengeverBaseLayer
+from plone.dexterity.interfaces import IDexterityContent
+from plone.restapi.deserializer.relationfield import RelationChoiceFieldDeserializer
+from plone.restapi.interfaces import IFieldDeserializer
+from Products.CMFCore.utils import getToolByName
+from z3c.relationfield.interfaces import IRelationChoice
+from zope.component import adapter
+from zope.component import getMultiAdapter
+from zope.component import queryUtility
+from zope.interface import implementer
+from zope.intid.interfaces import IIntIds
+import six
+
+
+@implementer(IFieldDeserializer)
+@adapter(IRelationChoice, IDexterityContent, IOpengeverBaseLayer)
+class GeverRelationChoiceFieldDeserializer(RelationChoiceFieldDeserializer):
+    """Customize the RelationFieldDeserializer to fix a problem when
+    referencing objects by the path or URL and not the complete path is
+    accessible for the current user.
+    """
+
+    def __call__(self, value):
+        """Copied from the baseclass"""
+
+        obj = None
+
+        if isinstance(value, dict):
+            # We are trying to deserialize the output of a serialization
+            # which is enhanced, extract it and put it on the loop again
+            value = value["@id"]
+
+        if isinstance(value, int):
+            # Resolve by intid
+            intids = queryUtility(IIntIds)
+            obj = intids.queryObject(value)
+            resolved_by = "intid"
+        elif isinstance(value, six.string_types):
+            if six.PY2 and isinstance(value, six.text_type):
+                value = value.encode("utf8")
+            portal = getMultiAdapter(
+                (self.context, self.request), name="plone_portal_state"
+            ).portal()
+            portal_url = portal.absolute_url()
+            if value.startswith(portal_url):
+                # Resolve by URL
+                obj = self.get_obj_by_path(portal, value[len(portal_url) + 1:])
+                resolved_by = "URL"
+            elif value.startswith("/"):
+                # Resolve by path
+                obj = self.get_obj_by_path(portal, value.lstrip("/"))
+                resolved_by = "path"
+            else:
+                # Resolve by UID
+                catalog = getToolByName(self.context, "portal_catalog")
+                brain = catalog(UID=value)
+                if brain:
+                    obj = brain[0].getObject()
+                resolved_by = "UID"
+
+        if obj is None:
+            self.request.response.setStatus(400)
+            raise ValueError(
+                u"Could not resolve object for {}={}".format(resolved_by, value)
+            )
+
+        self.field.validate(obj)
+        return obj
+
+    def get_obj_by_path(self, portal, path):
+        """restrictedTraverse checks all of the objects along the path are
+        validated with the security machinery. But we only need to know if the
+        user is able to access the given object."""
+        path = path.rstrip('/').split('/')
+        parent = portal.unrestrictedTraverse(path[:-1], None)
+        if not parent:
+            return None
+
+        return parent.restrictedTraverse(path[-1], None)
