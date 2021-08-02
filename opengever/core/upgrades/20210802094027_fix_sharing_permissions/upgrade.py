e9d767060b82379d9250570271f1b7659c5cdf75
Niklaus Johner <niklaus.johner@4teamwork.ch>
2021-08-02 08:54:27 +0200

    Fix sharing permissions on private and inbox documents.
    In the shadow state, the owner had the permission to delegate roles.
    This should not be a problem, as documents are really only transiently
    in the shadow state.

---
+++ opengever/core/upgrades/20210802094027_fix_sharing_permissions/upgrade.py
@@ -0,0 +1,13 @@
+from ftw.upgrade import UpgradeStep
+
+
+class FixSharingPermissions(UpgradeStep):
+    """Fix sharing permissions.
+    """
+
+    def __call__(self):
+        self.install_upgrade_profile()
+        self.update_workflow_security(
+            ['opengever_inbox_document_workflow',
+             'opengever_private_document_workflow'],
+            reindex_security=False)
