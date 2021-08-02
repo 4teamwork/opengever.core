e3e815b0b7379a84d334fe5e97f153aed65c8aed
Niklaus Johner <niklaus.johner@4teamwork.ch>
2021-08-02 08:58:09 +0200

    Fix sharing permissions on tasktemplatefolders.
    Sharing permissions were not defined on the tasktemplatefolder so
    that they would be inherited from the folder. This allows role
    managers to manage permissions on tasktemplatefolders and
    tasktemplates, which is not correct.

--- opengever/core/upgrades/20210802094027_fix_sharing_permissions/upgrade.py
+++ opengever/core/upgrades/20210802094027_fix_sharing_permissions/upgrade.py
@@ -9,5 +9,6 @@ class FixSharingPermissions(UpgradeStep):
         self.install_upgrade_profile()
         self.update_workflow_security(
             ['opengever_inbox_document_workflow',
-             'opengever_private_document_workflow'],
+             'opengever_private_document_workflow',
+             'opengever_tasktemplatefolder_workflow'],
             reindex_security=False)
