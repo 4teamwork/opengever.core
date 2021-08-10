33f9618b1e5d39ea984de1da6b99dc903e26d4e6
Niklaus Johner <niklaus.johner@4teamwork.ch>
2021-08-10 13:51:33 +0200

    Reindex missing changed dates in solr.

---
+++ opengever/core/upgrades/20210810115730_reindex_missing_changed_date/upgrade.py
@@ -0,0 +1,41 @@
+from ftw.solr.interfaces import ISolrConnectionManager
+from ftw.solr.interfaces import ISolrIndexHandler
+from ftw.solr.interfaces import ISolrSearch
+from ftw.upgrade import UpgradeStep
+from opengever.base.utils import unrestrictedUuidToObject
+from zope.component import getMultiAdapter
+from zope.component import getUtility
+import logging
+
+
+log = logging.getLogger('ftw.upgrade')
+
+
+class ReindexMissingChangedDate(UpgradeStep):
+    """Reindex missing changed date.
+    """
+
+    deferrable = True
+
+    def __call__(self):
+        solr = getUtility(ISolrSearch)
+        manager = getUtility(ISolrConnectionManager)
+        solr_connection = manager.connection
+
+        filters = ["-changed:[* TO *]"]
+
+        nrows = solr.unrestrictedSearch(filters=filters, fl=["UID"], rows=0).num_found
+        results = solr.unrestrictedSearch(filters=filters, fl=["UID"], rows=nrows)
+
+        for index, doc in enumerate(results.docs, 1):
+
+            obj = unrestrictedUuidToObject(doc['UID'])
+            if not obj:
+                continue
+
+            handler = getMultiAdapter((obj, manager), ISolrIndexHandler)
+            handler.add(['changed'])
+
+            if index % 1000 == 0:
+                log.info("Committing items {}/{} to Solr.".format(index, results.num_found))
+                solr_connection.commit()
