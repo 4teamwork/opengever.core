c370a85c75e6f3e1d81ba6b84451f00f9d7f10d4
Niklaus Johner <niklaus.johner@4teamwork.ch>
2021-08-16 11:59:04 +0200

    Revert merge of upgrade reindexing reference and sortable_reference.
    Merging the older upgrade step reindexing sortable_reference
    with a newer one is counterproductive. Indeed this leads to
    indexing all sortable_reference numbers in solr a second time
    for deployments that are on a version between 2021.4 and 2021.12.

--- opengever/core/upgrades/20210205084521_add_sortable_reference_number_index/upgrade.py
+++ opengever/core/upgrades/20210205084521_add_sortable_reference_number_index/upgrade.py
@@ -16,14 +16,9 @@ class AddSortableReferenceNumberIndex(UpgradeStep):
         self.index_sortable_reference_number()
 
     def index_sortable_reference_number(self):
-
-        # Disable this upgradestep because it's done by the later upgradestep
-        # 20210601120606_update_reindex_reference_and_sortable_reference_index
-        return
-
-        # manager = getUtility(ISolrConnectionManager)
-        # query = {'object_provides': IDexterityContent.__identifier__}
-        # for obj in self.objects(query, 'Index sortable_reference in Solr'):
-        #     handler = getMultiAdapter((obj, manager), ISolrIndexHandler)
-        #     handler.add(['sortable_reference'])
-        # manager.connection.commit(soft_commit=False, extract_after_commit=False)
+        manager = getUtility(ISolrConnectionManager)
+        query = {'object_provides': IDexterityContent.__identifier__}
+        for obj in self.objects(query, 'Index sortable_reference in Solr'):
+            handler = getMultiAdapter((obj, manager), ISolrIndexHandler)
+            handler.add(['sortable_reference'])
+        manager.connection.commit(soft_commit=False, extract_after_commit=False)
--- opengever/core/upgrades/20210601180416_update_reindex_reference_and_sortable_reference_index/upgrade.py
+++ opengever/core/upgrades/20210601180416_update_reindex_reference_and_sortable_reference_index/upgrade.py
@@ -1,12 +1,8 @@
-from ftw.solr.interfaces import ISolrConnectionManager
-from ftw.solr.interfaces import ISolrIndexHandler
 from ftw.upgrade import UpgradeStep
 from ftw.upgrade.progresslogger import ProgressLogger
 from ftw.upgrade.utils import SavepointIterator
 from opengever.base.interfaces import IReferenceNumber
 from plone.dexterity.interfaces import IDexterityContent
-from zope.component import getMultiAdapter
-from zope.component import getUtility
 
 
 class UpdateReindexReferenceAndSortableReferenceIndex(UpgradeStep):
@@ -19,19 +15,12 @@ class UpdateReindexReferenceAndSortableReferenceIndex(UpgradeStep):
         self.index_sortable_reference_number()
 
     def index_sortable_reference_number(self):
-        manager = getUtility(ISolrConnectionManager)
         brains = self.catalog_unrestricted_search(
             {'object_provides': IDexterityContent.__identifier__})
         iterator = SavepointIterator.build(brains)
-        message = 'Reindex reference and sortable_reference in Solr'
+        message = 'Reindex reference'
         for brain in ProgressLogger(message, iterator):
             # update catalog but only if necessary
             obj = self.catalog_unrestricted_get_object(brain)
             if IReferenceNumber(obj).get_number() != brain.reference:
-                obj.reindexObject(idxs=['reference'])
-
-            # update solr
-            handler = getMultiAdapter((obj, manager), ISolrIndexHandler)
-            handler.add(['reference', 'sortable_reference'])
-
-        manager.connection.commit(soft_commit=False, extract_after_commit=False)
+                obj.reindexObject(idxs=['reference', 'sortable_reference'])
