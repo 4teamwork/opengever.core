e200e2699b00a89b6ee1116e45cbedfa6e18a288
Philippe Gross <philippe.gross@4teamwork.ch>
2021-11-04 17:18:36 +0100

    Return translated error message when trashing documents.

--- opengever/api/trash.py
+++ opengever/api/trash.py
@@ -1,6 +1,8 @@
+from opengever.api import _
 from opengever.trash.trash import ITrasher
 from opengever.trash.trash import TrashError
 from plone.restapi.services import Service
+from zExceptions import BadRequest
 from zope.interface import alsoProvides
 import plone.protect.interfaces
 
@@ -18,37 +20,26 @@ class TrashPost(Service):
         try:
             trasher.trash()
         except TrashError as exc:
-            self.request.response.setStatus(400)
             if exc.message == 'Already trashed':
-                return {'error': {
-                    'type': 'Bad Request',
-                    'message': 'Already trashed',
-                }}
+                raise BadRequest(
+                    _(u'msg_already_trashed', default=u'Already trashed'))
             elif exc.message == 'Document checked out':
-                return {'error': {
-                    'type': 'Bad Request',
-                    'message': 'Cannot trash a checked-out document',
-                }}
+                raise BadRequest(
+                    _(u'msg_trash_checked_out_doc',
+                      default=u'Cannot trash a checked-out document'))
             elif exc.message == 'The document is locked':
-                return {'error': {
-                    'type': 'Bad Request',
-                    'message': 'Cannot trash a locked document',
-                }}
+                raise BadRequest(
+                    _(u'msg_trash_locked_doc', default=u'Cannot trash a locked document'))
             elif exc.message == 'The document has been returned as excerpt':
-                return {'error': {
-                    'type': 'Bad Request',
-                    'message': 'Cannot trash a document that has been returned as excerpt',
-                }}
+                raise BadRequest(
+                    _(u'msg_trash_doc_returned_as_excerpt',
+                      default=u'Cannot trash a document that has been returned as excerpt'))
             elif exc.message == 'Not trashable':
-                return {'error': {
-                    'type': 'Bad Request',
-                    'message': 'Object is not trashable',
-                }}
+                raise BadRequest(
+                    _(u'msg_obj_not_trashable', default=u'Object is not trashable'))
             else:
-                return {'error': {
-                    'type': 'Bad Request',
-                    'message': 'Object is not trashable',
-                }}
+                raise BadRequest(
+                    _(u'msg_obj_not_trashable', default=u'Object is not trashable'))
 
         self.request.response.setStatus(204)
         return super(TrashPost, self).reply()
