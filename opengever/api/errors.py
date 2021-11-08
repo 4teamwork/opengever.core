d37fb735eaeb6dd996990d2f655eadef29a07805
Philippe Gross <philippe.gross@4teamwork.ch>
2021-11-08 12:48:56 +0100

    Add special error handling for the TUS and invitations endpoints.

--- opengever/api/errors.py
+++ opengever/api/errors.py
@@ -11,11 +11,23 @@ class GeverErrorHandling(ErrorHandling):
 
     def render_exception(self, exception):
         result = super(GeverErrorHandling, self).render_exception(exception)
+        message = exception.message
 
         # Validation errors
-        if isinstance(exception.message, list):
+        if isinstance(message, list):
+            # Special handling for invitations etc.
+            if len(message) and isinstance(message[0], tuple):
+                return result
+
+            # Handle TUS error
+            elif len(message) and not message[0].get('field'):
+                result['additional_metadata'] = {}
+                result['translated_message'] = translate(
+                    message[0].get('message'), context=self.request)
+                return result
+
             fields = []
-            for error in exception.message:
+            for error in message:
                 fields.append({
                     'field': error['field'],
                     'type': str(error['error']).decode('utf-8'),
@@ -28,9 +40,9 @@ class GeverErrorHandling(ErrorHandling):
                 _('msg_inputs_not_valid', default=u'Inputs not valid'),
                 context=self.request)
 
-        elif isinstance(exception.message, Message):
+        elif isinstance(message, Message):
             result['additional_metadata'] = {}
             result['translated_message'] = translate(
-                exception.message, context=self.request)
+                message, context=self.request)
 
         return result
