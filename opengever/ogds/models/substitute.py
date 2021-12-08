d47c2b4d43513547feb3978977c9e6e3d371e2bd
Tina Gerber <tina.gerber@4teamwork.ch>
2021-12-08 17:23:50 +0100

    Add substitute model.

---
+++ opengever/ogds/models/substitute.py
@@ -0,0 +1,33 @@
+from opengever.base.model import Base
+from opengever.base.model import USER_ID_LENGTH
+from opengever.base.query import BaseQuery
+from opengever.ogds.models.user import User
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.schema import Sequence
+
+
+class SubstituteQuery(BaseQuery):
+
+    searchable_fields = ['userid', 'substitute_userid']
+
+    def by_userid(self, userid):
+        return self.filter_by(userid=userid)
+
+    def by_userid_and_substitute_userid(self, userid, substitute_userid):
+        return self.filter_by(userid=userid, substitute_userid=substitute_userid)
+
+
+class Substitute(Base):
+    """Substitute model
+    """
+
+    query_cls = SubstituteQuery
+
+    __tablename__ = 'substitutes'
+
+    substitution_id = Column("id", Integer, Sequence("substitution_id_seq"), primary_key=True)
+    userid = Column(String(USER_ID_LENGTH), ForeignKey(User.userid))
+    substitute_userid = Column(String(USER_ID_LENGTH), ForeignKey(User.userid))
