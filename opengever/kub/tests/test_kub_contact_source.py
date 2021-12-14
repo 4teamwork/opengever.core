e412a0282843f1a13ac9e1bb95ef7043f7efd703
Niklaus Johner <niklaus.johner@4teamwork.ch>
2021-12-14 17:56:50 +0100

    Support OGDS users in KuBContactSource.

--- opengever/kub/tests/test_kub_contact_source.py
+++ opengever/kub/tests/test_kub_contact_source.py
@@ -1,5 +1,6 @@
 from opengever.kub.sources import KuBContactsSource
 from opengever.kub.testing import KuBIntegrationTestCase
+from opengever.ogds.models.service import ogds_service
 import requests_mock
 
 
@@ -34,6 +35,20 @@ class TestKubContactSource(KuBIntegrationTestCase):
         self.assertItemsEqual(["Dupont Jean - 4Teamwork (CEO)", "4Teamwork"],
                               [term.title for term in terms])
 
+    def test_search_returns_ogds_users(self, mocker):
+        query_str = "Barfuss"
+        url = u'{}search?q={}'.format(self.client.kub_api_url, query_str)
+        mocker.get(url, json=[])
+        ogds_user = ogds_service().fetch_user(self.regular_user.getId())
+
+        terms = self.source.search(query_str)
+        self.assertItemsEqual(['kathi.barfuss'],
+                              [term.token for term in terms])
+        self.assertItemsEqual([ogds_user],
+                              [term.value for term in terms])
+        self.assertItemsEqual([u'B\xe4rfuss K\xe4thi (kathi.barfuss)'],
+                              [term.title for term in terms])
+
     def test_get_term_by_token_handles_persons(self, mocker):
         self.mock_get_by_id(mocker, self.person_jean)
         term = self.source.getTermByToken(self.person_jean)
@@ -54,6 +69,13 @@ class TestKubContactSource(KuBIntegrationTestCase):
         self.assertEqual(self.memb_jean_ftw, term.token)
         self.assertEqual(self.memb_jean_ftw, term.value)
         self.assertEqual('Dupont Jean - 4Teamwork (CEO)', term.title)
+
+    def test_get_by_id_handles_ogds_users(self, mocker):
+        term = self.source.getTermByToken(self.regular_user.getId())
+        ogds_user = ogds_service().fetch_user(self.regular_user.getId())
+        self.assertEqual(self.regular_user.getId(), term.token)
+        self.assertEqual(ogds_user, term.value)
+        self.assertEqual(u'B\xe4rfuss K\xe4thi (kathi.barfuss)', term.title)
 
     def test_get_by_id_raises_lookup_error_for_invalid_token(self, mocker):
         contact_id = "invalid-id"
