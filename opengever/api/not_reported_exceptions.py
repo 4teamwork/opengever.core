c95c4ab4184591c0c2058c1aaa851c47e58c5abf
Niklaus Johner <niklaus.johner@4teamwork.ch>
2022-01-07 11:32:40 +0100

    Add a BadRequest exception that does not get reported to sentry.

--- opengever/api/not_reported_exceptions.py
+++ opengever/api/not_reported_exceptions.py
@@ -1,3 +1,15 @@
+from zExceptions import BadRequest as _BadRequest
+
+
 class NotReportedException(Exception):
     """An exception that is not reported in sentry
+    """
+
+
+class BadRequest(_BadRequest, NotReportedException):
+    """BadRequest that is not reported in sentry.
+
+    This class needs to be named BadRequest so that ZPublisher's HTTPResponse
+    will set the status code to 400, and so that the API will return BadRequest
+    as error type.
     """
