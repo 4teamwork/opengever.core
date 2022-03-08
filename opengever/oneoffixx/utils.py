c34f82d7a40bb10a304c9532190e295f99a7f981
Thomas Buchberger <t.buchberger@4teamwork.ch>
2022-03-08 15:24:53 +0100

    Add label template to whitelist

--- opengever/oneoffixx/utils.py
+++ opengever/oneoffixx/utils.py
@@ -1,18 +1,27 @@
 whitelisted_template_types = {
+    # Content
     '275af32e-bc40-45c2-85b7-afb1c0382653': {
         'content-type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
         'extension': 'docx',
     },
+    # Excel template
     'e31ca353-2ab1-4408-921b-a70ae2f57ad1': {
         'content-type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
         'extension': 'xlsx',
     },
+    # PowerPoint template
     'a2c9b700-86cd-4481-a17f-533fe9c504a2': {
         'content-type': 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
         'extension': 'pptx',
     },
+    # Function
     '0a3ac64d-ec36-4bcd-b057-41379a502fed': {
         'content-type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
         'extension': 'docx',
-    }
+    },
+    # Label
+    '90b5aa90-dadd-47c1-8d1a-a49016c2bcb3': {
+        'content-type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        'extension': 'docx',
+    },
 }
