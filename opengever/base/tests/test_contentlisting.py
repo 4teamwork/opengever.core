306f3442bca56359611252c2c2c349ba5ce04434
Thomas Buchberger <t.buchberger@4teamwork.ch>
2022-04-20 18:24:30 +0200

    Use Solr for contentlisting tests

--- opengever/base/tests/test_contentlisting.py
+++ opengever/base/tests/test_contentlisting.py
@@ -1,14 +1,14 @@
 from ftw.testbrowser import browsing
 from opengever.document.widgets.document_link import DocumentLinkWidget
-from opengever.testing import IntegrationTestCase
-from opengever.testing import obj2brain
+from opengever.testing import obj2solr
 from opengever.testing import set_preferred_language
+from opengever.testing import SolrIntegrationTestCase
 from opengever.trash.trash import ITrasher
 from plone import api
 from plone.app.contentlisting.interfaces import IContentListingObject
 
 
-class TestOpengeverContentListing(IntegrationTestCase):
+class TestOpengeverContentListing(SolrIntegrationTestCase):
     """Test basic content listing functionality."""
 
     def setUp(self):
@@ -29,11 +29,11 @@ class TestOpengeverContentListing(IntegrationTestCase):
         self.login(self.regular_user)
 
         self.assertIsNone(
-            IContentListingObject(obj2brain(self.dossier)).getIcon(),
+            IContentListingObject(obj2solr(self.dossier)).getIcon(),
             )
 
         self.assertIsNone(
-            IContentListingObject(obj2brain(self.document)).getIcon(),
+            IContentListingObject(obj2solr(self.document)).getIcon(),
             )
 
     def test_ContentTypeClass_returns_the_contenttype_icon_css_class(self):
@@ -41,18 +41,18 @@ class TestOpengeverContentListing(IntegrationTestCase):
 
         self.assertEquals(
             'contenttype-opengever-dossier-businesscasedossier',
-            IContentListingObject(obj2brain(self.dossier)).ContentTypeClass(),
+            IContentListingObject(obj2solr(self.dossier)).ContentTypeClass(),
             )
 
         self.assertEquals(
             'icon-docx',
-            IContentListingObject(obj2brain(self.document)).ContentTypeClass(),
+            IContentListingObject(obj2solr(self.document)).ContentTypeClass(),
             )
 
     def test_containing_dossier_of_a_dossier_returns_dossiers_title(self):
         self.login(self.regular_user)
 
-        brain = obj2brain(self.dossier)
+        brain = obj2solr(self.dossier)
         self.assertEquals(
             u'Vertr\xe4ge mit der kantonalen Finanzverwaltung'.encode('utf-8'),
             IContentListingObject(brain).containing_dossier(),
@@ -64,7 +64,7 @@ class TestOpengeverContentListing(IntegrationTestCase):
         self.assertEquals(
             '',
             IContentListingObject(
-                obj2brain(self.leaf_repofolder),
+                obj2solr(self.leaf_repofolder),
                 ).containing_dossier(),
             )
 
@@ -74,7 +74,7 @@ class TestOpengeverContentListing(IntegrationTestCase):
         self.assertEquals(
             u'Vertr\xe4ge mit der kantonalen Finanzverwaltung'.encode('utf-8'),
             IContentListingObject(
-                obj2brain(self.document),
+                obj2solr(self.document),
                 ).containing_dossier(),
             )
 
@@ -83,11 +83,12 @@ class TestOpengeverContentListing(IntegrationTestCase):
 
         self.dossier.title = 25 * u'lorem ipsum '
         self.document.reindexObject(idxs=['containing_dossier'])
+        self.commit_solr()
 
         self.assertCropping(
             201,
             IContentListingObject(
-                obj2brain(self.document),
+                obj2solr(self.document),
                 ).containing_dossier(),
             )
 
@@ -96,10 +97,11 @@ class TestOpengeverContentListing(IntegrationTestCase):
 
         self.document.title = 25 * 'lorem ipsum '
         self.document.reindexObject()
+        self.commit_solr()
 
         self.assertCropping(
             201,
-            IContentListingObject(obj2brain(self.document)).CroppedTitle(),
+            IContentListingObject(obj2solr(self.document)).CroppedTitle(),
             )
 
     def test_cropped_description_returns_description_cropped_to_near_400_chars(
@@ -109,68 +111,71 @@ class TestOpengeverContentListing(IntegrationTestCase):
 
         self.document.description = 50 * 'lorem ipsum '
         self.document.reindexObject()
+        self.commit_solr()
 
         self.assertCropping(
             399,
             IContentListingObject(
-                obj2brain(self.document),
+                obj2solr(self.document),
                 ).CroppedDescription(),
             )
 
     def test_cropped_description_returns_empty_string_for_objs_without_description(self):
         self.login(self.regular_user)
-        self.assertEqual('', IContentListingObject(obj2brain(self.empty_document)).CroppedDescription())
+        self.assertEqual('', IContentListingObject(obj2solr(self.empty_document)).CroppedDescription())
 
     def test_is_document(self):
         self.login(self.regular_user)
 
         self.assertTrue(
-            IContentListingObject(obj2brain(self.document)).is_document,
+            IContentListingObject(obj2solr(self.document)).is_document,
             )
 
         self.assertFalse(
-            IContentListingObject(obj2brain(self.mail_eml)).is_document,
+            IContentListingObject(obj2solr(self.mail_eml)).is_document,
             )
 
         self.assertFalse(
-            IContentListingObject(obj2brain(self.dossier)).is_document,
+            IContentListingObject(obj2solr(self.dossier)).is_document,
             )
 
     def test_is_trashed(self):
         self.login(self.regular_user)
 
         self.assertFalse(
-            IContentListingObject(obj2brain(self.document)).is_trashed,
+            IContentListingObject(obj2solr(self.document)).is_trashed,
             )
 
         ITrasher(self.document).trash()
+        self.commit_solr()
 
         self.assertTrue(
             IContentListingObject(
-                obj2brain(self.document, unrestricted=True),
+                obj2solr(self.document, unrestricted=True),
                 ).is_trashed,
             )
 
         self.assertFalse(
-            IContentListingObject(obj2brain(self.dossier)).is_trashed,
+            IContentListingObject(obj2solr(self.dossier)).is_trashed,
             )
 
     def test_is_removed(self):
         self.login(self.regular_user)
 
         self.assertFalse(
-            IContentListingObject(obj2brain(self.dossier)).is_removed,
+            IContentListingObject(obj2solr(self.dossier)).is_removed,
             )
 
         self.assertFalse(
-            IContentListingObject(obj2brain(self.document)).is_removed,
+            IContentListingObject(obj2solr(self.document)).is_removed,
             )
 
         self.login(self.manager)
         self.set_workflow_state('document-state-removed', self.document)
+        self.commit_solr()
 
         self.assertTrue(
-            IContentListingObject(obj2brain(self.document, unrestricted=True))
+            IContentListingObject(obj2solr(self.document, unrestricted=True))
             .is_removed,
             )
 
@@ -211,34 +216,34 @@ class TestOpengeverContentListing(IntegrationTestCase):
 
         self.assertEquals(
             expected_breadcrumbs,
-            IContentListingObject(obj2brain(self.document)).get_breadcrumbs(),
+            IContentListingObject(obj2solr(self.document)).get_breadcrumbs(),
             )
 
     def test_responsible_fullname(self):
         self.login(self.regular_user)
         self.assertEqual(
-            IContentListingObject(obj2brain(
+            IContentListingObject(obj2solr(
                 self.dossier)).responsible_fullname(),
             u'Ziegler Robert')
 
     def test_responsible_fullname_for_proposals(self):
         self.login(self.regular_user)
         self.assertEqual(
-            IContentListingObject(obj2brain(
+            IContentListingObject(obj2solr(
                 self.proposal)).responsible_fullname(),
             u'Rechnungspr\xfcfungskommission')
 
     def test_creationdate_from_created(self):
         self.login(self.regular_user)
         self.assertEqual(
-            IContentListingObject(obj2brain(
+            IContentListingObject(obj2solr(
                 self.dossier)).CreationDate(),
             '2016-08-31T16:01:33+02:00')
 
     def test_modificationdate_from_modified(self):
         self.login(self.regular_user)
         self.assertEqual(
-            IContentListingObject(obj2brain(
+            IContentListingObject(obj2solr(
                 self.dossier)).ModificationDate(),
             '2016-08-31T20:25:33+02:00')
 
@@ -247,7 +252,7 @@ class TestOpengeverContentListing(IntegrationTestCase):
 
         self.assertEquals(
             u'In Bearbeitung',
-            IContentListingObject(obj2brain(self.dossier))
+            IContentListingObject(obj2solr(self.dossier))
             .translated_review_state(),
         )
 
@@ -256,7 +261,7 @@ class TestOpengeverContentListing(IntegrationTestCase):
 
         self.assertEquals(
             u'Eingereicht',
-            IContentListingObject(obj2brain(self.proposal))
+            IContentListingObject(obj2solr(self.proposal))
             .translated_review_state(),
         )
 
@@ -265,7 +270,7 @@ class TestOpengeverContentListing(IntegrationTestCase):
 
         self.assertEquals(
             u'Eingereicht',
-            IContentListingObject(obj2brain(self.submitted_proposal))
+            IContentListingObject(obj2solr(self.submitted_proposal))
             .translated_review_state(),
         )
 
@@ -274,12 +279,12 @@ class TestOpengeverContentListing(IntegrationTestCase):
 
         self.assertEquals(
             u'Aktiv',
-            IContentListingObject(obj2brain(self.committee))
+            IContentListingObject(obj2solr(self.committee))
             .translated_review_state(),
         )
 
 
-class TestBrainContentListingRenderLink(IntegrationTestCase):
+class TestBrainContentListingRenderLink(SolrIntegrationTestCase):
     """Test we render appropriate content listing links per content type."""
 
     def setUp(self):
@@ -302,7 +307,7 @@ class TestBrainContentListingRenderLink(IntegrationTestCase):
 
         self.assertEquals(
             u'PATCHED LINK Vertr\xe4gsentwurf'.encode('utf-8'),
-            IContentListingObject(obj2brain(self.document)).render_link(),
+            IContentListingObject(obj2solr(self.document)).render_link(),
             )
 
     def test_uses_documentlinkrenderer_for_mails(self):
@@ -310,7 +315,7 @@ class TestBrainContentListingRenderLink(IntegrationTestCase):
 
         self.assertEquals(
             u'PATCHED LINK Die B\xfcrgschaft'.encode('utf-8'),
-            IContentListingObject(obj2brain(self.mail_eml)).render_link(),
+            IContentListingObject(obj2solr(self.mail_eml)).render_link(),
             )
 
     @browsing
@@ -318,7 +323,7 @@ class TestBrainContentListingRenderLink(IntegrationTestCase):
         self.login(self.regular_user)
 
         simple_link = (
-            IContentListingObject(obj2brain(self.subdossier)).render_link()
+            IContentListingObject(obj2solr(self.subdossier)).render_link()
             )
 
         browser.open_html(simple_link.encode('utf-8'))
@@ -334,13 +339,13 @@ class TestBrainContentListingRenderLink(IntegrationTestCase):
             )
 
 
-class TestOpengeverContentListingWithDisabledBumblebee(IntegrationTestCase):
+class TestOpengeverContentListingWithDisabledBumblebee(SolrIntegrationTestCase):
     """Test we do not trip up in the lack of a bumblebee installation."""
 
     def setUp(self):
         super(TestOpengeverContentListingWithDisabledBumblebee, self).setUp()
         self.login(self.regular_user)
-        self.obj = IContentListingObject(obj2brain(self.document))
+        self.obj = IContentListingObject(obj2solr(self.document))
 
     def test_documents_are_not_bumblebeeable(self):
         self.assertFalse(self.obj.is_bumblebeeable())
@@ -367,7 +372,7 @@ class TestOpengeverContentListingWithDisabledBumblebee(IntegrationTestCase):
         self.assertIsNone(self.obj.get_overlay_url())
 
 
-class TestOpengeverContentListingWithEnabledBumblebee(IntegrationTestCase):
+class TestOpengeverContentListingWithEnabledBumblebee(SolrIntegrationTestCase):
     """Test we do not trip up in the presence of a bumblebee installation."""
 
     features = ('bumblebee', )
@@ -375,13 +380,13 @@ class TestOpengeverContentListingWithEnabledBumblebee(IntegrationTestCase):
     def setUp(self):
         super(TestOpengeverContentListingWithEnabledBumblebee, self).setUp()
         self.login(self.regular_user)
-        self.obj = IContentListingObject(obj2brain(self.document))
+        self.obj = IContentListingObject(obj2solr(self.document))
 
     def test_documents_are_bumblebeeable(self):
         self.assertTrue(self.obj.is_bumblebeeable())
 
     def test_dossiers_are_not_bumblebeeable(self):
-        listing = IContentListingObject(obj2brain(self.dossier))
+        listing = IContentListingObject(obj2solr(self.dossier))
         self.assertFalse(listing.is_bumblebeeable())
 
     def test_preview_image_url(self):
