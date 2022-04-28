bd0a6161c06fcc0442a513bdefe58fa70790f0e2
Niklaus Johner <niklaus.johner@4teamwork.ch>
2022-04-28 11:00:05 +0200

    Correctly set content type in tus-upload.

---
+++ opengever/core/upgrades/20220428110823_correct_content_type_for_cadwork/upgrade.py
@@ -0,0 +1,48 @@
+from ftw.upgrade import UpgradeStep
+
+CADWORK_EXTENSIONS = [
+    '.2d',
+    '.2dc',
+    '.2dl',
+    '.2dm',
+    '.2dr',
+    '.3d',
+    '.3dc',
+    '.iv',
+    '.ivx',
+    '.ivz',
+    '.l2d',
+    '.lx2d',
+    '.lxz',
+]
+
+
+class AddCadworkMimeTypes(UpgradeStep):
+    """Correct cadwork mimetypes, which were not set correctly by the
+    tus-upload.
+
+    There could be others that were not set correctly, but it's hard to find
+    out which. It would be limited to Cadwork as these mimetypes are not
+    official IANA mimetypes.
+    """
+
+    deferrable = True
+
+    def __call__(self):
+
+        mtr = self.getToolByName('mimetypes_registry')
+
+        for obj in self.objects(
+            {
+                'portal_type': 'opengever.document.document',
+                'file_extension': CADWORK_EXTENSIONS,
+            },
+            'Correct Cadwork mimetypes'
+        ):
+            mt = mtr.lookupExtension(obj.file.filename)
+            content_type = mt.mimetypes[0]
+            if not obj.file.contentType == content_type:
+                obj.file.contentType = content_type
+                # Update catalog metadata (getContentType)
+                # We also need getIcon for solr
+                obj.reindexObject(idxs=['filename', 'getIcon'])
