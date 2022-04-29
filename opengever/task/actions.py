84d8a3d657e6acbe7eb05ffeda1f0a5fb6e0c439
Tina Gerber <tina.gerber@4teamwork.ch>
2022-04-29 14:46:00 +0200

    Add context actions for task and forwarding

--- opengever/task/actions.py
+++ opengever/task/actions.py
@@ -1,6 +1,9 @@
+from opengever.base.context_actions import BaseContextActions
 from opengever.base.interfaces import IOpengeverBaseLayer
 from opengever.base.listing_actions import BaseListingActions
 from opengever.dossier.behaviors.dossier import IDossierMarker
+from opengever.task.task import ITask
+from plone import api
 from zope.component import adapter
 
 
@@ -18,3 +21,10 @@ class DossierTaskListingActions(TaskListingActions):
 
     def is_move_items_available(self):
         return self.context.is_open()
+
+
+@adapter(ITask, IOpengeverBaseLayer)
+class TaskContextActions(BaseContextActions):
+
+    def is_move_item_available(self):
+        return api.user.has_permission('Copy or Move', obj=self.context)
--- opengever/task/tests/test_actions.py
+++ opengever/task/tests/test_actions.py
@@ -1,5 +1,7 @@
+from opengever.base.interfaces import IContextActions
 from opengever.base.interfaces import IListingActions
 from opengever.testing import IntegrationTestCase
+from plone import api
 from zope.component import queryMultiAdapter
 
 
@@ -37,3 +39,26 @@ class TestTaskListingActions(IntegrationTestCase):
         self.login(self.secretariat_user)
         expected_actions = [u'export_tasks', u'pdf_taskslisting']
         self.assertEqual(expected_actions, self.get_actions(self.inbox))
+
+
+class TestTaskContextActions(IntegrationTestCase):
+
+    def get_actions(self, context):
+        adapter = queryMultiAdapter((context, self.request), interface=IContextActions)
+        return adapter.get_actions() if adapter else []
+
+    def test_task_context_actions(self):
+        self.login(self.regular_user)
+        expected_actions = [u'edit', u'move_item']
+        self.assertEqual(expected_actions, self.get_actions(self.task))
+
+    def test_move_item_action_only_available_if_user_has_move_permission(self):
+        self.login(self.regular_user)
+        self.assertIn(u'move_item', self.get_actions(self.task))
+        self.task.manage_permission('Copy or Move', roles=[])
+        self.assertNotIn(u'move_item', self.get_actions(self.task))
+
+    def test_move_item_not_available_for_forwarding(self):
+        self.login(self.secretariat_user)
+        self.assertTrue(api.user.has_permission('Copy or Move', obj=self.inbox_forwarding))
+        self.assertNotIn(u'move_item', self.get_actions(self.inbox_forwarding))
