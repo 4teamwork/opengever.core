36532f6f02e2b167b36329057bfb247b21bd8aad
Niklaus Johner <niklaus.johner@4teamwork.ch>
2022-06-28 14:01:21 +0200

    Avoid double recursion in dossier reactivation.
    get_subdossiers is recursive by default, returning all subdossier
    at any depth. So as we recursively execute _recursive_reactivate,
    we want to limit the subdossiers in the loop to depth=1.

--- opengever/dossier/reactivate.py
+++ opengever/dossier/reactivate.py
@@ -33,7 +33,7 @@ class Reactivator(object):
         self._recursive_reactivate(self.context)
 
     def _recursive_reactivate(self, dossier):
-        for subdossier in dossier.get_subdossiers():
+        for subdossier in dossier.get_subdossiers(depth=1):
             self._recursive_reactivate(subdossier.getObject())
 
         if self.wft.getInfoFor(dossier,
