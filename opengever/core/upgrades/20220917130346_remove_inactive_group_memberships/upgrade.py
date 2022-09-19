1a2ace845c269d07dc6d6a6aee7f775b9ceac4c9
Tina Gerber <tina.gerber@4teamwork.ch>
2022-09-19 07:52:28 +0200

    Remove group memberships in ogds sync when ldap group is deactivated

---
+++ opengever/core/upgrades/20220917130346_remove_inactive_group_memberships/upgrade.py
@@ -0,0 +1,40 @@
+from opengever.core.upgrade import SchemaMigration
+from sqlalchemy.sql.expression import column
+from sqlalchemy.sql.expression import select
+from sqlalchemy.sql.expression import table
+
+
+groups_table = table(
+    'groups',
+    column('active'),
+    column('external_id'),
+    column('groupid'),
+    column('groupname'),
+    column('is_local'),
+    column('title'),
+)
+
+groups_users_table = table(
+    'groups_users',
+    column('groupid'),
+    column('userid'),
+)
+
+
+class RemoveInactiveGroupMemberships(SchemaMigration):
+    """Remove inactive group memberships.
+    """
+
+    def migrate(self):
+
+        rows = self.execute(
+            select([groups_table.c.groupid])
+            .where(groups_table.c.active.is_(False))
+            .where(groups_table.c.is_local.is_(False))
+        ).fetchall()
+
+        groupids = [row[0] for row in rows]
+
+        self.execute(
+            groups_users_table.delete()
+            .where(groups_users_table.c.groupid.in_(groupids)))
