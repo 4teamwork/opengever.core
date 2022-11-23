80c3f3a988fcf2b904369165d3decb48f6c3c6e3
Niklaus Johner <niklaus.johner@4teamwork.ch>
2022-11-23 14:28:27 +0100

    Update task assignments for existing tasks.

---
+++ opengever/core/upgrades/20221123141742_update_task_assignment_role/upgrade.py
@@ -0,0 +1,29 @@
+from ftw.upgrade import UpgradeStep
+from opengever.base.role_assignments import ASSIGNMENT_VIA_TASK
+from opengever.base.role_assignments import ASSIGNMENT_VIA_TASK_AGENCY
+from opengever.base.role_assignments import RoleAssignmentStorage
+
+
+class UpdateTaskAssignmentRole(UpgradeStep):
+    """Update task assignment role.
+    There is no need to reindex the security indexes as view permissions
+    were not changed. We therefore do the update directly using the
+    RoleAssignmentManager.
+    """
+
+    deferrable = True
+
+    def __call__(self):
+        query = {'object_provides': 'opengever.dossier.behaviors.dossier.IDossierMarker'}
+        for dossier in self.objects(query, "Update task assignments."):
+            storage = RoleAssignmentStorage(dossier)
+            for assignment in storage.get_by_cause(ASSIGNMENT_VIA_TASK):
+                self.update_assignment(assignment)
+            for assignment in storage.get_by_cause(ASSIGNMENT_VIA_TASK_AGENCY):
+                self.update_assignment(assignment)
+
+    @staticmethod
+    def update_assignment(assignment):
+        if 'Contributor' in assignment['roles']:
+            assignment['roles'].remove('Contributor')
+            assignment['roles'].append('TaskResponsible')
