a9abdaf4755c7897136f3e42d5584d338475d41b
Philippe Gross <philippe.gross@4teamwork.ch>
2022-11-24 13:22:51 +0100

    Protect workspace-content-members endpoint for guests if guets are not allowed
    to access the members.

--- opengever/api/workspace_content_members.py
+++ opengever/api/workspace_content_members.py
@@ -6,6 +6,7 @@ from plone.restapi.interfaces import ISerializeToJson
 from plone.restapi.services import Service
 from Products.CMFPlone.utils import safe_unicode
 from zExceptions import BadRequest
+from zExceptions import Forbidden
 from zope.component import getMultiAdapter
 
 
@@ -13,6 +14,10 @@ class WorkspaceContentMembersGet(Service):
     def reply(self):
         if not is_within_workspace(self.context):
             raise BadRequest("'{}' is not within a workspace".format(self.context.getId()))
+
+        if not self.context.access_members_allowed():
+            raise Forbidden
+
         source = WorkspaceContentMemberUsersSource(self.context)
         query = safe_unicode(self.request.form.get('query', ''))
         results = source.search(query)
