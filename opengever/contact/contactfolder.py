37760df78ab86de7090ce04ffa6e35caa55ee7dc
Elio Schmutz <e.schmutz@4teamwork.ch>
2022-12-09 16:24:10 +0100

    Remove sql contacts from ContactFolder dexterity type

--- opengever/contact/contactfolder.py
+++ opengever/contact/contactfolder.py
@@ -1,7 +1,5 @@
 from opengever.base.behaviors.translated_title import TranslatedTitleMixin
-from opengever.contact import is_contact_feature_enabled
 from opengever.contact.interfaces import IContactFolder
-from opengever.contact.models import Contact
 from opengever.ogds.base.wrapper import TeamWrapper
 from opengever.ogds.models.team import Team
 from plone.dexterity.content import Container
@@ -29,12 +27,6 @@ class ContactFolder(Container, TranslatedTitleMixin):
         if obj is not default:
             return obj
 
-        if id_.startswith('contact-'):
-            contact_id = int(id_.split('-')[-1])
-            contact = Contact.query.get(contact_id)
-            if contact:
-                return contact.get_wrapper(self)
-
         if id_.startswith('team-'):
             team_id = int(id_.split('-')[-1])
             team = Team.query.get(team_id)
@@ -44,13 +36,3 @@ class ContactFolder(Container, TranslatedTitleMixin):
         if default is _marker:
             raise KeyError(id_)
         return default
-
-    def allowedContentTypes(self, *args, **kwargs):
-        types = super(ContactFolder, self).allowedContentTypes(*args, **kwargs)
-
-        def filter_type(fti):
-            if fti.id == "opengever.contact.contact":
-                return not is_contact_feature_enabled()
-            return True
-
-        return filter(filter_type, types)
