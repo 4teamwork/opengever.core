2dd3ef930305215f22ce977c5586c36acb435e2d
Philippe Gross <philippe.gross@4teamwork.ch>
2022-12-19 12:00:41 +0100

    Convert TestNoCheckedOutDocsValidator from mock to integrationtests.

--- opengever/task/tests/test_validators.py
+++ opengever/task/tests/test_validators.py
@@ -1,82 +1,53 @@
-from ftw.testing import MockTestCase
-from opengever.core.testing import COMPONENT_UNIT_TESTING
 from opengever.task.browser.complete import ICompleteSuccessorTaskSchema
 from opengever.task.browser.complete import NoCheckedoutDocsValidator
 from opengever.task.task import ITask
-from plone.uuid.interfaces import IAttributeUUID
+from opengever.testing import IntegrationTestCase
 from plone.uuid.interfaces import IUUID
 from zope.app.intid.interfaces import IIntIds
+from zope.component import getUtility
 from zope.interface import Invalid
-from zope.schema import getFields
 
 
-class TestTaskCompletion(MockTestCase):
-    layer = COMPONENT_UNIT_TESTING
+class TestNoCheckedOutDocsValidator(IntegrationTestCase):
 
-    def test_nocheckedout_docs_validator(self):
+    def test_nocheckedout_docs_validator_in_complete_form(self):
+        self.login(self.regular_user)
 
-        uuidtocatalogbrain = self.mocker.replace(
-            'plone.app.uuid.utils.uuidToCatalogBrain')
+        intids = getUtility(IIntIds)
 
-        intids = self.stub()
-        self.mock_utility(intids, IIntIds)
-
-        uuid_adapter = self.stub()
-        self.mock_adapter(uuid_adapter, IUUID, [IAttributeUUID, ])
-
-        task1 = self.stub()
-        request = self.stub()
-
-        doc1 = self.providing_stub([IUUID])
-        self.expect(doc1.title).result('Doc 1')
-        self.expect(doc1.checked_out).result('hugo.boss')
-        self.expect(uuid_adapter(doc1)).result(111)
-        self.expect(intids.getObject(111)).result(doc1)
-        self.expect(uuidtocatalogbrain(doc1)).result(doc1).count(0, None)
-
-        doc2 = self.providing_stub([IUUID])
-        self.expect(doc2.title).result('Doc 2')
-        self.expect(doc2.checked_out).result(None)
-        self.expect(uuid_adapter(doc2)).result(222)
-        self.expect(intids.getObject(222)).result(doc2)
-        self.expect(uuidtocatalogbrain(doc2)).result(doc2).count(0, None)
-
-        doc3 = self.providing_stub([IUUID])
-        self.expect(doc3.title).result('Doc 3')
-        self.expect(doc3.checked_out).result('james.bond')
-        self.expect(uuid_adapter(doc3)).result(333)
-        self.expect(intids.getObject(333)).result(doc3)
-        self.expect(uuidtocatalogbrain(doc3)).result(doc3).count(0, None)
-
-        self.replay()
-
-        field = getFields(ICompleteSuccessorTaskSchema).get('documents')
-        validator = NoCheckedoutDocsValidator(task1, request, None, field,
-                                              None)
+        validator = NoCheckedoutDocsValidator(
+            self.task, self.request, None,
+            ICompleteSuccessorTaskSchema['documents'], None)
 
         # no docs selected
         validator.validate([])
 
-        # checked out doc selected
-        with self.assertRaises(Invalid):
-            validator.validate(['111', ])
-
         # no checked out doc selected
-        validator.validate(['222'])
+        validator.validate([intids.getId(self.document)])
+
+        # checked out doc selected
+        self.checkout_document(self.document)
+        with self.assertRaises(Invalid):
+            validator.validate([intids.getId(self.document)])
 
         # mutlitple checked out docs selected
+        self.checkout_document(self.taskdocument)
         with self.assertRaises(Invalid):
-            validator.validate(['111', '222', '333'])
+            validator.validate([intids.getId(self.document),
+                                intids.getId(self.taskdocument)])
 
-        # check also for relation list field
-        field = getFields(ITask).get('relatedItems')
+    def test_nocheckedout_docs_validator_in_task_form(self):
+        self.login(self.regular_user)
+
         validator = NoCheckedoutDocsValidator(
-            task1, request, None, field, None)
+            self.task, self.request, None, ITask['relatedItems'], None)
 
-        validator.validate([doc2, ])
+        validator.validate([self.document, self.taskdocument])
+
+        self.checkout_document(self.document)
 
         with self.assertRaises(Invalid):
-            validator.validate([doc1, doc2, doc3])
+            validator.validate([self.document, self.taskdocument])
 
         # check also without a value
         validator.validate(None)
