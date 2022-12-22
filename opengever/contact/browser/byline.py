290e9c97d4cb8054e7ab6af998e653e40aad1e94
Lukas Graf <lukas.graf@4teamwork.ch>
2022-12-22 21:37:46 +0100

    Remove SQL contacts byline customizations.

--- opengever/contact/browser/byline.py
+++ opengever/contact/browser/byline.py
@@ -1,33 +1,4 @@
 from opengever.base.viewlets.byline import BylineBase
-from opengever.contact import _
-from Products.CMFPlone import PloneMessageFactory as PMF
-
-
-class ContactByline(BylineBase):
-
-    def get_items(self):
-        return [
-            {'class': 'sequenceNumber',
-             'label': _('label_sequence_number', default='Sequence Number'),
-             'content': self.context.model.contact_id,
-             'replace': False},
-
-            {'class': 'active',
-             'label': _('label_active', default='Active'),
-             'content': self.active_label(),
-             'replace': False},
-
-            {'class': 'former_contact_id',
-             'label': _('label_former_contact_id',
-                        default='Former contact ID'),
-             'content': self.context.model.former_contact_id,
-             'replace': False}
-        ]
-
-    def active_label(self):
-        if self.context.model.is_active:
-            return PMF('Yes')
-        return PMF('No')
 
 
 class TeamByline(BylineBase):
