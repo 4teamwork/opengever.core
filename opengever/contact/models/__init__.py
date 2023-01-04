08736d5cabc0e948fc3c92bd8e83465cd953036f
Lukas Graf <lukas.graf@4teamwork.ch>
2023-01-04 15:49:25 +0100

    Remove SQL Contact base model.

--- opengever/contact/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from opengever.contact.models.archivedcontact import ArchivedContact  # noqa
-from opengever.contact.models.contact import Contact  # noqa
-
-
-tables = [
-    'archived_contacts',
-    'contacts',
-]
