fc4f53890a8eb511b97bbc3ae4a5905079ce3a2e
Elio Schmutz <e.schmutz@4teamwork.ch>
2023-01-11 08:43:42 +0100

    Add touched date index for workspaces.

--- opengever/core/upgrades/20230110082930_add_i_touched_behavior/upgrade.py
+++ opengever/core/upgrades/20230110082930_add_i_touched_behavior/upgrade.py
@@ -10,6 +10,7 @@ class AddITouchedBehavior(UpgradeStep):
     def __call__(self):
         self.install_upgrade_profile()
         self.migrate_touched_to_new_behavior()
+        self.index_touched_on_worskspaces()
 
     def migrate_touched_to_new_behavior(self):
         TOUCHED_ANNOTATIONS_KEY = '{}.touched'.format(IDossier.__identifier__)
@@ -22,3 +23,19 @@ class AddITouchedBehavior(UpgradeStep):
                 # fast. So there is also no need for a nightly upgrade step.
                 ITouched(obj).touched = dossier_schema.__dict__['annotations'].get(TOUCHED_ANNOTATIONS_KEY)
                 del dossier_schema.__dict__['annotations'][TOUCHED_ANNOTATIONS_KEY]
+
+    def index_touched_on_worskspaces(self):
+        query = {'object_provides': 'opengever.workspace.interfaces.IWorkspace'}
+        for obj in self.objects(query, "Index touched on workspaces."):
+            # Get the last changed object within the workspace or the workspace
+            # itself to determine the current touched date.
+            ITouched(obj).touched = self.catalog_unrestricted_search(
+                {
+                    'path': {'query': '/'.join(obj.getPhysicalPath())},
+                    'sort_on': 'changed',
+                    'sort_order': 'descending',
+                    'sort_limit': 1
+                }
+            )[0].changed
+
+            obj.reindexObject(idxs=['UID', 'touched'])
