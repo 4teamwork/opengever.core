1199d3a72a54767fdf84a9dc29e07890fb3c5cb8
Elio Schmutz <e.schmutz@4teamwork.ch>
2023-01-18 08:58:21 +0100

    Map group_name to group_id for policyless deployments

--- opengever/setup/creation/orgunit.py
+++ opengever/setup/creation/orgunit.py
@@ -1,9 +1,12 @@
+from opengever.ogds.models.group import Group
 from opengever.ogds.models.org_unit import OrgUnit
 from opengever.ogds.models.service import ogds_service
 from opengever.setup import DEVELOPMENT_USERS_GROUP
+from opengever.setup.creation import OGDS_GROUP_KEY_REPLACE_MARKER_PREFIX
 from opengever.setup.creation.unit import UnitCreator
 from opengever.setup.exception import GeverSetupException
 from plone import api
+import uuid
 
 
 class OrgUnitCreator(UnitCreator):
@@ -16,8 +19,22 @@ class OrgUnitCreator(UnitCreator):
                            'inbox_group_id')
 
     def apply_development_config(self, item):
-        item['users_group_id'] = DEVELOPMENT_USERS_GROUP
-        item['inbox_group_id'] = DEVELOPMENT_USERS_GROUP
+        if self.is_policyless:
+            item['users_group_name'] = DEVELOPMENT_USERS_GROUP
+            item['inbox_group_name'] = DEVELOPMENT_USERS_GROUP
+        else:
+            item['users_group_id'] = DEVELOPMENT_USERS_GROUP
+            item['inbox_group_id'] = DEVELOPMENT_USERS_GROUP
+
+    def preprocess(self, item):
+        users_group_name = item.get('users_group_name')
+        inbox_group_name = item.get('inbox_group_name')
+        if users_group_name:
+            item['users_group_id'] = self.get_or_create_group_by_name(users_group_name).groupid
+            del item['users_group_name']
+        if inbox_group_name:
+            item['inbox_group_id'] = self.get_or_create_group_by_name(inbox_group_name).groupid
+            del item['inbox_group_name']
 
     def check_constraints(self, item):
         super(OrgUnitCreator, self).check_constraints(item)
@@ -40,7 +57,7 @@ class OrgUnitCreator(UnitCreator):
         group = ogds_service().fetch_group(groupid)
         if group is None:
             raise GeverSetupException(
-                "Missing Group '{}' while creating Org-Unit '{}'".format(
+                "Missing Group with groupid '{}' while creating Org-Unit '{}'".format(
                     groupid, org_unit_id))
 
     def create_unit(self, item):
@@ -49,3 +66,12 @@ class OrgUnitCreator(UnitCreator):
         site = api.portal.get()
         site.acl_users.portal_role_manager.assignRoleToPrincipal(
             'Member', item['users_group_id'])
+
+    def get_or_create_group_by_name(self, group_name):
+        group = Group.query.filter(Group.groupname == group_name).one_or_none()
+        if not group:
+            external_id = OGDS_GROUP_KEY_REPLACE_MARKER_PREFIX + group_name
+            group = Group(**{'groupid': uuid.uuid4().hex, 'groupname': group_name, 'external_id': external_id})
+            self.session.add(group)
+
+        return group
