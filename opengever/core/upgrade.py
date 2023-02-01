0bcaf06a75d159c319c14f71675f7bee81cdb3a8
Lukas Graf <lukas.graf@4teamwork.ch>
2023-02-01 10:50:19 +0100

    Fix NightlyMailAttachmentInfoUpdater: The _update_attachment_infos method
    that was used before would have overwritten our 'extracted' key in the
    attachment infos. Instead, iterate over existing attachments and selectively
    update them using _modify_attachment_info.

--- opengever/core/upgrade.py
+++ opengever/core/upgrade.py
@@ -4,6 +4,7 @@ from BTrees.IIBTree import IITreeSet
 from BTrees.OOBTree import OOTreeSet
 from decorator import decorator
 from ftw.bumblebee.interfaces import IBumblebeeDocument
+from ftw.mail.utils import get_attachments
 from ftw.solr.interfaces import ISolrConnectionManager
 from ftw.solr.interfaces import ISolrIndexHandler
 from ftw.upgrade import UpgradeStep
@@ -613,6 +614,22 @@ class NightlyMailAttachmentInfoUpdater(UIDMaintenanceJobContextManagerMixin):
     @classmethod
     def update_attachment_infos(cls, key):
         mail = cls.key_to_obj(key)
+
         if not mail:
             return
-        mail._update_attachment_infos()
+
+        pos = itemgetter('position')
+
+        existing_attachments = sorted(mail.attachment_infos, key=pos)
+        updated_attachments = sorted(get_attachments(mail.msg), key=pos)
+
+        if not list(map(pos, existing_attachments)) == list(map(pos, updated_attachments)):
+            logger.warn(
+                'NightlyMailAttachmentInfoUpdater: Failed to update '
+                'attachments for mail %r' % mail)
+            return
+
+        for existing, updated in zip(existing_attachments, updated_attachments):
+            assert existing['position'] == updated['position']
+            assert existing['size'] == updated['size']
+            mail._modify_attachment_info(**updated)
