e395a76048bf939dd3b0079103121203c2fd5753
Niklaus Johner <niklaus.johner@4teamwork.ch>
2023-02-08 15:36:02 +0100

    Handle broken references when generating the meeting minutes PDF.

--- opengever/workspace/tests/test_meeting_minutes.py
+++ opengever/workspace/tests/test_meeting_minutes.py
@@ -1,6 +1,8 @@
 from ftw.testbrowser import browsing
+from opengever.base.interfaces import IDeleter
 from opengever.core.testing import WEASYPRINT_SERVICE_INTEGRATION_TESTING
 from opengever.testing import IntegrationTestCase
+from opengever.trash.trash import ITrasher
 from opengever.workspace.browser.meeting_pdf import MeetingMinutesPDFView
 from zope.globalrequest import getRequest
 
@@ -8,6 +10,8 @@ from zope.globalrequest import getRequest
 class TestMeetingMinutes(IntegrationTestCase):
 
     layer = WEASYPRINT_SERVICE_INTEGRATION_TESTING
+
+    features = ('workspace', )
 
     @browsing
     def test_meeting_minutes_pdf_view_returns_a_pdf(self, browser):
@@ -28,3 +32,19 @@ class TestMeetingMinutes(IntegrationTestCase):
         self.assertEqual(['Teamraumdokument'], browser.css('ul.related_items a').text)
         self.assertEqual(self.workspace_document.absolute_url(),
                          browser.css('ul.related_items a')[0].get('href'))
+
+    @browsing
+    def test_meeting_minutes_html_handles_broken_relations(self, browser):
+        self.login(self.workspace_member, browser)
+
+        # We delete the document referenced by the agendaitem
+        ITrasher(self.workspace_document).trash()
+        deleter = IDeleter(self.workspace_document)
+        deleter.delete()
+
+        view = MeetingMinutesPDFView(self.workspace_meeting, getRequest())
+        html = view.meeting_minutes_html()
+        browser.parse(html)
+
+        self.assertEqual(['Decision:'], browser.css('h3').text)
+        self.assertEqual([], browser.css('ul.related_items a').text)
