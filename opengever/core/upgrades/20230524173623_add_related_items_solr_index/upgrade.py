80adfe1c6660f19c2a39b21bc53b99947ec5aaaa
Niklaus Johner <niklaus.johner@4teamwork.ch>
2023-05-31 10:18:11 +0200

    Only index related items when necessary.
    We expect only a minority of documents and tasks to have related
    items, so we conditionally only index objects that have related items.
    This requires getting the objects during the upgrade step, but
    will largely diminish the number of nightly jobs.

--- opengever/core/upgrades/20230524173623_add_related_items_solr_index/upgrade.py
+++ opengever/core/upgrades/20230524173623_add_related_items_solr_index/upgrade.py
@@ -1,5 +1,6 @@
 from ftw.upgrade import UpgradeStep
 from opengever.core.upgrade import NightlyIndexer
+from opengever.document.behaviors.related_docs import IRelatedDocuments
 from opengever.document.document import IDocumentSchema
 from opengever.task.task import ITask
 
@@ -11,10 +12,15 @@ class AddRelatedItemsSolrIndex(UpgradeStep):
     deferrable = True
 
     def __call__(self):
-        query = {'object_provides': [IDocumentSchema.__identifier__,
-                                     ITask.__identifier__]}
+        query_tasks = {'object_provides': ITask.__identifier__}
+        query_docs = {'object_provides': IDocumentSchema.__identifier__}
 
         with NightlyIndexer(idxs=["related_items"],
                             index_in_solr_only=True) as indexer:
-            for brain in self.brains(query, 'Index related_items in Solr'):
-                indexer.add_by_brain(brain)
+            for obj in self.objects(query_tasks, 'Index related_items in Solr for tasks'):
+                if ITask(obj).relatedItems:
+                    indexer.add_by_obj(obj)
+
+            for obj in self.objects(query_docs, 'Index related_items in Solr for documents'):
+                if IRelatedDocuments(obj).relatedItems:
+                    indexer.add_by_obj(obj)
