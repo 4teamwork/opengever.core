e2b49ba008918d9e93480141584b1661d29bc9b8
Lukas Graf <l.graf@4teamwork.ch>
2023-11-06 10:51:17 +0100

    Disable IPropertiesPlugin for OGDS auth plugin for now:
    This is to limit test failures and avoid having to fix them all at once.

--- opengever/testing/integration_test_case.py
+++ opengever/testing/integration_test_case.py
@@ -55,6 +55,7 @@ from Products.CMFDiffTool.utils import safe_utf8
 from Products.PlonePAS.interfaces.group import IGroupIntrospection
 from Products.PluggableAuthService.interfaces.plugins import IGroupEnumerationPlugin
 from Products.PluggableAuthService.interfaces.plugins import IGroupsPlugin
+from Products.PluggableAuthService.interfaces.plugins import IPropertiesPlugin
 from Products.PluggableAuthService.interfaces.plugins import IUserEnumerationPlugin
 from sqlalchemy.sql.expression import desc
 from urllib import urlencode
@@ -160,6 +161,7 @@ class IntegrationTestCase(TestCase):
             IGroupIntrospection,
             IGroupEnumerationPlugin,
             IGroupsPlugin,
+            IPropertiesPlugin,
         ]
         for iface in ifaces_to_disable:
             plugin_registry.deactivatePlugin(iface, 'ogds_auth')
