adbfc5731ca9dc5304f36a2ead6c5113fa6a30f7
Thomas Buchberger <t.buchberger@4teamwork.ch>
2024-03-05 16:09:36 +0100

    Make upgrade setp for RIS proposal workflow deferrable

--- opengever/core/upgrades/20240207151101_add_ris_proposal_workflow/upgrade.py
+++ opengever/core/upgrades/20240207151101_add_ris_proposal_workflow/upgrade.py
@@ -5,6 +5,8 @@ class AddRisProposalWorkflow(UpgradeStep):
     """Add ris proposal workflow.
     """
 
+    deferrable = True
+
     def __call__(self):
         self.install_upgrade_profile()
         self.update_workflow_security(['opengever_dossier_workflow'],
