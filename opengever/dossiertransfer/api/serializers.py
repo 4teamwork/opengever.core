d940dd8412cbf010ead87849bd5bcdc490836aa3
Lukas Graf <l.graf@4teamwork.ch>
2024-03-22 16:33:27 +0100

    WIP: Serialize (sub)dossiers for a transfer in full_content mode.

--- opengever/dossiertransfer/api/serializers.py
+++ opengever/dossiertransfer/api/serializers.py
@@ -1,9 +1,12 @@
 from opengever.base.interfaces import IOpengeverBaseLayer
+from opengever.dossier.behaviors.dossier import IDossierMarker
 from opengever.dossiertransfer.model import DossierTransfer
 from plone import api
 from plone.restapi.interfaces import ISerializeToJson
 from plone.restapi.serializer.converters import json_compatible
 from zope.component import adapter
+from zope.component import getMultiAdapter
+from zope.globalrequest import getRequest
 from zope.interface import implementer
 
 
@@ -59,8 +62,54 @@ class FullTransferContentSerializer(object):
     def __call__(self):
         # TODO
         content = {
-            'dossiers': [],
+            'dossiers': DossiersSerializer(self.transfer)(),
             'documents': [],
             'contacts': [],
         }
         return content
+
+
+class DossiersSerializer(object):
+    """Returns the list of (sub)dossiers included in the transfer.
+    """
+
+    ignored_keys = (
+        '@components',
+        'items',
+        'items_total',
+        'next_item',
+        'previous_item',
+    )
+
+    def __init__(self, transfer):
+        self.transfer = transfer
+
+    def __call__(self):
+        # This currently returns a flat list of dossiers, ordered by path
+        # (parents before children, so in the order they should be created).
+        # Depending on what is easiest to use on the deserialization side, we
+        # might want to change this to return a hierarchical structure.
+        serialized_dossiers = []
+
+        catalog = api.portal.get_tool('portal_catalog')
+
+        root_dossier = api.content.uuidToObject(self.transfer.root)
+        query = {
+            'path': '/'.join(root_dossier.getPhysicalPath()),
+            'object_provides': IDossierMarker.__identifier__,
+            'sort_on': 'path',
+        }
+        all_dossiers = catalog.unrestrictedSearchResults(**query)
+
+        for brain in all_dossiers:
+            dossier = brain.getObject()
+            serialized = getMultiAdapter(
+                (dossier, getRequest()), ISerializeToJson)()
+
+            for key in self.ignored_keys:
+                serialized.pop(key, None)
+
+            serialized['@id'] = dossier.absolute_url()
+            serialized_dossiers.append(serialized)
+
+        return serialized_dossiers
