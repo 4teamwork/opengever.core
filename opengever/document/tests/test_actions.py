2d557677e1e63f37c2b9ea5946755da1b48da8bc
Elio Schmutz <e.schmutz@4teamwork.ch>
2024-04-22 16:24:47 +0200

    Fix docoument actions test to test with bumblebee-feature

--- opengever/document/tests/test_actions.py
+++ opengever/document/tests/test_actions.py
@@ -286,7 +286,8 @@ class TestDocumentContextActions(IntegrationTestCase):
 
         self.login(self.workspace_guest)
         document = create(Builder('document')
-                          .within(self.workspace))
+                          .within(self.workspace)
+                          .with_dummy_content())
 
-        expected_actions_restricted_guest = [u'revive_bumblebee_preview']
+        expected_actions_restricted_guest = [u'revive_bumblebee_preview', 'save_document_as_pdf']
         self.assertEqual(expected_actions_restricted_guest, self.get_actions(document))
