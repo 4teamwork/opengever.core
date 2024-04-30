fa426aea4c767669e02adcf4216cdf8b4866bf7e
Philippe Gross <philippe.gross@4teamwork.ch>
2024-04-30 11:15:36 +0200

    Remove oneoffixx template selection in form and service.

--- opengever/oneoffixx/browser/form.py
+++ opengever/oneoffixx/browser/form.py
@@ -1,151 +1,14 @@
-from copy import deepcopy
-from opengever.base.schema import TableChoice
-from opengever.base.sentry import log_msg_to_sentry
 from opengever.oneoffixx import _
-from opengever.oneoffixx.api_client import OneoffixxAPIClient
 from opengever.oneoffixx.command import CreateDocumentFromOneOffixxTemplateCommand
-from opengever.oneoffixx.templates import get_whitelisted_oneoffixx_templates
-from opengever.oneoffixx.templates import OneOffixxTemplate
-from plone import api
 from plone.supermodel import model
 from plone.z3cform.layout import FormWrapper
 from z3c.form import button
 from z3c.form.field import Fields
 from z3c.form.form import Form
 from zope import schema
-from zope.globalrequest import getRequest
-from zope.i18n import translate
-from zope.interface import provider
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary
-
-
-FAVORITES_FAKE_ID = '__favorites__'
-
-
-def get_oneoffixx_favorites():
-    """Return the user chosen favorites as a template group, if any."""
-    api_client = OneoffixxAPIClient()
-    favorites = api_client.get_oneoffixx_favorites()
-    if favorites:
-        return favorites
-    return None
-
-
-def get_oneoffixx_template_groups():
-    """Return the template groups.
-
-    Potentially amended with user chosen favorites.
-    """
-    api_client = OneoffixxAPIClient()
-    # We need to work on a copy to not pollute the cached one
-    template_groups = deepcopy(api_client.get_oneoffixx_template_groups())
-    favorites = get_oneoffixx_favorites()
-    if favorites:
-        favorites_template_group = {
-            u'id': FAVORITES_FAKE_ID,
-            u'localizedName': translate(_(u'label_favorites', default=u'Favorites'),
-                                        context=getRequest()),
-            u'templates': favorites}
-
-        template_groups.insert(0, favorites_template_group)
-    return template_groups
-
-
-def get_oneoffixx_templates():
-    """Return all oneoffixx templates.
-
-    We do not want duplicates from favorites here.
-    """
-    return get_whitelisted_oneoffixx_templates(OneoffixxAPIClient())
-
-
-def default_template_group():
-    """Returns the template group id or the favorites fake-groupid, if user has
-    favorites defined."""
-
-    favorites = get_oneoffixx_favorites()
-    if favorites:
-        return FAVORITES_FAKE_ID
-    return None
-
-
-@provider(IContextSourceBinder)
-def list_templates(context):
-    """Return a list available templates."""
-    templates = get_oneoffixx_templates()
-    template_group = context.REQUEST.form.get('form.widgets.template_group')
-    terms = []
-
-    for template in templates:
-        terms.append(SimpleVocabulary.createTerm(
-            template, template.template_id, template.title))
-
-    # We filter templates when template_group has been selected
-    if template_group is not None:
-        favorites = get_oneoffixx_favorites()
-        # Favorites are a special case
-        if favorites and template_group[0] == FAVORITES_FAKE_ID:
-            favorite_label = translate(
-                _(u'label_favorites', default=u'Favorites'), context=getRequest()),
-            terms = [
-                SimpleVocabulary.createTerm(
-                    OneOffixxTemplate(template, favorite_label),
-                    template.get('id'),
-                    template.get('localizedName'),
-                )
-                for template in favorites
-            ]
-        elif template_group[0] != '--NOVALUE--':
-            terms = [term for term in terms if term.value.group == template_group[0]]
-
-    return MutableObjectVocabulary(terms)
-
-
-@provider(IContextSourceBinder)
-def list_template_groups(context):
-    """Return the list of available template groups."""
-    template_groups = get_oneoffixx_template_groups()
-    terms = []
-    for group in template_groups:
-        terms.append(SimpleVocabulary.createTerm(group.get("id"),
-                                                 group.get("id"),
-                                                 group.get("localizedName")))
-    return MutableObjectVocabulary(terms)
-
-
-class MutableObjectVocabulary(SimpleVocabulary):
-
-    def __contains__(self, value):
-        try:
-            return any([value == val for val in self.by_value])
-        except TypeError:
-            return False
 
 
 class ICreateDocumentFromOneOffixxTemplate(model.Schema):
-
-    # XXX - this always renders the --NOVALUE-- as the actually chosen
-    # default is actually loaded over AJAX - confusing and bad UX
-    template_group = schema.Choice(
-        title=_(u'label_template_group', default=u'Template group'),
-        source=list_template_groups,
-        required=False,
-        defaultFactory=default_template_group,
-    )
-
-    template = TableChoice(
-        title=_(u"label_template", default=u"Template"),
-        source=list_templates,
-        required=True,
-        show_filter=True,
-        vocabulary_depends_on=['form.widgets.template_group'],
-        columns=(
-            {'column': 'title',
-             'column_title': _(u'label_title', default=u'Title'),
-             'sort_index': 'sortable_title'},
-        )
-    )
 
     title = schema.TextLine(
         title=_(u"label_title", default=u"Title"),
@@ -158,11 +21,6 @@ class SelectOneOffixxTemplateDocumentWizardStep(Form):
     ignoreContext = True
     fields = Fields(ICreateDocumentFromOneOffixxTemplate)
 
-    def updateWidgets(self, prefix=None):
-        super(SelectOneOffixxTemplateDocumentWizardStep, self).updateWidgets(prefix=prefix)
-        self.widgets['template_group'].noValueMessage = translate(
-            _(u'label_all_template_groups', default=u'All templates'), context=self.request)
-
     def finish_document_creation(self, data):
         new_doc = self.create_document(data)
         self.activate_external_editing(new_doc)
@@ -174,7 +32,7 @@ class SelectOneOffixxTemplateDocumentWizardStep(Form):
 
     def create_document(self, data):
         """Create a new document based on a template."""
-        command = CreateDocumentFromOneOffixxTemplateCommand(self.context, data['title'], data['template'])
+        command = CreateDocumentFromOneOffixxTemplateCommand(self.context, data['title'])
         return command.execute()
 
     @button.buttonAndHandler(_('button_save', default=u'Save'), name='save')
@@ -195,15 +53,3 @@ class SelectOneOffixxTemplateDocumentWizardStep(Form):
 class SelectOneOffixxTemplateDocumentView(FormWrapper):
 
     form = SelectOneOffixxTemplateDocumentWizardStep
-
-    def __call__(self):
-        try:
-            return super(SelectOneOffixxTemplateDocumentView, self).__call__()
-        except Exception as exc:
-            log_msg_to_sentry(exc.message, context=self.context,
-                              request=self.request, url=self.request.URL)
-
-            msg = _(u'msg_could_not_connect_to_oneoffixx',
-                    u'Connection to OneOffixx failed.')
-            api.portal.show_message(message=msg, request=self.request, type='error')
-            self.request.RESPONSE.redirect(self.context.absolute_url())
--- opengever/oneoffixx/command.py
+++ opengever/oneoffixx/command.py
@@ -1,5 +1,4 @@
 from opengever.base.command import BaseObjectCreatorCommand
-from zope.annotation.interfaces import IAnnotations
 
 
 class CreateDocumentFromOneOffixxTemplateCommand(BaseObjectCreatorCommand):
@@ -9,20 +8,6 @@ class CreateDocumentFromOneOffixxTemplateCommand(BaseObjectCreatorCommand):
     portal_type = 'opengever.document.document'
     primary_field_name = 'file'
 
-    def __init__(self, context, title, template):
-        self.template_id = template.template_id
-        self.filename = template.filename
-        self.languages = template.languages
-        self.content_type = template.content_type
-        super(CreateDocumentFromOneOffixxTemplateCommand, self).__init__(
-            context, title)
-
     def execute(self):
         obj = super(CreateDocumentFromOneOffixxTemplateCommand, self).execute()
-        annotations = IAnnotations(obj)
-        annotations["template-id"] = self.template_id
-        annotations["languages"] = self.languages
-        annotations["filename"] = self.filename
-        annotations["content-type"] = self.content_type
-
         return obj.as_shadow_document()
