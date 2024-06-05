2fa91decd05508e08257c91590ed8b4205c71989
Abdelrahman Moustafa <abdelrahman.moustafa@4teamwork.ch>
2024-06-05 16:05:08 +0200

    Fix access for workspace admin on export users

--- opengever/workspace/report.py
+++ opengever/workspace/report.py
@@ -9,7 +9,12 @@ class WorkspaceParticipantsReport(UserReport):
     filename = "workspace_participants_report.xlsx"
 
     def check_permissions(self):
-        if not api.user.has_permission(
-            'opengever.workspace.AccessAllUsersAndGroups', obj=self.context
-        ):
+
+        all_users_and_group_permission = api.user.has_permission(
+            'opengever.workspace: Access all users and groups', obj=self.context
+        )
+        workspace_admin_permission = api.user.has_permission(
+            'Sharing page: Delegate WorkspaceAdmin role', obj=self.context
+        )
+        if not all_users_and_group_permission or not workspace_admin_permission:
             raise Unauthorized
