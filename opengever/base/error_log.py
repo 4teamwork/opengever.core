e6932e27550aa7fe060a0498634697539b5bfc00
Elio Schmutz <e.schmutz@4teamwork.ch>
2024-06-25 13:44:37 +0200

    Implement ErrorLogRedis object to store error log items into redis

---
+++ opengever/base/error_log.py
@@ -0,0 +1,96 @@
+from copy import deepcopy
+from datetime import datetime
+from opengever.base.utils import to_safe_html
+from opengever.redis.client import redis_client_manager
+import json
+
+
+class ErrorLogRedis:
+    STORAGE_KEY = 'error_log'
+    MAX_LOG_ITEMS = 200
+
+    def __init__(self, redis_client):
+        self.client = redis_client
+
+    def push(self, error_log_item):
+        if not isinstance(error_log_item, ErrorLogItem):
+            raise Exception('Requires an ErrorLogItem')
+
+        self.client.lpush(self.STORAGE_KEY, error_log_item.to_json())
+        self.trim()
+
+    def list_all(self):
+        for value in self.client.lrange(self.STORAGE_KEY, 0, self.MAX_LOG_ITEMS):
+            yield ErrorLogItem.from_json(value)
+
+    def list_all_by_userid(self, userid):
+        for item in self.list_all():
+            if item.userid == userid:
+                yield item
+
+    def trim(self):
+        self.client.ltrim(self.STORAGE_KEY, 0, self.MAX_LOG_ITEMS - 1)
+
+    def clear(self):
+        self.client.delete(self.STORAGE_KEY)
+
+
+class DisabledErrorLogRedis:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def push(self, *args, **kwargs):
+        pass
+
+    def list_all(self):
+        return []
+
+    def list_all_by_userid(self, *args, **kwargs):
+        return []
+
+    def trim(self):
+        pass
+
+    def clear(self):
+        pass
+
+
+class ErrorLogItem:
+    def __init__(self, log_data):
+        self.entry = {
+            'type': log_data.get('type'),
+            'value': log_data.get('value'),
+            'time': log_data.get('time'),
+            'id': log_data.get('id'),
+            'tb_html': to_safe_html(log_data.get('tb_html')),
+            'userid': log_data.get('userid')
+        }
+
+    @classmethod
+    def from_json(cls, value):
+        return ErrorLogItem(json.loads(value))
+
+    def to_json(self):
+        return json.dumps(self.entry)
+
+    @property
+    def userid(self):
+        return self.entry.get('userid', '')
+
+    def serialize(self):
+        entry = deepcopy(self.entry)
+
+        if self.entry.get('time'):
+            entry['time'] = datetime.fromtimestamp(self.entry.get('time')).isoformat()
+
+        return entry
+
+    def __eq__(self, other):
+        return self.entry == other.entry
+
+    def __repr__(self):
+        return "<ErrorLogItem {}>".format(self.entry.get('id', 'missing-id'))
+
+
+def get_error_log_redis():
+    return ErrorLogRedis(redis_client_manager.get_client())
---
+++ opengever/base/tests/test_error_log.py
@@ -0,0 +1,60 @@
+from opengever.base.error_log import ErrorLogItem
+from opengever.base.error_log import get_error_log_redis
+from opengever.core.testing import REDIS_INTEGRATION_TESTING
+from opengever.testing import IntegrationTestCase
+
+
+class TestErrorLogRedis(IntegrationTestCase):
+
+    layer = REDIS_INTEGRATION_TESTING
+
+    features = ('user_visible_error_logs', )
+
+    def test_can_push_error_log_items(self):
+        error_item1 = ErrorLogItem({'id': 1})
+        error_item2 = ErrorLogItem({'id': 2})
+
+        logger = get_error_log_redis()
+        logger.push(error_item1)
+        logger.push(error_item2)
+
+        self.assertEqual([error_item2, error_item1],
+                         list(logger.list_all()))
+
+    def test_auto_trim_items_to_max_log_items(self):
+        error_item1 = ErrorLogItem({'id': 1})
+        error_item2 = ErrorLogItem({'id': 2})
+        error_item3 = ErrorLogItem({'id': 3})
+
+        logger = get_error_log_redis()
+        logger.MAX_LOG_ITEMS = 2
+
+        logger.push(error_item1)
+        logger.push(error_item2)
+        logger.push(error_item3)
+
+        self.assertEqual(2, len(list(logger.list_all())))
+
+    def test_sanitizes_html_values(self):
+        error_item = ErrorLogItem({'id': 1, 'tb_html': '<p><script>danger</script></p>'})
+
+        logger = get_error_log_redis()
+        logger.push(error_item)
+        self.assertEqual(u'<p></p>', list(logger.list_all())[0].entry.get('tb_html'))
+
+    def test_can_list_items_by_userid(self):
+        error_item1 = ErrorLogItem({'id': 1, 'userid': 'user-a'})
+        error_item2 = ErrorLogItem({'id': 2, 'userid': 'user-b'})
+        error_item3 = ErrorLogItem({'id': 3, 'userid': 'user-a'})
+
+        logger = get_error_log_redis()
+
+        logger.push(error_item1)
+        logger.push(error_item2)
+        logger.push(error_item3)
+
+        self.assertEqual([error_item3, error_item1],
+                         list(logger.list_all_by_userid('user-a')))
+
+        self.assertEqual([error_item2],
+                         list(logger.list_all_by_userid('user-b')))
