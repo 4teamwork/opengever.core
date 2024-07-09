21b4b4e41f38175256abab94573a7223b109b6a8
Abdelrahman Moustafa <abdelrahman.moustafa@4teamwork.ch>
2024-07-09 10:34:47 +0200

    Include workspace title in participant export file name

--- opengever/workspace/report.py
+++ opengever/workspace/report.py
@@ -1,10 +1,22 @@
 from opengever.globalindex.browser.report import UserReport
+from zope.i18n import translate
+from zope.i18nmessageid import MessageFactory
+
+
+_ = MessageFactory('opengever.workspace')
 
 
 class WorkspaceParticipantsReport(UserReport):
     """View that generate an excel spreadsheet which list all selected users
     """
-    filename = "workspace_participants_report.xlsx"
+
+    @property
+    def filename(self):
+        participants = translate(_("Participants"), context=self.request)
+        return "{participants}_{workspace_title}.xlsx".format(
+            participants=participants,
+            workspace_title=self.context.title
+        )
 
     def check_permissions(self):
         pass
