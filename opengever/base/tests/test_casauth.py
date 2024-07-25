f601b949f453c067dc8c485edbb4a0436ee69bcd
Thomas Buchberger <t.buchberger@4teamwork.ch>
2024-07-25 09:12:38 +0200

    Bump ftw.casauth to version 1.7.0

--- opengever/base/tests/test_casauth.py
+++ opengever/base/tests/test_casauth.py
@@ -52,7 +52,7 @@ class TestCASServerURLForGeverPortal(IntegrationTestCase):
     def test_cas_server_url_is_fetched_from_plugin(self):
         applyProfile(self.layer['portal'], 'opengever.setup:casauth')
         cas_plugin = self.portal.acl_users.cas_auth
-        cas_plugin.cas_server_url = 'http://cas.example.org'
+        cas_plugin._cas_server_url = 'http://cas.example.org'
         self.assertEquals('http://cas.example.org', get_cas_server_url())
 
     def test_trailing_slashes_are_stripped(self):
