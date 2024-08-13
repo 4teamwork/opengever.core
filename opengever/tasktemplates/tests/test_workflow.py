fb7cb1ee6e5669087cb11af2ba8a342a505f7c54
Rakhym Annabayev <rakhym.annabayev@4teamwork.ch>
2024-08-13 11:58:51 +0200

    Make cancellation of subtasks recursive

--- opengever/tasktemplates/tests/test_workflow.py
+++ opengever/tasktemplates/tests/test_workflow.py
@@ -1,3 +1,5 @@
+from ftw.builder import Builder
+from ftw.builder import create
 from ftw.testbrowser import browsing
 from opengever.tasktemplates.interfaces import IContainSequentialProcess
 from opengever.testing import IntegrationTestCase
@@ -24,3 +26,54 @@ class TestWorfklowForTasksFromTemplatefolders(IntegrationTestCase):
             'task-state-cancelled', api.content.get_state(self.task))
         self.assertEquals(
             'task-state-cancelled', api.content.get_state(self.subtask))
+
+    @browsing
+    def test_cancelnested(self, browser):
+        self.login(self.regular_user, browser=browser)
+        alsoProvides(self.task, IContainSequentialProcess)
+
+        subtask_level3 = create(
+            Builder('task')
+            .within(self.subtask)
+            .titled(u'Subsubtask')
+            .having(
+                responsible_client='fa',
+                responsible=self.dossier_manager.getId(),
+                issuer=self.dossier_responsible.getId(),
+                task_type='correction'
+            )
+            .in_state('task-state-in-progress')
+        )
+
+        subtask_level4 = create(
+            Builder('task')
+            .within(subtask_level3)
+            .titled(u'Subtask of Subsubtask')
+            .having(
+                responsible_client='fa',
+                responsible=self.dossier_manager.getId(),
+                issuer=self.dossier_responsible.getId(),
+                task_type='correction'
+            )
+            .in_state('task-state-in-progress')
+        )
+
+        self.set_workflow_state('task-state-in-progress', self.task)
+        self.set_workflow_state('task-state-in-progress', self.subtask)
+        self.set_workflow_state('task-state-in-progress', subtask_level3)
+        self.set_workflow_state('task-state-in-progress', subtask_level4)
+
+
+        browser.open(self.task, view='tabbedview_view-overview')
+        browser.click_on('Cancel')
+        browser.fill({'Response': u'Projekt wurde gestoppt.'})
+        browser.click_on('Save')
+
+        self.assertEquals(
+            'task-state-cancelled', api.content.get_state(self.task))
+        self.assertEquals(
+            'task-state-cancelled', api.content.get_state(self.subtask))
+        self.assertEquals(
+            'task-state-cancelled', api.content.get_state(subtask_level3))
+        self.assertEquals(
+            'task-state-cancelled', api.content.get_state(subtask_level4))
