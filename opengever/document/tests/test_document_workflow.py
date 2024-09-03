2f047c6f9c77fadc68d881c74ce57eab461ab620
Elio Schmutz <e.schmutz@4teamwork.ch>
2024-09-03 10:21:22 +0200

    Implement transition extenders to start and abort the sign process

--- opengever/document/tests/test_document_workflow.py
+++ opengever/document/tests/test_document_workflow.py
@@ -1,12 +1,20 @@
+from base64 import b64encode
 from ftw.builder import Builder
 from ftw.builder import create
 from ftw.testbrowser import browsing
+from ftw.testing import freeze
 from opengever.base.security import as_internal_workflow_transition
 from opengever.document.document import Document
+from opengever.document.versioner import Versioner
+from opengever.sign.sign import Signer
+from opengever.sign.tests.test_sign import DEFAULT_MOCK_RESPONSE
+from opengever.sign.tests.test_sign import FROZEN_NOW
 from opengever.testing import IntegrationTestCase
 from plone import api
 from plone.api.exc import InvalidParameterError
 from zExceptions import Unauthorized
+import re
+import requests_mock
 
 
 class TestDocumentWorkflow(IntegrationTestCase):
@@ -199,31 +207,62 @@ class TestDocumentWorkflow(IntegrationTestCase):
             api.content.transition(obj=self.document,
                                    transition=Document.final_signing_transition)
 
-    def test_can_start_signing_a_document_in_draft_state(self):
+    @requests_mock.Mocker()
+    def test_can_start_signing_a_document_in_draft_state(self, mocker):
         self.activate_feature('sign')
+        mocker.post(re.compile('/signing-jobs'), json=DEFAULT_MOCK_RESPONSE)
+
         self.login(self.regular_user)
 
-        api.content.transition(obj=self.document,
-                               transition=Document.draft_signing_transition)
+        with freeze(FROZEN_NOW):
+            api.content.transition(obj=self.document,
+                                   transition=Document.draft_signing_transition)
 
         self.assertEquals(Document.signing_state,
                           api.content.get_state(obj=self.document))
 
-    def test_can_start_signing_a_document_in_final_state(self):
+        self.assertEqual(self.regular_user.getId(), self.document.finalizer)
+        self.assertDictEqual(
+            {
+                'created': FROZEN_NOW,
+                'job_id': '1',
+                'redirect_url': 'http://external.example.org/signing-requests/123',
+                'signers': ['foo@example.com'],
+                'userid': 'regular_user',
+                'version': 0
+            }, Signer(self.document).metadata_storage.read())
+
+    @requests_mock.Mocker()
+    def test_can_start_signing_a_document_in_final_state(self, mocker):
         self.activate_feature('sign')
+        mocker.post(re.compile('/signing-jobs'), json=DEFAULT_MOCK_RESPONSE)
+
         self.login(self.regular_user)
 
         api.content.transition(obj=self.document,
                                transition=Document.finalize_transition)
 
-        api.content.transition(obj=self.document,
-                               transition=Document.final_signing_transition)
+        with freeze(FROZEN_NOW):
+            api.content.transition(obj=self.document,
+                                   transition=Document.final_signing_transition)
 
         self.assertEquals(Document.signing_state,
                           api.content.get_state(obj=self.document))
 
-    def test_sign_transition_is_only_for_internal_use(self):
+        self.assertDictEqual(
+            {
+                'created': FROZEN_NOW,
+                'job_id': '1',
+                'redirect_url': 'http://external.example.org/signing-requests/123',
+                'signers': ['foo@example.com'],
+                'userid': 'regular_user',
+                'version': 0
+            }, Signer(self.document).metadata_storage.read())
+
+    @requests_mock.Mocker()
+    def test_sign_transition_is_only_for_internal_use(self, mocker):
         self.activate_feature('sign')
+        mocker.post(re.compile('/signing-jobs'), json=DEFAULT_MOCK_RESPONSE)
         self.login(self.regular_user)
 
         api.content.transition(obj=self.document,
@@ -231,20 +270,45 @@ class TestDocumentWorkflow(IntegrationTestCase):
 
         with self.assertRaises(InvalidParameterError):
             api.content.transition(obj=self.document,
-                                   transition=Document.signing_signed_transition)
+                                   transition=Document.signing_signed_transition,
+                                   transition_params={'filedata': b64encode('<DATA>')})
 
         self.assertEquals(Document.signing_state,
                           api.content.get_state(obj=self.document))
 
         with as_internal_workflow_transition():
             api.content.transition(obj=self.document,
-                                   transition=Document.signing_signed_transition)
+                                   transition=Document.signing_signed_transition,
+                                   transition_params={'filedata': b64encode('<DATA>')})
 
         self.assertEquals(Document.signed_state,
                           api.content.get_state(obj=self.document))
 
-    def test_can_abort_signing_document(self):
+    @requests_mock.Mocker()
+    def test_sign_transition_creates_a_new_version_with_the_given_data(self, mocker):
         self.activate_feature('sign')
+        mocker.post(re.compile('/signing-jobs'), json=DEFAULT_MOCK_RESPONSE)
+        self.login(self.regular_user)
+
+        api.content.transition(obj=self.document,
+                               transition=Document.draft_signing_transition)
+
+        self.assertEqual(0, self.document.get_current_version_id(missing_as_zero=True))
+
+        with as_internal_workflow_transition():
+            api.content.transition(obj=self.document,
+                                   transition=Document.signing_signed_transition,
+                                   transition_params={'filedata': b64encode('<DATA>')})
+
+        self.assertEqual(1, self.document.get_current_version_id(missing_as_zero=True))
+        signed_version = Versioner(self.document).retrieve(1)
+        self.assertEqual(u'<DATA>', signed_version.file.data)
+
+    @requests_mock.Mocker()
+    def test_can_abort_signing_document(self, mocker):
+        self.activate_feature('sign')
+        mocker.post(re.compile('/signing-jobs'), json=DEFAULT_MOCK_RESPONSE)
+        delete_mocker = mocker.delete(re.compile('/signing-jobs/1'))
         self.login(self.regular_user)
         api.content.transition(obj=self.document,
                                transition=Document.draft_signing_transition)
@@ -255,31 +319,51 @@ class TestDocumentWorkflow(IntegrationTestCase):
         api.content.transition(obj=self.document,
                                transition=Document.signing_final_transition)
 
+        self.assertEqual(1, delete_mocker.call_count)
         self.assertEquals(Document.final_state,
                           api.content.get_state(obj=self.document))
 
-    def test_can_abort_signed_document(self):
+    @requests_mock.Mocker()
+    def test_can_revise_signed_document(self, mocker):
         self.activate_feature('sign')
+        mocker.post(re.compile('/signing-jobs'), json=DEFAULT_MOCK_RESPONSE)
         self.login(self.regular_user)
+
+        FINALIZED_FILE_DATA = '<FINALIZED_FILE_DATA>'
+        SIGNED_FILE_DATA = '<SIGNED_FILE_DATA>'
+
+        self.document.update_file(
+            data=FINALIZED_FILE_DATA,
+            create_version=True)
+
+        self.assertEqual(1, self.document.get_current_version_id(missing_as_zero=True))
+
         api.content.transition(obj=self.document,
                                transition=Document.draft_signing_transition)
 
         with as_internal_workflow_transition():
             api.content.transition(obj=self.document,
-                                   transition=Document.signing_signed_transition)
+                                   transition=Document.signing_signed_transition,
+                                   transition_params={'filedata': b64encode(SIGNED_FILE_DATA)})
 
         self.assertEquals(Document.signed_state,
                           api.content.get_state(obj=self.document))
+        self.assertEqual(2, self.document.get_current_version_id(missing_as_zero=True))
+        self.assertEqual(SIGNED_FILE_DATA, self.document.file.data)
 
         api.content.transition(obj=self.document,
                                transition=Document.signed_draft_transition)
 
         self.assertEquals(Document.active_state,
                           api.content.get_state(obj=self.document))
+        self.assertEqual(3, self.document.get_current_version_id(missing_as_zero=True))
+        self.assertEqual(FINALIZED_FILE_DATA, self.document.file.data)
 
     @browsing
-    def test_signing_document_cannot_be_edited(self, browser):
+    @requests_mock.Mocker()
+    def test_signing_document_cannot_be_edited(self, browser, mocker):
         self.activate_feature('sign')
+        mocker.post(re.compile('/signing-jobs'), json=DEFAULT_MOCK_RESPONSE)
         self.login(self.administrator, browser)
         browser.open(self.document, view='edit')
 
@@ -290,8 +374,10 @@ class TestDocumentWorkflow(IntegrationTestCase):
         with self.assertRaises(Unauthorized):
             browser.open(self.document, view='edit')
 
-    def test_signing_document_cannot_be_checked_out(self):
+    @requests_mock.Mocker()
+    def test_signing_document_cannot_be_checked_out(self, mocker):
         self.activate_feature('sign')
+        mocker.post(re.compile('/signing-jobs'), json=DEFAULT_MOCK_RESPONSE)
         self.login(self.administrator)
         self.assertTrue(self.document.is_checkout_permitted())
 
@@ -301,8 +387,10 @@ class TestDocumentWorkflow(IntegrationTestCase):
         self.assertFalse(self.document.is_checkout_permitted())
 
     @browsing
-    def test_signed_document_cannot_be_edited(self, browser):
+    @requests_mock.Mocker()
+    def test_signed_document_cannot_be_edited(self, browser, mocker):
         self.activate_feature('sign')
+        mocker.post(re.compile('/signing-jobs'), json=DEFAULT_MOCK_RESPONSE)
         self.login(self.administrator, browser)
         browser.open(self.document, view='edit')
 
@@ -311,14 +399,17 @@ class TestDocumentWorkflow(IntegrationTestCase):
 
         with as_internal_workflow_transition():
             api.content.transition(obj=self.document,
-                                   transition=Document.signing_signed_transition)
+                                   transition=Document.signing_signed_transition,
+                                   transition_params={'filedata': b64encode('<DATA>')})
 
         browser.exception_bubbling = True
         with self.assertRaises(Unauthorized):
             browser.open(self.document, view='edit')
 
-    def test_signed_document_cannot_be_checked_out(self):
+    @requests_mock.Mocker()
+    def test_signed_document_cannot_be_checked_out(self, mocker):
         self.activate_feature('sign')
+        mocker.post(re.compile('/signing-jobs'), json=DEFAULT_MOCK_RESPONSE)
         self.login(self.administrator)
         self.assertTrue(self.document.is_checkout_permitted())
 
@@ -327,6 +418,7 @@ class TestDocumentWorkflow(IntegrationTestCase):
 
         with as_internal_workflow_transition():
             api.content.transition(obj=self.document,
-                                   transition=Document.signing_signed_transition)
+                                   transition=Document.signing_signed_transition,
+                                   transition_params={'filedata': b64encode('<DATA>')})
 
         self.assertFalse(self.document.is_checkout_permitted())
--- opengever/document/transition.py
+++ opengever/document/transition.py
@@ -1,10 +1,20 @@
+from base64 import b64decode
 from opengever.base.transition import ITransitionExtender
 from opengever.base.transition import TransitionExtender
+from opengever.document import _
 from opengever.document.document import IDocumentSchema
+from opengever.document.interfaces import ICheckinCheckoutManager
+from opengever.ogds.models.service import ogds_service
+from opengever.sign.sign import Signer
 from plone import api
+from plone.supermodel.model import Schema
+from zope import schema
 from zope.component import adapter
+from zope.component import getMultiAdapter
+from zope.i18n import translate
 from zope.interface import implementer
 from zope.publisher.interfaces.browser import IBrowserRequest
+import os
 
 
 @implementer(ITransitionExtender)
@@ -15,3 +25,93 @@ class DocumentFinalizeTransitionExtender(TransitionExtender):
         """Set finalizer
         """
         self.context.finalizer = api.user.get_current().getId()
+
+
+@implementer(ITransitionExtender)
+@adapter(IDocumentSchema, IBrowserRequest)
+class DocumentFinalSigningTransitionExtender(TransitionExtender):
+
+    def after_transition_hook(self, *args, **kwargs):
+        """Start signing process
+        """
+        actor = ogds_service().fetch_user(api.user.get_current().id)
+        if not actor or not actor.email:
+            raise NoEmailError()
+
+        Signer(self.context).start_signing(signers=[actor.email])
+
+
+@implementer(ITransitionExtender)
+@adapter(IDocumentSchema, IBrowserRequest)
+class DocumentDraftSigningTransitionExtender(TransitionExtender):
+
+    def after_transition_hook(self, *args, **kwargs):
+        finalize_transition_extender = getMultiAdapter(
+            (self.context, self.request),
+            ITransitionExtender,
+            name="document-transition-finalize")
+
+        finalize_transition_extender.after_transition_hook(*args, **kwargs)
+
+        signing_transition_extender = getMultiAdapter(
+            (self.context, self.request),
+            ITransitionExtender,
+            name="document-transition-final-signing")
+
+        signing_transition_extender.after_transition_hook(*args, **kwargs)
+
+
+@implementer(ITransitionExtender)
+@adapter(IDocumentSchema, IBrowserRequest)
+class DocumentSigningFinalTransitionExtender(TransitionExtender):
+
+    def after_transition_hook(self, *args, **kwargs):
+        Signer(self.context).abort_signing()
+
+
+@implementer(ITransitionExtender)
+@adapter(IDocumentSchema, IBrowserRequest)
+class DocumentSignedDraftTransitionExtender(TransitionExtender):
+
+    def after_transition_hook(self, *args, **kwargs):
+        finalized_version_number = self.context.get_current_version_id(
+            missing_as_zero=True) - 1
+
+        manager = getMultiAdapter((self.context, self.request),
+                                  ICheckinCheckoutManager)
+        manager.revert_to_version(finalized_version_number)
+        Signer(self.context).clear_metadata()
+
+
+class ISignSchema(Schema):
+    filedata = schema.ASCIILine(
+        title=_('label_signed_pdf_filedata',
+                default='Base64 encoded signed pdf file data'),
+        required=True,
+    )
+
+
+@implementer(ITransitionExtender)
+@adapter(IDocumentSchema, IBrowserRequest)
+class DocumentSigningSignedTransitionExtender(TransitionExtender):
+
+    schemas = [ISignSchema]
+
+    def after_transition_hook(self, transition, disable_sync, transition_params):
+        filedata = b64decode(transition_params.get('filedata'))
+        comment = _(u'label_document_signed', default=u'Document signed')
+        self.context.update_file(
+            data=filedata,
+            content_type=u'application/pdf',
+            filename=self.get_file_name(),
+            create_version=True,
+            comment=translate(comment, context=self.request))
+
+    def get_file_name(self):
+        filename, ext = os.path.splitext(self.context.get_filename())
+        return u'{}.pdf'.format(filename)
+
+
+class NoEmailError(Exception):
+    """The current user does not have an email defined.
+    """
