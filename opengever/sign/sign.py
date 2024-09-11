fbb670c52025d985c3ef0341d73f10f949e0df64
Elio Schmutz <e.schmutz@4teamwork.ch>
2024-09-11 11:31:14 +0200

    Implement Signer object

---
+++ opengever/sign/sign.py
@@ -0,0 +1,79 @@
+from base64 import urlsafe_b64decode
+from base64 import urlsafe_b64encode
+from opengever.base.security import as_internal_workflow_transition
+from opengever.document.document import Document
+from opengever.sign.client import SignServiceClient
+from opengever.sign.storage import MetadataStorage
+from opengever.sign.token import TokenManager
+from plone import api
+from plone.dexterity.utils import safe_utf8
+from requests.exceptions import ConnectionError
+
+
+class Signer(object):
+    """Provides all necessary methods to sign an objects through an external
+    service.
+    """
+
+    def __init__(self, context):
+        self.context = context
+        self.token_manager = TokenManager(context)
+        self.metadata_storage = MetadataStorage(context)
+
+    def issue_token(self):
+        return urlsafe_b64encode(self.token_manager.issue_token())
+
+    def validate_token(self, token):
+        return self.token_manager.validate_token(urlsafe_b64decode(safe_utf8(token)))
+
+    def invalidate_token(self):
+        self.token_manager.invalidate_token()
+
+    def start_signing(self, signers):
+        token = self.issue_token()
+        response = SignServiceClient().queue_signing(self.context, token, signers)
+        response = response.json()
+        self.metadata_storage.store(
+            userid=api.user.get_current().id,
+            version=self.context.get_current_version_id(missing_as_zero=True),
+            signers=signers,
+            job_id=response.get('id'),
+            redirect_url=response.get('redirect_url'),
+        )
+        return token
+
+    def complete_signing(self, signed_pdf_data):
+        with as_internal_workflow_transition():
+            api.content.transition(
+                obj=self.context,
+                transition=Document.signing_signed_transition,
+                transition_params={'filedata': signed_pdf_data})
+
+    def abort_signing(self):
+        self.invalidate_token()
+        signing_data = self.metadata_storage.read()
+        try:
+            SignServiceClient().abort_signing(signing_data.get('job_id'))
+        except ConnectionError:
+            # The user should always be able to abort the job
+            pass
+
+        self.clear_metadata()
+
+    def clear_metadata(self):
+        self.metadata_storage.clear()
+
+    def adopt_issuer(self):
+        userid = self.metadata_storage.read().get('userid')
+        user = api.user.get(userid=userid)
+        if not user:
+            raise IssuerNotFound()
+        return api.env.adopt_user(user=user)
+
+    def serialize(self):
+        return self.metadata_storage.read()
+
+
+class IssuerNotFound(Exception):
+    """Sign issuer not found.
+    """
---
+++ opengever/sign/tests/test_sign.py
@@ -0,0 +1,90 @@
+from base64 import b64encode
+from datetime import datetime
+from ftw.testing import freeze
+from opengever.document.document import Document
+from opengever.document.versioner import Versioner
+from opengever.sign.sign import Signer
+from opengever.sign.token import InvalidToken
+from opengever.testing import IntegrationTestCase
+from plone import api
+import re
+import requests_mock
+
+
+FROZEN_NOW = datetime(2024, 2, 18, 15, 45)
+
+DEFAULT_MOCK_RESPONSE = {
+    'id': '1',
+    'redirect_url': 'http://external.example.org/signing-requests/123',
+}
+
+
+@requests_mock.Mocker()
+class TestSigning(IntegrationTestCase):
+
+    features = ['sign']
+
+    def test_store_signing_document_metadata_when_starting_sign_process(self, mocker):
+        self.login(self.regular_user)
+
+        mocker.post(re.compile('/signing-jobs'), json=DEFAULT_MOCK_RESPONSE)
+        signer = Signer(self.document)
+
+        with freeze(FROZEN_NOW):
+            signer.start_signing(['foo.bar@example.com'])
+
+        self.assertDictEqual(
+            {
+                'created': FROZEN_NOW,
+                'job_id': '1',
+                'redirect_url': 'http://external.example.org/signing-requests/123',
+                'signers': ['foo.bar@example.com'],
+                'userid': 'regular_user',
+                'version': 0
+            }, signer.serialize())
+
+    def test_can_issue_and_invalidate_tokens(self, mocker):
+        self.login(self.regular_user)
+
+        signer = Signer(self.document)
+        token = signer.issue_token()
+        self.assertTrue(signer.validate_token(token))
+
+        signer.invalidate_token()
+        with self.assertRaises(InvalidToken):
+            signer.validate_token(token)
+
+    def test_can_abort_signing_process(self, mocker):
+        self.login(self.regular_user)
+        mocker.post(re.compile('/signing-jobs'), json=DEFAULT_MOCK_RESPONSE)
+        delete_mocker = mocker.delete(re.compile('/signing-jobs/1'))
+
+        signer = Signer(self.document)
+        token = signer.start_signing(['foo.bar@example.com'])
+        signer.abort_signing()
+
+        # current token should be invalidated
+        with self.assertRaises(InvalidToken):
+            signer.validate_token(token)
+
+        # signing-job should be removed
+        self.assertEqual(1, delete_mocker.call_count)
+
+        # metadata-content should be cleared
+        self.assertEqual({}, signer.serialize())
+
+    def test_can_complete_signing_process(self, mocker):
+        self.login(self.regular_user)
+        mocker.post(re.compile('/signing-jobs'), json=DEFAULT_MOCK_RESPONSE)
+        api.content.transition(obj=self.document,
+                               transition=Document.draft_signing_transition)
+
+        self.assertEqual(0, self.document.get_current_version_id(missing_as_zero=True))
+
+        signer = Signer(self.document)
+        signer.complete_signing(b64encode('<DATA>'))
+
+        self.assertEqual(1, self.document.get_current_version_id(missing_as_zero=True))
+
+        signed_version = Versioner(self.document).retrieve(1)
+        self.assertEqual(u'<DATA>', signed_version.file.data)
