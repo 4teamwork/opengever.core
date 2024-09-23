230251a05808f06390d7a49937a3e3595f250ed0
Elio Schmutz <e.schmutz@4teamwork.ch>
2024-09-23 11:57:06 +0200

    Add email_to_userid lookup utils function

--- opengever/sign/utils.py
+++ opengever/sign/utils.py
@@ -1,6 +1,15 @@
+from opengever.ogds.models.user import User
 from opengever.sign.interfaces import ISignSettings
 from plone import api
+from sqlalchemy import func
 
 
 def is_sign_feature_enabled():
     return api.portal.get_registry_record('is_feature_enabled', interface=ISignSettings)
+
+
+def email_to_userid(email):
+    if not email:
+        return ''
+    user = User.query.filter(func.lower(User.email) == email.lower()).first()
+    return user.userid if user else ''
