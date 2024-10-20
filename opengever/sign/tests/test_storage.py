0d91f2851f6937ac930f5db49a1f2acdb57918ce
Elio Schmutz <e.schmutz@4teamwork.ch>
2024-10-20 11:13:06 +0200

    Add PendingSigningJobStorage object

--- opengever/sign/tests/test_storage.py
+++ opengever/sign/tests/test_storage.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from ftw.testing import freeze
 from opengever.sign.storage import MetadataStorage
+from opengever.sign.storage import PendingSigningJobStorage
 from opengever.testing import IntegrationTestCase
 
 
@@ -31,3 +32,18 @@ class TestMetadataStorage(IntegrationTestCase):
                 'version': 1
             },
             storage.read())
+
+
+class TestPendingSigningJobStorage(IntegrationTestCase):
+    def test_can_store_and_load_data(self):
+        self.login(self.regular_user)
+        storage = PendingSigningJobStorage(self.document)
+        storage.store({'userid': 'foo.bar'})
+
+        self.assertDictEqual({'userid': 'foo.bar'}, storage.load())
+
+    def test_data_is_stored_on_the_context(self):
+        self.login(self.regular_user)
+        PendingSigningJobStorage(self.subdocument).store({'userid': 'foo.bar'})
+
+        self.assertIsNone(PendingSigningJobStorage(self.document).load())
