ebc882fc8d21838328cc3d7752cc4f5279bf0760
Elio Schmutz <e.schmutz@4teamwork.ch>
2024-10-21 16:03:24 +0200

    wip

--- opengever/sign/tests/test_storage.py
+++ opengever/sign/tests/test_storage.py
@@ -4,6 +4,3 @@ from opengever.testing import IntegrationTestCase
 class TestPendingSigningJobStorage(IntegrationTestCase):
     def test_can_store_and_load_data(self):
         self.login(self.regular_user)
-
-    def test_data_is_stored_on_the_context(self):
-        self.login(self.regular_user)
