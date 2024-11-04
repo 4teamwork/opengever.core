bd7d41a3b62803c43573fceceaf7f18682b0544a
Elio Schmutz <e.schmutz@4teamwork.ch>
2024-11-04 14:38:33 +0100

    Remove sign feature flag

--- opengever/sign/utils.py
+++ opengever/sign/utils.py
@@ -1,6 +1,2 @@
-from opengever.sign.interfaces import ISignSettings
-from plone import api
-
-
 def is_sign_feature_enabled():
-    return api.portal.get_registry_record('is_feature_enabled', interface=ISignSettings)
+    return True
