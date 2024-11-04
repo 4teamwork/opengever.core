c812670a8e5a1e31b73c3465cc45ec71f7aa39d1
Elio Schmutz <e.schmutz@4teamwork.ch>
2024-11-04 09:14:57 +0100

    Remove token manager

--- opengever/sign/sign.py
+++ opengever/sign/sign.py
@@ -1,12 +1,12 @@
-from base64 import urlsafe_b64decode
+# from base64 import urlsafe_b64decode
 from base64 import urlsafe_b64encode
 # from opengever.base.security import as_internal_workflow_transition
 # from opengever.document.document import Document
 from opengever.sign.client import SignServiceClient
 from opengever.sign.storage import MetadataStorage
-from opengever.sign.token import TokenManager
+# from opengever.sign.token import TokenManager
 from plone import api
-from plone.dexterity.utils import safe_utf8
+# from plone.dexterity.utils import safe_utf8
 from requests.exceptions import ConnectionError
 
 
@@ -17,17 +17,19 @@ class Signer(object):
 
     def __init__(self, context):
         self.context = context
-        self.token_manager = TokenManager(context)
+        # self.token_manager = TokenManager(context)
         self.metadata_storage = MetadataStorage(context)
 
     def issue_token(self):
         return urlsafe_b64encode(self.token_manager.issue_token())
 
     def validate_token(self, token):
-        return self.token_manager.validate_token(urlsafe_b64decode(safe_utf8(token)))
+        pass
+        # return self.token_manager.validate_token(urlsafe_b64decode(safe_utf8(token)))
 
     def invalidate_token(self):
-        self.token_manager.invalidate_token()
+        pass
+        # self.token_manager.invalidate_token()
 
     def start_signing(self, signers):
         token = self.issue_token()
