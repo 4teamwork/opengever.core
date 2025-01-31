dfa033a17cb8cb358fa5131e003578f0df5e209c
Elio Schmutz <e.schmutz@4teamwork.ch>
2025-01-31 11:03:55 +0100

    Properly raise http errors when communicating with ogsign

--- opengever/sign/client.py
+++ opengever/sign/client.py
@@ -13,7 +13,7 @@ class SignServiceClient(object):
     def queue_signing(self, document, token, signers, editors):
         bumblebee_service = IBumblebeeServiceV3(getRequest())
 
-        return requests.post(
+        resp = requests.post(
             '{}/signing-jobs/'.format(self.sign_service_url),
             headers={"Accept": "application/json"},
             json={'access_token': token,
@@ -25,7 +25,10 @@ class SignServiceClient(object):
                   'title': document.title_or_id(),
                   'signers': signers,
                   'editors': editors,
-                  }).json()
+                  })
+
+        resp.raise_for_status()
+        return resp.json()
 
     def abort_signing(self, job_id):
         if job_id is None:
