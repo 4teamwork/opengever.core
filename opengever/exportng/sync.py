35c5b6a37a91026a74ed5b4c53ce6ba1edfb0b49
Thomas Buchberger <t.buchberger@4teamwork.ch>
2025-02-06 11:57:17 +0100

    Refactor CatalogSyncer

--- opengever/exportng/sync.py
+++ opengever/exportng/sync.py
@@ -32,6 +32,8 @@ Attribute = namedtuple(
     ['name', 'col_name', 'col_type', 'getter'],
 )
 
+SQL_CHUNK_SIZE = 5000
+BLOB_VERSION_KEY = 'CloneNamedFileBlobs/opengever.document.document.IDocumentSchema.file'
 CACHE = {}
 
 
@@ -211,54 +213,68 @@ class CatalogSyncer(object):
 
     def __init__(self, query=None):
         self.base_query = query or {}
+        self.rtool = api.portal.get_tool('portal_repository')
+        self.hstool = api.portal.get_tool('portal_historiesstorage')
+        self._catalog_items = None
+        self._sql_items = None
 
-    def get_catalog_items(self):
-        query = dict(self.base_query, **self.query)
-        ct = api.portal.get_tool('portal_catalog')
-        return ct.unrestrictedSearchResults(query)
+    @property
+    def catalog_items(self):
+        if self._catalog_items is None:
+            query = dict(self.base_query, **self.query)
+            ct = api.portal.get_tool('portal_catalog')
+            self._catalog_items = {
+                getattr(item, self.catalog_key): item
+                for item in ct.unrestrictedSearchResults(query)
+            }
+        return self._catalog_items
 
-    def get_sql_items(self):
-        table = metadata.tables[self.table]
-        stmt = table.select().where(table.c._deleted == false())
-        with engine.connect() as conn:
-            return conn.execute(stmt).fetchall()
+    @property
+    def sql_items(self):
+        if self._sql_items is None:
+            table = metadata.tables[self.table]
+            stmt = table.select().where(table.c._deleted == false())
+            with engine.connect() as conn:
+                self._sql_items = {
+                    getattr(item, self.sql_key): item
+                    for item in conn.execute(stmt).fetchall()
+                }
+        return self._sql_items
 
     def sync(self):
         logger.info('Processing %s...', self.table)
-        self.init_sync()
-        catalog_items = self.get_catalog_items()
-        sql_items = self.get_sql_items()
-        catalog_keys = set([
-            getattr(item, self.catalog_key) for item in catalog_items])
-        sql_keys = set([getattr(item, self.sql_key) for item in sql_items])
-        catalog_items_by_key = {
-            getattr(item, self.catalog_key): item for item in catalog_items}
-        sql_items_by_key = {
-            getattr(item, self.sql_key): item for item in sql_items}
+        catalog_keys = set(self.catalog_items.keys())
+        sql_keys = set(self.sql_items.keys())
 
         added = catalog_keys - sql_keys
         deleted = sql_keys - catalog_keys
         existing = catalog_keys & sql_keys
-
         modified = set()
         for key in existing:
             if (
-                catalog_items_by_key[key].modified.asdatetime().replace(tzinfo=None)
-                != sql_items_by_key[key].modified
+                self.catalog_items[key].modified.asdatetime().replace(tzinfo=None)
+                > self.sql_items[key]._modified_at
             ):
                 modified.add(key)
 
+        self.add_objects(added)
+        self.update_objects(modified)
+        self.delete_objects(deleted)
+
+    def add_objects(self, added):
         total_time = timer()
         lap_time = timer()
         counter = 0
 
         inserts = []
+        version_inserts = []
         added_len = len(added)
+        logger.info('Adding %s %s...', added_len, self.table)
         for key in added:
-            item = catalog_items_by_key[key]
+            item = self.catalog_items[key]
             obj = item._unrestrictedGetObject()
             inserts.append(self.get_values(obj))
-            self.post_insert_obj(obj)
+            version_inserts.extend(self.get_versions(obj))
             counter += 1
             if counter % 100 == 0:
                 logger.info(
@@ -267,37 +283,73 @@ class CatalogSyncer(object):
                 )
         logger.info('Processed %d items in %s.', counter, next(total_time))
 
-        table = metadata.tables[self.table]
-        with engine.connect() as conn:
-            for chunk in chunks(inserts, 5000):
-                conn.execute(table.insert(), chunk)
-        logger.info('Added %s: %s', table, len(added))
-        self.post_insert()
+        if inserts:
+            table = metadata.tables[self.table]
+            with engine.connect() as conn:
+                for chunk in chunks(inserts, SQL_CHUNK_SIZE):
+                    conn.execute(table.insert(), chunk)
+        logger.info('Added %s: %s', self.table, len(added))
+
+        if version_inserts:
+            table = metadata.tables[self.versions_table]
+            with engine.connect() as conn:
+                for chunk in chunks(version_inserts, SQL_CHUNK_SIZE):
+                    conn.execute(table.insert(), chunk)
+            logger.info('Added %s: %s', table, len(version_inserts))
+
+    def update_objects(self, modified):
+        total_time = timer()
+        lap_time = timer()
+        counter = 0
 
         updates = []
+        modified_len = len(modified)
+        logger.info('Modifying %s %s...', modified_len, self.table)
         for key in modified:
-            item = catalog_items_by_key[key]
+            item = self.catalog_items[key]
             obj = item._unrestrictedGetObject()
             updates.append(rename_dict_key(self.get_values(obj), self.sql_key, 'b_key'))
+            counter += 1
+            if counter % 100 == 0:
+                logger.info(
+                    '%d of %d items processed (%.2f %%), last batch in %s',
+                    counter, modified_len, 100 * float(counter) / modified_len, next(lap_time),
+                )
+        logger.info('Processed %d items in %s.', counter, next(total_time))
+
         if updates:
+            table = metadata.tables[self.table]
             with engine.connect() as conn:
                 conn.execute(table.update().where(getattr(table.c, self.sql_key) == bindparam('b_key')), updates)
-        logger.info('Updated %s: %s', table, len(modified))
+        logger.info('Updated %s: %s', self.table, len(modified))
+
+    def delete_objects(self, deleted):
+        total_time = timer()
+        lap_time = timer()
+        counter = 0
 
         deletes = []
+        deleted_len = len(deleted)
+        logger.info('Deleting %s %s...', deleted_len, self.table)
         for key in deleted:
             deletes.append({'b_key': key, '_deleted': True})
+            counter += 1
+            if counter % 100 == 0:
+                logger.info(
+                    '%d of %d items processed (%.2f %%), last batch in %s',
+                    counter, deleted_len, 100 * float(counter) / deleted_len, next(lap_time),
+                )
+        logger.info('Processed %d items in %s.', counter, next(total_time))
+
         if deletes:
+            table = metadata.tables[self.table]
             with engine.connect() as conn:
                 conn.execute(table.update().where(
                     getattr(table.c, self.sql_key) == bindparam('b_key')), deletes)
-        logger.info('Deleted %s: %s', table, len(deleted))
+        logger.info('Deleted %s: %s', self.table, len(deleted))
 
     def get_values(self, obj):
         data = {}
-        # serializer = queryMultiAdapter(
-        #     (self.context, self.request), ISerializeToJson)
-        # obj_data = serializer(include_items=False)
         for attr in self.mapping:
             if attr.getter is not None:
                 value = attr.getter(obj, attr.name)
@@ -318,14 +370,8 @@ class CatalogSyncer(object):
             fields.update(getFields(schema))
         return fields
 
-    def init_sync(self):
-        return
-
-    def post_insert_obj(self, obj):
-        return
-
-    def post_insert(self):
-        return
+    def get_versions(self, obj):
+        return []
 
 
 class FileplanEntrySyncer(CatalogSyncer):
@@ -441,24 +487,19 @@ class DocumentSyncer(CatalogSyncer):
         Attribute('comment', 'versdesc', 'varchar', None),
     ]
 
-    def init_sync(self):
-        self.rtool = api.portal.get_tool('portal_repository')
-        self.hstool = api.portal.get_tool('portal_historiesstorage')
-        self._doc_version_inserts = []
-
-    def post_insert_obj(self, obj):
+    def get_versions(self, obj):
+        versions = []
         uid = obj.UID()
         history = self.rtool.getHistory(obj)
         if len(history) > 0:
             obj, history_id = dereference(obj=obj)
             for version in range(len(history)):
                 vdata = self.hstool.retrieve(history_id, selector=version)
-                if 'CloneNamedFileBlobs/opengever.document.document.IDocumentSchema.file' not in vdata.referenced_data:
+                if BLOB_VERSION_KEY not in vdata.referenced_data:
                     continue
-                filepath = vdata.referenced_data[
-                    'CloneNamedFileBlobs/opengever.document.document.IDocumentSchema.file'].committed()
+                filepath = vdata.referenced_data[BLOB_VERSION_KEY].committed()
                 filesize = os.stat(filepath).st_size
-                self._doc_version_inserts.append({
+                versions.append({
                     'objexternalkey': uid,
                     'version': version,
                     'filepath': filepath,
@@ -471,7 +512,7 @@ class DocumentSyncer(CatalogSyncer):
         else:
             data = get_filedata(obj, None)
             if data is not None:
-                self._doc_version_inserts.append({
+                versions.append({
                     'objexternalkey': uid,
                     'version': 0,
                     'filepath': data['filepath'],
@@ -481,13 +522,7 @@ class DocumentSyncer(CatalogSyncer):
                     'verschangedat': as_datetime(obj, 'modified'),
                     'versdesc': '',
                 })
-        return
-
-    def post_insert(self):
-        table = metadata.tables[self.versions_table]
-        with engine.connect() as conn:
-            conn.execute(table.insert(), self._doc_version_inserts)
-        logger.info('Added %s: %s', table, len(self._doc_version_inserts))
+        return versions
 
 
 class OGDSSyncer(object):
