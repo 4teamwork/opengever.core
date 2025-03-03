dd893499b1511fd810064c7f023fe4ae0794d1aa
Elio Schmutz <e.schmutz@4teamwork.ch>
2025-03-03 15:34:11 +0100

    Sign service uses SIGN_SERVICE_GEVER_URL environment variable to construct the callback links.

--- opengever/sign/client.py
+++ opengever/sign/client.py
@@ -1,5 +1,6 @@
 from ftw.bumblebee.interfaces import IBumblebeeServiceV3
 from os import environ
+from plone import api
 from zope.globalrequest import getRequest
 import requests
 
@@ -10,17 +11,29 @@ class SignServiceClient(object):
     def sign_service_url(self):
         return environ.get('SIGN_SERVICE_URL', '').strip('/')
 
+    @property
+    def sign_service_gever_url(self):
+        """An URL which point to gever and is accessible by the sign service.
+        """
+        sign_gever_url = environ.get('SIGN_SERVICE_GEVER_URL', '').strip('/')
+        return sign_gever_url or api.portal.get().absolute_url()
+
+    def construct_callback_url(self, url):
+        return url.replace(api.portal.get().absolute_url(),
+                           self.sign_service_gever_url)
+
     def queue_signing(self, document, token, editors):
         bumblebee_service = IBumblebeeServiceV3(getRequest())
+        document_url = self.construct_callback_url(document.absolute_url())
 
         resp = requests.post(
             '{}/signing-jobs/'.format(self.sign_service_url),
             headers={"Accept": "application/json"},
             json={'access_token': token,
-                  'document_url': document.absolute_url(),
+                  'document_url': document_url,
                   'download_url': bumblebee_service.get_download_url(document),
-                  'upload_url': '{}/@upload-signed-pdf'.format(document.absolute_url()),
-                  'update_url': '{}/@update-pending-signing-job'.format(document.absolute_url()),
+                  'upload_url': '{}/@upload-signed-pdf'.format(document_url),
+                  'update_url': '{}/@update-pending-signing-job'.format(document_url),
                   'document_uid': document.UID(),
                   'title': document.title_or_id(),
                   'editors': editors,
