9b110bc5d4101dc2629341dafef0a21a33c27080
Thomas Buchberger <t.buchberger@4teamwork.ch>
2025-03-06 09:58:59 +0100

    Include username in mapping as well

--- opengever/exportng/utils.py
+++ opengever/exportng/utils.py
@@ -8,6 +8,11 @@ def userid_to_email(userid):
     userid_email_mapping = CACHE.get('userid_email_mapping', None)
     if userid_email_mapping is None:
         users = ogds_service().all_users()
-        userid_email_mapping = {user.userid: user.email for user in users}
+        userid_email_mapping = {}
+        for user in users:
+            if user.email:
+                userid_email_mapping[user.userid] = user.email
+                if user.userid != user.username:
+                    userid_email_mapping[user.username] = user.email
         CACHE['userid_email_mapping'] = userid_email_mapping
     return userid_email_mapping.get(userid, userid)
