cb53f892ac1f48e9ba2c14619f45039747964982
Abdelrahman Moustafa <abdelrahman.moustafa@4teamwork.ch>
2025-05-27 11:01:10 +0200

    Enable attach attachments to submitted ris proposal

---
+++ opengever/core/upgrades/20250526150229_enable_edit_ris_proposal_in_submitted_state/upgrade.py
@@ -0,0 +1,13 @@
+from ftw.upgrade import UpgradeStep
+from opengever.core.upgrade import NightlyWorkflowSecurityUpdater
+
+
+class EnableEditRisProposalInSubmittedState(UpgradeStep):
+    """Enable Edit Ris  Proposal In Submitted State.
+    """
+
+    def __call__(self):
+        self.install_upgrade_profile()
+        with NightlyWorkflowSecurityUpdater(reindex_security=False) as updater:
+            updater.update(
+                ['opengever_ris_proposal_workflow'])
