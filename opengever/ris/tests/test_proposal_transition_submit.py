277b49f2db3b85894287c905909bd7bf06261a35
Abdelrahman Moustafa <abdelrahman.moustafa@4teamwork.ch>
2025-05-27 11:05:31 +0200

    Revert "Enable attach attachments to submitted ris proposal"
    This reverts commit cb53f892ac1f48e9ba2c14619f45039747964982.

--- opengever/ris/tests/test_proposal_transition_submit.py
+++ opengever/ris/tests/test_proposal_transition_submit.py
@@ -6,7 +6,6 @@ from ftw.testbrowser.pages import statusmessages
 from opengever.base.role_assignments import RoleAssignmentManager
 from opengever.base.role_assignments import SharingRoleAssignment
 from opengever.testing import IntegrationTestCase
-import json
 
 
 class TestRisProposalTransitionSubmit(IntegrationTestCase):
@@ -75,41 +74,3 @@ class TestRisProposalTransitionSubmit(IntegrationTestCase):
         statusmessages.assert_message('Item state changed.')
 
         self.assert_workflow_state('proposal-state-submitted', self.ris_proposal)
-
-    @browsing
-    def test_can_submit_attachments_to_submitted_proposal(self, browser):
-        self.login(self.manager, browser)
-        ris_proposal = create(
-            Builder('ris_proposal')
-            .within(self.dossier)
-            .having(document=self.document)
-            .in_state('proposal-state-submitted')
-        )
-        attachment_1 = create(
-            Builder('document')
-            .within(self.dossier)
-            .titled(u'proposal attachment 1')
-        )
-        attachment_2 = create(
-            Builder('document')
-            .within(self.dossier)
-            .titled(u'proposal attachment 2')
-        )
-
-        # assert ris proposal has no attachments
-        self.assertEqual(len(ris_proposal.attachments), 0)
-        data = json.dumps({
-            "attachments": [
-                attachment_1.absolute_url(),
-                attachment_2.absolute_url()
-            ]
-        })
-        # patch request to update attachments
-        browser.open(
-            ris_proposal.absolute_url(),
-            data,
-            method='PATCH',
-            headers=self.api_headers
-        )
-        # assert ris proposal attachments after patch update request
-        self.assertEqual(len(ris_proposal.attachments), 2)
