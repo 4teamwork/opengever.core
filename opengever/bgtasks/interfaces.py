1399fad52d0eda196484850d684be8bc41cc8967
Thomas Buchberger <t.buchberger@4teamwork.ch>
2026-07-02 16:14:16 +0200

    Fix indention

--- opengever/bgtasks/interfaces.py
+++ opengever/bgtasks/interfaces.py
@@ -7,6 +7,6 @@ class IBackgroundTaskSettings(Interface):
     is_feature_enabled = schema.Bool(
         title=u'Enable background tasks',
         description=u'Whether background tasks are queued and executed '
-                     u'asynchronously by the worker. When disabled, '
-                     u'queue_task() executes tasks synchronously instead.',
+                    u'asynchronously by the worker. When disabled, '
+                    u'queue_task() executes tasks synchronously instead.',
         default=True)
